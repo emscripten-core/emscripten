ade3a58044de2a95197e9efd1ba235610f660baa
alon@honor <none@none>
2010-09-11 23:14:16 -0700

    docs in emscripten.py

--- emscripten.py
+++ emscripten.py
@@ -13,7 +13,17 @@ def emscripten(filename, js_engine, settings):
 
 if __name__ == '__main__':
   if sys.argv.__len__() not in [3,4]:
-    print '''\nEmscripten usage:          emscripten.py INFILE PATH-TO-JS-ENGINE [SETTINGS]\n'''
+    print '''
+Emscripten usage:    emscripten.py INFILE PATH-TO-JS-ENGINE [SETTINGS]
+
+  INFILE must be in human-readable LLVM disassembly form (i.e., as text,
+      not binary).
+  PATH-TO-JS-ENGINE should be a path to the JavaScript engine used to
+      run the compiler (which is in JavaScript itself). You can later use
+      the same engine to run the code, or another one, that is a separate
+      issue.
+  SETTINGS is an optional set of compiler settings, overriding the defaults.
+'''
   else:
     emscripten(sys.argv[1], sys.argv[2],  sys.argv[3] if len(sys.argv) == 4 else "{}")
 
