4a37bdf9e060010d3d4e6b1f6f503a13227f5de8
alon@honor <none@none>
2010-10-31 19:23:10 -0700

    allow both modularized and non-modularized shell.js

--- src/shell.js
+++ src/shell.js
@@ -1,11 +1,14 @@
 
 // Capture the output of this into a variable, if you want
-(function(Module, args) {
-  Module = Module || {};
-  args = args || [];
+//(function(Module, args) {
+//  Module = Module || {};
+//  args = args || [];
 
+// Runs much faster, for some reason
+var Module = {};
+var args = arguments;
   {{BODY}}
 
-  return Module;
-})({}, this.arguments); // Replace parameters as needed
+//  return Module;
+//})({}, this.arguments); // Replace parameters as needed
 
