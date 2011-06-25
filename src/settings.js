77e318392f888b3affe5e4d05520abb13cbff197
max99x <max99x@gmail.com>
2011-06-26 02:17:23 +0300

    Added support for including the whole library.

--- src/settings.js
+++ src/settings.js
@@ -105,6 +105,11 @@ EXPORTED_GLOBALS = []; // Global non-function variables that are explicitly
                        // exported, so they are guaranteed to be
                        // accessible outside of the generated code.
 
+INCLUDE_FULL_LIBRARY = 0; // Whether to include the whole library rather than just the
+                          // functions used by the generated code. This is needed when
+                          // dynamically loading modules that make use of runtime
+                          // library functions that are not used in the main module.
+
 SHOW_LABELS = 0; // Show labels in the generated code
 
 BUILD_AS_SHARED_LIB = 0; // Whether to build the code as a shared library, which
