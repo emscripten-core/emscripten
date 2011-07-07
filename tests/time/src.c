d94b2423aef4593ce2b07725e992b7eb25d5f34c
max99x <max99x@gmail.com>
2011-07-07 07:34:16 +0300

    Added proper clock() test; fixed inverted clock() sign.

--- tests/time/src.c
+++ tests/time/src.c
@@ -91,10 +91,13 @@ int main() {
   asctime_r(tm_ptr, buffer);
   printf("old asctime again: %s", formatted);
 
-  // Verify that clock() is initially 0 and doesn't crash.
-  printf("clock: %d\n", clock());
+  // Verify that clock() advances.
+  clock_t start = clock();
+  printf("clock(start): %d\n", start >= 0);
+  while (clock() - start < 50000); // Poor man's sleep().
+  printf("clock(end): %d\n", clock() - start >= 50000);
 
-  // Verify that ctime_r(x, buf) is equivalent to asctime_r(localtime(x), buf2).
+  // Verify that ctime_r(x, buf) is equivalent to asctime_r(localtime(x), buf).
   time_t t7 = time(0);
   char buffer2[30];
   char buffer3[30];
