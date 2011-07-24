211884f29996dc8beb89d1bcf6b643c1fbf5d6a4
max99x <max99x@gmail.com>
2011-07-24 05:26:25 +0300

    Fixed unistd sleep test to be more flexible.

--- tests/unistd/sleep.c
+++ tests/unistd/sleep.c
@@ -5,16 +5,19 @@
 
 int main() {
   time_t start;
+  time_t taken;
 
   start = time(0);
   printf("sleep(2) ret: %d\n", sleep(2));
-  printf("after sleep(2): %d\n", time(0) - start);
+  taken = time(0) - start;
+  printf("after sleep(2) between 2 and 3: %d\n", taken >= 2 && taken <= 3);
   printf("errno: %d\n", errno);
   errno = 0;
 
   start = time(0);
-  printf("usleep(3000000) ret: %d\n", usleep(3100000));
-  printf("after usleep(3000000): %d\n", time(0) - start);
+  printf("usleep(3000000) ret: %d\n", usleep(3000000));
+  taken = time(0) - start;
+  printf("after usleep(3000000) between 3 and 4: %d\n", taken >= 3 && taken <= 4);
   printf("errno: %d\n", errno);
 
   return 0;
