45cb24500f1680152bca4ede40b7edaf2f9453f8
Alon Zakai <alonzakai@gmail.com>
2011-08-02 11:11:01 -0700

    improve file2json

--- tools/file2json.py
+++ tools/file2json.py
@@ -13,8 +13,15 @@ VARNAME - the variable to store it in (the output will be VARNAME = [...])
 import os, sys
 
 data = open(sys.argv[1], 'r').read()
-sdata = map(lambda x: str(ord(x)), data)
-json = '[' + ','.join(sdata) + ']'
+sdata = map(lambda x: str(ord(x)) + ',', data)
+sdata[-1] = sdata[-1].replace(',', '')
+lined = []
+while len(sdata) > 0:
+  lined += sdata[:30]
+  sdata = sdata[30:]
+  if len(sdata) > 0:
+    lined += ['\n']
+json = '[' + ''.join(lined) + ']'
 
 print 'var ' + sys.argv[2] + '=' + json + ';'
 
