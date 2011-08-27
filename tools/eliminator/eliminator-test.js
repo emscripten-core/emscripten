42e4d9cb7e37d1a79ac928852f8a34a6232e70d7
max99x <max99x@gmail.com>
2011-08-27 20:27:59 +0300

    Updated the eliminator, mainly to support use-before-declaration cases.

--- tools/eliminator/eliminator-test.js
+++ tools/eliminator/eliminator-test.js
@@ -1,12 +1,11 @@
 function f() {
-  var __label__;
+  var unused;
   var x = GLOB[1];
   var y = x + 1;
   var z = y / 2;
   HEAP[123] = z;
 }
 var g = function (a1, a2) {
-  var __label__;
   var a = 1;
   var b = a * 2;
   var c = b - 1;
@@ -16,13 +15,15 @@ var g = function (a1, a2) {
   var ww = 1, www, zzz = 2;
   foo(zzz);
   for (var i = 0; i < 5; i++) {
-    var q = {a:1} + [2,3];
+    var q = {
+      a: 1
+    } + [ 2, 3 ];
   }
   for (var iterator in SOME_GLOBAL) {
     quux(iterator);
   }
   var $0 = HEAP[5];
-  HEAP[myglobal] = 123;
+  MAYBE_HEAP[myglobal] = 123;
   var $1 = $0 < 0;
   if ($1) {
     __label__ = 1;
@@ -38,4 +39,45 @@ var g = function (a1, a2) {
     unquoted: 3,
     4: 5
   };
+};
+function h() {
+  var out;
+  bar(hello);
+  var hello = 5;
+  if (0) {
+    var sb1 = 21;
+  }
+  out = sb1;
+  if (0) {
+    var sb2 = 23;
+  } else {
+    out = sb2;
+  }
+  if (0) {
+    out = sb3;
+  } else {
+    var sb3 = 23;
+  }
+  for (var it = 0; it < 5; it++) {
+    x = y ? x + 1 : 7;
+    var x = -5;
+  }
+  return out;
+}
+function strtok_part(b, j, f) {
+  var a;
+  for (;;) {
+    h = a == 13 ? h : 0;
+    a = HEAP[d + h];
+    if (a == g != 0) break;
+    var h = h + 1;
+    if (a != 0) a = 13;
+  }
+}
+function py() {
+  var $4 = HEAP[__PyThreadState_Current];
+  var $5 = $4 + 12;
+  var $7 = HEAP[$5] + 1;
+  var $8 = $4 + 12;
+  HEAP[$8] = $7;
 }
