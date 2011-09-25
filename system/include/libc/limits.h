dd0230c9cf09a7b19712a4acefe0ae27ea40ea85
Alon Zakai <alonzakai@gmail.com>
2011-09-24 19:04:30 -0700

    fix limits.h

--- system/include/libc/limits.h
+++ system/include/libc/limits.h
@@ -16,7 +16,7 @@
 
 /* if do not have #include_next support, then we
    have to define the limits here. */
-# if !defined __GNUC__ || __GNUC__ < 2
+# if 1 /* XXX Emscripten: force this !defined __GNUC__ || __GNUC__ < 2  */
 
 #  ifndef _LIMITS_H
 #   define _LIMITS_H	1
