c419492d856348a16b08003a094c82caad4433b8
Alon Zakai <alonzakai@gmail.com>
2011-10-02 15:57:23 +0200

    fix fs_base

--- tests/runner.py
+++ tests/runner.py
@@ -2745,7 +2745,7 @@ if 'benchmark' not in str(sys.argv):
           open(filename, 'w').write(src)
         src = 'int main() {return 0;}\n'
         expected = open(path_from_root('tests', 'filesystem', 'output.txt'), 'r').read()
-        self.do_test(src, expected, post_build=addJS)
+        self.do_test(src, expected, post_build=addJS, extra_emscripten_args=['-H', 'libc/fcntl.h,libc/sys/unistd.h'])
       finally:
         INCLUDE_FULL_LIBRARY = 0
 
