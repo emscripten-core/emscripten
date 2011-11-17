c77c200fba4cd0918c8d7cbecb453f772116950b
LCID Fire <lcid-fire@gmx.net>
2011-11-17 20:08:28 +0100

    Remove muting or std outputting stderr.

--- emscripten.py
+++ emscripten.py
@@ -150,7 +150,7 @@ def emscript(infile, settings, outfile):
   s.write(settings)
   s.close()
   compiler = path_from_root('src', 'compiler.js')
-  shared.run_js(shared.COMPILER_ENGINE, compiler, [settings_file, infile], stdout=outfile, stderr=subprocess.STDOUT, cwd=path_from_root('src'))
+  shared.run_js(shared.COMPILER_ENGINE, compiler, [settings_file, infile], stdout=outfile, cwd=path_from_root('src'))
   outfile.close()
 
 
