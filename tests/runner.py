9b375268703182c08c3f64433d3f0cb7a17880bc
Alon Zakai <alonzakai@gmail.com>
2011-12-05 15:43:20 -0800

    fix test runner breakage after test_runtimeload

--- tests/runner.py
+++ tests/runner.py
@@ -4707,7 +4707,7 @@ class %s(T):
     Settings.PROFILE = 0
     Settings.INCLUDE_FULL_LIBRARY = 0
     Settings.BUILD_AS_SHARED_LIB = 0
-    Settings.LIBS_TO_LOAD = []
+    Settings.RUNTIME_LINKED_LIBS = []
     Settings.CATCH_EXIT_CODE = 0
     Settings.TOTAL_MEMORY = Settings.FAST_MEMORY = None
     Settings.EMULATE_UNALIGNED_ACCESSES = Settings.USE_TYPED_ARRAYS == 2 and Building.LLVM_OPTS == 2
