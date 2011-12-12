94476d0fcde4f3b55dd05014c7394ba2d088f076
Alon Zakai <alonzakai@gmail.com>
2011-12-12 15:08:04 -0800

    add missing file test_hello_world_loop.cpp

---
+++ tests/hello_world_loop.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main() {
+  char *original = (char*)"h e l l o ,   w o r l d ! ";
+  char *copy = (char*)malloc(strlen(original));
+  for (int i = 0; i < strlen(original); i += 2) {
+    copy[i/2] = original[i];
+  }
+  copy[strlen(copy)+1] = (int)&original; // force original to be on the stack
+  puts(copy);
+  return 1;
+}
+
