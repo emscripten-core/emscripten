aa5dfee7c6cef6bd51ab93e94a3590b7a4d590a6
Alon Zakai <alonzakai@gmail.com>
2012-01-12 11:20:22 -0800

    some inet.h additions

--- system/include/net/arpa/inet.h
+++ system/include/net/arpa/inet.h
@@ -2,8 +2,9 @@
 
 #include <stdint.h>
 
+uint32_t htonl(uint32_t hostlong);
+uint16_t htons(uint16_t hostshort);
 uint32_t ntohl(uint32_t netlong);
-
 uint16_t ntohs(uint16_t netshort);
 
 int inet_aton(const char *cp, struct in_addr *addr);
