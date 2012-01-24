b066b1fa4bc39528a728bd98ec3134a72c943e8e
Alon Zakai <alonzakai@gmail.com>
2012-01-23 18:45:31 -0800

    reminders

--- tests/runner.py
+++ tests/runner.py
@@ -5550,6 +5550,8 @@ f.close()
     def test_reminder(self):
       assert False, 'Optimize makeGet/SetValue to do 16-bit reads/writes when possible, not just 8'
       assert False, 'Make sure unaligned loads are not done unnecessarily (add some comments and inspect the source)'
+      assert False, 'Why is libcxx/ created in e.g. test_python, with EMCC_DEBUG, when it does not need libcxx?'
+      assert False, 'Make sure Poppler builds with llvm full opts'
 
 elif 'benchmark' in str(sys.argv):
   # Benchmarks. Run them with argument |benchmark|. To run a specific test, do
