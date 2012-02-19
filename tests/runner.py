5606427b7035a599f3a523ff22be22cbb9fbbca5
Alon Zakai <alonzakai@gmail.com>
2012-02-19 11:59:52 -0800

    fix test_structbyval

--- tests/runner.py
+++ tests/runner.py
@@ -2424,7 +2424,7 @@ def process(filename):
           generated = open(os.path.join(self.get_dir(), 'src.cpp.o.js')).read()
           if self.emcc_args is None or self.emcc_args == []: # Optimized code is missing the warning comments
             assert 'Casting a function pointer type to another with a different number of arguments.' in generated, 'Missing expected warning'
-            assert 'void (i32, i32)* ==> void (%struct.point.0*)*' in generated, 'Missing expected warning details'
+            assert 'void (i32, i32)* ==> void (%struct.point*)*' in generated, 'Missing expected warning details'
           return
         raise Exception('We should not have gotten to here!')
 
