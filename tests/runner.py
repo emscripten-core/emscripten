95cd1b8e2068d5ec18d69a06b8222b649b49d1f2
Alon Zakai <alonzakai@gmail.com>
2012-03-05 18:34:51 -0800

    fix i64 comparisons

--- tests/runner.py
+++ tests/runner.py
@@ -689,6 +689,51 @@ if 'benchmark' not in str(sys.argv) and 'sanity' not in str(sys.argv):
 
         self.do_run(src, '*1,1,0,0,1,0*\n')
 
+    def test_i64_cmp2(self):
+        if Settings.USE_TYPED_ARRAYS != 2: return self.skip('full i64 stuff only in ta2')
+        src = r'''
+          #include <inttypes.h>
+          #include <stdio.h>
+
+          typedef int32_t INT32;
+          typedef int64_t INT64;
+          typedef uint8_t UINT8;
+
+          void interface_clock_changed()
+          {
+	          UINT8 m_divshift;
+	          INT32 m_divisor;
+
+	          //INT64 attos = m_attoseconds_per_cycle;
+	          INT64 attos = 279365114840;
+	          m_divshift = 0;
+	          while (attos >= (1UL << 31))
+	          {
+		          m_divshift++;
+		          printf("m_divshift is %i, on %Ld >?= %lu\n", m_divshift, attos, 1UL << 31);
+		          attos >>= 1;
+	          }
+	          m_divisor = attos;
+
+	          printf("m_divisor is %i\n",m_divisor);
+          }
+
+          int main() {
+	          interface_clock_changed();
+	          return 0;
+          }
+        '''
+        self.do_run(src, '''m_divshift is 1, on 279365114840 >?= 2147483648
+m_divshift is 2, on 139682557420 >?= 2147483648
+m_divshift is 3, on 69841278710 >?= 2147483648
+m_divshift is 4, on 34920639355 >?= 2147483648
+m_divshift is 5, on 17460319677 >?= 2147483648
+m_divshift is 6, on 8730159838 >?= 2147483648
+m_divshift is 7, on 4365079919 >?= 2147483648
+m_divshift is 8, on 2182539959 >?= 2147483648
+m_divisor is 1091269979
+''')
+
     def test_i64_double(self):
         if Settings.USE_TYPED_ARRAYS != 2: return self.skip('full i64 stuff only in ta2')
         src = r'''
