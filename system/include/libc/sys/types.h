emscripten-core/emscripten

commit f4018dfb9741d111a5358e918e445c5b1b10a370

Make sure that sys/types.h defines old-style C sized types

system/include/libc/sys/types.h
@@ -134,7 +134,7 @@ typedef	unsigned long ino_t; /* XXX Emscripten */
 #endif
 #endif /*__CYGWIN__*/
 
-#ifdef __MS_types__
+#if defined(__MS_types__) || defined(EMSCRIPTEN)
 typedef unsigned long vm_offset_t;
 typedef unsigned long vm_size_t;
 

tests/runner.py
@@ -3822,6 +3822,11 @@ def process(filename):
       expected = open(path_from_root('tests', 'env', 'output.txt'), 'r').read()
       self.do_run(src, expected)
 
+    def test_systypes(self):
+      src = open(path_from_root('tests', 'systypes', 'src.c'), 'r').read()
+      expected = open(path_from_root('tests', 'systypes', 'output.txt'), 'r').read()
+      self.do_run(src, expected)
+
     def test_getloadavg(self):
       src = r'''
         #include <stdio.h>

tests/systypes/src.c
@@ -1,0 +1,20 @@
+#include <sys/types.h>
+
+// Declare puts manually, we don't want to include any other headers here
+#ifdef __cplusplus
+extern "C"
+#endif
+int puts(const char*);
+
+int main() {
+  int8_t i8 = 0;
+  u_int8_t ui8 = 0;
+  int16_t i16 = 0;
+  u_int16_t ui16 = 0;
+  int32_t i32 = 0;
+  u_int32_t ui32 = 0;
+  int64_t i64 = 0;
+  u_int64_t ui64 = 0;
+  puts("success");
+  return 0;
+}

commit 80bc58af4cf2717fed6fd2aabd627c69c016e988

Don't define u_int64_t and similar types twice
They are already typedef'ed earlier in the file.  This breaks emcc on my
Mac at least.

This was broken by f4018dfb.

system/include/libc/sys/types.h
@@ -141,13 +141,9 @@ typedef unsigned long vm_size_t;
 #define __BIT_TYPES_DEFINED__
 
 typedef signed char int8_t;
-typedef unsigned char u_int8_t;
 typedef short int16_t;
-typedef unsigned short u_int16_t;
 typedef int int32_t;
-typedef unsigned int u_int32_t;
 typedef long long int64_t;
-typedef unsigned long long u_int64_t;
 typedef int32_t register_t;
 #endif /* __MS_types__ */
 