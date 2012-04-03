6fe076df5c55d1601a1b5ba738169f3eef4201f2
Alon Zakai <alonzakai@gmail.com>
2012-04-03 13:27:18 -0700

    update webconsole cleaner tool

--- tools/clean_webconsole.py
+++ tools/clean_webconsole.py
@@ -9,6 +9,7 @@ lines = sys.stdin.read().split('\n')
 for line in lines:
   if line.startswith('['):
     line = line[15:]
+  if line.startswith(('[', ']')):
     line = line.split('@')[0]
   print line
 
