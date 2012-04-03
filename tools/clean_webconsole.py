acc4a44f5dd3dc6269eca282f0aa6b208701927d
Alon Zakai <alonzakai@gmail.com>
2012-04-03 11:10:28 -0700

    add little tool to clean web console logs

---
+++ tools/clean_webconsole.py
@@ -0,0 +1,14 @@
+'''
+Removes timestamp and line info from a webgl log
+'''
+
+import os, sys
+
+lines = sys.stdin.read().split('\n')
+
+for line in lines:
+  if line.startswith('['):
+    line = line[15:]
+    line = line.split('@')[0]
+  print line
+
