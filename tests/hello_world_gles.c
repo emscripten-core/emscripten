448b5333ebdabac059a2481160d707e12afb97ee
Behdad Esfahbod <behdad@behdad.org>
2012-04-05 01:41:16 -0400

    Make hello_world_gles build on the Mac natively
    Useful for testing only.

--- tests/hello_world_gles.c
+++ tests/hello_world_gles.c
@@ -46,16 +46,24 @@
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h>
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <Glut/glut.h>
+#else
 #include <GL/gl.h>
 #include <GL/glut.h>
-
-#ifndef HAVE_BUILTIN_SINCOS
-#include "sincos.h"
 #endif
 
 #define STRIPS_PER_TOOTH 7
 #define VERTICES_PER_TOOTH 34
 #define GEAR_VERTEX_STRIDE 6
+
+static void
+sincos (double a, double *s, double *c)
+{
+  *s = sin (a);
+  *c = cos (a);
+}
 
 /**
  * Struct describing the vertices in triangle strip
