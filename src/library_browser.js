75bf701aa2b9e946ecb1e7f741c3a9b3ede43573
Alon Zakai <alonzakai@gmail.com>
2012-07-21 09:48:20 -0700

    nicer status text

--- src/library_browser.js
+++ src/library_browser.js
@@ -29,9 +29,9 @@ mergeInto(LibraryManager.library, {
           var expected = Browser.mainLoop.expectedBlockers;
           if (remaining) {
             if (remaining < expected) {
-              Module['setStatus']('Please wait.. (' + (expected - remaining) + '/' + expected + ')');
+              Module['setStatus']('Please wait... (' + (expected - remaining) + '/' + expected + ')');
             } else {
-              Module['setStatus']('Please wait..');
+              Module['setStatus']('Please wait...');
             }
           } else {
             Module['setStatus']('');
