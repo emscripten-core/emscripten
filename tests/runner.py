ef417d0e7f4d387e0dc378502651851a5f942a26
Alon Zakai <alonzakai@gmail.com>
2012-09-05 09:56:21 -0700

    add more inet tests

--- tests/runner.py
+++ tests/runner.py
@@ -5013,11 +5013,11 @@ def process(filename):
         #include <arpa/inet.h>
 
         int main() {
-          printf("*%x,%x,%x,%x*\n", htonl(0xa1b2c3d4), htons(0xabcd), ntohl(0x43211234), ntohs(0xbeaf));
+          printf("*%x,%x,%x,%x,%x,%x*\n", htonl(0xa1b2c3d4), htonl(0xfe3572e0), htonl(0x07abcdf0), htons(0xabcd), ntohl(0x43211234), ntohs(0xbeaf));
           return 0;
         }
       '''
-      self.do_run(src, '*d4c3b2a1,cdab,34122143,afbe*')
+      self.do_run(src, '*d4c3b2a1,e07235fe,f0cdab07,cdab,34122143,afbe*')
 
     def test_ctype(self):
       # The bit fiddling done by the macros using __ctype_b_loc requires this.
