574559b420b9525ce2f6e45f4777fae837c71c26
LCID Fire <lcid-fire@gmx.net>
2012-09-22 00:22:38 +0200

    Remove X11 references from ES code. Implement WinCreate in Javascript

--- tests/glbook/Common/esUtil.c
+++ tests/glbook/Common/esUtil.c
@@ -27,13 +27,6 @@
 #include <EGL/egl.h>
 #include "esUtil.h"
 
-#include  <X11/Xlib.h>
-#include  <X11/Xatom.h>
-#include  <X11/Xutil.h>
-
-// X11 related local variables
-static Display *x_display = NULL;
-
 ///
 // CreateEGLContext()
 //
@@ -53,7 +46,7 @@ EGLBoolean CreateEGLContext ( EGLNativeWindowType hWnd, EGLDisplay* eglDisplay,
    EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE, EGL_NONE };
 
    // Get Display
-   display = eglGetDisplay((EGLNativeDisplayType)x_display);
+   display = eglGetDisplay((EGLNativeDisplayType)EGL_DEFAULT_DISPLAY);
    if ( display == EGL_NO_DISPLAY )
    {
       return EGL_FALSE;
@@ -105,76 +98,8 @@ EGLBoolean CreateEGLContext ( EGLNativeWindowType hWnd, EGLDisplay* eglDisplay,
 
 
 ///
-//  WinCreate()
-//
-//      This function initialized the native X11 display and window for EGL
-//
-EGLBoolean WinCreate(ESContext *esContext, const char *title)
-{
-    Window root;
-    XSetWindowAttributes swa;
-    XSetWindowAttributes  xattr;
-    Atom wm_state;
-    XWMHints hints;
-    XEvent xev;
-    EGLConfig ecfg;
-    EGLint num_config;
-    Window win;
-
-    /*
-     * X11 native display initialization
-     */
-
-    x_display = XOpenDisplay(NULL);
-    if ( x_display == NULL )
-    {
-        return EGL_FALSE;
-    }
-
-    root = DefaultRootWindow(x_display);
-
-    swa.event_mask  =  ExposureMask | PointerMotionMask | KeyPressMask;
-    win = XCreateWindow(
-               x_display, root,
-               0, 0, esContext->width, esContext->height, 0,
-               CopyFromParent, InputOutput,
-               CopyFromParent, CWEventMask,
-               &swa );
-
-    xattr.override_redirect = FALSE;
-    XChangeWindowAttributes ( x_display, win, CWOverrideRedirect, &xattr );
-
-    hints.input = TRUE;
-    hints.flags = InputHint;
-    XSetWMHints(x_display, win, &hints);
-
-    // make the window visible on the screen
-    XMapWindow (x_display, win);
-    XStoreName (x_display, win, title);
-
-    // get identifiers for the provided atom name strings
-    wm_state = XInternAtom (x_display, "_NET_WM_STATE", FALSE);
-
-    memset ( &xev, 0, sizeof(xev) );
-    xev.type                 = ClientMessage;
-    xev.xclient.window       = win;
-    xev.xclient.message_type = wm_state;
-    xev.xclient.format       = 32;
-    xev.xclient.data.l[0]    = 1;
-    xev.xclient.data.l[1]    = FALSE;
-    XSendEvent (
-       x_display,
-       DefaultRootWindow ( x_display ),
-       FALSE,
-       SubstructureNotifyMask,
-       &xev );
-
-    esContext->hWnd = (EGLNativeWindowType) win;
-    return EGL_TRUE;
-}
 
 
-///
 //  userInterrupt()
 //
 //      Reads from X11 event loop and interrupt program if there is a keypress, or
@@ -239,6 +164,7 @@ void ESUTIL_API esInitContext ( ESContext *esContext )
 //          ES_WINDOW_STENCIL     - specifies that a stencil buffer should be created
 //          ES_WINDOW_MULTISAMPLE - specifies that a multi-sample buffer should be created
 //
+GLboolean WinCreate( ESContext *esContext, const char* title );
 GLboolean ESUTIL_API esCreateWindow ( ESContext *esContext, const char* title, GLint width, GLint height, GLuint flags )
 {
    EGLint attribList[] =
