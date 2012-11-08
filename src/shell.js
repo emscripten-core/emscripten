3a88c5e9df6323e9833183c75a4c8a7fdb36b39b
Alon Zakai <alonzakai@gmail.com>
2012-11-08 15:40:56 -0800

    improve node detection

--- src/shell.js
+++ src/shell.js
@@ -8,7 +8,7 @@ try {
 
 // The environment setup code below is customized to use Module.
 // *** Environment setup code ***
-var ENVIRONMENT_IS_NODE = typeof process === 'object';
+var ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function';
 var ENVIRONMENT_IS_WEB = typeof window === 'object';
 var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
 var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
