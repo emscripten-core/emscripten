d81f5bd1f567bb0c18b0be2a4bf7015e6435fced
Alon Zakai <alonzakai@gmail.com>
2012-11-08 20:47:58 -0800

    improve node detection in compiler

--- src/compiler.js
+++ src/compiler.js
@@ -13,7 +13,7 @@ try {
 // *** Environment setup code ***
 var arguments_ = [];
 
-var ENVIRONMENT_IS_NODE = typeof process === 'object';
+var ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function';
 var ENVIRONMENT_IS_WEB = typeof window === 'object';
 var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
 var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
