ffe92eb41e2513a0525bd35e5180ed88c127ae9b
Alon Zakai <alonzakai@gmail.com>
2012-11-29 10:56:13 -0800

    add asm.js eliminator testing

--- tools/eliminator/eliminator-test-output.js
+++ tools/eliminator/eliminator-test-output.js
@@ -6132,4 +6132,12 @@ function _mallocNoU($bytes) {
   return $mem_0;
   return null;
 }
+function asm(x, y) {
+  x = +x;
+  y = y | 0;
+  var a = 0, b = +0;
+  a = cheez(y + ~~x | 0) | 0;
+  b = a * a;
+  fleefl(b | 0, a | 0);
+}
 
--- tools/eliminator/eliminator-test.js
+++ tools/eliminator/eliminator-test.js
@@ -8828,5 +8828,14 @@ function _mallocNoU($bytes) {
   return $mem_0;
   return null;
 }
-// EMSCRIPTEN_GENERATED_FUNCTIONS: ["a", "b", "c", "f", "g", "h", "py", "r", "t", "f2", "f3", "llvm3_1", "_inflate", "_malloc", "_mallocNoU"]
+function asm(x, y) {
+  x = +x;
+  y = y|0;
+  var a = 0, b = +0, c = 0;
+  var label = 0;
+  a = cheez((y+~~x)|0)|0;
+  b = a*a;
+  fleefl(b|0, a|0);
+}
+// EMSCRIPTEN_GENERATED_FUNCTIONS: ["a", "b", "c", "f", "g", "h", "py", "r", "t", "f2", "f3", "llvm3_1", "_inflate", "_malloc", "_mallocNoU", "asm"]
 
