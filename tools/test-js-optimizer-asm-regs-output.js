01abee3ccd7d32f16ab0fc792c29652a8d8daed9
Alon Zakai <alonzakai@gmail.com>
2012-12-04 18:11:11 -0800

    add registerizeAsm testcase

---
+++ tools/test-js-optimizer-asm-regs-output.js
@@ -0,0 +1,13 @@
+function asm(x, y) {
+  x = +x;
+  y = y | 0;
+  var i1 = 0, i2 = 0;
+  var d1 = +0, d2 = +0;
+  i1 = (x+x)|0;
+  d1 = d();
+  i2 = (i+2)|0;
+  print(i);
+  d2 = d1*5;
+  return d2;
+}
+
---
+++ tools/test-js-optimizer-asm-regs.js
@@ -0,0 +1,14 @@
+function asm(x, y) {
+  x = +x;
+  y = y | 0;
+  var int1 = 0, int2 = 0; // do not mix the types!
+  var double1 = +0, double2 = +0;
+  int1 = (x+x)|0;
+  double1 = d();
+  int2 = (int1+2)|0;
+  print(int2);
+  double2 = double1*5;
+  return double2;
+}
+// EMSCRIPTEN_GENERATED_FUNCTIONS: ["asm", "__Z11printResultPiS_j"]
+
