1f4842db0437edb73896e0d7a03558c5a2c692ba
Alon Zakai <alonzakai@gmail.com>
2012-12-07 15:17:57 -0800

    disable test_linespecific in asm

--- tests/runner.py
+++ tests/runner.py
@@ -7104,6 +7104,8 @@ def process(filename):
         assert 'Assertion failed' in str(e), str(e)
 
     def test_linespecific(self):
+      if Settings.ASM_JS: return self.skip('asm always has corrections on')
+
       if '-g' not in Building.COMPILER_TEST_OPTS: Building.COMPILER_TEST_OPTS.append('-g')
       if self.emcc_args: self.emcc_args += ['--llvm-opts', '0'] # llvm full opts make the expected failures here not happen
 
