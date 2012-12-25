2833fb1d294ffa18e9c58e8eb2ca1a72670afd87
Alon Zakai <alonzakai@gmail.com>
2012-12-24 16:06:11 -0800

    debugging tool tweaks

--- tools/autodebugger.py
+++ tools/autodebugger.py
@@ -20,6 +20,8 @@ POSTAMBLE = '''
 @.emscripten.autodebug.str.f = private constant [11 x i8] c"AD:%d,%lf\\0A\\00", align 1 ; [#uses=1]
 @.emscripten.autodebug.str.64 = private constant [13 x i8] c"AD:%d,%d,%d\\0A\\00", align 1 ; [#uses=1]
 
+declare i32 @printf(i8*, ...)
+
 ; [#uses=1]
 define void @emscripten_autodebug_i64(i32 %line, i64 %value) {
 entry:
@@ -90,6 +92,8 @@ return:                                           ; preds = %entry
 POSTAMBLE_NEW = '''
 @.emscripten.autodebug.str = private constant [10 x i8] c"AD:%d,%d\\0A\\00", align 1 ; [#uses=1]
 @.emscripten.autodebug.str.f = private constant [11 x i8] c"AD:%d,%lf\\0A\\00", align 1 ; [#uses=1]
+
+declare i32 @printf(i8*, ...)
 
 ; [#uses=1]
 define void @emscripten_autodebug_i64(i32 %line, i64 %value) {
