8e283ee92b501aff711d28063db263a63cc1071a
Alon Zakai <alonzakai@gmail.com>
2013-01-09 12:38:43 -0800

    rename dlmalloc to libc in cache, in preparation for adding further code there

--- tools/shared.py
+++ tools/shared.py
@@ -172,7 +172,7 @@ def check_node_version():
 # we re-check sanity when the settings are changed)
 # We also re-check sanity and clear the cache when the version changes
 
-EMSCRIPTEN_VERSION = '1.2.2'
+EMSCRIPTEN_VERSION = '1.2.3'
 
 def check_sanity(force=False):
   try:
