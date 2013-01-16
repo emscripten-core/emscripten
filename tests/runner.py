f628fe21b9df8fd0fa03bfe0ee56f2b9629bd490
Alon Zakai <alonzakai@gmail.com>
2013-01-16 15:53:16 -0800

    fix test_utf

--- tests/runner.py
+++ tests/runner.py
@@ -5514,7 +5514,7 @@ def process(filename):
         int main() {
           char *c = "μ†ℱ ╋ℯ╳╋";
           printf("%d %d %d %d %s\n", c[0]&0xff, c[1]&0xff, c[2]&0xff, c[3]&0xff, c);
-          emscripten_run_script("cheez = Module._malloc(100);"
+          emscripten_run_script("cheez = _malloc(100);"
                                 "Module.writeStringToMemory(\"μ†ℱ ╋ℯ╳╋\", cheez);"
                                 "Module.print([Pointer_stringify(cheez), Module.getValue(cheez, 'i8')&0xff, Module.getValue(cheez+1, 'i8')&0xff, Module.getValue(cheez+2, 'i8')&0xff, Module.getValue(cheez+3, 'i8')&0xff, ]);");
         }
