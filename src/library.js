5cf7c9a2ddf5b6d6895bf975ce174a99330d38d3
Chad Austin <chad@imvu.com>
2013-01-23 19:36:53 -0800

    Statically allocate the strtok state block so it doesn't trip up our leak detection.

--- src/library.js
+++ src/library.js
@@ -4619,10 +4619,8 @@ LibraryManager.library = {
 
   __strtok_state: 0,
   strtok__deps: ['__strtok_state', 'strtok_r'],
+  strtok__postset: '___strtok_state = Runtime.staticAlloc(4);',
   strtok: function(s, delim) {
-    if (!___strtok_state) {
-      ___strtok_state = _malloc(4);
-    }
     return _strtok_r(s, delim, ___strtok_state);
   },
 
