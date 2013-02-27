b8f773919b96e36e8c614bff1e20cbc8e63484b3
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2013-02-26 20:05:40 -0500

    The default TLS entry value should be 0 not JS null

--- src/library.js
+++ src/library.js
@@ -6743,7 +6743,7 @@ LibraryManager.library = {
 
   pthread_key_create: function(key, destructor) {
     if (!_pthread_key_create.keys) _pthread_key_create.keys = {};
-    _pthread_key_create.keys[key] = null;
+    _pthread_key_create.keys[key] = 0;
   },
 
   pthread_getspecific: function(key) {
