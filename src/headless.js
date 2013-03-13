be239eec25922e45c66c3f8811e1d8f4b770c5c4
Alon Zakai <alonzakai@gmail.com>
2013-03-12 18:18:42 -0700

    headless improvements

--- src/headless.js
+++ src/headless.js
@@ -537,7 +537,7 @@ var document = {
                       case /* GL_MAX_FRAGMENT_UNIFORM_VECTORS     */ 0x8DFD: return 4096;
                       case /* GL_MAX_VARYING_VECTORS              */ 0x8DFC: return 32;
                       case /* GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS */ 0x8B4D: return 32;
-                      default: throw 'getParameter ' + pname;
+                      default: console.log('getParameter ' + pname + '?'); return 0;
                     }
                   },
                   getSupportedExtensions: function() {
@@ -686,6 +686,7 @@ var document = {
               document.callEventListeners('pointerlockchange');
             });
           },
+          exitPointerLock: function(){},
           style: {},
           eventListeners: {},
           addEventListener: document.addEventListener,
@@ -748,6 +749,8 @@ var document = {
   body: {
     appendChild: function(){},
   },
+  exitPointerLock: function(){},
+  cancelFullScreen: function(){},
 };
 var alert = function(x) {
   print(x);
