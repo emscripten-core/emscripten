3023a0fc8a7021bba969ba99fa4adea09f916770
Alon Zakai <alonzakai@gmail.com>
2013-03-14 16:07:19 -0700

    optimize strcat a little

--- src/library.js
+++ src/library.js
@@ -4391,10 +4391,10 @@ LibraryManager.library = {
   strcat__deps: ['strlen'],
   strcat: function(pdest, psrc) {
     pdest = pdest|0; psrc = psrc|0;
-    var len = 0, i = 0;
-    var len = _strlen(pdest);
+    var i = 0;
+    pdest = (pdest + _strlen(pdest))|0;
     do {
-      {{{ makeCopyValues('pdest+len+i', 'psrc+i', 1, 'i8', null, 1) }}};
+      {{{ makeCopyValues('pdest+i', 'psrc+i', 1, 'i8', null, 1) }}};
       i = (i+1)|0;
     } while ({{{ makeGetValueAsm('psrc', 'i-1', 'i8') }}} != 0);
     return pdest|0;
