4b4e8582dd7312062d5d19c7856101aae920ccee
Alon Zakai <alonzakai@gmail.com>
2013-03-20 10:35:06 -0700

    restore test_sdl_mouse to the proper state, after the last fix

--- tests/runner.py
+++ tests/runner.py
@@ -11042,7 +11042,7 @@ elif 'browser' in str(sys.argv):
       open(os.path.join(self.get_dir(), 'sdl_mouse.c'), 'w').write(self.with_report_result(open(path_from_root('tests', 'sdl_mouse.c')).read()))
 
       Popen([PYTHON, EMCC, os.path.join(self.get_dir(), 'sdl_mouse.c'), '-O2', '--minify', '0', '-o', 'page.html', '--pre-js', 'pre.js']).communicate()
-      self.run_browser('page.html', '', '/report_result?722')
+      self.run_browser('page.html', '', '/report_result?740')
 
     def test_sdl_audio(self):
       shutil.copyfile(path_from_root('tests', 'sounds', 'alarmvictory_1.ogg'), os.path.join(self.get_dir(), 'sound.ogg'))
