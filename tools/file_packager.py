1ce0f6c6b288eff8313e022b8ac62051e2ccc9f4
tingyuan <thuang@mozilla.com>
2013-03-20 16:53:42 +0800

    Add checks to existences of Module['setStatus'].

--- tools/file_packager.py
+++ tools/file_packager.py
@@ -368,9 +368,9 @@ if has_preloaded:
           num++;
         }
         total = Math.ceil(total * Module.expectedDataFileDownloads/num);
-        Module['setStatus']('Downloading data... (' + loaded + '/' + total + ')');
+        if (Module['setStatus']) Module['setStatus']('Downloading data... (' + loaded + '/' + total + ')');
       } else if (!Module.dataFileDownloads) {
-        Module['setStatus']('Downloading data...');
+        if (Module['setStatus']) Module['setStatus']('Downloading data...');
       }
     }
     dataFile.open('GET', '%s', true);
