f99012f271c056ddc171f6a4ec011ad31ad936cd
Alon Zakai <alonzakai@gmail.com>
2013-03-22 14:10:07 -0700

    v1.3.2

--- tools/shared.py
+++ tools/shared.py
@@ -181,7 +181,7 @@ def check_node_version():
 # we re-check sanity when the settings are changed)
 # We also re-check sanity and clear the cache when the version changes
 
-EMSCRIPTEN_VERSION = '1.3.1'
+EMSCRIPTEN_VERSION = '1.3.2'
 
 def check_sanity(force=False):
   try:
