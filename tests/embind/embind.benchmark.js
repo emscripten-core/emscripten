75881fce897a3c5849c139866fe46d39d6b05bcd
Jukka Jylänki <jujjyl@gmail.com>
2013-04-11 14:07:23 +0300

    Added benchmark for embind code.

---
+++ tests/embind/embind.benchmark.js
@@ -0,0 +1,153 @@
+function _increment_counter_benchmark_js(N) {
+    var ctr = _get_counter();
+    var a = _emscripten_get_now();
+    for(i = 0; i < N; ++i) {
+        _increment_counter();
+        _increment_counter();
+        _increment_counter();
+        _increment_counter();
+        _increment_counter();
+        _increment_counter();
+        _increment_counter();
+        _increment_counter();
+        _increment_counter();
+        _increment_counter();
+    }
+    var b = _emscripten_get_now();
+    var ctr2 = _get_counter();
+    Module.print("JS increment_counter " + N + " iters: " + (b-a)*1000 + " msecs. result: " + (ctr2-ctr));
+}
+
+function _returns_input_benchmark_js() {
+    var a = _emscripten_get_now();
+    var t = 0;
+    for(i = 0; i < 100000; ++i) {
+        t += _returns_input(i);
+        t += _returns_input(i);
+        t += _returns_input(i);
+        t += _returns_input(i);
+        t += _returns_input(i);
+        t += _returns_input(i);
+        t += _returns_input(i);
+        t += _returns_input(i);
+        t += _returns_input(i);
+        t += _returns_input(i);
+    }
+    var b = _emscripten_get_now();
+    Module.print("JS returns_input 100000 iters: " + (b-a)*1000 + " msecs. result: " + t);
+}
+
+function _sum_int_benchmark_js() {
+    var a = _emscripten_get_now();
+    var r = 0;
+    for(i = 0; i < 100000; ++i) {
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_int(i, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
+    var b = _emscripten_get_now();
+    Module.print("JS sum_int 100000 iters: " + (b-a)*1000 + " msecs. result: " + r);
+}
+
+function _sum_float_benchmark_js() {
+    var a = _emscripten_get_now();
+    var r = 0;
+    for(i = 0; i < 100000; ++i) {
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += _sum_float(i, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
+    var b = _emscripten_get_now();
+    Module.print("JS sum_float 100000 iters: " + (b-a)*1000 + " msecs. result: " + r);
+}
+
+function _increment_counter_benchmark_embind_js(N) {
+    var ctr = _get_counter();
+    var a = _emscripten_get_now();
+    for(i = 0; i < N; ++i) {
+        Module['increment_counter']();
+        Module['increment_counter']();
+        Module['increment_counter']();
+        Module['increment_counter']();
+        Module['increment_counter']();
+        Module['increment_counter']();
+        Module['increment_counter']();
+        Module['increment_counter']();
+        Module['increment_counter']();
+        Module['increment_counter']();
+    }
+    var b = _emscripten_get_now();
+    var ctr2 = _get_counter();
+    Module.print("JS embind increment_counter " + N + " iters: " + (b-a)*1000 + " msecs. result: " + (ctr2-ctr));
+}
+
+function _returns_input_benchmark_embind_js() {
+    var a = _emscripten_get_now();
+    var t = 0;
+    for(i = 0; i < 100000; ++i) {
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+        t += Module['returns_input'](i);
+    }
+    var b = _emscripten_get_now();
+    Module.print("JS embind returns_input 100000 iters: " + (b-a)*1000 + " msecs. result: " + t);
+}
+
+function _sum_int_benchmark_embind_js() {
+    var a = _emscripten_get_now();
+    var r = 0;
+    for(i = 0; i < 100000; ++i) {
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_int'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
+    var b = _emscripten_get_now();
+    Module.print("JS embind sum_int 100000 iters: " + (b-a)*1000 + " msecs. result: " + r);
+}
+
+function _sum_float_benchmark_embind_js() {
+    var a = _emscripten_get_now();
+    var r = 0;
+    for(i = 0; i < 100000; ++i) {
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+        r += Module['sum_float'](i, 2, 3, 4, 5, 6, 7, 8, 9);
+    }
+    var b = _emscripten_get_now();
+    Module.print("JS embind sum_float 100000 iters: " + (b-a)*1000 + " msecs. result: " + r);
+}
---
+++ tests/embind/embind_benchmark.cpp
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <emscripten.h>
+#include <bind.h>
+
+int counter = 0;
+
+extern "C"
+{
+
+int __attribute__((noinline)) get_counter()
+{
+    return counter;
+}
+
+void __attribute__((noinline)) increment_counter()
+{
+    ++counter;
+}
+
+int __attribute__((noinline)) sum_int(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+{
+    return v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9;
+}
+
+float __attribute__((noinline)) sum_float(float v1, float v2, float v3, float v4, float v5, float v6, float v7, float v8, float v9)
+{
+    return v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9;
+}
+
+int __attribute__((noinline)) returns_input(int i)
+{
+    return i;
+}
+
+extern void increment_counter_benchmark_js(int N);
+extern void returns_input_benchmark_js();
+extern void sum_int_benchmark_js();
+extern void sum_float_benchmark_js();
+
+extern void increment_counter_benchmark_embind_js(int N);
+extern void returns_input_benchmark_embind_js();
+extern void sum_int_benchmark_embind_js();
+extern void sum_float_benchmark_embind_js();
+
+}
+
+EMSCRIPTEN_BINDINGS(benchmark)
+{
+    using namespace emscripten;
+    
+    function("get_counter", &get_counter);
+    function("increment_counter", &increment_counter);
+    function("returns_input", &returns_input);
+    function("sum_int", &sum_int);
+    function("sum_float", &sum_float);
+}
+
+void __attribute__((noinline)) emscripten_get_now_benchmark(int N)
+{
+    volatile float t = emscripten_get_now();
+    for(int i = 0; i < N; ++i)
+    {
+        emscripten_get_now();
+        emscripten_get_now();
+        emscripten_get_now();
+        emscripten_get_now();
+        emscripten_get_now();
+        emscripten_get_now();
+        emscripten_get_now();
+        emscripten_get_now();
+        emscripten_get_now();
+        emscripten_get_now();
+    }
+    volatile float t2 = emscripten_get_now();
+    printf("C++ emscripten_get_now %d iters: %f msecs.\n", N, 1000.f*(t2-t));
+}
+
+void __attribute__((noinline)) increment_counter_benchmark(int N)
+{
+    volatile float t = emscripten_get_now();
+    for(int i = 0; i < N; ++i)
+    {
+        increment_counter();
+        increment_counter();
+        increment_counter();
+        increment_counter();
+        increment_counter();
+        increment_counter();
+        increment_counter();
+        increment_counter();
+        increment_counter();
+        increment_counter();
+    }
+    volatile float t2 = emscripten_get_now();
+    printf("C++ increment_counter %d iters: %f msecs.\n", N, 1000.f*(t2-t));
+}
+
+void __attribute__((noinline)) returns_input_benchmark()
+{
+    volatile int r = 0;
+    volatile float t = emscripten_get_now();
+    for(int i = 0; i < 100000; ++i)
+    {
+        r += returns_input(i);
+        r += returns_input(i);
+        r += returns_input(i);
+        r += returns_input(i);
+        r += returns_input(i);
+        r += returns_input(i);
+        r += returns_input(i);
+        r += returns_input(i);
+        r += returns_input(i);
+        r += returns_input(i);
+    }
+    volatile float t2 = emscripten_get_now();
+    printf("C++ returns_input 100000 iters: %f msecs.\n", 1000.f*(t2-t));
+}
+
+void __attribute__((noinline)) sum_int_benchmark()
+{
+    volatile float t = emscripten_get_now();
+    volatile int r = 0;
+    for(int i = 0; i < 100000; ++i)
+    {
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+        r += sum_int(i,2,3,4,5,6,7,8,9);
+    }
+    volatile float t2 = emscripten_get_now();
+    printf("C++ sum_int 100000 iters: %f msecs.\n", 1000.f*(t2-t));
+}
+
+void __attribute__((noinline)) sum_float_benchmark()
+{
+    volatile float f = 0.f;
+    volatile float t = emscripten_get_now();
+    for(int i = 0; i < 100000; ++i)
+    {
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+        f += sum_float((float)i,2.f,3.f,4.f,5.f,6.f,7.f,8.f,9.f);
+    }
+    volatile float t2 = emscripten_get_now();
+    printf("C++ sum_float 100000 iters: %f msecs.\n", 1000.f*(t2-t));
+}
+
+int main()
+{
+    for(int i = 1000; i <= 100000; i *= 10)
+        emscripten_get_now_benchmark(i);
+
+    printf("\n");
+    for(int i = 1000; i <= 100000; i *= 10)
+    {
+        increment_counter_benchmark(i);
+        increment_counter_benchmark_js(i);
+        increment_counter_benchmark_embind_js(i);
+        printf("\n");
+    }
+
+    returns_input_benchmark();
+    returns_input_benchmark_js();
+    returns_input_benchmark_embind_js();
+    printf("\n");
+    sum_int_benchmark();
+    sum_int_benchmark_js();
+    sum_int_benchmark_embind_js();
+    printf("\n");
+    sum_float_benchmark();
+    sum_float_benchmark_js();
+    sum_float_benchmark_embind_js();
+}
