2db451959aafa8e8e344177bacdad0706cf1afb6
Alan Kligman <ack@mozilla.com>
2013-04-16 18:19:56 -0400

    Default to websockets, since it's more widely supported at the moment.

--- src/settings.js
+++ src/settings.js
@@ -174,7 +174,7 @@ var LIBRARY_DEBUG = 0; // Print out when we enter a library call (library*.js). 
                        // want it back. A simple way to set it in C++ is
                        //   emscripten_run_script("Runtime.debug = ...;");
 var SOCKET_DEBUG = 0; // Log out socket/network data transfer.
-var SOCKET_WEBRTC = 1; // Select socket backend, either webrtc or websockets.
+var SOCKET_WEBRTC = 0; // Select socket backend, either webrtc or websockets.
 
 var OPENAL_DEBUG = 0; // Print out debugging information from our OpenAL implementation.
 
