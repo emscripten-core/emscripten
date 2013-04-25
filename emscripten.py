fac94229d8765d20aa00fdfcad1e7f940b5cb322
Alon Zakai <alonzakai@gmail.com>
2013-04-25 10:54:03 -0700

    debugging text cleanup

--- emscripten.py
+++ emscripten.py
@@ -305,7 +305,7 @@ def emscript(infile, settings, outfile, libraries=[], compiler_engine=None,
   i = 2
   if settings['ASM_JS']: i += 2*settings['RESERVED_FUNCTION_POINTERS']
   for indexed in indexed_functions:
-    #print >> sys.stderr, 'indaxx', indexed, i
+    #print >> sys.stderr, 'function indexing', indexed, i
     forwarded_json['Functions']['indexedFunctions'][indexed] = i # make sure not to modify this python object later - we use it in indexize
     i += 2
   forwarded_json['Functions']['nextIndex'] = i
