e64bcc5d2e3386582f82d98cae381b7526a82174
Alon Zakai <alonzakai@gmail.com>
2013-04-30 16:09:54 -0700

    simplify compiler flags in test runner, now that we use emcc

--- tests/runner.py
+++ tests/runner.py
@@ -238,10 +238,7 @@ process(sys.argv[1])
         os.remove(f + '.o')
       except:
         pass
-      compiler_flags = ['-emit-llvm']
-      if not f.endswith('.c'):
-         compiler_flags = compiler_flags + ['-std=c++03']
-      args = [PYTHON, EMCC] + compiler_flags + COMPILER_OPTS + Building.COMPILER_TEST_OPTS + \
+      args = [PYTHON, EMCC] + Building.COMPILER_TEST_OPTS + \
              ['-I', dirname, '-I', os.path.join(dirname, 'include')] + \
              map(lambda include: '-I' + include, includes) + \
              ['-c', f, '-o', f + '.o']
