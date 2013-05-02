10aab3b1717a01d9aa4b566eda72a66959fabf37
Anthony Liot <anthony.liot@gmail.com>
2013-05-02 10:42:25 +0200

    Remove glVertexAttrib1f
    After the merge with the last verison of incoming, Emscripten seems
    already define this function ;)

--- src/library_gl.js
+++ src/library_gl.js
@@ -1009,11 +1009,6 @@ var LibraryGL = {
 
     Module.ctx.bindBuffer(target, bufferObj);
   },
-  
-  glVertexAttrib1f__sig: 'vii',
-  glVertexAttrib1f: function(index, v) {
-    Module.ctx.vertexAttrib1fv(index, v);
-  },
 
   glVertexAttrib1fv__sig: 'vii',
   glVertexAttrib1fv: function(index, v) {
