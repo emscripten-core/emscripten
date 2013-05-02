6ec63a2dec08592c51ed7c9bc2fe871c4ac11f01
Anthony Liot <anthony.liot@gmail.com>
2013-05-02 10:13:26 +0200

    Add some function openal
    Modify the behavior when pass the attrList (don't return 0 and put a
    warning).
    Add other function need by Unigine

--- src/library_openal.js
+++ src/library_openal.js
@@ -61,9 +61,8 @@ var LibraryOpenAL = {
 
     if (attrList) {
 #if OPENAL_DEBUG
-      console.log("The attrList argument of alcCreateContext is not supported yet");
+      console.warning("The attrList argument of alcCreateContext is not supported yet");
 #endif
-      return 0;
     }
 
     var ctx;
@@ -72,7 +71,9 @@ var LibraryOpenAL = {
     } catch (e) {
       try {
         ctx = new webkitAudioContext();
-      } catch (e) {}
+      } catch (e) {
+ 
+      }
     }
 
     if (ctx) {
@@ -82,12 +83,48 @@ var LibraryOpenAL = {
       return 0;
     }
   },
+  
+  alIsBuffer: function(buffer) {
+    if (!AL.currentContext) {
+#if OPENAL_DEBUG
+      console.error("alIsBuffer called without a valid context");
+#endif
+      return 0;
+    }
+    if (source > AL.currentContext.buf.length) {
+#if OPENAL_DEBUG
+      console.error("alSourcePause called with an invalid buffer");
+#endif
+      return 0;
+    }
+    
+	  return AL.currentContext.buf[buffer - 1].buf != null;
+  },
+  
+  alIsSource: function(source) {
+    if (!AL.currentContext) {
+#if OPENAL_DEBUG
+      console.error("alSourcePause called without a valid context");
+#endif
+      return;
+    }
+    if (source > AL.currentContext.src.length) {
+#if OPENAL_DEBUG
+      console.error("alSourcePause called with an invalid source");
+#endif
+      return;
+    }
+    
+    return AL.currentContext.src[source - 1] != 0;
+  },
 
   alGetError: function() {
     if (!AL.currentContext) {
       return 0xA004 /* AL_INVALID_OPERATION */;
     } else {
-      return AL.currentContext.err;
+      var err = AL.currentContext.err;
+      AL.currentContext.err = 0
+      return err;
     }
   },
 
@@ -95,6 +132,41 @@ var LibraryOpenAL = {
   alcGetError: function(device) {
     // We have only one audio device, so just return alGetError.
     return _alGetError();
+  },
+  
+  alGetEnumValue: function(ename) {
+	  switch (ename) {
+      case "AL_FORMAT_QUAD16":
+        return 0;
+      case "AL_FORMAT_51CHN16":
+        return 0;
+      case "AL_FORMAT_61CHN16":
+        return 0;
+      case "AL_FORMAT_71CHN16":
+        return 0;
+      default:
+        return 0xA002 /* AL_INVALID_ENUM */
+    }
+  },
+  
+  alGetString: function(param) {
+	  switch (param) {
+      case 0xB001 /* AL_VENDOR */:
+      case 0xB002 /* AL_VERSION */:
+      case 0xB003 /* AL_RENDERER */:
+      case 0xB004 /* AL_EXTENSIONS */:
+        return allocate(intArrayFromString('OpenAL Audio Context'), 'i8', ALLOC_NORMAL);
+      default:
+	      return "";
+    }
+  },
+  
+  alcGetString: function(param) {
+	  switch (param) {
+      case 0x1006 /* ALC_EXTENSIONS */:
+      default:
+        return "";
+    }
   },
 
   alDeleteSources: function(count, sources)
@@ -138,7 +210,7 @@ var LibraryOpenAL = {
       {{{ makeSetValue('sources', 'i*4', 'AL.currentContext.src.length', 'i32') }}};
     }
   },
-
+  
   alSourcei: function(source, param, value) {
     if (!AL.currentContext) {
 #if OPENAL_DEBUG
@@ -231,6 +303,29 @@ var LibraryOpenAL = {
           {{{ makeGetValue('value', '8', 'float') }}}
         );
       break;
+    default:
+#if OPENAL_DEBUG
+      console.log("alSourcefv with param " + param + " not implemented yet");
+#endif
+      break;
+    }
+  },
+  
+  alSource3i: function(source, param, value1, value2, value3) {
+    if (!AL.currentContext) {
+#if OPENAL_DEBUG
+      console.error("alSource3i called without a valid context");
+#endif
+      return;
+    }
+    if (source > AL.currentContext.src.length) {
+#if OPENAL_DEBUG
+      console.error("alSource3i called with an invalid source");
+#endif
+      return;
+    }
+    switch (param) {
+    case 0x20006 /* AL_AUXILIARY_SEND_FILTER */:
     default:
 #if OPENAL_DEBUG
       console.log("alSourcefv with param " + param + " not implemented yet");
@@ -475,6 +570,27 @@ var LibraryOpenAL = {
     }
   },
 
+  alGetSourcef: function(source, param, value) {
+    if (!AL.currentContext) {
+#if OPENAL_DEBUG
+      console.error("alGetSourcef called without a valid context");
+#endif
+      return;
+    }
+    if (source > AL.currentContext.src.length) {
+#if OPENAL_DEBUG
+      console.error("alGetSourcef called with an invalid source");
+#endif
+      return;
+    }
+    switch (param) {
+    case 0x1024 /* AL_SEC_OFFSET */:
+      // Always return 1
+      {{{ makeSetValue('value', '0', '1', 'float') }}};
+      break;
+    }
+  },
+  
   alGetSourcei: function(source, param, value) {
     if (!AL.currentContext) {
 #if OPENAL_DEBUG
@@ -580,6 +696,45 @@ var LibraryOpenAL = {
     }
   },
 
+  alListener3f: function(param, value1, value2, value3) {
+    if (!AL.currentContext) {
+#if OPENAL_DEBUG
+      console.error("alListener3f called without a valid context");
+#endif
+      return;
+    }
+    switch (param) {
+    case 0x1004 /* AL_POSITION */:
+      AL.currentContext.ctx.listener.setPosition(value1,value2,value3);
+      break;
+    case 0x1006 /* AL_VELOCITY */:
+      AL.currentContext.ctx.listener.setVelocity(value1,value2,value3);
+      break;
+    default:
+#if OPENAL_DEBUG
+      console.log("alListener3f with param " + param + " not implemented yet");
+#endif
+      break;
+    }
+  },
+  
+  alListenerf: function(param, values) {
+    if (!AL.currentContext) {
+#if OPENAL_DEBUG
+      console.error("alListenerf called without a valid context");
+#endif
+      return;
+    }
+    switch (param) {
+    case 0x100A /* AL_GAIN */:
+    default:
+#if OPENAL_DEBUG
+      console.log("alListenerf with param " + param + " not implemented yet");
+#endif
+      break;
+    }
+  },
+
   alIsExtensionPresent: function(extName) {
     return 0;
   },
@@ -595,6 +750,19 @@ var LibraryOpenAL = {
   alcGetProcAddress: function(device, fname) {
     return 0;
   },
+  
+  alDopplerFactor: function(value) {
+    AL.currentContext.ctx.listener.dopplerFactor = value;
+  },
+  
+  alSpeedOfSound: function(value) {
+    AL.currentContext.ctx.listener.speedOfSound = value;
+  },
+  
+  alcGetIntegerv: function(device, param, size, data) {
+
+  },
+  
 };
 
 autoAddDeps(LibraryOpenAL, '$AL');
