0fdf95ce714591173bec8ef881db317f5cba5fa3
Alon Zakai <alonzakai@gmail.com>
2013-05-02 18:58:05 -0700

    1.4.2

--- tools/shared.py
+++ tools/shared.py
@@ -200,7 +200,7 @@ def check_node_version():
 # we re-check sanity when the settings are changed)
 # We also re-check sanity and clear the cache when the version changes
 
-EMSCRIPTEN_VERSION = '1.4.1'
+EMSCRIPTEN_VERSION = '1.4.2'
 
 def generate_sanity():
   return EMSCRIPTEN_VERSION + '|' + get_llvm_target()
