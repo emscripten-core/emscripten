45a3601db61e796019ef0e6da6cf8d1378eb99af
Alon Zakai <alonzakai@gmail.com>
2013-05-08 14:54:30 -0700

    improve mathfuncptr test

--- tests/runner.py
+++ tests/runner.py
@@ -3309,10 +3309,10 @@ Exiting setjmp function, level: 0, prev_jmp: -1
           #include <stdio.h>
 
           int
-          main(void) {
-           float (*fn)(float) = &sqrtf;
-           float (*fn2)(float) = &fabsf;
-           float (*fn3)(float) = &erff;
+          main(int argc, char **argv) {
+           float (*fn)(float) =  argc != 12 ? &sqrtf : &fabsf;
+           float (*fn2)(float) = argc != 13 ? &fabsf : &sqrtf;
+           float (*fn3)(float) = argc != 14 ? &erff  : &fabsf;
            printf("fn2(-5) = %d, fn(10) = %.2f, erf(10) = %.2f\\n", (int)fn2(-5), fn(10), fn3(10));
            return 0;
           }
