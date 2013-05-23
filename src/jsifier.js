emscripten-core/emscripten

commit 5efd506b7a9bc9ebae72f883e8fa5f1b18a85de2

correctly count the number of normal vars even when there are i64s before a varargs

src/parseTools.js
@@ -286,11 +286,22 @@ function isVarArgsFunctionType(type) {
   return type.substr(-varArgsSuffix.length) == varArgsSuffix;
 }
 
+function getNumVars(type) { // how many variables are needed to represent this type
+  if (type in Runtime.FLOAT_TYPES) return 1;
+  return Math.max(getNumIntChunks(type), 1);
+}
+
 function countNormalArgs(type, out) {
   out = out || {};
   if (!isFunctionType(type, out)) return -1;
-  if (isVarArgsFunctionType(type)) out.numArgs--;
-  return out.numArgs;
+  var ret = 0;
+  if (out.segments) {
+    for (var i = 0; i < out.segments.length; i++) {
+      ret += getNumVars(out.segments[i][0].text);
+    }
+  }
+  if (isVarArgsFunctionType(type)) ret--;
+  return ret;
 }
 
 function addIdent(token) {

tests/runner.py
@@ -1179,6 +1179,46 @@ m_divisor is 1091269979
       '''
       self.do_run(src, 'Succeeded!')
 
+    def test_i64_varargs(self):
+      if Settings.USE_TYPED_ARRAYS != 2: return self.skip('full i64 stuff only in ta2')
+
+      src = r'''
+        #include <stdio.h>
+        #include <stdint.h>
+        #include <stdarg.h>
+
+        void ccv_cache_generate_signature(char *msg, int len, int64_t sig_start, ...) {
+          if (sig_start < 10123)
+            printf("%s\n", msg+len);
+          va_list v;
+          va_start(v, sig_start);
+          if (sig_start > 1413)
+            printf("%d\n", va_arg(v, int));
+          else
+            printf("nada\n");
+          va_end(v);
+        }
+
+        int main(int argc, char **argv)
+        {
+          for (int i = 0; i < argc; i++) {
+            if (i % 123123 == 0)
+              ccv_cache_generate_signature(argv[i], i+2, (int64_t)argc*argc, 54.111);
+            else
+              ccv_cache_generate_signature(argv[i], i+2, (int64_t)argc*argc, 13);
+          }
+        };
+      '''
+      self.do_run(src, '''in/this.program
+nada
+a
+nada
+fl
+nada
+sdfasdfasdf
+nada
+''', 'waka fleefl asdfasdfasdfasdf'.split(' '))
+
     def test_i32_mul_precise(self):
       if self.emcc_args == None: return self.skip('needs ta2')
 

commit 48b7d2a76594a0136444cc666c2532c6bec70e0b

fix argument counting regression from 5efd506 ; fixes #1202

src/jsifier.js
@@ -1357,7 +1357,7 @@ function JSify(data, functionsOnly, givenFunctions) {
     var ignoreFunctionIndexizing = [];
     var useJSArgs = (simpleIdent + '__jsargs') in LibraryManager.library;
     var hasVarArgs = isVarArgsFunctionType(type);
-    var normalArgs = (hasVarArgs && !useJSArgs) ? countNormalArgs(type) : -1;
+    var normalArgs = (hasVarArgs && !useJSArgs) ? countNormalArgs(type, null, true) : -1;
     var byPointer = getVarData(funcData, ident);
     var byPointerForced = false;
 

src/parseTools.js
@@ -290,18 +290,18 @@ function isVarArgsFunctionType(type) {
   return type.substr(-varArgsSuffix.length) == varArgsSuffix;
 }
 
-function getNumVars(type) { // how many variables are needed to represent this type
+function getNumLegalizedVars(type) { // how many legalized variables are needed to represent this type
   if (type in Runtime.FLOAT_TYPES) return 1;
   return Math.max(getNumIntChunks(type), 1);
 }
 
-function countNormalArgs(type, out) {
+function countNormalArgs(type, out, legalized) {
   out = out || {};
   if (!isFunctionType(type, out)) return -1;
   var ret = 0;
   if (out.segments) {
     for (var i = 0; i < out.segments.length; i++) {
-      ret += getNumVars(out.segments[i][0].text);
+      ret += legalized ? getNumLegalizedVars(out.segments[i][0].text) : 1;
     }
   }
   if (isVarArgsFunctionType(type)) ret--;