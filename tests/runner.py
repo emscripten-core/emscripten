4d6a04143f094293e1d4c969a56020b2a33c6ad8
Alon Zakai <alonzakai@gmail.com>
2013-05-25 19:46:48 -0700

    improve test_llvmswitch

--- tests/runner.py
+++ tests/runner.py
@@ -4071,7 +4071,7 @@ def process(filename):
 
           int main( int argc, const char *argv[] ) {
             unsigned int x = 0xfffffff1;
-            x >>= 0; // force it to be unsigned for purpose of checking our switch comparison in signed/unsigned
+            x >>= (argc-1); // force it to be unsigned for purpose of checking our switch comparison in signed/unsigned
             printf("*%d,%d,%d,%d,%d,%d*\\n", switcher('a'), switcher('b'), switcher('c'), switcher(x), switcher(-15), switcher('e'));
             return 0;
           }
