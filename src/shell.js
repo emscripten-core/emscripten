861cb27e28959b59c9b5bcf77d9aa7ed606322c6
Jazz <jazzzz@gmail.com>
2013-06-04 15:42:22 +0200

    Check that module is an object instead of exports

--- src/shell.js
+++ src/shell.js
@@ -11,7 +11,7 @@ var ENVIRONMENT_IS_WEB = typeof window === 'object';
 var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
 var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
 
-if (typeof exports === "object") {
+if (typeof module === "object") {
   module.exports = Module;
 }
 
