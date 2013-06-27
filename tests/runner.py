46d3849df6a76f8c5b041172dc9fa2cc8a28be6f
Alon Zakai <alonzakai@gmail.com>
2013-06-27 15:47:47 -0700

    fix test_stack

--- tests/runner.py
+++ tests/runner.py
@@ -2104,12 +2104,12 @@ Succeeded!
             }
             return int(&x); // both for the number, and forces x to not be nativized
           }
-          int main()
+          int main(int argc, char **argv)
           {
             // We should get the same value for the first and last - stack has unwound
-            int x1 = test(0);
+            int x1 = test(argc - 2);
             int x2 = test(100);
-            int x3 = test(0);
+            int x3 = test((argc - 2) / 4);
             printf("*%d,%d*\\n", x3-x1, x2 != x1);
             return 0;
           }
