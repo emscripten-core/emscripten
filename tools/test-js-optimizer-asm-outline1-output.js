f2f45b8499559181ae0a0b1b5e198a480b77e76f
Alon Zakai <alonzakai@gmail.com>
2013-07-16 16:40:56 -0700

    enable outline test and add a case for outlining in a while loop

--- tools/test-js-optimizer-asm-outline1-output.js
+++ tools/test-js-optimizer-asm-outline1-output.js
@@ -6,6 +6,16 @@ function lin() {
  lin$1(sp);
  lin$0(sp);
 }
+function lin2() {
+ while (1) {
+  c(1);
+  c(2);
+  c(3);
+  c(4);
+  lin2$1(sp);
+  lin2$0(sp);
+ }
+}
 function lin$0(sp) {
  sp = sp | 0;
  c(13);
@@ -18,6 +28,28 @@ function lin$0(sp) {
  c(20);
 }
 function lin$1(sp) {
+ sp = sp | 0;
+ c(5);
+ c(6);
+ c(7);
+ c(8);
+ c(9);
+ c(10);
+ c(11);
+ c(12);
+}
+function lin2$0(sp) {
+ sp = sp | 0;
+ c(13);
+ c(14);
+ c(15);
+ c(16);
+ c(17);
+ c(18);
+ c(19);
+ c(20);
+}
+function lin2$1(sp) {
  sp = sp | 0;
  c(5);
  c(6);
