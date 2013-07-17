f09da648e3edf1716477772d85e2556c147fa26a
Alon Zakai <alonzakai@gmail.com>
2013-07-16 17:49:40 -0700

    add test for outlining of continue

--- tools/test-js-optimizer-asm-outline1-output.js
+++ tools/test-js-optimizer-asm-outline1-output.js
@@ -51,6 +51,20 @@ function lin4() {
  }
  return 20;
 }
+function lin5() {
+ while (1) {
+  c(1);
+  c(2);
+  c(3);
+  c(4);
+  lin5$1(sp);
+  lin5$0(sp);
+  if (HEAP32[sp + 0 >> 2] == 3) {
+   continue;
+  }
+ }
+ return 20;
+}
 function lin$0(sp) {
  sp = sp | 0;
  c(13);
@@ -134,6 +148,31 @@ function lin4$0(sp) {
  } while (0);
 }
 function lin4$1(sp) {
+ sp = sp | 0;
+ c(5);
+ c(6);
+ c(7);
+ c(8);
+ c(9);
+ c(10);
+ c(11);
+ c(12);
+}
+function lin5$0(sp) {
+ sp = sp | 0;
+ OL : do {
+  c(13);
+  c(14);
+  c(15);
+  c(16);
+  c(17);
+  c(18);
+  c(19);
+  c(20);
+  HEAP32[sp + 0 >> 2] = 3, break OL;
+ } while (0);
+}
+function lin5$1(sp) {
  sp = sp | 0;
  c(5);
  c(6);
--- tools/test-js-optimizer-asm-outline1.js
+++ tools/test-js-optimizer-asm-outline1.js
@@ -96,5 +96,31 @@ function lin4() {
   }
   return 20;
 }
+function lin5() {
+  while (1) {
+    c(1);
+    c(2);
+    c(3);
+    c(4);
+    c(5);
+    c(6);
+    c(7);
+    c(8);
+    c(9);
+    c(10);
+    c(11);
+    c(12);
+    c(13);
+    c(14);
+    c(15);
+    c(16);
+    c(17);
+    c(18);
+    c(19);
+    c(20);
+    continue;
+  }
+  return 20;
+}
 // EMSCRIPTEN_GENERATED_FUNCTIONS
 // EXTRA_INFO: { "sizeToOutline": 30 }
