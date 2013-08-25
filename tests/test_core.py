8f4fa60b38fe344c178aef97f04b966bb87fcdfb
Alon Zakai <alonzakai@gmail.com>
2013-08-24 23:11:48 -0700

    test for closure+mem init file in a more focused manner

--- tests/test_core.py
+++ tests/test_core.py
@@ -6789,8 +6789,13 @@ def process(filename):
     other.close()
 
     src = open(path_from_root('tests', 'files.cpp'), 'r').read()
+
+    mem_file = 'src.cpp.o.js.mem'
+    try_delete(mem_file)
     self.do_run(src, ('size: 7\ndata: 100,-56,50,25,10,77,123\nloop: 100 -56 50 25 10 77 123 \ninput:hi there!\ntexto\n$\n5 : 10,30,20,11,88\nother=some data.\nseeked=me da.\nseeked=ata.\nseeked=ta.\nfscanfed: 10 - hello\nok.\ntexte\n', 'size: 7\ndata: 100,-56,50,25,10,77,123\nloop: 100 -56 50 25 10 77 123 \ninput:hi there!\ntexto\ntexte\n$\n5 : 10,30,20,11,88\nother=some data.\nseeked=me da.\nseeked=ata.\nseeked=ta.\nfscanfed: 10 - hello\nok.\n'),
                  post_build=post, extra_emscripten_args=['-H', 'libc/fcntl.h'])
+    if self.emcc_args and '--memory-init-file' in self.emcc_args:
+      assert os.path.exists(mem_file)
 
   def test_files_m(self):
     # Test for Module.stdin etc.
