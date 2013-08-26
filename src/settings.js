02cc30eb48181981114d29dc7f82f6af8412c8ce
Alon Zakai <alonzakai@gmail.com>
2013-08-26 16:06:53 -0700

    comment

--- src/settings.js
+++ src/settings.js
@@ -356,7 +356,8 @@ var LINKABLE = 0; // If set to 1, this file can be linked with others, either as
 
 var DLOPEN_SUPPORT = 0; // Whether to support dlopen(NULL, ...) which enables dynamic access to the
                         // module's functions and globals. Implies LINKABLE=1, because we do not want
-                        // dead code elimination.
+                        // dead code elimination. XXX remove LINKABLE=1 here, people should keep
+                        // the things they want alive using the normal mechanisms (see faq).
 
 var RUNTIME_TYPE_INFO = 0; // Whether to expose type info to the script at run time. This
                            // increases the size of the generated script, but allows you
