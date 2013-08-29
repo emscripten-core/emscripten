285db365a3a0a5ccba055c5b4911e2a86befdacb
Alon Zakai <alonzakai@gmail.com>
2013-08-29 15:30:39 -0700

    use a separate stack in each asm module; fixes asm2g.test_dlfcn_stack_forward

--- src/shell_sharedlib.js
+++ src/shell_sharedlib.js
@@ -6,6 +6,13 @@
   Module.print = parentModule.print;
   Module.printErr = parentModule.printErr;
 
+#if ASM_JS
+  // Each module has its own stack
+  var STACKTOP = parentModule['_malloc'](TOTAL_STACK);
+  assert(STACKTOP % 8 == 0);
+  var STACK_MAX = STACKTOP + TOTAL_STACK;
+#endif
+
   {{BODY}}
 
   // {{MODULE_ADDITIONS}}
