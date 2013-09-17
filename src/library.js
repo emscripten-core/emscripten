16f7372c5f340116085567934997e1defc7713cc
ngld <ngld@tproxy.de>
2013-09-17 15:52:42 +0200

    Fix #1214 by implementing the proposed PRNG.

--- src/library.js
+++ src/library.js
@@ -3440,6 +3440,159 @@ LibraryManager.library = {
     return limit;
   },
 
+#if ENABLE_SRAND
+  // The following license (BSD) applies to the mixkey() function, ARC4 implementation and the drand48 function.
+  // The code was adapted from http://davidbau.com/encode/seedrandom.js.
+  //
+  // Copyright 2013 David Bau, all rights reserved.
+  //
+  // Redistribution and use in source and binary forms, with or without
+  // modification, are permitted provided that the following conditions are met:
+  //
+  //   1. Redistributions of source code must retain the above copyright
+  //      notice, this list of conditions and the following disclaimer.
+  //
+  //   2. Redistributions in binary form must reproduce the above copyright
+  //      notice, this list of conditions and the following disclaimer in the
+  //      documentation and/or other materials provided with the distribution.
+  //
+  //   3. Neither the name of this module nor the names of its contributors may
+  //      be used to endorse or promote products derived from this software
+  //      without specific prior written permission.
+  //
+  // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  // A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  // OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  // SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  // LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  // DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  __ARC4: {
+    _inst: null,
+    width: 256,     // each RC4 output is 0 <= x < 256
+    chunks: 6,      // at least six RC4 outputs for each double
+    digits: 52,     // there are 52 significant digits in a double
+    
+    get: function(key) {
+      if (this._inst === null) {
+        this.startdenom = Math.pow(this.width, this.chunks);
+        this.significance = Math.pow(2, this.digits);
+        this.overflow = this.significance * 2;
+        this.mask = this.width - 1;
+      }
+      if (this._inst === null || key) {
+        if (!key) {
+          // Seed the PRNG.
+          key = [];
+          this.mixkey(Math.random(), key);
+        }
+        this._inst = new this._cls(key, this.width);
+      }
+      
+      return this._inst;
+    },
+    //
+    // mixkey()
+    // Mixes a string seed into a key that is an array of integers, and
+    // returns a shortened string seed that is equivalent to the result key.
+    //
+    mixkey: function(seed, key) {
+      var stringseed = seed + '', smear, j = 0;
+      while (j < stringseed.length) {
+        key[this.mask & j] =
+          this.mask & ((smear ^= key[this.mask & j] * 19) + stringseed.charCodeAt(j++));
+      }
+    },
+    //
+    // ARC4
+    //
+    // An ARC4 implementation.  The constructor takes a key in the form of
+    // an array of at most (width) integers that should be 0 <= x < (width).
+    //
+    // The g(count) method returns a pseudorandom integer that concatenates
+    // the next (count) outputs from ARC4.  Its return value is a number x
+    // that is in the range 0 <= x < (width ^ count).
+    //
+    /** @constructor */
+    _cls: function(key, width) {
+      var t, keylen = key.length, mask = width - 1,
+          me = this, i = 0, j = me.i = me.j = 0, s = me.S = [];
+
+      // The empty key [] is treated as [0].
+      if (!keylen) { key = [keylen++]; }
+
+      // Set up S using the standard key scheduling algorithm.
+      while (i < width) {
+        s[i] = i++;
+      }
+      for (i = 0; i < width; i++) {
+        s[i] = s[j = mask & (j + key[i % keylen] + (t = s[i]))];
+        s[j] = t;
+      }
+
+      // The "g" method returns the next (count) outputs as one number.
+      (me.g = function(count) {
+        // Using instance members instead of closure state nearly doubles speed.
+        var t, r = 0,
+            i = me.i, j = me.j, s = me.S;
+        while (count--) {
+          t = s[i = mask & (i + 1)];
+          r = r * width + s[mask & ((s[i] = s[j = mask & (j + t)]) + (s[j] = t))];
+        }
+        me.i = i; me.j = j;
+        return r;
+        // For robust unpredictability discard an initial batch of values.
+        // See http://www.rsa.com/rsalabs/node.asp?id=2009
+      })(width);
+    }
+  },
+  
+  srand__deps: ['__ARC4'],
+  srand: function(seed) {
+    // NOTE: seed is unsigned int.
+    
+    var key = [];
+    ___ARC4.mixkey(seed, key);
+    ___ARC4.get(key);
+  },
+  
+  drand48__deps: ['__ARC4'],
+  drand48: function() {
+    // This function returns a random double in [0, 1) that contains
+    // randomness in every bit of the mantissa of the IEEE 754 value.
+    
+    var arc4 = ___ARC4.get(),
+        n = arc4.g(___ARC4.chunks),                     // Start with a numerator n < 2 ^ 48
+        d = Math.pow(___ARC4.width, ___ARC4.chunks),    //   and denominator d = 2 ^ 48.
+        x = 0;                                          //   and no 'extra last byte'.
+    while (n < ___ARC4.significance) {                  // Fill up all significant digits by
+      n = (n + x) * ___ARC4.width;                      //   shifting numerator and
+      d *= ___ARC4.width;                               //   denominator and generating a
+      x = arc4.g(1);                                    //   new least-significant-byte.
+    }
+    while (n >= ___ARC4.overflow) {                     // To avoid rounding up, before adding
+      n /= 2;                                           //   last byte, shift everything
+      d /= 2;                                           //   right using integer math until
+      x >>>= 1;                                         //   we have exactly the desired bits.
+    }
+    return (n + x) / d;
+  },
+  
+  rand__deps: ['drand48'],
+  rand: function() {
+    return _drand48() * 0x80000000;
+  },
+  rand_r__deps: ['srand', 'drand48'],
+  rand_r: function(seed) {
+    _srand(seed);
+    return _drand48() * 0x80000000;
+  },
+  
+#else
   // Use browser's Math.random(). We can't set a seed, though.
   srand: function(seed) {}, // XXX ignored
   rand: function() {
@@ -3452,6 +3605,7 @@ LibraryManager.library = {
   drand48: function() {
     return Math.random();
   },
+#endif
 
   realpath__deps: ['$FS', '__setErrNo'],
   realpath: function(file_name, resolved_name) {
