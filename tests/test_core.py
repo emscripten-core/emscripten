be93f604b8c5b529814efacd729455856e96ebf0
Alon Zakai <alonzakai@gmail.com>
2013-10-03 10:49:08 -0700

    disable heap align check in test_files, it mixes poorly with closure

--- tests/test_core.py
+++ tests/test_core.py
@@ -7266,6 +7266,7 @@ date: 18.07.2013w; day 18, month  7, year 2013, extra: 201, 3
     if self.emcc_args is not None and '-O2' in self.emcc_args:
       self.emcc_args += ['--closure', '1'] # Use closure here, to test we don't break FS stuff
       self.emcc_args = filter(lambda x: x != '-g', self.emcc_args) # ensure we test --closure 1 --memory-init-file 1 (-g would disable closure)
+      self.emcc_args += ["-s", "CHECK_HEAP_ALIGN=0"] # disable heap align check here, it mixes poorly with closure
 
     Settings.CORRECT_SIGNS = 1 # Just so our output is what we expect. Can flip them both.
     post = '''
