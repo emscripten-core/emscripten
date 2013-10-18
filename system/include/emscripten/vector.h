634513d0c75e7d09a9c9055d4077d631e443ecb4
Alon Zakai <alonzakai@gmail.com>
2013-10-18 11:01:53 -0700

    add vector.h

---
+++ system/include/emscripten/vector.h
@@ -0,0 +1,6 @@
+
+// Support for the JS SIMD API proposal, https://github.com/johnmccutchan/ecmascript_simd
+
+typedef float float32x4 __attribute__((__vector_size__(16)));
+typedef unsigned int uint32x4 __attribute__((__vector_size__(16)));
+
