bc43e51243961d032c19317db7c834ac286f8949
John Vilk <jvilk@cs.umass.edu>
2013-11-07 02:08:51 -0500

    Forgot a semicolon.

--- src/library_sdl.js
+++ src/library_sdl.js
@@ -770,7 +770,7 @@ var LibrarySDL = {
     // value [-32768, 32767]
     joystickAxisValueConversion: function(value) {
       // Ensures that 0 is 0, 1 is 32767, and -1 is 32768.
-      return Math.ceil(((value+1) * 32767.5) - 32768)
+      return Math.ceil(((value+1) * 32767.5) - 32768);
     },
 
     getGamepads: function() {
