9592ccadf69a65fbb742499c7430d5c04085a2b7
Vasilis Kalintiris <ehostunreach@gmail.com>
2013-12-07 14:06:56 +0200

    Use do_run_from_file() for test_gmtime

--- tests/test_core.py
+++ tests/test_core.py
@@ -2456,35 +2456,10 @@ The current type of b is: 9
     self.do_run_from_file(src, output)
 
   def test_gmtime(self):
-    src = r'''
-      #include <stdio.h>
-      #include <stdlib.h>
-      #include <time.h>
-      #include <assert.h>
+    test_path = path_from_root('tests', 'core', 'test_gmtime')
+    src, output = (test_path + s for s in ('.in', '.out'))
 
-      int main(void)
-      {
-          time_t t=time(NULL);
-          struct tm *ptm=gmtime(&t);
-          struct tm tmCurrent=*ptm;
-          int hour=tmCurrent.tm_hour;
-
-          t-=hour*3600; // back to midnight
-          int yday = -1;
-          for(hour=0;hour<24;hour++)
-          {
-              ptm=gmtime(&t);
-              // tm_yday must be constant all day...
-              printf("yday: %d, hour: %d\n", ptm->tm_yday, hour);
-              if (yday == -1) yday = ptm->tm_yday;
-              else assert(yday == ptm->tm_yday);
-              t+=3600; // add one hour
-          }
-          printf("ok!\n");
-          return(0);
-      }
-    '''
-    self.do_run(src, '''ok!''')
+    self.do_run_from_file(src, output)
 
   def test_strptime_tm(self):
     src=r'''
