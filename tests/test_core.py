547966c6f728c6aa4743383c68fc0ac8e8303aba
Vasilis Kalintiris <ehostunreach@gmail.com>
2013-12-07 16:24:10 +0200

    Use do_run_from_file() for test_poll

--- tests/test_core.py
+++ tests/test_core.py
@@ -3981,46 +3981,10 @@ def process(filename):
   )
   open(filename, 'w').write(src)
 '''
-    src = r'''
-      #include <stdio.h>
-      #include <errno.h>
-      #include <fcntl.h>
-      #include <poll.h>
+    test_path = path_from_root('tests', 'core', 'test_poll')
+    src, output = (test_path + s for s in ('.in', '.out'))
 
-      int main() {
-        struct pollfd multi[5];
-        multi[0].fd = open("/file", O_RDONLY, 0777);
-        multi[1].fd = open("/device", O_RDONLY, 0777);
-        multi[2].fd = 123;
-        multi[3].fd = open("/file", O_RDONLY, 0777);
-        multi[4].fd = open("/file", O_RDONLY, 0777);
-        multi[0].events = POLLIN | POLLOUT | POLLNVAL | POLLERR;
-        multi[1].events = POLLIN | POLLOUT | POLLNVAL | POLLERR;
-        multi[2].events = POLLIN | POLLOUT | POLLNVAL | POLLERR;
-        multi[3].events = 0x00;
-        multi[4].events = POLLOUT | POLLNVAL | POLLERR;
-
-        printf("ret: %d\n", poll(multi, 5, 123));
-        printf("errno: %d\n", errno);
-        printf("multi[0].revents: %d\n", multi[0].revents == (POLLIN | POLLOUT));
-        printf("multi[1].revents: %d\n", multi[1].revents == (POLLIN | POLLOUT));
-        printf("multi[2].revents: %d\n", multi[2].revents == POLLNVAL);
-        printf("multi[3].revents: %d\n", multi[3].revents == 0);
-        printf("multi[4].revents: %d\n", multi[4].revents == POLLOUT);
-
-        return 0;
-      }
-      '''
-    expected = r'''
-      ret: 4
-      errno: 0
-      multi[0].revents: 1
-      multi[1].revents: 1
-      multi[2].revents: 1
-      multi[3].revents: 1
-      multi[4].revents: 1
-      '''
-    self.do_run(src, re.sub('(^|\n)\s+', '\\1', expected), post_build=add_pre_run, extra_emscripten_args=['-H', 'libc/fcntl.h,poll.h'])
+    self.do_run_from_file(src, output, post_build=add_pre_run, extra_emscripten_args=['-H', 'libc/fcntl.h,poll.h'])
 
   def test_statvfs(self):
     src = r'''
