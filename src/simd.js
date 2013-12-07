b2cf264470afce659451a4d74b5a5bbe6807dad7
Heidi Pan <heidi.pan@intel.com>
2013-12-06 17:55:39 -0800

    fix accidental rewrite of original url source of simd.js

--- src/simd.js
+++ src/simd.js
@@ -16,6 +16,8 @@
   2. Altered source versions must be plainly marked as such, and must not be
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
+
+  https://github.com/johnmccutchan/ecmascript_simd/blob/master/src/ecmascript_simd.js
 */
 
 "use strict";
