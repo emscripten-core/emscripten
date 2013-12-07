155d737f1fa091ca34404f73d7fc315af4439e09
Vasilis Kalintiris <ehostunreach@gmail.com>
2013-12-07 12:21:04 +0200

    Use do_run_from_file() for test_constglobalstructs

--- tests/test_core.py
+++ tests/test_core.py
@@ -1725,37 +1725,10 @@ class T(RunnerCore): # Short name, to make it more fun to use manually on the co
     self.do_run_from_file(src, output)
 
   def test_constglobalstructs(self):
-      src = '''
-        #include <stdio.h>
-        struct IUB {
-           int c;
-           double p;
-           unsigned int pi;
-        };
+    test_path = path_from_root('tests', 'core', 'test_constglobalstructs')
+    src, output = (test_path + s for s in ('.in', '.out'))
 
-        IUB iub[] = {
-           { 'a', 0.27, 5 },
-           { 'c', 0.15, 4 },
-           { 'g', 0.12, 3 },
-           { 't', 0.27, 2 },
-        };
-
-        const unsigned char faceedgesidx[6][4] =
-        {
-            { 4, 5, 8, 10 },
-            { 6, 7, 9, 11 },
-            { 0, 2, 8, 9  },
-            { 1, 3, 10,11 },
-            { 0, 1, 4, 6 },
-            { 2, 3, 5, 7 },
-        };
-
-        int main( int argc, const char *argv[] ) {
-           printf("*%d,%d,%d,%d*\\n", iub[0].c, int(iub[1].p*100), iub[2].pi, faceedgesidx[3][2]);
-           return 0;
-        }
-        '''
-      self.do_run(src, '*97,15,3,10*')
+    self.do_run_from_file(src, output)
 
   def test_conststructs(self):
       src = '''
