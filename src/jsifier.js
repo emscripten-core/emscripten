08e2026a2132e26829b296e473e8517c4476cb86
Lu Wang <coolwanglu@gmail.com>
2014-01-02 00:09:12 +0800

    ..

--- src/jsifier.js
+++ src/jsifier.js
@@ -1140,6 +1140,7 @@ function JSify(data, functionsOnly) {
       }
     }
   }
+  // TODO: need to handle async cases?
   function switchHandler(item) {
     // use a switch if the range is not too big or sparse
     var minn = Infinity, maxx = -Infinity;
