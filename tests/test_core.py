5528ef9f827a475d71574b56783f8d4d07637f60
Alon Zakai <alonzakai@gmail.com>
2014-01-03 11:22:30 -0800

    disable test_emscripten_log in spidermonkey due to breakage; issue #1970

--- tests/test_core.py
+++ tests/test_core.py
@@ -6091,6 +6091,7 @@ def process(filename):
     self.build(src, dirname, os.path.join(dirname, 'src.cpp'), post_build=(None, post))
 
   def test_emscripten_log(self):
+    self.banned_js_engines = [SPIDERMONKEY_ENGINE] # XXX, emscripten_log is broken in spidermonkey currently, issue #1970
     if self.emcc_args is None: return self.skip('This test needs libc.')
     if '-g' not in Building.COMPILER_TEST_OPTS: Building.COMPILER_TEST_OPTS.append('-g')
     self.do_run('#define RUN_FROM_JS_SHELL\n' + open(path_from_root('tests', 'emscripten_log', 'emscripten_log.cpp')).read(), "Success!")
