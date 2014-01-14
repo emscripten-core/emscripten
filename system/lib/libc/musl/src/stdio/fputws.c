c5522c645635939ac49e6cfecb02a64e768a76d6
Alon Zakai <alonzakai@gmail.com>
2014-01-13 17:48:55 -0800

    add musl fputws, fix vswprintf, add testing for fwprintf and swprintf as well

---
+++ system/lib/libc/musl/src/stdio/fputws.c
@@ -0,0 +1,30 @@
+#include "stdio_impl.h"
+#include <wchar.h>
+
+int fputws(const wchar_t *restrict ws, FILE *restrict f)
+{
+	unsigned char buf[BUFSIZ];
+	size_t l=0;
+
+	FLOCK(f);
+
+#if 0 // XXX EMSCRIPTEN
+	f->mode |= f->mode+1;
+#endif
+
+	while (ws && (l = wcsrtombs((void *)buf, (void*)&ws, sizeof buf, 0))+1 > 1)
+#if 0 // XXX EMSCRIPTEN
+		if (__fwritex(buf, l, f) < l) {
+#else
+		if (fwrite(buf, 1, l, f) < l) {
+#endif
+			FUNLOCK(f);
+			return -1;
+		}
+
+	FUNLOCK(f);
+
+	return l; /* 0 or -1 */
+}
+
+weak_alias(fputws, fputws_unlocked);
