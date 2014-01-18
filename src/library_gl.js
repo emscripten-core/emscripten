7a1c4076a28a4a7eee344dff199c43d03c996f2c
Jukka Jylänki <jujjyl@gmail.com>
2014-01-18 12:52:43 +0200

    Fix GL emulation cleanup to properly update the state it changes.

--- src/library_gl.js
+++ src/library_gl.js
@@ -3972,6 +3972,7 @@ var LibraryGL = {
           }
           if (!GL.currProgram) {
             GLctx.useProgram(null);
+            GLImmediate.fixedFunctionProgram = 0;
           }
           if (!GL.currArrayBuffer) {
             GLctx.bindBuffer(GLctx.ARRAY_BUFFER, null);
