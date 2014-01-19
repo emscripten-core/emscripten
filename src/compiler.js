7fff4e3eaa209df9a7f967a89dca1362d1ede536
Alon Zakai <alonzakai@gmail.com>
2014-01-19 12:09:18 -0800

    better error message for asm.js not supporting growable heap yet

--- src/compiler.js
+++ src/compiler.js
@@ -183,9 +183,6 @@ if (SAFE_HEAP) USE_BSS = 0; // must initialize heap for safe heap
 // Settings sanity checks
 
 assert(!(USE_TYPED_ARRAYS === 2 && QUANTUM_SIZE !== 4), 'For USE_TYPED_ARRAYS == 2, must have normal QUANTUM_SIZE of 4');
-if (ASM_JS) {
-  assert(!ALLOW_MEMORY_GROWTH, 'Cannot grow asm.js heap');
-}
 assert(!(!NAMED_GLOBALS && BUILD_AS_SHARED_LIB), 'shared libraries must have named globals');
 
 // Output some info and warnings based on settings
