8232a25d37d77fe523757ed0cf32557df07e011a
Alon Zakai <alonzakai@gmail.com>
2014-01-20 12:03:21 -0800

    enable first setjmp test in fastcomp

--- tests/test_core.py
+++ tests/test_core.py
@@ -1117,12 +1117,10 @@ class T(RunnerCore): # Short name, to make it more fun to use manually on the co
       self.do_run_from_file(src, output)
 
   def test_longjmp(self):
-      if os.environ.get('EMCC_FAST_COMPILER') == '1': return self.skip('todo in fastcomp')
-
-      test_path = path_from_root('tests', 'core', 'test_longjmp')
-      src, output = (test_path + s for s in ('.in', '.out'))
-
-      self.do_run_from_file(src, output)
+    if os.environ.get('EMCC_FAST_COMPILER') == '1': Settings.DISABLE_EXCEPTION_CATCHING = 1
+    test_path = path_from_root('tests', 'core', 'test_longjmp')
+    src, output = (test_path + s for s in ('.in', '.out'))
+    self.do_run_from_file(src, output)
 
   def test_longjmp2(self):
     if os.environ.get('EMCC_FAST_COMPILER') == '1': return self.skip('todo in fastcomp')
