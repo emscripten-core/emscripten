cde8796577b2d1959b5322172bec85ba6b10647c
Alon Zakai <alonzakai@gmail.com>
2014-02-28 12:50:02 -0800

    fix some whitespace

--- src/library_browser.js
+++ src/library_browser.js
@@ -622,53 +622,52 @@ mergeInto(LibraryManager.library, {
     },
 
     updateCanvasDimensions : function(canvas, wNative, hNative) {
-        if (wNative && hNative) {
-          canvas.widthNative = wNative;
-          canvas.heightNative = hNative;
+      if (wNative && hNative) {
+        canvas.widthNative = wNative;
+        canvas.heightNative = hNative;
+      } else {
+        wNative = canvas.widthNative;
+        hNative = canvas.heightNative;
+      }
+      var w = wNative;
+      var h = hNative;
+      if (Module['forcedAspectRatio'] && Module['forcedAspectRatio'] > 0) {
+        if (w/h < Module['forcedAspectRatio']) {
+          w = Math.round(h * Module['forcedAspectRatio']);
         } else {
-          wNative = canvas.widthNative;
-          hNative = canvas.heightNative;
+          h = Math.round(w / Module['forcedAspectRatio']);
         }
-        var w = wNative;
-        var h = hNative;
-        if (Module['forcedAspectRatio'] && Module['forcedAspectRatio'] > 0) {
-          if (w/h < Module['forcedAspectRatio']) {
-            w = Math.round(h * Module['forcedAspectRatio']);
+      }
+      if (((document['webkitFullScreenElement'] || document['webkitFullscreenElement'] ||
+           document['mozFullScreenElement'] || document['mozFullscreenElement'] ||
+           document['fullScreenElement'] || document['fullscreenElement'] ||
+           document['msFullScreenElement'] || document['msFullscreenElement'] ||
+           document['webkitCurrentFullScreenElement']) === canvas.parentNode) && (typeof screen != 'undefined')) {
+         var factor = Math.min(screen.width / w, screen.height / h);
+         w = Math.round(w * factor);
+         h = Math.round(h * factor);
+      }
+      if (Browser.resizeCanvas) {
+        if (canvas.width  != w) canvas.width  = w;
+        if (canvas.height != h) canvas.height = h;
+        if (typeof canvas.style != 'undefined') {
+          canvas.style.removeProperty( "width");
+          canvas.style.removeProperty("height");
+        }
+      } else {
+        if (canvas.width  != wNative) canvas.width  = wNative;
+        if (canvas.height != hNative) canvas.height = hNative;
+        if (typeof canvas.style != 'undefined') {
+          if (w != wNative || h != hNative) {
+            canvas.style.setProperty( "width", w + "px", "important");
+            canvas.style.setProperty("height", h + "px", "important");
           } else {
-            h = Math.round(w / Module['forcedAspectRatio']);
-          }
-        }
-        if (((document['webkitFullScreenElement'] || document['webkitFullscreenElement'] ||
-             document['mozFullScreenElement'] || document['mozFullscreenElement'] ||
-             document['fullScreenElement'] || document['fullscreenElement'] ||
-             document['msFullScreenElement'] || document['msFullscreenElement'] ||
-             document['webkitCurrentFullScreenElement']) === canvas.parentNode) && (typeof screen != 'undefined')) {
-           var factor = Math.min(screen.width / w, screen.height / h);
-           w = Math.round(w * factor);
-           h = Math.round(h * factor);
-        }
-        if (Browser.resizeCanvas) {
-          if (canvas.width  != w) canvas.width  = w;
-          if (canvas.height != h) canvas.height = h;
-          if (typeof canvas.style != 'undefined') {
             canvas.style.removeProperty( "width");
             canvas.style.removeProperty("height");
           }
-        } else {
-          if (canvas.width  != wNative) canvas.width  = wNative;
-          if (canvas.height != hNative) canvas.height = hNative;
-          if (typeof canvas.style != 'undefined') {
-            if (w != wNative || h != hNative) {
-              canvas.style.setProperty( "width", w + "px", "important");
-              canvas.style.setProperty("height", h + "px", "important");
-            } else {
-              canvas.style.removeProperty( "width");
-              canvas.style.removeProperty("height");
-            }
-          }
         }
       }
-
+    }
   },
 
   emscripten_async_wget: function(url, file, onload, onerror) {
