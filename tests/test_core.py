fe4bffcd5171d47e7d4e21e65023bdaec233cdcc
Alon Zakai <alonzakai@gmail.com>
2014-03-10 13:18:43 -0700

    restore some closure testing

--- tests/test_core.py
+++ tests/test_core.py
@@ -1938,7 +1938,7 @@ def process(filename):
   def test_emscripten_get_now(self):
       if Settings.USE_TYPED_ARRAYS != 2: return self.skip('requires ta2')
 
-      if self.run_name == 'o2':
+      if self.run_name == 'slow2asm':
         self.emcc_args += ['--closure', '1'] # Use closure here for some additional coverage
       self.do_run(open(path_from_root('tests', 'emscripten_get_now.cpp')).read(), 'Timer resolution is good.')
 
