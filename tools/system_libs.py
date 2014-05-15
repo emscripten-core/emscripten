35533bfd8524e04291b85ac7ea444b24283c83dc
Jukka Jylänki <jujjyl@gmail.com>
2014-05-15 23:08:55 +0300

    Migrate clock(), time(), gmtime(), gmtime_r() and difftime() to use musl libc for asm.js performance.

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -103,6 +103,9 @@ def calculate(temp_files, in_temp, stdout, stderr):
        'strcmp.c',
        'strncasecmp.c',
        'strncmp.c',
+      ]],
+      ['time', [
+       'time.c',
       ]]
     ]
     for directory, sources in musl_files:
@@ -343,6 +346,14 @@ def calculate(temp_files, in_temp, stdout, stderr):
          'wmemcpy.c',
          'wmemmove.c',
          'wmemset.c',
+       ]],
+       ['time', [
+         '__secs_to_tm.c',
+         '__tz.c',
+         'clock.c',
+         'difftime.c',
+         'gmtime_r.c',
+         'gmtime.c',
        ]]
     ]
     libcextra_files = []
