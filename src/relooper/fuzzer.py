72dbb4c60a4ee7d786e3ffaddf94fc7a7b7d2e19
Alon Zakai <alonzakai@gmail.com>
2014-05-20 12:57:41 -0700

    fuzz larger testcases in relooper fuzzer

--- src/relooper/fuzzer.py
+++ src/relooper/fuzzer.py
@@ -3,7 +3,7 @@ import random, subprocess, difflib
 
 while True:
   # Random decisions
-  num = random.randint(2, 250)
+  num = random.randint(2, 500)
   density = random.random() * random.random()
   decisions = [random.randint(1, num*20) for x in range(num*3)]
   branches = [0]*num
