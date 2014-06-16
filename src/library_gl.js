ae49b8f0bde8098e2b2361cf1eecd0d93002fea6
Alon Zakai <alonzakai@gmail.com>
2014-06-16 16:35:07 -0700

    return 0 in gl emulation code for GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT

--- src/library_gl.js
+++ src/library_gl.js
@@ -4347,6 +4347,10 @@ var LibraryGL = {
             }
             break;
           }
+          case 0x84FF: { // GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT
+            {{{ makeSetValue('params', '0', '0', 'i32') }}}; // no valid value to return here
+            return;
+          }
         }
         glGetIntegerv(pname, params);
       };
