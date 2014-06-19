04df9e7deda3ebf698ea0cea5cc58c04203f2c7f
Alon Zakai <alonzakai@gmail.com>
2014-06-19 12:50:25 -0700

    assume pointer lock in workers, to get some mouse support working

--- src/library_browser.js
+++ src/library_browser.js
@@ -64,7 +64,11 @@ mergeInto(LibraryManager.library, {
       }
     },
     isFullScreen: false,
+#if PROXY_TO_WORKER == 0
     pointerLock: false,
+#else
+    pointerLock: true, // XXX assume pointerlock in workers, for now
+#endif
     moduleContextCreatedCallbacks: [],
     workers: [],
 
