def1a97e075acb5fd23284d1a862e778366cf8ad
Alon Zakai <alonzakai@gmail.com>
2014-07-23 13:37:48 -0700

    fix truncate extending a file from size 0; fixes #2572

--- tests/test_other.py
+++ tests/test_other.py
@@ -3521,3 +3521,119 @@ int main()
     Popen([PYTHON, EMCC, 'src.cpp']).communicate()
     self.assertContained('ok!', run_js('a.out.js'))
 
+  def test_truncate_from_0(self):
+    open('src.cpp', 'w').write(r'''
+#include <cerrno>
+#include <cstring>
+#include <iostream>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using std::endl;
+
+//============================================================================
+// :: Helpers
+
+namespace
+{
+  // Returns the size of the regular file specified as 'path'.
+  ::off_t getSize(const char* const path)
+  {
+    // Stat the file and make sure that it's the expected size.
+    struct ::stat path_stat;
+    if (::stat(path, &path_stat) != 0) {
+      const int error = errno;
+      std::cout
+        << "Failed to lstat path: " << path << "; errno=" << error << "; "
+        << std::strerror(error) << endl;
+      return -1;
+    }
+
+    std::cout
+      << "Size of file is: " << path_stat.st_size << endl;
+    return path_stat.st_size;
+  }
+
+  // Causes the regular file specified in 'path' to have a size of 'length'
+  // bytes.
+  void resize(const char* const path,
+              const ::off_t length)
+  {
+    std::cout
+      << "Truncating file=" << path << " to length=" << length << endl;
+    if (::truncate(path, length) == -1)
+    {
+      const int error = errno;
+      std::cout
+        << "Failed to truncate file=" << path << "; errno=" << error
+        << "; " << std::strerror(error) << endl;
+    }
+
+    const ::off_t size = getSize(path);
+    if (size != length) {
+      std::cout
+        << "Failed to truncate file=" << path << " to length=" << length
+        << "; got size=" << size << endl;
+    }
+  }
+
+  // Helper to create a file with the given content.
+  void createFile(const std::string& path, const std::string& content)
+  {
+    std::cout
+      << "Creating file: " << path << " with content=" << content << endl;
+
+    const int fd = ::open(path.c_str(), O_CREAT | O_WRONLY, 0644);
+    if (fd == -1) {
+      const int error = errno;
+      std::cout
+        << "Failed to open file for writing: " << path << "; errno=" << error
+        << "; " << std::strerror(error) << endl;
+      return;
+    }
+
+    if (::write(fd, content.c_str(), content.size()) != content.size()) {
+      const int error = errno;
+      std::cout
+        << "Failed to write content=" << content << " to file=" << path
+        << "; errno=" << error << "; " << std::strerror(error) << endl;
+
+      // Fall through to close FD.
+    }
+
+    ::close(fd);
+  }
+}
+
+//============================================================================
+// :: Entry Point
+int main()
+{
+  const char* const file = "/tmp/file";
+  createFile(file, "This is some content");
+  getSize(file);
+  resize(file, 32);
+  resize(file, 17);
+  resize(file, 0);
+
+  // This throws a JS exception.
+  resize(file, 32);
+  return 0;
+}
+''')
+    Popen([PYTHON, EMCC, 'src.cpp']).communicate()
+    self.assertContained(r'''Creating file: /tmp/file with content=This is some content
+Size of file is: 20
+Truncating file=/tmp/file to length=32
+Size of file is: 32
+Truncating file=/tmp/file to length=17
+Size of file is: 17
+Truncating file=/tmp/file to length=0
+Size of file is: 0
+Truncating file=/tmp/file to length=32
+Size of file is: 32
+''', run_js('a.out.js'))
+
