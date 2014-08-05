emscripten-core/emscripten

commit 398cded47b7e08fbb753fb7b4788da29a22195bb

merge loop and helper variables when their use ranges do not overlap

tools/eliminator/asm-eliminator-test-output.js
@@ -816,7 +816,7 @@ function selfAssign() {
 function elimOneLoopVar($argc, $argv) {
  $argc = $argc | 0;
  $argv = $argv | 0;
- var $arg$0 = 0, $call10 = Math_fround(0), $curri$012 = 0, $j$010 = 0, $ok$0 = 0, $primes$011 = 0, $retval$0 = 0, $vararg_buffer1 = 0, $j$010$looptemp = 0;
+ var $arg$0 = 0, $call10 = Math_fround(0), $curri$012 = 0, $j$010 = 0, $ok$0 = 0, $primes$011 = 0, $retval$0 = 0, $vararg_buffer1 = 0;
  $curri$012 = 2;
  $primes$011 = 0;
  while (1) {
@@ -825,12 +825,11 @@ function elimOneLoopVar($argc, $argv) {
    if ($call10 > Math_fround(+2)) {
     $j$010 = 2;
     while (1) {
-     $j$010$looptemp = $j$010;
-     $j$010 = $j$010 + 1 | 0;
-     if ((($curri$012 | 0) % ($j$010$looptemp | 0) & -1 | 0) == 0) {
+     if ((($curri$012 | 0) % ($j$010 | 0) & -1 | 0) == 0) {
       $ok$0 = 0;
       break L15;
      }
+     $j$010 = $j$010 + 1 | 0;
      if (!(Math_fround($j$010 | 0) < $call10)) {
       $ok$0 = 1;
       break;
@@ -895,10 +894,23 @@ function elimOneLoopVar4() {
  }
 }
 function elimOneLoopVarStillUsed() {
+ var $call10 = Math_fround(0), $curri$012 = 0, $j$010 = 0, $retval$0 = 0;
+ while (1) {
+  if ((($curri$012 | 0) % ($j$010 | 0) & -1 | 0) == 0) {
+   break;
+  }
+  $j$010 = $j$010 + 1 | 0;
+  if (!(Math_fround($j$010 | 0) < $call10)) {
+   break;
+  }
+ }
+ return $retval$0 | 0;
+}
+function elimOneLoopVarStillUsedSE() {
  var $call10 = Math_fround(0), $curri$012 = 0, $j$010 = 0, $retval$0 = 0, $j$010$looptemp = 0;
  while (1) {
   $j$010$looptemp = $j$010;
-  $j$010 = $j$010 + 1 | 0;
+  $j$010 = $j$010 + sideeffect() | 0;
   if ((($curri$012 | 0) % ($j$010$looptemp | 0) & -1 | 0) == 0) {
    break;
   }

tools/js-optimizer.js
@@ -1034,6 +1034,27 @@ function hasSideEffects(node) { // this is 99% incomplete!
   }
 }
 
+// checks if a node has just basic operations, nothing with side effects nor that can notice side effects, which
+// implies we can move it around in the code
+function triviallySafeToMove(node, asmData) {
+  var ok = true;
+  traverse(node, function(node, type) {
+    switch (type) {
+      case 'stat': case 'binary': case 'unary-prefix': case 'assign': case 'num':
+        break;
+      case 'name':
+        if (!(node[1] in asmData.vars) && !(node[1] in asmData.params)) ok = false;
+        break;
+      case 'call':
+        if (callHasSideEffects(node)) ok = false;
+        break;
+      default:
+        ok = false;
+    }  
+  });
+  return ok;
+}
+
 // Clear out empty ifs and blocks, and redundant blocks/stats and so forth
 // Operates on generated functions only
 function vacuum(ast) {
@@ -3662,21 +3683,51 @@ function eliminate(ast, memSafe) {
               // if a loop variable is used after we assigned to the helper, we must save its value and use that.
               // (note that this can happen due to elimination, if we eliminate an expression containing the
               // loop var far down, past the assignment!)
-              var temp = looper + '$looptemp';
-              var looperUsed = false;
-              assert(!(temp in asmData.vars)); 
+              // first, see if the looper and helper overlap
+              var firstLooperUsage = -1;
+              var lastLooperUsage = -1;
+              var firstHelperUsage = -1;
+              var lastHelperUsage = -1;
               for (var i = found+1; i < stats.length; i++) {
                 var curr = i < stats.length-1 ? stats[i] : last[1]; // on the last line, just look in the condition
                 traverse(curr, function(node, type) {
-                  if (type === 'name' && node[1] === looper) {
-                    node[1] = temp;
-                    looperUsed = true;
+                  if (type === 'name') {
+                    if (node[1] === looper) {
+                      if (firstLooperUsage < 0) firstLooperUsage = i;
+                      lastLooperUsage = i;
+                    } else if (node[1] === helper) {
+                      if (firstHelperUsage < 0) firstHelperUsage = i;
+                      lastHelperUsage = i;
+                    }
                   }
                 });
               }
-              if (looperUsed) {
-                asmData.vars[temp] = asmData.vars[looper];
-                stats.splice(found, 0, ['stat', ['assign', true, ['name', temp], ['name', looper]]]);
+              if (firstLooperUsage >= 0) {
+                // the looper is used, we cannot simply merge the two variables
+                if ((firstHelperUsage < 0 || firstHelperUsage > lastLooperUsage) && lastLooperUsage+1 < stats.length && triviallySafeToMove(stats[found], asmData)) {
+                  // the helper is not used, or it is used after the last use of the looper, so they do not overlap,
+                  // and the last looper usage is not on the last line (where we could not append after it), and the
+                  // just move the looper definition to after the looper's last use
+                  stats.splice(lastLooperUsage+1, 0, stats[found]);
+                  stats.splice(found, 1);
+                } else {
+                  // they overlap, we can still proceed with the loop optimization, but we must introduce a
+                  // loop temp helper variable
+                  var temp = looper + '$looptemp';
+                  assert(!(temp in asmData.vars)); 
+                  for (var i = firstLooperUsage; i <= lastLooperUsage; i++) {
+                    var curr = i < stats.length-1 ? stats[i] : last[1]; // on the last line, just look in the condition
+                    traverse(curr, function(node, type) {
+                      if (type === 'name') {
+                        if (node[1] === looper) {
+                          node[1] = temp;
+                        }
+                      }
+                    });
+                  }
+                  asmData.vars[temp] = asmData.vars[looper];
+                  stats.splice(found, 0, ['stat', ['assign', true, ['name', temp], ['name', looper]]]);
+                }
               }
             }
             for (var l = 0; l < helpers.length; l++) {

tools/eliminator/asm-eliminator-test.js
@@ -1152,6 +1152,27 @@ function elimOneLoopVarStillUsed() {
  }
  return $retval$0 | 0;
 }
+function elimOneLoopVarStillUsedSE() {
+ var $0 = 0, $1 = 0, $arg$0 = 0, $arrayidx = 0, $call10 = Math_fround(0), $cmp = 0, $cmp11 = 0, $cmp119 = 0, $cmp12 = 0, $cmp7 = 0, $conv = 0, $conv8 = Math_fround(0), $conv9 = Math_fround(0), $curri$012 = 0, $inc = 0, $inc14$primes$0 = 0, $inc16 = 0, $j$010 = 0, $ok$0 = 0;
+ var $primes$011 = 0, $rem = 0, $retval$0 = 0, $sub = 0, $vararg_buffer1 = 0, label = 0, sp = 0;
+ while (1) {
+  $rem = ($curri$012 | 0) % ($j$010 | 0) & -1;
+  $cmp12 = ($rem | 0) == 0;
+  $inc = $j$010 + sideeffect() | 0; // side effect!
+  if ($cmp12) {
+   $ok$0 = 0;
+   break;
+  }
+  $conv8 = Math_fround($inc | 0);
+  $cmp11 = $conv8 < $call10;
+  if ($cmp11) {
+   $j$010 = $inc;
+  } else {
+   break;
+  }
+ }
+ return $retval$0 | 0;
+}
 function elimOneLoopVar5() {
  var $storemerge3$neg9 = 0, $18 = 0, $25 = 0, $26 = 0, $30 = 0, $jp = 0;
  $storemerge3$neg9 = -1;
@@ -1168,5 +1189,5 @@ function elimOneLoopVar5() {
   }
  }
 }
-// EMSCRIPTEN_GENERATED_FUNCTIONS: ["asm", "__Z11printResultPiS_j", "_segment_holding", "__ZN5identC2EiPKcPci", "_vec2Length", "exc", "label", "confuusion", "tempDouble", "_org_apache_harmony_luni_util_NumberConverter_freeFormat__", "__ZN23b2EdgeAndPolygonContact8EvaluateEP10b2ManifoldRK11b2TransformS4_", "_java_nio_charset_Charset_forNameInternal___java_lang_String", "looop2", "looop3", "looop4", "looop5", "looop6", "looop7", "looop8", "multiloop", "multiloop2", "tempDouble2", "watIf", "select2", "binary", "cute", "selfAssign", "elimOneLoopVar", "elimOneLoopVar2", "elimOneLoopVar3", "elimOneLoopVar4", "elimOneLoopVarStillUsed", "elimOneLoopVar5"]
+// EMSCRIPTEN_GENERATED_FUNCTIONS: ["asm", "__Z11printResultPiS_j", "_segment_holding", "__ZN5identC2EiPKcPci", "_vec2Length", "exc", "label", "confuusion", "tempDouble", "_org_apache_harmony_luni_util_NumberConverter_freeFormat__", "__ZN23b2EdgeAndPolygonContact8EvaluateEP10b2ManifoldRK11b2TransformS4_", "_java_nio_charset_Charset_forNameInternal___java_lang_String", "looop2", "looop3", "looop4", "looop5", "looop6", "looop7", "looop8", "multiloop", "multiloop2", "tempDouble2", "watIf", "select2", "binary", "cute", "selfAssign", "elimOneLoopVar", "elimOneLoopVar2", "elimOneLoopVar3", "elimOneLoopVar4", "elimOneLoopVarStillUsed", "elimOneLoopVarStillUsedSE", "elimOneLoopVar5"]
 

commit d2cba267957c8555afcb4a4dad0d5c14923df734

be careful with loop helpers, any of them may require a looptemp, not just the one matching the loop var, due to substitution of them all at once; fixes a regression from 398cded47b7e08fbb753fb7b4788da29a22195bb ; fixes #2626

tools/eliminator/asm-eliminator-test-output.js
@@ -968,4 +968,49 @@ function helperExtraUse() {
  }
  return i;
 }
+function mixed_up_loop_helpers() {
+ var $iterations$0 = 0, $iterations$0$ph = 0, $p$sroa$0$0$ph = +0, $p$sroa$1$0$ph = +0, $p$sroa$1$0$ph$phi = +0, $vararg_buffer = 0, $vararg_ptr1 = 0, sp = 0, $iterations$0$looptemp = 0, $p$sroa$0$0$ph$looptemp = +0;
+ sp = STACKTOP;
+ STACKTOP = STACKTOP + 16 | 0;
+ $vararg_buffer = sp;
+ $iterations$0$ph = 0;
+ $p$sroa$0$0$ph = +0;
+ $p$sroa$1$0$ph = +1;
+ L1 : while (1) {
+  $iterations$0 = $iterations$0$ph;
+  while (1) {
+   $iterations$0$looptemp = $iterations$0;
+   $iterations$0 = $iterations$0 + 1 | 0;
+   if (!(($iterations$0 | 0) < 10)) {
+    break L1;
+   }
+   HEAPF64[tempDoublePtr >> 3] = $p$sroa$0$0$ph;
+   HEAP32[$vararg_buffer >> 2] = HEAP32[tempDoublePtr >> 2];
+   HEAP32[$vararg_buffer + 4 >> 2] = HEAP32[tempDoublePtr + 4 >> 2];
+   $vararg_ptr1 = $vararg_buffer + 8 | 0;
+   HEAPF64[tempDoublePtr >> 3] = $p$sroa$1$0$ph;
+   HEAP32[$vararg_ptr1 >> 2] = HEAP32[tempDoublePtr >> 2];
+   HEAP32[$vararg_ptr1 + 4 >> 2] = HEAP32[tempDoublePtr + 4 >> 2];
+   _printf(8 | 0, $vararg_buffer | 0) | 0;
+   if (($iterations$0$looptemp | 0) > 0) {
+    break;
+   }
+  }
+  if (!((($iterations$0 | 0) % 3 & -1 | 0) == 0)) {
+   $p$sroa$1$0$ph$phi = $p$sroa$0$0$ph;
+   $iterations$0$ph = $iterations$0;
+   $p$sroa$0$0$ph = -$p$sroa$1$0$ph;
+   $p$sroa$1$0$ph = $p$sroa$1$0$ph$phi;
+   continue;
+  }
+  $p$sroa$0$0$ph$looptemp = $p$sroa$0$0$ph;
+  $p$sroa$0$0$ph = $p$sroa$1$0$ph * +-.5;
+  $p$sroa$1$0$ph = $p$sroa$0$0$ph$looptemp * +.5;
+  if ($p$sroa$0$0$ph == +0 & $p$sroa$1$0$ph == +0) {
+   break;
+  } else {
+   $iterations$0$ph = $iterations$0;
+  }
+ }
+}
 

tools/js-optimizer.js
@@ -4104,11 +4104,12 @@ function eliminate(ast, memSafe) {
               // if a loop variable is used after we assigned to the helper, we must save its value and use that.
               // (note that this can happen due to elimination, if we eliminate an expression containing the
               // loop var far down, past the assignment!)
-              // first, see if the looper and helper overlap
+              // first, see if the looper and helpers overlap. Note that we check for this looper, compared to
+              // *ALL* the helpers. Helpers will be replaced by loopers as we eliminate them, potentially
+              // causing conflicts, so any helper is a concern.
               var firstLooperUsage = -1;
               var lastLooperUsage = -1;
               var firstHelperUsage = -1;
-              var lastHelperUsage = -1;
               for (var i = found+1; i < stats.length; i++) {
                 var curr = i < stats.length-1 ? stats[i] : last[1]; // on the last line, just look in the condition
                 traverse(curr, function(node, type) {
@@ -4116,9 +4117,8 @@ function eliminate(ast, memSafe) {
                     if (node[1] === looper) {
                       if (firstLooperUsage < 0) firstLooperUsage = i;
                       lastLooperUsage = i;
-                    } else if (node[1] === helper) {
+                    } else if (helpers.indexOf(node[1]) >= 0) {
                       if (firstHelperUsage < 0) firstHelperUsage = i;
-                      lastHelperUsage = i;
                     }
                   }
                 });

tools/eliminator/asm-eliminator-test.js
@@ -1223,5 +1223,61 @@ function helperExtraUse() {
  }
  return inc;
 }
-// EMSCRIPTEN_GENERATED_FUNCTIONS: ["asm", "__Z11printResultPiS_j", "_segment_holding", "__ZN5identC2EiPKcPci", "_vec2Length", "exc", "label", "confuusion", "tempDouble", "_org_apache_harmony_luni_util_NumberConverter_freeFormat__", "__ZN23b2EdgeAndPolygonContact8EvaluateEP10b2ManifoldRK11b2TransformS4_", "_java_nio_charset_Charset_forNameInternal___java_lang_String", "looop2", "looop3", "looop4", "looop5", "looop6", "looop7", "looop8", "multiloop", "multiloop2", "tempDouble2", "watIf", "select2", "binary", "cute", "selfAssign", "elimOneLoopVar", "elimOneLoopVar2", "elimOneLoopVar3", "elimOneLoopVar4", "elimOneLoopVarStillUsed", "elimOneLoopVarStillUsedSE", "elimOneLoopVar5", "helperExtraUse"]
+function mixed_up_loop_helpers() {
+ var $cmp = 0, $cmp13 = 0, $cmp15 = 0, $cmp2 = 0, $cmp7 = 0, $div = +0, $div11 = +0, $inc = 0, $iterations$0 = 0, $iterations$0$ph = 0, $or$cond = 0, $p$sroa$0$0$ph = +0, $p$sroa$1$0$ph = +0, $p$sroa$1$0$ph$phi = +0, $rem = 0, $sub = +0, $vararg_buffer = 0, $vararg_ptr1 = 0, label = 0, sp = 0;
+ sp = STACKTOP;
+ STACKTOP = STACKTOP + 16 | 0;
+ $vararg_buffer = sp;
+ $iterations$0$ph = 0;
+ $p$sroa$0$0$ph = +0;
+ $p$sroa$1$0$ph = +1;
+ L1 : while (1) {
+  $iterations$0 = $iterations$0$ph;
+  while (1) {
+   $inc = $iterations$0 + 1 | 0;
+   $cmp = ($inc | 0) < 10;
+   if (!$cmp) {
+    label = 8;
+    break L1;
+   }
+   HEAPF64[tempDoublePtr >> 3] = $p$sroa$0$0$ph;
+   HEAP32[$vararg_buffer >> 2] = HEAP32[tempDoublePtr >> 2];
+   HEAP32[$vararg_buffer + 4 >> 2] = HEAP32[tempDoublePtr + 4 >> 2];
+   $vararg_ptr1 = $vararg_buffer + 8 | 0;
+   HEAPF64[tempDoublePtr >> 3] = $p$sroa$1$0$ph;
+   HEAP32[$vararg_ptr1 >> 2] = HEAP32[tempDoublePtr >> 2];
+   HEAP32[$vararg_ptr1 + 4 >> 2] = HEAP32[tempDoublePtr + 4 >> 2];
+   _printf(8 | 0, $vararg_buffer | 0) | 0;
+   $cmp2 = ($iterations$0 | 0) > 0;
+   if ($cmp2) {
+    break;
+   } else {
+    $iterations$0 = $inc;
+   }
+  }
+  $sub = -$p$sroa$1$0$ph;
+  $rem = ($inc | 0) % 3 & -1;
+  $cmp7 = ($rem | 0) == 0;
+  if (!$cmp7) {
+   $p$sroa$1$0$ph$phi = $p$sroa$0$0$ph;
+   $iterations$0$ph = $inc;
+   $p$sroa$0$0$ph = $sub;
+   $p$sroa$1$0$ph = $p$sroa$1$0$ph$phi;
+   continue;
+  }
+  $div = $p$sroa$1$0$ph * +-.5;
+  $div11 = $p$sroa$0$0$ph * +.5;
+  $cmp13 = $div == +0;
+  $cmp15 = $div11 == +0;
+  $or$cond = $cmp13 & $cmp15;
+  if ($or$cond) {
+   break;
+  } else {
+   $iterations$0$ph = $inc;
+   $p$sroa$0$0$ph = $div;
+   $p$sroa$1$0$ph = $div11;
+  }
+ }
+}
+// EMSCRIPTEN_GENERATED_FUNCTIONS: ["asm", "__Z11printResultPiS_j", "_segment_holding", "__ZN5identC2EiPKcPci", "_vec2Length", "exc", "label", "confuusion", "tempDouble", "_org_apache_harmony_luni_util_NumberConverter_freeFormat__", "__ZN23b2EdgeAndPolygonContact8EvaluateEP10b2ManifoldRK11b2TransformS4_", "_java_nio_charset_Charset_forNameInternal___java_lang_String", "looop2", "looop3", "looop4", "looop5", "looop6", "looop7", "looop8", "multiloop", "multiloop2", "tempDouble2", "watIf", "select2", "binary", "cute", "selfAssign", "elimOneLoopVar", "elimOneLoopVar2", "elimOneLoopVar3", "elimOneLoopVar4", "elimOneLoopVarStillUsed", "elimOneLoopVarStillUsedSE", "elimOneLoopVar5", "helperExtraUse", "mixed_up_loop_helpers"]
 