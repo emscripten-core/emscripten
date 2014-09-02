cfe29d32357534defd19a9ea347c0f173126cb50
Jukka Jylänki <jujjyl@gmail.com>
2014-09-02 13:57:37 +0300

    Update test_pthread_create.

--- tests/pthread/test_pthread_create.cpp
+++ tests/pthread/test_pthread_create.cpp
@@ -13,14 +13,16 @@ int fib(int n)
 	return fib(n-1) + fib(n-2);
 }
 
+unsigned int global_shared_data[NUM_THREADS];
+
 void *ThreadMain(void *arg)
 {
-	unsigned int param = *(unsigned int*)arg; // param == random-generated integer in main thread.
-	param &= 0x7FFFFFFF; // Arbitrary, for printing convenience.
+	int idx = (int)arg;
+	unsigned int param = global_shared_data[idx];
 
 #define N 100
 
-	EM_ASM_INT( { Module['printErr']('Thread ID '+$0+': sorting ' + $1 + ' numbers.') }, param, N);
+	EM_ASM_INT( { Module['printErr']('Thread idx '+$0+': sorting ' + $1 + ' numbers with param ' + $2 + '.') }, idx, N, param);
 
 	unsigned int n[N];
 	for(unsigned int i = 0; i < N; ++i)
@@ -43,10 +45,10 @@ void *ThreadMain(void *arg)
 		if (n[i] == i) ++numGood;
 		else EM_ASM_INT( { Module['printErr']('n['+$0+']='+$1); }, i, n[i]);
 
+	EM_ASM_INT( { Module['print']('Thread idx ' + $0 + ' with param '+$1+': all done with result '+$2+'.'); }, idx, param, numGood);
 	pthread_exit((void*)numGood);
 }
 
-int global_shared_data[NUM_THREADS];
 pthread_t thread[NUM_THREADS];
 
 int numThreadsToCreate = 1000;
@@ -56,8 +58,10 @@ void CreateThread(int i)
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	global_shared_data[i] = numThreadsToCreate * 12141231;
-	int rc = pthread_create(&thread[i], &attr, ThreadMain, &global_shared_data[i]);
+	static int counter = 1;
+	global_shared_data[i] = (counter++ * 12141231) & 0x7FFFFFFF; // Arbitrary random'ish data for perturbing the sort for this thread task.
+//	EM_ASM_INT( { Module['print']('Main: Creating thread idx ' + $0 + ' (param ' + $1 + ')'); }, i, global_shared_data[i]);
+	int rc = pthread_create(&thread[i], &attr, ThreadMain, (void*)i);
 	assert(rc == 0);
 	pthread_attr_destroy(&attr);
 }
@@ -78,7 +82,7 @@ int main()
 			int status;
 			int rc = pthread_join(thread[i], (void**)&status);
 			assert(rc == 0);
-			EM_ASM_INT( { Module['printErr']('Main: Joined thread ' + $0 + ' with status ' + $1); }, thread[i], (int)status);
+			EM_ASM_INT( { Module['printErr']('Main: Joined thread idx ' + $0 + ' (param ' + $1 + ') with status ' + $2); }, i, global_shared_data[i], (int)status);
 			assert(status == N);
 			thread[i] = 0;
 			if (numThreadsToCreate > 0)
