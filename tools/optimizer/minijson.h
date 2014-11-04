2076afb706c6dfc80f6a61cef54b7fbf3189551d
Alon Zakai <alonzakai@gmail.com>
2014-11-03 16:09:28 -0800

    minijson.h

---
+++ tools/optimizer/minijson.h
@@ -0,0 +1,217 @@
+/*
+ * Extremely minimal JSON, just enough to manipulate an AST in the format the JS optimizer wants: arrays, strings, numbers, bools (no objects, no non-ascii, etc.).
+ * Optimized for fast parsing and also manipulation of the AST.
+ * Uses shared_ptr for simplicity, basically everywhere. TODO: measure impact
+ */
+
+#include <ctype.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <memory>
+#include <vector>
+#include <ostream>
+
+struct Value;
+
+typedef std::shared_ptr<Value> Ref;
+typedef std::vector<Ref> ArrayStorage;
+
+struct Value {
+  enum Type {
+    String = 0,
+    Number = 1,
+    Array = 2,
+    Null = 3,
+    Bool = 4
+  };
+
+  Type type;
+
+  union {
+    std::string *str;
+    double num;
+    ArrayStorage *arr;
+    bool boo;
+  };
+
+  // constructors all copy their input
+  Value() : type(Null), num(0) {}
+  Value(const char *s) : type(Null) {
+    set(s);
+  }
+  Value(double n) : type(Null) {
+    set(n);
+  }
+  Value(ArrayStorage &a) : type(Null) {
+    setArray();
+    *arr = a;
+  }
+  Value(bool b) : type(Null) {
+    setBool(b);
+  }
+
+  ~Value() {
+    free();
+  }
+
+  void free() {
+    if (type == String) delete str;
+    else if (type == Array) delete arr;
+    type = Null;
+    num = 0;
+  }
+
+  void set(const char *s) {
+    free();
+    type = String;
+    str = new std::string(s);
+  }
+  void set(double n) {
+    free();
+    type = Null;
+    num = n;
+  }
+  void set(ArrayStorage &a) {
+    free();
+    type = Array;
+    arr = new ArrayStorage();
+    *arr = a;
+  }
+  void setArray() {
+    free();
+    type = Array;
+    arr = new ArrayStorage();
+  }
+  void setNull() {
+    free();
+    type = Null;
+  }
+  void setBool(bool b) {
+    free();
+    type = Bool;
+    boo = b;
+  }
+
+  Value& operator=(const Value& other) {
+    free();
+    switch (other.type) {
+      case String:
+        set(other.str->c_str());
+        break;
+      case Number:
+        set(other.num);
+        break;
+      case Array:
+        set(*other.arr);
+        break;
+      case Null:
+        setNull();
+        break;
+      case Bool:
+        setBool(other.boo);
+        break;
+    }
+    return *this;
+  }
+
+  bool operator==(const Value& other) {
+    if (type != other.type) return false;
+    switch (other.type) {
+      case String:
+        return *str == *(other.str);
+      case Number:
+        return num == other.num;
+      case Array:
+        if (arr->size() != other.arr->size()) return false;
+        for (unsigned i = 0; i < arr->size(); i++) {
+          if ((*arr)[i] != (*other.arr)[i]) return false;
+        }
+        break;
+      case Null:
+        break;
+      case Bool:
+        return boo == other.boo;
+    }
+    return true;
+  }
+
+  char* parse(char* curr) {
+    #define skip() { while (*curr && isspace(*curr)) curr++; }
+    skip();
+    if (*curr == '"') {
+      // String
+      curr++;
+      char *close = strchr(curr, '"');
+      assert(close);
+      *close = 0;
+      set(curr);
+      *close = '"';
+      curr = close+1;
+    } else if (*curr == '[') {
+      // Array
+      curr++;
+      skip();
+      setArray();
+      while (*curr != ']') {
+        Ref temp(new Value);
+        arr->push_back(temp);
+        curr = temp->parse(curr);
+        skip();
+        if (*curr == ']') break;
+        assert(*curr == ',');
+        curr++;
+        skip();
+      }
+      curr++;
+    } else if (*curr == 'n') {
+      // Null
+      assert(strncmp(curr, "null", 4) == 0);
+      setNull();
+      curr += 4;
+    } else if (*curr == 't') {
+      // Bool true
+      assert(strncmp(curr, "true", 4) == 0);
+      setBool(true);
+      curr += 4;
+    } else if (*curr == 'f') {
+      // Bool false
+      assert(strncmp(curr, "false", 5) == 0);
+      setBool(false);
+      curr += 5;
+    } else {
+      // Number
+      char *after;
+      num = strtod(curr, &after);
+      curr = after;
+    }
+    return curr;
+  }
+
+  void stringify(std::ostream &os) {
+    switch (type) {
+      case String:
+        os << '"' << *str << '"';
+        break;
+      case Number:
+        os << num;
+        break;
+      case Array:
+        os << '[';
+        for (unsigned i = 0; i < arr->size(); i++) {
+          if (i > 0) os << ", ";
+          (*arr)[i]->stringify(os);
+        }
+        os << ']';
+        break;
+      case Null:
+        os << "null";
+        break;
+      case Bool:
+        os << (boo ? "true" : "false");
+        break;
+    }
+  }
+};
+
