e75930d8aa92d0b798e6f9290e525053b58be72c
Alon Zakai <alonzakai@gmail.com>
2014-11-12 11:24:12 -0800

    little optimization on traversePre

--- tools/optimizer/optimizer.cpp
+++ tools/optimizer/optimizer.cpp
@@ -127,7 +127,7 @@ void traversePre(Ref node, std::function<void (Ref)> visit) {
   visit(node);
   std::vector<TraverseInfo> stack;
   stack.push_back({ node, 0 });
-  while (stack.size() > 0) {
+  while (1) {
     TraverseInfo& top = stack.back();
     if (top.index < top.node->size()) {
       Ref sub = top.node[top.index];
@@ -137,6 +137,7 @@ void traversePre(Ref node, std::function<void (Ref)> visit) {
         stack.push_back({ sub, 0 });
       }
     } else {
+      if (stack.size() == 1) break;
       stack.pop_back();
     }
   }
