e282f4b56743bbe547faece65800a58fdb854321
Alon Zakai <alonzakai@gmail.com>
2014-11-24 17:53:36 -0800

    remove unnecessary whitespace

--- src/library_glfw.js
+++ src/library_glfw.js
@@ -799,7 +799,7 @@ var LibraryGLFW = {
   glfwGetMonitors: function(count) {
     setValue(count, 1, 'i32');
     if (!GLFW.monitors) {
-      GLFW.monitors = allocate([1, 0, 0 , 0], 'i32', ALLOC_NORMAL);
+      GLFW.monitors = allocate([1, 0, 0, 0], 'i32', ALLOC_NORMAL);
     }
     return GLFW.monitors;
   },
