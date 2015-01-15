d5346965fba9b4614d8d6ba0f5928dbe1dd79aac
Jukka Jylänki <jujjyl@gmail.com>
2015-01-15 12:20:03 +0200

    Update UTF8 test.

--- tests/utf8.cpp
+++ tests/utf8.cpp
@@ -20,8 +20,8 @@ int main() {
   EM_ASM_INT({
     var str = Module.UTF8ToString($0);
     Module.print(str);
-    Module.stringToUTF8(str, $1);
-  }, asciiString, asciiString3);
+    Module.stringToUTF8(str, $1, $2);
+  }, asciiString, asciiString3, 128);
   assert(!strcmp(asciiString, asciiString3));
 
   const char utf8String[] = u8"Hyv\u00E4\u00E4 p\u00E4iv\u00E4\u00E4! T\u00F6\u00F6\u00F6\u00F6t! abc\u2603\u20AC\U0002007C123 --- abc\u2603\u20AC\U0002007C123."; // U+2603 is snowman, U+20AC is the Euro sign, U+2007C is a Chinese Han character that looks like three raindrops.
@@ -29,13 +29,38 @@ int main() {
   EM_ASM_INT({
     var str = Module.UTF8ToString($0);
     Module.print(str);
-    Module.stringToUTF8(str, $1);
-  }, utf8String, utf8String2);
+    Module.stringToUTF8(str, $1, $2);
+  }, utf8String, utf8String2, 128);
   assert(strlen(utf8String) == strlen(utf8String2));
   for(int i = 0; i < strlen(utf8String)+1; ++i)
     if (utf8String[i] != utf8String2[i])
       printf("i=%d:%u,%u\n", i, (unsigned int)(unsigned char)utf8String[i], (unsigned int)(unsigned char)utf8String2[i]);
   assert(!strcmp(utf8String, utf8String2));
+
+  // Test that text gets properly cut off if output buffer is too small.
+  EM_ASM_INT({
+    var str = Module.UTF8ToString($0);
+    Module.print(str);
+    Module.stringToUTF8(str, $1, $2);
+  }, utf8String, utf8String2, 10);
+  assert(strlen(utf8String2) == 9);
+
+  // Zero-length string.
+  EM_ASM_INT({
+    var str = Module.UTF8ToString($0);
+    Module.print(str);
+    Module.stringToUTF8(str, $1, $2);
+  }, utf8String, utf8String2, 1);
+  assert(utf8String2[0] == 0);
+
+  // No bytes output.
+  utf8String2[0] = 'X';
+  EM_ASM_INT({
+    var str = Module.UTF8ToString($0);
+    Module.print(str);
+    Module.stringToUTF8(str, $1, $2);
+  }, utf8String, utf8String2, 0);
+  assert(utf8String2[0] == 'X');
 
   printf("OK.\n");
 }
