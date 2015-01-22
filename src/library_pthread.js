6ec73b1b09476f702cf15c3824947a83f3841116
Jukka Jylänki <jujjyl@gmail.com>
2015-01-22 11:58:20 +0200

    Replace assert() in pthread_cancel with proper error handling.

--- src/library_pthread.js
+++ src/library_pthread.js
@@ -248,7 +248,7 @@ var LibraryPThread = {
       Module['printErr']('PThread ' + thread + ' does not exist!');
       return ERRNO_CODES.ESRCH;
     }
-    assert(pthread.threadBlock);
+    if (!thread.threadBlock) return ERRNO_CODES.EINVAL; // Trying to cancel a thread that is no longer running.
     Atomics.store(HEAPU32, (pthread.threadBlock + {{{ C_STRUCTS.pthread.threadStatus }}} ) >> 2, 2); // Signal the thread that it needs to cancel itself.
     pthread.worker.postMessage({ cmd: 'cancel' });
     return 0;
