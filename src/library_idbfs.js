2e03d9ab27f7cff271776aef618737ddd01913d8
Alon Zakai <alonzakai@gmail.com>
2015-02-02 16:51:06 -0800

    we handle errors in IDBFS, so prevent them from propagating

--- src/library_idbfs.js
+++ src/library_idbfs.js
@@ -63,8 +63,9 @@ mergeInto(LibraryManager.library, {
         IDBFS.dbs[name] = db;
         callback(null, db);
       };
-      req.onerror = function() {
+      req.onerror = function(e) {
         callback(this.error);
+        e.preventDefault();
       };
     },
     getLocalSet: function(mount, callback) {
@@ -107,7 +108,10 @@ mergeInto(LibraryManager.library, {
         if (err) return callback(err);
 
         var transaction = db.transaction([IDBFS.DB_STORE_NAME], 'readonly');
-        transaction.onerror = function() { callback(this.error); };
+        transaction.onerror = function(e) {
+          callback(this.error);
+          e.preventDefault();
+        };
 
         var store = transaction.objectStore(IDBFS.DB_STORE_NAME);
         var index = store.index('timestamp');
@@ -184,17 +188,26 @@ mergeInto(LibraryManager.library, {
     loadRemoteEntry: function(store, path, callback) {
       var req = store.get(path);
       req.onsuccess = function(event) { callback(null, event.target.result); };
-      req.onerror = function() { callback(this.error); };
+      req.onerror = function(e) {
+        callback(this.error);
+        e.preventDefault();
+      };
     },
     storeRemoteEntry: function(store, path, entry, callback) {
       var req = store.put(entry, path);
       req.onsuccess = function() { callback(null); };
-      req.onerror = function() { callback(this.error); };
+      req.onerror = function(e) {
+        callback(this.error);
+        e.preventDefault();
+      };
     },
     removeRemoteEntry: function(store, path, callback) {
       var req = store.delete(path);
       req.onsuccess = function() { callback(null); };
-      req.onerror = function() { callback(this.error); };
+      req.onerror = function(e) {
+        callback(this.error);
+        e.preventDefault();
+      };
     },
     reconcile: function(src, dst, callback) {
       var total = 0;
@@ -242,7 +255,10 @@ mergeInto(LibraryManager.library, {
         }
       };
 
-      transaction.onerror = function() { done(this.error); };
+      transaction.onerror = function(e) {
+        done(this.error);
+        e.preventDefault();
+      };
 
       // sort paths in ascending order so directory entries are created
       // before the files inside them
