cd41b65e169bda415b855de91f2ec4cb1f9cb993
Jukka Jylänki <jujjyl@gmail.com>
2015-02-10 01:52:51 +0200

    Add browser test for file IO in pthreads.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -2594,6 +2594,10 @@ window.close = function() {
   def test_pthread_setspecific_mainthread(self):
     self.btest(path_from_root('tests', 'pthread', 'test_pthread_setspecific_mainthread.cpp'), expected='0', args=['-lpthread'])
 
+  # Test that pthreads have access to filesystem.
+  def test_pthread_file_io(self):
+    self.btest(path_from_root('tests', 'pthread', 'test_pthread_file_io.cpp'), expected='0', args=['-lpthread', '-s', 'PTHREAD_POOL_SIZE=1'])
+
   # Test that it is possible to send a signal via calling alarm(timeout), which in turn calls to the signal handler set by signal(SIGALRM, func);
   def test_sigalrm(self):
     self.btest(path_from_root('tests', 'sigalrm.cpp'), expected='0')
