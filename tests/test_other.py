3702801d1fddc80d2a058507b953ede9dff09b11
Alon Zakai <alonzakai@gmail.com>
2015-02-23 16:51:30 -0800

    add EMCONFIGURE_JS=2 option, to force js checks

--- tests/test_other.py
+++ tests/test_other.py
@@ -4647,19 +4647,27 @@ int main(void) {
 
   def test_emconfigure_js_o(self):
     # issue 2994
-    for i in [0, 1]:
-      print i
-      try:
-        os.environ['EMCONFIGURE_JS'] = str(i)
-        self.clear()
-        Popen([PYTHON, path_from_root('emconfigure'), PYTHON, EMCC, '-c', '-o', 'a.o', path_from_root('tests', 'hello_world.c')]).communicate()
-        Popen([PYTHON, EMCC, 'a.o']).communicate()
-        if i == 0:
-          assert not os.path.exists('a.out.js') # native .o, not bitcode!
-        else:
-          assert 'hello, world!' in run_js(self.in_dir('a.out.js'))
-      finally:
-        del os.environ['EMCONFIGURE_JS']
+    for i in [0, 1, 2]:
+      for f in ['hello_world.c', 'files.cpp']:
+        print i, f
+        try:
+          os.environ['EMCONFIGURE_JS'] = str(i)
+          self.clear()
+          Popen([PYTHON, path_from_root('emconfigure'), PYTHON, EMCC, '-c', '-o', 'a.o', path_from_root('tests', f)]).communicate()
+          Popen([PYTHON, EMCC, 'a.o']).communicate()
+          if f == 'hello_world.c':
+            if i == 0:
+              assert not os.path.exists('a.out.js') # native .o, not bitcode!
+            else:
+              assert 'hello, world!' in run_js(self.in_dir('a.out.js'))
+          else:
+            # file access, need 2 to force js
+            if i == 0 or i == 1:
+              assert not os.path.exists('a.out.js') # native .o, not bitcode!
+            else:
+              assert os.path.exists('a.out.js')
+        finally:
+          del os.environ['EMCONFIGURE_JS']
 
   def test_emcc_c_multi(self):
     def test(args, llvm_opts=None):
