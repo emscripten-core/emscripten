5820d410db9830395cd7a171a34a181ec306e747
Alon Zakai <alonzakai@gmail.com>
2015-03-03 14:59:41 -0800

    handle recent spidermonkey in sanity.test_d8_path

--- tests/print_args.js
+++ tests/print_args.js
@@ -7,9 +7,13 @@ var args = [];
 if (typeof process !== 'undefined') {
   // Use process global in node
   args = process.argv.slice(2);
-} else {
-  // Use arguments object for d8 and jsc
+} else if (typeof arguments !== 'undefined') {
+  // Use arguments object for d8 and jsc, older sm
   args = Array.prototype.slice.call(arguments);
+} else if (typeof scriptArgs !== 'undefined') {
+  args = Array.prototype.slice.call(scriptArgs);
+} else {
+  throw 'what!';
 }
 
 args.forEach(function (val, index, array) {
