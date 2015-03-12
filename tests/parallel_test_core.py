421c187fb643d72c55bd854b5a69ffdbc025f97f
Alon Zakai <alonzakai@gmail.com>
2015-03-12 13:56:10 -0700

    specify an optimal order to run in parallel_test_core.py

--- tests/parallel_test_core.py
+++ tests/parallel_test_core.py
@@ -4,6 +4,11 @@
 import os, sys, subprocess, multiprocessing
 from runner import test_modes, PYTHON, path_from_root
 
+# run slower ones first, to optimize total time
+optimal_order = ['asm3i', 'asm1i', 'asm2nn', 'asm3', 'asm2', 'asm2g', 'asm2f', 'asm1', 'default']
+
+assert set(optimal_order) == set(test_modes), 'need to update the list of slowest modes'
+
 def run_mode(mode):
   print '<< running %s >>' % mode
   proc = subprocess.Popen([PYTHON, path_from_root('tests', 'runner.py'), mode], stdout=open(mode + '.out', 'w'), stderr=open(mode + '.err', 'w'))
@@ -12,5 +17,5 @@ def run_mode(mode):
 
 cores = int(os.environ.get('EMCC_CORES') or multiprocessing.cpu_count())
 pool = multiprocessing.Pool(processes=cores)
-filenames = pool.map(run_mode, test_modes, chunksize=1)
+filenames = pool.map(run_mode, optimal_order, chunksize=1)
 
