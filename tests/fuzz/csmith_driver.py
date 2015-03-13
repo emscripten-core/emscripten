e48f7a619ac98fc6d6422109619fff124742cd2c
Alon Zakai <alonzakai@gmail.com>
2015-03-12 17:24:10 -0700

    fix suffix in fuzzer fail saving

--- tests/fuzz/csmith_driver.py
+++ tests/fuzz/csmith_driver.py
@@ -136,7 +136,7 @@ while 1:
     print "EMSCRIPTEN BUG"
     notes['embug'] += 1
     fails += 1
-    shutil.copyfile(fullname, 'newfail_%d_%d' % (os.getpid(), fails))
+    shutil.copyfile(fullname, 'newfail_%d_%d%s' % (os.getpid(), fails, suffix))
 
   if not try_js():
     fail()
