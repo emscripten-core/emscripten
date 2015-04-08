4a099cdd199fbcd60d69262b0ea88778fd70fc3b
Jukka Jylänki <jujjyl@gmail.com>
2015-04-08 17:28:41 +0300

    Fix tests/parallel_test_core.py to be multiprocess-compatible.

--- tests/parallel_test_core.py
+++ tests/parallel_test_core.py
@@ -8,11 +8,6 @@ from runner import test_modes, PYTHON, path_from_root
 optimal_order = ['asm3i', 'asm1i', 'asm2nn', 'asm3', 'asm2', 'asm2g', 'asm2f', 'asm1', 'default']
 assert set(optimal_order) == set(test_modes), 'need to update the list of slowest modes'
 
-# clean up previous output
-for mode in optimal_order:
-  if os.path.exists(mode + '.err'):
-    os.unlink(mode + '.err')
-
 # set up a background thread to report progress
 class Watcher(threading.Thread):
   stop = False
@@ -29,9 +24,6 @@ class Watcher(threading.Thread):
         print >> sys.stderr, '[parallel_test_copy.py watcher] total output: %d' % total
       time.sleep(1)
 
-watcher = Watcher()
-watcher.start()
-
 # run tests for one mode
 def run_mode(mode):
   print '<< running %s >>' % mode
@@ -39,18 +31,29 @@ def run_mode(mode):
   proc.communicate()
   print '<< %s finished >>' % mode
 
-# run all modes
-cores = int(os.environ.get('EMCC_CORES') or multiprocessing.cpu_count())
-pool = multiprocessing.Pool(processes=cores)
-filenames = pool.map(run_mode, optimal_order, chunksize=1)
+def main():  
+  # clean up previous output
+  for mode in optimal_order:
+    if os.path.exists(mode + '.err'):
+      os.unlink(mode + '.err')
 
-# quit watcher
-Watcher.stop = True
+  watcher = Watcher()
+  watcher.start()
 
-# emit all outputs
-for mode in optimal_order:
-  print >> sys.stderr, '=== %s ===' % mode
-  if os.path.exists(mode + '.err'):
-    print >> sys.stderr, open(mode + '.err').read()
-  print >> sys.stderr
+  # run all modes
+  cores = int(os.environ.get('EMCC_CORES') or multiprocessing.cpu_count())
+  pool = multiprocessing.Pool(processes=cores)
+  filenames = pool.map(run_mode, optimal_order, chunksize=1)
 
+  # quit watcher
+  Watcher.stop = True
+
+  # emit all outputs
+  for mode in optimal_order:
+    print >> sys.stderr, '=== %s ===' % mode
+    if os.path.exists(mode + '.err'):
+      print >> sys.stderr, open(mode + '.err').read()
+    print >> sys.stderr
+
+if __name__ == '__main__':
+  sys.exit(main())
