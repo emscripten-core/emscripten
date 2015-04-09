8c8a2b8bf2f041e7fbe316ae51c3179bc61ff3e8
Alon Zakai <alonzakai@gmail.com>
2015-04-09 11:11:56 -0700

    avoid fprintf usage in gl.c #3327

--- system/lib/gl.c
+++ system/lib/gl.c
@@ -2,7 +2,7 @@
 
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <emscripten.h>
 
 #define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
@@ -1728,7 +1728,9 @@ void* emscripten_GetProcAddress(const char *name_) {
   else if (!strcmp(name, "glCopyTexSubImage2D")) return emscripten_glCopyTexSubImage2D;
   else if (!strcmp(name, "glDrawBuffers")) return emscripten_glDrawBuffers;
 
-  fprintf(stderr, "bad name in getProcAddress: %s | %s\n", name_, name);
+  EM_ASM_({
+    Module.printErr('bad name in getProcAddress: ' + [Pointer_stringify($0), Pointer_stringify($1)]);
+  }, name_, name);
   return 0;
 }
 
