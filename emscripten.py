cd0a6dc3d3c05c89dba29b932e96b23fe4ac9e14
Jukka Jylänki <jujjyl@gmail.com>
2015-04-16 17:39:49 +0300

    Don't add static initializers to atinit when running in pthreads.

--- emscripten.py
+++ emscripten.py
@@ -222,8 +222,8 @@ def emscript(infile, settings, outfile, libraries=[], compiler_engine=None,
     staticbump = mem_init.count(',')+1
     while staticbump % 16 != 0: staticbump += 1
     pre = pre.replace('STATICTOP = STATIC_BASE + 0;', '''STATICTOP = STATIC_BASE + %d;
-  /* global initializers */ __ATINIT__.push(%s);
-  %s''' % (staticbump, global_initializers, mem_init)) # XXX wrong size calculation!
+  /* global initializers */ %s __ATINIT__.push(%s);
+  %s''' % (staticbump, 'if (!ENVIRONMENT_IS_PTHREAD)' if settings['USE_PTHREADS'] else '', global_initializers, mem_init)) # XXX wrong size calculation!
 
     if settings['SIDE_MODULE']:
       pre = pre.replace('Runtime.GLOBAL_BASE', 'gb').replace('{{{ STATIC_BUMP }}}', str(staticbump))
