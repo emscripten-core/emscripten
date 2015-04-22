1d48ce78d31cc4ad61dc5c9234de49e6c684df98
Fábio Santos <fabiosantosart@gmail.com>
2015-04-22 03:19:46 +0100

    detect node even when Browserify put ,  functions in our environment

--- src/shell.js
+++ src/shell.js
@@ -34,8 +34,8 @@ for (var key in Module) {
 
 // The environment setup code below is customized to use Module.
 // *** Environment setup code ***
-var ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function';
 var ENVIRONMENT_IS_WEB = typeof window === 'object';
+var ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'function' && !ENVIRONMENT_IS_WEB;
 var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
 var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
 
