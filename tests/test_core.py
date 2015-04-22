cce64a788b3548cda48de81ca65dd01056241b36
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-04-22 15:26:05 +0700

    Remove PRECISE_I32_MUL option.
    This is set to true and asm.js always does precise math, so no
    need to be able to turn it off.

--- tests/test_core.py
+++ tests/test_core.py
@@ -425,16 +425,6 @@ class T(RunnerCore): # Short name, to make it more fun to use manually on the co
 
     self.do_run_from_file(src, output)
 
-  def test_i32_mul_semiprecise(self):
-    if Settings.ASM_JS: return self.skip('asm is always fully precise')
-
-    Settings.PRECISE_I32_MUL = 0 # we want semiprecise here
-
-    test_path = path_from_root('tests', 'core', 'test_i32_mul_semiprecise')
-    src, output = (test_path + s for s in ('.in', '.out'))
-
-    self.do_run_from_file(src, output)
-
   def test_i16_emcc_intrinsic(self):
     Settings.CORRECT_SIGNS = 1 # Relevant to this test
 
