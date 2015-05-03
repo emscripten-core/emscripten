f3ceed909fe4c6fe24b8aa141e9bbfd2b1231f10
Alon Zakai <alonzakai@gmail.com>
2015-05-03 16:02:11 -0700

    support __ATPRERUN__ in shared modules

--- src/preamble_sharedlib.js
+++ src/preamble_sharedlib.js
@@ -4,20 +4,11 @@
 // Runtime essentials
 //========================================
 
-function callRuntimeCallbacks(callbacks) {
-  while(callbacks.length > 0) {
-    var callback = callbacks.pop();
-    var func = callback.func;
-    if (typeof func === 'number') {
-      func = FUNCTION_TABLE[func];
-    }
-    func(callback.arg === undefined ? null : callback.arg);
-  }
-}
-
+var __ATPRERUN__  = []; // functions called before the runtime is initialized
 var __ATINIT__ = []; // functions called during startup
 
 function initRuntime() {
+  callRuntimeCallbacks(__ATPRERUN__);
   callRuntimeCallbacks(__ATINIT__);
 }
 
