00c32b74d9ac04b4c814e9a5a28519a8c1873572
Jukka Jylänki <jujjyl@gmail.com>
2015-05-21 12:54:53 +0300

    Polyfill SharedArrayBuffer.slice() operation which doesn't exist currently so that JS code that interacts with Emscripten HEAP when it is a SAB can operate normally.

--- src/preamble.js
+++ src/preamble.js
@@ -1138,6 +1138,34 @@ assert(typeof Int32Array !== 'undefined' && typeof Float64Array !== 'undefined' 
 var buffer;
 #if USE_PTHREADS
 if (!ENVIRONMENT_IS_PTHREAD) buffer = new SharedArrayBuffer(TOTAL_MEMORY);
+
+// Currently SharedArrayBuffer does not have a slice() operation, so polyfill it in.
+// Adapted from https://github.com/ttaubert/node-arraybuffer-slice, (c) 2014 Tim Taubert <tim@timtaubert.de>
+// arraybuffer-slice may be freely distributed under the MIT license.
+(function (undefined) {
+  "use strict";
+  function clamp(val, length) {
+    val = (val|0) || 0;
+    if (val < 0) return Math.max(val + length, 0);
+    return Math.min(val, length);
+  }
+  if (!SharedArrayBuffer.prototype.slice) {
+    SharedArrayBuffer.prototype.slice = function (from, to) {
+      var length = this.byteLength;
+      var begin = clamp(from, length);
+      var end = length;
+      if (to !== undefined) end = clamp(to, length);
+      if (begin > end) return new ArrayBuffer(0);
+      var num = end - begin;
+      var target = new ArrayBuffer(num);
+      var targetArray = new Uint8Array(target);
+      var sourceArray = new SharedUint8Array(this, begin, num);
+      targetArray.set(sourceArray);
+      return target;
+    };
+  }
+})();
+
 HEAP8 = new SharedInt8Array(buffer);
 HEAP16 = new SharedInt16Array(buffer);
 HEAP32 = new SharedInt32Array(buffer);
