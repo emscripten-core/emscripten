a27550e4a000ac9605a51b22bcd5a6f837d43952
Alon Zakai <alonzakai@gmail.com>
2015-05-22 17:46:59 -0700

    add __SC_* network syscall constants

--- system/lib/libc/musl/arch/emscripten/syscall_arch.h
+++ system/lib/libc/musl/arch/emscripten/syscall_arch.h
@@ -40,3 +40,24 @@ static inline long __syscall6(long n, long a, long b, long c, long d, long e, lo
 	return (__syscall)(n, a, b, c, d, e, f);
 }
 
+#define __SC_socket      1
+#define __SC_bind        2
+#define __SC_connect     3
+#define __SC_listen      4
+#define __SC_accept      5
+#define __SC_getsockname 6
+#define __SC_getpeername 7
+#define __SC_socketpair  8
+#define __SC_send        9
+#define __SC_recv        10
+#define __SC_sendto      11
+#define __SC_recvfrom    12
+#define __SC_shutdown    13
+#define __SC_setsockopt  14
+#define __SC_getsockopt  15
+#define __SC_sendmsg     16
+#define __SC_recvmsg     17
+#define __SC_accept4     18
+#define __SC_recvmmsg    19
+#define __SC_sendmmsg    20
+
