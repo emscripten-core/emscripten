9f4c103a6f4313af7ed3272938878b173972ec64
Nicholas Wilson <nicholas.wilson@realvnc.com>
2015-06-05 12:38:04 +0100

    Add comment in emcc, fix tests caused by bad merge
    Comment per review in PR2809
    Tests fixed: other.test_dup_o_in_one_a other.test_symlink

--- tools/shared.py
+++ tools/shared.py
@@ -1636,6 +1636,17 @@ class Building:
       safe_ensure_dirs(temp_dir)
       os.chdir(temp_dir)
       contents = filter(lambda x: len(x) > 0, Popen([LLVM_AR, 't', f], stdout=PIPE).communicate()[0].split('\n'))
+      # llvm-ar appears to just use basenames inside archives. as a result, files with the same basename
+      # will trample each other when we extract them. to help warn of such situations, we warn if there
+      # are duplicate entries in the archive
+      if len(contents) != len(set(contents)):
+        logging.warning('loading from archive %s, which has duplicate entries (files with identical base names). this is dangerous as only the last will be taken into account, and you may see surprising undefined symbols later. you should rename source files to avoid this problem (or avoid .a archives, and just link bitcode together to form libraries for later linking)' % f)
+        warned = set()
+        for i in range(len(contents)):
+          curr = contents[i]
+          if curr not in warned and curr in contents[i+1:]:
+            logging.warning('   duplicate: %s' % curr)
+            warned.add(curr)
       if len(contents) == 0:
         logging.debug('Archive %s appears to be empty (recommendation: link an .so instead of .a)' % f)
       else:
