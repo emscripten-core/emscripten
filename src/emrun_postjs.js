emscripten-core/emscripten

commit 7cff83670de4c021633473d4ce539daff930195f

Make --emrun compatible with -lpthread.

src/emrun_postjs.js
@@ -1,23 +1,25 @@
-function emrun_register_handlers() {
-  function post(msg) {
-    var http = new XMLHttpRequest();
-    http.open("POST", "stdio.html", true);
-    http.send(msg);
-  }
+if (typeof window === "object" && !ENVIRONMENT_IS_PTHREAD) {
+  function emrun_register_handlers() {
+    function post(msg) {
+      var http = new XMLHttpRequest();
+      http.open("POST", "stdio.html", true);
+      http.send(msg);
+    }
   // If the address contains localhost, or we are running the page from port 6931, we can assume we're running the test runner and should post stdout logs.
-  if (document.URL.search("localhost") != -1 || document.URL.search(":6931/") != -1) {
-    var emrun_http_sequence_number = 1;
-    var prevPrint = Module['print'];
-    var prevErr = Module['printErr'];
-    function emrun_exit() { post('^exit^'+EXITSTATUS); };
-    Module['addOnExit'](emrun_exit);
-    Module['print'] = function emrun_print(text) { post('^out^'+(emrun_http_sequence_number++)+'^'+encodeURIComponent(text)); prevPrint(text); }
-    Module['printErr'] = function emrun_printErr(text) { post('^err^'+(emrun_http_sequence_number++)+'^'+encodeURIComponent(text)); prevErr(text); }
+    // If the address contains localhost, or we are running the page from port 6931, we can assume we're running the test runner and should post stdout logs.
+    if (document.URL.search("localhost") != -1 || document.URL.search(":6931/") != -1) {
+      var emrun_http_sequence_number = 1;
+      var prevPrint = Module['print'];
+      var prevErr = Module['printErr'];
+      function emrun_exit() { post('^exit^'+EXITSTATUS); };
+      Module['addOnExit'](emrun_exit);
+      Module['print'] = function emrun_print(text) { post('^out^'+(emrun_http_sequence_number++)+'^'+encodeURIComponent(text)); prevPrint(text); }
+      Module['printErr'] = function emrun_printErr(text) { post('^err^'+(emrun_http_sequence_number++)+'^'+encodeURIComponent(text)); prevErr(text); }
+    }
+    // Notify emrun web server that this browser has successfully launched the page.
+    post('^pageload^');
   }
-  // Notify emrun web server that this browser has successfully launched the page.
-  post('^pageload^');
 }
-window.addEventListener('load', emrun_register_handlers);
 
 // POSTs the given binary data represented as a (typed) array data back to the emrun-based web server.
 // To use from C code, call e.g. EM_ASM_({emrun_file_dump("file.dat", HEAPU8.subarray($0, $0 + $1));}, my_data_pointer, my_data_pointer_byte_length);

commit f0ed1b6b335f55c6903d078268cc72ef92007ad5

fix browser.test_emrun after pthreads merge broke it in 7cff83670de4c021633473d4ce539daff930195f

src/emrun_postjs.js
@@ -1,4 +1,4 @@
-if (typeof window === "object" && !ENVIRONMENT_IS_PTHREAD) {
+if (typeof window === "object" && (typeof ENVIRONMENT_IS_PTHREAD === 'undefined' || !ENVIRONMENT_IS_PTHREAD)) {
   function emrun_register_handlers() {
     function post(msg) {
       var http = new XMLHttpRequest();
@@ -19,6 +19,7 @@ if (typeof window === "object" && !ENVIRONMENT_IS_PTHREAD) {
       post('^pageload^');
     }
   }
+  window.addEventListener('load', emrun_register_handlers);
 }
 
 // POSTs the given binary data represented as a (typed) array data back to the emrun-based web server.