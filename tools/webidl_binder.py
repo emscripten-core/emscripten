35aad0cd10467af23e286357a1976f58ac0f6144
Mark Logan <mark@artillery.com>
2015-06-23 15:16:07 -0700

    Document the interface sorting code.

--- tools/webidl_binder.py
+++ tools/webidl_binder.py
@@ -406,6 +406,14 @@ for name, interface in interfaces.iteritems():
   if not js_impl: continue
   implements[name] = [js_impl[0]]
 
+# Compute the height in the inheritance tree of each node. Note that the order of interation
+# of `implements` is irrelevant.
+#
+# After one iteration of the loop, all ancestors of child are guaranteed to have a a larger
+# height number than the child, and this is recursively true for each ancestor. If the height
+# of child is later increased, all its ancestors will be readjusted at that time to maintain
+# that invariant. Further, the height of a node never decreases. Therefore, when the loop
+# finishes, all ancestors of a given node should have a larger height number than that node.
 nodeHeight = {}
 for child, parent in implements.iteritems():
   parent = parent[0]
