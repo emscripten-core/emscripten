1913a60c3cc55ad2e4d2cbd19c55b3c4ff01a728
Alon Zakai <alonzakai@gmail.com>
2015-07-23 15:19:50 -0700

    clean up wasm packer logging

--- third_party/wasm-polyfill/src/pack-asmjs.js
+++ third_party/wasm-polyfill/src/pack-asmjs.js
@@ -2,11 +2,9 @@
 var inputFile = Module['arguments'][0];
 var outputFile = Module['arguments'][1];
 
-Module.printErr('loading ' + inputFile)
 FS.createDataFile('/', 'input.js', Module['readBinary'](inputFile), true, false);
 
 __ATEXIT__.push(function() {
-  Module.printErr('dumping binary as JS array from ' + outputFile)
   Module.print(JSON.stringify(MEMFS.getFileDataAsRegularArray(FS.root.contents[outputFile]).map(function(x) { return unSign(x, 8) })));
 });
 
