emscripten-core/emscripten

commit c002379445706ef01d5c838d504e2eff130a94f7

receive arity info for each asm const from the backend; fixes #3730

emscripten.py
@@ -272,16 +272,15 @@ def emscript(infile, settings, outfile, libraries=[], compiler_engine=None,
       if const[0] == '"' and const[-1] == '"':
         const = const[1:-1]
       const = '{ ' + const + ' }'
-      i = 0
       args = []
-      while ('$' + str(i)) in const:
+      arity = metadata['asmConstArities'][k]
+      for i in range(arity):
         args.append('$' + str(i))
-        i += 1
       const = 'function(' + ', '.join(args ) + ') ' + const
       asm_consts[int(k)] = const
 
     asm_const_funcs = []
-    for arity in metadata['asmConstArities']:
+    for arity in set(metadata['asmConstArities'].values()):
       forwarded_json['Functions']['libraryFunctions']['_emscripten_asm_const_%d' % arity] = 1
       args = ['a%d' % i for i in range(arity)]
       all_args = ['code'] + args

tests/test_core.py
@@ -2134,6 +2134,21 @@ int main() {
 }
 ''', 'hello world…')
 
+  def test_em_asm_unused_arguments(self):
+    src = r'''
+      #include <stdio.h>
+      #include <emscripten.h>
+
+      int main(int argc, char **argv) {
+        int sum = EM_ASM_INT({
+           return $0 + $2;
+        }, 0, 1, 2);
+        printf("0+2=%d\n", sum);
+        return 0;
+      }
+    '''
+    self.do_run(src, '''0+2=2''')
+
   def test_memorygrowth(self):
     self.banned_js_engines = [V8_ENGINE] # stderr printing limitations in v8
 

commit af8323da2af5d497fb2e34577745dd54b28d534e

Fix some breakage from c002379445706ef0

emscripten.py
@@ -277,7 +277,7 @@ def emscript(infile, settings, outfile, libraries=[], compiler_engine=None,
       for i in range(arity):
         args.append('$' + str(i))
       const = 'function(' + ', '.join(args ) + ') ' + const
-      asm_consts[int(k)] = const
+      asm_consts[k] = const
 
     asm_const_funcs = []
     for arity in set(metadata['asmConstArities'].values()):