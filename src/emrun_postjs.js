a3dcfdd50c835703cdc658d1a35e3a2452d9b074
Jukka Jylänki <jujjyl@gmail.com>
2015-09-07 20:32:02 +0300

    Fix emrun test script to only call to user_pref() to set prefs (the other directives did not quite work). When building with --emrun, have exit() close the current browser window automatically. Run browser.test_emrun with --safe_firefox_profile.

--- src/emrun_postjs.js
+++ src/emrun_postjs.js
@@ -5,12 +5,26 @@ if (typeof window === "object" && (typeof ENVIRONMENT_IS_PTHREAD === 'undefined'
       http.open("POST", "stdio.html", true);
       http.send(msg);
     }
+    function postExit(msg) {
+      var http = new XMLHttpRequest();
+      http.onreadystatechange = function() {
+        if (http.readyState == 4 /*DONE*/) {
+          try {
+            // Try closing the current browser window, since it exit()ed itself. This can shut down the browser process
+            // and emrun does not need to kill the whole browser process.
+            if (typeof window !== 'undefined' && window.close) window.close();
+          } catch(e) {}
+        }
+      }
+      http.open("POST", "stdio.html", true);
+      http.send(msg);
+    }
     // If the address contains localhost, or we are running the page from port 6931, we can assume we're running the test runner and should post stdout logs.
     if (document.URL.search("localhost") != -1 || document.URL.search(":6931/") != -1) {
       var emrun_http_sequence_number = 1;
       var prevPrint = Module['print'];
       var prevErr = Module['printErr'];
-      function emrun_exit() { post('^exit^'+EXITSTATUS); };	
+      function emrun_exit() { postExit('^exit^'+EXITSTATUS); };	
       Module['addOnExit'](emrun_exit);
       Module['print'] = function emrun_print(text) { post('^out^'+(emrun_http_sequence_number++)+'^'+encodeURIComponent(text)); prevPrint(text); }
       Module['printErr'] = function emrun_printErr(text) { post('^err^'+(emrun_http_sequence_number++)+'^'+encodeURIComponent(text)); prevErr(text); }
--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1856,7 +1856,7 @@ void *getBindBuffer() {
     # and the browser will not close as part of the test, pinning down the cwd on Windows and it wouldn't be possible to delete it. Therefore switch away from that directory
     # before launching.
     os.chdir(path_from_root())
-    args = [PYTHON, path_from_root('emrun'), '--timeout', '30', '--verbose', '--log_stdout', os.path.join(outdir, 'stdout.txt'), '--log_stderr', os.path.join(outdir, 'stderr.txt')]
+    args = [PYTHON, path_from_root('emrun'), '--timeout', '30', '--safe_firefox_profile', '--verbose', '--log_stdout', os.path.join(outdir, 'stdout.txt'), '--log_stderr', os.path.join(outdir, 'stderr.txt')]
     if emscripten_browser is not None:
       args += ['--browser', emscripten_browser]
     args += [os.path.join(outdir, 'hello_world.html'), '1', '2', '--3']
