884c5a52103573b224e3c58fe95e3083af3769c6
Alon Zakai <alonzakai@gmail.com>
2015-09-14 17:44:17 -0700

    proper support for realloc and memalign in split memory

--- system/lib/split_malloc.cpp
+++ system/lib/split_malloc.cpp
@@ -3,6 +3,7 @@
 */
 
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <emscripten.h>
@@ -14,11 +15,11 @@ typedef void* mspace;
 mspace create_mspace_with_base(void* base, size_t capacity, int locked);
 void* mspace_malloc(mspace space, size_t size);
 void mspace_free(mspace space, void* ptr);
+void* mspace_realloc(mspace msp, void* oldmem, size_t bytes);
+void* mspace_memalign(mspace msp, size_t alignment, size_t bytes);
 
 void* sbrk(intptr_t increment) {
-  EM_ASM({
-    Module.printErr("sbrk() should never be called when SPLIT_MEMORY!");
-  });
+  EM_ASM({ Module.printErr("sbrk() should never be called when SPLIT_MEMORY!"); });
   abort();
 }
 
@@ -27,17 +28,15 @@ void* sbrk(intptr_t increment) {
 #define MAX_SPACES 1000
 
 static bool initialized = false;
+static size_t total_memory = 0;
 static size_t split_memory = 0;
 static size_t num_spaces = 0;
 static mspace spaces[MAX_SPACES]; // 0 is for the stack, static, etc - not used by malloc # TODO: make a small space in there?
 
 void init() {
-  split_memory = EM_ASM_INT_V({
-    return SPLIT_MEMORY;
-  });
-  num_spaces = EM_ASM_INT_V({
-    return HEAPU8s.length;
-  });
+  total_memory = EM_ASM_INT_V({ return TOTAL_MEMORY; });
+  split_memory = EM_ASM_INT_V({ return SPLIT_MEMORY; });
+  num_spaces = EM_ASM_INT_V({ return HEAPU8s.length; });
   if (num_spaces >= MAX_SPACES) abort();
   spaces[0] = 0;
   for (int i = 1; i < num_spaces; i++) {
@@ -47,8 +46,10 @@ void init() {
 }
 
 // TODO: optimize, these are powers of 2
+// TODO: add optional asserts in these
 #define space_index(ptr) (((unsigned)ptr) / split_memory)
 #define space_relative(ptr) (((unsigned)ptr) % split_memory)
+#define get_space(ptr) spaces[space_index(ptr)]
 
 extern "C" {
 
@@ -80,13 +81,53 @@ void* malloc(size_t size) {
 
 void free(void* ptr) {
   if (ptr == 0) return;
-  if (!initialized) abort();
-  unsigned index = space_index(ptr);
-  if (index == 0 || index >= num_spaces) {
-    fprintf(stderr, "bad free ptr %p\n", ptr);
-    abort();
+  mspace_free(get_space(ptr), ptr);
+}
+
+void* realloc(void* ptr, size_t newsize) {
+  void* ret = mspace_realloc(get_space(ptr), ptr, newsize);
+  if (ret) return ret;
+  ret = malloc(newsize);
+  if (!ret) return 0;
+  size_t copysize = newsize;
+  if (((unsigned)ptr) + copysize > total_memory) copysize = total_memory - ((unsigned)ptr);
+  memcpy(ret, ptr, copysize);
+  free(ptr);
+  return ret;
+}
+
+void* calloc(size_t num, size_t size) {
+  size_t bytes = num * size;
+  void* ret = malloc(bytes);
+  if (!ret) return 0;
+  memset(ret, 0, bytes);
+  return ret;
+}
+
+void* memalign(size_t alignment, size_t size) {
+  if (!initialized) {
+    init();
   }
-  mspace_free(spaces[index], ptr);
+  if (size >= split_memory) {
+    static bool warned = false;
+    if (!warned) {
+      EM_ASM_({
+        Module.print("trying to malloc " + $0 + ", a size larger than SPLIT_MEMORY (" + $1 + "), increase SPLIT_MEMORY if you want that to work");
+      }, size, split_memory);
+      warned = true;
+    }
+    return 0;
+  }
+  static int next = 1;
+  int start = next;
+  while (1) { // simple round-robin, while keeping to use the same one as long as it keeps succeeding
+    void *ret = mspace_memalign(spaces[next], alignment, size);
+    if (ret) return ret;
+    next++;
+    if (next == num_spaces) next = 1;
+    if (next == start) break;
+  }
+  return 0; // we cycled, so none of them can allocate
 }
 
 }
