bbf01bb787a371df4b09a373a515a1b96e9a128e
Alon Zakai <alonzakai@gmail.com>
2015-09-21 18:17:45 -0700

    test for 3787

--- tests/test_core.py
+++ tests/test_core.py
@@ -5886,6 +5886,13 @@ return malloc(size);
     self.emcc_args = self.emcc_args + ['-msse']
     self.do_run_from_file(src, output)
 
+  def test_simd14(self):
+    if self.is_emterpreter(): return self.skip('todo')
+    test_path = path_from_root('tests', 'core', 'test_simd14')
+    src, output = (test_path + s for s in ('.c', '.out'))
+    self.emcc_args = self.emcc_args + ['-msse', '-msse2']
+    self.do_run_from_file(src, output)
+
   def test_simd_dyncall(self):
     if self.is_emterpreter(): return self.skip('todo')
     if self.is_wasm(): return self.skip('wasm will not support SIMD in the MVP')
