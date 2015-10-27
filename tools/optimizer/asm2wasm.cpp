c34ab824a17bcb3cb182e5aa5ab31dde25164af2
Alon Zakai <alonzakai@gmail.com>
2015-10-27 11:47:35 -0700

    parse while

--- tools/optimizer/asm2wasm.cpp
+++ tools/optimizer/asm2wasm.cpp
@@ -514,6 +514,19 @@ Function* Asm2WasmModule::processFunction(Ref ast) {
       ret->condition = nullptr;
       ret->value = nullptr;
       return ret;
+    } else if (what == WHILE) {
+      assert(ast[1][0] == NUM && ast[1][1]->getInteger() == 1);
+      auto ret = allocator.alloc<Loop>();
+      IString out = getNextId("while-out");
+      IString in = getNextId("while-in");
+      ret->out = out;
+      ret->in = in;
+      breakStack.push_back(out);
+      continueStack.push_back(in);
+      ret->body = process(ast[2]);
+      continueStack.pop_back();
+      breakStack.pop_back();
+      return ret;
     } else if (what == SWITCH) {
       IString name = getNextId("switch");
       breakStack.push_back(name);
