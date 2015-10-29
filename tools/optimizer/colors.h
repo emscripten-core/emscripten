9ab6a534efe6491c95b6b2eb6eaffd420c13a546
Alon Zakai <alonzakai@gmail.com>
2015-10-28 17:51:48 -0700

    more colors

--- tools/optimizer/colors.h
+++ tools/optimizer/colors.h
@@ -2,23 +2,43 @@
 
 struct Colors {
   static void normal(std::ostream& stream) {
-#if __linux__
+#if defined(__linux__) || defined(__apple__)
     stream << "\033[0m";
 #endif
   }
   static void red(std::ostream& stream) {
-#if __linux__
+#if defined(__linux__) || defined(__apple__)
     stream << "\033[31m";
 #endif
   }
+  static void magenta(std::ostream& stream) {
+#if defined(__linux__) || defined(__apple__)
+    stream << "\033[35m";
+#endif
+  }
   static void orange(std::ostream& stream) {
-#if __linux__
+#if defined(__linux__) || defined(__apple__)
     stream << "\033[33m";
 #endif
   }
   static void grey(std::ostream& stream) {
-#if __linux__
+#if defined(__linux__) || defined(__apple__)
     stream << "\033[37m";
+#endif
+  }
+  static void green(std::ostream& stream) {
+#if defined(__linux__) || defined(__apple__)
+    stream << "\033[32m";
+#endif
+  }
+  static void blue(std::ostream& stream) {
+#if defined(__linux__) || defined(__apple__)
+    stream << "\033[34m";
+#endif
+  }
+  static void bold(std::ostream& stream) {
+#if defined(__linux__) || defined(__apple__)
+    stream << "\033[1m";
 #endif
   }
 };
