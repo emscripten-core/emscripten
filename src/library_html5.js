9b8665d8fe73f10c39f5defcc029312ec37556e1
Owen Delahoy <owen.delahoy@gmail.com>
2015-11-07 23:29:44 +0900

    In webglcontextrestored callback extensions were automatically initialized are reinitialized and in webglcontextlost callbacks they are marked as invalid

--- src/library_html5.js
+++ src/library_html5.js
@@ -993,6 +993,11 @@ var LibraryJSEvents = {
       var handlerFunc = function(event) {
         var e = event || window.event;
 
+        if(target.GLctxObject) {
+          if(eventTypeId == {{{ cDefine('EMSCRIPTEN_EVENT_WEBGLCONTEXTLOST') }}}) target.GLctxObject.initExtensionsDone = false;
+          if(eventTypeId == {{{ cDefine('EMSCRIPTEN_EVENT_WEBGLCONTEXTRESTORED') }}}) GL.initExtensions(target.GLctxObject);
+        }
+
         var shouldCancel = Runtime.dynCall('iiii', callbackfunc, [eventTypeId, 0, userData]);
         if (shouldCancel) {
           e.preventDefault();
