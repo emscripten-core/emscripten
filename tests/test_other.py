3a4348461b34e4c840a21ee2217d4c1e2244d295
Jukka Jylänki <jujjyl@gmail.com>
2015-11-23 22:10:10 +0200

    Change -s USE_PTHREADS=2 and -s PRECISE_F32=2 to imply --separate-asm, instead of aborting when it is not specified. Closes #3829.

--- tests/test_other.py
+++ tests/test_other.py
@@ -5832,3 +5832,15 @@ int main() {
     size = os.stat('a.out.js.mem').st_size
     assert size < 4096, size
 
+  def test_separate_asm_warning(self):
+    # Test that -s PRECISE_F32=2 raises a warning that --separate-asm is implied.
+    stdout, stderr = Popen([PYTHON, EMCC, path_from_root('tests', 'hello_world.c'), '-s', 'PRECISE_F32=2'], stderr=PIPE).communicate()
+    self.assertContained('forcing separate asm output', stderr)
+
+    # Test that -s PRECISE_F32=2 --separate-asm should not post a warning.
+    stdout, stderr = Popen([PYTHON, EMCC, path_from_root('tests', 'hello_world.c'), '-s', 'PRECISE_F32=2', '--separate-asm'], stderr=PIPE).communicate()
+    self.assertNotContained('forcing separate asm output', stderr)
+
+    # Test that -s PRECISE_F32=1 should not post a warning.
+    stdout, stderr = Popen([PYTHON, EMCC, path_from_root('tests', 'hello_world.c'), '-s', 'PRECISE_F32=1'], stderr=PIPE).communicate()
+    self.assertNotContained('forcing separate asm output', stderr)
