9d3463bd55a09b38c7037a26bac9c71d2a40f4cf
Jukka Jylänki <jujjyl@gmail.com>
2015-12-07 23:52:41 +0200

    Add testing for C++11 atomic_thread_fence and atomic_fetch_add/sub(_explicit) functions.

--- tests/pthread/test_pthread_64bit_cxx11_atomics.cpp
+++ tests/pthread/test_pthread_64bit_cxx11_atomics.cpp
@@ -30,6 +30,30 @@ int main(void)
     assert(old == 0x3703370337033000ULL);
     assert(a.load(std::memory_order_relaxed) == 0xC803C80337FC30FFULL);
 
+    std::atomic_thread_fence(std::memory_order_acquire);
+
+    std::atomic<int64_t> b(0x0F0F0F0F0F0F0F0FULL);
+    old = std::atomic_fetch_add<int64_t>(&b, 1);
+    printf("%llx\n", old);
+    assert(old == 0x0F0F0F0F0F0F0F0FULL);
+    assert(b.load(std::memory_order_relaxed) == 0x0F0F0F0F0F0F0F10ULL);
+
+    old = std::atomic_fetch_sub<int64_t>(&b, 1);
+    printf("%llx\n", old);
+    assert(old == 0x0F0F0F0F0F0F0F10ULL);
+    assert(b.load(std::memory_order_relaxed) == 0x0F0F0F0F0F0F0F0FULL);
+
+    std::atomic<int64_t> c(0x0F0F0F0F0F0F0F0FULL);
+    old = std::atomic_fetch_add_explicit<int64_t>(&c, 1, std::memory_order_relaxed);
+    printf("%llx\n", old);
+    assert(old == 0x0F0F0F0F0F0F0F0FULL);
+    assert(c.load(std::memory_order_relaxed) == 0x0F0F0F0F0F0F0F10ULL);
+
+    old = std::atomic_fetch_sub_explicit<int64_t>(&c, 1, std::memory_order_relaxed);
+    printf("%llx\n", old);
+    assert(old == 0x0F0F0F0F0F0F0F10ULL);
+    assert(c.load(std::memory_order_relaxed) == 0x0F0F0F0F0F0F0F0FULL);
+
 #ifdef REPORT_RESULT
     int result = 0;
     REPORT_RESULT();
