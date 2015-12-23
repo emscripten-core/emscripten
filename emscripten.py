63d0624d3cd0b136efe438705630342186a31a13
Alon Zakai <alonzakai@gmail.com>
2015-12-23 12:05:45 -0800

    send global base to s2wasm

--- emscripten.py
+++ emscripten.py
@@ -1220,6 +1220,7 @@ def emscript_wasm_backend(infile, settings, outfile, outfile_name, libraries=[],
   assert settings['BINARYEN'], 'need BINARYEN option set so we can use Binaryen s2wasm on the backend output'
   wasm = outfile_name[:-3] + '.wast'
   s2wasm_args = [os.path.join(settings['BINARYEN'], 'bin', 's2wasm'), temp_s]
+  s2wasm_args += ['--global-base=%d' % shared.Settings.GLOBAL_BASE]
   if DEBUG:
     logging.debug('emscript: binaryen s2wasm: ' + ' '.join(s2wasm_args))
     t = time.time()
