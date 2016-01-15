efe3178cd6fe11091e2bb0bc4de391f505d6d511
Alon Zakai <alonzakai@gmail.com>
2016-01-15 13:41:06 -0800

    use a separate cache dir for wasm

--- emcc.py
+++ emcc.py
@@ -945,6 +945,9 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
       shared.set_llvm_target('asmjs-unknown-emscripten')
     else:
       shared.set_llvm_target('wasm32-unknown-unknown')
+      # use a separate directory for wasm
+      os.environ['EM_CACHE'] = os.path.join(shared.Cache.dirname, 'wasm')
+      shared.reconfigure_cache()
 
     newargs += shared.get_llvm_target_command()
 
