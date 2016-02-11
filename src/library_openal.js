74e56381ee2d7cd711e88f08def3d714b246b554
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-02-11 11:22:59 -0500

    Setting the global gain value should not replace the GainNode but set it's value instead.

--- src/library_openal.js
+++ src/library_openal.js
@@ -1254,7 +1254,7 @@ var LibraryOpenAL = {
     }
     switch (param) {
     case 0x100A /* AL_GAIN */:
-      AL.currentContext.gain.value = value;
+      AL.currentContext.gain.gain.value = value;
       break;
     default:
 #if OPENAL_DEBUG
