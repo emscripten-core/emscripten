54765d494e27733c223cfb32674865e5adb92fe3
Alon Zakai <alonzakai@gmail.com>
2016-02-24 11:16:22 -0800

    further work on ctor evaller

--- tools/ctor_evaller.py
+++ tools/ctor_evaller.py
@@ -10,6 +10,7 @@ import shared, js_optimizer
 js_file = sys.argv[1]
 mem_init_file = sys.argv[2]
 total_memory = int(sys.argv[3])
+global_base = int(sys.argv[4])
 
 temp_file = js_file + '.ctorEval.js'
 
@@ -25,17 +26,52 @@ def eval_ctor(js, mem_init):
   ctors_text = js[ctors_start:ctors_end]
   ctors = filter(lambda ctor: ctor.endswith('()') and not ctor == 'function()' and '.' not in ctor, ctors_text.split(' '))
   if len(ctors) == 0: return False
-  print ctors_text
   ctor = ctors[0].replace('()', '')
   shared.logging.debug('trying to eval ctor: ' + ctor)
   # Find the asm module, and receive the mem init.
   asm = js[js.find(js_optimizer.start_asm_marker):js.find(js_optimizer.end_asm_marker)]
   assert len(asm) > 0
   asm = asm.replace('use asm', 'not asm') # don't try to validate this
+  # hack out some special functions
+  def kill_func(asm, name):
+    before = len(asm)
+    asm = asm.replace('function ' + name + '(', 'function KILLED_' + name + '(', 1)
+    assert len(asm) > before
+    return asm
+  def add_func(asm, func):
+    before = len(asm)
+    asm = asm.replace('function ', ' ' + func + '\nfunction ', 1)
+    assert len(asm) > before
+    return asm
+  # replace malloc/free with a deterministic method that allocs unfreeable blocks at the end of static memory
+  asm = kill_func(asm, '_malloc')
+  asm = kill_func(asm, '_free')
+  asm = add_func(asm, 'function _malloc() { abort() }')
+  asm = add_func(asm, 'function _free() { abort() }')
   # Generate a safe sandboxed environment. We replace all ffis with errors. Otherwise,
   # asm.js can't call outside, so we are ok.
   open(temp_file, 'w').write('''
-var buffer = new ArrayBuffer(%s);
+var totalMemory = %s;
+
+var buffer = new ArrayBuffer(totalMemory);
+var heap = new Uint8Array(buffer);
+
+var memInit = %s;
+var globalBase = %d;
+
+heap.set(memInit, globalBase);
+
+if (!Math.imul) {
+  Math.imul = Math.imul || function(a, b) {
+    var ah = (a >>> 16) & 0xffff;
+    var al = a & 0xffff;
+    var bh = (b >>> 16) & 0xffff;
+    var bl = b & 0xffff;
+    // the shift by 0 fixes the sign on the high part
+    // the final |0 converts the unsigned value into a signed value
+    return ((al * bl) + (((ah * bl + al * bh) << 16) >>> 0)|0);
+  };
+}
 
 var globalArg = {
   Int8Array: Int8Array,
@@ -60,10 +96,16 @@ var libraryArg = {
 
 // Try to run the constructor
 asm['%s']();
+// We succeeded!
 
-// We succeeded - verify asm global vars, and write out new mem init
+// Verify asm global vars
 
-''' % (total_memory, asm, ctor))
+// Write out new mem init. It might be bigger, look for non-0 bytes
+var newSize = totalMemory;
+while (newSize > globalBase && heap[newSize-1] == 0) newSize--;
+console.log(Array.prototype.slice.call(heap.subarray(globalBase, newSize)));
+
+''' % (total_memory, mem_init, global_base, asm, ctor))
   # Execute the sandboxed code. If an error happened due to calling an ffi, that's fine,
   # us exiting with an error tells the caller that we failed.
   proc = subprocess.Popen(shared.NODE_JS + [temp_file], stdout=subprocess.PIPE)
