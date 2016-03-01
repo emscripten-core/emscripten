78b4cdf1b8f2c7153763afc19cf49a3081f4a19a
Alon Zakai <alonzakai@gmail.com>
2016-02-29 20:44:12 -0800

    fix indentation

--- system/lib/libc/musl/src/locale/newlocale.c
+++ system/lib/libc/musl/src/locale/newlocale.c
@@ -8,17 +8,17 @@ locale_t newlocale(int mask, const char *name, locale_t base)
 	if (*name && strcmp(name, "C") && strcmp(name, "POSIX"))
 		return 0;
 	if (!base) {
-    // XXX EMSCRIPTEN: avoid a malloc (which does time(), sysconf(), sbrk(), etc.) during startup
-    static struct __locale_struct first;
-    static int used_first = 0;
-    if (!used_first) {
-      used_first = 1;
-      memset(&first, 0, sizeof first);
-      base = &first;
-    } else {
-      base = calloc(1, sizeof *base);
-    }
-  }
+		// XXX EMSCRIPTEN: avoid a malloc (which does time(), sysconf(), sbrk(), etc.) during startup
+		static struct __locale_struct first;
+		static int used_first = 0;
+		if (!used_first) {
+			used_first = 1;
+			memset(&first, 0, sizeof first);
+			base = &first;
+		} else {
+			base = calloc(1, sizeof *base);
+		}
+	}
 	return base;
 }
 
