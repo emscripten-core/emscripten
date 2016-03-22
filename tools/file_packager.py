47b370ee4d4802f3c74bf56fe4dac56b62e975f8
Alon Zakai <alonzakai@gmail.com>
2016-03-22 10:53:58 -0700

    escape file names for run deps in file packager codegen

--- tools/file_packager.py
+++ tools/file_packager.py
@@ -226,7 +226,7 @@ def should_ignore(fullname):
 
 # Returns the given string with escapes added so that it can safely be placed inside a string in JS code.
 def escape_for_js_string(s):
-  s = s.replace("'", "\\'").replace('"', '\\"')
+  s = s.replace("'", "\\'").replace('"', '\\"').replace('\\', '/')
   return s
 
 # Expand directories into individual files
@@ -515,7 +515,7 @@ if has_preloaded:
             DataRequest.prototype.requests[files[i].filename].onload();
           }
     '''
-    use_data += "          Module['removeRunDependency']('datafile_%s');\n" % data_target
+    use_data += "          Module['removeRunDependency']('datafile_%s');\n" % escape_for_js_string(data_target)
 
   else:
     # LZ4FS usage
@@ -529,7 +529,7 @@ if has_preloaded:
           assert(typeof LZ4 === 'object', 'LZ4 not present - was your app build with  -s LZ4=1  ?');
           LZ4.loadPackage({ 'metadata': metadata, 'compressedData': compressedData });
           Module['removeRunDependency']('datafile_%s');
-    ''' % (meta, data_target)
+    ''' % (meta, escape_for_js_string(data_target))
 
   package_uuid = uuid.uuid4();
   package_name = data_target
@@ -718,7 +718,7 @@ if has_preloaded:
       %s
     };
     Module['addRunDependency']('datafile_%s');
-  ''' % (use_data, data_target) # use basename because from the browser's point of view, we need to find the datafile in the same dir as the html file
+  ''' % (use_data, escape_for_js_string(data_target)) # use basename because from the browser's point of view, we need to find the datafile in the same dir as the html file
 
   code += r'''
     if (!Module.preloadResults) Module.preloadResults = {};
