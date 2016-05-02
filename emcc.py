a617d155bc99db05a3f1ac3f63f972ddddb34fe7
cynecx <me@cynecx.net>
2016-05-02 21:46:43 +0200

    Address corner case when dealing with files which have defined handling eg. bitcode

--- emcc.py
+++ emcc.py
@@ -1242,19 +1242,22 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
       logging.debug(('just preprocessor ' if '-E' in newargs else 'just dependencies: ') + ' '.join(cmd))
       exit(subprocess.call(cmd))
 
+    def compile_source_file(i, input_file):
+      logging.debug('compiling source file: ' + input_file)
+      output_file = get_bitcode_file(input_file)
+      temp_files.append((i, output_file))
+      args = get_bitcode_args([input_file]) + ['-emit-llvm', '-c', '-o', output_file]
+      logging.debug("running: " + ' '.join(args))
+      execute(args) # let compiler frontend print directly, so colors are saved (PIPE kills that)
+      if not os.path.exists(output_file):
+        logging.error('compiler frontend failed to generate LLVM bitcode, halting')
+        sys.exit(1)
+
     # First, generate LLVM bitcode. For each input file, we get base.o with bitcode
     for i, input_file in input_files:
       file_ending = filename_type_ending(input_file)
-      if file_ending.endswith(SOURCE_ENDINGS) or has_fixed_language_mode:
-        logging.debug('compiling source file: ' + input_file)
-        output_file = get_bitcode_file(input_file)
-        temp_files.append((i, output_file))
-        args = get_bitcode_args([input_file]) + ['-emit-llvm', '-c', '-o', output_file]
-        logging.debug("running: " + ' '.join(args))
-        execute(args) # let compiler frontend print directly, so colors are saved (PIPE kills that)
-        if not os.path.exists(output_file):
-          logging.error('compiler frontend failed to generate LLVM bitcode, halting')
-          sys.exit(1)
+      if file_ending.endswith(SOURCE_ENDINGS):
+        compile_source_file(i, input_file)
       else: # bitcode
         if file_ending.endswith(BITCODE_ENDINGS):
           logging.debug('using bitcode file: ' + input_file)
@@ -1269,8 +1272,11 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
             shared.Building.llvm_as(input_file, temp_file)
             temp_files.append((i, temp_file))
         else:
-          logging.error(input_file + ': Unknown file suffix when compiling to LLVM bitcode!')
-          sys.exit(1)
+          if(has_fixed_language_mode):
+            compile_source_file(i, input_file)
+          else:
+            logging.error(input_file + ': Unknown file suffix when compiling to LLVM bitcode!')
+            sys.exit(1)
 
     log_time('bitcodeize inputs')
 
