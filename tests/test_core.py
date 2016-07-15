7543d073fc64e5c3ebe89688b98a586b0a9ddff9
Alon Zakai <alonzakai@gmail.com>
2016-07-14 17:40:01 -0700

    update some tests for binaryen

--- tests/test_core.py
+++ tests/test_core.py
@@ -5344,6 +5344,7 @@ def process(filename):
 
   @no_wasm
   def test_fs_base(self):
+    if self.is_wasm(): return self.skip('wasm libc overlaps js lib, so no INCLUDE_FULL_LIBRARY')
     Settings.INCLUDE_FULL_LIBRARY = 1
     try:
       addJS = '''
@@ -6677,6 +6678,7 @@ def process(filename):
 
   def test_fuzz(self):
     Building.COMPILER_TEST_OPTS += ['-I' + path_from_root('tests', 'fuzz', 'include'), '-w']
+    Settings.BINARYEN_IMPRECISE = 0 # some of these tests - 2.c', '9.c', '19.c', '21.c', '20.cpp' - div or rem i32 by 0, which traps in wasm
 
     def run_all(x):
       print x
