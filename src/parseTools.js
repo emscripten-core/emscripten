8e66034fc5223c366e21e9c8ca4299533340aeef
aheejin <aheejin@gmail.com>
2016-08-30 17:53:09 -0700

    {, } around if and else bodies

--- src/parseTools.js
+++ src/parseTools.js
@@ -1292,10 +1292,11 @@ function makeGetTempRet0() {
 }
 
 function makeSetTempRet0(value) {
-  if (WASM_BACKEND == 1)
+  if (WASM_BACKEND == 1) {
     return 'asm["setTempRet0"](' + value + ')';
-  else
+  } else {
     return RELOCATABLE ? "setTempRet0((" + value + ") | 0)" : ("tempRet0 = " + value);
+  }
 }
 
 function makeStructuralReturn(values, inAsm) {
