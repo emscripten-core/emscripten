a6713050c83b4f8e2568c98cd2db1a54f8349b38
Jukka Jylänki <jujjyl@gmail.com>
2016-10-11 23:18:51 +0300

    Fix polyfill of Atomics.wake() to return 0 instead of undefined to avoid catching a -s ASSERTIONS=1 assertion in emscripten_futex_wake() when running with -s USE_PTHREADS=2 on a browser that does not support multithreading.

--- src/preamble.js
+++ src/preamble.js
@@ -1181,8 +1181,8 @@ if (typeof Atomics === 'undefined') {
   Atomics['and'] = function(t, i, v) { var w = t[i]; t[i] &= v; return w; }
   Atomics['compareExchange'] = function(t, i, e, r) { var w = t[i]; if (w == e) t[i] = r; return w; }
   Atomics['wait'] = function(t, i, v, o) { if (t[i] != v) abort('Multithreading is not supported, cannot sleep to wait for futex!'); }
-  Atomics['wake'] = function(t, i, c) {}
-  Atomics['wakeOrRequeue'] = function(t, i1, c, i2, v) {}
+  Atomics['wake'] = function(t, i, c) { return 0; }
+  Atomics['wakeOrRequeue'] = function(t, i1, c, i2, v) { return 0; }
   Atomics['isLockFree'] = function(s) { return true; }
   Atomics['load'] = function(t, i) { return t[i]; }
   Atomics['or'] = function(t, i, v) { var w = t[i]; t[i] |= v; return w; }
