emscripten-core/emscripten

commit c5af8f63f00bb03aa2177c71d16628474918b60f

Fix module pollution (#4582)
* Fix module pollution

* export_name on modularize

* author :D

tests/test_browser.py
@@ -2754,11 +2754,12 @@ window.close = function() {
     self.btest('emterpreter_async_iostream.cpp', '1', args=['-s', 'EMTERPRETIFY=1', '-s', 'EMTERPRETIFY_ASYNC=1'])
 
   def test_modularize(self):
-    for opts in [[], ['-O1'], ['-O2', '-profiling'], ['-O2']]:
+    for opts in [[], ['-O1'], ['-O2', '-profiling'], ['-O2'], ['-O2', '--closure', '1']]:
       for args, code in [
         ([], 'Module();'), # defaults
         (['-s', 'EXPORT_NAME="HelloWorld"'], '''
           if (typeof Module !== "undefined") throw "what?!"; // do not pollute the global scope, we are modularized!
+          HelloWorld.noInitialRun = true; // errorneous module capture will load this and cause timeout
           HelloWorld();
         '''), # use EXPORT_NAME
         (['-s', 'EXPORT_NAME="HelloWorld"'], '''

AUTHORS
@@ -261,3 +261,4 @@ a license to everyone to use it as detailed in LICENSE.)
 * Mateusz Borycki <mateuszborycki@gmail.com>
 * Franklin Ta <fta2012@gmail.com>
 * Jacob Gravelle <jgravelle@google.com> (copyright owned by Google, Inc.)
+* Kagami Sascha Rosylight <saschanaz@outlook.com>

emcc.py
@@ -1862,12 +1862,12 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
       src = open(final).read()
       final = final + '.modular.js'
       f = open(final, 'w')
-      f.write('var ' + shared.Settings.EXPORT_NAME + ' = function(Module) {\n')
-      f.write('  Module = Module || {};\n')
+      f.write('var ' + shared.Settings.EXPORT_NAME + ' = function(' + shared.Settings.EXPORT_NAME + ') {\n')
+      f.write('  ' + shared.Settings.EXPORT_NAME + ' = ' + shared.Settings.EXPORT_NAME + ' || {};\n')
       f.write('\n')
       f.write(src)
       f.write('\n')
-      f.write('  return Module;\n')
+      f.write('  return ' + shared.Settings.EXPORT_NAME + ';\n')
       f.write('};\n')
       f.close()
       src = None

commit ab755c672ec23ffb7786ab6d544a4e51bc787e9e

add a test illustrating a regression since commit c5af8f6

tests/test_browser.py
@@ -2789,6 +2789,41 @@ window.close = function() {
         ''' % code)
         self.run_browser('a.html', '...', '/report_result?0')
 
+  # test illustrating the regression on the modularize feature since commit c5af8f6
+  # when compiling with the --preload-file option
+  def test_modularize_and_preload_files(self):
+    # amount of memory different from the default one that will be allocated for the emscripten heap
+    totalMemory = 33554432
+    for opts in [[], ['-O1'], ['-O2', '-profiling'], ['-O2'], ['-O2', '--closure', '1']]:
+      # the main function simply checks that the amount of allocated heap memory is correct
+      src = r'''
+        #include <stdio.h>
+        #include <emscripten.h>
+        int main() {
+          EM_ASM({
+            // use eval here in order for the test with closure compiler enabled to succeed
+            var totalMemory = eval('Module.TOTAL_MEMORY');
+            assert(totalMemory === %d, 'bad memory size');
+          });
+          int result = 0;
+          REPORT_RESULT();
+          return 0;
+        }
+      ''' % totalMemory
+      open('test.c', 'w').write(self.with_report_result(src))
+      # generate a dummy file
+      open('dummy_file', 'w').write('dummy')
+      # compile the code with the modularize feature and the preload-file option enabled
+      Popen([PYTHON, EMCC, 'test.c', '-s', 'MODULARIZE=1', '-s', 'EXPORT_NAME="Foo"', '--preload-file', 'dummy_file'] + opts).communicate()
+      open('a.html', 'w').write('''
+        <script src="a.out.js"></script>
+        <script>
+          // instantiate the Foo module with custom TOTAL_MEMORY value
+          var foo = Foo({ TOTAL_MEMORY: %d });
+        </script>
+      ''' % totalMemory)
+      self.run_browser('a.html', '...', '/report_result?0')
+
   def test_webidl(self):
     # see original in test_core.py
     output = Popen([PYTHON, path_from_root('tools', 'webidl_binder.py'),