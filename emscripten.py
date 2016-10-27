435dd044be9777214efe11ce1ba1525e94afd069
Jukka Jylanki <jujjyl@gmail.com>
2016-10-27 17:55:29 +0300

    Remove SIMD {Float,Int}32x4.{load,store}3() functions, which don't validate as asm.js. See #4645.

--- emscripten.py
+++ emscripten.py
@@ -574,7 +574,7 @@ function _emscripten_asm_const_%s(%s) {
                          'equal', 'lessThan', 'greaterThan',
                          'notEqual', 'lessThanOrEqual', 'greaterThanOrEqual',
                          'select', 'swizzle', 'shuffle',
-                         'load', 'store', 'load1', 'store1', 'load2', 'store2', 'load3', 'store3']
+                         'load', 'store', 'load1', 'store1', 'load2', 'store2']
     simdintboolfuncs = ['and', 'xor', 'or', 'not']
     if metadata['simdUint8x16']:
       simdinttypes += ['Uint8x16']
@@ -830,7 +830,7 @@ function ftCall_%s(%s) {%s
         return False
       nonexisting_simd_symbols = ['Int8x16_fromInt8x16', 'Uint8x16_fromUint8x16', 'Int16x8_fromInt16x8', 'Uint16x8_fromUint16x8', 'Int32x4_fromInt32x4', 'Uint32x4_fromUint32x4', 'Float32x4_fromFloat32x4', 'Float64x2_fromFloat64x2']
       nonexisting_simd_symbols += ['Int32x4_addSaturate', 'Int32x4_subSaturate', 'Uint32x4_addSaturate', 'Uint32x4_subSaturate']
-      nonexisting_simd_symbols += [(x + '_' + y) for x in ['Int8x16', 'Uint8x16', 'Int16x8', 'Uint16x8', 'Float64x2'] for y in ['load2', 'load3', 'store2', 'store3']]
+      nonexisting_simd_symbols += [(x + '_' + y) for x in ['Int8x16', 'Uint8x16', 'Int16x8', 'Uint16x8', 'Float64x2'] for y in ['load2', 'store2']]
       nonexisting_simd_symbols += [(x + '_' + y) for x in ['Int8x16', 'Uint8x16', 'Int16x8', 'Uint16x8'] for y in ['load1', 'store1']]
 
       asm_global_funcs += ''.join(['  var SIMD_' + ty + '=global' + access_quote('SIMD') + access_quote(ty) + ';\n' for ty in simdtypes])
