1f5e49bceb01e3ace3c09584df935e0bc43552cc
Alon Zakai <alonzakai@gmail.com>
2016-11-03 14:43:54 -0700

    Emmake binaryen (#4676)
    When emmake is run, we set env vars to cross-compile using emcc. But when building binaryen as a side task from such an invocation, we need to build it natively, so use a native building environment.
    
    Also fix up the native building environment to remove our extra cross-compiling vars.

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -955,13 +955,26 @@ fi
   def test_binaryen(self):
     import tools.ports.binaryen as binaryen
     tag_file = Cache.get_path('binaryen_tag_' + binaryen.TAG + '.txt')
-    try_delete(tag_file)
-    # if BINARYEN_ROOT is set, we don't build the port. Check we do built it if not
-    restore()
-    config = open(CONFIG_FILE).read()
-    config = config.replace('BINARYEN_ROOT', '#')
-    open(CONFIG_FILE, 'w').write(config)
+    def prep():
+      wipe()
+      self.do([PYTHON, EMCC, '--clear-ports'])
+      try_delete(tag_file)
+      # if BINARYEN_ROOT is set, we don't build the port. Check we do built it if not
+      restore()
+      config = open(CONFIG_FILE).read()
+      config = config.replace('BINARYEN_ROOT', '#')
+      open(CONFIG_FILE, 'w').write(config)
+
+    print 'build using embuilder'
+    prep()
     subprocess.check_call([PYTHON, 'embuilder.py', 'build', 'binaryen'])
     assert os.path.exists(tag_file)
     subprocess.check_call([PYTHON, 'emcc.py', 'tests/hello_world.c', '-s', 'BINARYEN=1'])
     self.assertContained('hello, world!', run_js('a.out.js'))
+
+    print 'see if building with emmake works (should not break native compilation of binaryen port'
+    prep()
+    subprocess.check_call([PYTHON, 'emmake.py', EMCC, 'tests/hello_world.c', '-s', 'BINARYEN=1'])
+    assert os.path.exists(tag_file)
+    self.assertContained('hello, world!', run_js('a.out.js'))
+
--- tools/shared.py
+++ tools/shared.py
@@ -1183,6 +1183,13 @@ class Building:
       env['CXX'] = CLANG_CPP
       env['LD'] = CLANG
       env['CFLAGS'] = '-O2 -fno-math-errno'
+      # get a non-native one, and see if we have some of its effects - remove them if so
+      non_native = Building.get_building_env()
+      # the ones that a non-native would modify
+      EMSCRIPTEN_MODIFIES = ['LDSHARED', 'AR', 'CROSS_COMPILE', 'NM', 'RANLIB']
+      for dangerous in EMSCRIPTEN_MODIFIES:
+        if env.get(dangerous) and env.get(dangerous) == non_native.get(dangerous):
+          del env[dangerous] # better to delete it than leave it, as the non-native one is definitely wrong
       return env
     env['CC'] = EMCC if not WINDOWS else 'python %r' % EMCC
     env['CXX'] = EMXX if not WINDOWS else 'python %r' % EMXX
