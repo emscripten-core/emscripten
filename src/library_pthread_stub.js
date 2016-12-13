6c5091820f50542f92fa23d8462ae8a968ab9061
Jacob Gravelle <jgravelle@google.com>
2016-12-13 11:53:28 -0800

    Always allocate pthread_self, even in the stub

--- src/library_pthread_stub.js
+++ src/library_pthread_stub.js
@@ -79,10 +79,12 @@ var LibraryPThreadStub = {
     return 0;
   },
 
+  _pthread_self_impl: '{{{ makeStaticAlloc(C_STRUCTS.pthread.__size__) }}}',
+  pthread_self__deps: ['_pthread_self_impl'],
   pthread_self__asm: true,
   pthread_self__sig: 'i',
   pthread_self: function() {
-    return 0;
+    return __pthread_self_impl;
   },
 
   pthread_attr_init: function(attr) {
