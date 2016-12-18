608e97fd58143fa24759c581553e5e48953a23e9
Jukka Jylänki <jujjyl@gmail.com>
2016-12-18 13:53:49 +0200

    Clean up temporary .wast output file if it is not needed. #4799.

--- emcc.py
+++ emcc.py
@@ -2074,6 +2074,11 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
             cmd += ['--symbolmap=' + target + '.symbols']
           logging.debug('wasm-as (text => binary): ' + ' '.join(cmd))
           subprocess.check_call(cmd)
+
+          # The .wast file is only needed when targeting output mode that interprets it. Otherwise clean it up.
+          if not 'interpret-s-expr' in shared.Settings.BINARYEN_METHOD and not DEBUG:
+            shared.try_delete(wasm_text_target)
+
         if shared.Settings.BINARYEN_SCRIPTS:
           binaryen_scripts = os.path.join(shared.Settings.BINARYEN_ROOT, 'scripts')
           script_env = os.environ.copy()
