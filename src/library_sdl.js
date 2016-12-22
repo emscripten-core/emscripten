emscripten-core/emscripten

commit 789657c5ed8f625ca16c466a46dafc5444fa37ed

use fileno to get fds from FILE*

src/library_sdl.js
@@ -2577,7 +2577,8 @@ var LibrarySDL = {
 
       if (type === 2/*SDL_RWOPS_STDFILE*/) {
         var fp = {{{ makeGetValue('rwopsID + ' + 28 /*hidden.stdio.fp*/, '0', 'i32') }}};
-        var stream = FS.getStreamFromPtr(fp);
+        var fd = Module['_fileno'](file);
+        var stream = FS.getStream(fd);
         if (stream) {
           rwops = { filename: stream.path };
         }

src/library_browser.js
@@ -1380,7 +1380,8 @@ mergeInto(LibraryManager.library, {
 
   emscripten_get_preloaded_image_data_from_FILE__deps: ['emscripten_get_preloaded_image_data'],
   emscripten_get_preloaded_image_data_from_FILE: function(file, w, h) {
-    var stream = FS.getStreamFromPtr(file);
+    var fd = Module['_fileno'](file);
+    var stream = FS.getStream(fd);
     if (stream) {
       return _emscripten_get_preloaded_image_data(stream.path, w, h);
     }

commit c90bb49491c5acc789ceaf43f44fa850b45e8f13

Fix loading from file in SDL_mixer
This resolves a regression in Mix_LoadWAV_RW() which was introduced in
commit 789657c5ed8f.

src/library_sdl.js
@@ -2586,7 +2586,7 @@ var LibrarySDL = {
 
       if (type === 2/*SDL_RWOPS_STDFILE*/) {
         var fp = {{{ makeGetValue('rwopsID + ' + 28 /*hidden.stdio.fp*/, '0', 'i32') }}};
-        var fd = Module['_fileno'](file);
+        var fd = Module['_fileno'](fp);
         var stream = FS.getStream(fd);
         if (stream) {
           rwops = { filename: stream.path };