cb5b2cce602556ddc0e59a0518c89e59bd80c151
Anatoly Trosinenko <anatoly.trosinenko@gmail.com>
2017-01-21 16:32:18 +0300

    Make anonymous mmap return page-aligned addresses
    Make addresses returned by anonymous mmap consistent with the reported
    page size (currently, 4096).
    
    An obsolete memalign function was used instead of posix_memalign,
    since it passes pointer to the allocated memory as a return value and
    do not require us to pass void **ptr from the JS library code.
    
    In theory, pointers returned by memalign (but not posix_memalign)
    may be non free()-able, but particular implementation from dlmalloc
    is documented as having minor differences from posix_memalign and there
    is already a test that mmaps some memory and then unmaps it.

--- tests/core/test_mmap.c
+++ tests/core/test_mmap.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <sys/mman.h>
 #include <assert.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
+  // Alignment check for mmap below should be consistent with the reported page size
+  // For now, just require it to be 4096
+  assert(getpagesize() == 4096);
+  assert(sysconf(_SC_PAGESIZE) == 4096);
+
   for (int i = 0; i < 10; i++) {
     int* map = (int*)mmap(0, 5000, PROT_READ | PROT_WRITE,
                           MAP_SHARED | MAP_ANON, -1, 0);
-    /* TODO: Should we align to 4k?
+    assert(map != MAP_FAILED);
     assert(((int)map) % 4096 == 0); // aligned
-    */
     assert(munmap(map, 5000) == 0);
   }
 
