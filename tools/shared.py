0e39e1bfef580823256fcdc7f72bd79fb013433d
Hubert Figuière <hub@figuiere.net>
2017-02-02 13:31:46 -0500

    Check for LLVM env to set llvm_root on initial startup

--- tools/shared.py
+++ tools/shared.py
@@ -224,7 +224,7 @@ else:
     config_file = '\n'.join(config_file)
     # autodetect some default paths
     config_file = config_file.replace('\'{{{ EMSCRIPTEN_ROOT }}}\'', repr(__rootpath__))
-    llvm_root = os.path.dirname(find_executable('llvm-dis') or '/usr/bin/llvm-dis')
+    llvm_root = os.environ.get('LLVM') or os.path.dirname(find_executable('llvm-dis') or '/usr/bin/llvm-dis')
     config_file = config_file.replace('\'{{{ LLVM_ROOT }}}\'', repr(llvm_root))
     binaryen_root = os.path.dirname(find_executable('asm2wasm') or '/usr/bin/asm2wasm')
     config_file = config_file.replace('\'{{{ BINARYEN_ROOT }}}\'', repr(binaryen_root))
