96fe9ed5d0fe282f74715f8be0005aaba654a580
Jacob Gravelle <jgravelle@google.com>
2017-02-14 15:30:27 -0800

    Simplify further, add string support (slow)

--- system/include/emscripten/em_asm.h
+++ system/include/emscripten/em_asm.h
@@ -18,47 +18,53 @@
     base(const char* code, __VA_ARGS__);
 #define EM_DECL_REC_2(base, ...) \
     EM_DECL_REC_1(base, int _1, __VA_ARGS__) \
-    EM_DECL_REC_1(base, double _1, __VA_ARGS__)
+    EM_DECL_REC_1(base, double _1, __VA_ARGS__) \
+    EM_DECL_REC_1(base, const char* _1, __VA_ARGS__)
 #define EM_DECL_REC_3(base, ...) \
     EM_DECL_REC_2(base, int _2, __VA_ARGS__) \
-    EM_DECL_REC_2(base, double _2, __VA_ARGS__)
+    EM_DECL_REC_2(base, double _2, __VA_ARGS__) \
+    EM_DECL_REC_2(base, const char* _2, __VA_ARGS__)
 #define EM_DECL_REC_4(base, ...) \
     EM_DECL_REC_3(base, int _3, __VA_ARGS__) \
-    EM_DECL_REC_3(base, double _3, __VA_ARGS__)
+    EM_DECL_REC_3(base, double _3, __VA_ARGS__) \
+    EM_DECL_REC_3(base, const char* _3, __VA_ARGS__)
 #define EM_DECL_REC_5(base, ...) \
     EM_DECL_REC_4(base, int _4, __VA_ARGS__) \
-    EM_DECL_REC_4(base, double _4, __VA_ARGS__)
+    EM_DECL_REC_4(base, double _4, __VA_ARGS__) \
+    EM_DECL_REC_4(base, const char* _4, __VA_ARGS__)
 #define EM_DECL_REC_6(base, ...) \
     EM_DECL_REC_5(base, int _5, __VA_ARGS__) \
-    EM_DECL_REC_5(base, double _5, __VA_ARGS__)
+    EM_DECL_REC_5(base, double _5, __VA_ARGS__) \
+    EM_DECL_REC_5(base, const char* _5, __VA_ARGS__)
 #define EM_DECL_REC_7(base, ...) \
     EM_DECL_REC_6(base, int _6, __VA_ARGS__) \
-    EM_DECL_REC_6(base, double _6, __VA_ARGS__)
+    EM_DECL_REC_6(base, double _6, __VA_ARGS__) \
+    EM_DECL_REC_6(base, const char* _6, __VA_ARGS__)
 #define EM_DECL_REC_N(N, base) \
     CONCAT(EM_DECL_REC_, N)(base, int _N) \
-    CONCAT(EM_DECL_REC_, N)(base, double _N)
+    CONCAT(EM_DECL_REC_, N)(base, double _N) \
+    CONCAT(EM_DECL_REC_, N)(base, const char* _N)
 #define EM_DECL(N) \
-    EM_DECL_REC_N(N, extern void emscripten_asm_const_##N) \
-    EM_DECL_REC_N(N, extern int emscripten_asm_const_int_##N) \
-    EM_DECL_REC_N(N, extern double emscripten_asm_const_double_##N)
-#else
-#define EM_DECL(N) \
-    extern void emscripten_asm_const_##N(); \
-    extern int emscripten_asm_const_int_##N(); \
-    extern double emscripten_asm_const_double_##N();
-#endif
-#define EM_ASM_IMPL(base, code, ...) CONCAT3(base, _, NARG(__VA_ARGS__))(#code, __VA_ARGS__)
-extern void emscripten_asm_const(const char* code);
-
-#define EM_ASM(...) emscripten_asm_const(#__VA_ARGS__)
-#define EM_ASM_(...) EM_ASM_IMPL(emscripten_asm_const_int, __VA_ARGS__)
-#define EM_ASM_ARGS(...) EM_ASM_IMPL(emscripten_asm_const_int, __VA_ARGS__)
-#define EM_ASM_INT(...) EM_ASM_IMPL(emscripten_asm_const_int, __VA_ARGS__)
-#define EM_ASM_DOUBLE(...) EM_ASM_IMPL(emscripten_asm_const_double, __VA_ARGS__)
-#define EM_ASM_INT_V(code) emscripten_asm_const_int_0(#code)
-#define EM_ASM_DOUBLE_V(code) emscripten_asm_const_double_0(#code)
+    EM_DECL_REC_N(N, void emscripten_asm_const) \
+    EM_DECL_REC_N(N, int emscripten_asm_const_int) \
+    EM_DECL_REC_N(N, double emscripten_asm_const_double)
 
 EM_DECL(0); EM_DECL(1); EM_DECL(2); EM_DECL(3);
-EM_DECL(4); EM_DECL(5); EM_DECL(6); EM_DECL(7);
+// EM_DECL(4); EM_DECL(5); EM_DECL(6); EM_DECL(7);
+#else
+extern void emscripten_asm_const(); \
+extern int emscripten_asm_const_int(); \
+extern double emscripten_asm_const_double();
+#endif
+
+extern void emscripten_asm_const(const char* code);
+
+#define EM_ASM(code) emscripten_asm_const(#code)
+#define EM_ASM_(code, ...) emscripten_asm_const_int(#code, __VA_ARGS__)
+#define EM_ASM_ARGS(code, ...) emscripten_asm_const_int(#code, __VA_ARGS__)
+#define EM_ASM_INT(code, ...) emscripten_asm_const_int(#code, __VA_ARGS__)
+#define EM_ASM_DOUBLE(code, ...) emscripten_asm_const_double(#code, __VA_ARGS__)
+#define EM_ASM_INT_V(code) emscripten_asm_const_int(#code)
+#define EM_ASM_DOUBLE_V(code) emscripten_asm_const_double(#code)
 
 #endif // __em_asm_h__
