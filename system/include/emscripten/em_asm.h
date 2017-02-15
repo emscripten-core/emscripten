885336c241fd1441b921a40cce65a2c0a62791f0
Jacob Gravelle <jgravelle@google.com>
2017-02-14 16:19:11 -0800

    Remove extern, unused macros

--- system/include/emscripten/em_asm.h
+++ system/include/emscripten/em_asm.h
@@ -1,27 +1,17 @@
 #ifndef __em_asm_h__
 #define __em_asm_h__
 
-
-#define CONCAT(x, y) CONCAT_(x, y)
-#define CONCAT_(x, y) x##y
-#define CONCAT3(x, y, z) CONCAT(CONCAT(x, y), z)
-
-#define NARG(...) NARG_(__VA_ARGS__, RSEQ_N())
-#define NARG_(...) ARG_N(__VA_ARGS__)
-#define ARG_N(_1, _2, _3, _4, _5, _6, _7, N, ...) N
-#define RSEQ_N() 7, 6, 5, 4, 3, 2, 1, 0
-
 #ifdef __cplusplus
 template <typename... Args> void emscripten_asm_const(const char* code, Args...);
 template <typename... Args> int emscripten_asm_const_int(const char* code, Args...);
 template <typename... Args> double emscripten_asm_const_double(const char* code, Args...);
 #else
-extern void emscripten_asm_const(); \
-extern int emscripten_asm_const_int(); \
-extern double emscripten_asm_const_double();
+void emscripten_asm_const(); \
+int emscripten_asm_const_int(); \
+double emscripten_asm_const_double();
 #endif
 
-extern void emscripten_asm_const(const char* code);
+void emscripten_asm_const(const char* code);
 
 #define EM_ASM(code) emscripten_asm_const(#code)
 #define EM_ASM_(code, ...) emscripten_asm_const_int(#code, __VA_ARGS__)
