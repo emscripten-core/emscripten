5a37fde930f37458450df7dfa6b0ab2a00ec4c2f
Jacob Gravelle <jgravelle@google.com>
2017-02-16 16:15:22 -0800

    Add explanation for EM_ASM declarations. Reorder C++ asm/wasm cases for readability

--- system/include/emscripten/em_asm.h
+++ system/include/emscripten/em_asm.h
@@ -1,23 +1,38 @@
 #ifndef __em_asm_h__
 #define __em_asm_h__
 
+// The wasm backend calls vararg functions by passing the variadic arguments as
+// an array on the stack. For EM_ASM's underlying functions, s2wasm needs to
+// translate them to emscripten_asm_const_[signature], but the expected
+// signature and arguments has been lost as part of the vararg buffer.
+// Therefore, we declare EM_ASM's implementing functions as non-variadic.
+
 #ifndef __cplusplus
+// In C, declare these as non-prototype declarations. This is obsolete K&R C
+// (that is still supported) that causes the C frontend to consider any calls
+// to them as valid, and avoids using the vararg calling convention.
 void emscripten_asm_const();
 int emscripten_asm_const_int();
 double emscripten_asm_const_double();
 #else
-#ifdef __asmjs
+#ifndef __asmjs
+// C++ interprets an empty parameter list as a function taking no arguments,
+// instead of a K&R C function declaration. Variadic templates are lowered as
+// non-vararg calls to the instantiated templated function, which we then
+// replace in s2wasm.
+template <typename... Args> void emscripten_asm_const(const char* code, Args...);
+template <typename... Args> int emscripten_asm_const_int(const char* code, Args...);
+template <typename... Args> double emscripten_asm_const_double(const char* code, Args...);
+#else
+// asmjs has no problem handling these when they're vararg, but doesn't handle
+// mangled C++ names, so let them be vararg when we don't use the wasm backend.
 extern "C" {
     void emscripten_asm_const(const char* code);
     int emscripten_asm_const_int(const char* code, ...);
     double emscripten_asm_const_double(const char* code, ...);
 }
-#else
-template <typename... Args> void emscripten_asm_const(const char* code, Args...);
-template <typename... Args> int emscripten_asm_const_int(const char* code, Args...);
-template <typename... Args> double emscripten_asm_const_double(const char* code, Args...);
-#endif
-#endif
+#endif // __asmjs
+#endif // __cplusplus
 
 void emscripten_asm_const(const char* code);
 
