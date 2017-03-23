0125c795c2221ea124a594718216485a226e3a47
Jacob Gravelle <jgravelle@google.com>
2017-03-23 14:24:00 -0700

    Simpler faking of traceback objects

--- tests/parallel_runner.py
+++ tests/parallel_runner.py
@@ -15,125 +15,20 @@ class TestResultType(enum.Enum):
   Error = 3
 
 
-def can_pickle(item):
-  try:
-    pickle.dumps(item)
-    return True
-  except TypeError as e:
-    # print 'cannot pickle', item, 'because:', e
-    # traceback.print_tb(sys.exc_info()[2])
-    return False
-  except:
-    print "UNEXPECTED EXCEPTION FROM pickle.dumps"
+class FakeTraceback(object):
+  def __init__(self, tb):
+    self.tb_frame = FakeFrame(tb.tb_frame)
+    self.tb_lineno = tb.tb_lineno
+    self.tb_next = FakeTraceback(tb.tb_next) if tb.tb_next is not None else None
+class FakeFrame(object):
+  def __init__(self, f):
+    self.f_code = FakeCode(f.f_code)
+    self.f_globals = [] # f.f_globals
+class FakeCode(object):
+  def __init__(self, co):
+    self.co_filename = co.co_filename
+    self.co_name = co.co_name
 
-class FakeObject(object):
-  def __init__(self):
-    print 'initing', self
-    self.__real_attrs = {}
-
-  def __getstate__(self):
-    print 'getting state of', self
-    print 'state is', self.__real_attrs
-    return self.__real_attrs
-
-  def __setstate__(self, state):
-    print 'setting state of', self, state
-    self.__real_attrs = state
-
-  def __getattr__(self, key):
-    print 'getting attr of', self, key
-    if key in ('__getinitargs__', '__getnewargs__'):
-      raise AttributeError
-    if key in ('_FakeObject__real_attrs'):
-      print 'oh dear'
-      return self.__dict__[key]
-    print 'dict is', self.__real_attrs
-    return self.__real_attrs[key]
-
-  def __setattr__(self, key, value):
-    print 'setting attr of', self, key, value
-    if key in ('_FakeObject__real_attrs'):
-      self.__dict__[key] = value
-    else:
-      self.__real_attrs[key] = value
-
-class AutoFaker(object):
-  def __init__(self, wrapped):
-    print 'initing w/', wrapped
-    self.__wrapped = wrapped
-    self.__faked = {}
-    for method in dir(wrapped):
-      print 'considering', method
-      a = getattr(wrapped, method)
-      if callable(a) and method != '__class__':
-        print 'trying to set', method
-        def wrapped(s, *args, **kwargs):
-          a(s, *args, **kwargs)
-        setattr(self, method, wrapped)
-      print '  worked'
-    print 'done w/ init'
-
-  def __getattr__(self, key):
-    print 'autofake get:', key
-    if not self.__faked.get(key):
-      val = getattr(self.__wrapped, key)
-      if not can_pickle(val):
-        val = AutoFaker(val)
-      self.__faked[key] = val
-    return self.__faked[key]
-
-  # def __iter__(self):
-  #   print 'itering'
-  #   setattr(self.__faked, '__iter__', self.__wrapped.__iter__)
-  #   return self.__wrapped.__iter__()
-
-  # def next(self):
-  #   print 'nexting'
-  #   setattr(self.__faked, 'next', self.__wrapped.next)
-  #   return self.__wrapped.next()
-
-  def reify_fake_object(self):
-    print 'reifying'
-    result = FakeObject()
-    for k, v in self.__faked.iteritems():
-      if isinstance(v, AutoFaker):
-        setattr(result, k, v.reify_fake_object())
-    for method in dir(self.wrapped):
-      a = getattr(self.wrapped, method)
-      if callable(a):
-        setattr(result, method, a)
-    return result
-
-  def make_dict(self):
-    result = {}
-    for k, v in self.__faked.iteritems():
-      if isinstance(v, AutoFaker):
-        result[k] = v.make_dict()
-      else:
-        result[k] = v
-    return result
-    # return self.__faked
-
-def fake_for(item, funcs):
-  print 'faking:', item
-  fake = AutoFaker(item)
-  print 'trying'
-  try:
-    for f in funcs:
-      f(fake)
-    ret = fake.reify_fake_object()
-  except Exception as e:
-    print 'aw dang:', e
-    raise e
-  print 'fake:', ret
-  print ret.__dict__
-  print 'fakedict:', fake.make_dict()
-  print 'can pickle:', can_pickle(ret)
-  print 'pickled:', pickle.dumps(ret)
-  rounded = pickle.loads(pickle.dumps(ret))
-  print 'roundtripped:', rounded.__dict__
-  print 'wamp'
-  return ret
 
 class BufferedParallelTestResult(object):
   def __init__(self):
@@ -178,48 +73,8 @@ class BufferedParallelTestResult(object):
     self._set_error(err)
 
   def _set_error(self, err):
-    if can_pickle(err):
-      self.result_error = err
-    else:
-      a, b, tb = err
-      self.result_error = (a, b, self.fake_traceback(tb))
-
-  @staticmethod
-  def fake_traceback(real_traceback):
-    # TODO: create a fake traceback object that mirrors the string info of the real one
-    '''
-    Need to mock:
-      tb_frame
-      tb_lineno
-      tb_next
-
-    In particular:
-      list = []
-      n = 0
-      while tb is not None and (limit is None or n < limit):
-          f = tb.tb_frame
-          lineno = tb.tb_lineno
-          co = f.f_code
-          filename = co.co_filename
-          name = co.co_name
-          linecache.checkcache(filename)
-          line = linecache.getline(filename, lineno, f.f_globals)
-          if line: line = line.strip()
-          else: line = None
-          list.append((filename, lineno, name, line))
-          tb = tb.tb_next
-          n = n+1
-      return list
-    '''
-    return fake_for(real_traceback, [
-      traceback.extract_tb,
-      lambda tb: 'foo' in tb.tb_frame.f_globals
-    ])
-    # try:
-    #   # We need to construct a traceback object and this is a hacky way to do that
-    #   1/0
-    # except Exception as e:
-    #   return sys.exc_info()[2]
+    a, b, tb = err
+    self.result_error = (a, b, FakeTraceback(tb))
 
 
 class ParallelTestSuite(unittest.BaseTestSuite):
