804ca6ad52e2b605d7769eaf31ca0defa14127f5
Jacob Gravelle <jgravelle@google.com>
2017-03-30 13:40:08 -0700

    Disable unsupported wasm backend test

--- tests/test_core.py
+++ tests/test_core.py
@@ -6061,6 +6061,7 @@ def process(filename):
     '''
     self.do_run(src, '|1.266,1|\n')
 
+  @no_wasm_backend('Need support for -g in wasm backend')
   def test_demangle_stacks(self):
     Settings.DEMANGLE_SUPPORT = 1
     if '-O' in str(self.emcc_args):
