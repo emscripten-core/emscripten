bc8333f853083b825b4cf5bfd2c08e0ed08eb3ea
Jacob Gravelle <jgravelle@google.com>
2017-04-26 09:27:58 -0700

    Explicitly pass zero as the high bits of the offset in __stdio_seek as well as lseek

--- system/lib/libc/musl/src/stdio/__stdio_seek.c
+++ system/lib/libc/musl/src/stdio/__stdio_seek.c
@@ -4,7 +4,13 @@ off_t __stdio_seek(FILE *f, off_t off, int whence)
 {
 	off_t ret;
 #ifdef SYS__llseek
-	if (syscall(SYS__llseek, f->fd, off>>32, off, &ret, whence)<0)
+	off_t off_hi =
+#ifndef __EMSCRIPTEN__
+		off >> 32;
+#else
+		0;
+#endif
+	if (syscall(SYS__llseek, f->fd, off_hi, off, &ret, whence)<0)
 		ret = -1;
 #else
 	ret = syscall(SYS_lseek, f->fd, off, whence);
