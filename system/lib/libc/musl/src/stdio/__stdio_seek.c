cd8158d7a0be532e4d402038e1273b5dbd7760ce
Jacob Gravelle <jgravelle@google.com>
2017-05-01 09:54:03 -0700

    Remove assert from syscall140, remove Emscripten-specific offset logic from libc

--- system/lib/libc/musl/src/stdio/__stdio_seek.c
+++ system/lib/libc/musl/src/stdio/__stdio_seek.c
@@ -4,13 +4,7 @@ off_t __stdio_seek(FILE *f, off_t off, int whence)
 {
 	off_t ret;
 #ifdef SYS__llseek
-	off_t off_hi =
-#ifndef __EMSCRIPTEN__
-		off >> 32;
-#else
-		0;
-#endif
-	if (syscall(SYS__llseek, f->fd, off_hi, off, &ret, whence)<0)
+	if (syscall(SYS__llseek, f->fd, off>>32, off, &ret, whence)<0)
 		ret = -1;
 #else
 	ret = syscall(SYS_lseek, f->fd, off, whence);
