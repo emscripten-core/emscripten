5c0dba9eb836ca9fbba7e341e2714e16f7dbfd1b
Aleksander Guryanov <caiiiycuk@gmail.com>
2017-05-04 01:38:37 +0700

    cocos2d add subset of v2 + extensions (#5188)

--- tools/ports/cocos2d.py
+++ tools/ports/cocos2d.py
@@ -3,18 +3,17 @@ import shutil
 import logging
 import re
 
-TAG = 'version_3_1e'
+TAG = 'version_3_2'
 
 
 def get(ports, settings, shared):
   if settings.USE_COCOS2D == 3:
     ports.fetch_project(
-      'Cocos2d', 'https://github.com/emscripten-ports/Cocos2d/archive/version_3_1e.zip', 'Cocos2d-' + TAG)
+      'Cocos2d', 'https://github.com/emscripten-ports/Cocos2d/archive/' + TAG + '.zip', 'Cocos2d-' + TAG)
 
     def create():
       logging.info('building port: Cocos2d v3')
       logging.warn('Cocos2d: library is experimental, do not expect that it will work out of the box')
-      logging.warn('Cocos2d: please use STB_IMAGE=1 or --use-preload-plugin to work with images')
 
       cocos2d_build = os.path.join(ports.get_dir(), 'Cocos2d')
       cocos2d_root = os.path.join(cocos2d_build, 'Cocos2d-' + TAG)
@@ -89,48 +88,62 @@ def show():
 
 
 def make_source_list(cocos2d_root, cocos2dx_root):
-  makefile = os.path.join(cocos2dx_root, 'proj.emscripten', 'Makefile')
   sources = []
-  with open(makefile) as infile:
-    add_next = False
-    for line in infile:
-      if line.startswith('SOURCES'):
-        sources.append(
-          re.search('=\s*(.*?)(\s*\\\\$|\s*$)', line, re.IGNORECASE).group(1))
-        add_next = line.endswith('\\\n')
-        continue
-      if add_next:
-        sources.append(
-          re.search('\s*(.*?)(\s*\\\\$|\s*$)', line, re.IGNORECASE).group(1))
-        add_next = line.endswith('\\\n')
-  
+
+  def add_makefile(makefile):
+    with open(makefile) as infile:
+      add_next = False
+      for line in infile:
+        if line.startswith('SOURCES'):
+          file = re.search('=\s*(.*?)(\s*\\\\$|\s*$)', line, re.IGNORECASE).group(1)
+          absfile = os.path.abspath(os.path.join(os.path.dirname(makefile), file))
+          sources.append(absfile)
+          add_next = line.endswith('\\\n')
+          continue
+        if add_next:
+          file = re.search('\s*(.*?)(\s*\\\\$|\s*$)', line, re.IGNORECASE).group(1)
+          absfile = os.path.abspath(os.path.join(os.path.dirname(makefile), file))
+          sources.append(absfile)
+          add_next = line.endswith('\\\n')
+
+  # core
+  add_makefile(os.path.join(cocos2dx_root, 'proj.emscripten', 'Makefile'))
+  # extensions
+  add_makefile(os.path.join(cocos2d_root, 'extensions', 'proj.emscripten', 'Makefile'))
+  # external
+  add_makefile(os.path.join(cocos2d_root, 'external', 'Box2D', 'proj.emscripten', 'Makefile'))
+  add_makefile(os.path.join(cocos2d_root, 'external', 'chipmunk', 'proj.emscripten', 'Makefile'))
+  add_makefile(os.path.join(cocos2dx_root, 'platform', 'third_party', 'Makefile'))
+  # misc
   sources.append(os.path.join(cocos2d_root, 'CocosDenshion', 'emscripten', 'SimpleAudioEngine.cpp'))
+  sources.append(os.path.join(cocos2dx_root, 'CCDeprecated.cpp')) # subset of cocos2d v2
   return sources
 
 
 def make_includes(cocos2d_root, cocos2dx_root):
   return [os.path.join(cocos2d_root, 'CocosDenshion', 'include'),
       os.path.join(cocos2d_root, 'extensions'),
-      os.path.join(cocos2d_root, 'extensions'),
+      os.path.join(cocos2d_root, 'extensions', 'AssetsManager'),
+      os.path.join(cocos2d_root, 'extensions', 'CCArmature'),
       os.path.join(cocos2d_root, 'extensions', 'CCBReader'),
-      os.path.join(cocos2d_root, 'extensions',
-             'GUI', 'CCControlExtension'),
+      os.path.join(cocos2d_root, 'extensions', 'GUI', 'CCControlExtension'),
+      os.path.join(cocos2d_root, 'extensions', 'GUI', 'CCEditBox'),
+      os.path.join(cocos2d_root, 'extensions', 'GUI', 'CCScrollView'),
       os.path.join(cocos2d_root, 'extensions', 'network'),
       os.path.join(cocos2d_root, 'extensions', 'Components'),
-      os.path.join(cocos2d_root, 'external',
-             'chipmunk', 'include', 'chipmunk'),
+      os.path.join(cocos2d_root, 'extensions', 'LocalStorage'),
+      os.path.join(cocos2d_root, 'extensions', 'physics_nodes'),
+      os.path.join(cocos2d_root, 'extensions', 'spine'),
+      os.path.join(cocos2d_root, 'external'),
+      os.path.join(cocos2d_root, 'external', 'chipmunk', 'include', 'chipmunk'),
       cocos2dx_root,
       os.path.join(cocos2dx_root, 'cocoa'),
       os.path.join(cocos2dx_root, 'include'),
       os.path.join(cocos2dx_root, 'kazmath', 'include'),
       os.path.join(cocos2dx_root, 'platform'),
       os.path.join(cocos2dx_root, 'platform', 'emscripten'),
-      os.path.join(cocos2dx_root, 'platform',
-             'third_party', 'linux', 'libfreetype2'),
-      os.path.join(cocos2dx_root, 'platform',
-             'third_party', 'common', 'etc'),
-      os.path.join(cocos2dx_root, 'platform', 'third_party',
-             'emscripten', 'libtiff', 'include'),
-      os.path.join(cocos2dx_root, 'platform',
-             'third_party', 'emscripten', 'libjpeg'),
+      os.path.join(cocos2dx_root, 'platform', 'third_party', 'linux', 'libfreetype2'),
+      os.path.join(cocos2dx_root, 'platform', 'third_party', 'common', 'etc'),
+      os.path.join(cocos2dx_root, 'platform', 'third_party', 'emscripten', 'libtiff', 'include'),
+      os.path.join(cocos2dx_root, 'platform', 'third_party', 'emscripten', 'libjpeg'),
       os.path.join(cocos2dx_root, 'platform', 'third_party', 'emscripten', 'libwebp')]
