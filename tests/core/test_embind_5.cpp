8654549be4f7d8ea8fe8970229bf8767b4a5d154
jgravelle-google <jgravelle@google.com>
2017-05-03 11:31:59 -0700

    Fix embinding derived classes (#5193)
    Embinding derived classes uses template-generated upcast/downcast
    functions for construction internally. Always converting them to
    void-returning function pointers causes runtime pointer aliasing
    errors. The type here should only be void-returning in the case of the
    NoBaseClass internal struct.

--- tests/core/test_embind_5.cpp
+++ tests/core/test_embind_5.cpp
@@ -2,29 +2,50 @@
 #include <emscripten/bind.h>
 
 using namespace emscripten;
+
+
 class MyFoo {
 public:
-    MyFoo() {
-        EM_ASM({print("constructing my foo");});
-    }
-    void doit() {
-        EM_ASM({print("doing it");});
-    }
+  MyFoo() {
+    EM_ASM({Module.print("constructing my foo");});
+  }
+  virtual void doit() {
+    EM_ASM({Module.print("doing it");});
+  }
 };
+
+class MyBar : public MyFoo {
+public:
+  MyBar() {
+    EM_ASM({Module.print("constructing my bar");});
+  }
+  void doit() override {
+    EM_ASM({Module.print("doing something else");});
+  }
+};
+
+
 EMSCRIPTEN_BINDINGS(my_module) {
-    class_<MyFoo>("MyFoo")
-      .constructor<>()
-      .function("doit", &MyFoo::doit)
-      ;
+  class_<MyFoo>("MyFoo")
+    .constructor<>()
+    .function("doit", &MyFoo::doit)
+    ;
+  class_<MyBar, base<MyFoo>>("MyBar")
+    .constructor<>()
+    ;
 }
+
+
 int main(int argc, char **argv) {
-    EM_ASM(
-      try {
-        var foo = new Module.MyFoo();
-        foo.doit();
-      } catch(e) {
-        Module.print(e);
-      }
-    );
-    return 0;
+  EM_ASM(
+    try {
+      var foo = new Module.MyFoo();
+      foo.doit();
+      var bar = new Module.MyBar();
+      bar.doit();
+    } catch(e) {
+      Module.print(e);
+    }
+  );
+  return 0;
 }
