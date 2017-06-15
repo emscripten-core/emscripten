a8f1f89d15df77abf5710db530c709affb37bd10
Jukka Jylänki <jujjyl@gmail.com>
2017-06-15 14:21:20 +0300

    Add test for #5258.

--- src/preamble.js
+++ src/preamble.js
@@ -1166,9 +1166,9 @@ assert(typeof Int32Array !== 'undefined' && typeof Float64Array !== 'undefined' 
 #if IN_TEST_HARNESS
 
 // Test runs in browsers should always be free from uncaught exceptions. If an uncaught exception is thrown, we fail browser test execution in the REPORT_RESULT() macro to output an error value.
-if (typeof window !== 'undefined') {
+if (ENVIRONMENT_IS_WEB) {
   window.addEventListener('error', function(e) {
-    if (e == 'SimulateInfiniteLoop' || e.message.indexOf('SimulateInfiniteLoop') != -1) return;
+    if (e === 'SimulateInfiniteLoop' || e.message.indexOf('SimulateInfiniteLoop') != -1) return;
     console.error('Page threw an exception ' + e);
     Module['pageThrewException'] = true;
   });
