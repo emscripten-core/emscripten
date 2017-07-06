8a4a798564d1487d643db92cc7fadd4c88e3e77c
etienne02 <etienne02@users.noreply.github.com>
2017-07-06 15:44:35 +0200

    Update threads.h

--- system/include/libc/threads.h
+++ system/include/libc/threads.h
@@ -17,8 +17,8 @@ typedef unsigned tss_t;
 typedef int (*thrd_start_t)(void *);
 typedef void (*tss_dtor_t)(void *);
 
-#define __NEED_cnd_t
-#define __NEED_mtx_t
+#define __NEED_pthread_cond_t
+#define __NEED_pthread_mutex_t
 
 #include <bits/alltypes.h>
 
