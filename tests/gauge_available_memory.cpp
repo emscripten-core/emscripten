f510b6846b221944762ea849e9026c9fc168bdb8
Jukka Jylänki <jujjyl@gmail.com>
2017-08-26 18:14:16 +0300

    Do not use up all memory when poking amount of memory available, because REPORT_RESULT() needs to malloc() a bit too after that.

--- tests/gauge_available_memory.cpp
+++ tests/gauge_available_memory.cpp
@@ -13,9 +13,10 @@ int main()
 	// Keep allocating in ~powers of two until we fail, then print out how much we got.
 	uint64_t availableMemory = 0;
 	uint64_t i = 0x80000000ULL;
-	while(i > 0)
+	while(i > 512) // Leak almost all available memory, but not the last bytes, because REPORT_RESULT() needs to malloc() a bit in order to proxy from pthread to main thread.
 	{
 		void *ptr = leak_alloc(i);
+		printf("Alloc %llu: %p\n", i, ptr);
 		if (ptr) availableMemory += i;
 		else i >>= 1;
 	}
