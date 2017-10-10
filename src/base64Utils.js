60fc52b015f002cb703722fe305a9f0d74cc3252
Ryan Lester <hacker@linux.com>
2017-10-10 12:15:24 -0400

    remove unneeded function (#5296)

--- src/base64Utils.js
+++ src/base64Utils.js
@@ -8,36 +8,6 @@
 
 var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-var btoa = btoa || function (input) {
-  /**
-   * Encodes a string in base64
-   * @param {String} input The string to encode in base64.
-   */
-  var output = '';
-  var chr1, chr2, chr3;
-  var enc1, enc2, enc3, enc4;
-  var i = 0;
-  do {
-    chr1 = input.charCodeAt(i++);
-    chr2 = input.charCodeAt(i++);
-    chr3 = input.charCodeAt(i++);
-
-    enc1 = chr1 >> 2;
-    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-    enc4 = chr3 & 63;
-
-    if (isNaN(chr2)) {
-      enc2 = ((chr1 & 3) << 4);
-      enc3 = enc4 = 64;
-    } else if (isNaN(chr3)) {
-      enc4 = 64;
-    }
-    output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
-  } while (i < input.length);
-  return output;
-};
-
 var atob = atob || function (input) {
   /**
    * Decodes a base64 string.
