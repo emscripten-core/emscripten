deb852086fe5dc28aafb9344dd5062d1ee59f088
Jukka Jylänki <jujjyl@gmail.com>
2017-10-12 20:48:07 +0300

    Add test case for the failing scenario where file is loaded from a relative path.
    (cherry picked from commit 6afe133)

--- tests/test_other.py
+++ tests/test_other.py
@@ -7993,3 +7993,10 @@ int main() {
                     assert expect_wast == os.path.exists('a.out.wast')
                     if should_run_js:
                       self.assertContained('hello, world!', run_js('a.out.js'))
+
+  # Tests that Emscripten-compiled applications can be run from a relative path with node command line that is different than the current working directory.
+  def test_node_js_run_from_different_directory(self):
+    os.mkdir('subdir')
+    Popen([PYTHON, EMCC, path_from_root('tests', 'hello_world.c'), '-o', os.path.join('subdir', 'a.js'), '-O3']).communicate()
+    ret = Popen(NODE_JS + [os.path.join('subdir', 'a.js')], stdout=PIPE).communicate()[0]
+    assert 'hello, world!' in ret
