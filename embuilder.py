4a8b083c59c38ffd395d6fbfd94cac11bbb2d516
Alon Zakai <alonzakai@gmail.com>
2017-10-16 16:00:32 -0700

    fix

--- embuilder.py
+++ embuilder.py
@@ -94,26 +94,27 @@ temp_files = shared.configuration.get_temp_files()
 def build(src, result_libs, args=[]):
   # if a library is a .a, also build the .bc, as we need it when forcing a
   # a system library - in that case, we always want all the code linked in
-  need_forced = []
-  with_forced = []
-  for result_lib in result_libs:
-    print(result_lib)
-    if result_lib.endswith('.a'):
-      print('append!')
-      short = result_lib[:-2]
-      need_forced.append(short)
-      with_forced.append(short + '.bc')
-    else:
-      with_forced.append(result_lib)
-  if need_forced:
-    if os.environ.get('EMCC_FORCE_STDLIBS'):
-      print('skipping forced (.bc) versions of .a libraries, since EMCC_FORCE_STDLIBS already set')
-    else:
-      os.environ['EMCC_FORCE_STDLIBS'] = ','.join(need_forced)
-      try:
-        build(src, with_forced, args)
-      finally:
-        del os.environ['EMCC_FORCE_STDLIBS']
+  if result_libs:
+    need_forced = []
+    with_forced = []
+    for result_lib in result_libs:
+      print(result_lib)
+      if result_lib.endswith('.a'):
+        print('append!')
+        short = result_lib[:-2]
+        need_forced.append(short)
+        with_forced.append(short + '.bc')
+      else:
+        with_forced.append(result_lib)
+    if need_forced:
+      if os.environ.get('EMCC_FORCE_STDLIBS'):
+        print('skipping forced (.bc) versions of .a libraries, since EMCC_FORCE_STDLIBS already set')
+      else:
+        os.environ['EMCC_FORCE_STDLIBS'] = ','.join(need_forced)
+        try:
+          build(src, with_forced, args)
+        finally:
+          del os.environ['EMCC_FORCE_STDLIBS']
 
   # build in order to generate the libraries
   with temp_files.get_file('.cpp') as temp:
