48822745a4cb5efb5d2675ededae9d86839adac9
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2017-11-11 04:40:59 +0200

    Added very basic WebGL2 externs. Some tweaks to GL externs that caused certain tests to fail.

--- src/closure-externs.js
+++ src/closure-externs.js
@@ -1122,32 +1122,113 @@ var define = function (wrapper) {};
 /**
  * GL functions (https://github.com/kripken/emscripten/commit/97a464a654fdadf5dfb8aa082b48516e6bf8d402#commitcomment-25520648)
  */
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glDrawArrays;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glDrawElements;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glActiveTexture;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glEnable;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glDisable;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glTexEnvf;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glTexEnvi;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glTexEnvfv;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glGetIntegerv;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glIsEnabled;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glGetBooleanv;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glGetString;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glCreateShader;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glShaderSource;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glCompileShader;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glAttachShader;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glDetachShader;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glUseProgram;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glDeleteProgram;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glBindAttribLocation;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glLinkProgram;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glBindBuffer;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glGetFloatv;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glHint;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glEnableVertexAttribArray;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glDisableVertexAttribArray;
+/**
+ * @suppress {duplicate, undefinedVars}
+ */
 var _emscripten_glVertexAttribPointer;
 
 var _glDrawArrays;
@@ -1175,3 +1256,21 @@ var minorVersion;
  * @param {!Function} listener
  */
 var addEventListener = function (type, listener) {};
+
+/**
+ * @param {HTMLCanvasElement} canvas
+ *
+ * @suppress {duplicate}
+ * @todo: https://github.com/kripken/emscripten/commit/946a27ee58ddd6cdcfcc896fea0f8187e2263795#commitcomment-25545119
+ */
+var __registerRestoreOldStyle = function(canvas) {};
+
+/**
+ * @param {HTMLCanvasElement} element
+ * @param {number} topBottom
+ * @param {number} leftRight
+ *
+ * @suppress {duplicate}
+ * @todo: https://github.com/kripken/emscripten/commit/946a27ee58ddd6cdcfcc896fea0f8187e2263795#commitcomment-25545119
+ */
+var __setLetterbox = function(element, topBottom, leftRight) {};
