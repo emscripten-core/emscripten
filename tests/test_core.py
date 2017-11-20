aa30ea90c5a115d45e382a17960b2b0177cbdb2f
Alon Zakai <alonzakai@gmail.com>
2017-11-20 14:09:22 -0800

    emscripten_log closure fixes (#5808)
    * fix emscripten_log with closure

--- tests/test_core.py
+++ tests/test_core.py
@@ -6768,6 +6768,11 @@ Module.printErr = Module['printErr'] = function(){};
 
 Success!
 ''')
+    # test closure compiler as well
+    if self.run_name == 'asm2':
+      print('closure')
+      self.emcc_args += ['--closure', '1', '-g1'] # extra testing
+      self.do_run_in_out_file_test('tests', 'emscripten_log', 'emscripten_log_with_closure')
 
   def test_float_literals(self):
     self.do_run_in_out_file_test('tests', 'test_float_literals')
