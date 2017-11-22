e7d9ddf5c9115ae60ebb433a14ec4af0125085ce
Jukka Jylanki <jujjyl@gmail.com>
2017-11-22 14:09:01 +0200

    Mark test_main_thread_em_asm and test_main_thread_async_em_asm tests @no_wasm_backend

--- tests/test_core.py
+++ tests/test_core.py
@@ -1611,6 +1611,7 @@ int main(int argc, char **argv) {
   # Tests various different ways to invoke the MAIN_THREAD_EM_ASM(), MAIN_THREAD_EM_ASM_INT() and MAIN_THREAD_EM_ASM_DOUBLE() macros.
   # This test is identical to test_em_asm_2, just search-replaces EM_ASM to MAIN_THREAD_EM_ASM on the test file. That way if new
   # test cases are added to test_em_asm_2.cpp for EM_ASM, they will also get tested in MAIN_THREAD_EM_ASM form.
+  @@no_wasm_backend
   def test_main_thread_em_asm(self):
     src = open(path_from_root('tests', 'core', 'test_em_asm_2.cpp'), 'r').read()
     test_file = 'src.cpp'
@@ -1623,6 +1624,7 @@ int main(int argc, char **argv) {
     self.do_run_from_file(test_file, expected_result_file)
     self.do_run_from_file(test_file, expected_result_file, force_c=True)
 
+  @no_wasm_backend
   def test_main_thread_async_em_asm(self):
     self.do_run_in_out_file_test('tests', 'core', 'test_main_thread_async_em_asm')
     self.do_run_in_out_file_test('tests', 'core', 'test_main_thread_async_em_asm', force_c=True)
