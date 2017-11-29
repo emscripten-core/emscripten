096f7ec51d7558f809c40f35ccb69330685d8968
Alon Zakai (kripken) <alonzakai@gmail.com>
2017-11-29 13:55:03 -0800

    fix

--- tests/test_core.py
+++ tests/test_core.py
@@ -1240,6 +1240,7 @@ int main() {
     self.do_run_in_out_file_test('tests', 'core', 'test_inherit')
 
   def test_isdigit_l(self):
+      Settings.NO_EXIT_RUNTIME = 0 # needs to flush stdio streams
       self.do_run_in_out_file_test('tests', 'core', 'test_isdigit_l')
 
   def test_iswdigit(self):
