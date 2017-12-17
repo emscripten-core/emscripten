d71bc837f06bcaf04f7dd9913a57b7da541ce6af
Alon Zakai (kripken) <alonzakai@gmail.com>
2017-12-17 08:41:36 -0800

    fix

--- src/library_fs.js
+++ src/library_fs.js
@@ -13,7 +13,9 @@ mergeInto(LibraryManager.library, {
   $FS__postset: 'FS.staticInit();' +
                 '__ATINIT__.unshift(function() { if (!Module["noFSInit"] && !FS.init.initialized) FS.init() });' +
                 '__ATMAIN__.push(function() { FS.ignorePermissions = false });' +
-                '__ATEXIT__.push(function() { FS.quit() });',
+                '__ATEXIT__.push(function() { FS.quit() });' +
+                // Get module methods from settings
+                '{{{ EXPORTED_RUNTIME_METHODS.filter(function(func) { return func.substr(0, 3) === 'FS_' }).map(function(func){return 'Module["' + func + '"] = FS.' + func.substr(3) + ";"}).reduce(function(str, func){return str + func;}, '') }}}',
   $FS: {
     root: null,
     mounts: [],
