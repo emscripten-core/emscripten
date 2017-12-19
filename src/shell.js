b10820ff07a015e44eac9e49b5d719ca9c5cbe1f
Derek Schuff <dschuff@chromium.org>
2017-12-18 23:01:31 -0800

    Exit node.js with error status on unhandled promise rejections (#5948)
    Currently node will swallow unhandled promise rejections and continue as
    if no error occurred. Today it prints a deprecation warning when that
    happens, and in the future it will exit with a non-zero status. That
    behavior is what we want in any case, so until then, use the
    unhandled promise rejection hook to get behavior to match other shells.

--- src/shell.js
+++ src/shell.js
@@ -137,6 +137,14 @@ if (ENVIRONMENT_IS_NODE) {
     }
   });
 #endif
+  // Currently node will swallow unhandled rejections, but this behavior is
+  // deprecated, and in the future it will exit with error status.
+  process['on']('unhandledRejection', function(reason, p) {
+#if ASSERTIONS
+    Module['printErr']('node.js exiting due to unhandled promise rejection');
+#endif
+    process['exit'](1);
+  });
 
   Module['inspect'] = function () { return '[Emscripten Module object]'; };
 }
