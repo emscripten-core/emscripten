1fd9d0bfc06fb5817997aae795419ac0bf7594e6
Alon Zakai <alonzakai@gmail.com>
2017-12-21 13:20:12 -0800

    fix isDataURI, it must be used in the HTML as well as JS

--- src/preamble.js
+++ src/preamble.js
@@ -1987,17 +1987,6 @@ Module['FS_createPreloadedFile'] = FS.createPreloadedFile;
 var cyberDWARFFile = '{{{ BUNDLED_CD_DEBUG_FILE }}}';
 #endif
 
-// Prefix of data URIs emitted by SINGLE_FILE and related options.
-var dataURIPrefix = 'data:application/octet-stream;base64,';
-
-// Indicates whether filename is a base64 data URI.
-function isDataURI(filename) {
-  return String.prototype.startsWith ?
-      filename.startsWith(dataURIPrefix) :
-      filename.indexOf(dataURIPrefix) === 0
-  ;
-}
-
 #if BINARYEN
 function integrateWasmJS() {
   // wasm.js has several methods for creating the compiled code module here:
