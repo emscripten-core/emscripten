28c39211b30d710a8dc7e07a5ad7e5aecf4c08fa
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-01-04 13:43:31 -0800

    test

---
+++ tests/optimizer/emitDCEGraph5-output.js
@@ -0,0 +1,13 @@
+[
+ {
+  "name": "emcc$defun$__GLOBAL__I_000101",
+  "reaches": [
+   "emcc$export$_GLOBAL__I_000101"
+  ]
+ },
+ {
+  "name": "emcc$export$_GLOBAL__I_000101",
+  "export": "_GLOBAL__I_000101",
+  "reaches": []
+ }
+]
---
+++ tests/optimizer/emitDCEGraph5.js
@@ -0,0 +1,7 @@
+// wasm backend notation has one fewer _ in the wasm
+__ATINIT__.push({ func: function() { __GLOBAL__I_000101() } }, { func: function() { __GLOBAL__sub_I_iostream_cpp() } });
+var __GLOBAL__I_000101 = Module["__GLOBAL__I_000101"] = function() { return Module["asm"]["_GLOBAL__I_000101"].apply(null, arguments) };
+
+Module.asmLibraryArg = {
+};
+
