967a8a2f2ee79434219c26c00e8075ecc9a528a0
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-01-04 17:04:28 -0800

    improve

--- tests/core/stackAlloc.cpp
+++ tests/core/stackAlloc.cpp
@@ -2,17 +2,20 @@
 
 int main() {
   EM_ASM({
-    var size = 10;
+    var size = 100;
+    var before;
+    before = stackSave();
     var x = stackAlloc(size);
     var y = stackAlloc(size);
-    var z = stackAlloc(size);
-    assert(x != y && y != z && x != z);
     var direction = y > x ? 1 : -1;
-    assert((y - x)*(z - y) > 0); // same direction
+    assert(x == Math.min(before, before + direction*size), "allocation must return the start of the range allocated");
+    var z = stackAlloc(size);
+    assert(x != y && y != z && x != z, "allocations must be unique");
+    assert((y - x)*(z - y) > 0, "allocations must be in the same direction");
     // no overlaps
     function notInRange(value, oneSide, otherSide) {
       assert(!(Math.min(oneSide, otherSide) <= value &&
-               value <= Math.max(oneSide, otherSide)));
+               value <= Math.max(oneSide, otherSide)), value + " must not be in the range " + [oneSide, otherSide]);
     }
     notInRange(x, y, y + direction*size);
     notInRange(x, z, z + direction*size);
