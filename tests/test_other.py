9fc522a1f8ec8c73c4e508b29754a5617833cc7c
Jukka Jylänki <jujjyl@gmail.com>
2018-01-15 11:49:34 +0200

    Fix test other.test_fs_stream_proto on Windows.

--- tests/test_other.py
+++ tests/test_other.py
@@ -2605,7 +2605,7 @@ void wakaw::Cm::RasterBase<wakaw::watwat::Polocator>::merbine1<wakaw::Cm::Raster
                          run_js ('index.js', engine=NODE_JS, stderr=STDOUT, assert_returncode=None))
 
   def test_fs_stream_proto(self):
-    open('src.cpp', 'w').write(r'''
+    open('src.cpp', 'wb').write(r'''
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
