055c82ad465dd26d06b509ac51f6bc8c19cf406e
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-02-15 10:58:16 -0800

    test 5ms in get_now, as browsers limit to 2ms

--- tests/emscripten_get_now.cpp
+++ tests/emscripten_get_now.cpp
@@ -13,33 +13,32 @@ int main() {
   // a) Calling emscripten_get_now(), time actually proceeds.
   // b) Values returned by emscripten_get_now() are strictly nondecreasing.
   // c) emscripten_get_now() is able to return sub-millisecond precision timer values.
-  bool detected_good_timer_precision = false;
-  double smallest_delta = 0.f;
+  double smallest_delta = 1000.f;
   for(int x = 0; x < 1000; ++x) { // Have several attempts to find a good small delta, i.e. give time to JS engine to warm up the code and so on.
     double t = emscripten_get_now();
-    double t2 = emscripten_get_now();
-    for(int i = 0; i < 100 && t == t2; ++i) {
+    double t2 = t;
+    while (t2 == t) {
       t2 = emscripten_get_now();
     }
-
     if (t2 < t && t2 - t < 1000.f) { // Timer must be monotonous.
       printf("Timer is not monotonous!\n");
       smallest_delta = t2 - t;
       break;
     }
-    if (t2 > t && t2 - t < 0.7f) { // Must pass less than a millisecond between two calls.
-      detected_good_timer_precision = true;
+    if (t2 > t) { // Must pass less than a millisecond between two calls.
       smallest_delta = t2 - t;
       break;
     }
   }
   
-  if (detected_good_timer_precision) {
-    printf("Timer resolution is good. (%f msecs)\n", smallest_delta);
+  printf("Timer resolution (smallest delta): %.20f msecs\n", smallest_delta);
+
+  if (smallest_delta < 5.f) {
+    printf("Timer resolution is good (or as good as spectre allows...).\n");
     result = 1;
   } else {
-    printf("Error: Bad timer precision: Smallest timer delta: %f msecs. Likely due to spectre mitigations :( skipping\n", smallest_delta);
-    result = 1; // TODO: 0
+    printf("Error: Bad timer precision, too large\n");
+    result = 0;
   }
   REPORT_RESULT(result);
   return 0;
