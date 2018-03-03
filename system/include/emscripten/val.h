1bd6d46ebb2becf4c99d8642da1135e24d149f82
Rob Fors <github@robfors.com>
2018-03-03 17:53:51 -0500

    update comments

--- system/include/emscripten/val.h
+++ system/include/emscripten/val.h
@@ -273,7 +273,6 @@ namespace emscripten {
         // missing operators:
         // * delete
         // * in
-        // * instanceof
         // * ! ~ - + ++ --
         // * * / %
         // * + -
