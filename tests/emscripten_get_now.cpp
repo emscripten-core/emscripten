2d9c6ef56f7fa610c61cb64196be44a30d9629d5
Alon Zakai <alonzakai@gmail.com>
2018-03-16 14:09:06 -0700

    make browser.test_emscripten_get_now more robust: wait up to 5 seconds to look for the small-enough resolution

--- tests/emscripten_get_now.cpp
+++ tests/emscripten_get_now.cpp
@@ -8,32 +8,47 @@
 
 int result = 0;
 
+const double good_enough = 5.0; // good as we can do, given spectre...
+
 int main() {
   // This code tests three things:
   // a) Calling emscripten_get_now(), time actually proceeds.
   // b) Values returned by emscripten_get_now() are strictly nondecreasing.
   // c) emscripten_get_now() is able to return sub-millisecond precision timer values.
   double smallest_delta = 1000.f;
-  for(int x = 0; x < 1000; ++x) { // Have several attempts to find a good small delta, i.e. give time to JS engine to warm up the code and so on.
+  double start = emscripten_get_now();
+  while (1) { // Have several attempts to find a good small delta, i.e. give time to JS engine to warm up the code and so on.
     double t = emscripten_get_now();
+    if (t - start >= 5000) {
+      printf("We waited long enough, %f ms\n", t - start);
+      break;
+    }
     double t2 = t;
     while (t2 == t) {
       t2 = emscripten_get_now();
     }
-    if (t2 < t && t2 - t < 1000.f) { // Timer must be monotonous.
+    double delta = t2 - t;
+    if (delta < 0.) { // Timer must be monotonous.
       printf("Timer is not monotonous!\n");
-      smallest_delta = t2 - t;
-      break;
+      REPORT_RESULT(0);
+      return 1;
     }
-    if (t2 > t) { // Must pass less than a millisecond between two calls.
-      smallest_delta = t2 - t;
-      break;
+    if (delta < smallest_delta) {
+      smallest_delta = delta;
+      if (delta <= good_enough) {
+        break;
+      }
     }
   }
-  
+
+  if (smallest_delta <= 0.) {
+    printf("Smallest delta is invalid %f\n", smallest_delta);
+    REPORT_RESULT(0);
+  }
+
   printf("Timer resolution (smallest delta): %.20f msecs\n", smallest_delta);
 
-  if (smallest_delta <= 5.f) {
+  if (smallest_delta <= good_enough) {
     printf("Timer resolution is good (or as good as spectre allows...).\n");
     result = 1;
   } else {
