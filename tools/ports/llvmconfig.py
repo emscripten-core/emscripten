0e8d0014c7c0937ba398e6dd71b396e5f53ccab9
Aleksander Guryanov <caiiiycuk@gmail.com>
2018-04-13 12:06:02 +0700

    DEBUG

--- tools/ports/llvmconfig.py
+++ tools/ports/llvmconfig.py
@@ -48,21 +48,52 @@ def process_args(ports, args, settings, shared):
         temp_dir = shared.get_emscripten_temp_dir()
         so = os.path.join(temp_dir, lib_suffix(os.path.basename(src)))
 
-        logging.info("Building %s -> %s", src, so)
+        print("@@@DEBUG")
+        print("Building %s -> %s" % (src, so))
+
+        logging.info("Building %s -> %s" % (src, so))
 
         if len(compile_flags) == 0:
             binroot = os.path.join(getroot(ports), 'build', 'bin')
+
+            # llvm-config flags
             llvmconfig = os.path.join(binroot, shared.exe_suffix('llvm-config'))
             cmd = [llvmconfig, '--cxxflags']
             flags = shared.run_process(cmd, check=True, stdout=PIPE).stdout
             compile_flags += flags.split()
             compile_flags += ['-D_GLIBCXX_USE_CXX11_ABI=0']  # https://stackoverflow.com/questions/37366291/undefined-symbol-for-self-built-llvm-opt)
+
+            # clang includes
+            # http://clang-developers.42468.n3.nabble.com/How-to-determine-clang-s-system-include-dirs-to-set-in-ASTVisitor-td4029080.html
+            foo = os.path.join(shared.get_emscripten_temp_dir(), 'foo.cpp')
+            with open(foo, 'w') as f:
+                pass
+
+            out = shared.run_process([shared.CLANG_CPP, '-###', '-c', foo], stderr=PIPE).stderr
+            print("========\/")
+            print(out)
+            print("===")
+
+            idx = 0
+            tokens = out.split()
+            for next in tokens:
+                idx += 1
+                if '-resource-dir' in next:
+                    break
+
+            assert(idx < len(tokens))
+            compile_flags += ['-I' + os.path.join(tokens[idx].replace("'", "").replace('"', ''), 'include')]
             scope[1] = compile_flags
 
         cmd = [shared.CLANG_CPP]
         cmd += compile_flags
         cmd += ['-shared', src, '-o', so]
-        shared.check_execute(cmd)
+
+        print("Run", cmd)
+        out = shared.run_process(cmd, stdout=PIPE, stderr=PIPE, check=False)
+        print("== STDOUT:\n%s\n== STDERR:\n%s\n" % (out.stdout, out.stderr))
+        assert(out.returncode == 0)
+
         return so
 
     def getport():
