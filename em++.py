be97a5152e665ed9b6fe7aa6c108d37cc347b66d
Alon Zakai <alonzakai@gmail.com>
2018-04-18 19:13:33 -0700

    fix regression in 1.37.37 from 9a5a9b6224b79eafb33517226bdb42c6acbd137f : configure scripts should be pointed to emcc, not emcc.py, since emcc will pick the right python version. (#6460)

--- em++.py
+++ em++.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# This script should work in python 2 *or* 3. It loads emcc.py, which needs python 2.
+# This script should work in python 2 *or* 3. It loads the main code using
+# python_selector, which may pick a different version.
 # It also tells emcc.py that we want C++ and not C by default
 
 import os
