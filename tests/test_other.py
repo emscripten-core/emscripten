emscripten-core/emscripten

commit c60e235279365e6147c4b65bed136a1cf4ef1ec1

Replace uses of `eval` (#6069) (#6436)
Adds some custom parsing functions into emcc.py to replace evals.

Also ensures that --js-opts and --llvm-lto are integers

Additionally replaces eval in em-config

Closes #6069
Fixes #4342

Should Fix:

    DenisKolodin/yew#124
    Part of koute/cargo-web#60
    rust-lang/rust#47909

emcc.py
@@ -906,10 +906,10 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
         else:
           value = value.replace('\\', '\\\\')
         try:
-          setattr(shared.Settings, key, eval(value))
+          setattr(shared.Settings, key, parse_value(value))
         except Exception as e:
-          logging.error('a problem occurred in evaluating content after a "-s", specifically  %s . one possible cause of this is missing quotation marks (this depends on the shell you are running in; you may need quotation marks around the entire  %s , or on an individual element)' % (change, change))
-          raise e
+          exit_with_error('a problem occured in evaluating the content after a "-s", specifically "%s": %s' % (change, str(e)))
+
         if key == 'EXPORTED_FUNCTIONS':
           # used for warnings in emscripten.py
           shared.Settings.ORIGINAL_EXPORTED_FUNCTIONS = original_exported_response or shared.Settings.EXPORTED_FUNCTIONS[:]
@@ -1963,19 +1963,19 @@ def parse_args(newargs):
       options.opt_level = validate_arg_level(options.requested_level, 3, 'Invalid optimization level: ' + newargs[i], clamp=True)
     elif newargs[i].startswith('--js-opts'):
       check_bad_eq(newargs[i])
-      options.js_opts = eval(newargs[i+1])
+      options.js_opts = int(newargs[i+1])
       if options.js_opts:
         options.force_js_opts = True
       newargs[i] = ''
       newargs[i+1] = ''
     elif newargs[i].startswith('--llvm-opts'):
       check_bad_eq(newargs[i])
-      options.llvm_opts = eval(newargs[i+1])
+      options.llvm_opts = parse_value(newargs[i+1])
       newargs[i] = ''
       newargs[i+1] = ''
     elif newargs[i].startswith('--llvm-lto'):
       check_bad_eq(newargs[i])
-      options.llvm_lto = eval(newargs[i+1])
+      options.llvm_lto = int(newargs[i+1])
       newargs[i] = ''
       newargs[i+1] = ''
     elif newargs[i].startswith('--closure'):
@@ -2789,6 +2789,60 @@ def is_valid_abspath(options, path_name):
     if in_directory(valid_abspath, path_name):
       return True
   return False
+
+
+
+def parse_value(text):
+  def parse_string_value(text):
+    first = text[0]
+    if first == "'" or first == '"':
+      assert text[-1] == text[0] and len(text) > 1, 'unclosed opened quoted string. expected final character to be "%s" and length to be greater than 1 in "%s"' % (text[0],text)
+      return text[1:-1]
+    else:
+      return text
+
+  def parse_string_list_members(text, sep):
+    values = text.split(sep)
+    result = []
+    index = 0
+    while True:
+      current = values[index].lstrip() # Cannot safely rstrip for cases like: "HERE-> ,"
+      assert len(current) > 0, "string array should not contain an empty value"
+      first = current[0]
+      if not(first == "'" or first == '"'):
+        result.append(current.rstrip())
+      else:
+        start = index
+        while True: # Continue until closing quote found
+          assert index < len(values), "unclosed quoted string. expected final character to be '%s' in '%s'" % (first, values[start])
+          new = values[index].rstrip()
+          if not len(new) == 0 and new[-1] == first:
+            if start == index:
+              result.append(current[1:-1])
+            else:
+              result.append((current + sep + new)[1:-1])
+            break
+          else:
+            current += sep + values[index]
+            index += 1
+
+      index += 1
+      if index >= len(values):
+        break
+    return result
+
+  if text[0] == '[':
+    assert text[-1] == ']', 'unclosed opened string list. expected final character to be "]" in "%s"' % (text)
+    inner = text[1:-1]
+    if inner.strip() == "":
+      return []
+    else:
+      return parse_string_list_members(inner, ",")
+  else:
+    try:
+      return int(text)
+    except ValueError as e:
+      return parse_string_value(text)
 
 
 def check_bad_eq(arg):

tests/test_other.py
@@ -111,7 +111,7 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
       # -dumpmachine
       output = run_process([PYTHON, compiler, '-dumpmachine'], stdout=PIPE, stderr=PIPE)
       self.assertContained(get_llvm_target(), output.stdout)
-      
+
       # -dumpversion
       output = run_process([PYTHON, compiler, '-dumpversion'], stdout=PIPE, stderr=PIPE)
       self.assertEqual(EMSCRIPTEN_VERSION + os.linesep, output.stdout, 'results should be identical')
@@ -4480,7 +4480,7 @@ int main()
 
   def test_strptime_symmetry(self):
     Building.emcc(path_from_root('tests','strptime_symmetry.cpp'), output_filename='a.out.js')
-    self.assertContained('TEST PASSED', run_js('a.out.js'))    
+    self.assertContained('TEST PASSED', run_js('a.out.js'))
 
   def test_truncate_from_0(self):
     open('src.cpp', 'w').write(r'''
@@ -6917,11 +6917,27 @@ int main() {
     print(check_execute([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), '-s', '-std=c++03']))
     self.assertContained('hello, world!', run_js('a.out.js'))
 
-  def test_dash_s_error(self):
-    # missing quotes
-    err = run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), "-s", "EXTRA_EXPORTED_RUNTIME_METHODS=[addOnPostRun]"], stderr=PIPE, check=False).stderr
-    self.assertContained('NameError', err) # it failed
-    self.assertContained('one possible cause of this is missing quotation marks', err) # but we suggested the fix
+  def test_dash_s_unclosed_quote(self):
+    # Unclosed quote
+    err = run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), "-s", "TEST_KEY='MISSING_QUOTE"], stderr=PIPE, check=False).stderr
+    self.assertNotContained('AssertionError', err) # Do not mention that it is an assertion error
+    self.assertContained('unclosed opened quoted string. expected final character to be "\'"', err)
+
+  def test_dash_s_single_quote(self):
+    # Only one quote
+    err = run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), "-s", "TEST_KEY='"], stderr=PIPE, check=False).stderr
+    self.assertNotContained('AssertionError', err) # Do not mention that it is an assertion error
+    self.assertContained('unclosed opened quoted string.', err)
+
+  def text_dash_s_unclosed_list(self):
+    # Unclosed list
+    err = run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), "-s", "TEST_KEY=[Value1, Value2"], stderr=PIPE, check=False).stderr
+    self.assertNotContained('AssertionError', err) # Do not mention that it is an assertion error
+    self.assertContained('unclosed opened string list. expected final character to be "]"', err)
+
+  def text_dash_s_valid_list(self):
+    err = run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), "-s", "TEST_KEY=[Value1, \"Value2\"]"], stderr=PIPE, check=False).stderr
+    self.assertNotContained('a problem occured in evaluating the content after a "-s", specifically')
 
   def test_python_2_3(self): # check emcc/em++ can be called by any python
     # remove .py from EMCC(=emcc.py)

AUTHORS
@@ -331,4 +331,5 @@ a license to everyone to use it as detailed in LICENSE.)
 * Michael Droettboom <mdroettboom@mozilla.com>
 * Nicolas Bouquet <hgy01@hieroglyphe.net>
 * Miguel Saldivar <miguel.saldivar22@hotmail.com>
+* Daniel McNab <daniel.mcnab6+emcc(at)gmail.com>
 * Tyler Limkemann <tslimkemann42 gmail.com>

em-config
@@ -21,5 +21,5 @@ if len(sys.argv) != 2 or \
   print('Usage: em-config VAR_NAME')
   exit(1)
 
-print(eval('shared.' + sys.argv[1]))
+print(getattr(shared, sys.argv[1]))
 

commit 0ca6ba2e62ae3a04e76cc68e2b262d36aa65441c

Fix -s parsing bug introduced by #6436
`-s` supports using response files with the `-s FOO=@filename` syntax,
where the contents of the file are used as the value for `FOO`. Text
files typically end with a newline, so checking the last character of
the value should strip whitespace first.

emcc.py
@@ -2832,6 +2832,7 @@ def parse_value(text):
     return result
 
   if text[0] == '[':
+    text = text.rstrip()
     assert text[-1] == ']', 'unclosed opened string list. expected final character to be "]" in "%s"' % (text)
     inner = text[1:-1]
     if inner.strip() == "":

tests/test_other.py
@@ -6917,6 +6917,11 @@ int main() {
     print(check_execute([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), '-s', '-std=c++03']))
     self.assertContained('hello, world!', run_js('a.out.js'))
 
+  def test_dash_s_response_file_list(self):
+    open('response_file', 'w').write('["_main", "_malloc"]\n')
+    response_file = os.path.join(os.getcwd(), "response_file")
+    print(check_execute([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), '-s', 'EXPORTED_FUNCTIONS=@%s' % response_file, '-std=c++03']))
+
   def test_dash_s_unclosed_quote(self):
     # Unclosed quote
     err = run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.cpp'), "-s", "TEST_KEY='MISSING_QUOTE"], stderr=PIPE, check=False).stderr