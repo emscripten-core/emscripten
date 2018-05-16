a5860fc588c5bef426f9debd2cb99a8f481b9f2d
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-05-15 19:00:43 -0700

    whitespace

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -889,7 +889,7 @@ fi
 
     def make_fake(report):
       f = open(CONFIG_FILE, 'a')
-      f.write('LLVM_ROOT = "' + path_from_root('tests', 'fake', 'bin') + '"\n') 
+      f.write('LLVM_ROOT = "' + path_from_root('tests', 'fake', 'bin') + '"\n')
       # BINARYEN_ROOT needs to exist in the config, even though this test
       # doesn't actually use it.
       f.write('BINARYEN_ROOT= "%s"\n' % path_from_root('tests', 'fake', 'bin'))
