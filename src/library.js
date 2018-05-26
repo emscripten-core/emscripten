b0c287e00fd65b4e2c73aca00b5baec631c8c4f6
Alon Zakai <alonzakai@gmail.com>
2018-05-26 16:35:19 -0700

    Fix asm.js validation that broke with acac8d5 (#6588)
    Fixes cttz asm.js validation that broke with acac8d5fb18e256e4e583e9368b56d781f0bee1d

--- src/library.js
+++ src/library.js
@@ -1071,9 +1071,9 @@ LibraryManager.library = {
   llvm_cttz_i32__asm: true,
 #endif
   llvm_cttz_i32__sig: 'ii',
-  llvm_cttz_i32: function(x) { // Currently doesn't take isZeroUndef()
+  llvm_cttz_i32: function(x) { // Note: Currently doesn't take isZeroUndef()
     x = x | 0;
-    return x ? (31 - (Math_clz32((x ^ (x - 1))) | 0) | 0) : 32;
+    return (x ? (31 - (Math_clz32((x ^ (x - 1))) | 0) | 0) : 32) | 0;
   },
 
   llvm_cttz_i64__deps: ['llvm_cttz_i32'],
