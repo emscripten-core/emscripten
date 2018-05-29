fb13408084df0d0f5d7138faa50ee9b4e8d1ccaa
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-05-29 13:01:04 -0700

    clean up exit() by moving node-specific code to the node section of shell.js

--- src/shell.js
+++ src/shell.js
@@ -157,6 +157,10 @@ if (ENVIRONMENT_IS_NODE) {
     process['exit'](1);
   });
 
+  Module['quit'] = function(status) {
+    process['exit'](status);
+  };
+
   Module['inspect'] = function () { return '[Emscripten Module object]'; };
 } else
 #endif // ENVIRONMENT_MAY_BE_NODE
