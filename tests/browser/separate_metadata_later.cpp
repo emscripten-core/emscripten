2b13e1b503b13c4671f91b7446ff3084996dc2b9
Alon Zakai <alonzakai@gmail.com>
2018-06-14 15:57:24 -0700

    improve test

--- tests/browser/separate_metadata_later.cpp
+++ tests/browser/separate_metadata_later.cpp
@@ -7,17 +7,21 @@ EMSCRIPTEN_KEEPALIVE extern "C" void finish() {
 int main() {
   EM_ASM({
     setTimeout(function() {
+      // hijack run dep logic to see when the metadata is loaded ok.
+      var real = Module["removeRunDependency"];
+      Module["removeRunDependency"] = function(id) {
+        real(id);
+        if (id === "more.js.metadata") {
+          Module["_finish"]();
+        }
+      };
       function loadChildScript(name, then) {
         var js = document.createElement("script");
         if (then) js.onload = then;
         js.src = name;
         document.body.appendChild(js);
       }
-      loadChildScript("more.js", function() {
-        setTimeout(function() {
-          Module['_finish']();
-        }, 1000);
-      });
+      loadChildScript("more.js");
     }, 1);
   });
 }
