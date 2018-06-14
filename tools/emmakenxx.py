52e3caa4d426bc5e55f7569790370d8b9c1dc3a0
Sam Clegg <sbc@chromium.org>
2018-06-14 14:19:31 -0300

    Improve flake8 coverage (#6688)

--- tools/emmakenxx.py
+++ tools/emmakenxx.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python2
-
-'''
-see emmaken.py
-'''
-
-import os, subprocess, sys
+"""See emmaken.py
+"""
+import os
+import subprocess
+import sys
 
 __rootpath__ = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-def path_from_root(*pathelems):
-  return os.path.join(__rootpath__, *pathelems)
-sys.path += [path_from_root('')]
-from tools.shared import *
+sys.path.append(__rootpath__)
 
-emmaken = path_from_root('tools', 'emmaken.py')
+from tools import shared  # noqa
+
+emmaken = shared.path_from_root('tools', 'emmaken.py')
 os.environ['EMMAKEN_CXX'] = '1'
-exit(subprocess.call([PYTHON, emmaken] + sys.argv[1:]))
-
+sys.exit(subprocess.call([shared.PYTHON, emmaken] + sys.argv[1:]))
