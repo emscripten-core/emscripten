d18cc6828fa8cd7171dd2326ac348450b5299abc
Jason <yhorg@hotmail.com>
2018-06-14 09:32:05 +0800

    Fix precision lost when using 'constant' with Embind.

--- system/include/emscripten/bind.h
+++ system/include/emscripten/bind.h
@@ -222,7 +222,7 @@ namespace emscripten {
             void _embind_register_constant(
                 const char* name,
                 TYPEID constantType,
-                uintptr_t value);
+                double value);
         }
     }
 }
@@ -1578,8 +1578,8 @@ namespace emscripten {
 
     namespace internal {
         template<typename T>
-        uintptr_t asGenericValue(T t) {
-            return static_cast<uintptr_t>(t);
+        double asGenericValue(T t) {
+            return static_cast<double>(t);
         }
 
         template<typename T>
@@ -1595,7 +1595,7 @@ namespace emscripten {
         _embind_register_constant(
             name,
             TypeID<const ConstantType&>::get(),
-            asGenericValue(BT::toWireType(v)));
+            static_cast<double>(asGenericValue(BT::toWireType(v))));
     }
 }
 
