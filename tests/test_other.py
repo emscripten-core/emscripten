9a8452053361084b28779c1cbf4284d1fb2ca746
Alon Zakai <alonzakai@gmail.com>
2018-07-10 15:02:30 -0700

    Emterpreter-async state fix (#6822)
    Ok, this should be in good shape now. Here's a full explanation of what this PR does:
    
    *    Allow running code while sleeping: We had some arbitrary restrictions on this which we started to improve in #6749, and this PR should finish that up. After this PR, it is ok to run emterpreted or non-emterpreted code during a sleep, it is up to the user to do that only when it is valid.
    *    Clarify the emterpreter-async states: 0 is normal execution. 1 is set when we start to save the stack and continues as we sleep. We don't have a way to know when the stack has finished unwinding, since we don't have the machinery to do that. But if we start to run a function in state 1, that means we are running code during the sleep (as we cannot reach a function entry while saving the stack - when saving we are just exiting, going up the stack), so we switch to mode 3, which means definitely asleep (i.e., after saving the stack). The distinction is important, because if we see state 1 in the middle of a function - not the entry - then something is wrong (for example, a non-emterpreted function on the stack while saving). Also, if we see mode 1 on entry to an emterpreted function, it's important that we know saving the stack has finished so that we don't assume it is still ongoing (if it were, we should exit as part of that). Finally, mode 2 is resuming execution, and is set from JS at the proper time.
    *    Add an assertion in EM_ASMs: we cannot start an async operation in the middle of them, as the JS is not emterpreted, so we can't jump into the middle to resume. That is, we'd end up running the entire EM_ASM from the start.
    *    Add a bunch of tests and improve existing ones.

--- tests/test_other.py
+++ tests/test_other.py
@@ -5536,6 +5536,55 @@ function _main() {
       run_process([PYTHON, EMCC, 'src.c', '-s', 'EMTERPRETIFY=1', '-s', 'EMTERPRETIFY_ASYNC=1', '-s', 'EMTERPRETIFY_WHITELIST=["_fleefl"]', '-s', 'PRECISE_F32=1'])
       self.assertContained('result: ' + out, run_js('a.out.js'))
 
+  def test_call_nonemterpreted_during_sleep(self):
+    open('src.c', 'w').write(r'''
+#include <stdio.h>
+#include <emscripten.h>
+
+EMSCRIPTEN_KEEPALIVE void emterpreted_yielder() {
+  int counter = 0;
+  while (1) {
+    printf("emterpreted_yielder() sleeping...\n");
+    emscripten_sleep_with_yield(10);
+    counter++;
+    if (counter == 3) {
+      printf("Success\n");
+      break;
+    }
+  }
+}
+
+EMSCRIPTEN_KEEPALIVE void not_emterpreted() {
+  printf("Entering not_emterpreted()\n");
+}
+
+int main() {
+  EM_ASM({
+    setTimeout(function () {
+      console.log("calling not_emterpreted()");
+      Module["_not_emterpreted"]();
+    }, 0);
+    console.log("calling emterpreted_yielder()");
+#ifdef BAD_EM_ASM
+    Module['_emterpreted_yielder']();
+#endif
+  });
+#ifndef BAD_EM_ASM
+  emterpreted_yielder();
+#endif
+}
+    ''')
+    run_process([PYTHON, EMCC, 'src.c', '-s', 'EMTERPRETIFY=1', '-s', 'EMTERPRETIFY_ASYNC=1', '-s', 'EMTERPRETIFY_BLACKLIST=["_not_emterpreted"]'])
+    self.assertContained('Success', run_js('a.out.js'))
+
+    print('check calling of emterpreted as well')
+    run_process([PYTHON, EMCC, 'src.c', '-s', 'EMTERPRETIFY=1', '-s', 'EMTERPRETIFY_ASYNC=1'])
+    self.assertContained('Success', run_js('a.out.js'))
+
+    print('check for invalid EM_ASM usage')
+    run_process([PYTHON, EMCC, 'src.c', '-s', 'EMTERPRETIFY=1', '-s', 'EMTERPRETIFY_ASYNC=1', '-s', 'EMTERPRETIFY_BLACKLIST=["_not_emterpreted"]', '-DBAD_EM_ASM'])
+    self.assertContained('cannot have an EM_ASM on the stack when emterpreter pauses/resumes', run_js('a.out.js', stderr=STDOUT, assert_returncode=None))
+
   def test_link_with_a_static(self):
     for args in [[], ['-O2']]:
       print(args)
