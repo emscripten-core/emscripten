926724ea5526feb5f40608afa371124b06826bb7
Siim Kallas <siimkallas@gmail.com>
2018-07-22 15:59:20 +0300

    Avoid division by zero when using relative OpenAL sources.

--- src/library_openal.js
+++ src/library_openal.js
@@ -499,6 +499,16 @@ var LibraryOpenAL = {
       }
     },
 
+    inverseMagnitude: function(x, y, z) {
+      var length = Math.sqrt(x * x + y * y + z * z);
+
+      if (length < Number.EPSILON) {
+        return 0.0;
+      }
+
+      return 1.0 / length;
+    },
+
     updateSourceSpace: function(src) {
       if (!src.panner) {
         return;
@@ -540,13 +550,13 @@ var LibraryOpenAL = {
         var lUpZ = listener.up[2];
 
         // Normalize the Back vector
-        var invMag = 1.0 / Math.sqrt(lBackX * lBackX + lBackY * lBackY + lBackZ * lBackZ);
+        var invMag = AL.inverseMagnitude(lBackX, lBackY, lBackZ);
         lBackX *= invMag;
         lBackY *= invMag;
         lBackZ *= invMag;
 
         // ...and the Up vector
-        var invMag = 1.0 / Math.sqrt(lUpX * lUpX + lUpY * lUpY + lUpZ * lUpZ);
+        var invMag = AL.inverseMagnitude(lUpX, lUpY, lUpZ);
         lUpX *= invMag;
         lUpY *= invMag;
         lUpZ *= invMag;
@@ -557,7 +567,7 @@ var LibraryOpenAL = {
         var lRightZ = (lUpX * lBackY - lUpY * lBackX);
 
         // Back and Up might not be exactly perpendicular, so the cross product also needs normalization
-        var invMag = 1.0 / Math.sqrt(lRightX * lRightX + lRightY * lRightY + lRightZ * lRightZ);
+        var invMag = AL.inverseMagnitude(lRightX, lRightY, lRightZ);
         lRightX *= invMag;
         lRightY *= invMag;
         lRightZ *= invMag;
