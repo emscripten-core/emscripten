2b8db0ba66207f2a30a1bbd7b0b6ee4422323169
Alon Zakai <alonzakai@gmail.com>
2018-07-30 21:14:51 -0400

    remove assertion on EMSCRIPTEN_ROOT - on windows we have seen failures due to C:\PROGRA~1 being the same as C:\Program Files (despite the strings differing), and there is no simple way to check that. On other platforms links may cause similar issues, so it's not worth it

--- tools/shared.py
+++ tools/shared.py
@@ -273,7 +273,6 @@ else:
     sys.exit(0)
 
 # The following globals can be overridden by the config file.
-EMSCRIPTEN_ROOT = __rootpath__
 NODE_JS = None
 BINARYEN_ROOT = None
 EM_POPEN_WORKAROUND = None
@@ -295,13 +294,6 @@ try:
 except Exception as e:
   logging.error('Error in evaluating %s (at %s): %s, text: %s' % (EM_CONFIG, CONFIG_FILE, str(e), config_text))
   sys.exit(1)
-
-# EMSCRIPTEN_ROOT is set in the config file so that external tools such as
-# scons can find emscripten by looking at the config.  Although its not used
-# within emscripten itself this is good to time sanity check the value.
-EMSCRIPTEN_ROOT = os.path.expanduser(os.path.normpath(EMSCRIPTEN_ROOT))
-if EMSCRIPTEN_ROOT != __rootpath__:
-  exit_with_error('Incorrect EMSCRIPTEN_ROOT in config file: %s (Expected %s)', EMSCRIPTEN_ROOT, __rootpath__)
 
 
 # Returns a suggestion where current .emscripten config file might be located
