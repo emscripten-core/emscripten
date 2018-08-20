7d2bae05b46a4b1d3953e3ceb6ca053dd1ad149a
Alon Zakai <alonzakai@gmail.com>
2018-08-20 09:08:20 -0700

    improve other.test_bad_triple

--- tests/test_other.py
+++ tests/test_other.py
@@ -3588,7 +3588,7 @@ int main()
     assert p.returncode == 0, 'LLVM tests must pass with exit code 0'
 
   def test_bad_triple(self):
-    Popen([CLANG, path_from_root('tests', 'hello_world.c'), '-c', '-emit-llvm', '-o', 'a.bc'] + get_clang_native_args(), env=get_clang_native_env(), stdout=PIPE, stderr=PIPE).communicate()
+    run_process([CLANG, path_from_root('tests', 'hello_world.cpp'), '-c', '-emit-llvm', '-o', 'a.bc'] + get_clang_native_args(), env=get_clang_native_env())
     err = run_process([PYTHON, EMCC, 'a.bc'], stdout=PIPE, stderr=PIPE).stderr
     if self.is_wasm_backend():
       assert 'machine type must be wasm32' in err, err
