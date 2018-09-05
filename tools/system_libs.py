618961638fe479b72264c24bff1958f3d092d925
t.schander <t.schander@enscape3d.com>
2018-09-05 17:58:25 +0200

    Corrected tabs vs. white spaces

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -293,9 +293,9 @@ def calculate(temp_files, in_temp, stdout_, stderr_, forced=[]):
       'variant.cpp',
       'vector.cpp',
       os.path.join('experimental', 'memory_resource.cpp'),
-	  os.path.join('experimental', 'filesystem', 'directory_iterator.cpp'),
-	  os.path.join('experimental', 'filesystem', 'path.cpp'),
-	  os.path.join('experimental', 'filesystem', 'operations.cpp')
+      os.path.join('experimental', 'filesystem', 'directory_iterator.cpp'),
+      os.path.join('experimental', 'filesystem', 'path.cpp'),
+      os.path.join('experimental', 'filesystem', 'operations.cpp')
     ]
     libcxxabi_include = shared.path_from_root('system', 'lib', 'libcxxabi', 'include')
     return build_libcxx(
