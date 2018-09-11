6a694ea1f69b06c0870e58b441f59df55708f37a
Vladimír Vondruš <mosra@centrum.cz>
2018-09-11 11:49:26 +0200

    Fix function dependecy handling in library_gl.js.
    This is how code generated for WebGL 2 glDrawRangeElements() looked
    before this fix:
    
        function _glDrawElements(mode, count, type, indices) {
            GLctx.drawElements(mode, count, type, indices)
        }
        function _glDrawElementsInstanced(mode, count, type, indices, primcount)
        {
            GLctx["drawElementsInstanced"](mode, count, type, indices, primcount)
        }
        function _emscripten_glDrawElements(mode, count, type, indices) {
            GLctx.drawElements(mode, count, type, indices)
        }
        function _glDrawRangeElements(_emscripten_glDrawRangeElementsmode, start, end, count, type, indices) {
            _emscripten_glDrawElements(mode, count, type, indices)
        }
    
    (Note the _emscripten_glDrawRangeElementsmode, which should be just
    mode).

--- src/library_gl.js
+++ src/library_gl.js
@@ -7971,7 +7971,8 @@ keys(LibraryGL).forEach(function(x) {
       var orig = dep;
       dep = 'emscripten_' + dep;
       var fixed = LibraryGL[x].toString().replace(new RegExp('_' + orig + '\\(', 'g'), '_' + dep + '(');
-      fixed = fixed.substr(0, 9) + '_' + y + fixed.substr(9);
+      // `function` is 8 characters, add space and an explicit name after
+      fixed = fixed.substr(0, 8) + ' _' + y + fixed.substr(8);
       LibraryGL[x] = eval('(function() { return ' + fixed + ' })()');
     }
     return dep;
