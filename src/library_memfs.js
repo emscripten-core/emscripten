841071ab7591bea7ebda3a68791d53596b1b514d
Alon Zakai <alonzakai@gmail.com>
2018-09-17 17:12:44 -0700

    warn too

--- src/library_memfs.js
+++ src/library_memfs.js
@@ -284,12 +284,18 @@ mergeInto(LibraryManager.library, {
         // If memory can grow, we don't want to hold on to references of
         // the memory Buffer, as they may get invalidated. That means
         // we need to do a copy here.
+#if ASSERTIONS
         // FIXME: this is inefficient as the file packager may have
         //        copied the data into memory already - we may want to
         //        integrate more there and let the file packager loading
         //        code be able to query if memory growth is on or off.
+        if (canOwn) {
+          warnOnce('file packager has copied file data into memory, but in memory growth we are forced to copy it again (see --no-heap-copy)');
+        }
+#endif // ASSERTIONS
         canOwn = false;
-#endif
+#endif // ALLOW_MEMORY_GROWTH
+
         if (!length) return 0;
         var node = stream.node;
         node.timestamp = Date.now();
