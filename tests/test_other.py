360faea0e832973e6e5d49f2bbb10cea0601a244
Alon Zakai <azakai@google.com>
2018-10-17 16:21:34 -0700

    update test

--- tests/test_other.py
+++ tests/test_other.py
@@ -8228,7 +8228,7 @@ int main() {
                  0, [],                         ['tempDoublePtr', 'waka'],     8,   0,    0), # noqa; totally empty!
       # but we don't metadce with linkable code! other modules may want it
       (['-O3', '-s', 'MAIN_MODULE=1'],
-              1529, ['invoke_i'],               ['waka'],                 469663, 149, 1443),
+              1538, ['invoke_i'],               ['waka'],                 469663, 146, 1443),
     ]) # noqa
 
     print('test on a minimal pure computational thing')
