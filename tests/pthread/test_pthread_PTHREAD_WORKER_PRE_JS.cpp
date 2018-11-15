f02033d2dbda5c35ac11ad9a3a165fe4a8673ec9
Alon Zakai <azakai@google.com>
2018-11-15 14:39:12 -0800

    spacing

--- tests/pthread/test_pthread_PTHREAD_WORKER_PRE_JS.cpp
+++ tests/pthread/test_pthread_PTHREAD_WORKER_PRE_JS.cpp
@@ -24,57 +24,57 @@ void *ThreadMain(void *arg)
   atomicSum += EM_ASM_INT({
     return Module["something"];
   });
-	pthread_exit(0);
+  pthread_exit(0);
 }
 
 void RunThreads()
-{	
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	pthread_attr_setstacksize(&attr, 4*1024);
+{  
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+  pthread_attr_setstacksize(&attr, 4*1024);
 
-	printf("Main thread has thread ID %d\n", (int)pthread_self());
-	assert(pthread_self() != 0);
+  printf("Main thread has thread ID %d\n", (int)pthread_self());
+  assert(pthread_self() != 0);
 
   pthread_t thread[NUM_THREADS];
 
-	for(int i = 0; i < NUM_THREADS; ++i)
-	{
-		int rc = pthread_create(&thread[i], &attr, ThreadMain, NULL);
-		assert(rc == 0);
-	}
+  for(int i = 0; i < NUM_THREADS; ++i)
+  {
+    int rc = pthread_create(&thread[i], &attr, ThreadMain, NULL);
+    assert(rc == 0);
+  }
 
-	pthread_attr_destroy(&attr);
+  pthread_attr_destroy(&attr);
 
-	for(int i = 0; i < NUM_THREADS; ++i)
-	{
-		int status = 1;
-		int rc = pthread_join(thread[i], (void**)&status);
-		assert(rc == 0);
-		assert(status == 0);
-	}
+  for(int i = 0; i < NUM_THREADS; ++i)
+  {
+    int status = 1;
+    int rc = pthread_join(thread[i], (void**)&status);
+    assert(rc == 0);
+    assert(status == 0);
+  }
 }
 
 int main()
 {
   atomicSum.store(0);
 
-	if (!emscripten_has_threading_support())
-	{
+  if (!emscripten_has_threading_support())
+  {
 #ifdef REPORT_RESULT
-		REPORT_RESULT(0);
+    REPORT_RESULT(0);
 #endif
-		printf("Skipped: Threading is not supported.\n");
-		return 0;
-	}
+    printf("Skipped: Threading is not supported.\n");
+    return 0;
+  }
 
   RunThreads();
 
   printf("%d threads, sum: %d\n", NUM_THREADS, atomicSum.load());
 
 #ifdef REPORT_RESULT
-	int result = (atomicSum == NUM_THREADS);
-	REPORT_RESULT(result);
+  int result = (atomicSum == NUM_THREADS);
+  REPORT_RESULT(result);
 #endif
 }
