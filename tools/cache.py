emscripten-core/emscripten

commit 95ca1a5b06c9d28de693f19b4695cbd2e625a184

Clear the cache from orbit (#7522)
Don't try to clear just a subdir, as it's not worth the risk (and was broken on the wasm object files subdir, which is picked based on a -s option which emcc --clear-cache didn't see).

tools/cache.py
@@ -22,10 +22,12 @@ class Cache(object):
   EM_EXCLUSIVE_CACHE_ACCESS = int(os.environ.get('EM_EXCLUSIVE_CACHE_ACCESS') or 0)
 
   def __init__(self, dirname=None, debug=False, use_subdir=True):
+    # figure out the root directory for all caching
     if dirname is None:
       dirname = os.environ.get('EM_CACHE')
     if not dirname:
       dirname = os.path.expanduser(os.path.join('~', '.emscripten_cache'))
+    self.root_dirname = dirname
 
     def try_remove_ending(thestring, ending):
       if thestring.endswith(ending):
@@ -35,6 +37,7 @@ class Cache(object):
     self.filelock_name = try_remove_ending(try_remove_ending(dirname, '/'), '\\') + '.lock'
     self.filelock = filelock.FileLock(self.filelock_name)
 
+    # if relevant, use a subdir of the cache
     if use_subdir:
       if not shared.Settings.WASM_BACKEND:
         dirname = os.path.join(dirname, 'asmjs')
@@ -79,7 +82,7 @@ class Cache(object):
       self.release_cache_lock()
 
   def erase(self):
-    tempfiles.try_delete(self.dirname)
+    tempfiles.try_delete(self.root_dirname)
     try:
       open(self.dirname + '__last_clear', 'w').write('last clear: ' + time.asctime() + '\n')
     except Exception as e:

tests/test_sanity.py
@@ -493,9 +493,11 @@ fi
     # Manual cache clearing
     ensure_cache()
     self.assertTrue(os.path.exists(EMCC_CACHE))
+    self.assertTrue(os.path.exists(Cache.root_dirname))
     output = self.do([PYTHON, EMCC, '--clear-cache'])
     self.assertIn(ERASING_MESSAGE, output)
     self.assertFalse(os.path.exists(EMCC_CACHE))
+    self.assertFalse(os.path.exists(Cache.root_dirname))
     self.assertIn(SANITY_MESSAGE, output)
 
     # Changing LLVM_ROOT, even without altering .emscripten, clears the cache

commit 38997fb38a0c406f2277d6e12b34c54819543b1a

remove cache __last_clear indicator file - it broke in #7522, and while it's easy to fix, it seems of zero utility at this point

tools/cache.py
@@ -83,10 +83,6 @@ class Cache(object):
 
   def erase(self):
     tempfiles.try_delete(self.root_dirname)
-    try:
-      open(self.dirname + '__last_clear', 'w').write('last clear: ' + time.asctime() + '\n')
-    except Exception as e:
-      print('failed to save last clear time: ', e, file=sys.stderr)
     self.filelock = None
     tempfiles.try_delete(self.filelock_name)
     self.filelock = filelock.FileLock(self.filelock_name)