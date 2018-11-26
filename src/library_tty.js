emscripten-core/emscripten

commit 82ea68073767449a278f85b065902afb8e630fa1

handle musl fflush properly ; fixes #7360 (#7376)
This interprets the writev syscall of a NULL buffer as a flush, which is what musl does in practice

src/library_tty.js
@@ -78,12 +78,19 @@ mergeInto(LibraryManager.library, {
         if (!stream.tty || !stream.tty.ops.put_char) {
           throw new FS.ErrnoError(ERRNO_CODES.ENXIO);
         }
-        for (var i = 0; i < length; i++) {
-          try {
-            stream.tty.ops.put_char(stream.tty, buffer[offset+i]);
-          } catch (e) {
-            throw new FS.ErrnoError(ERRNO_CODES.EIO);
+        var i = 0;
+        try {
+          if (offset === 0 && length === 0) {
+            // musl implements an fflush using a write of a NULL buffer of size 0
+            stream.tty.ops.flush(stream.tty);
+          } else {
+            while (i < length) {
+              stream.tty.ops.put_char(stream.tty, buffer[offset+i]);
+              i++;
+            }
           }
+        } catch (e) {
+          throw new FS.ErrnoError(ERRNO_CODES.EIO);
         }
         if (length) {
           stream.node.timestamp = Date.now();

tests/other/fflush_fs/test.cpp
@@ -1,0 +1,9 @@
+#include <emscripten.h>
+#include <stdio.h>
+int main()
+{
+  printf("Hey1\nHey2");
+  fflush(stdout);
+  return 0;
+}
+

tests/other/fflush_fs/test.out
@@ -1,0 +1,3 @@
+Hey1
+Hey2
+

tests/test_other.py
@@ -90,7 +90,7 @@ class other(RunnerCore):
     shutil.copyfile(path_from_root('tests', dirname, 'test.cpp'), 'test.cpp')
     run_process([PYTHON, EMCC, 'test.cpp'] + emcc_args)
     expected = open(path_from_root('tests', dirname, 'test.out')).read()
-    seen = run_js('a.out.js', args=run_args) + '\n'
+    seen = run_js('a.out.js', args=run_args, stderr=PIPE, full_output=True) + '\n'
     self.assertContained(expected, seen)
 
   def test_emcc_v(self):
@@ -8647,6 +8647,14 @@ end
 
   def test_fd_closed(self):
     self.do_other_test(os.path.join('other', 'fd_closed'))
+
+  def test_fflush(self):
+    # fflush without the full filesystem won't quite work
+    self.do_other_test(os.path.join('other', 'fflush'))
+
+  def test_fflush_fs(self):
+    # fflush with the full filesystem will
+    self.do_other_test(os.path.join('other', 'fflush_fs'), emcc_args=['-s', 'FORCE_FILESYSTEM=1'])
 
   @no_wasm_backend('tests js optimizer')
   def test_js_optimizer_parse_error(self):

tests/other/fflush/test.cpp
@@ -1,0 +1,9 @@
+#include <emscripten.h>
+#include <stdio.h>
+int main()
+{
+  printf("Hey1\nHey2");
+  fflush(stdout);
+  return 0;
+}
+

tests/other/fflush/test.out
@@ -1,0 +1,5 @@
+Hey1
+
+stdio streams had content in them that was not flushed. you should set EXIT_RUNTIME to 1 (see the FAQ), or make sure to emit a newline when you printf etc.
+(this may also be due to not including full filesystem support - try building with -s FORCE_FILESYSTEM=1)
+

src/postamble.js
@@ -351,6 +351,9 @@ function checkUnflushedContent() {
   err = printErr;
   if (has) {
     warnOnce('stdio streams had content in them that was not flushed. you should set EXIT_RUNTIME to 1 (see the FAQ), or make sure to emit a newline when you printf etc.');
+#if FILESYSTEM == 0
+    warnOnce('(this may also be due to not including full filesystem support - try building with -s FORCE_FILESYSTEM=1)');
+#endif
   }
 }
 #endif // EXIT_RUNTIME

commit 726f962783ace5b455e7db7837a7c6b99e5b1e9f

Fix stream flushing behavior, which regressed in #7376. (#7551)
musl flushes just indicate pushing data from libc through syscalls, but we have additional buffering at the JS level since console.log etc. can only emit a full line at each time. So fflush() cannot ensure a partial line is actually printed. However, during exit of the program we do flush with a newline, as no more code will be run anyhow, and that makes us match native behavior.

fixes #7464

src/library_tty.js
@@ -78,16 +78,9 @@ mergeInto(LibraryManager.library, {
         if (!stream.tty || !stream.tty.ops.put_char) {
           throw new FS.ErrnoError(ERRNO_CODES.ENXIO);
         }
-        var i = 0;
         try {
-          if (offset === 0 && length === 0) {
-            // musl implements an fflush using a write of a NULL buffer of size 0
-            stream.tty.ops.flush(stream.tty);
-          } else {
-            while (i < length) {
-              stream.tty.ops.put_char(stream.tty, buffer[offset+i]);
-              i++;
-            }
+          for (var i = 0; i < length; i++) {
+            stream.tty.ops.put_char(stream.tty, buffer[offset+i]);
           }
         } catch (e) {
           throw new FS.ErrnoError(ERRNO_CODES.EIO);

tests/other/fflush_fs/test.cpp
@@ -3,7 +3,7 @@
 int main()
 {
   printf("Hey1\nHey2");
-  fflush(stdout);
+  fflush(stdout); // flushes libc, but does not send out a newline for our JS emitting code, so Hey2 is not printed
   return 0;
 }
 

tests/other/fflush_fs/test.out
@@ -1,3 +1,4 @@
 Hey1
-Hey2
+
+stdio streams had content in them that was not flushed. you should set EXIT_RUNTIME to 1 (see the FAQ), or make sure to emit a newline when you printf etc.
 

tests/other/fflush_fs_exit/test.out
@@ -1,0 +1,3 @@
+Hey1
+Hey2
+

tests/test_other.py
@@ -8360,8 +8360,12 @@ end
     self.do_other_test(os.path.join('other', 'fflush'))
 
   def test_fflush_fs(self):
-    # fflush with the full filesystem will
+    # fflush with the full filesystem will flush from libc, but not the JS logging, which awaits a newline
     self.do_other_test(os.path.join('other', 'fflush_fs'), emcc_args=['-s', 'FORCE_FILESYSTEM=1'])
+
+  def test_fflush_fs_exit(self):
+    # on exit, we can send out a newline as no more code will run
+    self.do_other_test(os.path.join('other', 'fflush_fs_exit'), emcc_args=['-s', 'FORCE_FILESYSTEM=1', '-s', 'EXIT_RUNTIME=1'])
 
   @no_wasm_backend('tests js optimizer')
   def test_js_optimizer_parse_error(self):