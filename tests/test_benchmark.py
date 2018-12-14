8c7780026f4a9a94346598abee7db72483d00d89
Thomas Lively <tlively@google.com>
2018-12-14 10:25:53 -0800

    Pass '--mvp-features' to wasm-opt (temp solution)

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -152,7 +152,7 @@ class NativeBenchmarker(Benchmarker):
 
 def run_binaryen_opts(filename, opts):
   run_process([
-    os.path.join(Building.get_binaryen_bin(), 'wasm-opt'),
+    os.path.join(Building.get_binaryen_bin(), 'wasm-opt', '--mvp-features'),
     filename,
     '-o', filename
   ] + opts)
