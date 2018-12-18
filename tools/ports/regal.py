9f48a83f4d8910165c42e72cb4f62c8e128f5b39
gab <contact@gabrielcuvillier.pro>
2018-12-18 22:03:35 +0100

    Regal port: disabled GLSL optimizer

--- tools/ports/regal.py
+++ tools/ports/regal.py
@@ -15,7 +15,8 @@ def get(ports, settings, shared):
             ports.clear_project_build('regal')
 
             # copy sources
-            # only what is needed is copied: regal, md5, jsonsl, boost, lookup3, glslopt
+            # only what is needed is copied: regal, md5, jsonsl, boost, lookup3
+            # Note: GLSL Optimizer is not included in the build
             source_path_src = os.path.join(ports.get_dir(), 'regal', 'regal-' + TAG, 'src')
             dest_path_src = os.path.join(ports.get_build_dir(), 'regal', 'src')
 
@@ -24,13 +25,11 @@ def get(ports, settings, shared):
             source_path_jsonsl = os.path.join(source_path_src, 'jsonsl')
             source_path_boost = os.path.join(source_path_src, 'boost')
             source_path_lookup3 = os.path.join(source_path_src, 'lookup3')
-            source_path_glslopt = os.path.join(source_path_src, 'glsl')
             dest_path_regal = os.path.join(dest_path_src, 'regal')
             dest_path_md5 = os.path.join(dest_path_src, 'md5')
             dest_path_jsonsl = os.path.join(dest_path_src, 'jsonsl')
             dest_path_boost = os.path.join(dest_path_src, 'boost')
             dest_path_lookup3 = os.path.join(dest_path_src, 'lookup3')
-            dest_path_glslopt = os.path.join(dest_path_src, 'glsl')
 
             shutil.rmtree(dest_path_src, ignore_errors=True)
             shutil.copytree(source_path_regal, dest_path_regal)
@@ -38,7 +37,6 @@ def get(ports, settings, shared):
             shutil.copytree(source_path_jsonsl, dest_path_jsonsl)
             shutil.copytree(source_path_boost, dest_path_boost)
             shutil.copytree(source_path_lookup3, dest_path_lookup3)
-            shutil.copytree(source_path_glslopt, dest_path_glslopt)
 
             # includes
             source_path_include = os.path.join(ports.get_dir(), 'regal', 'regal-' + TAG, 'include')
@@ -46,7 +44,6 @@ def get(ports, settings, shared):
             shutil.copytree(source_path_include, dest_path_include)
 
             # build
-            # there is two separate libraries, with different compilation options: regal and glslopt
             srcs_regal = ['regal/RegalShaderInstance.cpp',
                           'regal/RegalIff.cpp',
                           'regal/RegalQuads.cpp',
@@ -104,113 +101,9 @@ def get(ports, settings, shared):
                           'md5/src/md5.c',
                           'jsonsl/jsonsl.c']
 
-            srcs_glslopt = [ 'glsl/src/glsl/ast_array_index.cpp',
-                             'glsl/src/glsl/ast_expr.cpp',
-                             'glsl/src/glsl/ast_function.cpp',
-                             'glsl/src/glsl/ast_to_hir.cpp',
-                             'glsl/src/glsl/ast_type.cpp',
-                             'glsl/src/glsl/builtin_function.cpp',
-                             'glsl/src/glsl/builtin_variables.cpp',
-                             'glsl/src/glsl/glsl_lexer.cpp',
-                             'glsl/src/glsl/glsl_optimizer.cpp',
-                             'glsl/src/glsl/glsl_parser.cpp',
-                             'glsl/src/glsl/glsl_parser_extras.cpp',
-                             'glsl/src/glsl/glsl_symbol_table.cpp',
-                             'glsl/src/glsl/glsl_types.cpp',
-                             'glsl/src/glsl/hir_field_selection.cpp',
-                             'glsl/src/glsl/ir.cpp',
-                             'glsl/src/glsl/ir_basic_block.cpp',
-                             'glsl/src/glsl/ir_builder.cpp',
-                             'glsl/src/glsl/ir_clone.cpp',
-                             'glsl/src/glsl/ir_constant_expression.cpp',
-                             'glsl/src/glsl/ir_expression_flattening.cpp',
-                             'glsl/src/glsl/ir_function.cpp',
-                             'glsl/src/glsl/ir_function_can_inline.cpp',
-                             'glsl/src/glsl/ir_function_detect_recursion.cpp',
-                             'glsl/src/glsl/ir_hierarchical_visitor.cpp',
-                             'glsl/src/glsl/ir_hv_accept.cpp',
-                             'glsl/src/glsl/ir_import_prototypes.cpp',
-                             'glsl/src/glsl/ir_print_glsl_visitor.cpp',
-                             'glsl/src/glsl/ir_print_visitor.cpp',
-                             'glsl/src/glsl/ir_reader.cpp',
-                             'glsl/src/glsl/ir_rvalue_visitor.cpp',
-                             'glsl/src/glsl/ir_unused_structs.cpp',
-                             'glsl/src/glsl/ir_validate.cpp',
-                             'glsl/src/glsl/ir_variable_refcount.cpp',
-                             'glsl/src/glsl/link_functions.cpp',
-                             'glsl/src/glsl/link_uniform_block_active_visitor.cpp',
-                             'glsl/src/glsl/link_uniform_blocks.cpp',
-                             'glsl/src/glsl/link_uniform_initializers.cpp',
-                             'glsl/src/glsl/link_uniforms.cpp',
-                             'glsl/src/glsl/link_varyings.cpp',
-                             'glsl/src/glsl/linker.cpp',
-                             'glsl/src/glsl/loop_analysis.cpp',
-                             'glsl/src/glsl/loop_controls.cpp',
-                             'glsl/src/glsl/loop_unroll.cpp',
-                             'glsl/src/glsl/lower_clip_distance.cpp',
-                             'glsl/src/glsl/lower_discard.cpp',
-                             'glsl/src/glsl/lower_discard_flow.cpp',
-                             'glsl/src/glsl/lower_if_to_cond_assign.cpp',
-                             'glsl/src/glsl/lower_instructions.cpp',
-                             'glsl/src/glsl/lower_jumps.cpp',
-                             'glsl/src/glsl/lower_mat_op_to_vec.cpp',
-                             'glsl/src/glsl/lower_noise.cpp',
-                             'glsl/src/glsl/lower_packed_varyings.cpp',
-                             'glsl/src/glsl/lower_variable_index_to_cond_assign.cpp',
-                             'glsl/src/glsl/lower_vec_index_to_cond_assign.cpp',
-                             'glsl/src/glsl/lower_vec_index_to_swizzle.cpp',
-                             'glsl/src/glsl/lower_vector.cpp',
-                             'glsl/src/glsl/opt_algebraic.cpp',
-                             'glsl/src/glsl/opt_array_splitting.cpp',
-                             'glsl/src/glsl/opt_constant_folding.cpp',
-                             'glsl/src/glsl/opt_constant_propagation.cpp',
-                             'glsl/src/glsl/opt_constant_variable.cpp',
-                             'glsl/src/glsl/opt_copy_propagation.cpp',
-                             'glsl/src/glsl/opt_copy_propagation_elements.cpp',
-                             'glsl/src/glsl/opt_dead_code.cpp',
-                             'glsl/src/glsl/opt_dead_code_local.cpp',
-                             'glsl/src/glsl/opt_dead_functions.cpp',
-                             'glsl/src/glsl/opt_flatten_nested_if_blocks.cpp',
-                             'glsl/src/glsl/opt_function_inlining.cpp',
-                             'glsl/src/glsl/opt_if_simplification.cpp',
-                             'glsl/src/glsl/opt_noop_swizzle.cpp',
-                             'glsl/src/glsl/opt_redundant_jumps.cpp',
-                             'glsl/src/glsl/opt_structure_splitting.cpp',
-                             'glsl/src/glsl/opt_swizzle_swizzle.cpp',
-                             'glsl/src/glsl/opt_tree_grafting.cpp',
-                             'glsl/src/glsl/ralloc.c',
-                             'glsl/src/glsl/s_expression.cpp',
-                             'glsl/src/glsl/standalone_scaffolding.cpp',
-                             'glsl/src/glsl/strtod.c',
-                             'glsl/src/glsl/glcpp/glcpp-lex.c',
-                             'glsl/src/glsl/glcpp/glcpp-parse.c',
-                             'glsl/src/glsl/glcpp/pp.c',
-                             'glsl/src/mesa/main/imports.c',
-                             'glsl/src/mesa/main/hash_table.c',
-                             'glsl/src/mesa/program/symbol_table.c',
-                             'glsl/src/mesa/program/prog_hash_table.c']
-
             commands = []
             o_s = []
 
-            # commands for glslopt
-            for src in srcs_glslopt:
-                c = os.path.join(dest_path_src, src)
-                o = os.path.join(dest_path_src, src + '.o')
-                shared.safe_ensure_dirs(os.path.dirname(o))
-                commands.append([shared.PYTHON, shared.EMCC, c,
-                                 '-DNDEBUG',
-                                 '-fvisibility=hidden',
-                                 '-Wno-sign-compare',
-                                 '-O2',
-                                 '-I' + os.path.join(dest_path_glslopt, 'include'),
-                                 '-I' + os.path.join(dest_path_glslopt,'src','glsl'),
-                                 '-I' + os.path.join(dest_path_glslopt,'src','mesa'),
-                                 '-o', o,
-                                 '-w'])
-                o_s.append(o)
-
-            # commands for regal
             for src in srcs_regal:
                 c = os.path.join(dest_path_src, src)
                 o = os.path.join(dest_path_src, src + '.o')
@@ -223,6 +116,7 @@ def get(ports, settings, shared):
                                  '-DREGAL_LOG=0',
                                  '-DREGAL_NO_TLS=1',
                                  '-DREGAL_THREAD_LOCKING=0',
+                                 '-DREGAL_GLSL_OPTIMIZER=0',
                                  '-fomit-frame-pointer',
                                  '-Wno-constant-logical-operand',
                                  '-fvisibility=hidden',
@@ -234,9 +128,6 @@ def get(ports, settings, shared):
                                  '-I' + dest_path_lookup3,
                                  '-I' + dest_path_jsonsl,
                                  '-I' + dest_path_boost,
-                                 '-I' + os.path.join(dest_path_glslopt, 'include'),
-                                 '-I' + os.path.join(dest_path_glslopt,'src','glsl'),
-                                 '-I' + os.path.join(dest_path_glslopt,'src','mesa'),
                                  '-w'])
                 o_s.append(o)
 
