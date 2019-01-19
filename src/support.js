951464df3add13543773b08e056d35d1f7380c07
Jukka Jylänki <jujjyl@gmail.com>
2019-01-19 18:52:41 +0200

    Print more descriptive error messages on some assert failures

--- src/support.js
+++ src/support.js
@@ -359,7 +359,9 @@ function loadWebAssemblyModule(binary, flags) {
     tableAlign = Math.pow(2, tableAlign);
     // finalize alignments and verify them
     memoryAlign = Math.max(memoryAlign, STACK_ALIGN); // we at least need stack alignment
-    assert(tableAlign === 1);
+#if ASSERTIONS
+    assert(tableAlign === 1, 'invalid tableAlign ' + tableAlign);
+#endif
     // prepare memory
     var memoryBase = alignMemory(getMemory(memorySize + memoryAlign), memoryAlign); // TODO: add to cleanups
     // The static area consists of explicitly initialized data, followed by zero-initialized data.
