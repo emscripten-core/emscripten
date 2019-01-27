9fd22bca2da6e90751a62e5f9866d747d4e0dbd1
Alon Zakai (kripken) <alonzakai@gmail.com>
2019-01-26 17:25:13 -0800

    main jsdce tests pass..? [ci skip]

--- tests/optimizer/AJSDCE-output.js
+++ tests/optimizer/AJSDCE-output.js
@@ -1,27 +1,36 @@
-
+;
+;
 var z = fleefl();
 var zz = fleefl();
+;
 function g(a) {
  return a + 1;
 }
-Module["g"] = g;
+Module['g'] = g;
 function h(a) {
+ ;
  return a + 1;
 }
 print(h(123));
-((function() {
+(function () {
+ ;
+ ;
  var z = fleefl();
  var zz = fleefl();
+ ;
  function g(a) {
   return a + 1;
  }
- Module["g"] = g;
+ Module['g'] = g;
  function hh(a) {
+  ;
   return a + 1;
  }
  print(hh(123));
-}))();
+})();
 function glue() {
+ ;
+ ;
 }
 glue();
 
