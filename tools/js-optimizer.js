dfeab97453dca21e30d3c7ecf4ead693e47e21f4
Alon Zakai (kripken) <alonzakai@gmail.com>
2019-01-27 09:13:03 -0800

    remove unused code

--- tools/js-optimizer.js
+++ tools/js-optimizer.js
@@ -7914,121 +7914,6 @@ function eliminateDeadGlobals(ast) {
   });
 }
 
-// Removes obviously-unused code. Similar to closure compiler in its rules -
-// export e.g. by Module['..'] = theThing; , or use it somewhere, otherwise
-// it goes away.
-function JSDCE(ast, multipleIterations) {
-  function iteration() {
-    var removed = false;
-    var scopes = [{}]; // begin with empty toplevel scope
-    function DUMP() {
-      printErr('vvvvvvvvvvvvvv');
-      for (var i = 0; i < scopes.length; i++) {
-        printErr(i + ' : ' + JSON.stringify(scopes[i]));
-      }
-      printErr('^^^^^^^^^^^^^^');
-    }
-    function ensureData(scope, name) {
-      if (Object.prototype.hasOwnProperty.call(scope, name)) return scope[name];
-      scope[name] = {
-        def: 0,
-        use: 0,
-        param: 0 // true for function params, which cannot be eliminated
-      };
-      return scope[name];
-    }
-    function cleanUp(ast, names) {
-      traverse(ast, function(node, type) {
-        if (type === 'defun' && Object.prototype.hasOwnProperty.call(names, node[1])) {
-          removed = true;
-          return emptyNode();
-        }
-        if (type === 'defun' || type === 'function') return null; // do not enter other scopes
-        if (type === 'var') {
-          node[1] = node[1].filter(function(varItem, j) {
-            var curr = varItem[0];
-            var value = varItem[1];
-            var keep = !(curr in names) || (value && hasSideEffects(value));
-            if (!keep) removed = true;
-            return keep;
-          });
-          if (node[1].length === 0) return emptyNode();
-        }
-      });
-      return ast;
-    }
-    traverse(ast, function(node, type) {
-      if (type === 'var') {
-        node[1].forEach(function(varItem, j) {
-          var name = varItem[0];
-          ensureData(scopes[scopes.length-1], name).def = 1;
-        });
-        return;
-      }
-      if (type === 'object') {
-        return;
-      }
-      if (type === 'defun' || type === 'function') {
-        // defun names matter - function names (the y in var x = function y() {..}) are just for stack traces.
-        if (type === 'defun') ensureData(scopes[scopes.length-1], node[1]).def = 1;
-        var scope = {};
-        node[2].forEach(function(param) {
-          ensureData(scope, param).def = 1;
-          scope[param].param = 1;
-        });
-        scopes.push(scope);
-        return;
-      }
-      if (type === 'name') {
-        ensureData(scopes[scopes.length-1], node[1]).use = 1;
-      }
-    }, function(node, type) {
-      if (type === 'defun' || type === 'function') {
-        // we can ignore self-references, i.e., references to ourselves inside
-        // ourselves, for named defined (defun) functions
-        var ownName = type === 'defun' ? node[1] : '';
-        var scope = scopes.pop();
-        var names = set();
-        for (name in scope) {
-          if (name === ownName) continue;
-          var data = scope[name];
-          if (data.use && !data.def) {
-            // this is used from a higher scope, propagate the use down
-            ensureData(scopes[scopes.length-1], name).use = 1;
-            continue;
-          }
-          if (data.def && !data.use && !data.param) {
-            // this is eliminateable!
-            names[name] = 0;
-          }
-        }
-        cleanUp(node[3], names);
-      }
-    });
-    // toplevel
-    var scope = scopes.pop();
-    assert(scopes.length === 0);
-
-    var names = set();
-    for (var name in scope) {
-      var data = scope[name];
-      if (data.def && !data.use) {
-        assert(!data.param); // can't be
-        // this is eliminateable!
-        names[name] = 0;
-      }
-    }
-    cleanUp(ast, names);
-    return removed;
-  }
-  while (iteration() && multipleIterations) { }
-}
-
-// Aggressive JSDCE - multiple iterations
-function AJSDCE(ast) {
-  JSDCE(ast, /* multipleIterations= */ true);
-}
-
 function isAsmLibraryArgAssign(node) {
   return node[0] === 'assign' && node[2][0] === 'dot'
                               && node[2][1][0] === 'name' && node[2][1][1] === 'Module'
@@ -8421,8 +8306,6 @@ var passes = {
   findReachable: findReachable,
   dumpCallGraph: dumpCallGraph,
   asmLastOpts: asmLastOpts,
-  JSDCE: JSDCE,
-  AJSDCE: AJSDCE,
   emitDCEGraph: emitDCEGraph,
   applyDCEGraphRemovals: applyDCEGraphRemovals,
   applyImportAndExportNameChanges: applyImportAndExportNameChanges,
