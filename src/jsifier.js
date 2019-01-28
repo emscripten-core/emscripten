02d14b201c467d81607a63a92de1cd9dd6eed8a8
Alon Zakai <azakai@google.com>
2019-01-28 14:04:23 -0800

    fix

--- src/jsifier.js
+++ src/jsifier.js
@@ -190,16 +190,17 @@ function JSify(data, functionsOnly) {
       var noExport = false;
 
       if ((!LibraryManager.library.hasOwnProperty(ident) && !LibraryManager.library.hasOwnProperty(ident + '__inline')) || SIDE_MODULE) {
+        if (item.ignoreIfMissing) {
+          return '';
+        }
         if (!(finalName in IMPLEMENTED_FUNCTIONS)) {
           if (VERBOSE || ident.substr(0, 11) !== 'emscripten_') { // avoid warning on emscripten_* functions which are for internal usage anyhow
             if (!LINKABLE) {
-              if (!item.ignoreIfMissing) {
-                if (ERROR_ON_UNDEFINED_SYMBOLS) {
-                  error('undefined symbol: ' + ident);
-                  warnOnce('To disable errors for undefined symbols use `-s ERROR_ON_UNDEFINED_SYMBOLS=0`')
-                } else if (VERBOSE || WARN_ON_UNDEFINED_SYMBOLS) {
-                  warn('undefined symbol: ' + ident);
-                }
+              if (ERROR_ON_UNDEFINED_SYMBOLS) {
+                error('undefined symbol: ' + ident);
+                warnOnce('To disable errors for undefined symbols use `-s ERROR_ON_UNDEFINED_SYMBOLS=0`')
+              } else if (VERBOSE || WARN_ON_UNDEFINED_SYMBOLS) {
+                warn('undefined symbol: ' + ident);
               }
             }
           }
