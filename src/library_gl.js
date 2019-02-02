c78243f0e207deeabe45a39e5a11fed1513dd450
Jukka Jylänki <jujjyl@gmail.com>
2019-02-02 14:39:08 +0200

    Micro-optimize WebGL glBindAttribLocation and glGetUniformBlockIndex. Optimizes -4 bytes (0.07%)

--- src/library_gl.js
+++ src/library_gl.js
@@ -2614,9 +2614,7 @@ var LibraryGL = {
 #if GL_ASSERTIONS
     GL.validateGLObjectID(GL.programs, program, 'glGetUniformBlockIndex', 'program');
 #endif
-    program = GL.programs[program];
-    uniformBlockName = UTF8ToString(uniformBlockName);
-    return GLctx['getUniformBlockIndex'](program, uniformBlockName);
+    return GLctx['getUniformBlockIndex'](GL.programs[program], UTF8ToString(uniformBlockName));
   },
 
   glGetActiveUniformBlockiv__sig: 'viiii',
@@ -4103,8 +4101,7 @@ var LibraryGL = {
 #if GL_ASSERTIONS
     GL.validateGLObjectID(GL.programs, program, 'glBindAttribLocation', 'program');
 #endif
-    name = UTF8ToString(name);
-    GLctx.bindAttribLocation(GL.programs[program], index, name);
+    GLctx.bindAttribLocation(GL.programs[program], index, UTF8ToString(name));
   },
 
   glBindFramebuffer__sig: 'vii',
