372081460625f578a1ce2296b13f637cb8005617
Jukka Jylänki <jujjyl@gmail.com>
2019-02-14 00:16:43 +0200

    Update code sizes

--- tests/test_other.py
+++ tests/test_other.py
@@ -8968,11 +8968,11 @@ int main () {
     hello_webgl2_sources = hello_webgl_sources + ['-s', 'USE_WEBGL2=1']
 
     test_cases = [
-      (asmjs + opts, hello_world_sources, {'a.html': 665, 'a.js': 289, 'a.asm.js': 113, 'a.mem': 6}),
-      (opts, hello_world_sources, {'a.html': 623, 'a.js': 624, 'a.wasm': 86}),
-      (asmjs + opts, hello_webgl_sources, {'a.html': 665, 'a.js': 4949, 'a.asm.js': 10975, 'a.mem': 321}),
-      (opts, hello_webgl_sources, {'a.html': 623, 'a.js': 5015, 'a.wasm': 8978}),
-      (opts, hello_webgl2_sources, {'a.html': 623, 'a.js': 6150, 'a.wasm': 8978}) # Compare how WebGL2 sizes stack up with WebGL 1
+      (asmjs + opts, hello_world_sources, {'a.html': 476, 'a.js': 289, 'a.asm.js': 113, 'a.mem': 6}),
+      (opts, hello_world_sources, {'a.html': 452, 'a.js': 624, 'a.wasm': 86}),
+      (asmjs + opts, hello_webgl_sources, {'a.html': 476, 'a.js': 4949, 'a.asm.js': 10975, 'a.mem': 321}),
+      (opts, hello_webgl_sources, {'a.html': 452, 'a.js': 5015, 'a.wasm': 8978}),
+      (opts, hello_webgl2_sources, {'a.html': 452, 'a.js': 6150, 'a.wasm': 8978}) # Compare how WebGL2 sizes stack up with WebGL 1
     ]
 
     success = True
