6e6acf0fe35ea0f65546e5e8cb9ccc2e912961e7
Alon Zakai <azakai@google.com>
2019-02-19 14:26:02 -0800

    fix nits [ci skip]

--- tests/filesystem/dev_random.cpp
+++ tests/filesystem/dev_random.cpp
@@ -1,3 +1,8 @@
+// Copyright 2019 The Emscripten Authors.  All rights reserved.
+// Emscripten is available under two separate licenses, the MIT license and the
+// University of Illinois/NCSA Open Source License.  Both these licenses can be
+// found in the LICENSE file.
+
 #include <stdio.h>
 int main() {
   int byte_count = 64;
@@ -9,4 +14,3 @@ int main() {
   REPORT_RESULT(0);
   return 0;
 }
-
