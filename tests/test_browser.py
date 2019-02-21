249df9528a95cd9c6e1a6f9d19622392de347f33
Jukka Jylänki <jujjyl@gmail.com>
2019-02-21 14:38:52 +0200

    Fix handling of WebGL uniform arrays that regressed in https://github.com/emscripten-core/emscripten/commit/71452872cd6aada1af36d1e0e039b1cc571e0580#commitcomment-32381622

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -4071,6 +4071,11 @@ window.close = function() {
   def test_webgl_workaround_webgl_uniform_upload_bug(self):
     self.btest('webgl_draw_triangle_with_uniform_color.c', '0', args=['-lGL', '-s', 'WORKAROUND_OLD_WEBGL_UNIFORM_UPLOAD_IGNORED_OFFSET_BUG=1'])
 
+  # Tests that using an array of structs in GL uniforms works.
+  @requires_graphics_hardware
+  def test_webgl_array_of_structs_uniform(self):
+    self.btest('webgl_array_of_structs_uniform.c', '0', args=['-lGL', '-s', 'USE_WEBGL2=1'], reference='webgl_array_of_structs_uniform.png')
+
   # Tests that if a WebGL context is created in a pthread on a canvas that has not been transferred to that pthread, WebGL calls are then proxied to the main thread
   # -DTEST_OFFSCREEN_CANVAS=1: Tests that if a WebGL context is created on a pthread that has the canvas transferred to it via using Emscripten's EMSCRIPTEN_PTHREAD_TRANSFERRED_CANVASES="#canvas", then OffscreenCanvas is used
   # -DTEST_OFFSCREEN_CANVAS=2: Tests that if a WebGL context is created on a pthread that has the canvas transferred to it via automatic transferring of Module.canvas when EMSCRIPTEN_PTHREAD_TRANSFERRED_CANVASES is not defined, then OffscreenCanvas is also used
