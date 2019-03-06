emscripten-core/emscripten

commit 5f88f2bef71a573e99d7fe8fb39128cdc0c57edc

include relevant filesystems based on the env (#7806)
Fixes #7803

src/modules.js
@@ -129,14 +129,21 @@ var LibraryManager = {
       // Additional filesystem libraries (in strict mode, link to these explicitly via -lxxx.js)
       if (!STRICT) {
         libraries = libraries.concat([
-          'library_idbfs.js',
-          'library_nodefs.js',
-          'library_proxyfs.js',
-          'library_sockfs.js',
-          'library_workerfs.js',
           'library_lz4.js',
         ]);
-
+        if (ENVIRONMENT_MAY_BE_WEB) {
+          libraries = libraries.concat([
+            'library_idbfs.js',
+            'library_proxyfs.js',
+            'library_sockfs.js',
+            'library_workerfs.js',
+          ]);
+        }
+        if (ENVIRONMENT_MAY_BE_NODE) {
+          libraries = libraries.concat([
+            'library_nodefs.js',
+          ]);
+        }
         if (NODERAWFS) {
           libraries.push('library_noderawfs.js')
         }

commit e821ec0ef40d80661f25c9e8958f9c4d04adad69

Fix filesystem regression - enable them for workers, not just web (#8221)
When we added checks for environments for the libraries, we put the filesystem ones like IDBFS behind web, but it can be used in workers too.

fixes #8213 (the regression was in #7806)

src/modules.js
@@ -94,7 +94,7 @@ var LibraryManager = {
         libraries = libraries.concat([
           'library_lz4.js',
         ]);
-        if (ENVIRONMENT_MAY_BE_WEB) {
+        if (ENVIRONMENT_MAY_BE_WEB || ENVIRONMENT_MAY_BE_WORKER) {
           libraries = libraries.concat([
             'library_idbfs.js',
             'library_proxyfs.js',