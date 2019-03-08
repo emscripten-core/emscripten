29852d44c88730fe596ec8e591b91f0c41cfb92e
Alon Zakai <azakai@google.com>
2019-03-08 15:07:50 -0800

    fix

--- src/library.js
+++ src/library.js
@@ -753,14 +753,13 @@ LibraryManager.library = {
     }
 
     totalMemory = _emscripten_get_heap_size()|0;
-    if ((newDynamicTop|0) <= (totalMemory|0)) {
-      HEAP32[DYNAMICTOP_PTR>>2] = newDynamicTop|0;
-    } else {
+    if ((newDynamicTop|0) > (totalMemory|0)) {
       if ((_emscripten_resize_heap(newDynamicTop|0)|0) == 0) {
         ___setErrNo({{{ cDefine('ENOMEM') }}});
         return -1;
       }
     }
+    HEAP32[DYNAMICTOP_PTR>>2] = newDynamicTop|0;
 #endif
     return oldDynamicTop|0;
   },
@@ -798,14 +797,13 @@ LibraryManager.library = {
     }
 
     totalMemory = _emscripten_get_heap_size()|0;
-    if ((newDynamicTop|0) <= (totalMemory|0)) {
-      HEAP32[DYNAMICTOP_PTR>>2] = newDynamicTop|0;
-    } else {
+    if ((newDynamicTop|0) > (totalMemory|0)) {
       if ((_emscripten_resize_heap(newDynamicTop|0)|0) == 0) {
         ___setErrNo({{{ cDefine('ENOMEM') }}});
         return -1;
       }
     }
+    HEAP32[DYNAMICTOP_PTR>>2] = newDynamicTop|0;
 #endif
     return 0;
   },
