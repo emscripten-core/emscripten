03c2739f077890742e0bb136f4e13c0f5b08f9d6
Marc Abramowitz <marc@marc-abramowitz.com>
2019-03-28 17:40:49 -0700

    Add libjpeg port (#8361)

--- tools/ports/__init__.py
+++ tools/ports/__init__.py
@@ -3,7 +3,7 @@
 # University of Illinois/NCSA Open Source License.  Both these licenses can be
 # found in the LICENSE file.
 
-from . import binaryen, bullet, cocos2d, freetype, harfbuzz, icu, libpng
+from . import binaryen, bullet, cocos2d, freetype, harfbuzz, icu, libjpeg, libpng
 from . import ogg, regal, sdl2, sdl2_gfx, sdl2_image, sdl2_mixer, sdl2_ttf
 from . import sdl2_net, vorbis, zlib, bzip2
 
@@ -12,6 +12,7 @@ ports = [
     icu,
     zlib,
     bzip2,
+    libjpeg,
     libpng,
     sdl2,
     sdl2_image,
