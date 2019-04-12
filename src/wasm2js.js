28af8754a7b84f295b141476eb1af7b22bfe8c75
Alon Zakai <azakai@google.com>
2019-04-12 09:16:06 -0700

    integration moar [ci skip]

--- src/wasm2js.js
+++ src/wasm2js.js
@@ -2,7 +2,27 @@
 // wasm2js code that way.
 
 var WebAssembly = {
-  // This will be replaced by the actual wasm2js code.
-  temp: Module['__wasm2jsInstantiate__'](asmLibraryArg, wasmMemory, wasmTable);
+  Memory: function(opts) {
+    // TODO: growth?
+    return {
+      buffer: new ArrayBuffer(opts['initial'] * {{{ WASM_PAGE_SIZE }}})
+    };
+  },
+
+  Table: function(opts) {
+    var ret = new Array(opts['initial']);
+    // TODO: add more properties here
+    return ret;
+  },
+
+  instantiate: function(binary, info) {
+    // TODO: use the binary and info somehow - right now the wasm2js output is embedded in
+    // the main JS
+    // XXX hack to get an atob implementation
+#include base64Utils.js
+    var atob = decodeBase64;
+    // This will be replaced by the actual wasm2js code.
+    Module['__wasm2jsInstantiate__'](asmLibraryArg, wasmMemory, wasmTable);
+  }
 };
 
