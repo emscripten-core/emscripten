ad72e94db2a3ab89c44828026f9d0a78e90893c8
Alon Zakai <azakai@google.com>
2019-05-08 10:11:42 -0700

    start to remove COMPILER_TEST_OPTS. remove outlining tests, as the feature should be removed anyhow

--- tests/runner.py
+++ tests/runner.py
@@ -481,8 +481,7 @@ class RunnerCore(unittest.TestCase):
     return output_obj
 
   def get_emcc_args(self):
-    # TODO(sbc): We should probably unify Building.COMPILER_TEST_OPTS and self.emcc_args
-    return self.serialize_settings() + self.emcc_args + Building.COMPILER_TEST_OPTS
+    return self.serialize_settings() + self.emcc_args
 
   # Build JavaScript code from source code
   def build(self, src, dirname, filename, main_file=None,
@@ -750,8 +749,10 @@ class RunnerCore(unittest.TestCase):
     build_dir = self.get_build_dir()
     output_dir = self.get_dir()
 
-    hash_input = (str(Building.COMPILER_TEST_OPTS) + ' $ ' + str(env_init)).encode('utf-8')
-    cache_name = name + ','.join([opt for opt in Building.COMPILER_TEST_OPTS if len(opt) < 7]) + '_' + hashlib.md5(hash_input).hexdigest() + cache_name_extra
+    emcc_args = self.get_emcc_args()
+
+    hash_input = (str(emcc_args) + ' $ ' + str(env_init)).encode('utf-8')
+    cache_name = name + ','.join([opt for opt in emcc_args if len(opt) < 7]) + '_' + hashlib.md5(hash_input).hexdigest() + cache_name_extra
 
     valid_chars = "_%s%s" % (string.ascii_letters, string.digits)
     cache_name = ''.join([(c if c in valid_chars else '_') for c in cache_name])
@@ -1030,7 +1031,7 @@ class RunnerCore(unittest.TestCase):
     # e.g. FcConfigSubstitute
     self.set_setting('ERROR_ON_UNDEFINED_SYMBOLS', 0)
 
-    Building.COMPILER_TEST_OPTS += [
+    self.emcc_args += [
       '-I' + path_from_root('tests', 'freetype', 'include'),
       '-I' + path_from_root('tests', 'poppler', 'include')
     ]
@@ -1039,7 +1040,7 @@ class RunnerCore(unittest.TestCase):
 
     # Poppler has some pretty glaring warning.  Suppress them to keep the
     # test output readable.
-    Building.COMPILER_TEST_OPTS += [
+    self.emcc_args += [
       '-Wno-sentinel',
       '-Wno-logical-not-parentheses',
       '-Wno-unused-private-field',
--- tests/test_core.py
+++ tests/test_core.py
@@ -530,7 +530,7 @@ class TestCoreBase(RunnerCore):
     self.do_run_in_out_file_test('tests', 'core', 'closebitcasts')
 
   def test_fast_math(self):
-    Building.COMPILER_TEST_OPTS += ['-ffast-math']
+    self.emcc_args += ['-ffast-math']
 
     self.do_run_in_out_file_test('tests', 'core', 'test_fast_math',
                                  args=['5', '6', '8'])
@@ -1698,7 +1698,7 @@ int main(int argc, char **argv) {
   # Then this must still be compiled separately from other code using EM_ASM
   # macros with arities 1-3. Otherwise this may incorrectly report a success.
   def test_em_asm_parameter_pack(self):
-    Building.COMPILER_TEST_OPTS += ['-std=c++11']
+    self.emcc_args += ['-std=c++11']
     self.do_run_in_out_file_test('tests', 'core', 'test_em_asm_parameter_pack')
 
   def test_em_js(self):
@@ -1868,7 +1868,7 @@ Success!''')
 
   @no_wasm_backend('no implementation of computed gotos')
   def test_indirectbr(self):
-      Building.COMPILER_TEST_OPTS = [x for x in Building.COMPILER_TEST_OPTS if x != '-g']
+      self.emcc_args = [x for x in self.emcc_args if x != '-g']
 
       self.do_run_in_out_file_test('tests', 'core', 'test_indirectbr')
 
@@ -4139,7 +4139,7 @@ ok
     # avoid using asm2wasm imports, which don't work in side modules yet (should they?)
     self.set_setting('BINARYEN_TRAP_MODE', 'clamp')
 
-    Building.COMPILER_TEST_OPTS += ['-I' + path_from_root('tests', 'zlib'), '-s', 'RELOCATABLE']
+    self.emcc_args += ['-I' + path_from_root('tests', 'zlib'), '-s', 'RELOCATABLE']
     zlib_archive = self.get_zlib_library()
     self.dylink_test(main=open(path_from_root('tests', 'zlib', 'example.c')).read(),
                      side=zlib_archive,
@@ -4148,7 +4148,7 @@ ok
 
   # @needs_dlfcn
   # def test_dylink_bullet(self):
-  #   Building.COMPILER_TEST_OPTS += ['-I' + path_from_root('tests', 'bullet', 'src')]
+  #   self.emcc_args += ['-I' + path_from_root('tests', 'bullet', 'src')]
   #   side = self.get_bullet_library(self, True)
   #   self.dylink_test(main=open(path_from_root('tests', 'bullet', 'Demos', 'HelloWorld', 'HelloWorld.cpp')).read(),
   #                    side=side,
@@ -4533,7 +4533,7 @@ Module = {
 
   def test_getdents64_special_cases(self):
     self.banned_js_engines = [V8_ENGINE] # https://bugs.chromium.org/p/v8/issues/detail?id=6881
-    Building.COMPILER_TEST_OPTS += ['-std=c++11']
+    self.emcc_args += ['-std=c++11']
     src = path_from_root('tests', 'fs', 'test_getdents64_special_cases.cpp')
     out = path_from_root('tests', 'fs', 'test_getdents64_special_cases.out')
     self.do_run_from_file(src, out, assert_identical=True)
@@ -4542,7 +4542,7 @@ Module = {
     self.banned_js_engines = [V8_ENGINE] # https://bugs.chromium.org/p/v8/issues/detail?id=6881
     src = path_from_root('tests', 'fs', 'test_getcwd_with_non_ascii_name.cpp')
     out = path_from_root('tests', 'fs', 'test_getcwd_with_non_ascii_name.out')
-    Building.COMPILER_TEST_OPTS += ['-std=c++11']
+    self.emcc_args += ['-std=c++11']
     self.do_run_from_file(src, out, assert_identical=True)
 
   def test_fwrite_0(self):
@@ -4552,11 +4552,11 @@ Module = {
     print('TODO: update this test once the musl ungetc-on-EOF-stream bug is fixed upstream and reaches us')
     self.set_setting('SYSCALL_DEBUG', 1)
     self.clear()
-    orig_compiler_opts = Building.COMPILER_TEST_OPTS[:]
+    orig_compiler_opts = self.emcc_args[:]
     for fs in ['MEMFS', 'NODEFS']:
       print(fs)
       src = open(path_from_root('tests', 'stdio', 'test_fgetc_ungetc.c')).read()
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-D' + fs]
+      self.emcc_args = orig_compiler_opts + ['-D' + fs]
       self.do_run(src, 'success', force_c=True, js_engines=[NODE_JS])
 
   def test_fgetc_unsigned(self):
@@ -4736,17 +4736,17 @@ main( int argv, char ** argc ) {
   def test_utf8(self):
     self.set_setting('EXTRA_EXPORTED_RUNTIME_METHODS',
                      ['UTF8ToString', 'stringToUTF8', 'AsciiToString', 'stringToAscii'])
-    Building.COMPILER_TEST_OPTS += ['-std=c++11']
+    self.emcc_args += ['-std=c++11']
     self.do_run(open(path_from_root('tests', 'utf8.cpp')).read(), 'OK.')
 
   def test_utf8_textdecoder(self):
     self.set_setting('EXTRA_EXPORTED_RUNTIME_METHODS', ['UTF8ToString', 'stringToUTF8'])
-    Building.COMPILER_TEST_OPTS += ['--embed-file', path_from_root('tests/utf8_corpus.txt') + '@/utf8_corpus.txt']
+    self.emcc_args += ['--embed-file', path_from_root('tests/utf8_corpus.txt') + '@/utf8_corpus.txt']
     self.do_run(open(path_from_root('tests', 'benchmark_utf8.cpp')).read(), 'OK.')
 
   def test_utf16_textdecoder(self):
     self.set_setting('EXTRA_EXPORTED_RUNTIME_METHODS', ['UTF16ToString', 'stringToUTF16', 'lengthBytesUTF16'])
-    Building.COMPILER_TEST_OPTS += ['--embed-file', path_from_root('tests/utf16_corpus.txt') + '@/utf16_corpus.txt']
+    self.emcc_args += ['--embed-file', path_from_root('tests/utf16_corpus.txt') + '@/utf16_corpus.txt']
     self.do_run(open(path_from_root('tests', 'benchmark_utf16.cpp')).read(), 'OK.')
 
   def test_wprintf(self):
@@ -4833,11 +4833,11 @@ main( int argv, char ** argc ) {
     self.do_run(src, 'success', force_c=True, js_engines=js_engines)
 
   def test_fs_mmap(self):
-    orig_compiler_opts = Building.COMPILER_TEST_OPTS[:]
+    orig_compiler_opts = self.emcc_args[:]
     for fs in ['MEMFS']:
       src = path_from_root('tests', 'fs', 'test_mmap.c')
       out = path_from_root('tests', 'fs', 'test_mmap.out')
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-D' + fs]
+      self.emcc_args = orig_compiler_opts + ['-D' + fs]
       self.do_run_from_file(src, out)
 
   @also_with_noderawfs
@@ -4873,15 +4873,15 @@ main( int argv, char ** argc ) {
 
   def test_unistd_access(self):
     self.clear()
-    orig_compiler_opts = Building.COMPILER_TEST_OPTS[:]
+    orig_compiler_opts = self.emcc_args[:]
     src = open(path_from_root('tests', 'unistd', 'access.c')).read()
     expected = open(path_from_root('tests', 'unistd', 'access.out')).read()
     for fs in ['MEMFS', 'NODEFS']:
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-D' + fs]
+      self.emcc_args = orig_compiler_opts + ['-D' + fs]
       self.do_run(src, expected, js_engines=[NODE_JS])
     # Node.js fs.chmod is nearly no-op on Windows
     if not WINDOWS:
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts
+      self.emcc_args = orig_compiler_opts
       self.emcc_args += ['-s', 'NODERAWFS=1']
       self.do_run(src, expected, js_engines=[NODE_JS])
 
@@ -4923,11 +4923,11 @@ main( int argv, char ** argc ) {
 
   def test_unistd_truncate(self):
     self.clear()
-    orig_compiler_opts = Building.COMPILER_TEST_OPTS[:]
+    orig_compiler_opts = self.emcc_args[:]
     for fs in ['MEMFS', 'NODEFS']:
       src = open(path_from_root('tests', 'unistd', 'truncate.c')).read()
       expected = open(path_from_root('tests', 'unistd', 'truncate.out')).read()
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-D' + fs]
+      self.emcc_args = orig_compiler_opts + ['-D' + fs]
       self.do_run(src, expected, js_engines=[NODE_JS])
 
   @no_windows("Windows throws EPERM rather than EACCES or EINVAL")
@@ -4968,30 +4968,30 @@ main( int argv, char ** argc ) {
 
   def test_unistd_unlink(self):
     self.clear()
-    orig_compiler_opts = Building.COMPILER_TEST_OPTS[:]
+    orig_compiler_opts = self.emcc_args[:]
     src = open(path_from_root('tests', 'unistd', 'unlink.c')).read()
     for fs in ['MEMFS', 'NODEFS']:
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-D' + fs]
+      self.emcc_args = orig_compiler_opts + ['-D' + fs]
       # symlinks on node.js on non-linux behave differently (e.g. on Windows they require administrative privileges)
       # so skip testing those bits on that combination.
       if fs == 'NODEFS':
         if WINDOWS:
-          Building.COMPILER_TEST_OPTS += ['-DNO_SYMLINK=1']
+          self.emcc_args += ['-DNO_SYMLINK=1']
         if MACOS:
           continue
       self.do_run(src, 'success', force_c=True, js_engines=[NODE_JS])
     # Several differences/bugs on non-linux including https://github.com/nodejs/node/issues/18014
     if not WINDOWS and not MACOS:
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-DNODERAWFS']
+      self.emcc_args = orig_compiler_opts + ['-DNODERAWFS']
       # 0 if root user
       if os.geteuid() == 0:
-        Building.COMPILER_TEST_OPTS += ['-DSKIP_ACCESS_TESTS']
+        self.emcc_args += ['-DSKIP_ACCESS_TESTS']
       self.emcc_args += ['-s', 'NODERAWFS=1']
       self.do_run(src, 'success', force_c=True, js_engines=[NODE_JS])
 
   def test_unistd_links(self):
     self.clear()
-    orig_compiler_opts = Building.COMPILER_TEST_OPTS[:]
+    orig_compiler_opts = self.emcc_args[:]
     src = open(path_from_root('tests', 'unistd', 'links.c')).read()
     expected = open(path_from_root('tests', 'unistd', 'links.out')).read()
     for fs in ['MEMFS', 'NODEFS']:
@@ -5001,7 +5001,7 @@ main( int argv, char ** argc ) {
         # test expects /, but Windows gives \ as path slashes.
         # Calling readlink() on a non-link gives error 22 EINVAL on Unix, but simply error 0 OK on Windows.
         continue
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-D' + fs]
+      self.emcc_args = orig_compiler_opts + ['-D' + fs]
       self.do_run(src, expected, js_engines=[NODE_JS])
 
   @no_windows('Skipping NODEFS test, since it would require administrative privileges.')
@@ -5023,19 +5023,19 @@ main( int argv, char ** argc ) {
     self.set_setting('INCLUDE_FULL_LIBRARY', 1) # uses constants from ERRNO_CODES
     self.set_setting('ERROR_ON_UNDEFINED_SYMBOLS', 0) # avoid errors when linking in full library
     self.clear()
-    orig_compiler_opts = Building.COMPILER_TEST_OPTS[:]
+    orig_compiler_opts = self.emcc_args[:]
     src = open(path_from_root('tests', 'unistd', 'io.c')).read()
     expected = open(path_from_root('tests', 'unistd', 'io.out')).read()
     for fs in ['MEMFS', 'NODEFS']:
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-D' + fs]
+      self.emcc_args = orig_compiler_opts + ['-D' + fs]
       self.do_run(src, expected, js_engines=[NODE_JS])
 
   def test_unistd_misc(self):
-    orig_compiler_opts = Building.COMPILER_TEST_OPTS[:]
+    orig_compiler_opts = self.emcc_args[:]
     src = open(path_from_root('tests', 'unistd', 'misc.c')).read()
     expected = open(path_from_root('tests', 'unistd', 'misc.out')).read()
     for fs in ['MEMFS', 'NODEFS']:
-      Building.COMPILER_TEST_OPTS = orig_compiler_opts + ['-D' + fs]
+      self.emcc_args = orig_compiler_opts + ['-D' + fs]
       self.do_run(src, expected, js_engines=[NODE_JS])
 
   def test_posixtime(self):
@@ -5098,10 +5098,10 @@ PORT: 3979
     self.do_run_in_out_file_test('tests', 'core', 'test_atomic')
 
   def test_atomic_cxx(self):
-    Building.COMPILER_TEST_OPTS += ['-std=c++11']
+    self.emcc_args += ['-std=c++11']
     # the wasm backend has lock-free atomics, but not asm.js or asm2wasm
     is_lock_free = self.is_wasm_backend()
-    Building.COMPILER_TEST_OPTS += ['-DIS_64BIT_LOCK_FREE=%d' % is_lock_free]
+    self.emcc_args += ['-DIS_64BIT_LOCK_FREE=%d' % is_lock_free]
     self.do_run_in_out_file_test('tests', 'core', 'test_atomic_cxx')
 
     if self.get_setting('ALLOW_MEMORY_GROWTH') == 0 and not self.is_wasm():
@@ -5165,7 +5165,7 @@ PORT: 3979
     self.do_run_in_out_file_test('tests', 'core', 'test_stdvec')
 
   def test_random_device(self):
-    Building.COMPILER_TEST_OPTS += ['-std=c++11']
+    self.emcc_args += ['-std=c++11']
 
     self.do_run_in_out_file_test('tests', 'core', 'test_random_device')
 
@@ -5447,7 +5447,7 @@ return malloc(size);
     if self.run_name == 'asm3':
       self.emcc_args += ['--closure', '1'] # Use closure here for some additional coverage
 
-    Building.COMPILER_TEST_OPTS = [x for x in Building.COMPILER_TEST_OPTS if x != '-g'] # remove -g, so we have one test without it by default
+    self.emcc_args = [x for x in self.emcc_args if x != '-g'] # remove -g, so we have one test without it by default
 
     def test():
       self.do_run(path_from_root('tests', 'cubescript'), '*\nTemp is 33\n9\n5\nhello, everyone\n*', main_file='command.cpp')
@@ -5550,7 +5550,7 @@ return malloc(size);
     self.do_run_in_out_file_test('tests', 'core', 'test_simd_shift_right')
 
   def test_gcc_unmangler(self):
-    Building.COMPILER_TEST_OPTS += ['-I' + path_from_root('third_party')]
+    self.emcc_args += ['-I' + path_from_root('third_party')]
 
     self.do_run(open(path_from_root('third_party', 'gcc_demangler.c')).read(), '*d_demangle(char const*, int, unsigned int*)*', args=['_ZL10d_demanglePKciPj'])
 
@@ -5748,7 +5748,7 @@ return malloc(size);
       return out
 
     # remove -g, so we have one test without it by default
-    Building.COMPILER_TEST_OPTS = [x for x in Building.COMPILER_TEST_OPTS if x != '-g']
+    self.emcc_args = [x for x in self.emcc_args if x != '-g']
 
     original_j2k = path_from_root('tests', 'openjpeg', 'syntensity_lobby_s.j2k')
     image_bytes = list(bytearray(open(original_j2k, 'rb').read()))
@@ -5965,7 +5965,7 @@ return malloc(size);
 
   @is_slow_test
   def test_fuzz(self):
-    Building.COMPILER_TEST_OPTS += ['-I' + path_from_root('tests', 'fuzz', 'include'), '-w']
+    self.emcc_args += ['-I' + path_from_root('tests', 'fuzz', 'include'), '-w']
     # some of these tests - 2.c', '9.c', '19.c', '21.c', '20.cpp' - div or rem i32 by 0, which traps in wasm
     self.set_setting('BINARYEN_TRAP_MODE', 'clamp')
 
@@ -6007,7 +6007,7 @@ return malloc(size);
     if self.is_wasm_backend() and self.get_setting('WASM_OBJECT_FILES') == 1:
       return self.skipTest('must use bitcode object files for bitcode autodebug')
 
-    Building.COMPILER_TEST_OPTS += ['--llvm-opts', '0']
+    self.emcc_args += ['--llvm-opts', '0']
 
     # Autodebug the code
     def do_autodebug(filename):
@@ -6427,7 +6427,7 @@ return malloc(size);
             self.assertContained(' ' + short_aborter + ' ' + '\n' + ' ' + full_aborter + ' ', output)
 
   def test_tracing(self):
-    Building.COMPILER_TEST_OPTS += ['--tracing']
+    self.emcc_args += ['--tracing']
 
     self.do_run_in_out_file_test('tests', 'core', 'test_tracing')
 
@@ -6556,7 +6556,7 @@ someweirdtext
     do_test(test3)
 
   def test_embind(self):
-    Building.COMPILER_TEST_OPTS += ['--bind']
+    self.emcc_args += ['--bind']
 
     src = r'''
       #include <stdio.h>
@@ -6577,7 +6577,7 @@ someweirdtext
     self.do_run(src, 'abs(-10): 10\nabs(-11): 11')
 
   def test_embind_2(self):
-    Building.COMPILER_TEST_OPTS += ['--bind', '--post-js', 'post.js']
+    self.emcc_args += ['--bind', '--post-js', 'post.js']
     create_test_file('post.js', '''
       function printLerp() {
           out('lerp ' + Module.lerp(100, 200, 66) + '.');
@@ -6602,7 +6602,7 @@ someweirdtext
     self.do_run(src, 'lerp 166')
 
   def test_embind_3(self):
-    Building.COMPILER_TEST_OPTS += ['--bind', '--post-js', 'post.js']
+    self.emcc_args += ['--bind', '--post-js', 'post.js']
     create_test_file('post.js', '''
       function ready() {
         try {
@@ -6631,7 +6631,7 @@ someweirdtext
 
   @no_wasm_backend('long doubles are f128s in wasm backend')
   def test_embind_4(self):
-    Building.COMPILER_TEST_OPTS += ['--bind', '--post-js', 'post.js']
+    self.emcc_args += ['--bind', '--post-js', 'post.js']
     create_test_file('post.js', '''
       function printFirstElement() {
         out(Module.getBufferView()[0]);
@@ -6663,7 +6663,7 @@ someweirdtext
     self.do_run(src, '107')
 
   def test_embind_5(self):
-    Building.COMPILER_TEST_OPTS += ['--bind']
+    self.emcc_args += ['--bind']
     self.do_run_in_out_file_test('tests', 'core', 'test_embind_5')
 
   def test_embind_float_constants(self):
@@ -6759,8 +6759,8 @@ err = err = function(){};
   def test_source_map(self):
     if not jsrun.check_engine(NODE_JS):
       self.skipTest('sourcemapper requires Node to run')
-    if '-g' not in Building.COMPILER_TEST_OPTS:
-      Building.COMPILER_TEST_OPTS.append('-g')
+    if '-g' not in self.emcc_args:
+      self.emcc_args.append('-g')
 
     src = '''
       #include <stdio.h>
@@ -6782,9 +6782,9 @@ err = err = function(){};
     wasm_filename = 'a.out.wasm'
     no_maps_filename = 'no-maps.out.js'
 
-    assert '-g4' not in Building.COMPILER_TEST_OPTS
+    assert '-g4' not in self.emcc_args
     Building.emcc('src.cpp',
-                  self.serialize_settings() + self.emcc_args + Building.COMPILER_TEST_OPTS,
+                  self.serialize_settings() + self.emcc_args + self.emcc_args,
                   out_filename)
     # the file name may find its way into the generated code, so make sure we
     # can do an apples-to-apples comparison by compiling with the same file name
@@ -6792,10 +6792,10 @@ err = err = function(){};
     with open(no_maps_filename) as f:
       no_maps_file = f.read()
     no_maps_file = re.sub(' *//[@#].*$', '', no_maps_file, flags=re.MULTILINE)
-    Building.COMPILER_TEST_OPTS.append('-g4')
+    self.emcc_args.append('-g4')
 
     Building.emcc(os.path.abspath('src.cpp'),
-                  self.serialize_settings() + self.emcc_args + Building.COMPILER_TEST_OPTS,
+                  self.serialize_settings() + self.emcc_args + self.emcc_args,
                   out_filename,
                   stderr=PIPE)
     map_referent = out_filename if not self.get_setting('WASM') else wasm_filename
@@ -6927,13 +6927,13 @@ err = err = function(){};
       # without this, stack traces are not useful (we jump emterpret=>emterpret)
       self.emcc_args += ['--profiling-funcs']
       # even so, we get extra emterpret() calls on the stack
-      Building.COMPILER_TEST_OPTS += ['-DEMTERPRETER']
+      self.emcc_args += ['-DEMTERPRETER']
     if self.get_setting('ASM_JS'):
       # XXX Does not work in SpiderMonkey since callstacks cannot be captured when running in asm.js, see https://bugzilla.mozilla.org/show_bug.cgi?id=947996
       self.banned_js_engines += [SPIDERMONKEY_ENGINE]
-    if '-g' not in Building.COMPILER_TEST_OPTS:
-      Building.COMPILER_TEST_OPTS.append('-g')
-    Building.COMPILER_TEST_OPTS += ['-DRUN_FROM_JS_SHELL']
+    if '-g' not in self.emcc_args:
+      self.emcc_args.append('-g')
+    self.emcc_args += ['-DRUN_FROM_JS_SHELL']
     self.do_run(open(path_from_root('tests', 'emscripten_log', 'emscripten_log.cpp')).read(), '''test print 123
 
 12.345679 9.123457 1.353180
@@ -7596,7 +7596,7 @@ def make_run(name, emcc_args, settings=None, env=None):
         del os.environ[k]
 
       # clear global changes to Building
-      Building.COMPILER_TEST_OPTS = []
+      self.emcc_args = []
 
   TT.tearDown = tearDown
 
@@ -7617,7 +7617,7 @@ def make_run(name, emcc_args, settings=None, env=None):
     self.test_mode_settings = settings
 
     # avoid various compiler warnings in our test output
-    Building.COMPILER_TEST_OPTS += [
+    self.emcc_args += [
       '-Werror', '-Wno-dynamic-class-memaccess', '-Wno-format',
       '-Wno-format-extra-args', '-Wno-format-security',
       '-Wno-pointer-bool-conversion', '-Wno-unused-volatile-lvalue',
@@ -7625,18 +7625,7 @@ def make_run(name, emcc_args, settings=None, env=None):
       '-Wno-invalid-pp-token', '-Wno-shift-negative-value'
     ]
 
-    # forward important flags to COMPILER_TEST_OPTS, which is sent via
-    # CFLAGS etc. to libraries that are built.
-    for arg in self.emcc_args:
-      # propagate optimization level
-      if arg.startswith('-O'):
-        Building.COMPILER_TEST_OPTS.append(arg)
-      # propagate LLVM machine arguments
-      if arg.startswith('-m'):
-        Building.COMPILER_TEST_OPTS.append(arg)
-
-    for key, value in settings.items():
-      Building.COMPILER_TEST_OPTS += ['-s', '%s=%s' % (key, json.dumps(value))]
+    # TODO: verif wasmlto* gives bvitcode in lib files
 
   TT.setUp = setUp
 
