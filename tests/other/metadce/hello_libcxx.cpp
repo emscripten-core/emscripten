d449897aa8f2e30e771e1048d9ac2ef492788bde
Sam Clegg <sbc@chromium.org>
2019-05-10 16:39:54 -0700

    Add support for more detailed metadce expectations (#8583)
    Includes support for automatically updating the expectation files.
    
    Initial this is only for the "sent" functions, but can be extended
    to imports and exports later.

---
+++ tests/other/metadce/hello_libcxx.cpp
@@ -0,0 +1,13 @@
+// Copyright 2012 The Emscripten Authors.  All rights reserved.
+// Emscripten is available under two separate licenses, the MIT license and the
+// University of Illinois/NCSA Open Source License.  Both these licenses can be
+// found in the LICENSE file.
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "hello, world!" << std::endl;
+  return 0;
+}
+
