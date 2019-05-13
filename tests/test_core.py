96e03342389054deb4f5e31a827c8e1205c44b94
Alon Zakai <azakai@google.com>
2019-05-13 14:11:52 -0700

    disable a wasm binary-using test

--- tests/test_core.py
+++ tests/test_core.py
@@ -6412,7 +6412,7 @@ return malloc(size);
   def test_eval_ctors(self):
     if '-O2' not in str(self.emcc_args) or '-O1' in str(self.emcc_args):
       self.skipTest('need js optimizations')
-    if self.get_setting('WASM2JS'):
+    if not self.get_setting('WASM'):
       self.skipTest('this test uses wasm binaries')
 
     orig_args = self.emcc_args
