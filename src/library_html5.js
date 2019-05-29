42016435de0a74b844c6206f45cf3ae6e5fb671f
Piotr Paczkowski (trzeci.eu) <kontakt@trzeci.eu>
2019-05-29 16:41:26 +0200

    Fix problem of incorrectly set isChanged flag on not changed touches
    Old implementation uses `Touch` instances of touching points to store `changed` flag. Which is problematic as those instances can recirculate. This PR makes sure that we clean old flags before handling new touches.

--- src/library_html5.js
+++ src/library_html5.js
@@ -1849,6 +1849,7 @@ var LibraryJSEvents = {
       var touches = {};
       for(var i = 0; i < e.touches.length; ++i) {
         var touch = e.touches[i];
+        touch.changed = false;
         touches[touch.identifier] = touch;
       }
       for(var i = 0; i < e.changedTouches.length; ++i) {
