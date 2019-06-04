b4659a0ba9b0bcfb8c9d3e4682b9c33b8d6b7a9b
Alon Zakai (kripken) <alonzakai@gmail.com>
2019-06-04 07:10:32 -0700

    disable another test that needs fixing

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -3536,6 +3536,7 @@ window.close = function() {
 
   # Test that the emscripten_ atomics api functions work.
   @requires_threads
+  @no_wasm_backend('TODO - fix final pthreads tests (#8718)')
   def test_pthread_atomics(self):
     self.btest(path_from_root('tests', 'pthread', 'test_pthread_atomics.cpp'), expected='0', args=['-s', 'TOTAL_MEMORY=64MB', '-O3', '-s', 'USE_PTHREADS=1', '-s', 'PTHREAD_POOL_SIZE=8'])
 
