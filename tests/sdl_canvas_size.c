7eb8a5c5dcab7297f07e2d90ca46e7bfc0885530
Tim Lander <tim57282@hotmail.com>
2019-06-04 10:00:12 +0800

    Replace uses of deprecated canvas_size functions

--- tests/sdl_canvas_size.c
+++ tests/sdl_canvas_size.c
@@ -29,15 +29,15 @@ int main(int argc, char *argv[])
     screen = SDL_SetVideoMode( 0, 0, 16, SDL_OPENGL ); // *changed*
 
     // Test 2: Check that getting current canvas size works.
-    int w, h, fs;
-    emscripten_get_canvas_size(&w, &h, &fs);
+    int w, h;
+    emscripten_get_canvas_size("#canvas", &w, &h);
     printf("w:%d,h:%d\n", w,h);
     assert(w == 700);
     assert(h == 200);
 
     // Test 3: Check that resizing the canvas works as well.
-    emscripten_set_canvas_size(640, 480);
-    emscripten_get_canvas_size(&w, &h, &fs);
+    emscripten_set_canvas_size("#canvas", 640, 480);
+    emscripten_get_canvas_size("#canvas", &w, &h);
     printf("w:%d,h:%d\n", w,h);
     assert(w == 640);
     assert(h == 480);
