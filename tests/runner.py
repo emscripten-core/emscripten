da20be0c80cfbfb3a448b64c5a1f9bf08ce27e57
Alon Zakai <azakai@google.com>
2019-06-12 14:50:07 -0700

    fix

--- tests/runner.py
+++ tests/runner.py
@@ -1396,7 +1396,7 @@ class BrowserCore(RunnerCore):
           print('[unresponsive tests: %d, vs %d]' % (BrowserCore.unresponsive_tests, BrowserCore.responsive_tests))
           BrowserCore.log_browser_harness_status()
           if BrowserCore.unresponsive_tests == 1 and BrowserCore.responsive_tests == 0:
-            print('[very first test was unresponsive, so maybe the browser just failed to start up; try to restart it.'])
+            print('[very first test was unresponsive, so maybe the browser just failed to start up; try to restart it]')
             BrowserCore.restart_browser_harness()
         else:
           BrowserCore.responsive_tests += 1
