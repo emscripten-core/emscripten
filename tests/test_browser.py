b3f068e23b5cc09055e9651c6de56070f9fdfff1
Alon Zakai <azakai@google.com>
2019-07-01 14:26:55 -0700

    Bysyncify: fix invoke_*s (#8895)
    An invoke_* call is basically the same as an indirect call, it just goes out into JS. The bysyncify analysis needs to be aware of that so it doesn't miss things.
    
    Fixes bysyncify on code that uses invokes, which includes longjmp and exceptions.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -3225,6 +3225,15 @@ window.close = function() {
       print(opts)
       self.btest('browser/async_virtual_2.cpp', '1', args=['-O' + str(opts), '-s', 'ASSERTIONS=1', '-s', 'SAFE_HEAP=1', '-profiling'] + self.get_async_args())
 
+  # Test async sleeps in the presence of invoke_* calls, which can happen with
+  # longjmp or exceptions.
+  @parameterized({
+    'O0': ([],), # noqa
+    'O3': (['-O3'],), # noqa
+  })
+  def test_async_longjmp(self, args):
+    self.btest('browser/async_longjmp.cpp', '2', args=args + self.get_async_args())
+
   @no_wasm_backend('emterpretify, with emterpreter-specific error logging')
   def test_emterpreter_async_bad(self):
     for opts in [0, 3]:
