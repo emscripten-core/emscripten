a0eac7c1b1560ce197606fde9586e9578752ee26
Guanzhong Chen <gzchen@google.com>
2019-07-01 14:42:46 -0700

    Only report null-pointer-dereference in first pag

--- system/lib/compiler-rt/lib/asan/asan_errors.cc
+++ system/lib/compiler-rt/lib/asan/asan_errors.cc
@@ -468,8 +468,13 @@ ErrorGeneric::ErrorGeneric(u32 tid, uptr pc_, uptr bp_, uptr sp_, uptr addr,
       if (far_from_bounds) scariness.Scare(10, "far-from-bounds");
     }
 #if SANITIZER_EMSCRIPTEN
-    else if (AddrIsInShadow(addr)) {
+    // If address is in the first page (64 KB), then it is likely that the
+    // access is a result of a null pointer dereference.
+    else if (addr < 65536) {
       bug_descr = "null-pointer-dereference";
+      scariness.Scare(25, bug_descr);
+    } else if (AddrIsInShadow(addr)) {
+      bug_descr = "shadow-access";
       scariness.Scare(25, bug_descr);
     }
 #endif
