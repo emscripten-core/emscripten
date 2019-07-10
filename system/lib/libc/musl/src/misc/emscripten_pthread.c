c882a9970341164f964eb2aa5b1abdb228dcf85e
Thomas Lively <tlively@google.com>
2019-07-10 16:15:18 -0700

    Fix MetaDCE test

--- system/lib/libc/musl/src/misc/emscripten_pthread.c
+++ system/lib/libc/musl/src/misc/emscripten_pthread.c
@@ -7,11 +7,13 @@ static struct pthread __main_pthread;
 pthread_t pthread_self(void) {
     return &__main_pthread;
 }
-#endif
+#endif // !__EMSCRIPTEN_PTHREADS__
 
 // Needs to be called after PThread.initRuntime, which in turn needs to be
 // called after constructors have run and memory is initialized.
+#if __EMSCRIPTEN_PTHREADS__
 EMSCRIPTEN_KEEPALIVE
 void __emscripten_pthread_data_constructor(void) {
     pthread_self()->locale = &libc.global_locale;
 }
+#endif // __EMSCRIPTEN_PTHREADS__
