b1defb8aaae19c822d6137f75537623a5f7f2ac3
Tim Lander <tim57282@hotmail.com>
2019-07-11 11:05:51 +0800

    Add missing semicolon.

--- src/shell.js
+++ src/shell.js
@@ -88,7 +88,7 @@ if (Module['ENVIRONMENT']) {
 // In MODULARIZE mode _scriptDir needs to be captured already at the very top of the page immediately when the page is parsed, so it is generated there
 // before the page load. In non-MODULARIZE modes generate it here.
 #if EXPORT_ES6
-var _scriptDir = import.meta.url
+var _scriptDir = import.meta.url;
 #else
 var _scriptDir = (typeof document !== 'undefined' && document.currentScript) ? document.currentScript.src : undefined;
 #endif
