510018aff04aa7104096ba17bff6a74480ccf4e1
Alon Zakai <azakai@google.com>
2019-07-11 15:38:05 -0700

    fix

--- src/library_exceptions_stub.js
+++ src/library_exceptions_stub.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 The Emscripten Authors.  All rights reserved.
+ * Copyright 2019 The Emscripten Authors.  All rights reserved.
  * Emscripten is available under two separate licenses, the MIT license and the
  * University of Illinois/NCSA Open Source License.  Both these licenses can be
  * found in the LICENSE file.
