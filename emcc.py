bf11b3d3d69ebbc8c3b98ef87762aa319e88028a
Tim Lander <tim57282@hotmail.com>
2019-07-15 09:51:27 +0800

    Update comment

--- emcc.py
+++ emcc.py
@@ -2945,6 +2945,8 @@ function(%(EXPORT_NAME)s) {
       # after document.currentScript is gone, so we save it.
       # (when MODULARIZE_INSTANCE, an instance is created
       # immediately anyhow, like in non-modularize mode)
+      # In EXPORT_ES6 + USE_PTHREADS the 'thread' is actually an ES6 module webworker running in strict mode,
+      # so doesn't have access to 'document'. In this case use 'import.meta' instead. 
       if shared.Settings.EXPORT_ES6 and shared.Settings.USE_PTHREADS:
         script_url = "import.meta.url"
       else:
