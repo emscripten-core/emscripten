emscripten-core/emscripten

commit c28a629cd5ddc9a4ec5f89ef7a55e8f78a808b31

Simplify and fix pthreads initialization code (#8973)
Move all that logic into emscripten_pthread.c, and call necessary things from there.

Use a constructor priority to ensure this is called at the right time: we must be after the bulk memory initialization of segments, but before any userland constructors that may use pthreads code, so use the lowest priority that is reserved for the system.

system/lib/libc/musl/src/misc/emscripten_pthread.c
@@ -10,13 +10,20 @@ pthread_t pthread_self(void) {
 #endif // !__EMSCRIPTEN_PTHREADS__
 
 #if __EMSCRIPTEN_PTHREADS__
-// Needs to be called after PThread.initRuntime, which in turn needs to be
-// called after constructors have run and memory is initialized.
+// In pthreads, we must initialize the runtime at the proper time, which
+// is after memory is initialized and before any userland global ctors.
+// We must also keep this function alive so it is always called; without
+// pthreads, if pthread_self is used then this file will be included,
+// and if not then it's fine to not have this.
+EM_JS(void, initPthreadsJS, (void), {
+  PThread.initRuntime();
+})
 EMSCRIPTEN_KEEPALIVE
-#else
-// Without threads, no reason not to just run this from C.
-__attribute__((constructor))
-#endif // __EMSCRIPTEN_PTHREADS__
+#endif
+__attribute__((constructor(100))) // This must run before any userland ctors
 void __emscripten_pthread_data_constructor(void) {
-    pthread_self()->locale = &libc.global_locale;
+#if __EMSCRIPTEN_PTHREADS__
+  initPthreadsJS();
+#endif
+  pthread_self()->locale = &libc.global_locale;
 }

src/library_pthread.js
@@ -24,8 +24,6 @@ var LibraryPThread = {
       // Global constructors trying to access this value will read the wrong value, but that is UB anyway.
       __register_pthread_ptr(PThread.mainThreadBlock, /*isMainBrowserThread=*/!ENVIRONMENT_IS_WORKER, /*isMainRuntimeThread=*/1);
       _emscripten_register_main_browser_thread_id(PThread.mainThreadBlock);
-      // Initialize data on main pthread now that we have initialized the thread id
-      ___emscripten_pthread_data_constructor();
     },
     initMainThreadBlock: function() {
       if (ENVIRONMENT_IS_PTHREAD) return undefined;

src/preamble.js
@@ -510,9 +510,6 @@ function initRuntime() {
   runtimeInitialized = true;
   {{{ getQuoted('ATINITS') }}}
   callRuntimeCallbacks(__ATINIT__);
-#if USE_PTHREADS
-  PThread.initRuntime();
-#endif
 }
 
 function preMain() {

commit fbdb43c4ed1ec5e19413769c83f94d56090ebad2

Fix cache testing on CI, and fix a regression from #8973 (#8994)
The emsdk ships libc and a few other libs, and we used those, instead of building libc here - so we missed a regression that was only caught later on releases CI. I'm surprised we didn't hit more problems earlier, actually...

The regression was that we added a constructor priority in pthreads, and apparently that keeps it not just alive but separate from other ctors, which adds an extra function to the number. I refactored that code to make it clear what we do in the pthreads and non-pthreads cases, and made us only use the priority when using pthreads.

.circleci/config.yml
@@ -345,6 +345,9 @@ jobs:
             cd -
             echo "final .emscripten:"
             cat ~/.emscripten
+            # Remove any system libs the emsdk installed: we want to rebuild
+            # them all from source here, to check whether new changes work.
+            rm -Rf ~/.emscripten_cache
       - run:
           name: embuilder build ALL
           command: |

system/lib/libc/musl/src/misc/emscripten_pthread.c
@@ -5,7 +5,12 @@
 #if !__EMSCRIPTEN_PTHREADS__
 static struct pthread __main_pthread;
 pthread_t pthread_self(void) {
-    return &__main_pthread;
+  return &__main_pthread;
+}
+
+__attribute__((constructor))
+void __emscripten_pthread_data_constructor(void) {
+  pthread_self()->locale = &libc.global_locale;
 }
 #endif // !__EMSCRIPTEN_PTHREADS__
 
@@ -18,12 +23,11 @@ pthread_t pthread_self(void) {
 EM_JS(void, initPthreadsJS, (void), {
   PThread.initRuntime();
 })
+
 EMSCRIPTEN_KEEPALIVE
-#endif
 __attribute__((constructor(100))) // This must run before any userland ctors
 void __emscripten_pthread_data_constructor(void) {
-#if __EMSCRIPTEN_PTHREADS__
   initPthreadsJS();
-#endif
   pthread_self()->locale = &libc.global_locale;
 }
+#endif

tests/test_other.py
@@ -8008,7 +8008,7 @@ int main() {
 
   @parameterized({
     'normal': (['-O2'], 32, ['abort'], ['waka'], 186423,  29,  38, 539), # noqa
-    'enumated_function_pointers':
+    'emulated_function_pointers':
               (['-O2', '-s', 'EMULATED_FUNCTION_POINTERS=1'],
                         32, ['abort'], ['waka'], 186423,  29,  39, 519), # noqa
   })