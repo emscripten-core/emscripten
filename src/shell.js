4684d9c320fae0b6f19e8e314cbcb76f0912b4ca
Alon Zakai (kripken) <alonzakai@gmail.com>
2019-07-20 11:48:10 -0700

    fix

--- src/shell.js
+++ src/shell.js
@@ -107,7 +107,7 @@ function locateFile(path) {
 }
 
 // Hooks that are implemented differently in different runtime environments.
-var read,
+var read_,
     readAsync,
     readBinary,
     setWindowTitle;
@@ -126,7 +126,7 @@ if (ENVIRONMENT_IS_NODE) {
   var nodeFS;
   var nodePath;
 
-  read = function shell_read(filename, binary) {
+  read_ = function shell_read(filename, binary) {
     var ret;
 #if SUPPORT_BASE64_EMBEDDING
     ret = tryParseAsDataURI(filename);
@@ -143,7 +143,7 @@ if (ENVIRONMENT_IS_NODE) {
   };
 
   readBinary = function readBinary(filename) {
-    var ret = read(filename, true);
+    var ret = read_(filename, true);
     if (!ret.buffer) {
       ret = new Uint8Array(ret);
     }
@@ -194,7 +194,7 @@ if (ENVIRONMENT_IS_SHELL) {
 #endif
 
   if (typeof read != 'undefined') {
-    read = function shell_read(f) {
+    read_ = function shell_read(f) {
 #if SUPPORT_BASE64_EMBEDDING
       var data = tryParseAsDataURI(f);
       if (data) {
@@ -264,7 +264,7 @@ if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) {
 #endif
 #endif
 
-  read = function shell_read(url) {
+  read_ = function shell_read(url) {
 #if SUPPORT_BASE64_EMBEDDING
     try {
 #endif
