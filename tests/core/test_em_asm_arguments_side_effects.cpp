6eefed144fe1984e4fbd9cf5b010d18680a61c29
Egor Suvorov <esuvorov@think-cell.com>
2019-07-23 10:16:41 +0200

    core/test_em_asm_arguments_side_effects: add more checks

--- tests/core/test_em_asm_arguments_side_effects.cpp
+++ tests/core/test_em_asm_arguments_side_effects.cpp
@@ -9,10 +9,11 @@
 int main(int argc, char **argv) {
   int counter_1 = 3, counter_2 = 4;
   // https://github.com/emscripten-core/emscripten/issues/9030
+  printf("counter_1=%d, counter_2=%d\n", counter_1, counter_2);
   int result = EM_ASM_INT({
      return $0 * 10 + $1;
   }, counter_1++, counter_2++);
-  printf("counter_1=3, counter_2=4\n");
   printf("counter_1++ * 10 + counter_2++=%d\n", result);
+  printf("counter_1=%d, counter_2=%d\n", counter_1, counter_2);
   return 0;
 }
