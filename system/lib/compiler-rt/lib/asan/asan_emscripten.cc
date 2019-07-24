7f2a33b4826b79ce7e26a1896394e3b4b1d24d94
Guanzhong Chen <gzchen@google.com>
2019-07-24 11:03:34 -0700

    Override pthread_create for ASan

--- system/lib/compiler-rt/lib/asan/asan_emscripten.cc
+++ system/lib/compiler-rt/lib/asan/asan_emscripten.cc
@@ -1,6 +1,8 @@
+#include "asan_interceptors.h"
 #include "asan_internal.h"
 #include "asan_mapping.h"
 #include "asan_poisoning.h"
+#include "asan_stack.h"
 #include "asan_thread.h"
 
 #if SANITIZER_EMSCRIPTEN
@@ -44,6 +46,63 @@ void AsanTSDInit(void (*destructor)(void *tsd)) {
 }
 
 void PlatformTSDDtor(void *tsd) { UNREACHABLE(__func__); }
+
+extern "C" {
+  void *emscripten_builtin_malloc(size_t size);
+  void emscripten_builtin_free(void *memory);
+  int emscripten_builtin_pthread_create(void *thread, void *attr,
+                                        void *(*callback)(void *), void *arg);
+  int pthread_attr_getdetachstate(void *attr, int *detachstate);
+}
+
+struct ThreadStartParam {
+  atomic_uintptr_t t;
+  atomic_uintptr_t is_registered;
+};
+
+static thread_return_t THREAD_CALLING_CONV asan_thread_start(void *arg) {
+  atomic_uintptr_t *param = reinterpret_cast<atomic_uintptr_t *>(arg);
+  AsanThread *t = nullptr;
+  while ((t = reinterpret_cast<AsanThread *>(
+              atomic_load(param, memory_order_acquire))) == nullptr)
+    internal_sched_yield();
+  emscripten_builtin_free(param);
+  SetCurrentThread(t);
+  return t->ThreadStart(GetTid(), nullptr);
+}
+
+INTERCEPTOR(int, pthread_create, void *thread,
+    void *attr, void *(*start_routine)(void*), void *arg) {
+  EnsureMainThreadIDIsCorrect();
+  // Strict init-order checking is thread-hostile.
+  if (flags()->strict_init_order)
+    StopInitOrderChecking();
+  GET_STACK_TRACE_THREAD;
+  int detached = 0;
+  if (attr)
+    pthread_attr_getdetachstate(attr, &detached);
+  atomic_uintptr_t *param = (atomic_uintptr_t *)
+      emscripten_builtin_malloc(sizeof(atomic_uintptr_t));
+  atomic_store(param, 0, memory_order_relaxed);
+  int result;
+  {
+    // Ignore all allocations made by pthread_create: thread stack/TLS may be
+    // stored by pthread for future reuse even after thread destruction, and
+    // the linked list it's stored in doesn't even hold valid pointers to the
+    // objects, the latter are calculated by obscure pointer arithmetic.
+#if CAN_SANITIZE_LEAKS
+    __lsan::ScopedInterceptorDisabler disabler;
+#endif
+    result = REAL(pthread_create)(thread, attr, asan_thread_start, param);
+  }
+  if (result == 0) {
+    u32 current_tid = GetCurrentTidOrInvalid();
+    AsanThread *t =
+        AsanThread::Create(start_routine, arg, current_tid, &stack, detached);
+    atomic_store(param, reinterpret_cast<uptr>(t), memory_order_release);
+  }
+  return result;
+}
 
 } // namespace __asan
 
