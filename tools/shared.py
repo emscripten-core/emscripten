9f936606976d1a2e6ccccdd122a0280d78294287
Thomas Lively <tlively@google.com>
2019-08-02 15:25:09 -0700

    Stop passive --passive-segments to lld
    LLD's default behavior has been updated to do the right thing based on
    the presence of the --shared-memory flag, so --passive-segments is no
    longer necessary. Removing it here allows it to be removed from LLD.

--- tools/shared.py
+++ tools/shared.py
@@ -1848,7 +1848,6 @@ class Building(object):
 
     if Settings.USE_PTHREADS:
       cmd.append('--shared-memory')
-      cmd.append('--passive-segments')
 
     for a in Building.llvm_backend_args():
       cmd += ['-mllvm', a]
