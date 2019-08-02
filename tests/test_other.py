2afafbff4e6d4d5b70b4a5734042585d3547022b
Alon Zakai <azakai@google.com>
2019-08-02 13:24:31 -0700

    INCOMING_MODULE_JS_API: Optimize setStatus, preInit, locateFile (#9125)
    Shrinks code size by almost 5%.

--- tests/test_other.py
+++ tests/test_other.py
@@ -9532,7 +9532,7 @@ int main () {
     # Changing this option to [] should decrease code size.
     self.assertLess(changed, normal)
     # Check an absolute code size as well, with some slack.
-    self.assertLess(abs(changed - 6768), 100)
+    self.assertLess(abs(changed - 6483), 100)
 
   def test_llvm_includes(self):
     self.build('#include <stdatomic.h>', self.get_dir(), 'atomics.c')
