c45d3b545529ae5c6abfe93d7639a5aeb4caabc1
Alon Zakai <azakai@google.com>
2019-08-06 16:53:25 -0700

    Fix console handling in SHELL environment. (#9161)
    In SpiderMonkey console.log exists, but not warn or error, so create them all to be safe.
    
    On V8, prefer printErr to console.warn since it prints to stderr.

--- src/shell.js
+++ src/shell.js
@@ -233,12 +233,11 @@ if (ENVIRONMENT_IS_SHELL) {
     };
   }
 
-  if (typeof console !== 'undefined') {
-    // Support odd shell environments that lack console.* but have other stuff.
-    console = {
-      log: print,
-      warn: typeof printErr !== 'undefined' ? printErr : print
-    }
+  if (typeof print !== 'undefined') {
+    // Prefer to use print/printErr where they exist, as they usually work better.
+    if (typeof console === 'undefined') console = {};
+    console.log = print;
+    console.warn = console.error = typeof printErr !== 'undefined' ? printErr : print;
   }
 } else
 #endif // ENVIRONMENT_MAY_BE_SHELL
@@ -346,10 +345,6 @@ if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) {
 
 // Set up the out() and err() hooks, which are how we can print to stdout or
 // stderr, respectively.
-// If the user provided Module.print or printErr, use that. Otherwise,
-// console.log is checked first, as 'print' on the web will open a print dialogue
-// printErr is preferable to console.warn (works better in shells)
-// bind(console) is necessary to fix IE/Edge closed dev tools panel behavior.
 {{{ makeModuleReceiveWithVar('out', 'print',    'console.log.bind(console)',  true) }}}
 {{{ makeModuleReceiveWithVar('err', 'printErr', 'console.warn.bind(console)', true) }}}
 
