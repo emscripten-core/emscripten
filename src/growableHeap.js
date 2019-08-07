cc062985e6661ffe37a5ab16122508661a9b5a2b
Jukka Jylänki <jujjyl@gmail.com>
2019-08-07 10:41:27 +0300

    Take into account pthreads growable heap resize

--- src/growableHeap.js
+++ src/growableHeap.js
@@ -2,93 +2,80 @@
 // must be updated.
 function GROWABLE_HEAP_STORE_I8(ptr, value) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   HEAP8[ptr] = value;
 }
 function GROWABLE_HEAP_STORE_I16(ptr, value) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   HEAP16[ptr >> 1] = value;
 }
 function GROWABLE_HEAP_STORE_I32(ptr, value) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   HEAP32[ptr >> 2] = value;
 }
 function GROWABLE_HEAP_STORE_F32(ptr, value) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   HEAPF32[ptr >> 2] = value;
 }
 function GROWABLE_HEAP_STORE_F64(ptr, value) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   HEAPF64[ptr >> 3] = value;
 }
 
 function GROWABLE_HEAP_LOAD_I8(ptr) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   return HEAP8[ptr];
 }
 function GROWABLE_HEAP_LOAD_U8(ptr) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   return HEAPU8[ptr];
 }
 function GROWABLE_HEAP_LOAD_I16(ptr) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   return HEAP16[ptr >> 1];
 }
 function GROWABLE_HEAP_LOAD_U16(ptr) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   return HEAPU16[ptr >> 1];
 }
 function GROWABLE_HEAP_LOAD_I32(ptr) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   return HEAP32[ptr >> 2];
 }
 function GROWABLE_HEAP_LOAD_U32(ptr) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   return HEAPU32[ptr >> 2];
 }
 function GROWABLE_HEAP_LOAD_F32(ptr) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   return HEAPF32[ptr >> 2];
 }
 function GROWABLE_HEAP_LOAD_F64(ptr) {
   if (wasmMemory.buffer != buffer) {
-    buffer = wasmMemory.buffer;
-    updateGlobalBufferViews();
+    updateGlobalBufferAndViews(wasmMemory.buffer);
   }
   return HEAPF64[ptr >> 3];
 }
