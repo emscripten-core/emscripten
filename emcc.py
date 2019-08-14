emscripten-core/emscripten

commit f9e6cfe8d7151d52bc036020541c0b24f169e644

[ES6] Only use import.meta when strictly necessary (#8940)
A webworker ES6 module doesn't have access to 'document.currentScript'. 'import.meta' was the only way I could see to achieve that functionality.

Fixes #8729.

Since this appears to fix the issue for everyone who commented on #8729, and no one proposed any better ideas, this is my suggestion.

It would be nice to use import.meta everywhere, and browsers seem to have good support, however the tooling appears to be lagging a bit behind.

emcc.py
@@ -2991,7 +2991,9 @@ function(%(EXPORT_NAME)s) {
       # after document.currentScript is gone, so we save it.
       # (when MODULARIZE_INSTANCE, an instance is created
       # immediately anyhow, like in non-modularize mode)
-      if shared.Settings.EXPORT_ES6:
+      # In EXPORT_ES6 + USE_PTHREADS the 'thread' is actually an ES6 module webworker running in strict mode,
+      # so doesn't have access to 'document'. In this case use 'import.meta' instead.
+      if shared.Settings.EXPORT_ES6 and shared.Settings.USE_PTHREADS:
         script_url = "import.meta.url"
       else:
         script_url = "typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined"

src/shell.js
@@ -89,7 +89,11 @@ if (Module['ENVIRONMENT']) {
 #if USE_PTHREADS && (!MODULARIZE || MODULARIZE_INSTANCE)
 // In MODULARIZE mode _scriptDir needs to be captured already at the very top of the page immediately when the page is parsed, so it is generated there
 // before the page load. In non-MODULARIZE modes generate it here.
+#if EXPORT_ES6
+var _scriptDir = import.meta.url;
+#else
 var _scriptDir = (typeof document !== 'undefined' && document.currentScript) ? document.currentScript.src : undefined;
+#endif
 #endif
 
 // `/` should be present at the end if `scriptDirectory` is not empty

commit aedb68072f01d4d424de72058b09507c037254f8

Add a specific flag to enable / disable the use of 'import.meta.url'
    This fixes the problem reported by #9233 and by #8729 while fixing the
    regression bug causes by #8940

AUTHORS
@@ -422,3 +422,4 @@ a license to everyone to use it as detailed in LICENSE.)
 * Egor Suvorov <esuvorov@think-cell.com> (copyright owned by think-cell Software GmbH)
 * James Kuszmaul <jabukuszmaul@gmail.com>
 * Wei Mingzhi <whistler_wmz@users.sourceforge.net>
+* Adrien Devresse <adev@adev.name>

emcc.py
@@ -3039,7 +3039,7 @@ function(%(EXPORT_NAME)s) {
       # immediately anyhow, like in non-modularize mode)
       # In EXPORT_ES6 + USE_PTHREADS the 'thread' is actually an ES6 module webworker running in strict mode,
       # so doesn't have access to 'document'. In this case use 'import.meta' instead.
-      if shared.Settings.EXPORT_ES6 and shared.Settings.USE_PTHREADS:
+      if shared.Settings.EXPORT_ES6 and shared.Settings.USE_ES6_IMPORT_META:
         script_url = "import.meta.url"
       else:
         script_url = "typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined"

src/settings.js
@@ -937,6 +937,11 @@ var SEPARATE_ASM_MODULE_NAME = '';
 // be enabled for ES6 exports.
 var EXPORT_ES6 = 0;
 
+// Use the ES6 Module relative import feature 'import.meta.url'
+// to auto-detect WASM Module path.
+// It might not be supported on old browsers / toolchains
+var USE_ES6_IMPORT_META = 1;
+
 // If 1, will just time how long main() takes to execute, and not print out
 // anything at all whatsoever. This is useful for benchmarking.
 var BENCHMARK = 0;