2ed96171a0c3b6d2291b07bdaae7c017e0c39fe6
Petr Penzin <41971413+penzn@users.noreply.github.com>
2019-08-27 16:09:08 -0700

    Include guard for wasm-simd128.h (#9299)

--- system/include/wasm_simd128.h
+++ system/include/wasm_simd128.h
@@ -1,6 +1,15 @@
 /*
-WebAssembly SIMD128 Intrinsics
-*/
+ * Copyright 2019 The Emscripten Authors.  All rights reserved.
+ * Emscripten is available under two separate licenses, the MIT license and the
+ * University of Illinois/NCSA Open Source License.  Both these licenses can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * WebAssembly SIMD128 Intrinsics
+ */
+
+#pragma once
 
 #include <stdint.h>
 #include <stdbool.h>
