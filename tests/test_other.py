9f07b63022d15c66a741eb0e9de2d0fc81a15f40
Jacob Gravelle <jgravelle@google.com>
2019-08-29 18:12:50 -0700

    Ignore function count in libcxx_metadce (#9351)

--- tests/test_other.py
+++ tests/test_other.py
@@ -8081,7 +8081,7 @@ int main() {
   @no_fastcomp()
   def test_binaryen_metadce_cxx(self):
     # test on libc++: see effects of emulated function pointers
-    self.run_metadce_test('hello_libcxx.cpp', ['-O2'], 37, [], ['waka'], 226582, 21, 33, 750) # noqa
+    self.run_metadce_test('hello_libcxx.cpp', ['-O2'], 37, [], ['waka'], 226582, 21, 33, None) # noqa
 
   @parameterized({
     'normal': (['-O2'], 36, ['abort'], ['waka'], 186423,  29,  38, 540), # noqa
