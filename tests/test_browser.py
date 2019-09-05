9ec2778661a2442bf1fda0a08b417c46f0431a57
Tayeb Al Karim <tay@google.com>
2019-09-05 14:59:28 -0400

    Change parameter for prewarm to a flag instead of a count. You can't join a newly created thread!

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -3646,14 +3646,12 @@ window.close = function() {
     test([])
     test(['-O3'])
     test(['-s', 'MODULARIZE_INSTANCE=1'])
-    test(['-s', 'PREWARM_PTHREAD_POOL_WORKERS_SIZE=4'])
-    test(['-s', 'PREWARM_PTHREAD_POOL_WORKERS_SIZE=8'])
-    test(['-s', 'PREWARM_PTHREAD_POOL_WORKERS_SIZE=12'])
+    test(['-s', 'PREWARM_POOL_WORKERS=1'])
 
   # Test that preallocating worker threads work.
   @requires_threads
   def test_pthread_preallocates_workers(self):
-    self.btest(path_from_root('tests', 'pthread', 'test_pthread_preallocates_workers.cpp'), expected='6765', args=['-O3', '-s', '-s', 'USE_PTHREADS=1', '-s', 'PTHREAD_POOL_SIZE=8', '-s', 'PREWARM_PTHREAD_POOL_WORKERS_SIZE=8', '-s', 'PTHREADS_DEBUG=1'])
+    self.btest(path_from_root('tests', 'pthread', 'test_pthread_preallocates_workers.cpp'), expected='6765', args=['-O3', '-s', '-s', 'USE_PTHREADS=1', '-s', 'PTHREAD_POOL_SIZE=4', '-s', 'PREWARM_POOL_WORKERS=1', '-s', 'PTHREADS_DEBUG=1'])
 
   # Tests the -s PROXY_TO_PTHREAD=1 option.
   @requires_threads
