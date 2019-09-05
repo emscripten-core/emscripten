233271a3b8e39e014ba4c55592f8ef5671659908
Tayeb Al Karim <tay@google.com>
2019-09-05 15:04:48 -0400

    Update test due to new flag.

--- tests/pthread/test_pthread_preallocates_workers.cpp
+++ tests/pthread/test_pthread_preallocates_workers.cpp
@@ -49,12 +49,12 @@ int main()
     return 0;
   }
 
-  // assert(EM_ASM_INT(return PThread.runningWorkers.length) == 0);
-  // // This test should be run with a prewarmed pool of size 8. 4 of the
-  // // preallocated workers should have been used already.
-  // assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 4);
-  // // This test should be run with a prepopulated pool of size 4.
-  // assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 4);
+  assert(EM_ASM_INT(return PThread.runningWorkers.length) == 0);
+  // This test should be run with a prewarmed pool of size 4. all 4 of the
+  // preallocated workers should have been used already.
+  assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 0);
+  // This test should be run with a prepopulated pool of size 4.
+  assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 4);
 
   int n = 20;
   EM_ASM(out('Main: Spawning thread to compute fib('+$0+')...'), n);
@@ -62,27 +62,26 @@ int main()
   EM_ASM(out('Main: Waiting for thread to join.'));
   int result = 0;
 
-  // assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 4);
-  // assert(EM_ASM_INT(return PThread.runningWorkers.length) == 1);
-  // assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 3);
+  assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 0);
+  assert(EM_ASM_INT(return PThread.runningWorkers.length) == 1);
+  assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 3);
 
   int s = pthread_join(threads[0], (void**)&result);
   assert(s == 0);
   EM_ASM(out('Main: Thread joined with result: '+$0+'.'), result);
 
-  // assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 0);
-  // assert(EM_ASM_INT(return PThread.runningWorkers.length) == 0);
-  // assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 4);
+  assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 0);
+  assert(EM_ASM_INT(return PThread.runningWorkers.length) == 0);
+  assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 4);
 
   for (int i = 0; i < 4; ++i) {
     EM_ASM(out('Main: Spawning thread '+$0+' to compute fib('+$1+')...'), i, n);
     CreateThread(i);
   }
-  sleep(5);
 
-  // assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 3);
-  // assert(EM_ASM_INT(return PThread.runningWorkers.length) == 5);
-  // assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 0);
+  assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 0);
+  assert(EM_ASM_INT(return PThread.runningWorkers.length) == 4);
+  assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 0);
 
   // Joining them all should result in 4 in the unused state.
   for (int i = 0; i < 4; ++i) {
@@ -91,9 +90,9 @@ int main()
     assert(s == 0);
   }
 
-  // assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 3);
-  // assert(EM_ASM_INT(return PThread.runningWorkers.length) == 0);
-  // assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 5);
+  assert(EM_ASM_INT(return PThread.preallocatedWorkers.length) == 0);
+  assert(EM_ASM_INT(return PThread.runningWorkers.length) == 0);
+  assert(EM_ASM_INT(return PThread.unusedWorkers.length) == 4);
 
 #ifdef REPORT_RESULT
   REPORT_RESULT(result);
