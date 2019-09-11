cc4a0b98560083747d9580756c1054d760ae2467
Alon Zakai <azakai@google.com>
2019-09-11 16:36:26 -0700

    Add error handling in ASSERTIONS for EM_JS errors with ASYNCIFY_IMPORTS (#9358)
    This matches what we already have for js library functions.
    
    This also disables wasm/JS import minification when ASSERTIONS are on. That makes this much simpler to write (no need to worry about names changing for these assertion checks), and really it seems better anyhow - in ASSERTIONS code size is not crucial, and that's the only purpose for that minification. And it makes debugging such builds easier, which is usually the point of ASSERTIONS.
    
    Also add more tests, including response file stuff.

--- tools/shared.py
+++ tools/shared.py
@@ -2465,7 +2465,7 @@ class Building(object):
         # If we are building with DECLARE_ASM_MODULE_EXPORTS=0, we must *not* minify the exports from the wasm module, since in DECLARE_ASM_MODULE_EXPORTS=0 mode, the code that
         # reads out the exports is compacted by design that it does not have a chance to unminify the functions. If we are building with DECLARE_ASM_MODULE_EXPORTS=1, we might
         # as well minify wasm exports to regain some of the code size loss that setting DECLARE_ASM_MODULE_EXPORTS=1 caused.
-        if Settings.EMITTING_JS and not Settings.AUTODEBUG:
+        if Settings.EMITTING_JS and not Settings.AUTODEBUG and not Settings.ASSERTIONS:
           js_file = Building.minify_wasm_imports_and_exports(js_file, wasm_file, minify_whitespace=minify_whitespace, minify_exports=Settings.DECLARE_ASM_MODULE_EXPORTS, debug_info=debug_info)
     return js_file
 
