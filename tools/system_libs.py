39a4975203e35d97f418ec4cbaeb33e5a785df72
Heejin Ahn <aheejin@gmail.com>
2019-09-13 15:25:15 -0700

    Support symbols files for different options (#9433)
    This aims at automating symbol generation a bit more and supporting
    different symbols files for different set of build options. For example,
    a library can be built with different set of options: such as with `-mt`
    and `-noexcept`, only `-mt`, only `-noexcept`, or with none of them. And
    libraries built with those different options can expose different set of
    symbols. Currently this kind of info is hard-coded into the code.
    
    After this PR, all system libraries read symbols file in
    `system/lib/symbols/wasm/library_filename.symbols` when using the wasm
    backend, and `system/lib/symbols/asmjs/library_filename.symbols` when
    using fastcomp. For example, if the filename of a library we are
    considering to link is `libc++-mt.a`, it will search for
    `system/lib/symbols/wasm/libc++-mt.symbols`. And if it doesn't exist, it
    will try the default library name without any options:
    `system/lib/symbols/wasm/libc++.symbols`.
    
    `tools/update_symbols.py` generates all symbols files automatically. It
    does not have `--asmjs` anymore: it will read `.emscripten` just as the
    normal emcc toolchain does. This script has `target_libs` variable, in
    which we only list several libraries that need symbol files. (This set
    of libraries is the libraries we currently have `.symbols` files.)
    This script builds all possible combination of those libraries if they
    don't exist in cache, and run `llvm-nm` on them, and generate files
    within `system/lib/symbols/wasm` or `system/lib/symbols/asmjs`
    directories. As mentioned above, to prevent proliferation of symbols
    files with same contents, if symbols files generated with different
    options are the same as the 'default' symbol file without any suffices,
    those symbol files will be deleted and the linking algorithm will fall
    back to the default symbol files for the library.
    
    One of the reasons I did this was, when we add the new wasm exception
    handling, the set of symbols exposed by different options for libraries
    such as libcxxabi will certainly be different, and I hoped to make it
    more general.

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -197,11 +197,8 @@ class Library(object):
   # For dynamic values, override `get_depends()` instead.
   depends = []
 
-  # A set of symbols that this library exports. If this library is intended to
-  # pulled in via undefined symbols, this should be overridden with a set
+  # A set of symbols that this library exports. This will be set with a set
   # returned by `read_symbols`.
-  # If this library is to be linked in manually, leave this empty.
-  # For dynamic values, override `get_symbols()` instead.
   symbols = set()
 
   # A list of symbols that must be exported to keep the JavaScript
@@ -259,6 +256,23 @@ class Library(object):
     if not self.name:
       raise NotImplementedError('Cannot instantiate an abstract library')
 
+    # Read .symbols file if it exists. This first tries to read a symbols file
+    # with the same basename with the library file name (e.g.
+    # libc++-mt.symbols), and if there isn't one, it tries to read the 'default'
+    # symbol file, which does not have any optional suffices (e.g.
+    # libc++.symbols).
+    basename = os.path.splitext(self.get_filename())[0]
+    if shared.Settings.WASM_BACKEND:
+      symbols_dir = shared.path_from_root('system', 'lib', 'symbols', 'wasm')
+    else:
+      symbols_dir = shared.path_from_root('system', 'lib', 'symbols', 'asmjs')
+    symbols_file = os.path.join(symbols_dir, basename + '.symbols')
+    default_symbols_file = os.path.join(symbols_dir, self.name + '.symbols')
+    if os.path.isfile(symbols_file):
+      self.symbols = read_symbols(symbols_file)
+    elif os.path.isfile(default_symbols_file):
+      self.symbols = read_symbols(default_symbols_file)
+
   def in_temp(cls, *args):
     """Gets the path of a file in our temporary directory."""
     return os.path.join(shared.get_emscripten_temp_dir(), *args)
@@ -286,7 +300,7 @@ class Library(object):
 
     This will trigger a build if this library is not in the cache.
     """
-    return shared.Cache.get(self.get_name(), self.build)
+    return shared.Cache.get(self.get_filename(), self.build)
 
   def get_files(self):
     """
@@ -325,7 +339,7 @@ class Library(object):
 
   def build(self):
     """Builds the library and returns the path to the file."""
-    lib_filename = self.in_temp(self.get_name())
+    lib_filename = self.in_temp(self.get_filename())
     create_lib(lib_filename, self.build_objects())
     return lib_filename
 
@@ -374,19 +388,11 @@ class Library(object):
     """
     return '.a' if shared.Settings.WASM_BACKEND and shared.Settings.WASM_OBJECT_FILES else '.bc'
 
-  def get_name(self):
+  def get_filename(self):
     """
     Return the full name of the library file, including the file extension.
     """
     return self.get_base_name() + self.get_ext()
-
-  def get_symbols(self):
-    """
-    Return a list of symbols this library exports. See `symbols`.
-
-    This is the dynamic version of `symbols`.
-    """
-    return self.symbols.copy()
 
   def get_depends(self):
     """
@@ -580,7 +586,6 @@ class NoBCLibrary(Library):
 
 class libcompiler_rt(Library):
   name = 'libcompiler_rt'
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'libcompiler_rt.symbols'))
   depends = ['libc']
 
   cflags = ['-O2']
@@ -590,11 +595,6 @@ class libcompiler_rt(Library):
 
 class libc(AsanInstrumentedLibrary, MuslInternalLibrary, MTLibrary):
   name = 'libc'
-
-  # XXX We also need to add libc symbols that use malloc, for example strdup. It's very rare to use just them and not
-  #     a normal malloc symbol (like free, after calling strdup), so we haven't hit this yet, but it is possible.
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'libc.symbols'))
-
   depends = ['libcompiler_rt']
 
   # Without -fno-builtin, LLVM can optimize away or convert calls to library
@@ -694,8 +694,6 @@ class libc(AsanInstrumentedLibrary, MuslInternalLibrary, MTLibrary):
 
 class libc_wasm(MuslInternalLibrary):
   name = 'libc-wasm'
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'libc-wasm.symbols'))
-
   cflags = ['-O2', '-fno-builtin']
   src_dir = ['system', 'lib', 'libc', 'musl', 'src', 'math']
   src_files = ['cos.c', 'cosf.c', 'cosl.c', 'sin.c', 'sinf.c', 'sinl.c',
@@ -711,15 +709,12 @@ class libc_wasm(MuslInternalLibrary):
 
 class libc_extras(MuslInternalLibrary):
   name = 'libc-extras'
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'libc-extras.symbols'))
-
   src_dir = ['system', 'lib', 'libc']
   src_files = ['extras.c']
 
 
 class libcxxabi(CXXLibrary, MTLibrary):
   name = 'libc++abi'
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'libcxxabi.symbols'))
   depends = ['libc']
   cflags = ['-std=c++11', '-Oz', '-D_LIBCPP_DISABLE_VISIBILITY_ANNOTATIONS']
 
@@ -750,7 +745,6 @@ class libcxxabi(CXXLibrary, MTLibrary):
 
 class libcxx(NoBCLibrary, CXXLibrary, NoExceptLibrary, MTLibrary):
   name = 'libc++'
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'libcxx.symbols'))
   depends = ['libc++abi']
 
   cflags = ['-std=c++11', '-DLIBCXX_BUILDING_LIBCXXABI=1', '-D_LIBCPP_BUILDING_LIBRARY', '-Oz',
@@ -877,7 +871,6 @@ class libmalloc(MTLibrary, NoBCLibrary):
 class libal(Library):
   name = 'libal'
   depends = ['libc']
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'al.symbols'))
 
   cflags = ['-Os']
   src_dir = ['system', 'lib']
@@ -887,7 +880,6 @@ class libal(Library):
 class libgl(MTLibrary):
   name = 'libgl'
   depends = ['libc']
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'gl.symbols'))
 
   src_dir = ['system', 'lib', 'gl']
   src_glob = '*.c'
@@ -988,7 +980,6 @@ class libasmfs(CXXLibrary, MTLibrary):
 
 class libhtml5(Library):
   name = 'libhtml5'
-  symbols = read_symbols(shared.path_from_root('system', 'lib', 'html5.symbols'))
 
   cflags = ['-Oz']
   src_dir = ['system', 'lib', 'html5']
@@ -1049,16 +1040,6 @@ class libpthreads(AsanInstrumentedLibrary, MuslInternalLibrary, MTLibrary):
 
   def get_base_name_prefix(self):
     return 'libpthreads' if self.is_mt else 'libpthreads_stub'
-
-  pthreads_symbols = read_symbols(shared.path_from_root('system', 'lib', 'pthreads.symbols'))
-  asmjs_pthreads_symbols = read_symbols(shared.path_from_root('system', 'lib', 'pthreads_asmjs.symbols'))
-  pthreads_stub_symbols = read_symbols(shared.path_from_root('system', 'lib', 'pthreads_stub.symbols'))
-
-  def get_symbols(self):
-    symbols = self.pthreads_symbols if self.is_mt else self.pthreads_stub_symbols
-    if self.is_mt and not shared.Settings.WASM_BACKEND:
-      symbols += self.asmjs_pthreads_symbols
-    return symbols
 
 
 class CompilerRTWasmLibrary(NoBCLibrary):
@@ -1272,7 +1253,7 @@ def calculate(temp_files, in_temp, stdout_, stderr_, forced=[]):
       return
     already_included.add(lib.name)
 
-    logger.debug('including %s (%s)' % (lib.name, lib.get_name()))
+    logger.debug('including %s (%s)' % (lib.name, lib.get_filename()))
 
     need_whole_archive = lib.name in force_include and lib.get_ext() == '.a'
     libs_to_link.append((lib.get_path(), need_whole_archive))
@@ -1301,7 +1282,7 @@ def calculate(temp_files, in_temp, stdout_, stderr_, forced=[]):
       for symbols in symbolses:
         if shared.Settings.VERBOSE:
           logger.debug('undefs: ' + str(symbols.undefs))
-        for library_symbol in lib.get_symbols():
+        for library_symbol in lib.symbols:
           if library_symbol in symbols.undefs:
             need_syms.add(library_symbol)
           if library_symbol in symbols.defs:
