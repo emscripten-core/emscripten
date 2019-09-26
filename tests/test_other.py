34d5c4695758cec87f3bfcff054a078405e6afbf
Alon Zakai <azakai@google.com>
2019-09-25 17:04:52 -0700

    test fixes

--- tests/test_other.py
+++ tests/test_other.py
@@ -8111,7 +8111,7 @@ int main() {
   @no_fastcomp()
   def test_binaryen_metadce_cxx(self):
     # test on libc++: see effects of emulated function pointers
-    self.run_metadce_test('hello_libcxx.cpp', ['-O2'], 34, [], ['waka'], 226582, 17, 33, None) # noqa
+    self.run_metadce_test('hello_libcxx.cpp', ['-O2'], 33, [], ['waka'], 226582, 16, 33, None) # noqa
 
   @parameterized({
     'normal': (['-O2'], 37, ['abort'], ['waka'], 186423, 23, 37, 541), # noqa
