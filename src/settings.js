0263daef86b973f40bb2a37d342b932fc7cba5f5
Alon Zakai <azakai@google.com>
2019-10-02 09:27:22 -0700

    wip [ci skip] need link to good length docs page

--- src/settings.js
+++ src/settings.js
@@ -1298,6 +1298,9 @@ var PTHREAD_HINT_NUM_CORES = 4;
 // True when building with --threadprofiler
 var PTHREADS_PROFILING = 0;
 
+// See ...
+var ALLOW_PTHREAD_JOIN_ON_MAIN_THREAD = 0;
+
 // If true, add in debug traces for diagnosing pthreads related issues.
 var PTHREADS_DEBUG = 0;
 
