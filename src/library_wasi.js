7b23b90fe7c087cfe40887298c7502a7f0e9cfdd
Alon Zakai <azakai@google.com>
2019-10-07 15:50:27 -0700

    wip

--- src/library_wasi.js
+++ src/library_wasi.js
@@ -11,19 +11,31 @@ var WasiLibrary = {
     return _exit(code);
   },
 
+  // This object can be modified by the user during startup, which affects
+  // the initial values of the environment accessible by getenv.
+  $ENV: {},
+
+  emscripten_get_environ__deps: ['$ENV'],
   emscripten_get_environ: function() {
     if (!_emscripten_get_environ.strings) {
-      var ENV = {};
-      ENV['USER'] = ENV['LOGNAME'] = 'web_user';
-      ENV['PATH'] = '/';
-      ENV['PWD'] = '/';
-      ENV['HOME'] = '/home/web_user';
-      // Browser language detection #8751
-      ENV['LANG'] = ((typeof navigator === 'object' &&navigator.languages && navigator.languages[0]) || 'C').replace('-', '_') + '.UTF-8';
-      ENV['_'] = thisProgram;
+      // Default values.
+      var env = {
+        'USER': 'web_user',
+        'LOGNAME': 'web_user',
+        'PATH': '/',
+        'PWD': '/',
+        'HOME': '/home/web_user',
+        // Browser language detection #8751
+        'LANG': ((typeof navigator === 'object' && navigator.languages && navigator.languages[0]) || 'C').replace('-', '_') + '.UTF-8',
+        '_': thisProgram
+      };
+      // Apply the user-provided values, if any.
+      for (var x in ENV) {
+        env[x] = ENV[x];
+      }
       var strings = [];
-      for (var key in ENV) {
-        strings.push(key + '=' + ENV[key]);
+      for (var x in env) {
+        strings.push(x + '=' + x[key]);
       }
       _emscripten_get_environ.strings = strings;
     }
