9a43c03fccfa69bd2079fe4512963f6ca1216889
Alon Zakai <azakai@google.com>
2019-10-14 13:18:45 -0700

    Implement isastream for wasi, and use that to replace fcntl:F_GETFD invocations

--- system/lib/libc/musl/src/legacy/isastream.c
+++ system/lib/libc/musl/src/legacy/isastream.c
@@ -3,5 +3,14 @@
 
 int isastream(int fd)
 {
+#ifdef __EMSCRIPTEN__
+	__wasi_fdstat_t statbuf;
+	int err = __wasi_fd_fdstat_get(fd, &statbuf);
+	if (err != __WASI_ESUCCESS) {
+		errno = err;
+		return 0;
+	}
+	return 1;
+#endif
 	return fcntl(fd, F_GETFD) < 0 ? -1 : 0;
 }
