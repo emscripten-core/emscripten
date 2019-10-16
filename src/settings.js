acbc31174fad4c905d536a8ede45e7535a517385
Sam Clegg <sbc@chromium.org>
2019-10-16 11:14:42 -0700

    feedback

--- src/settings.js
+++ src/settings.js
@@ -1523,6 +1523,17 @@ var WASM_TABLE_SIZE = 0;
 // for effective code size optimizations to take place.
 var SUPPORT_ERRNO = 1;
 
+// Internal: Tracks whether Emscripten should link in exception throwing (C++ 'throw')
+// support library. This does not need to be set directly, but pass -fno-exceptions
+// to the build disable exceptions support. (This is basically -fno-exceptions, but
+// checked at final link time instead of individual .cpp file compile time)
+// If the program *does* contain throwing code (some source files were not compiled
+// with `-fno-exceptions`), and this flag is set at link time, then you will get
+// errors on undefined symbols, as the exception throwing code is not linked in. If
+// so you should either unset the option (if you do want exceptions) or fix the
+// compilation of the source files so that indeed no exceptions are used).
+var DISABLE_EXCEPTION_THROWING = 0;
+
 // Advanced: Customize this array to reduce the set of asm.js runtime variables
 // that are generated. This allows minifying extra bit of asm.js code from unused
 // runtime code, if you know some of these are not needed.
--- src/settings_internal.js
+++ src/settings_internal.js
@@ -1,20 +1,14 @@
-// Copyright 2010 The Emscripten Authors.  All rights reserved.
+// Copyright 2019 The Emscripten Authors.  All rights reserved.
 // Emscripten is available under two separate licenses, the MIT license and the
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
 
-// Tracks whether Emscripten should link in exception throwing (C++ 'throw')
-// support library. This does not need to be set directly, but pass
-// -fno-exceptions to the build disable exceptions support. (This is basically
-// -fno-exceptions, but checked at final link time instead of individual .cpp
-// file compile time) If the program *does* contain throwing code (some source
-// files were not compiled with `-fno-exceptions`), and this flag is set at link
-// time, then you will get errors on undefined symbols, as the exception
-// throwing code is not linked in. If so you should either unset the option (if
-// you do want exceptions) or fix the compilation of the source files so that
-// indeed no exceptions are used).
-
-var DISABLE_EXCEPTION_THROWING = 0;
+//
+// Settings in this file work exactly like those in settings.js but are not
+// set-able from the command line and therefore are not part of the public
+// ABI.  This means that these settings are an internal detail of the toolchain
+// and can be added/removed/renamed without fear of breaking out users.
+//
 
 // An array of all symbols exported from asm.js/wasm module.
 var MODULE_EXPORTS = [];
@@ -28,23 +22,25 @@ var TEST_MEMORY_GROWTH_FAILS = 0;
 // stores the base name of the output file (-o TARGET_BASENAME.js)
 var TARGET_BASENAME = '';
 
-// If true, compiler supports setjmp() and longjmp(). If false, these APIs are not available.
-// If you are using C++ exceptions, but do not need setjmp()+longjmp() API, then you can set
-// this to 0 to save a little bit of code size and performance when catching exceptions.
+// If true, compiler supports setjmp() and longjmp(). If false, these APIs are
+// not available.  If you are using C++ exceptions, but do not need
+// setjmp()+longjmp() API, then you can set this to 0 to save a little bit of
+// code size and performance when catching exceptions.
 var SUPPORT_LONGJMP = 1;
 
-// Indicates that the syscalls (which we see statically) indicate that they need full
-// filesystem support. Otherwise, when just a small subset are used, we can get away without
-// including the full filesystem - in particular, if open() is never used, then we don't
-// actually need to support operations on streams.
+// Indicates that the syscalls (which we see statically) indicate that they need
+// full filesystem support. Otherwise, when just a small subset are used, we can
+// get away without including the full filesystem - in particular, if open() is
+// never used, then we don't actually need to support operations on streams.
 var SYSCALLS_REQUIRE_FILESYSTEM = 1;
 
-// A list of feature flags to pass to each binaryen invocation (like wasm-opt, etc.). This
-// is received from wasm-emscripten-finalize, which reads it from the features section.
+// A list of feature flags to pass to each binaryen invocation (like wasm-opt,
+// etc.). This is received from wasm-emscripten-finalize, which reads it from
+// the features section.
 var BINARYEN_FEATURES = [];
 
-// Whether EMCC_AUTODEBUG is on, which automatically instruments code for runtime
-// logging that can help in debugging.
+// Whether EMCC_AUTODEBUG is on, which automatically instruments code for
+// runtime logging that can help in debugging.
 var AUTODEBUG = 0;
 
 // Whether we should use binaryen's wasm2js to convert our wasm to JS. Set when
@@ -55,7 +51,8 @@ var WASM2JS = 0;
 // Whether we should link in the runtime for ubsan.
 // 0 means do not link ubsan, 1 means link minimal ubsan runtime.
 // This is not meant to be used with `-s`. Instead, to use ubsan, use clang flag
-// -fsanitize=undefined. To use minimal runtime, also pass `-fsanitize-minimal-runtime`.
+// -fsanitize=undefined. To use minimal runtime, also pass
+// `-fsanitize-minimal-runtime`.
 var UBSAN_RUNTIME = 0;
 
 // Whether we should link in LSan's runtime library. This is intended to be used
