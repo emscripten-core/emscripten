83c36cfe288de9e28dc9cbc5037e5d65bf8fefb7
Alon Zakai <azakai@google.com>
2019-10-22 12:16:37 -0700

    fix

--- tests/core/test_floatvars.cpp
+++ tests/core/test_floatvars.cpp
@@ -11,12 +11,15 @@
 #include <cfloat>
 #include <cmath>
 
-#define TEST(name, x, y) printf(#name "(%.2f, %.2f) => %.2f, %.2f\n", x, y, name(x, y), name(y, x));
+#define TEST(name, x, y)     printf("    " #name "(%.2f, %.2f) => %.2f, %.2f\n", x, y, name(x, y), name(y, x));
+#define TEST_ABS(name, x, y) printf("abs " #name "(%.2f, %.2f) => %.2f, %.2f\n", x, y, abs(name(x, y)), abs(name(y, x)));
 
+// fmin etc. are not specced to be sensitive to negative zero, and LLVM does
+// depend on that for optimizations, so check only the absolute value there
 #define TESTS(name) \
   TEST(name, 0.33, 0.5) \
   TEST(name, NAN, 0.5) \
-  TEST(name, 0.0, -0.0) \
+  TEST_ABS(name, 0.0, -0.0)
 
 int main(int argc, char **argv) {
   float x = 1.234, y = 3.5, q = 0.00000001;
