1587c1efee808bfa7ee2b08c066fc14a52cfb305
Derek Schuff <dschuff@chromium.org>
2019-10-23 10:24:14 -0700

    Use argparse, add tests

--- emsize.py
+++ emsize.py
@@ -15,14 +15,15 @@ shows the size of the JavaScript loader file.
 Currently there are many limitations; basically this tool is enough to
 be used by the LLVM testsuite runner code to analyze size output.
 
-Currently this tool only supports sysv output format and ignores any
-arguments other than the last, which is expected to be a JS file;
-the wasm file is expected to be in the same directory, and have the
-same basename with a '.wasm' extension.
+Currently this tool only supports sysv output format (it accepts but ignores
+any '-format' argument). It does not accept any other arguments aside from the
+input file, which is expected to be a JS file. The wasm file is expected to be
+in the same directory, and have the same basename with a '.wasm' extension.
 """
 
 from __future__ import print_function
 
+import argparse
 import os
 import subprocess
 import sys
@@ -35,6 +36,14 @@ LLVM_SIZE = os.path.expanduser(shared.build_llvm_tool_path(shared.exe_suffix('ll
 def error(text):
   print(text, file=sys.stderr, flush=True)
   return 1
+
+
+def parse_args(argv):
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument('-format')
+  parser.add_argument('file')
+  args = parser.parse_args(argv)
+  return args.file
 
 
 def print_sizes(js_file):
@@ -70,4 +79,4 @@ def print_sizes(js_file):
 
 
 if __name__ == '__main__':
-  sys.exit(print_sizes(sys.argv[-1]))
+  sys.exit(print_sizes(parse_args(sys.argv[1:])))
