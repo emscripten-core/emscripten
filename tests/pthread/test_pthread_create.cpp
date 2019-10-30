1e2e871c6f736dbc6236ee1d17749106208222d1
Alon Zakai <azakai@google.com>
2019-10-30 14:57:47 -0700

    revert

--- tests/pthread/test_pthread_create.cpp
+++ tests/pthread/test_pthread_create.cpp
@@ -10,51 +10,49 @@
 #include <emscripten/threading.h>
 #include <assert.h>
 
-#define NUM_THREADS 1
+#define NUM_THREADS 8
 
 int fib(int n)
 {
-  if (n <= 0) return 0;
-  if (n == 1) return 1;
-  return fib(n-1) + fib(n-2);
+	if (n <= 0) return 0;
+	if (n == 1) return 1;
+	return fib(n-1) + fib(n-2);
 }
 
 unsigned int global_shared_data[NUM_THREADS];
 
 void *ThreadMain(void *arg)
 {
-printf("ThreadMain! %d\n", (int)arg);
-
-  int idx = (int)arg;
-  unsigned int param = global_shared_data[idx];
+	int idx = (int)arg;
+	unsigned int param = global_shared_data[idx];
 
 #define N 100
 
-  EM_ASM(err('Thread idx '+$0+': sorting ' + $1 + ' numbers with param ' + $2 + '.'), idx, N, param);
+	EM_ASM(err('Thread idx '+$0+': sorting ' + $1 + ' numbers with param ' + $2 + '.'), idx, N, param);
 
-  unsigned int n[N];
-  for(unsigned int i = 0; i < N; ++i)
-    n[i] = (i + param) % N; // Create a shifted increasing sequence of numbers [0, N-1[
+	unsigned int n[N];
+	for(unsigned int i = 0; i < N; ++i)
+		n[i] = (i + param) % N; // Create a shifted increasing sequence of numbers [0, N-1[
 
-  // Sort the sequence to ordered [0, N[
-  for(unsigned int i = 0; i < N; ++i)
-    for(unsigned int j = i; j < N; ++j)
-    {
-      if (n[i] > n[j])
-      {
-        unsigned int t = n[i];
-        n[i] = n[j];
-        n[j] = t;
-      }
-    }
-  // Ensure all elements are in place.
-  int numGood = 0;
-  for(unsigned int i = 0; i < N; ++i)
-    if (n[i] == i) ++numGood;
-    else EM_ASM(err('n['+$0+']='+$1), i, n[i]);
+	// Sort the sequence to ordered [0, N[
+	for(unsigned int i = 0; i < N; ++i)
+		for(unsigned int j = i; j < N; ++j)
+		{
+			if (n[i] > n[j])
+			{
+				unsigned int t = n[i];
+				n[i] = n[j];
+				n[j] = t;
+			}
+		}
+	// Ensure all elements are in place.
+	int numGood = 0;
+	for(unsigned int i = 0; i < N; ++i)
+		if (n[i] == i) ++numGood;
+		else EM_ASM(err('n['+$0+']='+$1), i, n[i]);
 
-  EM_ASM(out('Thread idx ' + $0 + ' with param '+$1+': all done with result '+$2+'.'), idx, param, numGood);
-  pthread_exit((void*)numGood);
+	EM_ASM(out('Thread idx ' + $0 + ' with param '+$1+': all done with result '+$2+'.'), idx, param, numGood);
+	pthread_exit((void*)numGood);
 }
 
 pthread_t thread[NUM_THREADS];
@@ -63,43 +61,54 @@ int numThreadsToCreate = 1000;
 
 void CreateThread(int i)
 {
-  pthread_attr_t attr;
-  pthread_attr_init(&attr);
-  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-  static int counter = 1;
-  global_shared_data[i] = (counter++ * 12141231) & 0x7FFFFFFF; // Arbitrary random'ish data for perturbing the sort for this thread task.
-//  EM_ASM(out('Main: Creating thread idx ' + $0 + ' (param ' + $1 + ')'), i, global_shared_data[i]);
-  int rc = pthread_create(&thread[i], &attr, ThreadMain, (void*)i);
-  assert(rc == 0);
-  pthread_attr_destroy(&attr);
-}
-
-void mainn() {
-  printf("main iter\n");
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	static int counter = 1;
+	global_shared_data[i] = (counter++ * 12141231) & 0x7FFFFFFF; // Arbitrary random'ish data for perturbing the sort for this thread task.
+//	EM_ASM(out('Main: Creating thread idx ' + $0 + ' (param ' + $1 + ')'), i, global_shared_data[i]);
+	int rc = pthread_create(&thread[i], &attr, ThreadMain, (void*)i);
+	assert(rc == 0);
+	pthread_attr_destroy(&attr);
 }
 
 int main()
 {
-printf("mainne1\n");
-  if (!emscripten_has_threading_support())
-  {
+	if (!emscripten_has_threading_support())
+	{
 #ifdef REPORT_RESULT
-    REPORT_RESULT(0);
+		REPORT_RESULT(0);
 #endif
-    printf("Skipped: Threading is not supported.\n");
-    return 0;
-  }
-printf("mainne2\n");
+		printf("Skipped: Threading is not supported.\n");
+		return 0;
+	}
 
-  // Create initial threads.
-  for(int i = 0; i < NUM_THREADS; ++i) {
-printf("mainne3\n");
-    CreateThread(i);
-  }
-printf("mainne4\n");
+	// Create initial threads.
+	for(int i = 0; i < NUM_THREADS; ++i)
+		CreateThread(i);
 
-  // synchronous here would not let the worker start -
-  // the postMessage is only sent at the end of the event
-  // loop it seems :(
-  emscripten_set_main_loop(mainn, 1, 1);
+	// Join all threads and create more.
+        while (numThreadsToCreate > 0)
+        {
+		for(int i = 0; i < NUM_THREADS; ++i)
+		{
+			if (thread[i])
+			{
+				int status;
+				int rc = pthread_join(thread[i], (void**)&status);
+				assert(rc == 0);
+				EM_ASM(err('Main: Joined thread idx ' + $0 + ' (param ' + $1 + ') with status ' + $2), i, global_shared_data[i], (int)status);
+				assert(status == N);
+				thread[i] = 0;
+				if (numThreadsToCreate > 0)
+				{
+					--numThreadsToCreate;
+					CreateThread(i);
+				}
+			}
+		}
+	}
+#ifdef REPORT_RESULT
+	REPORT_RESULT(0);
+#endif
 }
