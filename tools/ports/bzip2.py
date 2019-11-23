37576aabec37ff54f6eabec3bd765b6e294e0799
Michael de Lang <kingoipo@gmail.com>
2019-11-23 16:12:19 +0100

    Clean up some more missing -c warnings

--- tools/ports/bzip2.py
+++ tools/ports/bzip2.py
@@ -37,7 +37,7 @@ def get(ports, settings, shared):
     for src in srcs:
       o = os.path.join(ports.get_build_dir(), 'bzip2', src + '.o')
       shared.safe_ensure_dirs(os.path.dirname(o))
-      commands.append([shared.PYTHON, shared.EMCC, os.path.join(dest_path, src), '-O2', '-o', o, '-I' + dest_path, '-w', ])
+      commands.append([shared.PYTHON, shared.EMCC, '-c', os.path.join(dest_path, src), '-O2', '-o', o, '-I' + dest_path, '-w', ])
       o_s.append(o)
     ports.run_commands(commands)
 
