2c80079e43b2f133c92672c5855eaeffbc48f12b
Alon Zakai <azakai@google.com>
2019-11-27 16:00:24 -0800

    Restore demangling optimization (#9914)
    Fixes #9905 where it was reported that we lost our localmod
    that prevents always including demangling support when
    exceptions are enabled. This restores that, and adds size
    testing for exceptions and exceptions + demangling support.

--- tests/test_other.py
+++ tests/test_other.py
@@ -8169,10 +8169,18 @@ int main() {
   def test_metadce_minimal_fastcomp(self, *args):
     self.run_metadce_test('minimal.c', *args)
 
+  @parameterized({
+    'noexcept': (['-O2'],                    19, [], ['waka'], 218988, 17, 33, None), # noqa
+    # exceptions increases code size significantly
+    'except':   (['-O2', '-fexceptions'],    52, [], ['waka'], 279827, 46, 46, None), # noqa
+    # exceptions does not pull in demangling by default, which increases code size
+    'mangle':   (['-O2', '-fexceptions',
+                  '-s', 'DEMANGLE_SUPPORT'], 52, [], ['waka'], 408028, 46, 47, None), # noqa
+  })
   @no_fastcomp()
-  def test_metadce_cxx(self):
+  def test_metadce_cxx(self, *args):
     # test on libc++: see effects of emulated function pointers
-    self.run_metadce_test('hello_libcxx.cpp', ['-O2'], 19, [], ['waka'], 226582, 17, 33, None) # noqa
+    self.run_metadce_test('hello_libcxx.cpp', *args)
 
   @parameterized({
     'normal': (['-O2'], 40, ['abort'], ['waka'], 186423, 23, 37, 541), # noqa
