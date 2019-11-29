6c082e6dd75adad30cf35a7baca81e091a366beb
Sam Clegg <sbc@chromium.org>
2019-11-29 16:34:17 -0600

    Revert "Fix relative paths in wasm backend source maps. Fixes #9837 (#9882)"
    This reverts commit 5486c4aaaf43648159ace3e9138469c44bc12593.

--- tests/test_core.py
+++ tests/test_core.py
@@ -7196,12 +7196,6 @@ err = err = function(){};
       else:
         return data
 
-    def source_map_file_loc(name):
-      if shared.Settings.WASM_BACKEND:
-        return name
-      # in fastcomp, we have the absolute path, which is not good
-      return os.path.abspath(name)
-
     data = json.load(open(map_filename))
     if str is bytes:
       # Python 2 compatibility
@@ -7211,7 +7205,7 @@ err = err = function(){};
       # the output file.
       self.assertPathsIdentical(map_referent, data['file'])
     assert len(data['sources']) == 1, data['sources']
-    self.assertPathsIdentical(source_map_file_loc('src.cpp'), data['sources'][0])
+    self.assertPathsIdentical(os.path.abspath('src.cpp'), data['sources'][0])
     if hasattr(data, 'sourcesContent'):
       # the sourcesContent attribute is optional, but if it is present it
       # needs to containt valid source text.
@@ -7224,7 +7218,7 @@ err = err = function(){};
       mappings = encode_utf8(mappings)
     seen_lines = set()
     for m in mappings:
-      self.assertPathsIdentical(source_map_file_loc('src.cpp'), m['source'])
+      self.assertPathsIdentical(os.path.abspath('src.cpp'), m['source'])
       seen_lines.add(m['originalLine'])
     # ensure that all the 'meaningful' lines in the original code get mapped
     # when optimizing, the binaryen optimizer may remove some of them (by inlining, etc.)
@@ -8264,7 +8258,7 @@ NODEFS is no longer included by default; build with -lnodefs.js
     'g4': ('-g4', [
       "src.cpp:3:12: runtime error: reference binding to null pointer of type 'int'",
       'in main ',
-      'src.cpp:3:8'
+      '/src.cpp:3:8'
     ]),
   })
   @no_fastcomp('ubsan not supported on fastcomp')
