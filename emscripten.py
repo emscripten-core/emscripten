ea39413a537867ece780c7b524c95199d7925595
Jonathan Feinberg <feinberg@google.com>
2019-11-30 12:03:15 -0500

    make pep8 happy

--- emscripten.py
+++ emscripten.py
@@ -2605,6 +2605,7 @@ def create_sending_wasm(invoke_funcs, forwarded_json, metadata):
   sorted_keys = sorted(send_items_map.keys())
   return '{ ' + ', '.join('"' + k + '": ' + send_items_map[k] for k in sorted_keys) + ' }'
 
+
 # It's best to avoid the use of "arguments" in generated JavaScript, as it
 # throws off garbage. Adding known argument counts to this table of receiving
 # functions will generate code with explicit argument lists.
@@ -2612,6 +2613,7 @@ known_arg_counts = {
   'stackSave': 0,
   'stackRestore': 1,
 }
+
 
 def create_receiving_wasm(exports):
   receiving = []
