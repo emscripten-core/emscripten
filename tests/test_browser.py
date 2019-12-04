2596615866c6294876245d5a9840116ad8d4013c
Gabriel Cuvillier <contact@gabrielcuvillier.pro>
2019-12-04 21:57:44 +0100

    removed duplicate test

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1524,15 +1524,6 @@ keydown(100);keyup(100); // trigger the end
                     '-pthread', '-DUSE_REGAL', '--use-preload-plugins', '-lSDL', '-lGL'],
               message='You should see an image with gray at the top.')
 
-  @requires_threads
-  @requires_graphics_hardware
-  def test_sdl_ogl_regal_mt(self):
-    shutil.copyfile(path_from_root('tests', 'screenshot.png'), 'screenshot.png')
-    self.btest('sdl_ogl.c', reference='screenshot-gray-purple.png', reference_slack=1,
-               args=['-O2', '--minify', '0', '--preload-file', 'screenshot.png', '-s', 'USE_REGAL=1', '-s', 'DISABLE_EXCEPTION_CATCHING=0', '-s', 'USE_PTHREADS=1',
-                     '-fexceptions', '-DUSE_REGAL', '--use-preload-plugins', '-lSDL', '-lGL'],
-               message='You should see an image with gray at the top.')
-
   @requires_graphics_hardware
   def test_sdl_ogl_defaultmatrixmode(self):
     shutil.copyfile(path_from_root('tests', 'screenshot.png'), 'screenshot.png')
