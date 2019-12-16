4d020597c862499585fc2e1d176a2bee343b91be
Alon Zakai <azakai@google.com>
2019-12-16 13:05:28 -0800

    Revert part of #8437 - the glfw part was invalid, as when processing JS libraries we have not yet included anything from libraries, so we should not read addedLibraryItems

--- src/library_glfw.js
+++ src/library_glfw.js
@@ -737,7 +737,6 @@ var LibraryGLFW = {
 
       event.preventDefault();
 
-#if '$FS' in addedLibraryItems
       var filenames = allocate(new Array(event.dataTransfer.files.length*4), 'i8*', ALLOC_NORMAL);
       var filenamesArray = [];
       var count = event.dataTransfer.files.length;
@@ -778,8 +777,6 @@ var LibraryGLFW = {
       for (var i = 0; i < count; ++i) {
         save(event.dataTransfer.files[i]);
       }
-
-#endif // '$FS' in addedLibraryItems
 
       return false;
     },
