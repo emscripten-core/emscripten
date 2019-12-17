aef152f36da62e13e2edf03424d1e579283e233e
Alon Zakai <azakai@google.com>
2019-12-17 11:26:07 -0800

    fixes

--- src/preamble.js
+++ src/preamble.js
@@ -376,7 +376,7 @@ if (ENVIRONMENT_IS_PTHREAD) {
   // At the 'load' stage of Worker startup, we are just loading this script
   // but not ready to run yet. At 'run' we receive proper values for the stack
   // etc. and can launch a pthread. Set some fake values there meanwhile to
-  // catch bugs, then set the real values in applyStackValues later.
+  // catch bugs, then set the real values in establishStackSpaceInJsModule later.
 #if ASSERTIONS || SAFE_STACK
   STACK_MAX = STACKTOP = STACK_MAX = 0x7FFFFFFF;
 #endif
