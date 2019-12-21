0350fac1a81d39176990110ba52bd708d4bb581c
Jukka Jylänki <jujjyl@gmail.com>
2019-12-21 12:40:42 +0200

    Avoid -O2 optimizing malloc() away in test_memorygrowth_geometric_step

--- tests/core/test_memorygrowth_geometric_step.c
+++ tests/core/test_memorygrowth_geometric_step.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <emscripten/heap.h>
 
 int main()
 {
-	malloc(60*1024*1024);
-	printf("Heap size: %zu\n", emscripten_get_heap_size());
+	void *ptr = malloc(60*1024*1024);
+	memset(ptr, 0x01, 60*1024*1024); // Avoid -O2 optimizing malloc() away
+	printf("Ptr: %d, value: %d, Heap size: %zu\n", (int)!!ptr, ((int*)ptr)[rand()%65536], emscripten_get_heap_size());
 }
