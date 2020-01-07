cf9a1a163f137ba236e2fd6c0ed0b5aa6db8cc07
Alon Zakai <azakai@google.com>
2020-01-07 10:46:26 -0800

    Revert "debug FIXME"
    This reverts commit bef9ca808e84bcc6d367381adbe42a479f867a0b.

--- tests/browser/test_offset_converter.c
+++ tests/browser/test_offset_converter.c
@@ -4,22 +4,12 @@
 void *get_pc(void) { return __builtin_return_address(0); }
 
 void magic_test_function(void) {
-  EM_ASM({
-    console.log($0);
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', encodeURI('http://localhost:8888?stderr=' + $0));
-    xhr.send();
-
-    console.log(wasmOffsetConverter.getName($0));
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', encodeURI('http://localhost:8888?stderr=' + wasmOffsetConverter.getName($0)));
-    xhr.send();
-
-    var result = wasmOffsetConverter.getName($0) == 'magic_test_function';
-
-    reportResultToServer(+result, true);
+  int result = EM_ASM_INT({
+    return wasmOffsetConverter.getName($0) == 'magic_test_function';
   }, get_pc());
+#ifdef REPORT_RESULT
+  REPORT_RESULT(result);
+#endif
 }
 
 int main(void) { magic_test_function(); }
-
