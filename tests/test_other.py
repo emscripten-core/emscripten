712ae829dea22a9fda286af4c28ee1e566c76dfe
Sam Clegg <sbc@chromium.org>
2020-01-09 11:37:46 -0800

    flake8

--- tests/test_other.py
+++ tests/test_other.py
@@ -2555,7 +2555,7 @@ int f() {
     self.assertExists('immutable.js.metadata')
     # verify js output JS file is not touched when the metadata is separated
     shutil.copy2('immutable.js', 'immutable.js.copy') # copy with timestamp preserved
-    # ensure some time passes before running the packager again so that if it does touch the 
+    # ensure some time passes before running the packager again so that if it does touch the
     # js file it will end up with the different timestamp.
     time.sleep(1.0)
     run_process([PYTHON, FILE_PACKAGER, 'test.data', '--preload', 'data1.txt', '--preload', 'subdir/data2.txt', '--js-output=immutable.js', '--separate-metadata'])
