6a2ab1811361b59559fe883d588d1c6b3842e1b1
Jukka Jylänki <jujjyl@gmail.com>
2020-01-19 13:26:49 +0200

    Update code size test

--- tests/test_other.py
+++ tests/test_other.py
@@ -9595,14 +9595,14 @@ int main () {
 
     if self.is_wasm_backend():
       test_cases = [
-        (opts, hello_world_sources, {'a.html': 930, 'a.js': 484, 'a.wasm': 176}),
+        (opts, hello_world_sources, {'a.html': 1205, 'a.js': 484, 'a.wasm': 176}),
         (opts, hello_webgl_sources, {'a.html': 1335, 'a.js': 4663, 'a.wasm': 11809}),
         (opts, hello_webgl2_sources, {'a.html': 1335, 'a.js': 5172, 'a.wasm': 11809}) # Compare how WebGL2 sizes stack up with WebGL 1
       ]
     else:
       test_cases = [
         (asmjs + opts, hello_world_sources, {'a.html': 1223, 'a.js': 289, 'a.asm.js': 113, 'a.mem': 6}),
-        (opts, hello_world_sources, {'a.html': 930, 'a.js': 633, 'a.wasm': 86}),
+        (opts, hello_world_sources, {'a.html': 1205, 'a.js': 633, 'a.wasm': 86}),
         (asmjs + opts, hello_webgl_sources, {'a.html': 1353, 'a.js': 4921, 'a.asm.js': 11129, 'a.mem': 321}),
         (opts, hello_webgl_sources, {'a.html': 1335, 'a.js': 4874, 'a.wasm': 8932}),
         (opts, hello_webgl2_sources, {'a.html': 1335, 'a.js': 5361, 'a.wasm': 8932}) # Compare how WebGL2 sizes stack up with WebGL 1
