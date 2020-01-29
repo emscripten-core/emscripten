8689a54d828e5e7d364ace15128b1dbf22cc1b5b
Sam Clegg <sbc@chromium.org>
2020-01-29 14:24:11 -0800

    Add regression test for MAIN_MODULE + longjmp issue (#9633)
    See #2180

--- tests/test_core.py
+++ tests/test_core.py
@@ -1019,6 +1019,13 @@ base align: 0, 0, 0, 0'''])
   def test_longjmp2(self):
     self.do_run_in_out_file_test('tests', 'core', 'test_longjmp2')
 
+  @needs_dlfcn
+  def test_longjmp2_main_module(self):
+    # Test for binaryen regression:
+    # https://github.com/WebAssembly/binaryen/issues/2180
+    self.set_setting('MAIN_MODULE')
+    self.do_run_in_out_file_test('tests', 'core', 'test_longjmp2')
+
   def test_longjmp3(self):
     self.do_run_in_out_file_test('tests', 'core', 'test_longjmp3')
 
