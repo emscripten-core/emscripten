50dea26a99d6e4432c1aa15e3fe6889bb077fe0d
Jukka Jylänki <jujjyl@gmail.com>
2020-02-04 19:26:23 +0200

    Clear DEFAULT_LIBRARY_FUNCS_TO_INCLUDE for MINIMAL_RUNTIME

--- emcc.py
+++ emcc.py
@@ -1105,9 +1105,7 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
 
     if shared.Settings.MINIMAL_RUNTIME or 'MINIMAL_RUNTIME=1' in settings_changes or 'MINIMAL_RUNTIME=2' in settings_changes:
       # Remove the default exported functions 'malloc', 'free', etc. those should only be linked in if used
-      for to_remove in ['malloc', 'free', 'memcpy', 'memset', 'emscripten_get_heap_size']:
-        if to_remove in shared.Settings.DEFAULT_LIBRARY_FUNCS_TO_INCLUDE:
-          shared.Settings.DEFAULT_LIBRARY_FUNCS_TO_INCLUDE.remove(to_remove)
+      shared.Settings.DEFAULT_LIBRARY_FUNCS_TO_INCLUDE = []
 
     # Set ASM_JS default here so that we can override it from the command line.
     shared.Settings.ASM_JS = 1 if options.opt_level > 0 else 2
