ad7409aede5a8b58d11c26d8ab03372351d8a408
juj <jujjyl@gmail.com>
2020-02-09 17:34:37 +0200

    Fix memory corruption issue in emmalloc implementation of memalign()  (#10377)
    * Fix memory corruption issue in memalign() implementation of emmalloc. Closes https://github.com/emscripten-core/emscripten/issues/10363 .
    
    * Address review and remove resize_region
    
    * Update code size test

--- tests/test_other.py
+++ tests/test_other.py
@@ -9626,7 +9626,7 @@ int main () {
       test_cases = [
         (asmjs + opts, hello_world_sources, {'a.html': 682, 'a.js': 289, 'a.asm.js': 113, 'a.mem': 6}),
         (opts, hello_world_sources, {'a.html': 665, 'a.js': 630, 'a.wasm': 86}),
-        (asmjs + opts, hello_webgl_sources, {'a.html': 580, 'a.js': 4896, 'a.asm.js': 11129, 'a.mem': 321}),
+        (asmjs + opts, hello_webgl_sources, {'a.html': 580, 'a.js': 4896, 'a.asm.js': 11091, 'a.mem': 321}),
         (opts, hello_webgl_sources, {'a.html': 563, 'a.js': 4850, 'a.wasm': 8932}),
         (opts, hello_webgl2_sources, {'a.html': 563, 'a.js': 5357, 'a.wasm': 8932}) # Compare how WebGL2 sizes stack up with WebGL 1
       ]
