f7e30c26bc3b84398fa2591635ffb4772cb68086
Jukka Jylänki <jujjyl@gmail.com>
2020-02-10 17:38:01 +0200

    Add Wasm2JS code size tests

--- tests/test_other.py
+++ tests/test_other.py
@@ -9602,6 +9602,7 @@ int main () {
                                '--output_eol', 'linux']
 
     asmjs = ['-s', 'WASM=0', '--separate-asm', '-s', 'ELIMINATE_DUPLICATE_FUNCTIONS=1', '--memory-init-file', '1']
+    wasm2js = ['-s', 'WASM=0', '--memory-init-file', '1']
     opts = ['-O3', '--closure', '1', '-DNDEBUG', '-ffast-math']
 
     hello_world_sources = [path_from_root('tests', 'small_hello_world.c'),
@@ -9618,7 +9619,9 @@ int main () {
 
     if self.is_wasm_backend():
       test_cases = [
+        (wasm2js + opts, hello_world_sources, {'a.html': 697, 'a.js': 2227, 'a.mem': 6}),
         (opts, hello_world_sources, {'a.html': 665, 'a.js': 450, 'a.wasm': 172}),
+        (wasm2js + opts, hello_webgl_sources, {'a.html': 588, 'a.js': 25932, 'a.mem': 3168}),
         (opts, hello_webgl_sources, {'a.html': 563, 'a.js': 4629, 'a.wasm': 11731}),
         (opts, hello_webgl2_sources, {'a.html': 563, 'a.js': 5137, 'a.wasm': 11731}) # Compare how WebGL2 sizes stack up with WebGL 1
       ]
