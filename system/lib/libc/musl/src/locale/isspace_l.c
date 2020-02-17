0e78eeec6d5c2982a18c11cf618734efe7e9df63
Sam Clegg <sbc@chromium.org>
2020-02-17 15:15:10 -0800

    Remove old files from musl source tree
    When musl was updated to version 1.1.5 in #4813 it looks like files
    that were removed upstream were not removed locally.
    
    Because we recursively build some directories this means we were
    build old files. For example the src/locale/is*.c family of functions
    were removed from musl in d89fdec51b5849ebdf8000ff1c2fb49878004f39
    but remained in our tree after the update.
    
    This fixes some strange failures we've been seeing on the CI builders
    where the name of a symbol alternates between __isxdigit_l (the new
    name) and isxdigit_l (the old name).   This is due to the fact that
    this symbol was defined in two different object files and filesystem
    ordering was determining which object was included first by the linker.
    
    This change was automatically generated by checking out musl at v1.1.15
    and running:
    
    `for f in $(find . -type f); do if [[ ! -e ../../../../../musl/$f ]]; then echo $f; fi; done  | grep -v emscripten | xargs git rm`

--- system/lib/libc/musl/src/locale/isspace_l.c
+++
@@ -1,6 +0,0 @@
-#include <ctype.h>
-
-int isspace_l(int c, locale_t l)
-{
-	return isspace(c);
-}
--- system/lib/libc/musl/src/locale/towlower_l.c
+++
@@ -1,9 +0,0 @@
-#include <wctype.h>
-#include "libc.h"
-
-wint_t towlower_l(wint_t c, locale_t l)
-{
-	return towlower(c);
-}
-
-weak_alias(towlower_l, __towlower_l);
