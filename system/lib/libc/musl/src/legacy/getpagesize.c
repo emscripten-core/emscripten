55a7fb656b0f92d0656ed717abc2232cced4c499
Alon Zakai <azakai@google.com>
2020-02-21 15:11:05 -0800

    style and update ABI [ci skip]

--- system/lib/libc/musl/src/legacy/getpagesize.c
+++ system/lib/libc/musl/src/legacy/getpagesize.c
@@ -6,7 +6,7 @@ int getpagesize(void)
 {
 #if __EMSCRIPTEN__
 	// A value used historically in emscripten, and which so far we don't have a
-  // strong reason to change.
+	// strong reason to change.
 	return 16384;
 #else
 	return PAGE_SIZE;
