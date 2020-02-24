bc08165aea205f45400c05c7b69e4197e46d6294
Alon Zakai <azakai@google.com>
2020-02-24 14:53:06 -0800

    test

--- tests/other/standalone_syscalls/test.cpp
+++ tests/other/standalone_syscalls/test.cpp
@@ -1,7 +1,9 @@
 #include <assert.h>
 #include <fcntl.h>
+#include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 extern "C" ssize_t write(int fd, const void *buf, size_t count);
 
@@ -13,6 +15,11 @@ int main() {
   assert(fd_out == 1);
   int fd_err = open("/dev/stderr", 0);
   assert(fd_err == 2);
+  // ioctl and fcntl fail
+  int i = ioctl(1, 0);
+  assert(i < 0);
+  int f = fcntl(1, 0);
+  assert(f < 0);
   // write to standard streams works.
   write(1, "hello, world!", 5);
   write(1, "\n", 1);
