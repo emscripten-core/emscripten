7c56a36a79df4801c9963199f978b80342710e87
Jukka Jylänki <jujjyl@gmail.com>
2020-02-24 10:33:51 +0200

    Require that when aliasing functions, either the alias function or the target function must contain a signature, so that using 'arguments' object can always be avoided. This enables more efficient JS VM execution as it will not need to deopt around the use of arguments object, and Closure can inline the aliased call if it shrinks code size.

--- tests/test_other.py
+++ tests/test_other.py
@@ -8795,7 +8795,7 @@ end
     # are including a lot of JS without corresponding compiled code for it. This still
     # lets us catch all other errors.
     with env_modify({'EMCC_CLOSURE_ARGS': '--jscomp_off undefinedVars'}):
-      run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.c'), '-O2', '--closure', '1', '-g1', '-s', 'INCLUDE_FULL_LIBRARY=1', '-s', 'ERROR_ON_UNDEFINED_SYMBOLS=0'])
+      run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.c'), '-O1', '--closure', '1', '-g1', '-s', 'INCLUDE_FULL_LIBRARY=1', '-s', 'ERROR_ON_UNDEFINED_SYMBOLS=0'])
 
   # Tests --closure-args command line flag
   def test_closure_externs(self):
