3fdc1965d00b679b34193277ff10b19443313206
Sam Clegg <sbc@chromium.org>
2020-03-29 19:09:49 -0700

    typo [skip ci]

--- tests/test_other.py
+++ tests/test_other.py
@@ -10658,7 +10658,7 @@ int main() {
     run_process([PYTHON, EMCC, path_from_root('tests', 'unistd', 'close.c'), '-nodefaultlibs'] + libs)
 
   def test_argument_match(self):
-    # Verify that emcc arguments match preciely.  We had a bug where only the prefix
+    # Verify that emcc arguments match precisely.  We had a bug where only the prefix
     # was matched
     run_process([PYTHON, EMCC, path_from_root('tests', 'hello_world.c'), '--js-opts', '10'])
     err = self.expect_fail([PYTHON, EMCC, path_from_root('tests', 'hello_world.c'), '--js-optsXX'])
