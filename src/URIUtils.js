481f3dfc9491a229f7c9427f9d0d2c647d5bd6e6
Vasily <just.one.man@yandex.ru>
2020-04-08 00:43:36 +0300

    Reduce code duplication

--- src/URIUtils.js
+++ src/URIUtils.js
@@ -4,21 +4,23 @@
  * SPDX-License-Identifier: MIT
  */
 
+function hasPrefix(str, prefix) {
+  return String.prototype.startsWith ?
+      str.startsWith(prefix) :
+      str.indexOf(prefix) === 0;
+}
+
 // Prefix of data URIs emitted by SINGLE_FILE and related options.
 var dataURIPrefix = 'data:application/octet-stream;base64,';
 
 // Indicates whether filename is a base64 data URI.
 function isDataURI(filename) {
-  return String.prototype.startsWith ?
-      filename.startsWith(dataURIPrefix) :
-      filename.indexOf(dataURIPrefix) === 0;
+  return hasPrefix(dataURIPrefix);
 }
 
 var fileURIPrefix = "file://";
 
 // Indicates whether filename is delivered via file protocol (as opposed to http/https)
 function isFileURI(filename) {
-  return String.prototype.startsWith ?
-      filename.startsWith(fileURIPrefix) :
-      filename.indexOf(fileURIPrefix) === 0;
+  return hasPrefix(fileURIPrefix);
 }
