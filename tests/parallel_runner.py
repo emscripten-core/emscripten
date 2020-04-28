c519c19e8ebab672f871590bf7b2e329bd7463b4
Sam Clegg <sbc@chromium.org>
2020-04-28 13:40:29 -0400

    Report duration of each test we run (#11009)
    I couldn't figure out an easy way to have this information repeated
    in the result summary.

--- tests/parallel_runner.py
+++ tests/parallel_runner.py
@@ -10,6 +10,7 @@ import subprocess
 import sys
 import unittest
 import tempfile
+import time
 
 from tools.tempfiles import try_delete
 
@@ -130,13 +131,15 @@ class BufferedParallelTestResult(object):
     result.stopTest(self.test)
 
   def startTest(self, test):
-    pass
+    self.start_time = time.perf_counter()
 
   def stopTest(self, test):
-    pass
+    # TODO(sbc): figure out a way to display this duration information again when
+    # these results get passed back to the TextTestRunner/TextTestResult.
+    self.buffered_result.duration = time.perf_counter() - self.start_time
 
   def addSuccess(self, test):
-    print(test, '... ok', file=sys.stderr)
+    print(test, '... ok (%.2fs)' % (time.perf_counter() - self.start_time), file=sys.stderr)
     self.buffered_result = BufferedTestSuccess(test)
 
   def addSkip(self, test, reason):
