1248ad37236c8f12355de0eb8c4beb1db6ed27dc
Derek Schuff <dschuff@chromium.org>
2020-04-28 19:31:01 -0700

    replace throw() with _NOEXCEPT

--- system/lib/libcxx/regex.cpp
+++ system/lib/libcxx/regex.cpp
@@ -66,7 +66,7 @@ regex_error::regex_error(regex_constants::error_type ecode)
       __code_(ecode)
 {}
 
-regex_error::~regex_error() throw() {}
+regex_error::~regex_error() _NOEXCEPT {}
 
 namespace {
 
