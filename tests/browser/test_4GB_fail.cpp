51a13349ad702323c089178149f5e98a96a6d49e
Alon Zakai <azakai@google.com>
2020-04-30 11:13:21 -0700

    simplify test

--- tests/browser/test_4GB_fail.cpp
+++ tests/browser/test_4GB_fail.cpp
@@ -1,30 +1,29 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <vector>
 
-std::vector<void*> allocations;
+void* allocation;
 
 int main() {
   const int MB = 1024 * 1024;
   const int CHUNK = 512 * MB;
-  while (1) {
-    void* allocation = malloc(CHUNK);
+  int i;
+  for (i = 0;; i++) {
+    allocation = malloc(CHUNK);
     if (!allocation) {
       puts("failed to allocate any more");
       break;
     }
-    allocations.push_back(allocation);
     printf("%ld: Allocated %d MB, total so far: %ld MB\n",
-           allocations.size(), CHUNK / MB,
-           (allocations.size() * CHUNK) / MB);
+           i, CHUNK / MB,
+           (i * CHUNK) / MB);
     printf("  (writing to make sure, to %p)\n", allocation);
     memset(allocation, 42, CHUNK);
   }
   // We should have allocated at least 2GB.
-  assert(((allocations.size() * CHUNK) / MB) >= (2 * 1024));
-  // We should have allocated less than 4GB.
-  assert(((allocations.size() * CHUNK) / MB) < (4 * 1024));
+  assert(((i * CHUNK) / MB) >= (2 * 1024));
+  // We should have allocated less than 4GB (we can't get to exactly 4GB
+  // since we started with some small amount, and then add 512MB chunks).
+  assert(((i * CHUNK) / MB) < (4 * 1024));
   puts("success");
 }
-
