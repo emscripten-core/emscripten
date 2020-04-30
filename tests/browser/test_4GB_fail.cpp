dd9cc5b4a598c936bee2e724b4f411458d2d2572
Alon Zakai <azakai@google.com>
2020-04-30 11:16:24 -0700

    simplify test

--- tests/browser/test_4GB_fail.cpp
+++ tests/browser/test_4GB_fail.cpp
@@ -1,29 +1,30 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void* allocation;
 
 int main() {
   const int MB = 1024 * 1024;
   const int CHUNK = 512 * MB;
-  int i;
-  for (i = 0;; i++) {
+  long num_allocations;
+  for (num_allocations = 0;; num_allocations++) {
     allocation = malloc(CHUNK);
     if (!allocation) {
       puts("failed to allocate any more");
       break;
     }
     printf("%ld: Allocated %d MB, total so far: %ld MB\n",
-           i, CHUNK / MB,
-           (i * CHUNK) / MB);
+           num_allocations, CHUNK / MB,
+           (num_allocations * CHUNK) / MB);
     printf("  (writing to make sure, to %p)\n", allocation);
     memset(allocation, 42, CHUNK);
   }
   // We should have allocated at least 2GB.
-  assert(((i * CHUNK) / MB) >= (2 * 1024));
+  assert(num_allocations >= 4);
   // We should have allocated less than 4GB (we can't get to exactly 4GB
   // since we started with some small amount, and then add 512MB chunks).
-  assert(((i * CHUNK) / MB) < (4 * 1024));
+  assert(num_allocations < 8);
   puts("success");
 }
