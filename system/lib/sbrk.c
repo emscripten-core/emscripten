66c8849668868da0ac09dfe99842eeecf27d9d94
Alon Zakai <azakai@google.com>
2020-04-30 15:32:46 -0700

    Check for a 32-bit overflow in sbrk, to error properly on trying to malloc >4GB (#11047)
    This increases code size a little, which seems unavoidable - this is an
    error we must always handle.
    
    Sadly the new test here is too much for the CI bot, so it's disabled there.

--- system/lib/sbrk.c
+++ system/lib/sbrk.c
@@ -29,12 +29,8 @@
 #define SET_ERRNO()
 #endif
 
-#define RETURN_ERROR() { \
-  SET_ERRNO()            \
-  return (void*)-1;      \
-}
-
 void *sbrk(intptr_t increment) {
+  uintptr_t old_size;
   // Enforce preserving a minimal 4-byte alignment for sbrk.
   increment = (increment + 3) & ~3;
 #if __EMSCRIPTEN_PTHREADS__
@@ -42,6 +38,7 @@ void *sbrk(intptr_t increment) {
   // work is necessary to keep things threadsafe, but we also make sure sbrk
   // itself is threadsafe so alternative allocators work. We do that by looping
   // and retrying if we hit interference with another thread.
+  intptr_t expected;
   while (1) {
 #endif // __EMSCRIPTEN_PTHREADS__
 
@@ -52,23 +49,27 @@ void *sbrk(intptr_t increment) {
     intptr_t old_brk = *sbrk_ptr;
 #endif
     intptr_t new_brk = old_brk + increment;
+    // Check for a 32-bit overflow, which would indicate that we are trying to
+    // allocate over 4GB, which is never possible in wasm32.
+    if (increment > 0 && (uint32_t)new_brk <= (uint32_t)old_brk) {
+      goto Error;
+    }
 #ifdef __wasm__
-    uintptr_t old_size = __builtin_wasm_memory_size(0) * WASM_PAGE_SIZE;
+    old_size = __builtin_wasm_memory_size(0) * WASM_PAGE_SIZE;
 #else
-    uintptr_t old_size = emscripten_get_heap_size();
+    old_size = emscripten_get_heap_size();
 #endif
     if (new_brk > old_size) {
       // Try to grow memory.
-      intptr_t diff = new_brk - old_size;
       if (!emscripten_resize_heap(new_brk)) {
-        RETURN_ERROR();
+        goto Error;
       }
     }
 #if __EMSCRIPTEN_PTHREADS__
     // Attempt to update the dynamic top to new value. Another thread may have
     // beat this one to the update, in which case we will need to start over
     // by iterating the loop body again.
-    intptr_t expected = old_brk;
+    expected = old_brk;
     __c11_atomic_compare_exchange_strong(
         (_Atomic(intptr_t)*)sbrk_ptr,
         &expected, new_brk,
@@ -88,6 +89,10 @@ void *sbrk(intptr_t increment) {
 #if __EMSCRIPTEN_PTHREADS__
   }
 #endif // __EMSCRIPTEN_PTHREADS__
+
+Error:
+  SET_ERRNO();
+  return (void*)-1;
 }
 
 int brk(intptr_t ptr) {
