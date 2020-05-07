45b36e917dd2acba82fa2038f89555ae5b1984e5
Sam Clegg <sbc@chromium.org>
2020-05-07 15:21:01 -0700

    Avoid raise RuntimeError in favor of diagnostics.error

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -1859,8 +1859,8 @@ class Ports(object):
       if sha512hash:
         actual_hash = hashlib.sha512(data).hexdigest()
         if actual_hash != sha512hash:
-          raise RuntimeError('Unexpected hash: ' + actual_hash + '\n'
-                             'If you are updating the port, please update the hash in the port module.')
+          shared.exit_with_error('Unexpected hash: ' + actual_hash + '\n'
+                                 'If you are updating the port, please update the hash in the port module.')
       open(fullpath, 'wb').write(data)
       State.retrieved = True
 
