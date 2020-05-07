91756bac7a187528e115a4a9b74e155b0fa11d51
Sam Clegg <sbc@chromium.org>
2020-05-06 21:37:25 -0400

    Remove SWAPPABLE_ASM_MODULE setting (#11090)
    For the wasm backend we stopped supporting this correctly back in #9922.
    This change means that the top level bindings and the `Module` bindings
    for were bound on first use.  This works fro wasm async compilation
    but not for replacign running modules.
    
    The only other use was emterpretify which is due to be removed
    soon in any case: #11020
    
    We may want to bring back full support for this later, but for now
    we don't support it correctly so better to remove it.

--- emscripten.py
+++ emscripten.py
@@ -1728,7 +1728,10 @@ def create_receiving(function_table_data, function_tables_defs, exported_impleme
   if not shared.Settings.DECLARE_ASM_MODULE_EXPORTS:
     receiving += 'exportAsmFunctions(asm);'
   else:
-    if not shared.Settings.SWAPPABLE_ASM_MODULE:
+    # with WASM_ASYNC_COMPILATION that asm object may not exist at this point in time
+    # so we need to support delayed assignment.
+    delay_assignment = (shared.Settings.WASM and shared.Settings.WASM_ASYNC_COMPILATION) and not shared.Settings.MINIMAL_RUNTIME
+    if not delay_assignment:
       if runtime_assertions:
         # assert on the runtime being in a valid state when calling into compiled code. The only
         # exceptions are some support code.
@@ -2639,7 +2642,10 @@ def create_receiving_wasm(exports, initializers):
   if shared.Settings.ASSERTIONS and not shared.Settings.MINIMAL_RUNTIME:
     runtime_assertions = RUNTIME_ASSERTIONS
 
-  if not shared.Settings.SWAPPABLE_ASM_MODULE:
+  # with WASM_ASYNC_COMPILATION that asm object may not exist at this point in time
+  # so we need to support delayed assignment.
+  delay_assignment = (shared.Settings.WASM and shared.Settings.WASM_ASYNC_COMPILATION) and not shared.Settings.MINIMAL_RUNTIME
+  if not delay_assignment:
     if runtime_assertions:
       # assert on the runtime being in a valid state when calling into compiled code. The only
       # exceptions are some support code
