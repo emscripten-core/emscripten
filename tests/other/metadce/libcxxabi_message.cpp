3f34aa87bf16cf6a660f3a2d67a246ad07057cd0
Alon Zakai <azakai@google.com>
2020-05-13 13:36:05 -0700

    more commenting

--- tests/other/metadce/libcxxabi_message.cpp
+++ tests/other/metadce/libcxxabi_message.cpp
@@ -3,8 +3,11 @@ struct Foo {
 };
 
 int main() {
-  // libcxxabi will emit some checks for proper static initialization, which
-  // can call abort_message()
+  // libc++abi will emit some checks for proper static initialization, which
+  // can call abort_message(). the point of this test is to see that getting
+  // that code does not lead to including printf logging which libc++abi
+  // does by default. And in general, that a tiny program like this should
+  // end up a tiny wasm file, with no other surprises.
   static Foo foo;
   return ++foo.counter;
 }
