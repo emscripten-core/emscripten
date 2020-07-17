emscripten-core/emscripten

commit 5f46470800ac6fa6875b0b54608b37667916acd1

Optimize readAsmConstArgs for performance and code size (#11227)

src/library.js
@@ -4547,32 +4547,26 @@ LibraryManager.library = {
     return STACK_BASE;
   },
 
+  _readAsmConstArgsArray: '=[]',
+  $readAsmConstArgs__deps: ['_readAsmConstArgsArray'],
   $readAsmConstArgs: function(sigPtr, buf) {
-    if (!readAsmConstArgs.array) {
-      readAsmConstArgs.array = [];
-    }
-    var args = readAsmConstArgs.array;
-    args.length = 0;
+#if ASSERTIONS
+    // Nobody should have mutated _readAsmConstArgsArray underneath us to be something else than an array.
+    assert(Array.isArray( __readAsmConstArgsArray));
+    // Input buffer must be a pre-existing varargs buffer, so already aligned to 4 bytes.
+    assert(buf % 4 == 0);
+#endif
+    __readAsmConstArgsArray.length = 0;
     var ch;
+    buf >>= 2; // Align buf up front to index Int32Array (HEAP32)
     while (ch = HEAPU8[sigPtr++]) {
-      if (ch === 100/*'d'*/ || ch === 102/*'f'*/) {
-        buf = (buf + 7) & ~7;
-        args.push(HEAPF64[(buf >> 3)]);
-        buf += 8;
-      } else
 #if ASSERTIONS
-      if (ch === 105 /*'i'*/)
+      assert(ch === 100/*'d'*/ || ch === 102/*'f'*/ || ch === 105 /*'i'*/);
 #endif
-      {
-        buf = (buf + 3) & ~3;
-        args.push(HEAP32[(buf >> 2)]);
-        buf += 4;
-      }
-#if ASSERTIONS
-      else abort("unexpected char in asm const signature " + ch);
-#endif
+      __readAsmConstArgsArray.push(ch < 105 ? HEAPF64[++buf >> 1] : HEAP32[buf]);
+      ++buf;
     }
-    return args;
+    return __readAsmConstArgsArray;
   },
 
 #if !DECLARE_ASM_MODULE_EXPORTS

commit 8eded23ae6c9c3a72189debc5277e26bda76f331

Fix padded double handling in EM_ASM varargs reading. Fixes #11665 which is a regression from #11227.

src/library.js
@@ -4540,17 +4540,23 @@ LibraryManager.library = {
 #if ASSERTIONS
     // Nobody should have mutated _readAsmConstArgsArray underneath us to be something else than an array.
     assert(Array.isArray( __readAsmConstArgsArray));
-    // Input buffer must be a pre-existing varargs buffer, so already aligned to 4 bytes.
-    assert(buf % 4 == 0);
+    // The input buffer is allocated on the stack, so it must be stack-aligned.
+    assert(buf % {{{ STACK_ALIGN }}} == 0);
 #endif
     __readAsmConstArgsArray.length = 0;
     var ch;
-    buf >>= 2; // Align buf up front to index Int32Array (HEAP32)
+    // Most arguments are i32s, so shift the buffer pointer so it is a plain
+    // index into HEAP32.
+    buf >>= 2;
     while (ch = HEAPU8[sigPtr++]) {
 #if ASSERTIONS
       assert(ch === 100/*'d'*/ || ch === 102/*'f'*/ || ch === 105 /*'i'*/);
 #endif
-      __readAsmConstArgsArray.push(ch < 105 ? HEAPF64[++buf >> 1] : HEAP32[buf]);
+      // A double takes two 32-bit slots, and must also be aligned - the backend
+      // will emit padding to avoid that.
+      var double = ch < 105;
+      if (double && (buf & 1)) buf++;
+      __readAsmConstArgsArray.push(double ? HEAPF64[buf++ >> 1] : HEAP32[buf]);
       ++buf;
     }
     return __readAsmConstArgsArray;

tests/core/test_em_asm.cpp
@@ -48,6 +48,43 @@ int main() {
   TEST()
 #undef FUNC
 
+  // Test mixing ints and doubles
+  EM_ASM({
+    console.log("idii");
+    out("a " + $0);
+    out("b " + $1);
+    out("c " + $2);
+    out("d " + $3);
+  }, 1, 3.14159, 3, 4);
+  EM_ASM({
+    console.log("diii");
+    out("a " + $0);
+    out("b " + $1);
+    out("c " + $2);
+    out("d " + $3);
+  }, 3.14159, 2, 3, 4);
+  EM_ASM({
+    console.log("iidi");
+    out("a " + $0);
+    out("b " + $1);
+    out("c " + $2);
+    out("d " + $3);
+  }, 1, 2, 3.14159, 4);
+  EM_ASM({
+    console.log("ddii");
+    out("a " + $0);
+    out("b " + $1);
+    out("c " + $2);
+    out("d " + $3);
+  }, 3.14159, 2.1828, 3, 4);
+  EM_ASM({
+    console.log("iddi");
+    out("a " + $0);
+    out("b " + $1);
+    out("c " + $2);
+    out("d " + $3);
+  }, 1, 3.14159, 2.1828, 4);
+
   printf("END\n");
   return 0;
 }

tests/core/test_em_asm.out
@@ -76,4 +76,29 @@ EM_ASM_DOUBLE :
     returned: 6.000000
   5.5 + 14.375
     returned: 19.875000
+idii
+a 1
+b 3.14159
+c 3
+d 4
+diii
+a 3.14159
+b 2
+c 3
+d 4
+iidi
+a 1
+b 2
+c 3.14159
+d 4
+ddii
+a 3.14159
+b 2.1828
+c 3
+d 4
+iddi
+a 1
+b 3.14159
+c 2.1828
+d 4
 END