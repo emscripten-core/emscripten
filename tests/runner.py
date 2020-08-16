d2de7fa3cd5f6808c73f5ba68b9df8b98f5492b7
Sam Clegg <sbc@chromium.org>
2020-08-16 08:15:47 -0700

    tests: Remove another unneeded argument from Runner.do_build

--- tests/runner.py
+++ tests/runner.py
@@ -1081,12 +1081,14 @@ class RunnerCore(RunnerMeta('TestCase', (unittest.TestCase,), {})):
   ## Does a complete test - builds, runs, checks output, etc.
   def do_run(self, src, expected_output, args=[], output_nicerizer=None,
              no_build=False,
-             js_engines=None, post_build=None, basename='src.cpp', libraries=[],
+             js_engines=None, post_build=None, libraries=[],
              includes=[], force_c=False,
              assert_returncode=0, assert_identical=False, assert_all=False,
              check_for_error=True):
     if force_c:
       basename = 'src.c'
+    else:
+      basename = 'src.cpp'
 
     if no_build:
       if src:
