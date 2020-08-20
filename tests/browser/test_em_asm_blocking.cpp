c35b84431afc7ab465d6ff897074998157300820
Alon Zakai <azakai@google.com>
2020-08-19 17:29:18 -0700

    moar test

--- tests/browser/test_em_asm_blocking.cpp
+++ tests/browser/test_em_asm_blocking.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <emscripten.h>
 #include <stdio.h>
 #include <thread>
@@ -7,8 +8,15 @@ std::atomic<int> ret;
 void foo() {
   int len = MAIN_THREAD_EM_ASM_INT({
     var elem = document.getElementById('elem');
+    window.almost_PI = 3.14159;
     return elem.innerText.length;
   });
+  double almost_PI = MAIN_THREAD_EM_ASM_DOUBLE({
+    // read a double from the main thread
+    return window.almost_PI;
+  });
+  printf("almost PI: %f\n", almost_PI);
+  assert(fabs(almost_PI - 3.14159) < 0.001);
   atomic_store(&ret, len);
 }
 
