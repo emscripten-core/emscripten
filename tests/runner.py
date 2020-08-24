b956da46d0c420e359225d5e396dfff828612017
Sam Clegg <sbc@chromium.org>
2020-08-24 08:37:25 -0700

    tests: more use of run_from_file. NFC. (#12021)

--- tests/runner.py
+++ tests/runner.py
@@ -574,7 +574,7 @@ class RunnerCore(unittest.TestCase, metaclass=RunnerMeta):
     dirname, basename = os.path.split(filename)
     output = shared.unsuffixed(basename) + suffix
     cmd = [compiler, filename, '-o', output] + self.get_emcc_args(main_file=True) + \
-        ['-I' + dirname, '-I' + os.path.join(dirname, 'include')] + \
+        ['-I.', '-I' + dirname, '-I' + os.path.join(dirname, 'include')] + \
         ['-I' + include for include in includes] + \
         libraries
 
