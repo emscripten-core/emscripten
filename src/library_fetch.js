d7ef8f38fe95c7bc2274473e4ec02fec410872d5
Sam Clegg <sbc@chromium.org>
2020-08-28 02:54:30 -0700

    Move static allocation for fetch queue from JS to C++ code (#12049)
    This avoids the runtime allocation on the JS side.
    
    See: #12040

--- src/library_fetch.js
+++ src/library_fetch.js
@@ -12,13 +12,7 @@ var LibraryFetch = {
 #else
   $Fetch__postset: 'Fetch.staticInit();',
 #endif
-  $fetchWorkQueue: '0',
   $Fetch: Fetch,
-  _emscripten_get_fetch_work_queue__deps: ['$fetchWorkQueue'],
-  _emscripten_get_fetch_work_queue: function() {
-    if (!fetchWorkQueue) fetchWorkQueue = _malloc(12);
-    return fetchWorkQueue;
-  },
   _emscripten_fetch_get_response_headers_length: _fetch_get_response_headers_length,
   _emscripten_fetch_get_response_headers: _fetch_get_response_headers,
   _emscripten_fetch_free: _fetch_free,
@@ -35,7 +29,7 @@ var LibraryFetch = {
 #if FETCH_SUPPORT_INDEXEDDB
   '$__emscripten_fetch_cache_data', '$__emscripten_fetch_load_cached_data', '$__emscripten_fetch_delete_cached_data',
 #endif
-  '_emscripten_get_fetch_work_queue', 'emscripten_is_main_browser_thread']
+  'emscripten_is_main_browser_thread']
 };
 
 mergeInto(LibraryManager.library, LibraryFetch);
