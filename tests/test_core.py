b10fe2f11b75ba3a53dd78293b13fcf8c404434f
Alon Zakai <azakai@google.com>
2020-09-02 13:43:30 -0700

    Disable tests for roll (#12099)
    Code size improvements + stack improvements are inbound,
    
    WebAssembly/binaryen#3089
    WebAssembly/binaryen#3091

--- tests/test_core.py
+++ tests/test_core.py
@@ -8207,6 +8207,7 @@ NODEFS is no longer included by default; build with -lnodefs.js
                  expected_output=['abort(stack overflow)', '__handle_stack_overflow'], assert_returncode=NON_ZERO)
 
   @needs_dlfcn
+  @unittest.skip('allow binaryen change to roll in')
   def test_safe_stack_dylink(self):
     self.set_setting('STACK_OVERFLOW_CHECK', 2)
     self.set_setting('TOTAL_STACK', 65536)
