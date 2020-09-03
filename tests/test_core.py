95b42d743a6261a19ec22c60918f34806f29670a
Thomas Lively <tlively@google.com>
2020-09-03 13:28:54 -0700

    Update test

--- tests/test_core.py
+++ tests/test_core.py
@@ -5902,9 +5902,7 @@ return malloc(size);
     self.emcc_args.append('-Wno-c++11-narrowing')
     self.emcc_args.append('-mfpu=neon')
     self.emcc_args.append('-msimd128')
-    self.do_run(open(path_from_root('tests', 'neon', 'test_neon_wasm_simd.cpp')).read(), 'Success!')
-    self.build(open(path_from_root('tests', 'neon', 'test_neon_wasm_simd.cpp')).read(),
-               self.get_dir(), os.path.join(self.get_dir(), 'src.cpp'))
+    self.do_runf(path_from_root('tests', 'neon', 'test_neon_wasm_simd.cpp'), 'Success!')
 
   # Tests invoking the SIMD API via x86 SSE1 xmmintrin.h header (_mm_x() functions)
   @wasm_simd
