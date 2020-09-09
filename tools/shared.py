f389f6d1e1b377f12483a4d47ff0095a516f1efa
Prashanth Nethi <prashant@adobe.com>
2020-09-10 00:09:53 +0530

    Addressing code review comments. Using default values for registering the warning

--- tools/shared.py
+++ tools/shared.py
@@ -66,7 +66,7 @@ diagnostics.add_warning('deprecated')
 diagnostics.add_warning('version-check')
 diagnostics.add_warning('export-main')
 diagnostics.add_warning('unused-command-line-argument', shared=True)
-diagnostics.add_warning('pthreads-mem-growth', enabled=True, part_of_all=False)
+diagnostics.add_warning('pthreads-mem-growth')
 
 
 def exit_with_error(msg, *args):
