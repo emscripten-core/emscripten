b1b0884edea38b172d009909f9a94ed00f21c524
Alon Zakai <azakai@google.com>
2020-09-24 12:42:49 -0700

    fix

--- src/library.js
+++ src/library.js
@@ -784,7 +784,7 @@ LibraryManager.library = {
     abort('Assertion failed: ' + (condition ? UTF8ToString(condition) : 'unknown condition') + ', at: ' + [filename ? UTF8ToString(filename) : 'unknown filename', line, func ? UTF8ToString(func) : 'unknown function']);
   },
 
-  terminate__sig: 'vi',
+  __cxa_call_unexpected__sig: 'vi',
   __cxa_call_unexpected: function(exception) {
     err('Unexpected exception thrown, this is not properly supported - aborting');
 #if !MINIMAL_RUNTIME
