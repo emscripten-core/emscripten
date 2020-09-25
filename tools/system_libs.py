704a676082caeeb08bd8440d7e1e631fecd0432c
Alon Zakai <azakai@google.com>
2020-09-25 11:02:45 -0700

    cleaner [ci skip]

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -635,7 +635,7 @@ class AsanInstrumentedLibrary(Library):
     return super(AsanInstrumentedLibrary, cls).get_default_variation(is_asan=shared.Settings.USE_ASAN, **kwargs)
 
 
-class libcompiler_rt(MuslInternalLibrary, MTLibrary):
+class libcompiler_rt(MTLibrary):
   name = 'libcompiler_rt'
   # compiler_rt files can't currently be part of LTO although we are hoping to remove this
   # restriction soon: https://reviews.llvm.org/D71738
