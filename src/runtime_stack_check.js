94f568968414d26ef19723c8fcc975a226f61a76
Alon Zakai <azakai@google.com>
2020-10-02 14:08:29 -0700

    fix

--- src/runtime_stack_check.js
+++ src/runtime_stack_check.js
@@ -8,15 +8,13 @@
 // Initializes the stack cookie. Called at the startup of main and at the startup of each thread in pthreads mode.
 function writeStackCookie() {
 #if ASSERTIONS
-  assert(STACK_MAX > 0);
   assert((STACK_MAX & 3) == 0);
 #endif
   // The stack grows downwards
   HEAPU32[(STACK_MAX >> 2)+1] = 0x2135467;
   HEAPU32[(STACK_MAX >> 2)+2] = 0x89BACDFE;
-#if !USE_ASAN
+#if !USE_ASAN && !SAFE_HEAP // ASan and SAFE_HEAP check address 0 themselves
   // Also test the global address 0 for integrity.
-  // We don't do this with ASan because ASan does its own checks for this.
   HEAP32[0] = 0x63736d65; /* 'emsc' */
 #endif
 }
@@ -27,9 +25,8 @@ function checkStackCookie() {
   if (cookie1 != 0x2135467 || cookie2 != 0x89BACDFE) {
     abort('Stack overflow! Stack cookie has been overwritten, expected hex dwords 0x89BACDFE and 0x2135467, but received 0x' + cookie2.toString(16) + ' ' + cookie1.toString(16));
   }
-#if !USE_ASAN
+#if !USE_ASAN && !SAFE_HEAP // ASan and SAFE_HEAP check address 0 themselves
   // Also test the global address 0 for integrity.
-  // We don't do this with ASan because ASan does its own checks for this.
   if (HEAP32[0] !== 0x63736d65 /* 'emsc' */) abort('Runtime error: The application has corrupted its heap memory area (address zero)!');
 #endif
 }
