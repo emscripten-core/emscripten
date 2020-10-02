c699bcc82b0157d9290de026eb84e700eb70a0a5
Tibor Klajnscek <tibor.klajnscek@gmail.com>
2020-10-02 16:47:45 +0200

    Reused an existing test instead of adding a new test (removed pthread/stack_overflow_check.c)

--- tests/test_core.py
+++ tests/test_core.py
@@ -8176,12 +8176,10 @@ NODEFS is no longer included by default; build with -lnodefs.js
     self.emcc_args += ['-DPOOL']
     test()
 
-  @node_pthreads
-  def test_pthread_stack_overflow_check(self):
-    # test pthreads with STACK_OVERFLOW_CHECK==2 and embind (https://github.com/emscripten-core/emscripten/issues/12356)
-    self.set_setting('STACK_OVERFLOW_CHECK', 2)
+    print('with embind and stack overflow checks (see #12356)')
+    self.set_setting('STACK_OVERFLOW_CHECK', '2')
     self.emcc_args += ['--bind']
-    self.do_run_in_out_file_test('tests', 'core', 'pthread', 'stack_overflow_check.c')
+    test()
 
   @node_pthreads
   def test_pthread_exceptions(self):
