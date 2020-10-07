8fae638b7524871cd12281422c9c2faaec8b635a
Alon Zakai <azakai@google.com>
2020-10-07 16:37:52 -0700

    fix an existing test

--- tests/test_other.py
+++ tests/test_other.py
@@ -189,9 +189,11 @@ class other(RunnerCore):
     # self.assertContained('hello, world!', self.run_js('hello_world.mjs'))
 
   def test_emcc_output_worker_mjs(self):
-    self.run_process([EMCC, '-o', 'hello_world.mjs', '-pthread', '-O1', path_from_root('tests', 'hello_world.c')])
+    self.run_process([EMCC, '-o', 'hello_world.mjs', '-pthread', '-O1',
+                      path_from_root('tests', 'hello_world.c'),
+                      '-s', 'EXPORT_NAME=FooModule'])
     with open('hello_world.mjs') as f:
-      self.assertContained('export default Module;', f.read())
+      self.assertContained('export default FooModule;', f.read())
     with open('hello_world.worker.js') as f:
       self.assertContained('import(', f.read())
 
