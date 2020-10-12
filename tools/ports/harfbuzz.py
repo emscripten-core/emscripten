6e59601200209c9a1b46a8468e37508875503fae
Paul m. p. P <mail.peny@free.fr>
2020-10-13 00:48:17 +0200

    whitespaces

--- tools/ports/harfbuzz.py
+++ tools/ports/harfbuzz.py
@@ -43,7 +43,7 @@ def get(ports, settings, shared):
       '-DFREETYPE_LIBRARY=' + freetype_lib,
       '-DHB_HAVE_FREETYPE=ON'
     ]
-        
+
     extra_cflags = []
 
     if settings.RELOCATABLE:
@@ -55,7 +55,7 @@ def get(ports, settings, shared):
     if len(extra_cflags):
       configure_args += ['-DCMAKE_CXX_FLAGS="{}"'.format(' '.join(extra_cflags))]
       configure_args += ['-DCMAKE_C_FLAGS="{}"'.format(' '.join(extra_cflags))]
-   
+
     building.configure(configure_args)
     building.make(['make', '-j%d' % building.get_num_cores(), '-C' + dest_path, 'install'])
 
