112b38f34b2f26712c66f19bed7ff70483bf8304
Rocco Musolino <roccomuso@users.noreply.github.com>
2020-10-22 18:25:44 +0200

    Add a check if "document" exists when looking for the script dir (#12553)

--- src/shell.js
+++ src/shell.js
@@ -290,7 +290,7 @@ if (ENVIRONMENT_IS_SHELL) {
 if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) {
   if (ENVIRONMENT_IS_WORKER) { // Check worker, not web, since window could be polyfilled
     scriptDirectory = self.location.href;
-  } else if (document.currentScript) { // web
+  } else if (typeof document !== 'undefined' && document.currentScript) { // web
     scriptDirectory = document.currentScript.src;
   }
 #if MODULARIZE
