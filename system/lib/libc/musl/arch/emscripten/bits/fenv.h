0e2b32720d3f3301c04e93253c484b4abce7b637
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-12-11 13:06:02 +0100

    Deduplicate arch/emscripten/bits directory

--- system/lib/libc/musl/arch/emscripten/bits/fenv.h
+++ system/lib/libc/musl/arch/emscripten/bits/fenv.h
@@ -6,22 +6,6 @@
 #define FE_TOWARDZERO 0xc00
 
 typedef unsigned short fexcept_t;
-
-typedef struct {
-	unsigned short __control_word;
-	unsigned short __unused1;
-	unsigned short __status_word;
-	unsigned short __unused2;
-	unsigned short __tags;
-	unsigned short __unused3;
-	unsigned int __eip;
-	unsigned short __cs_selector;
-	unsigned int __opcode:11;
-	unsigned int __unused4:5;
-	unsigned int __data_offset;
-	unsigned short __data_selector;
-	unsigned short __unused5;
-	unsigned int __mxcsr;
-} fenv_t;
+typedef unsigned short fenv_t;
 
 #define FE_DFL_ENV      ((const fenv_t *) -1)
--- system/lib/libc/musl/arch/emscripten/bits/posix.h
+++ system/lib/libc/musl/arch/emscripten/bits/posix.h
@@ -1,2 +1,9 @@
+#ifdef __wasm64__
+// 64-bit wide pointers under wasm64
+#define _POSIX_V6_LP64_OFF64  1
+#define _POSIX_V7_LP64_OFF64  1
+#else
+// 32-bit wide pointers under wasm32
 #define _POSIX_V6_ILP32_OFFBIG  1
 #define _POSIX_V7_ILP32_OFFBIG  1
+#endif
