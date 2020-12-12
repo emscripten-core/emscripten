2ae01a7bdc3335a8d188c453e5cabb69479427ed
Sam Clegg <sbc@chromium.org>
2020-12-11 21:33:06 -0800

    Re-enable test

--- tests/test_other.py
+++ tests/test_other.py
@@ -6791,8 +6791,7 @@ int main() {
     # we don't metadce with linkable code! other modules may want stuff
     # TODO(sbc): Investivate why the number of exports is order of magnitude
     # larger for wasm backend.
-    # TODO(sbc): Rename once llvm change rolls
-    #'main_module_2': (['-O3', '-s', 'MAIN_MODULE=2'], [], [],  10297), # noqa
+    'main_module_2': (['-O3', '-s', 'MAIN_MODULE=2'], [], [],  10297), # noqa
   })
   def test_metadce_hello(self, *args):
     self.run_metadce_test('hello_world.cpp', *args)
