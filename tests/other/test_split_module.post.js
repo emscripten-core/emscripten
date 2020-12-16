27df8e5ae17f8d07fdf400539c5dea03cf717f0b
Thomas Lively <tlively@google.com>
2020-12-16 15:52:41 -0800

    [ci skip] Simplify test
    Still waiting for Binaryen fix to roll.

--- tests/other/test_split_module.post.js
+++ tests/other/test_split_module.post.js
@@ -1,21 +1,13 @@
 function saveProfileData() {
   var __write_profile = Module['asm']['__write_profile'];
   if (__write_profile) {
-    var capacity = 1;
-    var offset;
-    var len;
-    while (true) {
-      offset = _malloc(capacity);
-      len = __write_profile(offset, capacity);
-      if (len <= capacity) {
-        console.log('writing profile of size', len, 'with capacity', capacity);
-        break;
-      }
-      _free(offset);
-      capacity *= 2;
-    }
+    var len = __write_profile(0, 0);
+    var offset = _malloc(len);
+    var actualLen = __write_profile(offset, len);
     var profile_data = new Uint8Array(buffer, offset, len);
     nodeFS.writeFileSync('profile.data', profile_data);
+    console.log('wrote profile of', actualLen, 'bytes (allocated', len, 'bytes)');
+    _free(offset);
   }
 
   // Say hello *after* recording the profile so that all functions are deferred.
