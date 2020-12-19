45bbade16a2d736a38e9559922c60cc6897c9084
Antetokounpo <antor.232@outlook.com>
2020-12-19 10:59:58 -0500

    Invert logic for skipping tests when EMTEST_NO_D8 is set

--- tests/test_core.py
+++ tests/test_core.py
@@ -38,9 +38,9 @@ logger = logging.getLogger("test_core")
 
 def wasm_simd(f):
   def decorated(self):
+    if os.getenv('EMTEST_NO_D8'):
+      unittest.skip('Skipped because EMTEST_NO_D8 is set')
     if not config.V8_ENGINE or config.V8_ENGINE not in config.JS_ENGINES:
-      if os.getenv('EMTEST_NO_D8') is None:
-        raise Exception('d8 is needed to run SIMD tests, set EMTEST_NO_D8 if you want to skip them')
       self.skipTest('wasm simd only supported in d8 for now')
     if not self.is_wasm():
       self.skipTest('wasm2js only supports MVP for now')
@@ -56,9 +56,9 @@ def wasm_simd(f):
 
 def bleeding_edge_wasm_backend(f):
   def decorated(self):
+    if os.getenv('EMTEST_NO_D8'):
+      unittest.skip('Skipped because EMTEST_NO_D8 is set')
     if not config.V8_ENGINE or config.V8_ENGINE not in config.JS_ENGINES:
-      if os.getenv('EMTEST_NO_D8') is None:
-        raise Exception('d8 is needed to run SIMD tests, set EMTEST_NO_D8 if you want to skip them')
       self.skipTest('only works in d8 for now')
     if not self.is_wasm():
       self.skipTest('wasm2js only supports MVP for now')
