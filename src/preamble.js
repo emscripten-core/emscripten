924973f96e7251e0e8999871121894cbc2f1aa44
Alon Zakai <azakai@google.com>
2021-02-05 10:49:50 -0800

    clarify the pthread case

--- src/preamble.js
+++ src/preamble.js
@@ -1122,16 +1122,22 @@ function createWasm() {
       exports = Asyncify.instrumentWasmExports(exports);
 #endif
 #if USE_OFFSET_CONVERTER
-      // We have no way to create an OffsetConverter in this code path since we
-      // have no access to the wasm binary (only the user does). Instead, create
-      // a fake one that reports we cannot identify functions from their binary
-      // offsets.
-      wasmOffsetConverter = {
-        getName: function() {
-          return 'unknown-due-to-instantiateWasm';
-        }
-      };
-      {{{ runOnMainThread("removeRunDependency('offset-converter');") }}}
+      {{{
+        runOnMainThread(`
+          // We have no way to create an OffsetConverter in this code path since
+          // we have no access to the wasm binary (only the user does). Instead,
+          // create a fake one that reports we cannot identify functions from
+          // their binary offsets.
+          // Note that we only do this on the main thread, as the workers
+          // receive the OffsetConverter data from there.
+          wasmOffsetConverter = {
+            getName: function() {
+              return 'unknown-due-to-instantiateWasm';
+            }
+          };
+          removeRunDependency('offset-converter');
+        `)
+      }}}
 #endif
       return exports;
     } catch(e) {
