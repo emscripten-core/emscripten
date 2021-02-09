4d829dae544e4e7e5dd27a0bccc401fc2172d047
Alon Zakai <azakai@google.com>
2021-02-09 12:58:16 -0800

    Fix Box2D benchmark compilation: don't assign a pointer to a bool

--- tests/third_party/box2d/Box2D/Collision/Shapes/b2ChainShape.h
+++ tests/third_party/box2d/Box2D/Collision/Shapes/b2ChainShape.h
@@ -95,8 +95,8 @@ inline b2ChainShape::b2ChainShape()
 	m_radius = b2_polygonRadius;
 	m_vertices = NULL;
 	m_count = 0;
-	m_hasPrevVertex = NULL;
-	m_hasNextVertex = NULL;
+	m_hasPrevVertex = false;
+	m_hasNextVertex = false;
 }
 
 #endif
