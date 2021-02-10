39f9f561633388853c86a027bc0d193fcda4ccbe
Alon Zakai <azakai@google.com>
2021-02-10 15:45:29 -0800

    work [ci skip]

--- tests/fuzz/fuzz_exceptions.py
+++ tests/fuzz/fuzz_exceptions.py
@@ -34,35 +34,41 @@ data allows for truncation easily, but changing bytes earlier can lead to
 dramatic differences in the output.)
 '''
 
+import json
 import random
 
-class Generator:
+class StructuredRandomData:
     # The overall maximum number of nodes we want.
-    MAX_NODES = 20
+    MAX_NODES = 200
 
-    # The maximum size of an array.
-    MAX_ARRAY = 7
+    # The minimum and maximum size of an array.
+    MIN_ARRAY_SIZE = 3
+    MAX_ARRAY_SIZE = 10
 
     # How much shorter arrays are the deeper we go.
-    ARRAY_DEPTH_SHORTENING = 0.9
+    ARRAY_DEPTH_SHORTENING = 0.95
 
     # How likely we are to create an array instead of an int.
     ARRAY_PROBABILITY = 0.3
 
     # How much each extra level of depth makes it less likely to have an array.
-    ARRAY_DEPTH_UNLIKELIHOOD = 0.9
+    ARRAY_DEPTH_UNLIKELIHOOD = 0.95
 
     # The maximum depth.
-    MAX_DEPTH = 5
+    MAX_DEPTH = 7
 
     def __init__(self):
         self.emitted_nodes = 0
+        # The root is always an array, so that we can append enough items to get
+        # to the size we want.
         self.root = self.make_array(depth=0)
         while self.emitted_nodes < self.MAX_NODES:
-            self.root.append(self.make_array(depth=1))
+            self.root.append(self.make(depth=1))
 
     def make_array(self, depth):
-        size = random.randrange(0, self.MAX_ARRAY - int(depth * self.ARRAY_DEPTH_SHORTENING))
+        max_size = self.MAX_ARRAY_SIZE - int(depth * self.ARRAY_DEPTH_SHORTENING)
+        max_size = max(max_size, self.MIN_ARRAY_SIZE + 1)
+        size = random.randint(self.MIN_ARRAY_SIZE, max_size)
         size = min(size, self.MAX_NODES - self.emitted_nodes)
         self.emitted_nodes += 1
         return [self.make(depth + 1) for i in range(0, size)]
@@ -78,7 +84,7 @@ class Generator:
             return self.make_array(depth)
         return self.make_int()
 
-print(Generator().root)
+print(json.dumps(StructuredRandomData().root, indent='  '))
 
 
 
