13a3741e3f9f7c54237ecc817b86944d09e0aa61
V-for-Vasili <vasili.skurydzin@protonmail.com>
2021-03-04 22:32:30 +0000

    Improve endianness check

--- src/runtime_assertions.js
+++ src/runtime_assertions.js
@@ -5,6 +5,17 @@
  */
 
 #if ASSERTIONS
+
+// Endianness check
+#if !SUPPORT_BIG_ENDIAN
+(function() {
+  var h16 = new Int16Array(1);
+  var h8 = new Int8Array(h16.buffer);
+  h16[0] = 0x6373;
+  if (h8[0] !== 0x73 || h8[1] !== 0x63) throw 'Runtime error: expected the system to be little-endian! (Run with -s SUPPORT_BIG_ENDIAN=1 to bypass)';
+})();
+#endif
+
 function abortFnPtrError(ptr, sig) {
 #if ASSERTIONS >= 2
 	var possibleSig = '';
