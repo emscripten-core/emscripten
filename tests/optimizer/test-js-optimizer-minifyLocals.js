7e6f4e7c82cc1bcf3dfacd13d59267f0b503a0a6
Alon Zakai <azakai@google.com>
2021-03-04 15:57:57 -0800

    wip [ci skip]

--- tests/optimizer/test-js-optimizer-minifyLocals.js
+++ tests/optimizer/test-js-optimizer-minifyLocals.js
@@ -1,6 +1,19 @@
 function main(x) {
  constructor(); // special word in JS, must not be confused by it
- x(); // this should assert!
+}
+function constructor() {
+}
+function foo() {
+  var bar = 10;
+  return bar;
+}
+function bar(foo, a, b) {
+  var main, c, d, e;
+  main = foo;
+  c = a;
+  d = b;
+  e = foo;
+  return foo + a + b + main + c + d + e;
 }
 // EMSCRIPTEN_GENERATED_FUNCTIONS
-// EXTRA_INFO: { "globals": { "main": "a", "constructor": "b" } }
+// EXTRA_INFO: { "globals": { "main": "a", "constructor": "b", "foo": "c", "bar": "d" } }
