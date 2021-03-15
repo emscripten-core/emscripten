f613c53d5febd44da2c440aecd0269cf8c493f54
Jukka Jylänki <jujjyl@gmail.com>
2021-03-15 09:11:03 +0200

    Update create_entry_points.py script

--- tools/create_entry_points.py
+++ tools/create_entry_points.py
@@ -7,25 +7,28 @@
 """Tool for creating/maintains the python launcher scripts for all the emscripten
 python tools.
 
-This tools makes copies or `run_python.sh` and `run_python.bat` script for each
-entry point.  On UNIX we previously used symbolic links for simplicity but this
-breaks MINGW users on windows who want use the shell script launcher but don't
-have symlink support (sigh).
+This tools makes copies or `run_python.sh/.bat` and `run_python_compiler.sh/.bat`
+script for each entry point. On UNIX we previously used symbolic links for
+simplicity but this breaks MINGW users on windows who want use the shell script
+launcher but don't have symlink support.
 """
 
 import os
 import shutil
 import sys
 
+compiler_entry_points = '''
+emcc
+em++
+'''.split()
+
 entry_points = '''
 emar
 embuilder
-emcc
 emcmake
 em-config
 emconfigure
 emmake
-em++
 emranlib
 emrun
 emscons
@@ -39,13 +42,23 @@ tests/runner
 def main():
   tools_dir = os.path.dirname(os.path.abspath(__file__))
   root_dir = os.path.dirname(tools_dir)
-  sh_file = os.path.join(tools_dir, 'run_python.sh')
-  bat_file = os.path.join(tools_dir, 'run_python.bat')
-  for entry_point in entry_points:
-    if os.path.exists(os.path.join(root_dir, entry_point)):
-        os.remove(os.path.join(root_dir, entry_point))
-    shutil.copy2(sh_file, os.path.join(root_dir, entry_point))
-    shutil.copy2(bat_file, os.path.join(root_dir, entry_point) + '.bat')
+
+  def generate_entry_points(cmd, path):
+    sh_file = path + '.sh'
+    bat_file = path + '.bat'
+    for entry_point in cmd:
+      dst = os.path.join(root_dir, entry_point)
+      if os.path.exists(dst):
+          os.remove(dst)
+      shutil.copy2(sh_file, dst)
+
+      dst += '.bat'
+      if os.path.exists(dst):
+          os.remove(dst)
+      shutil.copy2(bat_file, dst)
+
+  generate_entry_points(entry_points, os.path.join(tools_dir, 'run_python'))
+  generate_entry_points(compiler_entry_points, os.path.join(tools_dir, 'run_python_compiler'))
 
 
 if __name__ == '__main__':
