79400fd8d5a69f372e0422e996709c2bb248da29
Jukka Jylänki <jujjyl@gmail.com>
2021-03-16 12:50:05 +0200

    Add missing files.

---
+++ tools/run_python_compiler.bat
@@ -0,0 +1,18 @@
+:: Entry point for running python scripts on windows systems.
+:: To modify this file, edit `tools/run_python.bat` and then run
+:: `tools/create_entry_points.py`
+
+@setlocal
+@set EM_PY=%EMSDK_PYTHON%
+@if "%EM_PY%"=="" (
+  set EM_PY=python
+)
+
+@if "%EMCC_CCACHE%"=="" (
+  :: Do regular invocation of em++.py compiler
+  "%EM_PY%" "%~dp0\%~n0.py" %*
+) else (
+  :: Remove the ccache env. var, invoke ccache and re-enter this script to take the above branch.
+  set EMCC_CCACHE=
+  ccache "%~dp0\%~n0.bat" %*
+)
