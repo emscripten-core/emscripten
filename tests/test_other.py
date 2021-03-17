b8d6eccb2f596c7f0a382909e0b7d1cd39768baf
Sam Clegg <sbc@chromium.org>
2021-03-17 12:39:23 -0700

    Add support for simplified `-s` list parsing
    With this change the opening and closing braced are no
    longer required.  `e.g. -s EXPORTED_FUNCTIONS=_foo,_bar`
    
    This form is more inline with traditional compiler arguments
    where lists of C symbols are to be passed.

--- tests/test_other.py
+++ tests/test_other.py
@@ -6210,8 +6210,6 @@ high = 1234
     self.assertNotContained('a problem occurred in evaluating the content after a "-s", specifically', err)
 
   def test_dash_s_wrong_type(self):
-    err = self.expect_fail([EMCC, test_file('hello_world.cpp'), '-s', 'EXPORTED_FUNCTIONS=foo'])
-    self.assertContained("error: setting `EXPORTED_FUNCTIONS` expects `<class 'list'>` but got `<class 'str'>`", err)
     err = self.expect_fail([EMCC, test_file('hello_world.cpp'), '-s', 'EXIT_RUNTIME=[foo,bar]'])
     self.assertContained("error: setting `EXIT_RUNTIME` expects `<class 'int'>` but got `<class 'list'>`", err)
 
@@ -7956,7 +7954,7 @@ test_module().then((test_module_instance) => {
       f.write(b'!<arch>\n')
     self.assertTrue(building.is_ar(fname))
 
-  def test_emcc_parsing(self):
+  def test_dash_s_list_parsing(self):
     create_file('src.c', r'''
         #include <stdio.h>
         void a() { printf("a\n"); }
@@ -7977,6 +7975,10 @@ test_module().then((test_module_instance) => {
       ('EXPORTED_FUNCTIONS=[_a,_b,_c,_d]', ''),
       # No quotes with spaces
       ('EXPORTED_FUNCTIONS=[_a, _b, _c, _d]', ''),
+      # No brackets needed either
+      ('EXPORTED_FUNCTIONS=_a,_b,_c,_d', ''),
+      # No brackets with spaced
+      ('EXPORTED_FUNCTIONS=_a, _b, _c, _d', ''),
       # extra space at end - should be ignored
       ("EXPORTED_FUNCTIONS=['_a', '_b', '_c', '_d' ]", ''),
       # extra newline in response file - should be ignored
