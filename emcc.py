8a846d673245153ac2ba2b9aff565b5947ea4049
Sam Clegg <sbc@chromium.org>
2021-03-17 12:10:40 -0700

    Fail if compiler output file is a directory (#13689)
    When the output is a wasm file wasm-ld already fails
    in this way.  This change means that js and html output
    also fail in the same way.
    
    See #13684

--- emcc.py
+++ emcc.py
@@ -24,22 +24,23 @@ emcc can be influenced by a few environment variables:
 """
 
 
+import base64
 import json
 import logging
 import os
 import re
 import shlex
+import shutil
 import stat
 import sys
 import time
-import base64
 from enum import Enum
 from subprocess import PIPE
 
 import emscripten
 from tools import shared, system_libs
 from tools import colored_logger, diagnostics, building
-from tools.shared import unsuffixed, unsuffixed_basename, WINDOWS, safe_move, safe_copy
+from tools.shared import unsuffixed, unsuffixed_basename, WINDOWS, safe_copy
 from tools.shared import run_process, read_and_preprocess, exit_with_error, DEBUG
 from tools.shared import do_replace
 from tools.response_file import substitute_response_files
@@ -2273,6 +2274,19 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
   return 0
 
 
+def move_file(src, dst):
+  logging.debug('move: %s -> %s', src, dst)
+  if os.path.isdir(dst):
+    exit_with_error(f'cannot write output file `{dst}`: Is a directory')
+  src = os.path.abspath(src)
+  dst = os.path.abspath(dst)
+  if src == dst:
+    return
+  if dst == os.devnull:
+    return
+  shutil.move(src, dst)
+
+
 def post_link(options, in_wasm, wasm_target, target):
   global final_js
 
@@ -2425,7 +2439,7 @@ def post_link(options, in_wasm, wasm_target, target):
       js_target = unsuffixed(target) + '.js'
 
     # The JS is now final. Move it to its final location
-    safe_move(final_js, js_target)
+    move_file(final_js, js_target)
 
     if not shared.Settings.SINGLE_FILE:
       generated_text_files_with_native_eols += [js_target]
@@ -3145,9 +3159,13 @@ def generate_traditional_runtime_html(target, options, js_target, target_basenam
 
   html_contents = do_replace(shell, '{{{ SCRIPT }}}', script.replacement())
   html_contents = tools.line_endings.convert_line_endings(html_contents, '\n', options.output_eol)
-  # Force UTF-8 output for consistency across platforms and with the web.
-  with open(target, 'wb') as f:
-    f.write(html_contents.encode('utf-8'))
+
+  try:
+    with open(target, 'wb') as f:
+      # Force UTF-8 output for consistency across platforms and with the web.
+      f.write(html_contents.encode('utf-8'))
+  except OSError as e:
+    exit_with_error(f'cannot write output file: {e}')
 
 
 def minify_html(filename):
@@ -3222,7 +3240,7 @@ def generate_worker_js(target, js_target, target_basename):
 
   # compiler output goes in .worker.js file
   else:
-    safe_move(js_target, unsuffixed(js_target) + '.worker.js')
+    move_file(js_target, unsuffixed(js_target) + '.worker.js')
     worker_target_basename = target_basename + '.worker'
     proxy_worker_filename = (shared.Settings.PROXY_TO_WORKER_FILENAME or worker_target_basename) + '.js'
 
