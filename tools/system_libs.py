101c208b0e836c106360ce4693f0b5d31ffd678e
Sam Clegg <sbc@chromium.org>
2021-03-18 14:46:55 -0700

    Remove redundant argument to fetch_project. NFC (#13703)
    Given that we detect the type based on filename for other cases it seems
    like we don't need this extra arg.

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -1664,7 +1664,7 @@ class Ports(object):
   name_cache = set()
 
   @staticmethod
-  def fetch_project(name, url, subdir, is_tarbz2=False, sha512hash=None):
+  def fetch_project(name, url, subdir, sha512hash=None):
     # To compute the sha512 hash, run `curl URL | sha512sum`.
     fullname = os.path.join(Ports.get_dir(), name)
 
@@ -1706,7 +1706,7 @@ class Ports(object):
               Ports.clear_project_build(name)
             return
 
-    if is_tarbz2:
+    if url.endswith('.tar.bz2'):
       fullpath = fullname + '.tar.bz2'
     elif url.endswith('.tar.gz'):
       fullpath = fullname + '.tar.gz'
@@ -1738,7 +1738,7 @@ class Ports(object):
       open(fullpath, 'wb').write(data)
 
     def check_tag():
-      if is_tarbz2:
+      if url.endswith('.tar.bz2'):
         names = tarfile.open(fullpath, 'r:bz2').getnames()
       elif url.endswith('.tar.gz'):
         names = tarfile.open(fullpath, 'r:gz').getnames()
