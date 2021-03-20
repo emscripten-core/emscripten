42cd632ebfc187eeab263baeea49fa011f14b530
Heejin Ahn <aheejin@gmail.com>
2021-03-19 19:56:07 -0700

    Fix parentheses mix-up in batch files in Windows (#13698)
    After #13498, `test_asyncify_onlylist_a` and `test_asyncify_onlylist_b`
    started to failing. #13498 introduced `if ( ... ) else ( ... )` in
    emcc/em++.bat, and it turns out the parentheses inside the command line
    can be mixed with those `if`-`else`'s parentheses themselves. This does
    not cause errors for every command line that contain parentheses, but
    when there are both a comma and parentheses, this can trigger errors.
    
    For example, `test_asyncify_onlylist_a`'s argument contains something
    like this:
    ```
    'ASYNCIFY_ONLY=["main",...,"foo(int,double)",...]'
    ```
    Here the comma between `int` and `double` is mistaken as an item
    separator within the list, and `)` after `double` is consequently
    mistaken as ending `if`, because the whole body is within an `if`.
    
    This PR fixes the problem by assigning those argument `%*` into a
    variable `ARGS`, and within `if` and `else`, use it with a substitution
    of `)` with the escaped end-paren `^)`.

--- emcc.bat
+++ emcc.bat
@@ -1,5 +1,5 @@
 :: Entry point for running python scripts on windows systems.
-:: To modify this file, edit `tools/run_python.bat` and then run
+:: To modify this file, edit `tools/run_python_compiler.bat` and then run
 :: `tools/create_entry_points.py`
 
 @setlocal
@@ -8,11 +8,12 @@
   set EM_PY=python
 )
 
+@set ARGS=%*
 @if "%_EMCC_CCACHE%"=="" (
   :: Do regular invocation of em++.py compiler
-  "%EM_PY%" "%~dp0\%~n0.py" %*
+  "%EM_PY%" "%~dp0\%~n0.py" %ARGS:)=^)%
 ) else (
   :: Remove the ccache env. var, invoke ccache and re-enter this script to take the above branch.
   set _EMCC_CCACHE=
-  ccache "%~dp0\%~n0.bat" %*
+  ccache "%~dp0\%~n0.bat" %ARGS:)=^)%
 )
