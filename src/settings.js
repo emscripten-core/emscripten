f2d1f97a3073fb57f9d1c508797f923e1be121e9
Heejin Ahn <aheejin@gmail.com>
2021-04-01 17:01:39 -0700

    more typo fix

--- src/settings.js
+++ src/settings.js
@@ -1841,7 +1841,7 @@ var USE_OFFSET_CONVERTER = 0;
 
 // If set to 1, the JS compiler is run before wasm-ld so that the linker can
 // report undefined symbols within the binary.  Without this option that linker
-// doesn't know which symbols might be defined JS and so reporting of undefined
+// doesn't know which symbols might be defined in JS so reporting of undefined
 // symbols is delayed until the JS compiler is run.
 // [link]
 var LLD_REPORT_UNDEFINED = 0;
