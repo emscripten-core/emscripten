dd0b474481c79abdecbb2755f7e9dad8c17335eb
Alon Zakai <azakai@google.com>
2021-04-27 13:40:43 -0700

    missing files

---
+++ tests/other/wasm2c/my-code-multi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+extern void a_wasmbox_init(void);
+extern void b_wasmbox_init(void);
+
+extern int (*a_Z_twiceZ_ii)(int);
+extern int (*b_Z_thriceZ_ii)(int);
+
+int main() {
+  puts("Initializing sandboxed unsafe libraries");
+  a_wasmbox_init();
+  b_wasmbox_init();
+  printf("Calling twice on 21 returns %d\n", a_Z_twiceZ_ii(21));
+  printf("Calling thrice on 10 returns %d\n", b_Z_thriceZ_ii(10));
+}
