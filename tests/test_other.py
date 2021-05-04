c8ebeaf13ffa4a50ff1298b9c086f45171d98d84
Sharad Saxena <sharad.saxena@autodesk.com>
2021-05-04 12:44:15 +0800

    testing fixes and upgrading

--- tests/test_other.py
+++ tests/test_other.py
@@ -10152,7 +10152,7 @@ exec "$@"
     self.assertIn('Hello from lib!', result)
 
   def test_split_module_ex(self):
-    initialTableSize = 535
+    initialTableSize = 5632
     self.set_setting('SPLIT_MODULE')
     self.set_setting('WASM', 1)
     self.set_setting('WASM_BIGINT')
@@ -10166,7 +10166,7 @@ exec "$@"
     self.set_setting('GL_TRACK_ERRORS', 0)
     self.set_setting('WASM_OBJECT_FILES', 0)
     self.set_setting('INCLUDE_FULL_LIBRARY', 1) # https://github.com/emscripten-core/emscripten/issues/13794
-    # self.emcc_args += ['-sMAIN_MODULE=2']
+    self.emcc_args += ['-sMAIN_MODULE=2']
     # Exception: FROZEN_CACHE is set, but cache file is missing: sysroot/lib/wasm32-emscripten/lto-pic/libgl-webgl2-full_es3.a
     # self.emcc_args += ['-sUSE_WEBGL2']
     # self.emcc_args += ['-sUSE_GLFW=3']
@@ -10177,7 +10177,7 @@ exec "$@"
     self.emcc_args += ['-D_LIBCPP_ENABLE_CXX17_REMOVED_AUTO_PTR']
     self.emcc_args += ['--emit-symbol-map']
     self.emcc_args += ['--post-js', path_from_root('tests', 'other', 'test_split_module_ex.post.js')]
-    self.emcc_args += ['-sEXTRA_EXPORTED_RUNTIME_METHODS=[allocate]']
+    self.emcc_args += ['-sEXPORTED_RUNTIME_METHODS=[allocate]']
     self.emcc_args += ['-flto']
     self.emcc_args += ['-fPIC']
     self.emcc_args += ['-Oz']
