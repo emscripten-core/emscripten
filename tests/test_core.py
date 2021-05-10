559d85d1871b5752988faa68eb29725235197ce9
Alon Zakai <azakai@google.com>
2021-05-10 16:50:38 -0700

    clean

--- tests/test_core.py
+++ tests/test_core.py
@@ -7021,7 +7021,7 @@ someweirdtext
 
     # Export things on "TheModule". This matches the typical use pattern of the bound library
     # being used as Box2D.* or Ammo.*, and we cannot rely on "Module" being always present (closure may remove it).
-    self.emcc_args += ['--post-js', 'glue.js']
+    self.emcc_args += ['-s', 'EXPORTED_FUNCTIONS=_malloc,_free', '--post-js', 'glue.js']
     if allow_memory_growth:
       self.set_setting('ALLOW_MEMORY_GROWTH')
 
