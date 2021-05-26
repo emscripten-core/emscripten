b15ca40ca02f469de3bf9a4de67a2655aede8974
HCLJason <79225526+HCLJason@users.noreply.github.com>
2021-05-26 15:13:00 -0400

    Update libpng to use pristine upstream archive (#14300)
    The config file was generated by emconfigure.
    
    Use mirror of archive on google storage to avoid depending
    on sourceforge download links (which we have had issues
    with in the past).

--- tests/third_party/libpng/pngtest.c
+++ tests/third_party/libpng/pngtest.c
@@ -1,7 +1,7 @@
 
 /* pngtest.c - a simple test program to test libpng
  *
- * Last changed in libpng 1.6.17 [March 26, 2015]
+ * Last changed in libpng 1.6.37 [March 26, 2015]
  * Copyright (c) 1998-2015 Glenn Randers-Pehrson
  * (Version 0.96 Copyright (c) 1996, 1997 Andreas Dilger)
  * (Version 0.88 Copyright (c) 1995, 1996 Guy Eric Schalnat, Group 42, Inc.)
@@ -2028,4 +2028,4 @@ main(void)
 #endif
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_6_17 Your_png_h_is_not_version_1_6_17;
+typedef png_libpng_version_1_6_37 Your_png_h_is_not_version_1_6_37;
