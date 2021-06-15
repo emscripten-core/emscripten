8b1165caa9a878d7d6d6536603531292b4f6f313
Roman Yurchak <rth.yurchak@gmail.com>
2021-06-15 22:07:17 +0200

    Fix read_binary in file_packager.py

--- tools/file_packager.py
+++ tools/file_packager.py
@@ -458,7 +458,7 @@ def main():
     basename = os.path.basename(filename)
     if file_['mode'] == 'embed':
       # Embed
-      data = list(bytearray(shared.read_binary(file_['srcpath'], 'rb')))
+      data = list(bytearray(shared.read_binary(file_['srcpath'])))
       code += '''var fileData%d = [];\n''' % counter
       if data:
         parts = []
