9a760b08aa87b7c60d549344c81b6310446ff771
Alon Zakai <azakai@google.com>
2021-07-12 11:59:19 -0700

    emmalloc too

--- system/lib/emmalloc.cpp
+++ system/lib/emmalloc.cpp
@@ -39,6 +39,7 @@
  *    malloc.
  */
 
+#include <stddef.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <memory.h>
@@ -61,6 +62,9 @@ extern "C"
 // Configuration: specifies the minimum alignment that malloc()ed memory outputs. Allocation requests with smaller alignment
 // than this will yield an allocation with this much alignment.
 #define MALLOC_ALIGNMENT 8
+
+_Static_assert(MALLOC_ALIGNMENT == __alignof__(max_align_t), "max_align_t must be correct");
+
 
 #define EMMALLOC_EXPORT __attribute__((weak, __visibility__("default")))
 
