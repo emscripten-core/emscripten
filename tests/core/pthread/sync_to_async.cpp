bce97f38adac72e88d8a29257a8d95d94daaaf5a
Alon Zakai <azakai@google.com>
2021-07-15 11:47:01 -0700

    cleanup

--- tests/core/pthread/sync_to_async.cpp
+++ tests/core/pthread/sync_to_async.cpp
@@ -14,14 +14,9 @@ int main() {
 
   std::cout << "Perform an async task.\n";
 
-  // We need to be very careful about the lifetime of |resume| below, and the
-  // callback we construct from it, |asyncFunc| - things whose lifetime is that
-  // of the outer lambda will not live long enough for the async callback. For
-  // that reason, define asyncFunc on our stack here, which will definitely
-  // remain valid, since we wait synchronously for the work to be done on the
-  // thread.
   sync_to_async.invoke([](emscripten::sync_to_async::Callback resume) {
     std::cout << "  Hello from sync C++ before the async\n";
+
     // Set up async JS, just to prove an async JS callback happens before the
     // async C++.
     EM_ASM({
@@ -29,15 +24,20 @@ int main() {
         console.log("  Hello from async JS");
       }, 0);
     });
-    // Set up async C++, and also call resume after running it.
+
+    // Set up async C++..
     emscripten_async_call([](void* arg) {
       auto resume = (emscripten::sync_to_async::Callback)arg;
       std::cout << "  Hello from async C++\n";
+
+      // We are done with all the async things we want to do, and can call
+      // resume to continue execution on the calling thread.
       (*resume)();
     }, resume, 1);
   });
 
   std::cout << "Perform another synchronous task.\n";
+
   sync_to_async.invoke([](emscripten::sync_to_async::Callback resume) {
     std::cout << "  Hello again from sync C++\n";
     (*resume)();
