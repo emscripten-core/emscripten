3c1442731748855d1251cc28564f793f9def4798
Alon Zakai <azakai@google.com>
2021-07-20 12:56:50 -0700

    fix

--- system/lib/libc/musl/src/string/memcmp.c
+++ system/lib/libc/musl/src/string/memcmp.c
@@ -11,7 +11,7 @@ int memcmp(const void *vl, const void *vr, size_t n)
 #if !defined(EMSCRIPTEN_OPTIMIZE_FOR_OZ) && !__has_feature(address_sanitizer)
 	// If we have enough bytes, and everything is aligned, loop on words instead
 	// of single bytes.
-	if (n >= 4 && ((((uintptr_t)l) & 3) | (((uintptr_t)r) & 3))) {
+	if (n >= 4 && !((((uintptr_t)l) & 3) | (((uintptr_t)r) & 3))) {
 		while (n >= 4) {
 			if (*((uint32_t *)l) != *((uint32_t *)r)) {
 				// Go to the single-byte loop to find the specific byte.
