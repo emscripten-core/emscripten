3775ded78e37bf1c45edb7207651bb144a32f57a
Sam Clegg <sbc@chromium.org>
2021-07-21 08:06:34 -0700

    Use seprate system libraries for thinLTO
    Also, avoid passing `-flto` more than once to the compiler.
    
    ThinLTO is still pretty much untested, both here, and upstream.  This
    change doesn't effect that but does fix a real issue.
    
    Fixes: #14694

--- emcc.py
+++ emcc.py
@@ -842,10 +842,10 @@ def get_cflags(options, user_args):
     cflags.append('-fvisibility=default')
 
   if settings.LTO:
-    cflags.append('-flto=' + settings.LTO)
+    if not any(a.startswith('-flto') for a in user_args):
+      cflags.append('-flto=' + settings.LTO)
   else:
-    # With LTO mode these args get passed instead
-    # at link time when the backend runs.
+    # In LTO mode these args get passed instead at link time when the backend runs.
     for a in building.llvm_backend_args():
       cflags += ['-mllvm', a]
 
