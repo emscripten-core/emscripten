3cb7b07f3944833433b53ea185c75aa24fb099da
Hood <hood@mit.edu>
2021-07-29 04:30:08 +0200

    Fix test

--- tests/unistd/pipe.c
+++ tests/unistd/pipe.c
@@ -136,27 +136,29 @@ int main()
     test_read (fd[0], &rchar, 10);
     test_poll(fd, FALSE);
 
+    // Clear buffer
+    memset(buf, 0, sizeof(buf));
     // Test closing pipes.
     // Write to pipe
-    assert(write(fdw, "XXXX", 4) == 4);
+    assert(write(fd[1], "XXXX", 4) == 4);
     // Close write end
-    assert(close(fdw) == 0);
+    assert(close(fd[1]) == 0);
     // This write should fail
-    assert(write(fdw, "YYYY", 4) == -1);
+    assert(write(fd[1], "YYYY", 4) == -1);
     // The error number is EBADF
-    assert(errno != EBADF);
+    assert(errno == EBADF);
 
     // read from the other end of the pipe
-    assert(read(fdr, buf, 5) == 4);
+    assert(read(fd[0], buf, 5) == 4);
     // We should have read what we wrote to the other end
     assert( strncmp(buf, "XXXX", 5) == 0);
 
     // Close the read end
-    assert(close(fdr) == 0);
+    assert(close(fd[0]) == 0);
     // Now reading should return an error
-    assert(read(fdr, buf, 5) == -1);
+    assert(read(fd[0], buf, 5) == -1);
     // The error number is EBADF
-    assert(errno != EBADF);
+    assert(errno == EBADF);
 
     puts("success");
 
