5398478e4d418bfee307febe4ae7efb8bac0386d
Sam Clegg <sbc@chromium.org>
2021-08-09 17:28:12 -0700

    Restore disabled tests: test_mem_init and test_mem_init_request

--- tests/mem_init_request.cpp
+++ tests/mem_init_request.cpp
@@ -3,22 +3,21 @@
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
 
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 
 int main() {
   const char *hello = "hello world";
-  int result = 1;
   putc('g', stdout);
   putc('o', stdout);
   putc('!', stdout);
-  if (strchr(hello, 'l') != hello + 2) result = 0;
-  if (strchr(hello, 'w') != hello + 6) result = 0;
+  assert(strchr(hello, 'l') == hello + 2);
+  assert(strchr(hello, 'w') == hello + 6);
   putc('d', stdout);
   putc('o', stdout);
   putc('n', stdout);
   putc('e', stdout);
-  REPORT_RESULT(result);
   return 0;
 }
 
