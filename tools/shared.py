8357c2d044bb8bf7d3b2e9b65386788efed02e97
Sam Clegg <sbc@chromium.org>
2021-08-23 15:55:41 -0700

    Include `-git` in version file
    Previously we were adding the `-git` suffix programatically but not
    includig in the file contents itself.  Since emsdk creates its own
    version file this change should only effect git users.  It should make
    it more obvious when git versions are being used as opposed to official
    released.
    
    Also, remove the redundant quotes from this file.

--- tools/shared.py
+++ tools/shared.py
@@ -339,7 +339,7 @@ def set_version_globals():
   global EMSCRIPTEN_VERSION, EMSCRIPTEN_VERSION_MAJOR, EMSCRIPTEN_VERSION_MINOR, EMSCRIPTEN_VERSION_TINY
   filename = path_from_root('emscripten-version.txt')
   EMSCRIPTEN_VERSION = utils.read_file(filename).strip().strip('"')
-  parts = [int(x) for x in EMSCRIPTEN_VERSION.split('.')]
+  parts = [int(x) for x in EMSCRIPTEN_VERSION.split('-')[0].split('.')]
   EMSCRIPTEN_VERSION_MAJOR, EMSCRIPTEN_VERSION_MINOR, EMSCRIPTEN_VERSION_TINY = parts
 
 
