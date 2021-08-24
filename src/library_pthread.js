47f0c92dc175bde789b3c4366ec0c0e56aef9d01
Sam Clegg <sbc@chromium.org>
2021-08-24 10:24:37 -0700

    Fix closure errors with MINIMAL_RUNTIME + USE_PTHREADS. NFC.

--- src/library_pthread.js
+++ src/library_pthread.js
@@ -9,6 +9,7 @@ var LibraryPThread = {
   $PThread__deps: ['_emscripten_thread_init',
                    'emscripten_futex_wake', '$killThread',
                    '$cancelThread', '$cleanupThread',
+                   'exit',
 #if !MINIMAL_RUNTIME
                    '$handleException',
 #endif
@@ -57,7 +58,9 @@ var LibraryPThread = {
       // things.
       PThread['receiveObjectTransfer'] = PThread.receiveObjectTransfer;
       PThread['threadInit'] = PThread.threadInit;
+#if !MINIMAL_RUNTIME
       PThread['setExitStatus'] = PThread.setExitStatus;
+#endif
 #endif
     },
     // Maps pthread_t to pthread info objects
@@ -130,9 +133,11 @@ var LibraryPThread = {
     },
 #endif
 
+#if !MINIMAL_RUNTIME
     setExitStatus: function(status) {
       EXITSTATUS = status;
     },
+#endif
 
     terminateAllThreads: function() {
       for (var t in PThread.pthreads) {
@@ -305,10 +310,10 @@ var LibraryPThread = {
           err("exitProcess requested by worker");
 #endif
 #if MINIMAL_RUNTIME
-          exit(d['returnCode']);
+          _exit(d['returnCode']);
 #else
           try {
-            exit(d['returnCode']);
+            _exit(d['returnCode']);
           } catch (e) {
             handleException(e);
           }
