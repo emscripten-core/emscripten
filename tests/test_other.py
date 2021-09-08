emscripten-core/emscripten

commit 13535ca57a071ccfa9ae1570677115f7e140c57b

Split config file handling out of shared.py. NFC. (#12699)
Move config file handling into its own file.  Also, move a few small
utilities into `utils.py` to avoid circular dependencies.

For a while now shared.py has been way too big and this is step towards
breaking it up and breaking the existing circular dependencies between
python modules that use and also also used by shared.py (cache.py for
example).

tools/building.py
@@ -20,16 +20,18 @@ from . import diagnostics
 from . import response_file
 from . import shared
 from . import webassembly
+from . import config
 from .toolchain_profiler import ToolchainProfiler
 from .shared import Settings, CLANG_CC, CLANG_CXX, PYTHON
-from .shared import LLVM_NM, EMCC, EMAR, EMXX, EMRANLIB, NODE_JS, WASM_LD, LLVM_AR
+from .shared import LLVM_NM, EMCC, EMAR, EMXX, EMRANLIB, WASM_LD, LLVM_AR
 from .shared import LLVM_LINK, LLVM_OBJCOPY
 from .shared import try_delete, run_process, check_call, exit_with_error
 from .shared import configuration, path_from_root, EXPECTED_BINARYEN_VERSION
-from .shared import asmjs_mangle, DEBUG, WINDOWS, JAVA
-from .shared import EM_BUILD_VERBOSE, TEMP_DIR, print_compiler_stage, BINARYEN_ROOT
+from .shared import asmjs_mangle, DEBUG
+from .shared import EM_BUILD_VERBOSE, TEMP_DIR, print_compiler_stage
 from .shared import CANONICAL_TEMP_DIR, LLVM_DWARFDUMP, demangle_c_symbol_name, asbytes
-from .shared import get_emscripten_temp_dir, exe_suffix, which, is_c_symbol, shlex_join
+from .shared import get_emscripten_temp_dir, exe_suffix, is_c_symbol
+from .utils import which, WINDOWS
 
 logger = logging.getLogger('building')
 
@@ -289,10 +291,10 @@ def handle_cmake_toolchain(args, env):
   # Append the Emscripten toolchain file if the user didn't specify one.
   if not has_substr(args, '-DCMAKE_TOOLCHAIN_FILE'):
     args.append('-DCMAKE_TOOLCHAIN_FILE=' + path_from_root('cmake', 'Modules', 'Platform', 'Emscripten.cmake'))
-  node_js = NODE_JS
+  node_js = config.NODE_JS
 
   if not has_substr(args, '-DCMAKE_CROSSCOMPILING_EMULATOR'):
-    node_js = NODE_JS[0].replace('"', '\"')
+    node_js = config.NODE_JS[0].replace('"', '\"')
     args.append('-DCMAKE_CROSSCOMPILING_EMULATOR="%s"' % node_js)
 
   # On Windows specify MinGW Makefiles or ninja if we have them and no other
@@ -333,7 +335,7 @@ def configure(args, stdout=None, stderr=None, env=None, cflags=[], **kwargs):
     stdout = None
   if EM_BUILD_VERBOSE >= 1:
     stderr = None
-  print('configure: ' + shlex_join(args), file=sys.stderr)
+  print('configure: ' + shared.shlex_join(args), file=sys.stderr)
   run_process(args, stdout=stdout, stderr=stderr, env=env, **kwargs)
 
 
@@ -869,7 +871,7 @@ def acorn_optimizer(filename, passes, extra_info=None, return_output=False):
     with open(temp, 'a') as f:
       f.write('// EXTRA_INFO: ' + extra_info)
     filename = temp
-  cmd = NODE_JS + [optimizer, filename] + passes
+  cmd = config.NODE_JS + [optimizer, filename] + passes
   # Keep JS code comments intact through the acorn optimization pass so that JSDoc comments
   # will be carried over to a later Closure run.
   if Settings.USE_CLOSURE_COMPILER:
@@ -899,7 +901,7 @@ def eval_ctors(js_file, binary_file, debug_info=False): # noqa
 
 def get_closure_compiler():
   # First check if the user configured a specific CLOSURE_COMPILER in thier settings
-  if shared.CLOSURE_COMPILER:
+  if config.CLOSURE_COMPILER:
     return shared.CLOSURE_COMPILER
 
   # Otherwise use the one installed vai npm
@@ -942,7 +944,7 @@ def closure_compiler(filename, pretty=True, advanced=True, extra_closure_args=No
     # Closure compiler expects JAVA_HOME to be set *and* java.exe to be in the PATH in order
     # to enable use the java backend.  Without this it will only try the native and JavaScript
     # versions of the compiler.
-    java_bin = os.path.dirname(JAVA)
+    java_bin = os.path.dirname(config.JAVA)
     if java_bin:
       def add_to_path(dirname):
         env['PATH'] = env['PATH'] + os.pathsep + dirname
@@ -1569,7 +1571,7 @@ def check_binaryen(bindir):
 def get_binaryen_bin():
   assert Settings.WASM, 'non wasm builds should not ask for binaryen'
   global binaryen_checked
-  rtn = os.path.join(BINARYEN_ROOT, 'bin')
+  rtn = os.path.join(config.BINARYEN_ROOT, 'bin')
   if not binaryen_checked:
     check_binaryen(rtn)
     binaryen_checked = True

tools/config.py
@@ -1,0 +1,283 @@
+# Copyright 2020 The Emscripten Authors.  All rights reserved.
+# Emscripten is available under two separate licenses, the MIT license and the
+# University of Illinois/NCSA Open Source License.  Both these licenses can be
+# found in the LICENSE file.
+
+import os
+import sys
+import logging
+from .utils import path_from_root, exit_with_error, __rootpath__, which
+
+logger = logging.getLogger('shared')
+
+# The following class can be overridden by the config file and/or
+# environment variables.  Specifically any variable whose name
+# is in ALL_UPPER_CASE is condifered a valid config file key.
+# See parse_config_file below.
+EMSCRIPTEN_ROOT = __rootpath__
+NODE_JS = None
+BINARYEN_ROOT = None
+SPIDERMONKEY_ENGINE = None
+V8_ENGINE = None
+LLVM_ROOT = None
+LLVM_ADD_VERSION = None
+CLANG_ADD_VERSION = None
+CLOSURE_COMPILER = None
+JAVA = None
+JS_ENGINE = None
+JS_ENGINES = None
+WASMER = None
+WASMTIME = None
+WASM_ENGINES = []
+FROZEN_CACHE = None
+CACHE = None
+PORTS = None
+COMPILER_WRAPPER = None
+
+
+def listify(x):
+  if type(x) is not list:
+    return [x]
+  return x
+
+
+def fix_js_engine(old, new):
+  if old is None:
+    return
+  global JS_ENGINES
+  JS_ENGINES = [new if x == old else x for x in JS_ENGINES]
+  return new
+
+
+def root_is_writable():
+  return os.access(__rootpath__, os.W_OK)
+
+
+def normalize_config_settings():
+  global CACHE, PORTS, JAVA, LLVM_ADD_VERSION, CLANG_ADD_VERSION
+  global NODE_JS, V8_ENGINE, JS_ENGINE, JS_ENGINES, SPIDERMONKEY_ENGINE, WASM_ENGINES
+
+  # EM_CONFIG stuff
+  if not JS_ENGINES:
+    JS_ENGINES = [NODE_JS]
+  if not JS_ENGINE:
+    JS_ENGINE = JS_ENGINES[0]
+
+  # Engine tweaks
+  if SPIDERMONKEY_ENGINE:
+    new_spidermonkey = SPIDERMONKEY_ENGINE
+    if '-w' not in str(new_spidermonkey):
+      new_spidermonkey += ['-w']
+    SPIDERMONKEY_ENGINE = fix_js_engine(SPIDERMONKEY_ENGINE, new_spidermonkey)
+  NODE_JS = fix_js_engine(NODE_JS, listify(NODE_JS))
+  V8_ENGINE = fix_js_engine(V8_ENGINE, listify(V8_ENGINE))
+  JS_ENGINE = fix_js_engine(JS_ENGINE, listify(JS_ENGINE))
+  JS_ENGINES = [listify(engine) for engine in JS_ENGINES]
+  WASM_ENGINES = [listify(engine) for engine in WASM_ENGINES]
+  if not CACHE:
+    if root_is_writable():
+      CACHE = path_from_root('cache')
+    else:
+      # Use the legacy method of putting the cache in the user's home directory
+      # if the emscripten root is not writable.
+      # This is useful mostly for read-only installation and perhaps could
+      # be removed in the future since such installations should probably be
+      # setting a specific cache location.
+      logger.debug('Using home-directory for emscripten cache due to read-only root')
+      CACHE = os.path.expanduser(os.path.join('~', '.emscripten_cache'))
+  if not PORTS:
+    PORTS = os.path.join(CACHE, 'ports')
+
+  if JAVA is None:
+    logger.debug('JAVA not defined in ' + config_file_location() + ', using "java"')
+    JAVA = 'java'
+
+  # Tools/paths
+  if LLVM_ADD_VERSION is None:
+    LLVM_ADD_VERSION = os.getenv('LLVM_ADD_VERSION')
+
+  if CLANG_ADD_VERSION is None:
+    CLANG_ADD_VERSION = os.getenv('CLANG_ADD_VERSION')
+
+
+def parse_config_file():
+  """Parse the emscripten config file using python's exec.
+
+  Also check EM_<KEY> environment variables to override specific config keys.
+  """
+  config = {}
+  config_text = open(config_file, 'r').read() if config_file else EM_CONFIG
+  try:
+    exec(config_text, config)
+  except Exception as e:
+    exit_with_error('Error in evaluating %s (at %s): %s, text: %s', EM_CONFIG, config_file, str(e), config_text)
+
+  CONFIG_KEYS = (
+    'NODE_JS',
+    'BINARYEN_ROOT',
+    'SPIDERMONKEY_ENGINE',
+    'V8_ENGINE',
+    'LLVM_ROOT',
+    'LLVM_ADD_VERSION',
+    'CLANG_ADD_VERSION',
+    'CLOSURE_COMPILER',
+    'JAVA',
+    'JS_ENGINE',
+    'JS_ENGINES',
+    'WASMER',
+    'WASMTIME',
+    'WASM_ENGINES',
+    'FROZEN_CACHE',
+    'CACHE',
+    'PORTS',
+    'COMPILER_WRAPPER',
+  )
+
+  # Only propagate certain settings from the config file.
+  for key in CONFIG_KEYS:
+    env_var = 'EM_' + key
+    env_value = os.environ.get(env_var)
+    if env_value is not None:
+      globals()[key] = env_value
+    elif key in config:
+      globals()[key] = config[key]
+
+  # Certain keys are mandatory
+  for key in ('LLVM_ROOT', 'NODE_JS', 'BINARYEN_ROOT'):
+    if key not in config:
+      exit_with_error('%s is not defined in %s', key, config_file_location())
+    if not globals()[key]:
+      exit_with_error('%s is set to empty value in %s', key, config_file_location())
+
+  if not NODE_JS:
+    exit_with_error('NODE_JS is not defined in %s', config_file_location())
+
+  normalize_config_settings()
+
+
+# Returns the location of the emscripten config file.
+def config_file_location():
+  # Handle the case where there is no config file at all (i.e. If EM_CONFIG is passed as python code
+  # direclty on the command line).
+  if not config_file:
+    return '<inline config>'
+
+  return config_file
+
+
+def generate_config(path, first_time=False):
+  # Note: repr is used to ensure the paths are escaped correctly on Windows.
+  # The full string is replaced so that the template stays valid Python.
+  config_file = open(path_from_root('tools', 'settings_template.py')).read().splitlines()
+  config_file = config_file[3:] # remove the initial comment
+  config_file = '\n'.join(config_file)
+  # autodetect some default paths
+  config_file = config_file.replace('\'{{{ EMSCRIPTEN_ROOT }}}\'', repr(__rootpath__))
+  llvm_root = os.path.dirname(which('llvm-dis') or '/usr/bin/llvm-dis')
+  config_file = config_file.replace('\'{{{ LLVM_ROOT }}}\'', repr(llvm_root))
+
+  node = which('nodejs') or which('node') or 'node'
+  config_file = config_file.replace('\'{{{ NODE }}}\'', repr(node))
+
+  abspath = os.path.abspath(os.path.expanduser(path))
+  # write
+  with open(abspath, 'w') as f:
+    f.write(config_file)
+
+  if first_time:
+    print('''
+==============================================================================
+Welcome to Emscripten!
+
+This is the first time any of the Emscripten tools has been run.
+
+A settings file has been copied to %s, at absolute path: %s
+
+It contains our best guesses for the important paths, which are:
+
+  LLVM_ROOT       = %s
+  NODE_JS         = %s
+  EMSCRIPTEN_ROOT = %s
+
+Please edit the file if any of those are incorrect.
+
+This command will now exit. When you are done editing those paths, re-run it.
+==============================================================================
+''' % (path, abspath, llvm_root, node, __rootpath__), file=sys.stderr)
+
+
+# Emscripten configuration is done through the --em-config command line option
+# or the EM_CONFIG environment variable. If the specified string value contains
+# newline or semicolon-separated definitions, then these definitions will be
+# used to configure Emscripten.  Otherwise, the string is understood to be a
+# path to a settings file that contains the required definitions.
+# The search order from the config file is as follows:
+# 1. Specified on the command line (--em-config)
+# 2. Specified via EM_CONFIG environment variable
+# 3. Local .emscripten file, if found
+# 4. Local .emscripten file, as used by `emsdk --embedded` (two levels above,
+#    see below)
+# 5. User home directory config (~/.emscripten), if found.
+
+embedded_config = path_from_root('.emscripten')
+# For compatibility with `emsdk --embedded` mode also look two levels up.  The
+# layout of the emsdk puts emcc two levels below emsdk.  For exmaple:
+#  - emsdk/upstream/emscripten/emcc
+#  - emsdk/emscipten/1.38.31/emcc
+# However `emsdk --embedded` stores the config file in the emsdk root.
+# Without this check, when emcc is run from within the emsdk in embedded mode
+# and the user forgets to first run `emsdk_env.sh` (which sets EM_CONFIG) emcc
+# will not see any config file at all and fall back to creating a new/emtpy
+# one.
+# We could remove this special case if emsdk were to write its embedded config
+# file into the emscripten directory itself.
+# See: https://github.com/emscripten-core/emsdk/pull/367
+emsdk_root = os.path.dirname(os.path.dirname(path_from_root()))
+emsdk_embedded_config = os.path.join(emsdk_root, '.emscripten')
+user_home_config = os.path.expanduser('~/.emscripten')
+
+if '--em-config' in sys.argv:
+  EM_CONFIG = sys.argv[sys.argv.index('--em-config') + 1]
+  # And now remove it from sys.argv
+  skip = False
+  newargs = []
+  for arg in sys.argv:
+    if not skip and arg != '--em-config':
+      newargs += [arg]
+    elif arg == '--em-config':
+      skip = True
+    elif skip:
+      skip = False
+  sys.argv = newargs
+  if not os.path.isfile(EM_CONFIG):
+    if EM_CONFIG.startswith('-'):
+      exit_with_error('Passed --em-config without an argument. Usage: --em-config /path/to/.emscripten or --em-config LLVM_ROOT=/path;...')
+    if '=' not in EM_CONFIG:
+      exit_with_error('File ' + EM_CONFIG + ' passed to --em-config does not exist!')
+    else:
+      EM_CONFIG = EM_CONFIG.replace(';', '\n') + '\n'
+elif 'EM_CONFIG' in os.environ:
+  EM_CONFIG = os.environ['EM_CONFIG']
+elif os.path.exists(embedded_config):
+  EM_CONFIG = embedded_config
+elif os.path.exists(emsdk_embedded_config):
+  EM_CONFIG = emsdk_embedded_config
+elif os.path.exists(user_home_config):
+  EM_CONFIG = user_home_config
+else:
+  if root_is_writable():
+    generate_config(embedded_config, first_time=True)
+  else:
+    generate_config(user_home_config, first_time=True)
+  sys.exit(0)
+
+if '\n' in EM_CONFIG:
+  config_file = None
+  logger.debug('config is specified inline without a file')
+else:
+  config_file = os.path.expanduser(EM_CONFIG)
+  logger.debug('emscripten config is located in ' + config_file)
+  if not os.path.exists(config_file):
+    exit_with_error('emscripten config file not found: ' + config_file)
+
+parse_config_file()

em-config.py
@@ -16,17 +16,17 @@ is found, or exits with 1 if the variable does not exist.
 
 import sys
 import re
-from tools import shared
+from tools import config
 
 
 def main():
   if len(sys.argv) != 2 or \
     not re.match(r"^[\w\W_][\w\W_\d]*$", sys.argv[1]) or \
-    not hasattr(shared, sys.argv[1]):
+    not hasattr(config, sys.argv[1]):
     print('Usage: em-config VAR_NAME', file=sys.stderr)
     exit(1)
 
-  print(getattr(shared, sys.argv[1]))
+  print(getattr(config, sys.argv[1]))
   return 0
 
 

emcc.py
@@ -49,6 +49,7 @@ from tools.toolchain_profiler import ToolchainProfiler
 from tools import js_manipulation
 from tools import wasm2c
 from tools import webassembly
+from tools import config
 
 if __name__ == '__main__':
   ToolchainProfiler.record_process_start()
@@ -654,8 +655,8 @@ def backend_binaryen_passes():
 
 def make_js_executable(script):
   src = open(script).read()
-  cmd = shared.shlex_join(shared.JS_ENGINE)
-  if not os.path.isabs(shared.JS_ENGINE[0]):
+  cmd = shared.shlex_join(config.JS_ENGINE)
+  if not os.path.isabs(config.JS_ENGINE[0]):
     # TODO: use whereis etc. And how about non-*NIX?
     cmd = '/usr/bin/env -S ' + cmd
   logger.debug('adding `#!` to JavaScript file: %s' % cmd)
@@ -926,7 +927,7 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
     # warnings are properly printed during arg parse.
     newargs = diagnostics.capture_warnings(newargs)
 
-    if not shared.CONFIG_FILE:
+    if not config.config_file:
       diagnostics.warning('deprecated', 'Specifying EM_CONFIG as a python literal is deprecated. Please use a file instead.')
 
     for i in range(len(newargs)):
@@ -1947,10 +1948,10 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
 
     CXX = [shared.CLANG_CXX]
     CC = [shared.CLANG_CC]
-    if shared.COMPILER_WRAPPER:
-      logger.debug('using compiler wrapper: %s', shared.COMPILER_WRAPPER)
-      CXX.insert(0, shared.COMPILER_WRAPPER)
-      CC.insert(0, shared.COMPILER_WRAPPER)
+    if config.COMPILER_WRAPPER:
+      logger.debug('using compiler wrapper: %s', config.COMPILER_WRAPPER)
+      CXX.insert(0, config.COMPILER_WRAPPER)
+      CC.insert(0, config.COMPILER_WRAPPER)
 
     if 'EMMAKEN_COMPILER' in os.environ:
       diagnostics.warning('deprecated', '`EMMAKEN_COMPILER` is deprecated.\n'
@@ -2437,7 +2438,7 @@ def parse_args(newargs):
     elif check_arg('--extern-post-js'):
       options.extern_post_js += open(consume_arg()).read() + '\n'
     elif check_arg('--compiler-wrapper'):
-      shared.COMPILER_WRAPPER = consume_arg()
+      config.COMPILER_WRAPPER = consume_arg()
     elif check_flag('--post-link'):
       options.post_link = True
     elif check_arg('--oformat'):
@@ -2604,7 +2605,7 @@ def parse_args(newargs):
       if os.path.exists(path):
         exit_with_error('File ' + optarg + ' passed to --generate-config already exists!')
       else:
-        shared.generate_config(optarg)
+        config.generate_config(optarg)
       should_exit = True
     # Record USE_PTHREADS setting because it controls whether --shared-memory is passed to lld
     elif arg == '-pthread':

tests/clang_native.py
@@ -5,7 +5,8 @@
 
 import logging
 import os
-from tools.shared import MACOS, WINDOWS, path_from_root, PIPE, run_process, CLANG_CC, CLANG_CXX
+from tools.shared import PIPE, run_process, CLANG_CC, CLANG_CXX
+from tools.utils import MACOS, WINDOWS, path_from_root
 from tools import building
 
 logger = logging.getLogger('clang_native')

tests/jsrun.py
@@ -9,6 +9,7 @@ import sys
 from subprocess import Popen, PIPE, CalledProcessError
 
 from tools import shared
+from tools.shared import config
 
 WORKING_ENGINES = {} # Holds all configured engines and whether they work: maps path -> True/False
 
@@ -90,7 +91,7 @@ def run_js(filename, engine=None, args=[],
            stdin=None, stdout=PIPE, stderr=None, cwd=None,
            full_output=False, assert_returncode=0, skip_check=False):
   if not engine:
-    engine = shared.JS_ENGINES[0]
+    engine = config.JS_ENGINES[0]
 
   # We used to support True here but we no longer do.  Assert here just in case.
   assert(type(assert_returncode) == int)

tests/runner.py
@@ -55,12 +55,13 @@ import clang_native
 import jsrun
 import parallel_testsuite
 from jsrun import NON_ZERO
-from tools.shared import EM_CONFIG, TEMP_DIR, EMCC, EMXX, DEBUG
+from tools.config import EM_CONFIG
+from tools.shared import TEMP_DIR, EMCC, EMXX, DEBUG
 from tools.shared import EMSCRIPTEN_TEMP_DIR
-from tools.shared import MACOS, WINDOWS
 from tools.shared import EM_BUILD_VERBOSE
 from tools.shared import asstr, get_canonical_temp_dir, try_delete
-from tools.shared import asbytes, Settings
+from tools.shared import asbytes, Settings, config
+from tools.utils import MACOS, WINDOWS
 from tools import shared, line_endings, building
 
 
@@ -232,24 +233,24 @@ def chdir(dir):
 
 @contextlib.contextmanager
 def js_engines_modify(replacements):
-  """A context manager that updates shared.JS_ENGINES."""
-  original = shared.JS_ENGINES
-  shared.JS_ENGINES = replacements
+  """A context manager that updates config.JS_ENGINES."""
+  original = config.JS_ENGINES
+  config.JS_ENGINES = replacements
   try:
     yield
   finally:
-    shared.JS_ENGINES = original
+    config.JS_ENGINES = original
 
 
 @contextlib.contextmanager
 def wasm_engines_modify(replacements):
-  """A context manager that updates shared.WASM_ENGINES."""
-  original = shared.WASM_ENGINES
-  shared.WASM_ENGINES = replacements
+  """A context manager that updates config.WASM_ENGINES."""
+  original = config.WASM_ENGINES
+  config.WASM_ENGINES = replacements
   try:
     yield
   finally:
-    shared.WASM_ENGINES = original
+    config.WASM_ENGINES = original
 
 
 def ensure_dir(dirname):
@@ -989,7 +990,7 @@ class RunnerCore(unittest.TestCase, metaclass=RunnerMeta):
 
   def filtered_js_engines(self, js_engines=None):
     if js_engines is None:
-      js_engines = shared.JS_ENGINES
+      js_engines = config.JS_ENGINES
     for engine in js_engines:
       assert type(engine) == list
     for engine in self.banned_js_engines:
@@ -1046,7 +1047,7 @@ class RunnerCore(unittest.TestCase, metaclass=RunnerMeta):
     if self.get_setting('STANDALONE_WASM'):
       # TODO once standalone wasm support is more stable, apply use_all_engines
       # like with js engines, but for now as we bring it up, test in all of them
-      wasm_engines = shared.WASM_ENGINES
+      wasm_engines = config.WASM_ENGINES
       if len(wasm_engines) == 0:
         logger.warning('no wasm engine was found to run the standalone part of this test')
       engines += wasm_engines
@@ -1656,8 +1657,8 @@ def build_library(name,
 
 
 def check_js_engines():
-  working_engines = list(filter(jsrun.check_engine, shared.JS_ENGINES))
-  if len(working_engines) < len(shared.JS_ENGINES):
+  working_engines = list(filter(jsrun.check_engine, config.JS_ENGINES))
+  if len(working_engines) < len(config.JS_ENGINES):
     print('Not all the JS engines in JS_ENGINES appears to work.')
     exit(1)
 

commit eba13db5bc3719a8004651825b25b7a490561b58

Fix specifying CLOSURE_COMPILER in config settings
It seems this was broken back in #12699.  Add a test.

Fixes: #14995

tools/building.py
@@ -723,7 +723,7 @@ def eval_ctors(js_file, binary_file, debug_info=False): # noqa
 def get_closure_compiler():
   # First check if the user configured a specific CLOSURE_COMPILER in thier settings
   if config.CLOSURE_COMPILER:
-    return shared.CLOSURE_COMPILER
+    return config.CLOSURE_COMPILER
 
   # Otherwise use the one installed vai npm
   cmd = shared.get_npm_cmd('google-closure-compiler')
@@ -736,12 +736,15 @@ def get_closure_compiler():
 
 
 def check_closure_compiler(cmd, args, env, allowed_to_fail):
+  cmd = cmd + args + ['--version']
   try:
-    output = run_process(cmd + args + ['--version'], stdout=PIPE, env=env).stdout
+    output = run_process(cmd, stdout=PIPE, env=env).stdout
   except Exception as e:
     if allowed_to_fail:
       return False
-    logger.warn(str(e))
+    if isinstance(e, subprocess.CalledProcessError):
+      sys.stderr.write(e.stdout)
+    sys.stderr.write(str(e) + '\n')
     exit_with_error('closure compiler ("%s --version") did not execute properly!' % str(cmd))
 
   if 'Version:' not in output:

tools/config.py
@@ -38,9 +38,9 @@ COMPILER_WRAPPER = None
 
 
 def listify(x):
-  if type(x) is not list:
-    return [x]
-  return x
+  if x is None or type(x) is list:
+    return x
+  return [x]
 
 
 def fix_js_engine(old, new):
@@ -56,7 +56,7 @@ def root_is_writable():
 
 
 def normalize_config_settings():
-  global CACHE, PORTS, JAVA, LLVM_ADD_VERSION, CLANG_ADD_VERSION
+  global CACHE, PORTS, JAVA, LLVM_ADD_VERSION, CLANG_ADD_VERSION, CLOSURE_COMPILER
   global NODE_JS, V8_ENGINE, JS_ENGINE, JS_ENGINES, SPIDERMONKEY_ENGINE, WASM_ENGINES
 
   # EM_CONFIG stuff
@@ -76,6 +76,7 @@ def normalize_config_settings():
   JS_ENGINE = fix_js_engine(JS_ENGINE, listify(JS_ENGINE))
   JS_ENGINES = [listify(engine) for engine in JS_ENGINES]
   WASM_ENGINES = [listify(engine) for engine in WASM_ENGINES]
+  CLOSURE_COMPILER = listify(CLOSURE_COMPILER)
   if not CACHE:
     if FROZEN_CACHE or root_is_writable():
       CACHE = path_from_root('cache')

tests/test_other.py
@@ -10942,3 +10942,11 @@ void foo() {}
     self.build(test_file('other', 'test_pthread_js_exception.c'))
     err = self.run_js('test_pthread_js_exception.js', assert_returncode=NON_ZERO)
     self.assertContained('missing is not defined', err)
+
+  def test_config_closure_compiler(self):
+    self.run_process([EMCC, test_file('hello_world.c'), '--closure=1'])
+    with env_modify({'EM_CLOSURE_COMPILER': sys.executable}):
+      err = self.expect_fail([EMCC, test_file('hello_world.c'), '--closure=1'])
+    self.assertContained('closure compiler', err)
+    self.assertContained(sys.executable, err)
+    self.assertContained('not execute properly!', err)