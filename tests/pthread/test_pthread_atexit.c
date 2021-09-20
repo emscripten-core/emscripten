4f5be19df19e2967642e93d3aaf1ec8369d005d4
Kleis Auke Wolthuizen <github@kleisauke.nl>
2021-09-20 20:57:00 +0200

    Incorporate feedback

--- tests/pthread/test_pthread_atexit.c
+++ tests/pthread/test_pthread_atexit.c
@@ -1,37 +1,36 @@
 #include <assert.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-pthread_mutex_t count_lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t count_nonzero = PTHREAD_COND_INITIALIZER;
-unsigned count;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+bool should_exit;
 
 pthread_t thread;
 
 void *workerThread(void* arg) {
-  pthread_mutex_lock(&count_lock);
-  while (count == 0)
-    pthread_cond_wait(&count_nonzero, &count_lock);
-  count--;
-  pthread_mutex_unlock(&count_lock);
+  pthread_mutex_lock(&mutex);
+  while (!should_exit)
+    pthread_cond_wait(&cond, &mutex);
+  pthread_mutex_unlock(&mutex);
 
   return NULL;
 }
 
 void terminateThread() {
-  pthread_mutex_lock(&count_lock);
-  if (count == 0)
-    pthread_cond_signal(&count_nonzero);
-  count++;
-  pthread_mutex_unlock(&count_lock);
+  pthread_mutex_lock(&mutex);
+  should_exit = true;
+  pthread_cond_signal(&cond);
+  pthread_mutex_unlock(&mutex);
 
   int res = 0;
   int rc = pthread_join(thread, (void**)&res);
   assert(rc == 0);
   assert(res == 0);
 
-  printf("done waiting - counter is: %d\n", count);
+  printf("done waiting - thread successfully terminated\n");
 }
 
 int main(int argc, char* argv[]) {
