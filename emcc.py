28cda6f2b2df9d0531f33982fcc5b1035610f26c
Ng Zhi An <zhin@google.com>
2021-09-24 13:24:30 -0700

    Make -mrelaxed-simd imply -msimd128

--- emcc.py
+++ emcc.py
@@ -769,6 +769,10 @@ def emsdk_cflags(user_args):
       if n in hay:
         return True
 
+  # relaxed-simd implies simd128.
+  if '-mrelaxed-simd' in user_args:
+      user_args += ['-msimd128']
+
   if array_contains_any_of(user_args, SIMD_INTEL_FEATURE_TOWER) or array_contains_any_of(user_args, SIMD_NEON_FLAGS):
     if '-msimd128' not in user_args:
       exit_with_error('Passing any of ' + ', '.join(SIMD_INTEL_FEATURE_TOWER + SIMD_NEON_FLAGS) + ' flags also requires passing -msimd128!')
