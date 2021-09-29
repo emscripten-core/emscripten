5a0d0c175102a81c672bdd46a1aaca0685a01acb
Ethan Lee <nathe.eel@gmail.com>
2021-09-29 19:55:07 -0400

    dup and open file table implementation

--- system/lib/wasmfs/wasmfs.cpp
+++ system/lib/wasmfs/wasmfs.cpp
@@ -18,6 +18,8 @@ extern "C" {
 
 using block_t = uint32_t;
 
+static std::vector<char> fd_write_stdstream_buffer;
+
 class File {
   std::string name;
   uint32_t uid;  // User ID of the owner
@@ -32,23 +34,76 @@ class File {
   std::mutex file_mutex;
 
 public:
-  virtual size_t read(
-    __wasi_fd_t fd, const __wasi_iovec_t* iovs, size_t iovs_len, __wasi_size_t* nread) {
+  // TODO: stubbed virtual function - convert to pure virtual function later
+  virtual __wasi_errno_t read(const __wasi_iovec_t* iovs, size_t iovs_len, __wasi_size_t* nread) {
     return 1;
   };
-  virtual size_t write(
-    __wasi_fd_t fd, const __wasi_ciovec_t* iovs, size_t iovs_len, __wasi_size_t* nwritten) {
-    return 1;
-  };
+  virtual __wasi_errno_t write(
+    const __wasi_ciovec_t* iovs, size_t iovs_len, __wasi_size_t* nwritten) = 0;
+};
+
+class StdinFile : public File {
+public:
+  // TODO: fill in write for stdin
+  __wasi_errno_t write(
+    const __wasi_ciovec_t* iovs, size_t iovs_len, __wasi_size_t* nwritten) override {
+    return 0;
+  }
+};
+
+__wasi_errno_t write_buffer(const __wasi_ciovec_t* iovs, size_t iovs_len, __wasi_size_t* nwritten,
+  void (*func)(const char*)) {
+  __wasi_size_t num = 0;
+  for (size_t i = 0; i < iovs_len; i++) {
+    const uint8_t* buf = iovs[i].buf;
+    __wasi_size_t len = iovs[i].buf_len;
+    for (__wasi_size_t j = 0; j < len; j++) {
+      uint8_t current = buf[j];
+      if (current == 0 || current == 10) {
+        fd_write_stdstream_buffer.push_back('\0'); // for null-terminated C strings
+        func(&fd_write_stdstream_buffer[0]);
+        fd_write_stdstream_buffer.clear();
+      } else {
+        fd_write_stdstream_buffer.push_back(current);
+      }
+    }
+    num += len;
+  }
+  *nwritten = num;
+  return 0;
+}
+
+class StdoutFile : public File {
+public:
+  __wasi_errno_t write(
+    const __wasi_ciovec_t* iovs, size_t iovs_len, __wasi_size_t* nwritten) override {
+    return write_buffer(iovs, iovs_len, nwritten, &emscripten_console_log);
+  }
+};
+
+class StderrFile : public File {
+public:
+  // TODO: May not want to proxy stderr (fd == 2) to the main thread.
+  // This will not show in HTML - a console.warn in a worker is suffficient.
+  // This would be a change from the current FS.
+  __wasi_errno_t write(
+    const __wasi_ciovec_t* iovs, size_t iovs_len, __wasi_size_t* nwritten) override {
+    return write_buffer(iovs, iovs_len, nwritten, &emscripten_console_error);
+  }
 };
 
 class OpenFile {
-  std::shared_ptr<File> fileNode;
+  std::shared_ptr<File> file_node;
   uint32_t _offset;
   std::mutex openfile_mutex;
 
 public:
-  OpenFile(uint32_t offset) { this->_offset = offset; }
+  OpenFile(uint32_t offset, std::shared_ptr<File> _file_node) {
+    this->_offset = offset;
+    this->file_node = _file_node;
+  }
+
+  std::shared_ptr<File> get_file_node() { return file_node; }
 };
 
 class FileTable {
@@ -58,7 +113,7 @@ class FileTable {
 public:
   std::vector<std::shared_ptr<OpenFile>> get_file_handles() { return handles; }
   std::shared_ptr<OpenFile> get_open_file(__wasi_fd_t fd) {
-    // Check if the file descriptor is invalid
+    // Check if the file descriptor is invalid.
     const std::lock_guard<std::mutex> lock(ft_mutex);
     if (fd >= handles.size() || fd < 0)
       return nullptr;
@@ -70,13 +125,13 @@ public:
     const std::lock_guard<std::mutex> lock(ft_mutex);
     for (__wasi_fd_t i = 0; i < handles.size(); i++) {
       if (!handles[i]) {
-        // free open file entry
+        // Free open file entry.
         handles[i] = ptr;
         return i;
       }
     }
 
-    // could not find an empty open file table entry
+    // Could not find an empty open file table entry.
     handles.push_back(ptr);
 
     return handles.size() - 1;
@@ -84,7 +139,7 @@ public:
 
   int remove_open_file(__wasi_fd_t fd) {
     const std::lock_guard<std::mutex> lock(ft_mutex);
-    // Check if the file descriptor is invalid
+    // Check if the file descriptor is invalid.
     if (fd >= handles.size() || fd < 0)
       return -1;
 
@@ -94,22 +149,28 @@ public:
 
   int set_open_file(__wasi_fd_t fd, std::shared_ptr<OpenFile> ptr) {
     const std::lock_guard<std::mutex> lock(ft_mutex);
-    if (fd >= handles.size() || fd < 0)
+    if (fd < 0)
       return -1;
-    handles[fd] = ptr;
+
+    if (fd >= handles.size()) {
+      handles.resize(fd);
+      handles.insert(handles.begin() + fd, ptr);
+    } else {
+      handles[fd] = ptr;
+    }
 
     return 0;
   }
 };
 
-static std::vector<char> fd_write_stdstream_buffer;
+// Global filetable singleton
 static FileTable* ft = nullptr;
 
 void populate_file_table() {
   ft = new FileTable();
-  ft->add_open_file(std::make_shared<OpenFile>(1));
-  ft->add_open_file(std::make_shared<OpenFile>(1));
-  ft->add_open_file(std::make_shared<OpenFile>(1));
+  ft->add_open_file(std::make_shared<OpenFile>(0, std::make_shared<StdinFile>()));
+  ft->add_open_file(std::make_shared<OpenFile>(0, std::make_shared<StdoutFile>()));
+  ft->add_open_file(std::make_shared<OpenFile>(0, std::make_shared<StderrFile>()));
 }
 
 __wasi_fd_t __syscall63(__wasi_fd_t oldfd, __wasi_fd_t newfd) { // dup2
@@ -120,14 +181,14 @@ __wasi_fd_t __syscall63(__wasi_fd_t oldfd, __wasi_fd_t newfd) { // dup2
     populate_file_table();
   }
 
-  if (oldfd == newdf) {
+  if (oldfd == newfd) {
     return oldfd;
   }
 
   // If oldfd is not a valid file descriptor, then the call fails,
   // and newfd is not closed.
 
-  std::shared_ptr<OpenFile> oldfd_openfile = ft->get_open_file(fd);
+  std::shared_ptr<OpenFile> oldfd_openfile = ft->get_open_file(oldfd);
   // Return -1 if the fd is invalid or points to a nullptr
   if (oldfd_openfile == nullptr)
     return -1;
@@ -136,21 +197,18 @@ __wasi_fd_t __syscall63(__wasi_fd_t oldfd, __wasi_fd_t newfd) { // dup2
   // before being reused; the close is performed silently.
 
   std::shared_ptr<OpenFile> newfd_openfile = ft->get_open_file(newfd);
-  // TODO: what happens if newfd > size of the vector?
   if (newfd_openfile) {
     ft->remove_open_file(newfd);
   }
 
-  set_open_file(newfd, oldfd_openfile);
+  ft->set_open_file(newfd, oldfd_openfile);
 
   return newfd;
 }
 
 __wasi_fd_t __syscall41(__wasi_fd_t fd) { // dup
-  EM_ASM({ Module.print("dup entered"); });
-
   if (!ft) {
-    _populateFileTable();
+    populate_file_table();
   }
 
   std::shared_ptr<OpenFile> curr_openfile = ft->get_open_file(fd);
@@ -161,9 +219,9 @@ __wasi_fd_t __syscall41(__wasi_fd_t fd) { // dup
   // Find the first free open file entry
   __wasi_fd_t ret_fd = ft->add_open_file(curr_openfile);
 
-  for (auto x : ft->get_file_handles()) {
-    EM_ASM({ Module.print("something count:" + $0); }, x.use_count());
-  }
+  // for (auto x : ft->get_file_handles()) {
+  //   EM_ASM({ Module.print("something count:" + $0); }, x.use_count());
+  // }
 
   return ret_fd;
 }
@@ -171,52 +229,41 @@ __wasi_fd_t __syscall41(__wasi_fd_t fd) { // dup
 __wasi_errno_t __wasi_fd_write(
   __wasi_fd_t fd, const __wasi_ciovec_t* iovs, size_t iovs_len, __wasi_size_t* nwritten) {
   if (!ft) {
-    _populateFileTable();
+    populate_file_table();
   }
-  // FD 1 = STDOUT and FD 2 = STDERR.
-  // Temporary hardcoding of filedescriptor values.
-  // TODO: May not want to proxy stderr (fd == 2) to the main thread.
-  // This will not show in HTML - a console.warn in a worker is suffficient.
-  // This would be a change from the current FS.
-  if (fd == 1 || fd == 2) {
-    __wasi_size_t num = 0;
-    for (size_t i = 0; i < iovs_len; i++) {
-      const uint8_t* buf = iovs[i].buf;
-      __wasi_size_t len = iovs[i].buf_len;
-      for (__wasi_size_t j = 0; j < len; j++) {
-        uint8_t current = buf[j];
-        if (current == 0 || current == 10) {
-          fd_write_stdstream_buffer.push_back('\0'); // for null-terminated C strings
-          if (fd == 1) {
-            emscripten_console_log(&fd_write_stdstream_buffer[0]);
-          } else if (fd == 2) {
-            emscripten_console_error(&fd_write_stdstream_buffer[0]);
-          }
-          fd_write_stdstream_buffer.clear();
-        } else {
-          fd_write_stdstream_buffer.push_back(current);
-        }
-      }
-      num += len;
-    }
-    *nwritten = num;
+
+  // Get the corresponding OpenFile from the open file table
+  std::shared_ptr<OpenFile> selected_openfile = ft->get_open_file(fd);
+  if (!selected_openfile) {
+    return -1;
   }
-  return 0;
+
+  // Call the corresponding write function from the specified file
+  return selected_openfile->get_file_node()->write(iovs, iovs_len, nwritten);
 }
 
 __wasi_errno_t __wasi_fd_seek(
   __wasi_fd_t fd, __wasi_filedelta_t offset, __wasi_whence_t whence, __wasi_filesize_t* newoffset) {
+  if (!ft) {
+    populate_file_table();
+  }
   emscripten_console_log("__wasi_fd_seek has been temporarily stubbed and is inert");
   abort();
 }
 
 __wasi_errno_t __wasi_fd_close(__wasi_fd_t fd) {
+  if (!ft) {
+    populate_file_table();
+  }
   emscripten_console_log("__wasi_fd_close has been temporarily stubbed and is inert");
   abort();
 }
 
 __wasi_errno_t __wasi_fd_read(
   __wasi_fd_t fd, const __wasi_iovec_t* iovs, size_t iovs_len, __wasi_size_t* nread) {
+  if (!ft) {
+    populate_file_table();
+  }
   emscripten_console_log("__wasi_fd_read has been temporarily stubbed and is inert");
   abort();
 }
---
+++ tests/unistd/dup2.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 The Emscripten Authors.  All rights reserved.
+ * Emscripten is available under two separate licenses, the MIT license and the
+ * University of Illinois/NCSA Open Source License.  Both these licenses can be
+ * found in the LICENSE file.
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+  int f = 1;
+
+  printf("DUP\n");
+  int f2 = dup(f);
+
+  // Print to stdout and print to fd = 4
+  dprintf(f, "STDOUT\n");
+  dprintf(f2, "CAN PRINT TO STDOUT WITH fd = 4\n");
+
+  printf("DUP2\n");
+  int f3 = 5;
+  int f4 = dup2(f, f3);
+
+  // Print to stdout and print to fd = 5
+  dprintf(f, "STDOUT\n");
+  dprintf(f3, "CAN PRINT TO STDOUT WITH fd = 5\n");
+  dprintf(f4, "CAN PRINT TO STDOUT WITH fd = 5\n");
+
+  // Try printing to a non-existent fd
+  dprintf(10, "FD DOES NOT EXIST\n");
+
+  return 0;
+}
