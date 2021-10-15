fc8d7fdd5d4d1b46a936793c576af3d142650a47
Ethan Lee <nathe.eel@gmail.com>
2021-10-14 21:36:41 -0400

    Change .clang-format (#15299)
    * copy binaryen format for .clang-format

--- system/lib/wasmfs/file_table.cpp
+++ system/lib/wasmfs/file_table.cpp
@@ -12,8 +12,10 @@ namespace wasmfs {
 
 std::vector<std::shared_ptr<OpenFileState>> FileTable::entries;
 
-static __wasi_errno_t writeStdBuffer(const uint8_t* buf, __wasi_size_t len,
-  void (*console_write)(const char*), std::vector<char>& fd_write_buffer) {
+static __wasi_errno_t writeStdBuffer(const uint8_t* buf,
+                                     __wasi_size_t len,
+                                     void (*console_write)(const char*),
+                                     std::vector<char>& fd_write_buffer) {
   for (__wasi_size_t j = 0; j < len; j++) {
     uint8_t current = buf[j];
     if (current == '\0' || current == '\n') {
@@ -66,9 +68,12 @@ class StderrFile : public File {
 };
 
 FileTable::FileTable() {
-  entries.push_back(std::make_shared<OpenFileState>(0, std::make_shared<StdinFile>()));
-  entries.push_back(std::make_shared<OpenFileState>(0, std::make_shared<StdoutFile>()));
-  entries.push_back(std::make_shared<OpenFileState>(0, std::make_shared<StderrFile>()));
+  entries.push_back(
+    std::make_shared<OpenFileState>(0, std::make_shared<StdinFile>()));
+  entries.push_back(
+    std::make_shared<OpenFileState>(0, std::make_shared<StdoutFile>()));
+  entries.push_back(
+    std::make_shared<OpenFileState>(0, std::make_shared<StderrFile>()));
 }
 
 FileTable::Handle FileTable::get() {
@@ -85,7 +90,8 @@ FileTable::Handle::Entry::operator std::shared_ptr<OpenFileState>() const {
   return fileTableHandle.fileTable.entries[fd];
 }
 
-FileTable::Handle::Entry& FileTable::Handle::Entry::operator=(std::shared_ptr<OpenFileState> ptr) {
+FileTable::Handle::Entry&
+FileTable::Handle::Entry::operator=(std::shared_ptr<OpenFileState> ptr) {
   assert(fd >= 0);
 
   if (fd >= fileTableHandle.fileTable.entries.size()) {
