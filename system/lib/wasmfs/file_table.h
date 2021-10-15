3b176aff497e921d11ea8cb63e551e8fabbb54ed
Ethan Lee <nathe.eel@gmail.com>
2021-10-15 18:51:44 -0400

    [WASMFS] Make Handle Acquisition clearer with locked() and unlocked() (#15309)
    Relevant Issue: #15041
    - Introduce locked and unlocked on Entry, renamed get() on other Handles

--- system/lib/wasmfs/file_table.h
+++ system/lib/wasmfs/file_table.h
@@ -19,28 +19,30 @@ namespace wasmfs {
 
 std::shared_ptr<Directory> getRootDirectory();
 
-class OpenFileState {
+class OpenFileState : public std::enable_shared_from_this<OpenFileState> {
   std::shared_ptr<File> file;
   __wasi_filedelta_t offset;
-  // An OpenFileState needs a mutex if there are concurrent accesses on one open file
-  // descriptor. This could occur if there are multiple seeks on the same open file descriptor.
+  // An OpenFileState needs a mutex if there are concurrent accesses on one open
+  // file descriptor. This could occur if there are multiple seeks on the same
+  // open file descriptor.
   std::mutex mutex;
 
 public:
-  OpenFileState(uint32_t offset, std::shared_ptr<File> file) : offset(offset), file(file) {}
+  OpenFileState(uint32_t offset, std::shared_ptr<File> file)
+    : offset(offset), file(file) {}
 
   class Handle {
-    OpenFileState& openFileState;
+    std::shared_ptr<OpenFileState> openFileState;
     std::unique_lock<std::mutex> lock;
 
   public:
-    Handle(OpenFileState& openFileState)
-      : openFileState(openFileState), lock(openFileState.mutex) {}
+    Handle(std::shared_ptr<OpenFileState> openFileState)
+      : openFileState(openFileState), lock(openFileState->mutex) {}
 
-    std::shared_ptr<File>& getFile() { return openFileState.file; };
+    std::shared_ptr<File>& getFile() { return openFileState->file; };
   };
 
-  Handle get() { return Handle(*this); }
+  Handle get() { return Handle(shared_from_this()); }
 };
 
 class FileTable {
@@ -50,25 +52,30 @@ class FileTable {
   FileTable();
 
 public:
-  // Handle represents an RAII wrapper object. Access to the global FileTable must go through a
-  // Handle. A Handle holds the single global FileTable's lock for the duration of its lifetime.
-  // This is necessary because a FileTable may have atomic operations where the lock must be held
-  // across multiple methods. By providing access through the handle, callers of file table methods
-  // do not need to remember to take a lock for every access.
+  // Handle represents an RAII wrapper object. Access to the global FileTable
+  // must go through a Handle. A Handle holds the single global FileTable's lock
+  // for the duration of its lifetime. This is necessary because a FileTable may
+  // have atomic operations where the lock must be held across multiple methods.
+  // By providing access through the handle, callers of file table methods do
+  // not need to remember to take a lock for every access.
   class Handle {
     FileTable& fileTable;
     std::unique_lock<std::mutex> lock;
 
   public:
-    Handle(FileTable& fileTable) : fileTable(fileTable), lock(fileTable.mutex) {}
+    Handle(FileTable& fileTable)
+      : fileTable(fileTable), lock(fileTable.mutex) {}
 
-    // The Entry class abstracts over the list of entries, providing a simple and safe interface
-    // that looks much like accessing a std::map, in that table[x] = y will allocate a new entry if
-    // one is not already present there. One minor difference from std::map is that table[x] does
-    // not return a reference, and can be used to check for the lack of an item there without
-    // allocation (similar to how table[x] works on a JS object), which keeps syntax concise.
+    // The Entry class abstracts over the list of entries, providing a simple
+    // and safe interface that looks much like accessing a std::map, in that
+    // table[x] = y will allocate a new entry if one is not already present
+    // there. One minor difference from std::map is that table[x] does not
+    // return a reference, and can be used to check for the lack of an item
+    // there without allocation (similar to how table[x] works on a JS object),
+    // which keeps syntax concise.
     struct Entry {
-      // Need to store a reference to the single global filetable, which is a local static variable.
+      // Need to store a reference to the single global filetable, which is a
+      // local static variable.
       Handle& fileTableHandle;
       __wasi_fd_t fd;
 
@@ -76,9 +83,18 @@ public:
 
       Entry& operator=(std::shared_ptr<OpenFileState> ptr);
 
-      Entry& operator=(Entry& entry) { return *this = std::shared_ptr<OpenFileState>(entry); }
+      Entry& operator=(Entry& entry) {
+        return *this = std::shared_ptr<OpenFileState>(entry);
+      }
 
-      std::shared_ptr<OpenFileState>& operator->();
+      // Return a locked Handle to access OpenFileState members.
+      OpenFileState::Handle locked() {
+        assert(fd < fileTableHandle.fileTable.entries.size() && fd > 0);
+        return unlocked()->get();
+      }
+
+      // Return an OpenFileState without member access.
+      std::shared_ptr<OpenFileState> unlocked();
 
       // Check whether the entry exists (i.e. contains an OpenFileState).
       operator bool() const;
--- system/lib/wasmfs/wasmfs.cpp
+++ system/lib/wasmfs/wasmfs.cpp
@@ -41,7 +41,7 @@ long __syscall_dup2(long oldfd, long newfd) {
 
   // If the file descriptor newfd was previously open, it will just be
   // overwritten silently.
-  fileTable[newfd] = oldOpenFile;
+  fileTable[newfd] = oldOpenFile.unlocked();
   return newfd;
 }
 
@@ -54,32 +54,34 @@ long __syscall_dup(long fd) {
     return -(EBADF);
   }
 
-  return fileTable.add(openFile);
+  return fileTable.add(openFile.unlocked());
 }
 
 __wasi_errno_t __wasi_fd_write(__wasi_fd_t fd,
                                const __wasi_ciovec_t* iovs,
                                size_t iovs_len,
                                __wasi_size_t* nwritten) {
-  std::shared_ptr<OpenFileState> openFile = FileTable::get()[fd];
+  auto openFile = FileTable::get()[fd];
 
   if (!openFile) {
     return __WASI_ERRNO_BADF;
   }
 
-  auto file = openFile->get().getFile()->dynCast<DataFile>();
+  auto file = openFile.locked().getFile()->dynCast<DataFile>();
 
   // If file is nullptr, then the file was not a DataFile.
   if (!file) {
     return __WASI_ERRNO_ISDIR;
   }
 
+  auto lockedFile = file->locked();
+
   __wasi_size_t num = 0;
   for (size_t i = 0; i < iovs_len; i++) {
     const uint8_t* buf = iovs[i].buf;
     __wasi_size_t len = iovs[i].buf_len;
 
-    file->get().write(buf, len);
+    lockedFile.write(buf, len);
     num += len;
   }
   *nwritten = num;
@@ -107,16 +109,16 @@ __wasi_errno_t __wasi_fd_close(__wasi_fd_t fd) {
 
 long __syscall_fstat64(long fd, long buf) {
   // Release FileTable lock after accessing desired open file.
-  std::shared_ptr<File> file = FileTable::get()[fd]->get().getFile();
+  auto file = FileTable::get()[fd].locked().getFile();
 
   struct stat* buffer = (struct stat*)buf;
 
-  auto fileInfo = file->get();
+  auto lockedFile = file->locked();
 
   if (file->is<Directory>()) {
     buffer->st_size = 4096;
   } else if (file->is<DataFile>()) {
-    buffer->st_size = fileInfo.size();
+    buffer->st_size = lockedFile.size();
   } else { // TODO: add size of symlinks
     buffer->st_size = 0;
   }
@@ -126,7 +128,7 @@ long __syscall_fstat64(long fd, long buf) {
   buffer->st_dev =
     1; // ID of device containing file: Hardcode 1 for now, no meaning at the
   // moment for Emscripten.
-  buffer->st_mode = fileInfo.mode();
+  buffer->st_mode = lockedFile.mode();
   buffer->st_ino = fd;
   // The number of hard links is 1 since they are unsupported.
   buffer->st_nlink = 1;
@@ -139,9 +141,9 @@ long __syscall_fstat64(long fd, long buf) {
   buffer->st_blocks = (buffer->st_size + 511) / 512;
   buffer->st_blksize =
     4096; // Specifies the preferred blocksize for efficient disk I/O.
-  buffer->st_atim.tv_sec = fileInfo.atime();
-  buffer->st_mtim.tv_sec = fileInfo.mtime();
-  buffer->st_ctim.tv_sec = fileInfo.ctime();
+  buffer->st_atim.tv_sec = lockedFile.atime();
+  buffer->st_mtim.tv_sec = lockedFile.mtime();
+  buffer->st_ctim.tv_sec = lockedFile.ctime();
 
   return __WASI_ERRNO_SUCCESS;
 }
@@ -150,25 +152,27 @@ __wasi_errno_t __wasi_fd_read(__wasi_fd_t fd,
                               const __wasi_iovec_t* iovs,
                               size_t iovs_len,
                               __wasi_size_t* nread) {
-  std::shared_ptr<OpenFileState> openFile = FileTable::get()[fd];
+  auto openFile = FileTable::get()[fd];
 
   if (!openFile) {
     return __WASI_ERRNO_BADF;
   }
 
-  auto file = openFile->get().getFile()->dynCast<DataFile>();
+  auto file = openFile.locked().getFile()->dynCast<DataFile>();
 
   // If file is nullptr, then the file was not a DataFile.
   if (!file) {
     return __WASI_ERRNO_ISDIR;
   }
 
+  auto lockedFile = file->locked();
+
   __wasi_size_t num = 0;
   for (size_t i = 0; i < iovs_len; i++) {
     const uint8_t* buf = iovs[i].buf;
     __wasi_size_t len = iovs[i].buf_len;
 
-    file->get().read(buf, len);
+    lockedFile.read(buf, len);
     num += len;
   }
   *nread = num;
@@ -211,9 +215,9 @@ __wasi_fd_t __syscall_open(long pathname, long flags, long mode) {
 
     // Find the next entry in the current directory entry
 #ifdef WASMFS_DEBUG
-    directory->get().printKeys();
+    directory->locked().printKeys();
 #endif
-    curr = directory->get().getEntry(pathParts[i]);
+    curr = directory->locked().getEntry(pathParts[i]);
 
     // Requested entry (file or directory
     if (!curr) {
