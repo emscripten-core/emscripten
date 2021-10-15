ca171a67855a7cbcfaa80a18c0364e801e8d4dc2
Ethan Lee <nathe.eel@gmail.com>
2021-10-14 21:49:46 -0400

    rename test file and add fixme (#15290)
    Rename `dup2.c` to `wasmfs_dup.c`

--- tests/test_other.py
+++ tests/test_other.py
@@ -11145,6 +11145,6 @@ void foo() {}
 
   # WASMFS tests
 
-  def test_wasmfs_unistd_dup_write(self):
+  def test_wasmfs_unistd_dup(self):
     self.set_setting('WASMFS')
-    self.do_run_in_out_file_test('unistd/dup2.c')
+    self.do_run_in_out_file_test('unistd/wasmfs_dup.c')
