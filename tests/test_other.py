783c9c6bc86ba10ff3fcdaab238bc17d58d9ca1e
Ethan Lee <nathe.eel@gmail.com>
2021-10-18 14:03:59 -0400

    linting

--- tests/test_other.py
+++ tests/test_other.py
@@ -11171,7 +11171,7 @@ void foo() {}
   def test_wasmfs_unistd_fstat(self):
     self.set_setting('WASMFS')
     self.do_run_in_out_file_test('wasmfs/wasmfs_fstat.c')
-    
+
   def test_wasmfs_unistd_create(self):
     self.set_setting('WASMFS')
     self.do_run_in_out_file_test('wasmfs/wasmfs_create.c')
