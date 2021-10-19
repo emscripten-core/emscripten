1771a5d2f144a617400cd1808002daa7378dd08c
spsaxena <mail2spsaxena@autodesk.com>
2021-10-20 07:55:01 +0800

    try reproduce BIGINT error

--- tests/test_core.py
+++ tests/test_core.py
@@ -1274,8 +1274,10 @@ int main(int argc, char **argv)
     err = self.expect_fail([EMCC, test_file('hello_world.c')] + self.get_emcc_args())
     self.assertContained('error: DISABLE_EXCEPTION_CATCHING and EXCEPTION_CATCHING_ALLOWED are mutually exclusive', err)
 
+  @require_v8
   def test_exceptions_std_mm2(self):
     self.set_setting('MAIN_MODULE', 2)
+    self.set_setting('WASM_BIGINT')
     self.emcc_args.append('-fPIC')
     self.emcc_args.append('-fwasm-exceptions')
     self.do_core_test('test_exceptions_std.cpp')
