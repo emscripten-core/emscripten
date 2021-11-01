b55668b4d7cff0077103cc8467abeaa0f1fd3d91
Ethan Lee <nathe.eel@gmail.com>
2021-11-01 19:13:15 -0400

    final changes

--- system/lib/wasmfs/wasmfs.cpp
+++ system/lib/wasmfs/wasmfs.cpp
@@ -11,22 +11,22 @@
 
 namespace wasmfs {
 
-// The below lines are included to make the preprocessor believe that the global
-// constructor is included as a header. This ensures that the global state of
-// the file system is constructed before anything else. ATTENTION: No other
-// static global objects should be defined besides wasmFS.
-// Due to # define _LIBCPP_INIT_PRIORITY_MAX
+// The below lines are included to make the compiler believe that the global
+// constructor is part of a system header, which is necessary to work around a
+// compilation error about using a reserved init priority less than 101. This
+// ensures that the global state of the file system is constructed before
+// anything else. ATTENTION: No other static global objects should be defined
+// besides wasmFS. Due to # define _LIBCPP_INIT_PRIORITY_MAX
 // __attribute__((init_priority(101))), we must use init priority 100 (reserved
 // system priority) since wasmFS is a system level component.
 // TODO: consider instead adding this in libc's startup code.
 // WARNING: Maintain # n + 1 "wasmfs.cpp" 3 where n = line number.
-# 24 "wasmfs.cpp" 3
+# 25 "wasmfs.cpp" 3
 __attribute__((init_priority(100))) WasmFS wasmFS;
-# 26 "wasmfs.cpp"
+# 27 "wasmfs.cpp"
 
 std::shared_ptr<Directory> WasmFS::initRootDirectory() {
-  std::shared_ptr<Directory> rootDirectory =
-    std::make_shared<Directory>(S_IRUGO | S_IXUGO);
+  auto rootDirectory = std::make_shared<Directory>(S_IRUGO | S_IXUGO);
   auto devDirectory = std::make_shared<Directory>(S_IRUGO | S_IXUGO);
   rootDirectory->locked().setEntry("dev", devDirectory);
 
