b9c78a4834133f429a8f86b0088f038b5505d205
Ethan Lee <nathe.eel@gmail.com>
2021-11-05 19:58:09 -0400

    [WASMFS] Fix mode on file and dir creation (#15445)
    Relevant Issue: #15041
    * fix mode on file and dir creation, update tests to verify
    * file type is now derived from kind
    * Add optional mode to open and refactor tests

--- tests/wasmfs/wasmfs_create.c
+++ tests/wasmfs/wasmfs_create.c
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 // FIXME: Merge with other existing close and open tests.
@@ -17,7 +18,16 @@
 int main() {
   // Test creating a new file and writing and reading from it.
   errno = 0;
-  int fd = open("/test", O_RDWR | O_CREAT);
+  int fd = open("/test", O_RDWR | O_CREAT, 0777);
+
+  // Check that the file type is correct on mode.
+  struct stat file;
+  fstat(fd, &file);
+
+  assert((file.st_mode & S_IFMT) == S_IFREG);
+  printf("mode %i\n", file.st_mode);
+  assert(file.st_mode == (S_IRWXUGO | S_IFREG));
+
   assert(errno == 0);
   const char* msg = "Test\n";
   errno = 0;
