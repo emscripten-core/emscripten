6736c2b1ec3d4f1aa489af8945efe9bbe7c803b6
Sam Clegg <sbc@chromium.org>
2021-11-08 09:07:38 -0800

    Enable browser tests to be run under node (#12884)
    In some cases, specifically thread tests, it can be useful to expriment
    with running browser tests under node.
    
    This change add a new special value to EMTEST_BROSWER that will cause
    `btest` to attempt to run the test under node.
    
    I've also adding since browser test to the wasm2-test suite in circle CI
    so this features gets tested.

--- tests/report_result.cpp
+++ tests/report_result.cpp
@@ -6,35 +6,55 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#ifdef __EMSCRIPTEN__
+#include "report_result.h"
 
+#if defined __EMSCRIPTEN__ && !defined EMTEST_NODE
 #include <emscripten.h>
-
-#ifndef EMTEST_PORT_NUMBER
-#error "EMTEST_PORT_NUMBER not defined"
 #endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void EMSCRIPTEN_KEEPALIVE _ReportResult(int result, int sync)
-{
+#if defined __EMSCRIPTEN__ && !defined EMTEST_NODE
+#ifndef EMTEST_PORT_NUMBER
+#error "EMTEST_PORT_NUMBER not defined"
+#endif
+
+void EMSCRIPTEN_KEEPALIVE _ReportResult(int result, int sync) {
   EM_ASM({
     reportResultToServer($0, $1, $2);
   }, result, sync, EMTEST_PORT_NUMBER);
 }
 
-void EMSCRIPTEN_KEEPALIVE _MaybeReportResult(int result, int sync)
-{
+void EMSCRIPTEN_KEEPALIVE _MaybeReportResult(int result, int sync) {
   EM_ASM({
     maybeReportResultToServer($0, $1, $2);
   }, result, sync, EMTEST_PORT_NUMBER);
 }
 
+#else
+
+static bool reported = false;
+
+void _ReportResult(int result, int sync) {
+  if (reported) {
+    printf("ERROR: result already reported\n");
+    exit(1);
+  }
+  reported = true;
+  printf("RESULT: %d\n", result);
+}
+
+void _MaybeReportResult(int result, int sync) {
+  if (!reported) _ReportResult(result, sync);
+}
+
+#endif // __EMSCRIPTEN__ && !defined EMTEST_NODE
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif // __EMSCRIPTEN__
