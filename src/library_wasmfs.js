8762e71865c8166e369e1668eae86a61973f7628
Ethan Lee <nathe.eel@gmail.com>
2021-11-10 16:27:21 -0500

    makegetvalue - readfile

--- src/library_wasmfs.js
+++ src/library_wasmfs.js
@@ -65,7 +65,7 @@ var WasmfsLibrary = {
       // Copy the file into a JS buffer on the heap.
       var buf = _emscripten_wasmfs_read_file(pathName);
       // The integer length is returned in the first 8 bytes of the buffer.
-      var length = getValue(buf, 'i64');
+      var length = {{{ makeGetValue('buf', '0', 'i64') }}};
       
       // Default return type is binary.
       // The buffer contents exist 8 bytes after the returned pointer.
