e52c22fb5d171ce21614c2bc21ecfbdd42b40c3f
Sam Clegg <sbc@chromium.org>
2021-11-11 15:43:55 -0800

    Add directory containing useful maintenance scripts (#15490)
    It is sometimes useful to create scripts like this and in the past it has
    not been clear where they should live.

---
+++ tools/maint/check_struct_info.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+import subprocess
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(os.path.dirname(script_dir))
+
+sys.path.append(root_dir)
+
+import emscripten
+from tools.settings import settings
+
+def check_structs():
+  for struct in info['structs'].keys():
+    key = 'C_STRUCTS.' + struct + '.'
+    # grep --quiet ruturns 0 when there is a match
+    if subprocess.run(['git', 'grep', '--quiet', key], check=False).returncode != 0:
+      print(struct)
+
+def check_defines():
+  for define in info['defines'].keys():
+    key = 'cDefine(.' +  define + '.)'
+    # grep --quiet ruturns 0 when there is a match
+    if subprocess.run(['git', 'grep', '--quiet', key], check=False).returncode != 0:
+      print(define)
+
+
+emscripten.generate_struct_info()
+info = json.loads(open(settings.STRUCT_INFO).read())
+#check_structs()
+check_defines()
