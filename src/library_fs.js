c17c64fc2c112f128a484b77a4565a64cc95efd8
Sam Clegg <sbc@chromium.org>
2021-12-06 15:57:52 -0800

    Use `hasExportedFunction` to detect if fflush is exported. NFC

--- src/library_fs.js
+++ src/library_fs.js
@@ -1515,8 +1515,9 @@ FS.staticInit();` +
     quit: function() {
       FS.init.initialized = false;
       // force-flush all streams, so we get musl std streams printed out
-      var fflush = Module['_fflush'];
-      if (fflush) fflush(0);
+#if hasExportedFunction('_fflush')
+      _fflush(0);
+#endif
       // close all of our streams
       for (var i = 0; i < FS.streams.length; i++) {
         var stream = FS.streams[i];
