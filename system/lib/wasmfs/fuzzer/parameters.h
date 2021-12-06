8be8d23548d1a051a6037a301004718a83de2f25
Ethan Lee <nathe.eel@gmail.com>
2021-12-05 19:09:01 -0500

    [WASMFS] Fuzzer setup (#15689)
    Relevant Issue: #15041 
    
    Objective: Setup simple fuzzer to identify unexpected behaviour caused by data races and other thread-related issues in WasmFS.
    
    A test should check for tearing in file reads and writes. If thread A is performing a write and then thread B performs a read, the read should not interrupt the write. Reads and writes should ensure data consistency (i.e. it should not be possible to read a file in an intermediate state and observe only partial data that has been written).

---
+++ system/lib/wasmfs/fuzzer/parameters.h
@@ -0,0 +1,25 @@
+// Copyright 2021 The Emscripten Authors.  All rights reserved.
+// Emscripten is available under two separate licenses, the MIT license and the
+// University of Illinois/NCSA Open Source License.  Both these licenses can be
+// found in the LICENSE file.
+
+// This file defines global parameters used in the fuzzer.
+
+#pragma once
+
+namespace wasmfs {
+//
+// ReadWrite Parameters
+//
+// The number of writer threads.
+constexpr uint32_t NUM_WRITERS = 1;
+
+// The number of reader threads.
+constexpr uint32_t NUM_READERS = 20;
+
+// The size of the character strings written to a file.
+constexpr uint32_t NUM_WORK_SIZE = 20000000;
+
+// The number of strings that are written/read.
+constexpr uint32_t NUM_WORK = 10;
+} // namespace wasmfs
