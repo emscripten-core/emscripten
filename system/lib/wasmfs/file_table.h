97459ba8d803993e8c8df53f8b50b72566b5f371
Ethan Lee <nathe.eel@gmail.com>
2021-12-12 00:43:58 -0500

    formatting and remove redundant function

--- system/lib/wasmfs/file_table.h
+++ system/lib/wasmfs/file_table.h
@@ -57,8 +57,6 @@ public:
 
     off_t& position() { return openFileState->position; };
   };
-
-  Handle get() { return Handle(shared_from_this()); }
 };
 
 class FileTable {
@@ -72,13 +70,14 @@ class FileTable {
   FileTable();
 
 public:
+  class Entry;
+
   // Handle represents an RAII wrapper object. Access to the global FileTable
   // must go through a Handle. A Handle holds the single global FileTable's lock
   // for the duration of its lifetime. This is necessary because a FileTable may
   // have atomic operations where the lock must be held across multiple methods.
   // By providing access through the handle, callers of file table methods do
   // not need to remember to take a lock for every access.
-  class Entry;
   class Handle {
   protected:
     FileTable& fileTable;
@@ -88,21 +87,21 @@ public:
     Handle(FileTable& fileTable)
       : fileTable(fileTable), lock(fileTable.mutex) {}
 
-    // The Entry class abstracts over the list of entries, providing a simple
-    // and safe interface that looks much like accessing a std::map, in that
-    // table[x] = y will allocate a new entry if one is not already present
-    // there. One minor difference from std::map is that table[x] does not
-    // return a reference, and can be used to check for the lack of an item
-    // there without allocation (similar to how table[x] works on a JS object),
-    // which keeps syntax concise.
-
     Entry operator[](__wasi_fd_t fd);
 
     __wasi_fd_t add(std::shared_ptr<OpenFileState> openFileState);
   };
 };
 
+// The Entry class abstracts over the list of entries, providing a simple
+// and safe interface that looks much like accessing a std::map, in that
+// table[x] = y will allocate a new entry if one is not already present
+// there. One minor difference from std::map is that table[x] does not
+// return a reference, and can be used to check for the lack of an item
+// there without allocation (similar to how table[x] works on a JS object),
+// which keeps syntax concise.
 class FileTable::Entry : public FileTable::Handle {
+  // Handle must be a friend to access the private constructor of Entry.
   friend FileTable::Handle;
 
 public:
@@ -127,8 +126,6 @@ public:
   operator bool() const;
 
 private:
-  // Need to store a reference to the single global filetable, which is a
-  // local static variable.
   __wasi_fd_t fd;
 
   Entry(FileTable& fileTable, __wasi_fd_t fd)
