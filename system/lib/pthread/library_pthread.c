7163beb3ff64b10b8ed1eaa8de4b0b96e0970006
Sam Clegg <sbc@chromium.org>
2021-12-13 12:21:02 -0800

    Move internal-only parts of emscripten/threading.h (#15756)
    The layout of the em_called_queue struct does not need to
    be public.

--- system/lib/pthread/library_pthread.c
+++ system/lib/pthread/library_pthread.c
@@ -34,6 +34,8 @@
 #include <emscripten/threading.h>
 #include <emscripten/stack.h>
 
+#include "threading_internal.h"
+
 void __pthread_testcancel();
 
 int emscripten_pthread_attr_gettransferredcanvases(const pthread_attr_t* a, const char** str) {
@@ -90,10 +92,10 @@ void emscripten_thread_sleep(double msecs) {
   emscripten_current_thread_process_queued_calls();
 
   // If we have less than this many msecs left to wait, busy spin that instead.
-  const double minimumTimeSliceToSleep = 0.1;
+  double min_ms_slice_to_sleep = 0.1;
 
   // runtime thread may need to run proxied calls, so sleep in very small slices to be responsive.
-  const double maxMsecsSliceToSleep = emscripten_is_main_runtime_thread() ? 1 : 100;
+  double max_ms_slice_to_sleep = emscripten_is_main_runtime_thread() ? 1 : 100;
 
   emscripten_conditional_set_current_thread_status(
     EM_THREAD_STATUS_RUNNING, EM_THREAD_STATUS_SLEEPING);
@@ -105,11 +107,13 @@ void emscripten_thread_sleep(double msecs) {
     emscripten_current_thread_process_queued_calls();
 
     now = emscripten_get_now();
-    double msecsToSleep = target - now;
-    if (msecsToSleep > maxMsecsSliceToSleep)
-      msecsToSleep = maxMsecsSliceToSleep;
-    if (msecsToSleep >= minimumTimeSliceToSleep)
-      emscripten_futex_wait(&dummyZeroAddress, 0, msecsToSleep);
+    double ms_to_sleep = target - now;
+    if (ms_to_sleep > max_ms_slice_to_sleep) {
+      ms_to_sleep = max_ms_slice_to_sleep;
+    }
+    if (ms_to_sleep >= min_ms_slice_to_sleep) {
+      emscripten_futex_wait(&dummyZeroAddress, 0, ms_to_sleep);
+    }
     now = emscripten_get_now();
   };
 
