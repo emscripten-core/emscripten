283a8c54d1b319d023fd830112ccd7fc1f0d02a2
Sam Clegg <sbc@chromium.org>
2021-12-14 21:50:18 -0800

    Unify libc and libc_rt libraries
    The distinction between these two libraries was only about what C flags
    to use.  Rather than have two separate libraries add a facility for
    per-file cflags.
    
    This makes our libc more "normal" and makes it easier to have the
    clang driver use emscripten as linker.
    
    See: #15703

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -87,7 +87,7 @@ def create_lib(libname, inputs):
     building.emar('cr', libname, inputs)
 
 
-def get_libc_rt_files(is_optz=False, superset=False):
+def non_lto_libc_files():
   # Combining static linking with LTO is tricky under LLVM.  The codegen that
   # happens during LTO can generate references to new symbols that didn't exist
   # in the linker inputs themselves.
@@ -103,13 +103,6 @@ def get_libc_rt_files(is_optz=False, superset=False):
   # Note that this also includes things that may be depended on by those
   # functions - fmin uses signbit, for example, so signbit must be here (so if
   # fmin is added by codegen, it will have all it needs).
-  #
-  # This function is called from two different places:
-  # 1. From libc-rt to decide which files to include.  We pass is_optz if we
-  #    are building the size-optimized version of the library.
-  # 2. From the libc library to decide which files to ignore.  Here is pass
-  #    `superset=True` since we want ignore the superset all files that might
-  #    be part of libc-rt (size-optimized or otherwise).
   math_files = [
     'fmin.c', 'fminf.c', 'fminl.c',
     'fmax.c', 'fmaxf.c', 'fmaxl.c',
@@ -128,12 +121,9 @@ def get_libc_rt_files(is_optz=False, superset=False):
     '__math_oflow.c', '__math_oflowf.c',
     '__math_uflow.c', '__math_uflowf.c',
     '__math_invalid.c', '__math_invalidf.c', '__math_invalidl.c',
+    'pow_small.c', 'log_small.c', 'log2_small.c',
+    'pow.c', 'pow_data.c', 'log.c', 'log_data.c', 'log2.c', 'log2_data.c'
   ]
-  if superset or is_optz:
-    math_files += ['pow_small.c', 'log_small.c', 'log2_small.c']
-  if superset or not is_optz:
-    math_files += ['pow.c', 'pow_data.c', 'log.c', 'log_data.c', 'log2.c', 'log2_data.c']
-
   math_files = files_in_path(path='system/lib/libc/musl/src/math', filenames=math_files)
 
   other_files = files_in_path(
@@ -382,10 +372,14 @@ class Library:
         cmd.remove('-g')
       else:
         cmd += cflags
+      cmd = self.customize_build_cmd(cmd, src)
       commands.append(cmd + ['-c', src, '-o', o])
       objects.append(o)
     run_build_commands(commands)
     return objects
+
+  def customize_build_cmd(self, cmd, filename):  # noqa
+    return cmd
 
   def build(self, out_filename):
     """Builds the library and returns the path to the file."""
@@ -767,7 +761,11 @@ class libnoexit(Library):
   src_files = ['atexit_dummy.c']
 
 
-class libc(DebugLibrary, AsanInstrumentedLibrary, MuslInternalLibrary, MTLibrary):
+class libc(MuslInternalLibrary,
+           DebugLibrary,
+           OptimizedAggressivelyForSizeLibrary,
+           AsanInstrumentedLibrary,
+           MTLibrary):
   name = 'libc'
 
   # Without -fno-builtin, LLVM can optimize away or convert calls to library
@@ -783,6 +781,10 @@ class libc(DebugLibrary, AsanInstrumentedLibrary, MuslInternalLibrary, MTLibrary
              '-Wno-shift-op-parentheses',
              '-Wno-string-plus-int',
              '-Wno-pointer-sign']
+
+  def __init__(self, **kwargs):
+    self.non_lto_files = non_lto_libc_files()
+    super().__init__(**kwargs)
 
   def get_files(self):
     libc_files = []
@@ -877,11 +879,12 @@ class libc(DebugLibrary, AsanInstrumentedLibrary, MuslInternalLibrary, MTLibrary
           'pthread_self_stub.c'
         ])
 
-    # These are included in wasm_libc_rt instead
-    ignore += [os.path.basename(f) for f in get_libc_rt_files(superset=True)]
+    if self.is_optz:
+      ignore += ['pow.c', 'pow_data.c', 'log.c', 'log_data.c', 'log2.c', 'log2_data.c']
+    else:
+      ignore += ['pow_small.c', 'log_small.c', 'log2_small.c']
 
     ignore = set(ignore)
-    # TODO: consider using more math code from musl, doing so makes box2d faster
     for dirpath, dirnames, filenames in os.walk(musl_srcdir):
       # Don't recurse into ingored directories
       remove = [d for d in dirnames if d in ignore]
@@ -976,9 +979,26 @@ class libc(DebugLibrary, AsanInstrumentedLibrary, MuslInternalLibrary, MTLibrary
         path='system/lib/pthread',
         filenames=['emscripten_atomic.c', 'thread_profiler.c'])
 
+    libc_files += files_in_path(
+      path='system/lib/libc',
+      filenames=['emscripten_memcpy.c', 'emscripten_memset.c',
+                 'emscripten_scan_stack.c',
+                 'emscripten_memmove.c'])
+
     libc_files += glob_in_path('system/lib/libc/compat', '*.c')
 
     return libc_files
+
+  def customize_build_cmd(self, cmd, filename):
+    if filename in self.non_lto_files:
+      # These files act more like the part of compiler-rt in that
+      # references to them can be generated at compiler time.
+      # Treat them link compiler-rt in as much as never compile
+      # them as LTO and build them with -O2 rather then -Oz
+      cmd = [a for a in cmd if not a.startswith('-flto')]
+      cmd = [a for a in cmd if not a.startswith('-O')]
+      cmd += ['-O2']
+    return cmd
 
 
 class libprintf_long_double(libc):
@@ -1425,13 +1445,6 @@ class CompilerRTLibrary(Library):
   force_object_files = True
 
 
-class libc_rt(OptimizedAggressivelyForSizeLibrary, AsanInstrumentedLibrary, CompilerRTLibrary, MuslInternalLibrary, MTLibrary):
-  name = 'libc_rt'
-
-  def get_files(self):
-    return get_libc_rt_files(is_optz=self.is_optz)
-
-
 class libubsan_minimal_rt(CompilerRTLibrary, MTLibrary):
   name = 'libubsan_minimal_rt'
   never_force = True
@@ -1708,7 +1721,6 @@ def get_libs_to_link(args, forced, only_forced):
     return libs_to_link
 
   if only_forced:
-    add_library('libc_rt')
     add_library('libcompiler_rt')
     return libs_to_link
 
@@ -1728,6 +1740,8 @@ def get_libs_to_link(args, forced, only_forced):
   if settings.PRINTF_LONG_DOUBLE:
     add_library('libprintf_long_double')
 
+  if settings.STANDALONE_WASM:
+    add_library('libstandalonewasm')
   if settings.ALLOW_UNIMPLEMENTED_SYSCALLS:
     add_library('libstubs')
   if '-nolibc' not in args:
@@ -1743,9 +1757,6 @@ def get_libs_to_link(args, forced, only_forced):
     add_library('libc++abi')
     if settings.EXCEPTION_HANDLING:
       add_library('libunwind')
-  if settings.STANDALONE_WASM:
-    add_library('libstandalonewasm')
-  add_library('libc_rt')
 
   if settings.USE_ASAN:
     force_include.append('libasan_rt')
