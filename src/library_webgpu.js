db68e59a70b5dad74ea86f23f5402c59010a443a
Kai Ninomiya <kainino@chromium.org>
2021-12-16 15:40:26 -0800

    Fix errant return and argument list in wgpuQueueOnSubmittedWorkDone (#15515)
    - This return prevents the result from actually being handled.
    - The argument list didn't match the C header.

--- src/library_webgpu.js
+++ src/library_webgpu.js
@@ -1375,9 +1375,12 @@ var LibraryWebGPU = {
     '$runtimeKeepalivePush', '$runtimeKeepalivePop',
 #endif
   ],
-  wgpuQueueOnSubmittedWorkDone: function(queueId, callback, userdata) {
+  wgpuQueueOnSubmittedWorkDone: function(queueId, {{{ defineI64Param('signalValue') }}}, callback, userdata) {
     var queue = WebGPU.mgrQueue.get(queueId);
-    return queue["onSubmittedWorkDone"]();
+#if ASSERTIONS
+    assert(signalValue_low === 0 && signalValue_high === 0, 'signalValue not supported, must be 0');
+#endif
+
     {{{ runtimeKeepalivePush() }}}
     queue["onSubmittedWorkDone"]().then(function() {
       {{{ runtimeKeepalivePop() }}}
