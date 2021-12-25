4a5a31ece104d087555d98e25e778771ae1fd782
Kleis Auke Wolthuizen <github@kleisauke.nl>
2021-12-25 14:45:09 +0100

    Ensure system libraries maintains insertion order (#15844)
    Linking multiple system libraries could produce a different result,
    because sets in Python doesn't preserve insertion order.
    
    This change fixes this non-determinism behavior by removing the set
    in favor of a list, which is guaranteed to keep the elements in order
    in which they were inserted.
    
    See: #15706.

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -1650,6 +1650,7 @@ def calculate(input_files, forced):
 
   handle_reverse_deps(input_files)
 
+  force_include = []
   libs_to_link = []
   already_included = set()
   system_libs_map = Library.get_usable_variations()
@@ -1661,8 +1662,10 @@ def calculate(input_files, forced):
   # ones you want
   force = os.environ.get('EMCC_FORCE_STDLIBS')
   if force == '1':
-    force = ','.join(name for name, lib in system_libs_map.items() if not lib.never_force)
-  force_include = set((force.split(',') if force else []) + forced)
+    force_include = [name for name, lib in system_libs_map.items() if not lib.never_force]
+  elif force is not None:
+    force_include = force.split(',')
+  force_include += forced
   if force_include:
     logger.debug(f'forcing stdlibs: {force_include}')
 
@@ -1731,11 +1734,11 @@ def calculate(input_files, forced):
     add_library('libc_rt')
 
     if settings.USE_LSAN:
-      force_include.add('liblsan_rt')
+      force_include.append('liblsan_rt')
       add_library('liblsan_rt')
 
     if settings.USE_ASAN:
-      force_include.add('libasan_rt')
+      force_include.append('libasan_rt')
       add_library('libasan_rt')
       add_library('libasan_js')
 
