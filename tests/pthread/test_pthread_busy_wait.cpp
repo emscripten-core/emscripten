351f5f74553d737ed76df87111db787024c19467
Sam Clegg <sbc@chromium.org>
2022-01-06 11:50:10 -0800

    Process queued calls from other threads during sched_yeild (#15897)
    This is part of two fixes for #15896.  Ideally will follow up
    by also removing the proxying of atexit calls, but this alone
    should fix this issue as written.

--- tests/pthread/test_pthread_busy_wait.cpp
+++ tests/pthread/test_pthread_busy_wait.cpp
@@ -1,7 +1,6 @@
 #include <atomic>
 #include <thread>
 #include <cstdio>
-#include <emscripten/threading.h>
 
 int main() {
   std::atomic<bool> done(false);
@@ -12,9 +11,6 @@ int main() {
   });
 
   while (!done) {
-    // TODO(sbc): this call should not be needed.
-    // See: https://github.com/emscripten-core/emscripten/issues/15868
-    emscripten_main_thread_process_queued_calls();
     std::this_thread::yield();
   }
 
