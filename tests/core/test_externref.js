89883581ff9b4f613333a684f4f4696114f6ea9b
Sam Clegg <sbc@chromium.org>
2022-01-07 09:49:52 -0800

    Add example of using externref in assembly code (#15913)
    See #15878

---
+++ tests/core/test_externref.js
@@ -0,0 +1,10 @@
+mergeInto(LibraryManager.library, {
+  get_externref_js: function () {
+    var test_object = { foo: 1 };
+    return test_object;
+  },
+
+  log_externref_js: function (ref) {
+    console.log('log_externref_js: ' +  JSON.stringify(ref));
+  },
+});
