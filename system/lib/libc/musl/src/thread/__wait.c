ba84d4033b38a60f6d9cefb3b705b410238f57a5
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-01-20 22:01:44 +0100

    Simplify wait in slices logic. NFC. (#15739)

--- system/lib/libc/musl/src/thread/__wait.c
+++ system/lib/libc/musl/src/thread/__wait.c
@@ -16,9 +16,12 @@ void __wait(volatile int *addr, volatile int *waiters, int val, int priv)
 	if (waiters) a_inc(waiters);
 #ifdef __EMSCRIPTEN__
 	int is_runtime_thread = emscripten_is_main_runtime_thread();
+
+	// Main runtime thread may need to run proxied calls, so sleep in very small slices to be responsive.
+	double max_ms_slice_to_sleep = is_runtime_thread ? 1 : 100;
+
 	while (*addr==val) {
 		if (is_runtime_thread || pthread_self()->cancelasync == PTHREAD_CANCEL_ASYNCHRONOUS) {
-			// Must wait in slices in case this thread is cancelled in between.
 			int e;
 			do {
 				if (pthread_self()->cancel) {
@@ -27,10 +30,9 @@ void __wait(volatile int *addr, volatile int *waiters, int val, int priv)
 				}
 				// Assist other threads by executing proxied operations that are effectively singlethreaded.
 				if (is_runtime_thread) emscripten_main_thread_process_queued_calls();
-				// Main thread waits in _very_ small slices so that it stays responsive to assist proxied
-				// pthread calls.
-				e = emscripten_futex_wait((void*)addr, val, is_runtime_thread ? 1 : 100);
-			} while(e == -ETIMEDOUT);
+				// Must wait in slices in case this thread is cancelled in between.
+				e = emscripten_futex_wait((void*)addr, val, max_ms_slice_to_sleep);
+			} while (e == -ETIMEDOUT);
 		} else {
 			// Can wait in one go.
 			emscripten_futex_wait((void*)addr, val, INFINITY);
