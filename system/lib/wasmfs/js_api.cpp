0d9ccd7fc7d53710330a447319082983b1fd6ae3
Alon Zakai <azakai@google.com>
2022-01-20 12:10:07 -0800

    trim comment

--- system/lib/wasmfs/js_api.cpp
+++ system/lib/wasmfs/js_api.cpp
@@ -2,9 +2,6 @@
 // Emscripten is available under two separate licenses, the MIT license and the
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
-// syscalls.cpp will implement the syscalls of the new file system replacing the
-// old JS version. Current Status: Work in Progress. See
-// https://github.com/emscripten-core/emscripten/issues/15041.
 
 #include <syscall_arch.h>
 #include <unistd.h>
