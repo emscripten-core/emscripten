509457b1714a705809fe9819599dcb9a1265cf83
Alon Zakai <azakai@google.com>
2022-01-24 17:00:31 -0800

    [WasmFS] Support file embedding

--- src/library_wasmfs.js
+++ src/library_wasmfs.js
@@ -50,7 +50,8 @@ var WasmfsLibrary = {
     createDataFile: function(parent, name, data, canRead, canWrite, canOwn) {
       // Data files must be cached until the file system itself has been initialized.
       var mode = FS.getMode(canRead, canWrite);
-      wasmFS$preloadedFiles.push({pathName: parent, fileData: data, mode: mode});
+      var pathName = parent ? parent + '/' + name : name;
+      wasmFS$preloadedFiles.push({pathName: pathName, fileData: data, mode: mode});
     },
     createPath: function(parent, path, canRead, canWrite) {
       // Cache file path directory names.
