5dc1455a4d64386871b53649bbe740aaa3aaceb6
Alon Zakai <azakai@google.com>
2022-02-09 08:18:10 -0800

    work [ci skip]

--- src/library_wasmfs_fetch.js
+++ src/library_wasmfs_fetch.js
@@ -1,6 +1,7 @@
 mergeInto(LibraryManager.library, {
-  // Fetch backend: On first read, does a fetch() to get the data. It then
-  // behaves like JSFile
+  // Fetch backend: On first access of the file (either a read or a getSize), it
+  // will fetch() the data from the network asynchronously. Otherwise, after
+  // that fetch it behaves just like JSFile (and it reuses the code from there).
 
   _wasmfs_create_fetch_backend_js__deps: [
     '$wasmFS$backends',
@@ -8,9 +9,8 @@ mergeInto(LibraryManager.library, {
     '_wasmfs_create_js_file_backend_js',
   ],
   _wasmfs_create_fetch_backend_js: function(backend) {
-    // Start with the normal JSFile operations.
-    __wasmfs_create_js_file_backend_js(backend);
-
+    // Get a promise that fetches the data and stores it in JS memory (if it has
+    // not already been fetched).
     function getFile(file) {
       if (wasmFS$JSMemoryFiles[file]) {
         // The data is already here, so nothing to do before we continue on to
@@ -19,7 +19,7 @@ mergeInto(LibraryManager.library, {
       }
 
       // This is the first time we want the file's data.
-      // TODO: URL!
+      // TODO: real URL!
       var url = 'data.dat';
       return fetch(url).then((response) => {
         if (!response['ok']) {
@@ -31,9 +31,16 @@ mergeInto(LibraryManager.library, {
       });
     }
 
+    // Start with the normal JSFile operations. This sets
+    //   wasmFS$backends[backend]
+    // which we will then augment.
+    __wasmfs_create_js_file_backend_js(backend);
+
     // Add the async operations on top.
     var jsFileOps = wasmFS$backends[backend];
     wasmFS$backends[backend] = {
+      // alloc/free operations are not actually async. Just forward to the
+      // parent class, but we must return a Promise as the caller expects.
       allocFile: function(file) {
         jsFileOps.allocFile(file);
         return Promise.resolve();
@@ -42,9 +49,12 @@ mergeInto(LibraryManager.library, {
         jsFileOps.freeFile(file);
         return Promise.resolve();
       },
+
       write: function(file, buffer, length, offset) {
-        abort();
+        abort("TODO: file writing in fetch backend? read-only for now");
       },
+
+      // read/getSize fetch the data, then forward to the parent class.
       read: function(file, buffer, length, offset) {
         return getFile(file).then(() => {
           return jsFileOps.read(file, buffer, length, offset);
@@ -52,9 +62,7 @@ mergeInto(LibraryManager.library, {
       },
       getSize: function(file) {
         return getFile(file).then(() => {
-          var ret = jsFileOps.getSize(file);
-          console.log(['sizeey', ret] + '\n');
-          return ret;
+          return jsFileOps.getSize(file);
         });
       },
     };
