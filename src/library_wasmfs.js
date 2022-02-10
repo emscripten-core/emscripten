cc77ca9aee001d12387e71a1dddd513f9e21b87c
Alon Zakai <azakai@google.com>
2022-02-10 09:14:39 -0800

    comment

--- src/library_wasmfs.js
+++ src/library_wasmfs.js
@@ -197,6 +197,13 @@ var WasmFSLibrary = {
   // implementors of backends: the hooks we call should return Promises, which
   // we then connect to the calling C++.
 
+  // TODO: arg is void*, which for MEMORY64 will be 64-bit. we need a way to
+  //       declare arg in the function signature here (like defineI64Param,
+  //       but that varies for wasm32/wasm64), and a way to do makeDynCall that
+  //       adds a 'p' signature type for pointer, or something like that
+  //       (however, dyncalls might also just work, given in MEMORY64 we assume
+  //       WASM_BIGINT so the pointer is just a single argument, just like in
+  //       wasm32).
   _wasmfs_jsimpl_async_alloc_file: async function(backend, file, fptr, arg) {
 #if ASSERTIONS
     assert(wasmFS$backends[backend]);
