8dad972fb0107712df2c6b5fe2dcf674e4133d05
Hood Chatham <roberthoodchatham@gmail.com>
2022-02-21 22:25:51 -0500

    Add formatException to EXPORTED_FUNCTIONS rather than EXPORTED_RUNTIME_METHODS

--- emcc.py
+++ emcc.py
@@ -2079,8 +2079,7 @@ def phase_linker_setup(options, state, newargs, settings_map):
       settings.INCLUDE_FULL_LIBRARY and not settings.DISABLE_EXCEPTION_CATCHING
   ):
     settings.DEFAULT_LIBRARY_FUNCS_TO_INCLUDE += ['$formatException']
-    settings.EXPORTED_FUNCTIONS += ['_emscripten_format_exception', '_free']
-    settings.EXPORTED_RUNTIME_METHODS += ['formatException']
+    settings.EXPORTED_FUNCTIONS += ['formatException', '_emscripten_format_exception', '_free']
 
   if settings.FORCE_FILESYSTEM and not settings.MINIMAL_RUNTIME:
     # when the filesystem is forced, we export by default methods that filesystem usage
