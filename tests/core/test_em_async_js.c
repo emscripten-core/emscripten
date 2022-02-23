5bee23e380fc520c260a4f8fc036412ddbef6d75
Alon Zakai <azakai@google.com>
2022-02-23 13:09:02 -0800

    comments

--- tests/core/test_em_async_js.c
+++ tests/core/test_em_async_js.c
@@ -4,7 +4,11 @@
 double result = 0.0;
 
 EMSCRIPTEN_KEEPALIVE
-void set_result() {
+void inc_result() {
+  puts("inc result");
+  // this will be called twice, leading to 4.2 as the final result.
+  // (we use floating-point numbers here to make sure that the final result is
+  // returned using the proper type, and not an int)
   result += 2.1;
 }
 
@@ -18,10 +22,11 @@ EM_ASYNC_JS(double, foo, (int timeout), {
     () => {
       // Do a simple call back into compiled code synchronously. This is
       // allowed (an async one would not).
-      _set_result();
+      _inc_result();
       // Do the same with ccall.
-      ccall("set_result");
+      ccall("inc_result");
       // Finish the async operation.
+      out("resolving promise");
       resolve();
     },
     timeout
