emscripten-core/emscripten

commit 9ffd36df7165a7bc92c5b77349633104e894f738

Asyncify Embind exports (#14664)
* Asyncify Embind exports

Previously, if Embind function called an async function via Asyncify
somewhere in its call stack, it would return garbage default value
(`0` / `false` / class instance with a `NULL` pointer / etc.).

This PR changes such exports to instead return Promises when they
invoke an async function, and direct values otherwise. This preserves
backward compatibility for synchronous exports, while providing a more
useful value for async use-cases.

If Asyncify imports are invoked dynamically and user isn't sure which
exports will end up being async or not, they can simply use `await`
on all Embind exports and it will do the right thing as it works
correctly with both Promises and synchronous values alike.

Fixes #11890.

src/preamble.js
@@ -129,35 +129,28 @@ function ccall(ident, returnType, argTypes, args, opts) {
     }
   }
   var ret = func.apply(null, cArgs);
+  function onDone(ret) {
+    if (stack !== 0) stackRestore(stack);
+    return convertReturnValue(ret);
+  }
 #if ASYNCIFY
   var asyncMode = opts && opts.async;
-  var runningAsync = typeof Asyncify === 'object' && Asyncify.currData;
-  var prevRunningAsync = typeof Asyncify === 'object' && Asyncify.asyncFinalizers.length > 0;
-#if ASSERTIONS
-  assert(!asyncMode || !prevRunningAsync, 'Cannot have multiple async ccalls in flight at once');
-#endif
   // Check if we started an async operation just now.
-  if (runningAsync && !prevRunningAsync) {
+  if (Asyncify.currData) {
     // If so, the WASM function ran asynchronous and unwound its stack.
     // We need to return a Promise that resolves the return value
     // once the stack is rewound and execution finishes.
 #if ASSERTIONS
     assert(asyncMode, 'The call to ' + ident + ' is running asynchronously. If this was intended, add the async option to the ccall/cwrap call.');
 #endif
-    return new Promise(function(resolve) {
-      Asyncify.asyncFinalizers.push(function(ret) {
-        if (stack !== 0) stackRestore(stack);
-        resolve(convertReturnValue(ret));
-      });
-    });
+    return Asyncify.whenDone().then(onDone);
   }
 #endif
 
-  ret = convertReturnValue(ret);
-  if (stack !== 0) stackRestore(stack);
+  ret = onDone(ret);
 #if ASYNCIFY
   // If this is an async ccall, ensure we return a promise
-  if (opts && opts.async) return Promise.resolve(ret);
+  if (asyncMode) return Promise.resolve(ret);
 #endif
   return ret;
 }

src/embind/embind.js
@@ -938,7 +938,11 @@ var LibraryEmbind = {
   // (hand-written JS code) -> (autogenerated JS invoker) -> (template-generated C++ invoker) -> (target C++ function)
   // craftInvokerFunction generates the JS invoker function for each function exposed to JS through embind.
   $craftInvokerFunction__deps: [
-    '$makeLegalFunctionName', '$new_', '$runDestructors', '$throwBindingError'],
+    '$makeLegalFunctionName', '$new_', '$runDestructors', '$throwBindingError',
+  #if ASYNCIFY
+    '$Asyncify',
+  #endif
+  ],
   $craftInvokerFunction: function(humanName, argTypes, classType, cppInvokerFunc, cppTargetFunc) {
     // humanName: a human-readable string name for the function to be generated.
     // argTypes: An array that contains the embind type objects for all types in the function signature.
@@ -1005,24 +1009,34 @@ var LibraryEmbind = {
 
       var rv = cppInvokerFunc.apply(null, invokerFuncArgs);
 
-      if (needsDestructorStack) {
-        runDestructors(destructors);
-      } else {
-        for (var i = isClassMethodFunc ? 1 : 2; i < argTypes.length; i++) {
-          var param = i === 1 ? thisWired : argsWired[i - 2];
-          if (argTypes[i].destructorFunction !== null) {
-            argTypes[i].destructorFunction(param);
+      function onDone(rv) {
+        if (needsDestructorStack) {
+          runDestructors(destructors);
+        } else {
+          for (var i = isClassMethodFunc ? 1 : 2; i < argTypes.length; i++) {
+            var param = i === 1 ? thisWired : argsWired[i - 2];
+            if (argTypes[i].destructorFunction !== null) {
+              argTypes[i].destructorFunction(param);
+            }
           }
+        }
+
+  #if EMSCRIPTEN_TRACING
+        Module.emscripten_trace_exit_context();
+  #endif
+
+        if (returns) {
+          return argTypes[0].fromWireType(rv);
         }
       }
 
-#if EMSCRIPTEN_TRACING
-      Module.emscripten_trace_exit_context();
+#if ASYNCIFY
+      if (Asyncify.currData) {
+        return Asyncify.whenDone().then(onDone);
+      }
 #endif
 
-      if (returns) {
-        return argTypes[0].fromWireType(rv);
-      }
+      return onDone(rv);
     };
 #else
     var argsList = "";
@@ -1073,6 +1087,12 @@ var LibraryEmbind = {
     invokerFnBody +=
         (returns?"var rv = ":"") + "invoker(fn"+(argsListWired.length>0?", ":"")+argsListWired+");\n";
 
+#if ASYNCIFY
+    args1.push("Asyncify");
+    args2.push(Asyncify);
+    invokerFnBody += "function onDone(rv) {\n";
+#endif
+
     if (needsDestructorStack) {
         invokerFnBody += "runDestructors(destructors);\n";
     } else {
@@ -1097,6 +1117,12 @@ var LibraryEmbind = {
         invokerFnBody += "Module.emscripten_trace_exit_context();\n";
 #endif
     }
+
+#if ASYNCIFY
+    invokerFnBody += "}\n";
+    invokerFnBody += "return Asyncify.currData ? Asyncify.whenDone().then(onDone) : onDone(rv);\n"
+#endif
+
     invokerFnBody += "}\n";
 
     args1.push(invokerFnBody);
@@ -2028,7 +2054,7 @@ var LibraryEmbind = {
 
   _embind_register_class_constructor__deps: [
     '$heap32VectorToArray', '$embind__requireFunction', '$runDestructors',
-    '$throwBindingError', '$whenDependentTypesAreResolved'],
+    '$throwBindingError', '$whenDependentTypesAreResolved', '$registeredTypes'],
   _embind_register_class_constructor: function(
     rawClassType,
     argCount,
@@ -2058,21 +2084,9 @@ var LibraryEmbind = {
         };
 
         whenDependentTypesAreResolved([], rawArgTypes, function(argTypes) {
-            classType.registeredClass.constructor_body[argCount - 1] = function constructor_body() {
-                if (arguments.length !== argCount - 1) {
-                    throwBindingError(humanName + ' called with ' + arguments.length + ' arguments, expected ' + (argCount-1));
-                }
-                destructors.length = 0;
-                args.length = argCount;
-                for (var i = 1; i < argCount; ++i) {
-                    args[i] = argTypes[i]['toWireType'](destructors, arguments[i - 1]);
-                }
-
-                var ptr = invoker.apply(null, args);
-                runDestructors(destructors);
-
-                return argTypes[0]['fromWireType'](ptr);
-            };
+            // Insert empty slot for context type (argTypes[1]).
+            argTypes.splice(1, 0, null);
+            classType.registeredClass.constructor_body[argCount - 1] = craftInvokerFunction(humanName, argTypes, null, invoker, rawConstructor);
             return [];
         });
         return [];

src/library_async.js
@@ -148,6 +148,16 @@ mergeInto(LibraryManager.library, {
       }
     },
 
+    whenDone: function() {
+#if ASSERTIONS
+      assert(Asyncify.currData, 'Tried to wait for an async operation when none is in progress.');
+      assert(!Asyncify.asyncFinalizers.length, 'Cannot have multiple async operations in flight at once');
+#endif
+      return new Promise(function(resolve) {
+        Asyncify.asyncFinalizers.push(resolve);
+      });
+    },
+
     allocateData: function() {
       // An asyncify data structure has three fields:
       //  0  current stack pos

tests/core/embind_lib_with_asyncify.cpp
@@ -1,0 +1,52 @@
+#include <assert.h>
+
+#include <string>
+
+#include <emscripten.h>
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
+EM_JS(int, sleep_and_return, (int x), {
+	return Asyncify.handleSleep(wakeUp => setTimeout(wakeUp, 10, x));
+});
+
+int foo() {
+	return sleep_and_return(10);
+}
+
+class Bar {
+public:
+	Bar(): x(sleep_and_return(20)) {}
+
+	int getX() const {
+		return x;
+	}
+
+	int method() {
+		return sleep_and_return(30);
+	}
+
+	int method_without_async_calls() {
+		return 40;
+	}
+
+	static int static_method() {
+		return sleep_and_return(50);
+	}
+
+private:
+	int x;
+};
+
+EMSCRIPTEN_BINDINGS(embind_async) {
+	function("foo", &foo);
+
+	class_<Bar>("Bar")
+		.constructor<>()
+		.function("method", &Bar::method)
+		.function("method_without_async_calls", &Bar::method_without_async_calls)
+		.class_function("static_method", &Bar::static_method)
+		.property("x", &Bar::getX)
+		;
+}

tests/core/embind_lib_with_asyncify.out
@@ -1,0 +1,1 @@
+ok

tests/core/embind_lib_with_asyncify.test.js
@@ -1,0 +1,39 @@
+Module.onRuntimeInitialized = async () => {
+  try {
+    let fooResult = Module.foo();
+    assert(fooResult instanceof Promise);
+    fooResult = await fooResult;
+    assert(fooResult === 10);
+
+    let barInstancePromise = new Module.Bar();
+    assert(barInstancePromise instanceof Promise);
+    let barInstance = await barInstancePromise;
+    assert(barInstance instanceof Module.Bar);
+    assert(barInstance.x === 20);
+
+    let barMethodResult = barInstance.method();
+    assert(barMethodResult instanceof Promise);
+    assert(await barMethodResult === 30);
+
+    assert(barInstance.method_without_async_calls() === 40);
+
+    let barStaticMethodResult = Module.Bar.static_method();
+    assert(barStaticMethodResult instanceof Promise);
+    assert(await barStaticMethodResult === 50);
+
+    if (ASSERTIONS) {
+      let err = '';
+      try {
+        barInstance.method();
+        barInstance.method();
+      } catch (e) {
+        err = e.message;
+      }
+      assert(err.startsWith('abort(Assertion failed: Cannot have multiple async operations in flight at once)'));
+    }
+
+    console.log('ok');
+  } catch (e) {
+    console.log('error: ' + e.stack);
+  }
+};

tests/test_core.py
@@ -8538,6 +8538,22 @@ NODEFS is no longer included by default; build with -lnodefs.js
     self.set_setting('EXIT_RUNTIME')
     self.do_run_in_out_file_test(test_file('core/test_emscripten_async_call.c'))
 
+  @no_asan('asyncify stack operations confuse asan')
+  @parameterized({
+    '': ([],),
+    'no_dynamic_execution': (['-s', 'DYNAMIC_EXECUTION=0'],)
+  })
+  def test_embind_lib_with_asyncify(self, args):
+    self.uses_es6 = True
+    self.emcc_args += [
+      '--bind',
+      '-s', 'ASYNCIFY',
+      '-s', 'ASYNCIFY_IMPORTS=["sleep_and_return"]',
+      '--post-js', test_file('core/embind_lib_with_asyncify.test.js'),
+    ]
+    self.emcc_args += args
+    self.do_core_test('embind_lib_with_asyncify.cpp')
+
 
 # Generate tests for everything
 def make_run(name, emcc_args, settings=None, env=None):

commit 4a0a341a656485e3236ed28ac6ede5b8bab03ffa

Fix async ccall regression from #14664 (#16370)
See https://github.com/emscripten-core/emscripten/pull/14664/files#r810609810

Basically, we allow sync operations during async ones - the classic example is an
async fetch that does a sync malloc when the data arrives. That PR changed the
assertion to fire too eagerly.

Helps with #16291

src/preamble.js
@@ -137,6 +137,10 @@ function ccall(ident, returnType, argTypes, args, opts) {
       }
     }
   }
+#if ASYNCIFY
+  // Data for a previous async operation that was in flight before us.
+  var previousAsync = Asyncify.currData;
+#endif
   var ret = func.apply(null, cArgs);
   function onDone(ret) {
 #if ASYNCIFY
@@ -146,11 +150,22 @@ function ccall(ident, returnType, argTypes, args, opts) {
     return convertReturnValue(ret);
   }
 #if ASYNCIFY
+  // Keep the runtime alive through all calls. Note that this call might not be
+  // async, but for simplicity we push and pop in all calls.
   runtimeKeepalivePush();
   var asyncMode = opts && opts.async;
-  // Check if we started an async operation just now.
-  if (Asyncify.currData) {
-    // If so, the WASM function ran asynchronous and unwound its stack.
+  if (Asyncify.currData != previousAsync) {
+#if ASSERTIONS
+    // A change in async operation happened. If there was already an async
+    // operation in flight before us, that is an error: we should not start
+    // another async operation while one is active, and we should not stop one
+    // either. The only valid combination is to have no change in the async
+    // data (so we either had one in flight and left it alone, or we didn't have
+    // one), or to have nothing in flight and to start one.
+    assert(!(previousAsync && Asyncify.currData), 'We cannot start an async operation when one is already flight');
+    assert(!(previousAsync && !Asyncify.currData), 'We cannot stop an async operation in flight');
+#endif
+    // This is a new async operation. The wasm is paused and has unwound its stack.
     // We need to return a Promise that resolves the return value
     // once the stack is rewound and execution finishes.
 #if ASSERTIONS

tests/core/test_em_async_js.c
@@ -1,12 +1,41 @@
 #include <emscripten.h>
 #include <stdio.h>
 
+double result = 0.0;
+
+EMSCRIPTEN_KEEPALIVE
+void inc_result() {
+  puts("inc result");
+  // this will be called twice, leading to 4.2 as the final result.
+  // (we use floating-point numbers here to make sure that the final result is
+  // returned using the proper type, and not an int)
+  result += 2.1;
+}
+
+EMSCRIPTEN_KEEPALIVE
+double get_result() {
+  return result;
+}
+
 EM_ASYNC_JS(double, foo, (int timeout), {
-	await new Promise(resolve => setTimeout(resolve, timeout));
-	return 4.2;
+  await new Promise(resolve => setTimeout(
+    () => {
+      // Do a simple call back into compiled code synchronously. This is
+      // allowed (an async one would not).
+      _inc_result();
+      // Do the same with ccall.
+      ccall("inc_result");
+      // Finish the async operation.
+      out("resolving promise");
+      resolve();
+    },
+    timeout
+  ));
+  // Do another simple call back into compiled code synchronously.
+  return _get_result();
 });
 
 int main() {
-	printf("foo returned: %f\n", foo(10));
-	return 0;
+  printf("foo returned: %f\n", foo(10));
+  return 0;
 }