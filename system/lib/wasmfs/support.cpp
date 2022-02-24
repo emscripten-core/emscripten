12e57e66a2368ab62a79c1319f50faacd9e71ab2
Alon Zakai <azakai@google.com>
2022-02-24 11:00:15 -0800

    use emscripten_console*

--- system/lib/wasmfs/support.cpp
+++ system/lib/wasmfs/support.cpp
@@ -3,7 +3,12 @@
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
 
+#ifdef __EMSCRIPTEN__
+// In emscripten, avoid iostream to avoid using WasmFS code during logging.
+#include <emscripten/console.h>
+#else
 #include <iostream>
+#endif
 
 #include "support.h"
 
@@ -11,6 +16,15 @@ namespace wasmfs {
 
 void handle_unreachable(const char* msg, const char* file, unsigned line) {
 #ifndef NDEBUG
+#ifdef __EMSCRIPTEN__
+  if (msg) {
+    emscripten_console_error(msg);
+  }
+  emscripten_console_error("UNREACHABLE executed");
+  if (file) {
+    emscripten_console_errorf("at %s:%d", file, line);
+  }
+#else // EMSCRIPTEN
   if (msg) {
     std::cerr << msg << "\n";
   }
@@ -19,6 +33,7 @@ void handle_unreachable(const char* msg, const char* file, unsigned line) {
     std::cerr << " at " << file << ":" << line;
   }
   std::cerr << "!\n";
+#endif
   // TODO: sanitizer integration, see binaryen's similar code
 #endif
   __builtin_trap();
