d5921a9cc91afa358a9e7e34ff5a34660e1db8a9
Christian Heimes <christian@python.org>
2022-03-09 22:38:22 +0100

    Apply factor 1000 inside noderawfs layer

--- src/library_syscall.js
+++ src/library_syscall.js
@@ -1047,16 +1047,16 @@ var SyscallsLibrary = {
 #endif
     path = SYSCALLS.calculateAt(dirfd, path, true);
     if (!times) {
-      var atime = Date.now()/1000;
+      var atime = Date.now();
       var mtime = atime;
     } else {
       var seconds = {{{ makeGetValue('times', C_STRUCTS.timespec.tv_sec, 'i32') }}};
       var nanoseconds = {{{ makeGetValue('times', C_STRUCTS.timespec.tv_nsec, 'i32') }}};
-      atime = seconds + (nanoseconds/(1000*1000*1000));
+      atime = (seconds*1000) + (nanoseconds/(1000*1000));
       times += {{{ C_STRUCTS.timespec.__size__ }}};
       seconds = {{{ makeGetValue('times', C_STRUCTS.timespec.tv_sec, 'i32') }}};
       nanoseconds = {{{ makeGetValue('times', C_STRUCTS.timespec.tv_nsec, 'i32') }}};
-      mtime = seconds + (nanoseconds/(1000*1000*1000));
+      mtime = (seconds*1000) + (nanoseconds/(1000*1000));
     }
     FS.utime(path, atime, mtime);
     return 0;
