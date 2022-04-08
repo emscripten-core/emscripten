099d1d3d49e5f6ed9116745432b417927710ddd3
Alon Zakai <azakai@google.com>
2022-04-08 09:07:10 -0700

    add more weakness

--- system/lib/standalone/standalone.c
+++ system/lib/standalone/standalone.c
@@ -90,10 +90,12 @@ long __syscall_openat(int dirfd, const char* path, long flags, ...) {
   return -EPERM;
 }
 
+__attribute__((__weak__))
 int __syscall_ioctl(int fd, int op, ...) {
   return -ENOSYS;
 }
 
+__attribute__((__weak__))
 long __syscall_fcntl64(long fd, long cmd, ...) {
   return -ENOSYS;
 }
