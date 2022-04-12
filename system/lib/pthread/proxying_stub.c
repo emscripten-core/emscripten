dd75b422f8417250f6034ce028feba795da84e8b
Thomas Lively <tlively@google.com>
2022-04-12 13:49:58 -0700

    Abort in proxying functions

--- system/lib/pthread/proxying_stub.c
+++ system/lib/pthread/proxying_stub.c
@@ -8,6 +8,8 @@
 // Stub implementation of the proxying queue API that does nothing, for use in
 // single-threaded builds.
 
+#include <stdlib.h>
+
 #include "proxying.h"
 
 em_proxying_queue* em_proxying_queue_create() {
@@ -20,27 +22,27 @@ em_proxying_queue* emscripten_proxy_get_system_queue() {
   return NULL;
 }
 
-void emscripten_proxy_execute_queue(em_proxying_queue* q) {}
+void emscripten_proxy_execute_queue(em_proxying_queue* q) { abort(); }
 
-void emscripten_proxy_finish(em_proxying_ctx* ctx) {}
+void emscripten_proxy_finish(em_proxying_ctx* ctx) { abort(); }
 
 int emscripten_proxy_async(em_proxying_queue* q,
                            pthread_t target_thread,
                            void (*func)(void*),
                            void* arg) {
-  return 0;
+  abort();
 }
 
 int emscripten_proxy_sync(em_proxying_queue* q,
                           pthread_t target_thread,
                           void (*func)(void*),
                           void* arg) {
-  return 0;
+  abort();
 }
 
 int emscripten_proxy_sync_with_ctx(em_proxying_queue* q,
                                    pthread_t target_thread,
                                    void (*func)(em_proxying_ctx*, void*),
                                    void* arg) {
-  return 0;
+  abort();
 }
