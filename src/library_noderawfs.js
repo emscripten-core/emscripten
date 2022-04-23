emscripten-core/emscripten

commit a856e88ab85560b538d1b8d4f7e74ea0d1991c94

Fix dup syscalls so that they handle pipes correctly (#14808)
This fixes two problems with the `dup` system calls:
1. `dup` expects that every file descriptor has a corresponding file (so pipes and (https://github.com/emscripten-core/emscripten/issues/14640)
   files that have been unlinked (https://github.com/emscripten-core/emscripten/issues/15012) cannot be duplicated ).
2. The dup'd file descriptor does not share flags and position with the original file desciptor.

This is a simplification of an upstream pull request that would fix this problem.
https://github.com/emscripten-core/emscripten/pull/9396/files
This patch is simpler than the upstream one but leaves NODERAWFS broken.

src/library_noderawfs.js
@@ -45,9 +45,9 @@ mergeInto(LibraryManager.library, {
       return { path: path, node: { id: st.ino, mode: mode, node_ops: NODERAWFS, path: path }};
     },
     createStandardStreams: function() {
-      FS.streams[0] = { fd: 0, nfd: 0, position: 0, path: '', flags: 0, tty: true, seekable: false };
+      FS.streams[0] = FS.createStream({ fd: 0, nfd: 0, position: 0, path: '', flags: 0, tty: true, seekable: false });
       for (var i = 1; i < 3; i++) {
-        FS.streams[i] = { fd: i, nfd: i, position: 0, path: '', flags: 577, tty: true, seekable: false };
+        FS.streams[i] = FS.createStream({ fd: i, nfd: i, position: 0, path: '', flags: 577, tty: true, seekable: false });
       }
     },
     // generic function for all node creation
@@ -96,16 +96,32 @@ mergeInto(LibraryManager.library, {
       var newMode = NODEFS.getMode(pathTruncated);
       var fd = suggestFD != null ? suggestFD : FS.nextfd(nfd);
       var node = { id: st.ino, mode: newMode, node_ops: NODERAWFS, path: path }
-      var stream = { fd: fd, nfd: nfd, position: 0, path: path, flags: flags, node: node, seekable: true };
+      var stream = FS.createStream({ fd: fd, nfd: nfd, position: 0, path: path, flags: flags, node: node, seekable: true });
       FS.streams[fd] = stream;
       return stream;
     },
+    createStream: function(stream, fd_start, fd_end){
+      // Call the original FS.createStream
+      var rtn = VFS.createStream(stream, fd_start, fd_end);
+      if (typeof rtn.shared.refcnt == "undefined") {
+        rtn.shared.refcnf = 0;
+      } else {
+        rtn.shared.refcnf++;
+      }
+      return rtn;
+   },
+   closeStream: function(fd) {
+    if (FS.streams[fd]) {
+      FS.streams[fd].shared.refcnt--;
+    }
+     VFS.closeStream(fd);
+   },
     close: function(stream) {
-      if (!stream.stream_ops) {
-        // this stream is created by in-memory filesystem
+      FS.closeStream(stream.fd);
+      if (!stream.stream_ops && stream.shared.refcnt === 0) {
+        // this stream is created by in-memory filesystem        
         fs.closeSync(stream.nfd);
       }
-      FS.closeStream(stream.fd);
     },
     llseek: function(stream, offset, whence) {
       if (stream.stream_ops) {

src/library_fs.js
@@ -400,7 +400,9 @@ FS.staticInit();` +
     // SOCKFS is completed.
     createStream: (stream, fd_start, fd_end) => {
       if (!FS.FSStream) {
-        FS.FSStream = /** @constructor */ function(){};
+        FS.FSStream = /** @constructor */ function() {
+          this.shared = { };
+        };
         FS.FSStream.prototype = {
           object: {
             get: function() { return this.node; },
@@ -414,7 +416,15 @@ FS.staticInit();` +
           },
           isAppend: {
             get: function() { return (this.flags & {{{ cDefine('O_APPEND') }}}); }
-          }
+          },
+          flags: {
+            get: function() { return this.shared.flags; },
+            set: function(val) { this.shared.flags = val; },
+          },
+          position : {
+            get function() { return this.shared.position; },
+            set: function(val) { this.shared.position = val; },
+          },
         };
       }
       // clone it, so we can return an instance of FSStream

src/library_syscall.js
@@ -279,7 +279,7 @@ var SyscallsLibrary = {
   },
   __syscall_dup: function(fd) {
     var old = SYSCALLS.getStreamFromFD(fd);
-    return FS.open(old.path, old.flags, 0).fd;
+    return FS.createStream(old, 0).fd;
   },
   __syscall_pipe__deps: ['$PIPEFS'],
   __syscall_pipe: function(fdPtr) {
@@ -798,7 +798,7 @@ var SyscallsLibrary = {
           return -{{{ cDefine('EINVAL') }}};
         }
         var newStream;
-        newStream = FS.open(stream.path, stream.flags, 0, arg);
+        newStream = FS.createStream(stream, arg);
         return newStream.fd;
       }
       case {{{ cDefine('F_GETFD') }}}:
@@ -1017,7 +1017,7 @@ var SyscallsLibrary = {
     if (old.fd === suggestFD) return -{{{ cDefine('EINVAL') }}};
     var suggest = FS.getStream(suggestFD);
     if (suggest) FS.close(suggest);
-    return FS.open(old.path, old.flags, 0, suggestFD, suggestFD).fd;
+    return FS.createStream(old, suggestFD, suggestFD + 1).fd;
   },
 };
 

tests/unistd/dup.c
@@ -9,6 +9,8 @@
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <emscripten.h>
+
 
 int main() {
   int f, f2, f3;
@@ -44,7 +46,18 @@ int main() {
   printf("f: %d\n", f == -1);
   printf("errno: %d\n", errno);
   printf("close(f): %d\n", close(f));
+  printf("\n");
   errno = 0;
 
+
+  printf("DUP2 pipe\n");
+  int p[2];
+  pipe(p);
+  int g = dup2(p[0], 7);
+  write(p[1], "abc\n", 3);
+  char buf[5];
+  read(g, buf, 5);
+  // should print "buf: abc\n"
+  printf("buf: %s\n", buf);
   return 0;
 }

tests/unistd/dup.out
@@ -18,3 +18,6 @@ DUP2 err
 f: 1
 errno: 8
 close(f): -1
+
+DUP2 pipe
+buf: abc

commit 6f6418e01d20de4469141921dbca23ec47f2903e

Fix fd leak in NODERAWFS introduced by #14808
`refcnt` (and not `refcnf`) needs to be initialized by 1 when
creating the filestream.

Also, apply #16661 to the other `FS.createStream` invocations
as well.

src/library_noderawfs.js
@@ -45,9 +45,9 @@ mergeInto(LibraryManager.library, {
       return { path: path, node: { id: st.ino, mode: mode, node_ops: NODERAWFS, path: path }};
     },
     createStandardStreams: function() {
-      FS.streams[0] = FS.createStream({ fd: 0, nfd: 0, position: 0, path: '', flags: 0, tty: true, seekable: false });
+      FS.streams[0] = FS.createStream({ nfd: 0, position: 0, path: '', flags: 0, tty: true, seekable: false }, 0, 0);
       for (var i = 1; i < 3; i++) {
-        FS.streams[i] = FS.createStream({ fd: i, nfd: i, position: 0, path: '', flags: 577, tty: true, seekable: false });
+        FS.streams[i] = FS.createStream({ nfd: i, position: 0, path: '', flags: 577, tty: true, seekable: false }, i, i);
       }
     },
     // generic function for all node creation
@@ -103,19 +103,19 @@ mergeInto(LibraryManager.library, {
     createStream: function(stream, fd_start, fd_end){
       // Call the original FS.createStream
       var rtn = VFS.createStream(stream, fd_start, fd_end);
-      if (typeof rtn.shared.refcnt == "undefined") {
-        rtn.shared.refcnf = 0;
+      if (typeof rtn.shared.refcnt == 'undefined') {
+        rtn.shared.refcnt = 1;
       } else {
-        rtn.shared.refcnf++;
+        rtn.shared.refcnt++;
       }
       return rtn;
-   },
-   closeStream: function(fd) {
-    if (FS.streams[fd]) {
-      FS.streams[fd].shared.refcnt--;
-    }
-     VFS.closeStream(fd);
-   },
+    },
+    closeStream: function(fd) {
+      if (FS.streams[fd]) {
+        FS.streams[fd].shared.refcnt--;
+      }
+      VFS.closeStream(fd);
+    },
     close: function(stream) {
       FS.closeStream(stream.fd);
       if (!stream.stream_ops && stream.shared.refcnt === 0) {