ef8e4019a7dfde2533228064392b2af89b2fa629
Sam Clegg <sbc@chromium.org>
2022-04-25 20:08:40 +0000

    Add comments when passing extra args to makeGet/SetValue. NFC (#16810)
    I'm trying unify/remove these arguments where possible to simplify the
    code and documenting the current usage seems like a good start.
    
    For some reason runtime_safe_heap.js was passing the string "1" for the
    noSafe argument rather than just `true`.  This argument is only ever
    check for truthiness.

--- src/library_formatString.js
+++ src/library_formatString.js
@@ -82,18 +82,18 @@ mergeInto(LibraryManager.library, {
       var ret;
       argIndex = prepVararg(argIndex, type);
       if (type === 'double') {
-        ret = {{{ makeGetValue('argIndex', 0, 'double', undefined, undefined, true) }}};
+        ret = {{{ makeGetValue('argIndex', 0, 'double', undefined, undefined, /*ignore=*/true) }}};
         argIndex += 8;
       } else if (type == 'i64') {
-        ret = [{{{ makeGetValue('argIndex', 0, 'i32', undefined, undefined, true, 4) }}},
-               {{{ makeGetValue('argIndex', 4, 'i32', undefined, undefined, true, 4) }}}];
+        ret = [{{{ makeGetValue('argIndex', 0, 'i32', undefined, undefined, /*ignore=*/true, /*align=*/4) }}},
+               {{{ makeGetValue('argIndex', 4, 'i32', undefined, undefined, /*ignore=*/true, /*align=*/4) }}}];
         argIndex += 8;
       } else {
 #if ASSERTIONS
         assert((argIndex & 3) === 0);
 #endif
         type = 'i32'; // varargs are always i32, i64, or double
-        ret = {{{ makeGetValue('argIndex', 0, 'i32', undefined, undefined, true) }}};
+        ret = {{{ makeGetValue('argIndex', 0, 'i32', undefined, undefined, /*ignore=*/true) }}};
         argIndex += 4;
       }
       return ret;
