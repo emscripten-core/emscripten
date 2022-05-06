5e5edd85daa17575243e5bab3ce4094f890978d5
Thomas Lively <tlively@google.com>
2022-05-06 09:26:49 -0700

    Update file header

--- system/lib/wasmfs/special_files.h
+++ system/lib/wasmfs/special_files.h
@@ -1,10 +1,7 @@
-// Copyright 2021 The Emscripten Authors.  All rights reserved.
+// Copyright 2022 The Emscripten Authors.  All rights reserved.
 // Emscripten is available under two separate licenses, the MIT license and the
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
-// This file defines the standard streams of the new file system.
-// Current Status: Work in Progress.
-// See https://github.com/emscripten-core/emscripten/issues/15041.
 
 #pragma once
 
