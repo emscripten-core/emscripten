75f53151ceade70312441c4e6f49cab527f8176c
juj <jujjyl@gmail.com>
2022-05-20 19:30:29 +0300

    Fix unicode characters in js library (#16981)
    * Update unicode JS library test to cover more unicode characters that are typically used in the localized names of different languages.
    
    * Fix expanded test_unicode_js_library to work.

--- tests/unicode_library.js
+++ tests/unicode_library.js
@@ -1,5 +1,5 @@
 mergeInto(LibraryManager.library, {
 printey: function() {
-  out('Unicode snowman ☃ says hello!');
+  out('Unicode snowman ☃ says hello! àČšñéáúÍųåêâăščếệüçλληνικάбългарскиPусскийСрпскиУкраїнська한국어中文普通话(中国大陆)普通话(香港)中文(台灣)粵語(香港)日本語हिन्दीภาษาไทย');
 }
 });
