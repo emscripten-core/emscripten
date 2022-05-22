211b63f00e254707de06a32edc76116bd71c88ba
Sam Clegg <sbc@chromium.org>
2022-05-21 18:18:03 -0700

    Remove dead code in runtime_debug.js. NFC
    I was running with PTHREADS_DEBUG + closure and closure
    figured out that all this code is dead.
    
    I traced this all the way back to 2012 when it first became
    dead code: 9b2446d94b85bd192d8c257cdf52b1344cdf8098

--- src/runtime_debug.js
+++ src/runtime_debug.js
@@ -63,31 +63,7 @@ function prettyPrint(arg) {
     return arg + '\n\n';
   }
   if (arg.byteLength) {
-    return '{' + Array.prototype.slice.call(arg, 0, Math.min(arg.length, 400)) + '}'; // Useful for correct arrays, less so for compiled arrays, see the code below for that
-    var buf = new ArrayBuffer(32);
-    var i8buf = new Int8Array(buf);
-    var i16buf = new Int16Array(buf);
-    var f32buf = new Float32Array(buf);
-    switch (arg.toString()) {
-      case '[object Uint8Array]':
-        i8buf.set(arg.subarray(0, 32));
-        break;
-      case '[object Float32Array]':
-        f32buf.set(arg.subarray(0, 5));
-        break;
-      case '[object Uint16Array]':
-        i16buf.set(arg.subarray(0, 16));
-        break;
-      default:
-        alert('unknown array for debugging: ' + arg);
-        throw 'see alert';
-    }
-    var ret = '{' + arg.byteLength + ':\n';
-    var arr = Array.prototype.slice.call(i8buf);
-    ret += 'i8:' + arr.toString().replace(/,/g, ',') + '\n';
-    arr = Array.prototype.slice.call(f32buf, 0, 8);
-    ret += 'f32:' + arr.toString().replace(/,/g, ',') + '}';
-    return ret;
+    return '{' + Array.prototype.slice.call(arg, 0, Math.min(arg.length, 400)) + '}';
   }
   if (typeof arg == 'function') {
     return '<function>';
