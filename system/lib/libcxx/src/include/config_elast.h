139bced7a63e1bbdb640af37130d5f61db0bb60e
Sam Clegg <sbc@chromium.org>
2022-05-23 13:45:34 -0700

    Define _LIBCPP_HAS_MUSL_LIBC in libcxx build (#17022)
    This allows us to remove `include/compat/xlocale.h` which it seems was
    added to support libcxx, but libcxx already has support for musl
    internally when this macro is defined.

--- system/lib/libcxx/src/include/config_elast.h
+++ system/lib/libcxx/src/include/config_elast.h
@@ -29,12 +29,12 @@
 // No _LIBCPP_ELAST needed on Fuchsia
 #elif defined(__wasi__)
 // No _LIBCPP_ELAST needed on WASI
+#elif defined(__EMSCRIPTEN__)
+// No _LIBCPP_ELAST needed on Emscripten
 #elif defined(__linux__) || defined(_LIBCPP_HAS_MUSL_LIBC)
 #define _LIBCPP_ELAST 4095
 #elif defined(__APPLE__)
 // No _LIBCPP_ELAST needed on Apple
-#elif defined(__EMSCRIPTEN__)
-// No _LIBCPP_ELAST needed on Emscripten
 #elif defined(__sun__)
 #define _LIBCPP_ELAST ESTALE
 #elif defined(__MVS__)
