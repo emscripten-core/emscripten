emscripten-core/emscripten

commit eff9ae7106e8e562100fd554a3681d8e52ca5d8e

Use `__stdio_exit` rather then `fflush(0)` on exit (#16130)
This is how musl internally closes down stdio after main returns
(and its how we do it in standalone mode).

emcc.py
@@ -1985,12 +1985,13 @@ def phase_linker_setup(options, state, newargs, settings_map):
       ]
 
     if settings.FILESYSTEM and not settings.STANDALONE_WASM:
-      # to flush streams on FS exit, we need to be able to call fflush
-      # we only include it if the runtime is exitable, or when ASSERTIONS
+      # We use __stdio_exit to shut down musl's stdio subsystems and flush
+      # streams on exit.
+      # We only include it if the runtime is exitable, or when ASSERTIONS
       # (ASSERTIONS will check that streams do not need to be flushed,
       # helping people see when they should have enabled EXIT_RUNTIME)
       if settings.EXIT_RUNTIME or settings.ASSERTIONS:
-        settings.EXPORT_IF_DEFINED += ['fflush']
+        settings.EXPORT_IF_DEFINED += ['__stdio_exit']
 
     if settings.SUPPORT_ERRNO:
       # so setErrNo JS library function can report errno back to C

src/library_wasi.js
@@ -183,7 +183,9 @@ var WasiLibrary = {
 #if SYSCALLS_REQUIRE_FILESYSTEM == 0 && (!MINIMAL_RUNTIME || EXIT_RUNTIME)
   $flush_NO_FILESYSTEM: function() {
     // flush anything remaining in the buffers during shutdown
-    if (typeof _fflush !== 'undefined') _fflush({{{ pointerT(0) }}});
+#if hasExportedFunction('___stdio_exit')
+    ___stdio_exit();
+#endif
     var buffers = SYSCALLS.buffers;
     if (buffers[1].length) SYSCALLS.printChar(1, {{{ charCode("\n") }}});
     if (buffers[2].length) SYSCALLS.printChar(2, {{{ charCode("\n") }}});

src/postamble.js
@@ -364,8 +364,8 @@ function checkUnflushedContent() {
 #if SYSCALLS_REQUIRE_FILESYSTEM == 0
     var flush = {{{ '$flush_NO_FILESYSTEM' in addedLibraryItems ? 'flush_NO_FILESYSTEM' : 'null' }}};
     if (flush) flush();
-#elif hasExportedFunction('_fflush')
-    _fflush(0);
+#elif hasExportedFunction('___stdio_exit')
+    ___stdio_exit();
 #endif
 #if '$FS' in addedLibraryItems && '$TTY' in addedLibraryItems
     // also flush in the JS FS layer

src/library_fs.js
@@ -1490,9 +1490,10 @@ FS.staticInit();` +
     },
     quit: () => {
       FS.init.initialized = false;
-      // force-flush all streams, so we get musl std streams printed out
-#if hasExportedFunction('_fflush')
-      _fflush(0);
+      // Call musl-internal function to close all stdio streams, so nothing is
+      // left in internal buffers.
+#if hasExportedFunction('___stdio_exit')
+      ___stdio_exit();
 #endif
       // close all of our streams
       for (var i = 0; i < FS.streams.length; i++) {

src/postamble_minimal.js
@@ -31,12 +31,12 @@ function run() {
   PThread.terminateAllThreads();
 #endif
 
-#if IN_TEST_HARNESS
-  // fflush() filesystem stdio for test harness, since there are existing
+#if IN_TEST_HARNESS && hasExportedFunction('___stdio_exit')
+  // flush any stdio streams for test harness, since there are existing
   // tests that depend on this behavior.
   // For production use, instead print full lines to avoid this kind of lazy
   // behavior.
-  if (typeof _fflush !== 'undefined') _fflush();
+  ___stdio_exit();
 #endif
 
 #if ASSERTIONS

tests/other/metadce/hello_world.exports
@@ -1,11 +1,11 @@
 __errno_location
 __indirect_function_table
+__stdio_exit
 __wasm_call_ctors
 dynCall_jiji
 emscripten_stack_get_end
 emscripten_stack_get_free
 emscripten_stack_init
-fflush
 main
 memory
 stackAlloc

tests/other/metadce/hello_world.funcs
@@ -10,22 +10,21 @@ $__lockfile
 $__lshrti3
 $__memcpy
 $__ofl_lock
-$__ofl_unlock
 $__original_main
+$__stdio_exit
 $__stdio_write
 $__syscall_getpid
 $__towrite
 $__trunctfdf2
-$__unlock
 $__unlockfile
 $__vfprintf_internal
 $__wasi_syscall_ret
 $__wasm_call_ctors
+$close_file
 $dynCall_jiji
 $emscripten_stack_get_end
 $emscripten_stack_get_free
 $emscripten_stack_init
-$fflush
 $fmt_fp
 $fmt_o
 $fmt_u

tests/other/metadce/hello_world.jssize
@@ -1,1 +1,1 @@
-125367
+125354

commit eaba8f7d672c704aa0e5b23151ac1927c14c7aaa

Don't call `__stdio_exit` when we want to keep the runtime alive
`__stdio_exit` should only be used in cases where the runtime is no
longer going to be used but `checkUnflushedContent` is only used in the
cases where EXIT_RUNTIME=0 (i.e. the runtime stays alive forever).

This was a bug introduced in #16130, which manifested in deadlocks when
other threads attempted to use the stdio streams since __stdio_exit
called `FFINALLOCK` which leaving them in a forever-locked state.

emcc.py
@@ -2099,13 +2099,16 @@ def phase_linker_setup(options, state, newargs, user_settings):
         '$handleException'
       ]
 
-    if not settings.STANDALONE_WASM and (settings.EXIT_RUNTIME or settings.ASSERTIONS):
-      # We use __stdio_exit to shut down musl's stdio subsystems and flush
-      # streams on exit.
-      # We only include it if the runtime is exitable, or when ASSERTIONS
-      # (ASSERTIONS will check that streams do not need to be flushed,
-      # helping people see when they should have enabled EXIT_RUNTIME)
-      settings.EXPORT_IF_DEFINED += ['__stdio_exit']
+    if not settings.STANDALONE_WASM:
+      if settings.EXIT_RUNTIME:
+        # We use __stdio_exit to shut down musl's stdio subsystems and flush
+        # streams on exit.  We only include it if the runtime is exitable.
+        settings.EXPORT_IF_DEFINED += ['__stdio_exit']
+      elif settings.ASSERTIONS:
+        # When ASSERTIONS are enabled but EXIT_RUNTIME is not, we export
+        # fflush so that we can detect and report unflushed content.
+        # See `checkUnflushedContent`
+        settings.EXPORT_IF_DEFINED += ['fflush']
 
     if settings.SUPPORT_ERRNO:
       # so setErrNo JS library function can report errno back to C

src/library_wasi.js
@@ -227,23 +227,34 @@ var WasiLibrary = {
 
 #if SYSCALLS_REQUIRE_FILESYSTEM
   fd_write__deps: ['$doWritev'],
-#elif (!MINIMAL_RUNTIME || EXIT_RUNTIME)
-  $flush_NO_FILESYSTEM__deps: ['$printChar', '$printCharBuffers'],
-  $flush_NO_FILESYSTEM: function() {
-    // flush anything remaining in the buffers during shutdown
-#if hasExportedFunction('___stdio_exit')
-    ___stdio_exit();
-#endif
+#elif !STANDALONE_WASM && EXIT_RUNTIME
+  $stdio_exit_NO_FILESYSTEM__deps: ['$printChar', '$printCharBuffers', '__stdio_exit'],
+  $stdio_exit_NO_FILESYSTEM: function() {
+    // Shut down stdio streams, flushing any content.
+    ___stdio_exit(0);
     if (printCharBuffers[1].length) printChar(1, {{{ charCode("\n") }}});
     if (printCharBuffers[2].length) printChar(2, {{{ charCode("\n") }}});
   },
-  fd_write__deps: ['$flush_NO_FILESYSTEM', '$printChar'],
+  fd_write__deps: ['$stdio_exit_NO_FILESYSTEM', '$printChar'],
   fd_write__postset: function() {
-    addAtExit('flush_NO_FILESYSTEM()');
+    addAtExit('stdio_exit_NO_FILESYSTEM()');
+  },
+#elif !STANDALONE_WASM && ASSERTIONS
+  $stdio_flush_NO_FILESYSTEM__deps: ['$printChar', '$printCharBuffers', 'fflush'],
+  $stdio_flush_NO_FILESYSTEM: function() {
+    // flush anything remaining in the buffers
+    _fflush(0);
+    if (printCharBuffers[1].length) printChar(1, {{{ charCode("\n") }}});
+    if (printCharBuffers[2].length) printChar(2, {{{ charCode("\n") }}});
+  },
+  fd_write__deps: ['$stdio_flush_NO_FILESYSTEM', '$printChar'],
+  fd_write__postset: function() {
+    addAtExit('stdio_flush_NO_FILESYSTEM()');
   },
 #else
   fd_write__deps: ['$printChar'],
 #endif
+
   fd_write__sig: 'iippp',
   fd_write: function(fd, iov, iovcnt, pnum) {
 #if SYSCALLS_REQUIRE_FILESYSTEM

src/postamble.js
@@ -371,11 +371,11 @@ function checkUnflushedContent() {
     has = true;
   }
   try { // it doesn't matter if it fails
-#if SYSCALLS_REQUIRE_FILESYSTEM == 0
-    var flush = {{{ '$flush_NO_FILESYSTEM' in addedLibraryItems ? 'flush_NO_FILESYSTEM' : 'null' }}};
-    if (flush) flush();
-#elif hasExportedFunction('___stdio_exit')
-    ___stdio_exit();
+#if SYSCALLS_REQUIRE_FILESYSTEM == 0 && '$stdio_flush_NO_FILESYSTEM' in addedLibraryItems
+    stdio_flush_NO_FILESYSTEM();
+#elif hasExportedFunction('_fflush')
+    // fflush(NULL) flushes all streams.
+    _fflush(0);
 #endif
 #if '$FS' in addedLibraryItems && '$TTY' in addedLibraryItems
     // also flush in the JS FS layer