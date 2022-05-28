b4d760579bc580dc368dceedf8bd426ae48488ed
Sam Clegg <sbc@chromium.org>
2022-05-28 10:36:06 -0700

    Include worker JS size when measuring output JS size. NFC

--- tests/test_other.py
+++ tests/test_other.py
@@ -7289,6 +7289,8 @@ int main() {
       wasm_size = os.path.getsize('a.out.nodebug.wasm')
       size_file = expected_basename + '.size'
       js_size = os.path.getsize('a.out.js')
+      if self.get_setting('USE_PTHREADS'):
+        js_size += os.path.getsize('a.out.worker.js')
       js_size_file = expected_basename + '.jssize'
       self.check_expected_size_in_file('wasm', size_file, wasm_size)
       self.check_expected_size_in_file('js', js_size_file, js_size)
