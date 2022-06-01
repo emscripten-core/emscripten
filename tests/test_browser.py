4000aadf37157f7d570ce8eb460d2cc47c73446d
Gennadiy X <invulnerable.reason@gmail.com>
2022-06-01 03:22:03 +0300

    [WasmFS] Fix URL support in fetch backend (#16982)
    Construct a valid URL when creating the fetch backend. URL can be either
    relative or absolute, pointing at a file or a directory. Create FetchBackend,
    FetchDirectory and FetchFile classes based on Node backend. Store name in each
    fetch node state, then use it to get a file relative path for URL. Add a way to
    obtain node's path for JS via _wasmfs_fetch_get_file_path().
    
    Fixes #16954.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -5302,17 +5302,22 @@ window.close = function() {
   @parameterized({
     # the fetch backend works even on the main thread: we proxy to a background
     # thread and busy-wait
-    'main_thread': (['-sPTHREAD_POOL_SIZE=1'],),
+    'main_thread': (['-sPTHREAD_POOL_SIZE=4'],),
     # using proxy_to_pthread also works, of course
-    'proxy_to_pthread': (['-sPROXY_TO_PTHREAD'],),
+    'proxy_to_pthread': (['-sPROXY_TO_PTHREAD', '-sINITIAL_MEMORY=32MB', '-DPROXYING'],),
   })
   @requires_threads
   def test_wasmfs_fetch_backend(self, args):
     if is_firefox() and '-sPROXY_TO_PTHREAD' not in args:
       return self.skipTest('ff hangs on the main_thread version. browser bug?')
     create_file('data.dat', 'hello, fetch')
+    create_file('test.txt', 'fetch 2')
+    try_delete('subdir')
+    ensure_dir('subdir')
+    create_file('subdir/backendfile', 'file 1')
+    create_file('subdir/backendfile2', 'file 2')
     self.btest_exit(test_file('wasmfs/wasmfs_fetch.c'),
-                    args=['-sWASMFS', '-sUSE_PTHREADS'] + args)
+                    args=['-sWASMFS', '-sUSE_PTHREADS', '--js-library', test_file('wasmfs/wasmfs_fetch.js')] + args)
 
   @requires_threads
   @no_firefox('no OPFS support yet')
