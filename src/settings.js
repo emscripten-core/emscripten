999123819345b7206e249c7b4c0ddbe6a28f4daf
Hood Chatham <roberthoodchatham@gmail.com>
2022-06-02 11:55:50 -0700

    Try modifying default min browser settings when WASM_BIGINT is present

--- src/settings.js
+++ src/settings.js
@@ -1341,6 +1341,10 @@ var DYNCALLS = false;
 // WebAssembly integration with JavaScript BigInt. When enabled we don't need
 // to legalize i64s into pairs of i32s, as the wasm VM will use a BigInt where
 // an i64 is used.
+// If WASM_BIGINT is present, the default minimum browser versions increase to
+// Safari v15.0, Firefox v68, and Edge v79. It is possible to use WASM_BIGINT
+// with Safari >= v14.0, in that case Emscripten will polyfill BigInt64Array.
+// Use `-sMIN_SAFARI_VERSION=140000` to request this.
 // [link]
 var WASM_BIGINT = false;
 
