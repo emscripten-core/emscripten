b35006b05726770f987991207e875bc413e0f036
Alon Zakai <azakai@google.com>
2022-06-02 11:25:41 -0700

    [WasmFS] Initial mmap support (#16964)
    This matches the JS FS behavior, except that the JS FS could also
    return a pointer to data already in memory, when that is valid. That is
    not yet handled here, but this is enough to get some tests passing.

--- system/lib/standalone/standalone.c
+++ system/lib/standalone/standalone.c
@@ -64,21 +64,15 @@ int clock_getres(clockid_t clk_id, struct timespec *tp) {
 // mmap support is nonexistent. TODO: emulate simple mmaps using
 // stdio + malloc, which is slow but may help some things?
 
-intptr_t _mmap_js(size_t length,
-                  int prot,
-                  int flags,
-                  int fd,
-                  size_t offset,
-                  int* allocated) {
+// Mark these as weak so that wasmfs does not collide with it. That is, if
+// wasmfs is in use, we want to use that and not this.
+__attribute__((__weak__)) intptr_t _mmap_js(
+  size_t length, int prot, int flags, int fd, size_t offset, int* allocated) {
   return -ENOSYS;
 }
 
-int _munmap_js(intptr_t addr,
-               size_t length,
-               int prot,
-               int flags,
-               int fd,
-               size_t offset) {
+__attribute__((__weak__)) int _munmap_js(
+  intptr_t addr, size_t length, int prot, int flags, int fd, size_t offset) {
   return -ENOSYS;
 }
 
@@ -86,8 +80,6 @@ int _munmap_js(intptr_t addr,
 // corner case error checking; everything else is not permitted.
 // TODO: full file support for WASI, or an option for it
 // open()
-// Mark this as weak so that wasmfs does not collide with it. That is, if wasmfs
-// is in use, we want to use that and not this.
 __attribute__((__weak__))
 int __syscall_openat(int dirfd, intptr_t path, int flags, ...) {
   if (!strcmp((const char*)path, "/dev/stdin")) {
