049e8fd3a9f1219d4d7aebab9fd97922483df680
Alon Zakai <azakai@google.com>
2022-06-03 10:42:10 -0700

    Avoid filesystem syscall in test_syscall_intercept (#17149)
    The test checks that we can intercept an imported syscall from JS. In WasmFS,
    however, filesystem syscalls are not JS imports. Instead, move the test to use a
    function that is definitely an import.

--- tests/core/test_syscall_intercept.c
+++ tests/core/test_syscall_intercept.c
@@ -1,21 +1,18 @@
 #include <assert.h>
-#include <string.h>
+#include <emscripten.h>
 #include <stdio.h>
 
 // Import the syscall under a separate name
-__attribute__((import_module("env"), import_name("__syscall_getcwd")))
-int __orig_getcwd(intptr_t buf, size_t size);
+__attribute__((import_module("env"), import_name("emscripten_get_now"))) double
+__orig_emscripten_get_now(void);
 
-
-int __syscall_getcwd(intptr_t buf, size_t size) {
-  printf("__syscall_getcwd intercepted\n");
-  return __orig_getcwd(buf, size);
+double emscripten_get_now(void) {
+  printf("emscripten_get_now intercepted\n");
+  return __orig_emscripten_get_now();
 }
 
 int main() {
-  char cwd[1024];
-  int rtn = __syscall_getcwd((intptr_t)cwd, sizeof(cwd));
-  assert(rtn > 0);
-  printf("cwd = %s\n", cwd);
+  double now = emscripten_get_now();
+  assert(now > 0); // any non-negative time is valid
   return 0;
 }
