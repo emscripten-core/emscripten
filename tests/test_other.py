6ce08eb5760f1c641ea6c41b32ddc86503833550
yangxiaobing <yangxiaobing@jwzg.com>
2022-06-10 19:26:47 +0800

    add val_helper.h to cxx-only-headers

--- tests/test_other.py
+++ tests/test_other.py
@@ -7864,7 +7864,7 @@ int main() {
         # These headers are C++ only and cannot be included from C code.
         # But we still want to check they can be included on there own without
         # any errors or warnings.
-        cxx_only = header in ['wire.h', 'val.h', 'bind.h', 'webgpu_cpp.h']
+        cxx_only = header in ['wire.h', 'val.h', 'val_helper.h', 'bind.h', 'webgpu_cpp.h']
         if directory:
           header = f'{directory}/{header}'
         inc = f'#include <{header}>\n__attribute__((weak)) int foo;\n'
