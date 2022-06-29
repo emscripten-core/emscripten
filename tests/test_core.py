emscripten-core/emscripten

commit b86798af5d70008d2743277233fd303d86892ded

Move PROXY_TO_PTHREAD handling into native code. NFC (#17153)
Fixes: #17144

emcc.py
@@ -1741,7 +1741,10 @@ def phase_linker_setup(options, state, newargs, user_settings):
         settings.EXPECT_MAIN = 0
     else:
       assert not settings.EXPORTED_FUNCTIONS
-      settings.EXPORTED_FUNCTIONS = ['_main']
+      # With PROXY_TO_PTHREAD we don't export `main` at all but instead
+      # we export `emscripten_proxy_main` (elsewhwere).
+      if not settings.PROXY_TO_PTHREAD:
+        settings.EXPORTED_FUNCTIONS = ['_main']
 
   if settings.STANDALONE_WASM:
     # In STANDALONE_WASM mode we either build a command or a reactor.
@@ -2519,12 +2522,6 @@ def phase_linker_setup(options, state, newargs, user_settings):
       # they are indirect calls, since that is what they do - we can't see their
       # targets statically.
       settings.ASYNCIFY_IMPORTS += ['invoke_*']
-    # with pthreads we may call main through the __call_main mechanism, which can
-    # therefore reach anything in the program, so mark it as possibly causing a
-    # sleep (the asyncify analysis doesn't look through JS, just wasm, so it can't
-    # see what it itself calls)
-    if settings.USE_PTHREADS:
-      settings.ASYNCIFY_IMPORTS += ['__call_main']
     # add the default imports
     settings.ASYNCIFY_IMPORTS += DEFAULT_ASYNCIFY_IMPORTS
 

tests/other/metadce/test_metadce_minimal_pthreads.exports
@@ -2,7 +2,6 @@ A
 B
 C
 D
-E
 o
 p
 q

tests/other/metadce/test_metadce_minimal_pthreads.funcs
@@ -46,7 +46,6 @@ $emscripten_stack_set_limits
 $get_tasks_for_thread
 $init_file_lock
 $init_mparams
-$main
 $memset
 $pthread_mutexattr_destroy
 $sbrk

tests/other/metadce/test_metadce_minimal_pthreads.jssize
@@ -1,1 +1,1 @@
-16131
+16048

tests/other/metadce/test_metadce_minimal_pthreads.size
@@ -1,1 +1,1 @@
-18331
+18318

tests/test_core.py
@@ -9187,6 +9187,15 @@ NODEFS is no longer included by default; build with -lnodefs.js
 
   @needs_dylink
   @node_pthreads
+  def test_pthread_dylink_main_module_1(self):
+    self.emcc_args.append('-Wno-experimental')
+    self.set_setting('EXIT_RUNTIME')
+    self.set_setting('USE_PTHREADS')
+    self.set_setting('MAIN_MODULE')
+    self.do_runf(test_file('hello_world.c'))
+
+  @needs_dylink
+  @node_pthreads
   def test_Module_dynamicLibraries_pthreads(self):
     # test that Module.dynamicLibraries works with pthreads
     self.emcc_args += ['-pthread', '-Wno-experimental']
@@ -9249,7 +9258,7 @@ NODEFS is no longer included by default; build with -lnodefs.js
       # In standalone we don't support implicitly building without main.  The user has to explicitly
       # opt out (see below).
       err = self.expect_fail([EMCC, test_file('core/test_ctors_no_main.cpp')] + self.get_emcc_args())
-      self.assertContained('error: undefined symbol: main (referenced by top-level compiled C/C++ code)', err)
+      self.assertContained('error: undefined symbol: main/__main_argc_argv (referenced by top-level compiled C/C++ code)', err)
       self.assertContained('warning: To build in STANDALONE_WASM mode without a main(), use emcc --no-entry', err)
     elif not self.get_setting('LLD_REPORT_UNDEFINED') and not self.get_setting('STRICT'):
       # Traditionally in emscripten we allow main to be implicitly undefined.  This allows programs

emscripten.py
@@ -147,7 +147,7 @@ def update_settings_glue(wasm_file, metadata):
   if settings.MEMORY64:
     assert '--enable-memory64' in settings.BINARYEN_FEATURES
 
-  settings.HAS_MAIN = bool(settings.MAIN_MODULE) or settings.STANDALONE_WASM or 'main' in settings.WASM_EXPORTS or '__main_argc_argv' in settings.WASM_EXPORTS
+  settings.HAS_MAIN = bool(settings.MAIN_MODULE) or settings.PROXY_TO_PTHREAD or settings.STANDALONE_WASM or 'main' in settings.WASM_EXPORTS or '__main_argc_argv' in settings.WASM_EXPORTS
 
   # When using dynamic linking the main function might be in a side module.
   # To be safe assume they do take input parametes.
@@ -219,12 +219,6 @@ def report_missing_symbols(js_library_funcs):
     # standalone mode doesn't use main, and it always reports missing entry point at link time.
     # In this mode we never expect _main in the export list.
     return
-
-  # PROXY_TO_PTHREAD only makes sense with a main(), so error if one is
-  # missing. note that when main() might arrive from another module we cannot
-  # error here.
-  if settings.PROXY_TO_PTHREAD and not settings.HAS_MAIN:
-    exit_with_error('PROXY_TO_PTHREAD proxies main() for you, but no main exists')
 
   if settings.IGNORE_MISSING_MAIN:
     # The default mode for emscripten is to ignore the missing main function allowing

src/jsifier.js
@@ -17,6 +17,9 @@ global.proxiedFunctionTable = ['null'/* Reserve index 0 for an undefined functio
 
 // Mangles the given C/JS side function name to assembly level function name (adds an underscore)
 function mangleCSymbolName(f) {
+  if (f === '__main_argc_argv') {
+    f = 'main';
+  }
   return f[0] == '$' ? f.substr(1) : '_' + f;
 }
 
@@ -51,6 +54,9 @@ function stringifyWithFunctions(obj) {
 
 function isDefined(symName) {
   if (WASM_EXPORTS.has(symName) || SIDE_MODULE_EXPORTS.has(symName)) {
+    return true;
+  }
+  if (symName == '__main_argc_argv' && SIDE_MODULE_EXPORTS.has('main')) {
     return true;
   }
   // 'invoke_' symbols are created at runtime in libary_dylink.py so can
@@ -208,9 +214,13 @@ function ${name}(${args}) {
         }
         const isWeakImport = WEAK_IMPORTS.has(ident);
         if (!isDefined(ident) && !isWeakImport) {
+          if (PROXY_TO_PTHREAD && !MAIN_MODULE && ident == '__main_argc_argv') {
+            error('PROXY_TO_PTHREAD proxies main() for you, but no main exists');
+            return;
+          }
           let undefinedSym = ident;
           if (ident === '__main_argc_argv') {
-            undefinedSym = 'main';
+            undefinedSym = 'main/__main_argc_argv';
           }
           let msg = 'undefined symbol: ' + undefinedSym;
           if (dependent) msg += ` (referenced by ${dependent})`;
@@ -224,7 +234,7 @@ function ${name}(${args}) {
           } else if (VERBOSE || WARN_ON_UNDEFINED_SYMBOLS) {
             warn(msg);
           }
-          if (undefinedSym === 'main' && STANDALONE_WASM) {
+          if (ident === '__main_argc_argv' && STANDALONE_WASM) {
             warn('To build in STANDALONE_WASM mode without a main(), use emcc --no-entry');
           }
         }

commit 40580711fd0d204b4b2c9156d4320770fde304bb

Fix argv passing in PROXY_TO_PTHREAD mode
This was broken in #17153 but we didn't have any tests.  The fix is
to export `main` even though we don't technically need to.  We could
add a little complexity to allow metadce to remove it, but I'm not
sure its worth it.

Fixes: #17338

emcc.py
@@ -1747,9 +1747,7 @@ def phase_linker_setup(options, state, newargs, user_settings):
         settings.EXPECT_MAIN = 0
     else:
       assert not settings.EXPORTED_FUNCTIONS
-      # With PROXY_TO_PTHREAD we don't export `main` at all but instead `_emscripten_proxy_main`.
-      if not settings.PROXY_TO_PTHREAD:
-        settings.EXPORTED_FUNCTIONS = ['_main']
+      settings.EXPORTED_FUNCTIONS = ['_main']
 
   if settings.STANDALONE_WASM:
     # In STANDALONE_WASM mode we either build a command or a reactor.

tests/core/test_hello_argc.c
@@ -6,7 +6,13 @@
  */
 
 #include <stdio.h>
+
 int main(int argc, char** argv) {
   printf("hello, world! %d\n", argc);
+  // Skip arg 0 because it will be an absolute path that varies between
+  // machines.
+  for (int i = 1; i < argc; i++) {
+    printf("%d: %s\n", i, argv[i]);
+  }
   return 0;
 }

tests/core/test_hello_argc.out
@@ -1,1 +1,3 @@
-hello, world! 1
+hello, world! 3
+1: hello
+2: world

tests/other/metadce/test_metadce_minimal_pthreads.exports
@@ -2,6 +2,7 @@ A
 B
 C
 D
+E
 o
 p
 q

tests/other/metadce/test_metadce_minimal_pthreads.funcs
@@ -46,6 +46,7 @@ $emscripten_stack_set_limits
 $get_tasks_for_thread
 $init_file_lock
 $init_mparams
+$main
 $memset
 $pthread_attr_destroy
 $sbrk

tests/other/metadce/test_metadce_minimal_pthreads.jssize
@@ -1,1 +1,1 @@
-15917
+15983

tests/other/metadce/test_metadce_minimal_pthreads.size
@@ -1,1 +1,1 @@
-18289
+18299

tests/test_core.py
@@ -459,7 +459,13 @@ class TestCoreBase(RunnerCore):
 
   @also_with_standalone_wasm()
   def test_hello_argc(self):
-    self.do_core_test('test_hello_argc.c')
+    self.do_core_test('test_hello_argc.c', args=['hello', 'world'])
+
+  @node_pthreads
+  def test_hello_argc_pthreads(self):
+    self.set_setting('PROXY_TO_PTHREAD')
+    self.set_setting('EXIT_RUNTIME')
+    self.do_core_test('test_hello_argc.c', args=['hello', 'world'])
 
   @also_with_wasmfs
   def test_intvars(self):