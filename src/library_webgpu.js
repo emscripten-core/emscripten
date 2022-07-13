50db657b7949212ede2a62a530a6d269caf5c1af
Guillaume Racicot <grc@pixmob.com>
2022-07-13 00:23:32 -0400

    Set the width and height directly to the canvas

--- src/library_webgpu.js
+++ src/library_webgpu.js
@@ -2575,20 +2575,16 @@ var LibraryWebGPU = {
     assert({{{ gpu.PresentMode.Fifo }}} ===
       {{{ gpu.makeGetU32('descriptor', C_STRUCTS.WGPUSwapChainDescriptor.presentMode) }}});
 #endif
-    var configurationSize = [
-      {{{ gpu.makeGetU32('descriptor', C_STRUCTS.WGPUSwapChainDescriptor.width) }}},
-      {{{ gpu.makeGetU32('descriptor', C_STRUCTS.WGPUSwapChainDescriptor.height) }}},
-    ];
+
+    context.canvas.width = {{{ gpu.makeGetU32('descriptor', C_STRUCTS.WGPUSwapChainDescriptor.width) }}};
+    context.canvas.height = {{{ gpu.makeGetU32('descriptor', C_STRUCTS.WGPUSwapChainDescriptor.height) }}};
 
     var configuration = {
       "device": device,
       "format": WebGPU.TextureFormat[
         {{{ gpu.makeGetU32('descriptor', C_STRUCTS.WGPUSwapChainDescriptor.format) }}}],
       "usage": {{{ gpu.makeGetU32('descriptor', C_STRUCTS.WGPUSwapChainDescriptor.usage) }}},
-      "size": configurationSize[0] !== 0 && configurationSize[1] !== 0
-        ? configurationSize
-        : undefined,
-        "alphaMode": "opaque",
+      "alphaMode": "opaque",
     };
     context["configure"](configuration);
 
