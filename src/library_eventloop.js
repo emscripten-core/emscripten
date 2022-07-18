emscripten-core/emscripten

commit 7b4d403adc253bb83751abbd5d973555d3709c0a

More usage of arrow functions in JS library code. NFC (#17446)
See: #11984

src/library_eventloop.js
@@ -16,7 +16,7 @@ LibraryJSEventLoop = {
   $setImmediateWrapped: function(func) {
     if (!setImmediateWrapped.mapping) setImmediateWrapped.mapping = [];
     var id = setImmediateWrapped.mapping.length;
-    setImmediateWrapped.mapping[id] = setImmediate(function() {
+    setImmediateWrapped.mapping[id] = setImmediate(() => {
       setImmediateWrapped.mapping[id] = undefined;
       func();
     });
@@ -44,7 +44,7 @@ LibraryJSEventLoop = {
       'var __setImmediate_id_counter = 0;\n' +
       'var __setImmediate_queue = [];\n' +
       'var __setImmediate_message_id = "_si";\n' +
-      'function __setImmediate_cb(/** @type {Event} */e) {\n' +
+      'var __setImmediate_cb = (/** @type {Event} */e) => {\n' +
         'if (e.data === __setImmediate_message_id) {\n' +
           'e.stopPropagation();\n' +
           '__setImmediate_queue.shift()();\n' +
@@ -52,11 +52,11 @@ LibraryJSEventLoop = {
         '}\n' +
       '}\n' +
       'addEventListener("message", __setImmediate_cb, true);\n' +
-      'emSetImmediate = function(func) {\n' +
+      'emSetImmediate = (func) => {\n' +
         'postMessage(__setImmediate_message_id, "*");\n' +
         'return __setImmediate_id_counter + __setImmediate_queue.push(func) - 1;\n' +
       '}\n' +
-      'emClearImmediate = /**@type{function(number=)}*/(function(id) {\n' +
+      'emClearImmediate = /**@type{function(number=)}*/((id) => {\n' +
         'var index = id - __setImmediate_id_counter;\n' +
         'if (index >= 0 && index < __setImmediate_queue.length) __setImmediate_queue[index] = function() {};\n' + // must preserve the order and count of elements in the queue, so replace the pending callback with an empty function
       '})\n' +

src/library.js
@@ -440,8 +440,8 @@ mergeInto(LibraryManager.library, {
   //   AppleWebKit/605.1.15 Version/13.0.3 Intel Mac OS X 10_15_1 on Safari 13.0.3 (15608.3.10.1.4) on macOS Catalina 10.15.1
   // Hence the support status of .copyWithin() for Safari version range [10.0.0, 10.1.0] is unknown.
   emscripten_memcpy_big: '= Uint8Array.prototype.copyWithin\n' +
-    '  ? function(dest, src, num) { HEAPU8.copyWithin(dest, src, src + num); }\n' +
-    '  : function(dest, src, num) { HEAPU8.set(HEAPU8.subarray(src, src+num), dest); }\n',
+    '  ? (dest, src, num) => HEAPU8.copyWithin(dest, src, src + num)\n' +
+    '  : (dest, src, num) => HEAPU8.set(HEAPU8.subarray(src, src+num), dest)\n',
 #else
   emscripten_memcpy_big: function(dest, src, num) {
     HEAPU8.copyWithin(dest, src, src + num);
@@ -2210,8 +2210,8 @@ mergeInto(LibraryManager.library, {
   // ==========================================================================
 #if SOCKET_WEBRTC
   $Sockets__deps: ['$setErrNo',
-    function() { return 'var SocketIO = ' + read('../third_party/socket.io.js') + ';\n' },
-    function() { return 'var Peer = ' + read('../third_party/wrtcp.js') + ';\n' }],
+    () => 'var SocketIO = ' + read('../third_party/socket.io.js') + ';\n',
+    () => 'var Peer = ' + read('../third_party/wrtcp.js') + ';\n'],
 #else
   $Sockets__deps: ['$setErrNo'],
 #endif
@@ -2242,7 +2242,7 @@ mergeInto(LibraryManager.library, {
     if (typeof crypto == 'object' && typeof crypto['getRandomValues'] == 'function') {
       // for modern web browsers
       var randomBuffer = new Uint8Array(1);
-      return function() { crypto.getRandomValues(randomBuffer); return randomBuffer[0]; };
+      return () => { crypto.getRandomValues(randomBuffer); return randomBuffer[0]; };
     } else
 #if ENVIRONMENT_MAY_BE_NODE
     if (ENVIRONMENT_IS_NODE) {
@@ -2250,7 +2250,7 @@ mergeInto(LibraryManager.library, {
       try {
         var crypto_module = require('crypto');
         // nodejs has crypto support
-        return function() { return crypto_module['randomBytes'](1)[0]; };
+        return () => crypto_module['randomBytes'](1)[0];
       } catch (e) {
         // nodejs doesn't have crypto support
       }
@@ -2258,9 +2258,9 @@ mergeInto(LibraryManager.library, {
 #endif // ENVIRONMENT_MAY_BE_NODE
     // we couldn't find a proper implementation, as Math.random() is not suitable for /dev/random, see emscripten-core/emscripten/pull/7096
 #if ASSERTIONS
-    return function() { abort("no cryptographic support found for randomDevice. consider polyfilling it if you want to use something insecure like Math.random(), e.g. put this in a --pre-js: var crypto = { getRandomValues: function(array) { for (var i = 0; i < array.length; i++) array[i] = (Math.random()*256)|0 } };"); };
+    return () => abort("no cryptographic support found for randomDevice. consider polyfilling it if you want to use something insecure like Math.random(), e.g. put this in a --pre-js: var crypto = { getRandomValues: function(array) { for (var i = 0; i < array.length; i++) array[i] = (Math.random()*256)|0 } };");
 #else
-    return function() { abort("randomDevice"); };
+    return () => abort("randomDevice");
 #endif
   },
 
@@ -3198,7 +3198,7 @@ mergeInto(LibraryManager.library, {
     ['keydown', 'mousedown', 'touchstart'].forEach(function(event) {
       elements.forEach(function(element) {
         if (element) {
-          listenOnce(element, event, function() {
+          listenOnce(element, event, () => {
             if (ctx.state === 'suspended') ctx.resume();
           });
         }
@@ -3573,11 +3573,11 @@ mergeInto(LibraryManager.library, {
   $asyncLoad__docs: '/** @param {boolean=} noRunDep */',
   $asyncLoad: function(url, onload, onerror, noRunDep) {
     var dep = !noRunDep ? getUniqueRunDependency('al ' + url) : '';
-    readAsync(url, function(arrayBuffer) {
+    readAsync(url, (arrayBuffer) => {
       assert(arrayBuffer, 'Loading data file "' + url + '" failed (no arrayBuffer).');
       onload(new Uint8Array(arrayBuffer));
       if (dep) removeRunDependency(dep);
-    }, function(event) {
+    }, (event) => {
       if (onerror) {
         onerror();
       } else {

src/library_browser.js
@@ -201,7 +201,7 @@ var LibraryBrowser = {
           assert(typeof url == 'string', 'createObjectURL must return a url as a string');
 #endif
           var audio = new Audio();
-          audio.addEventListener('canplaythrough', function() { finish(audio) }, false); // use addEventListener due to chromium bug 124926
+          audio.addEventListener('canplaythrough', () => finish(audio), false); // use addEventListener due to chromium bug 124926
           audio.onerror = function audio_onerror(event) {
             if (done) return;
             err('warning: browser could not fully decode audio ' + name + ', trying slower base64 approach');
@@ -256,14 +256,12 @@ var LibraryBrowser = {
           // than just running the promises in parallel, this makes a chain of
           // promises to run in series.
           wasmPlugin['asyncWasmLoadPromise'] = wasmPlugin['asyncWasmLoadPromise'].then(
-            function() {
-              return loadWebAssemblyModule(byteArray, {loadAsync: true, nodelete: true});
-            }).then(
-              function(module) {
+            () => loadWebAssemblyModule(byteArray, {loadAsync: true, nodelete: true})).then(
+              (module) => {
                 preloadedWasm[name] = module;
                 onload();
               },
-              function(err) {
+              (err) => {
                 console.warn("Couldn't instantiate wasm: " + name + " '" + err + "'");
                 onerror();
               });
@@ -289,12 +287,12 @@ var LibraryBrowser = {
                                     canvas['mozRequestPointerLock'] ||
                                     canvas['webkitRequestPointerLock'] ||
                                     canvas['msRequestPointerLock'] ||
-                                    function(){};
+                                    (() => {});
         canvas.exitPointerLock = document['exitPointerLock'] ||
                                  document['mozExitPointerLock'] ||
                                  document['webkitExitPointerLock'] ||
                                  document['msExitPointerLock'] ||
-                                 function(){}; // no-op if function does not exist
+                                 (() => {}); // no-op if function does not exist
         canvas.exitPointerLock = canvas.exitPointerLock.bind(document);
 
         document.addEventListener('pointerlockchange', pointerLockChange, false);
@@ -303,7 +301,7 @@ var LibraryBrowser = {
         document.addEventListener('mspointerlockchange', pointerLockChange, false);
 
         if (Module['elementPointerLock']) {
-          canvas.addEventListener("click", function(ev) {
+          canvas.addEventListener("click", (ev) => {
             if (!Browser.pointerLock && Module['canvas'].requestPointerLock) {
               Module['canvas'].requestPointerLock();
               ev.preventDefault();
@@ -440,8 +438,8 @@ var LibraryBrowser = {
       canvasContainer.requestFullscreen = canvasContainer['requestFullscreen'] ||
                                           canvasContainer['mozRequestFullScreen'] ||
                                           canvasContainer['msRequestFullscreen'] ||
-                                         (canvasContainer['webkitRequestFullscreen'] ? function() { canvasContainer['webkitRequestFullscreen'](Element['ALLOW_KEYBOARD_INPUT']) } : null) ||
-                                         (canvasContainer['webkitRequestFullScreen'] ? function() { canvasContainer['webkitRequestFullScreen'](Element['ALLOW_KEYBOARD_INPUT']) } : null);
+                                         (canvasContainer['webkitRequestFullscreen'] ? () => canvasContainer['webkitRequestFullscreen'](Element['ALLOW_KEYBOARD_INPUT']) : null) ||
+                                         (canvasContainer['webkitRequestFullScreen'] ? () => canvasContainer['webkitRequestFullScreen'](Element['ALLOW_KEYBOARD_INPUT']) : null);
 
       canvasContainer.requestFullscreen();
     },
@@ -789,11 +787,11 @@ var LibraryBrowser = {
       PATH.dirname(_file),
       PATH.basename(_file),
       new Uint8Array(data.object.contents), true, true,
-      function() {
+      () => {
         {{{ runtimeKeepalivePop() }}}
         if (onload) {{{ makeDynCall('vi', 'onload') }}}(file);
       },
-      function() {
+      () => {
         {{{ runtimeKeepalivePop() }}}
         if (onerror) {{{ makeDynCall('vi', 'onerror') }}}(file);
       },
@@ -818,11 +816,11 @@ var LibraryBrowser = {
       name,
       {{{ makeHEAPView('U8', 'data', 'data + size') }}},
       true, true,
-      function() {
+      () => {
         {{{ runtimeKeepalivePop() }}}
         if (onload) {{{ makeDynCall('vii', 'onload') }}}(arg, cname);
       },
-      function() {
+      () => {
         {{{ runtimeKeepalivePop() }}}
         if (onerror) {{{ makeDynCall('vi', 'onerror') }}}(arg);
       },
@@ -864,7 +862,7 @@ var LibraryBrowser = {
         }
       }
     };
-    script.onerror = function() {
+    script.onerror = () => {
       {{{ runtimeKeepalivePop() }}}
       if (onerror) onerror();
     };
@@ -912,14 +910,15 @@ var LibraryBrowser = {
         // Emulate setImmediate. (note: not a complete polyfill, we don't emulate clearImmediate() to keep code size to minimum, since not needed)
         var setImmediates = [];
         var emscriptenMainLoopMessageId = 'setimmediate';
-        var Browser_setImmediate_messageHandler = function(/** @type {Event} */ event) {
+        /** @param {Event} event */
+        var Browser_setImmediate_messageHandler = (event) => {
           // When called in current thread or Worker, the main loop ID is structured slightly different to accommodate for --proxy-to-worker runtime listening to Worker events,
           // so check for both cases.
           if (event.data === emscriptenMainLoopMessageId || event.data.target === emscriptenMainLoopMessageId) {
             event.stopPropagation();
             setImmediates.shift()();
           }
-        }
+        };
         addEventListener("message", Browser_setImmediate_messageHandler, true);
         setImmediate = /** @type{function(function(): ?, ...?): number} */(function Browser_emulated_setImmediate(func) {
           setImmediates.push(func);
@@ -1103,7 +1102,7 @@ var LibraryBrowser = {
   emscripten_set_main_loop_arg__deps: ['$setMainLoop'],
   emscripten_set_main_loop_arg__sig: 'viiii',
   emscripten_set_main_loop_arg: function(func, arg, fps, simulateInfiniteLoop) {
-    var browserIterationFunc = function() { {{{ makeDynCall('vi', 'func') }}}(arg); };
+    var browserIterationFunc = () => {{{ makeDynCall('vi', 'func') }}}(arg);
     setMainLoop(browserIterationFunc, fps, simulateInfiniteLoop, arg);
   },
 

src/library_ccall.js
@@ -29,7 +29,7 @@ mergeInto(LibraryManager.library, {
 #if MEMORY64
       'pointer': (p) => {{{ to64('p') }}},
 #endif
-      'string': function(str) {
+      'string': (str) => {
         var ret = 0;
         if (str !== null && str !== undefined && str !== 0) { // null string
           // at most 4 bytes per UTF-8 code point, +1 for the trailing '\0'
@@ -39,7 +39,7 @@ mergeInto(LibraryManager.library, {
         }
         return {{{ to64('ret') }}};
       },
-      'array': function(arr) {
+      'array': (arr) => {
         var ret = stackAlloc(arr.length);
         writeArrayToMemory(arr, ret);
         return {{{ to64('ret') }}};

src/library_dylink.js
@@ -800,7 +800,7 @@ var LibraryDylink = {
 
       if (flags.loadAsync) {
         return new Promise(function(resolve, reject) {
-          readAsync(libFile, function(data) { resolve(new Uint8Array(data)); }, reject);
+          readAsync(libFile, (data) => resolve(new Uint8Array(data)), reject);
         });
       }
 

src/worker.js
@@ -22,9 +22,7 @@ if (ENVIRONMENT_IS_NODE) {
 
   var parentPort = nodeWorkerThreads.parentPort;
 
-  parentPort.on('message', function(data) {
-    onmessage({ data: data });
-  });
+  parentPort.on('message', (data) => onmessage({ data: data }));
 
   var fs = require('fs');
 

tests/other/metadce/test_metadce_cxx_ctors1.jssize
@@ -1,1 +1,1 @@
-26202
+26170

tests/other/metadce/test_metadce_cxx_ctors2.jssize
@@ -1,1 +1,1 @@
-26166
+26134

commit ee68576f657698e126a73f13ed915d86083a1f70

Fix closer error in wasm2s.core2s.test_dylink_syslibs_missing_assertions
This was broken by #17446 but we don't run wasm2s are part of our github
CI.

.circleci/config.yml
@@ -400,6 +400,7 @@ jobs:
             corez.test_dylink_iostream
             core2ss.test_pthread_dylink
             core2ss.test_pthread_thread_local_storage
+            core2s.test_dylink_syslibs_missing_assertions
             wasm2js3.test_memorygrowth_2
             other.test_unistd_fstatfs_wasmfs
             wasmfs.test_hello_world

src/library_eventloop.js
@@ -44,7 +44,8 @@ LibraryJSEventLoop = {
       'var __setImmediate_id_counter = 0;\n' +
       'var __setImmediate_queue = [];\n' +
       'var __setImmediate_message_id = "_si";\n' +
-      'var __setImmediate_cb = (/** @type {Event} */e) => {\n' +
+      '/** @param {Event} e */\n' +
+      'var __setImmediate_cb = (e) => {\n' +
         'if (e.data === __setImmediate_message_id) {\n' +
           'e.stopPropagation();\n' +
           '__setImmediate_queue.shift()();\n' +