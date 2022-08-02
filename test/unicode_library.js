12f69073e3900aa15f1598eee07f1603a9cc87e6
Mitch Foley <mitchfoley@chromium.org>
2022-08-02 17:13:27 -0400

    properly await for Module in the test

--- test/unicode_library.js
+++ test/unicode_library.js
@@ -1,5 +1,10 @@
+(async function main(){
+await Module();
+
 mergeInto(LibraryManager.library, {
 printey: function() {
   out('Unicode snowman ☃ says hello! àČšñéáúÍųåêâăščếệüçλληνικάбългарскиPусскийСрпскиУкраїнська한국어中文普通话(中国大陆)普通话(香港)中文(台灣)粵語(香港)日本語हिन्दीภาษาไทย');
 }
 });
+
+})();
