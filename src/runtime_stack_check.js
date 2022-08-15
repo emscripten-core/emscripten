111e387e78dc871012603cefdac4bdf17eb0baeb
Benji <64439681+benjitrosch@users.noreply.github.com>
2022-08-15 19:37:19 -0400

    Replace i32 with u32 in writeStackCookie

--- src/runtime_stack_check.js
+++ src/runtime_stack_check.js
@@ -7,7 +7,7 @@
 #if STACK_OVERFLOW_CHECK
 // Initializes the stack cookie. Called at the startup of main and at the startup of each thread in pthreads mode.
 function writeStackCookie() {
-  var max = _emscripten_stack_get_end() >> 2;
+  var max = _emscripten_stack_get_end();
 #if RUNTIME_DEBUG
   err('writeStackCookie: ' + max.toString(16));
 #endif
@@ -17,8 +17,8 @@ function writeStackCookie() {
   // The stack grow downwards towards _emscripten_stack_get_end.
   // We write cookies to the final two words in the stack and detect if they are
   // ever overwritten.
-  HEAPU32[max] = 0x2135467;
-  HEAPU32[max + 1] = 0x89BACDFE;
+  {{{ makeSetValue('max', 0, '0x2135467', 'u32' ) }}};
+  {{{ makeSetValue('max', 4, '0x89BACDFE', 'u32' ) }}};
 #if !USE_ASAN && !SAFE_HEAP // ASan and SAFE_HEAP check address 0 themselves
   // Also test the global address 0 for integrity.
   HEAPU32[0] = 0x63736d65; /* 'emsc' */
