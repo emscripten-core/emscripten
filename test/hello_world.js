0e39e2af9166fce4990b5eac23371a48119af078
Sam Clegg <sbc@chromium.org>
2022-08-22 13:10:31 -0700

    Document and simplify test/hello_world.js (#17691)
    Having a definition of ENVIRONMENT_IS_NODE here a little confusing
    because that name is used by the emscripten runtime code.

--- test/hello_world.js
+++ test/hello_world.js
@@ -2,15 +2,15 @@
  * @license
  * Copyright 2013 The Emscripten Authors
  * SPDX-License-Identifier: MIT
+ *
+ * Simple test JS program used by `test/jsrun.py` to verify that a JS engine
+ * is working.
  */
 
 'use strict';
 
-var ENVIRONMENT_IS_NODE = typeof process === 'object';
-if (ENVIRONMENT_IS_NODE) {
-  global.print = function(x) {
-    process['stdout'].write(x + '\n');
-  }
+if (typeof print === 'undefined') {
+  global.print = console.log
 }
 
 print('hello, world!');
