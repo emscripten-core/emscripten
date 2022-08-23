6fc80d49976379fdf33de627eba0f2ef3ef49363
Alon Zakai <azakai@google.com>
2022-08-23 15:26:20 -0700

    skip wasm64

--- test/test_core.py
+++ test/test_core.py
@@ -5843,6 +5843,7 @@ main( int argv, char ** argc ) {
   def test_fs_writeFile(self):
     self.do_run_in_out_file_test('fs/test_writeFile.cpp')
 
+  @no_wasm64('MEMORY64 does not yet support WasmFS')
   def test_fs_writeFile_wasmfs(self):
     self.emcc_args += ['-sWASMFS']
     self.emcc_args += ['-sFORCE_FILESYSTEM']
