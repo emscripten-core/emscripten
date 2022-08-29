88e06222680d96f158d00d781d353b2c893dbe64
Sam Clegg <sbc@chromium.org>
2022-08-29 09:06:38 -0700

    Rename runOnMainThread -> runIfMainThread (#17740)
    When ready code `runOnMainThread` sounds like its going to proxy the
    work to the main thread, but that is not what this function does.  Its
    really about conditional execution.

--- src/shell_minimal.js
+++ src/shell_minimal.js
@@ -120,7 +120,7 @@ function ready() {
   readyPromiseResolve(Module);
 #endif // MODULARIZE
 #if INVOKE_RUN && HAS_MAIN
-  {{{ runOnMainThread("run();") }}}
+  {{{ runIfMainThread("run();") }}}
 #elif ASSERTIONS
   console.log('ready() called, and INVOKE_RUN=0. The runtime is now ready for you to call run() to invoke application _main(). You can also override ready() in a --pre-js file to get this signal as a callback')
 #endif
