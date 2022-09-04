3d7c3b81dabe4ee965186a0631131123f09b9bfb
Hood Chatham <roberthoodchatham@gmail.com>
2022-09-04 09:46:57 -0700

    Fix generateDyncall when there is more than one bigint argument

--- test/test_runtime_dyncall_wrapper.c
+++ test/test_runtime_dyncall_wrapper.c
@@ -10,6 +10,10 @@ void f2(int i, uint64_t j, float f, double d){
     printf("i: %d j: %lld f: %f d: %lf\n", i, j, f, d);
 }
 
+void f3(uint64_t j1, uint64_t j2, uint64_t j3){
+    printf("j1: %lld, j2: %lld, j3: %lld\n", j1, j2, j3);
+}
+
 
 int main(){
     EM_ASM({
@@ -21,4 +25,9 @@ int main(){
         var w = createDyncallWrapper("vijfd");
         w($0, 2, 7, 2, 3.12, 77.12);
     }, f2);
+
+    EM_ASM({
+        var w = createDyncallWrapper("vjjj");
+        w($0, 7, 2, 2, 7, 3, 3);
+    }, f3);
 }
