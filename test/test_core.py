emscripten-core/emscripten

commit d947b506149902497628c3114dbb89e50ee12c0a

Remove finally block from callMain. NFC (#17486)
As of #17478 this finally block, and the try block itself, is not needed
under certain circumstances.

src/postamble.js
@@ -149,7 +149,9 @@ function callMain(args) {
   var argv = 0;
 #endif // MAIN_READS_PARAMS
 
+#if ABORT_ON_WASM_EXCEPTIONS || !PROXY_TO_PTHREAD
   try {
+#endif
 #if BENCHMARK
     var start = Date.now();
 #endif
@@ -185,13 +187,14 @@ function callMain(args) {
   }
   catch (e) {
     return handleException(e);
+  }
 #endif // !PROXY_TO_PTHREAD
-  } finally {
 #if ABORT_ON_WASM_EXCEPTIONS
+  finally {
     // See abortWrapperDepth in preamble.js!
     abortWrapperDepth -= 2;
-#endif
   }
+#endif
 }
 #endif // HAS_MAIN
 

tests/other/metadce/test_metadce_cxx_ctors1.jssize
@@ -1,1 +1,1 @@
-26119
+26110

tests/other/metadce/test_metadce_cxx_ctors2.jssize
@@ -1,1 +1,1 @@
-26083
+26074

tests/other/metadce/test_metadce_cxx_except.jssize
@@ -1,1 +1,1 @@
-31513
+31504

tests/other/metadce/test_metadce_cxx_except_wasm.jssize
@@ -1,1 +1,1 @@
-26273
+26264

tests/other/metadce/test_metadce_cxx_mangle.jssize
@@ -1,1 +1,1 @@
-31613
+31604

tests/other/metadce/test_metadce_cxx_noexcept.jssize
@@ -1,1 +1,1 @@
-26119
+26110

tests/other/metadce/test_metadce_hello_O0.jssize
@@ -1,1 +1,1 @@
-27528
+27514

commit dbbbb19e92af9036796628e19251fb8217e44b92

Fix ABORT_ON_WASM_EXCEPTIONS + PROXY_TO_PTHREAD
This was broken in #17486 but we were not testing this configuration.

src/postamble.js
@@ -180,6 +180,9 @@ function callMain(args) {
 #if ASSERTIONS
     assert(ret == 0, '_emscripten_proxy_main failed to start proxy thread: ' + ret);
 #endif
+#if ABORT_ON_WASM_EXCEPTIONS
+  }
+#endif
 #else
 #if ASYNCIFY == 2
     // The current spec of JSPI returns a promise only if the function suspends

test/test_core.py
@@ -9460,6 +9460,13 @@ NODEFS is no longer included by default; build with -lnodefs.js
     self.assertNotContained('unhandled exception', output)
     self.assertNotContained('Aborted', output)
 
+  @node_pthreads
+  def test_abort_on_exceptions_pthreads(self):
+    self.set_setting('ABORT_ON_WASM_EXCEPTIONS')
+    self.set_setting('PROXY_TO_PTHREAD')
+    self.set_setting('EXIT_RUNTIME')
+    self.do_core_test('test_hello_world.c')
+
   @needs_dylink
   def test_gl_main_module(self):
     self.set_setting('MAIN_MODULE')