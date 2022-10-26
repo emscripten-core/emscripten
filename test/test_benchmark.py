119bf2b7b1892460925c735eb485bab3f3829fac
Heejin Ahn <aheejin@gmail.com>
2022-10-26 14:52:32 -0700

    Add option to force rebuilding libraries
    Most big benchmarks (the ones with `_zzz_`) in `test_benchmark.py` has a
    separate directory in `test/third_party`, and a test source file with
    `main` that we build as an executable.
    https://github.com/emscripten-core/emscripten/blob/857bdf9cb9ae37812dbdeb107f2e65b508d8b484/test/test_benchmark.py#L142-L148
    
    `RunnerCore::get_library` copies `test/third_party/[LIBRARY]` into
    `building` dir within a temp dir, builds the library if it's not built
    and cached already, and returns the paths of the library files.
    https://github.com/emscripten-core/emscripten/blob/857bdf9cb9ae37812dbdeb107f2e65b508d8b484/test/common.py#L983-L990
    
    But when `native_exec` parameter of `Benchmarker::build` is true, In
    `NativeBenchmarker`, we don't have a separate test file that becomes an
    executable. After building a library, the library directory contains a
    native executable ready to be used, so we just copy them to
    `LIBRARY.native` file.
    https://github.com/emscripten-core/emscripten/blob/857bdf9cb9ae37812dbdeb107f2e65b508d8b484/test/test_benchmark.py#L151-L153
    
    Only `lua` (which is used by `test_zzz_lua_binarytrees` and
    `test_zzz_lua_scimark` is using this `native_exec` option:
    https://github.com/emscripten-core/emscripten/blob/857bdf9cb9ae37812dbdeb107f2e65b508d8b484/test/test_benchmark.py#L982
    
    This makes running more than one `lua` benchmarks in series error out.
    For example, suppose you run `NativeBenchmarker` on
    `test_zzz_lua_binarytrees` and then `test_zzz_lua_scimark`, by running
    ```console
    ./test/runner benchmark.test_zzz_lua_binarytrees benchmark.test_zzz_lua_scimark
    ```
    When you run `lua_binarytrees`, you run `lua`s `lib_builder`, which
    calls `get_library`, which copies `test/third_party/lua` into `building`
    within the temp directory and builds the native executable. And then
    when you run `lua_scimark`, it doesn't rebuild `lua` library but just
    retrieves the cached build. But cached builds are for generated
    libraries and not executables, which `native_exec` uses.
    
    We can possibly rewrite `get_library` so that it can nicely handle both
    libraries and native executables, but currently this becomes a problem
    only when we run two `lua_*` benchmarks with `NativeBenchmarker`, so
    doing something big seems like overkill.
    
    This adds `force_rebuild` option, which defaults to false, to
    `RunnerCore::get_library`, which is set only when `native_exec` is true
    in `test_benchmark.py` (Currently only `lua`).
    
    To reproduce the bug this PR fixes, currently you should actually run
    one more bennchmarks whose name comes before `lua` in alphabetical
    order:
    ```console
    ./test/runner benchmark.test_zzz_coremark benchmark.test_zzz_lua_binarytrees benchmark.test_zzz_lua_scimark
    ```
    Because this line alters the environment when `build_library` (which is
    called by `get_library`) is first run:
    https://github.com/emscripten-core/emscripten/blob/857bdf9cb9ae37812dbdeb107f2e65b508d8b484/test/common.py#L1863
    So if we run only the two `lua_*` benchmarks, the second run will have a
    different set of environment variables, which makes the hashes
    different, making `get_library` rebuild, not load, the library.

--- test/test_benchmark.py
+++ test/test_benchmark.py
@@ -969,7 +969,9 @@ class benchmark(common.RunnerCore):
     shutil.copyfile(test_file(f'third_party/lua/{benchmark}.lua'), benchmark + '.lua')
 
     def lib_builder(name, native, env_init):
-      ret = self.get_library(os.path.join('third_party', 'lua_native' if native else 'lua'), [os.path.join('src', 'lua.o'), os.path.join('src', 'liblua.a')], make=['make', 'generic'], configure=None, native=native, cache_name_extra=name, env_init=env_init)
+      # We force recomputation for the native benchmarker because this benchmark
+      # uses native_exec=True, so we need to copy the native executable
+      ret = self.get_library(os.path.join('third_party', 'lua_native' if native else 'lua'), [os.path.join('src', 'lua.o'), os.path.join('src', 'liblua.a')], make=['make', 'generic'], configure=None, native=native, cache_name_extra=name, env_init=env_init, force_rebuild=native)
       if native:
         return ret
       shutil.copyfile(ret[0], ret[0] + '.bc')
