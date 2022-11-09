34b924aeb2300f97738840f29ddebffa554a4c96
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-11-09 11:58:01 +0100

    Re-order location of `--pre-js` output
    To allow reference the `locateFile` function and the
    `ENVIRONMENT_IS_*` helpers.

--- src/shell.js
+++ src/shell.js
@@ -72,17 +72,6 @@ Module['ready'] = new Promise(function(resolve, reject) {
 #endif
 #endif
 
-// --pre-jses are emitted after the Module integration code, so that they can
-// refer to Module (if they choose; they can also define Module)
-{{{ preJS() }}}
-
-// Sometimes an existing Module object exists with properties
-// meant to overwrite the default module functionality. Here
-// we collect those properties and reapply _after_ we configure
-// the current environment's defaults to avoid having to be so
-// defensive during initialization.
-var moduleOverrides = Object.assign({}, Module);
-
 var arguments_ = [];
 var thisProgram = './this.program';
 var quit_ = (status, toThrow) => {
@@ -434,6 +423,19 @@ if (!ENVIRONMENT_IS_AUDIO_WORKLET)
   throw new Error('environment detection error');
 #endif // ASSERTIONS
 }
+
+// --pre-jses are emitted after the JS helpers, and the Module
+// integration code, so they can reference locateFile and
+// the ENVIRONMENT_IS_* helpers, as well as the Module itself
+// (or even overwrite the entire Module).
+{{{ preJS() }}}
+
+// Sometimes an existing Module object exists with properties
+// meant to overwrite the default module functionality. Here
+// we collect those properties and reapply _after_ we configure
+// the current environment's defaults to avoid having to be so
+// defensive during initialization.
+var moduleOverrides = Object.assign({}, Module);
 
 #if ENVIRONMENT_MAY_BE_NODE && USE_PTHREADS
 if (ENVIRONMENT_IS_NODE) {
