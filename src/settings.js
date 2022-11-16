91e35f38efa8144a5d1a9467cf4906b0c4f2b876
Sam Clegg <sbc@chromium.org>
2022-11-16 08:54:56 -0800

    Bump min browser versions such they all support llvm default features (#17690)
    LLVM and binaryen updated their defaults to include sign extension and
    mutable globals: https://reviews.llvm.org/D125728.
    
    According to https://webassembly.org/roadmap/ the min versions required
    for sign extension are:
    
    - firefox: 62
    - safai: 14.1
    - chrome: 71
    
    And for mutable globals:
    
    - firefox: 61
    - safai: ??
    - chrome: 72
    
    I believe safari implement mutable globals since the beginning which is
    perhaps why there is no min version specified.
    
    Since the minimum firefox and chrome versions are already 65 and 74, the
    only version we need to bump is the safari version which needs to go
    from 12.0 to 14.1.
    
    Also, drop support for the old EdgeHTML engine by default, we we don't
    know the status of these features on that engine (we also don't have any
    testing at all of this platform).

--- src/settings.js
+++ src/settings.js
@@ -1749,14 +1749,16 @@ var MIN_FIREFOX_VERSION = 65;
 
 // Specifies the oldest version of desktop Safari to target. Version is encoded
 // in MMmmVV, e.g. 70101 denotes Safari 7.1.1.
-// Safari 12.0.0 was released on September 17, 2018, bundled with macOS 10.14.0
-// Mojave.
+// Safari 14.1.0 was released on April 26, 2021, bundled with macOS 11.0 Big
+// Sur and iOS 14.5.
+// The previous default, Safari 12.0.0 was released on September 17, 2018,
+// bundled with macOS 10.14.0 Mojave.
 // NOTE: Emscripten is unable to produce code that would work in iOS 9.3.5 and
 // older, i.e. iPhone 4s, iPad 2, iPad 3, iPad Mini 1, Pod Touch 5 and older,
 // see https://github.com/emscripten-core/emscripten/pull/7191.
 // MAX_INT (0x7FFFFFFF, or -1) specifies that target is not supported.
 // [link]
-var MIN_SAFARI_VERSION = 120000;
+var MIN_SAFARI_VERSION = 140100;
 
 // Specifies the oldest version of Internet Explorer to target. E.g. pass -s
 // MIN_IE_VERSION = 11 to drop support for IE 10 and older.
@@ -1768,10 +1770,17 @@ var MIN_IE_VERSION = 0x7FFFFFFF;
 // Specifies the oldest version of Edge (EdgeHTML, the non-Chromium based
 // flavor) to target. E.g. pass -sMIN_EDGE_VERSION=40 to drop support for
 // EdgeHTML 39 and older.
-// Edge 44.17763 was released on November 13, 2018
+// EdgeHTML 44.17763 was released on November 13, 2018
+// EdgeHTML was completely in April 2021 and replaced by the current
+// Chromium-based Edge.
+// Since version 79, Edge version numbers have mirrored chromium version
+// numbers, so it no longer makes sense specify MIN_EDGE_VERSION independenly.
+// If Chromium and Edge ever start to diverage this setting may be revived with
+// more modern post-chromium default value.
+// See https://en.wikipedia.org/wiki/Microsoft_Edge#New_Edge_release_history
 // MAX_INT (0x7FFFFFFF, or -1) specifies that target is not supported.
 // [link]
-var MIN_EDGE_VERSION = 44;
+var MIN_EDGE_VERSION = 0x7FFFFFFF;
 
 // Specifies the oldest version of Chrome. E.g. pass -sMIN_CHROME_VERSION=58 to
 // drop support for Chrome 57 and older.
