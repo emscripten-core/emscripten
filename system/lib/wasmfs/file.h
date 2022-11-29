3a64fd404f0847bea5f29e95bd0cd20ebc77681d
Thomas Lively <tlively@google.com>
2022-11-29 15:52:56 -0600

    [WasmFS] Add virtual backend utilities (#18248)
    Add generic subclasses of DataFile, Directory, and Symlink that wrap other files
    and pass operations through to them. Use the new utilities in a rewrite of the
    ignore-case virtual backend that cleanly separates the underlying "real" file
    system from the virtual file system. This also fixes outstanding issues with the
    ignore case backend in which the original, non-normalized file names were not
    preserved.

--- system/lib/wasmfs/file.h
+++ system/lib/wasmfs/file.h
@@ -181,6 +181,10 @@ public:
       return 0;
     }
 
+    std::vector<Entry>& operator*() {
+      return *std::get_if<std::vector<Entry>>(this);
+    }
+
     std::vector<Entry>* operator->() {
       return std::get_if<std::vector<Entry>>(this);
     }
@@ -253,7 +257,7 @@ protected:
   //  1. Ensuring that all insert* and getChild calls returning a particular
   //     file return the same File object.
   //
-  //  2. Clearing the File's parent field in `removeChild`.
+  //  2. Clearing unlinked Files' parents in `removeChild` and `insertMove`.
   //
   //  3. Implementing `getName`, since it cannot be implemented in terms of the
   //     dcache.
--- system/lib/wasmfs/memory_backend.h
+++ system/lib/wasmfs/memory_backend.h
@@ -15,6 +15,7 @@
 #include <emscripten/threading.h>
 
 namespace wasmfs {
+
 // This class describes a file that lives in Wasm Memory.
 class MemoryDataFile : public DataFile {
   std::vector<uint8_t> buffer;
