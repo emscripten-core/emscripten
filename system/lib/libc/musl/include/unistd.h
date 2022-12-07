13d6863fed99bcad2d8861106cad5eb2fbf8bd95
Sam Clegg <sbc@chromium.org>
2022-12-07 11:19:09 -0800

    Don't define `_POSIX_THREADS` in single-threaded builds
    See https://github.com/WebAssembly/wasi-libc/issues/355

--- system/lib/libc/musl/include/unistd.h
+++ system/lib/libc/musl/include/unistd.h
@@ -258,7 +258,11 @@ pid_t gettid(void);
 
 #define _POSIX_VDISABLE         0
 
+#if defined(__EMSCRIPTEN__) && !defined(_REENTRANT) /* XXX Emscripten doesn't always support pthreads */
+#define _POSIX_THREADS          -1
+#else
 #define _POSIX_THREADS          _POSIX_VERSION
+#endif
 #define _POSIX_THREAD_PROCESS_SHARED _POSIX_VERSION
 #define _POSIX_THREAD_SAFE_FUNCTIONS _POSIX_VERSION
 #define _POSIX_THREAD_ATTR_STACKADDR _POSIX_VERSION
