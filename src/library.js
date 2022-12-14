82c9cd7b9d753d0df669b7540746cbd4519ba335
Sam Clegg <sbc@chromium.org>
2022-12-14 14:55:27 -0800

    Don't implicitly exit the main thread in PROXY_TO_PTHREAD mode (#18372)
    maybeExit should never exit when its run from the main thread in
    PROXY_TO_PTHREAD mode.  In this mode we always wait for the proxied main
    thread to signal the exit of the program.
    
    Without this change the last three lines of the test output are never
    printed because the whole program exits after running the callback.

--- src/library.js
+++ src/library.js
@@ -3543,6 +3543,11 @@ mergeInto(LibraryManager.library, {
   ],
   $maybeExit: function() {
 #if EXIT_RUNTIME
+#if PROXY_TO_PTHREAD
+    // In PROXY_TO_PTHREAD mode the main thread never implicitly exits, but
+    // waits for the proxied main function to exit.
+    if (!ENVIRONMENT_IS_PTHREAD) return;
+#endif
 #if RUNTIME_DEBUG
     dbg('maybeExit: user callback done: runtimeKeepaliveCounter=' + runtimeKeepaliveCounter);
 #endif
