53568c0107a334cc26e5edc230a280e7705c00fe
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-01-09 23:04:11 +0100

    Ensure compatibility with Safari for multi-environment ES6 builds (#18482)
    Use a different parameter name to prevent the variable from being
    incorrectly hoisted in JavaScriptCore-based engines (such as Safari).
    See: https://bugs.webkit.org/show_bug.cgi?id=223533.
    
    Regressed for multi-environment ES6 builds since commit ce4c405
    (Emscripten 3.1.27). `-sENVIRONMENT=web` is not affected, as that
    would avoid the emit of this async function altogether.
    
    Resolves: #18357.

--- emcc.py
+++ emcc.py
@@ -3769,9 +3769,13 @@ def modularize():
   if not settings.EXPORT_READY_PROMISE:
     return_value = '{}'
 
+  # TODO: Remove when https://bugs.webkit.org/show_bug.cgi?id=223533 is resolved.
+  if async_emit != '' and settings.EXPORT_NAME == 'config':
+    diagnostics.warning('emcc', 'EXPORT_NAME should not be named "config" when targeting Safari')
+
   src = '''
-%(maybe_async)sfunction(%(EXPORT_NAME)s) {
-  %(EXPORT_NAME)s = %(EXPORT_NAME)s || {};
+%(maybe_async)sfunction(config) {
+  var %(EXPORT_NAME)s = config || {};
 
 %(src)s
 
