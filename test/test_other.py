fc5d61b159d2549ed2a278182d5ac538429d856a
Sam Clegg <sbc@chromium.org>
2023-01-23 11:11:46 -0800

    Re-enable test_metadce_hello_main_module_2. NFC (#18581)

--- test/test_other.py
+++ test/test_other.py
@@ -7703,8 +7703,7 @@ int main() {
     # we don't metadce with linkable code! other modules may want stuff
     # TODO(sbc): Investivate why the number of exports is order of magnitude
     # larger for wasm backend.
-    # Disabled while llvm change rolls in
-    #'main_module_2': (['-O3', '-sMAIN_MODULE=2'], [], []), # noqa
+    'main_module_2': (['-O3', '-sMAIN_MODULE=2'], [], []), # noqa
   })
   def test_metadce_hello(self, *args):
     self.run_metadce_test('hello_world.cpp', *args)
