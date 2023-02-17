5415a72cd0efde90dfc1deb2283928a4f73691e2
Thomas Lively <tlively@google.com>
2023-02-16 16:31:05 -0800

    [Proxying] Add `emscripten_proxy_callback_with_ctx`
    This new proxying function is like `emscripten_proxy_callback`, but supports
    proxying of asynchronous work just like `emscripten_proxy_sync_with_ctx`. It
    uses the existing `em_proxying_ctx` type and `emscripten_proxy_finish` function
    to mark the work done, and that type and function are internally augmented to
    handle both synchronous and callback-based proxying. `emscripten_proxy_callback`
    is reimplemented to share most of its implementation with
    `emscripten_proxy_callback_with_ctx`, but it does break the abstraction slightly
    to avoid having to make an extra allocation.

--- test/pthread/test_pthread_proxying_canceled_work.c
+++ test/pthread/test_pthread_proxying_canceled_work.c
@@ -234,7 +234,10 @@ void test_proxy_callback_then_exit() {
   pthread_join(info.proxier, NULL);
 }
 
+enum proxy_kind { SYNC, CALLBACK };
+
 struct in_progress_state {
+  enum proxy_kind kind;
   int pattern_index;
   int proxier_index;
   _Atomic int running_count;
@@ -244,9 +247,10 @@ struct in_progress_state {
   _Atomic int ctx_count;
 };
 
-struct state_index {
+struct worker_args {
   int index;
   struct in_progress_state* state;
+  int* ret;
 };
 
 // The patterns of work completion to test so we sufficiently exercise the
@@ -260,7 +264,7 @@ int patterns[][5] = {{1, 2, 3, 4, 5},
                      {4, 2, 3, 0, 1}};
 
 void receive_ctx(em_proxying_ctx* ctx, void* arg) {
-  struct state_index* args = arg;
+  struct worker_args* args = arg;
   args->state->ctxs[args->index] = ctx;
   args->state->ctx_count++;
 }
@@ -285,14 +289,43 @@ void* in_progress_worker(void* arg) {
   return NULL;
 }
 
+void callback_ok(void* arg) {
+  struct worker_args* args = arg;
+  *args->ret = 1;
+}
+
+void callback_cancel(void* arg) {
+  struct worker_args* args = arg;
+  *args->ret = 0;
+}
+
 void* in_progress_proxier(void* arg) {
   struct in_progress_state* state = arg;
   int index = state->proxier_index;
   state->running_count++;
 
-  struct state_index proxy_args = {index, state};
-  int ret = emscripten_proxy_sync_with_ctx(
-    queue, state->worker, receive_ctx, &proxy_args);
+  int ret = -1;
+  struct worker_args worker_args = {index, state, &ret};
+  if (state->kind == SYNC) {
+    // Synchronously wait for the work to be completed or canceled.
+    ret = emscripten_proxy_sync_with_ctx(
+      queue, state->worker, receive_ctx, &worker_args);
+  } else if (state->kind == CALLBACK) {
+    // Spin until we execute a callback telling us whether the work was
+    // completed or canceled.
+    int proxied = emscripten_proxy_callback_with_ctx(queue,
+                                                     state->worker,
+                                                     receive_ctx,
+                                                     callback_ok,
+                                                     callback_cancel,
+                                                     &worker_args);
+    assert(proxied == 1);
+    while (ret == -1) {
+      // TODO: Add a way to wait for work to be executed.
+      emscripten_proxy_execute_queue(queue);
+    }
+  }
+
   // The expected result value depends on the pattern we are executing.
   assert(ret == (0 != patterns[state->pattern_index][index]));
   return NULL;
@@ -304,33 +337,35 @@ void test_cancel_in_progress() {
   int num_patterns = sizeof(patterns) / sizeof(patterns[0]);
   struct in_progress_state state;
 
-  for (state.pattern_index = 0; state.pattern_index < num_patterns;
-       state.pattern_index++) {
-    state.running_count = 0;
-    state.ctx_count = 0;
+  for (state.kind = 0; state.kind <= CALLBACK; state.kind++) {
+    for (state.pattern_index = 0; state.pattern_index < num_patterns;
+         state.pattern_index++) {
+      state.running_count = 0;
+      state.ctx_count = 0;
 
-    printf("checking pattern %d\n", state.pattern_index);
+      printf("checking pattern %d\n", state.pattern_index);
 
-    // Spawn the worker thread.
-    pthread_create(&state.worker, NULL, in_progress_worker, &state);
+      // Spawn the worker thread.
+      pthread_create(&state.worker, NULL, in_progress_worker, &state);
 
-    // Spawn the proxier threads.
-    for (state.proxier_index = 0; state.proxier_index < 5;
-         state.proxier_index++) {
-      pthread_create(&state.proxiers[state.proxier_index],
-                     NULL,
-                     in_progress_proxier,
-                     &state);
-      // Wait for the new proxier to start running so it gets the right index.
-      while (state.running_count == state.proxier_index) {
+      // Spawn the proxier threads.
+      for (state.proxier_index = 0; state.proxier_index < 5;
+           state.proxier_index++) {
+        pthread_create(&state.proxiers[state.proxier_index],
+                       NULL,
+                       in_progress_proxier,
+                       &state);
+        // Wait for the new proxier to start running so it gets the right index.
+        while (state.running_count == state.proxier_index) {
+        }
       }
-    }
 
-    // Wait for all the threads to finish.
-    for (int i = 0; i < 5; i++) {
-      pthread_join(state.proxiers[i], NULL);
+      // Wait for all the threads to finish.
+      for (int i = 0; i < 5; i++) {
+        pthread_join(state.proxiers[i], NULL);
+      }
+      pthread_join(state.worker, NULL);
     }
-    pthread_join(state.worker, NULL);
   }
 }
 
