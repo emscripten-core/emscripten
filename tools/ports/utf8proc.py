08145e23da55b0f0db26f47de4bfd91b4f84fd2e
Tim Babb <trbabb@gmail.com>
2023-02-28 00:57:07 -0800

    pass flake8

--- tools/ports/utf8proc.py
+++ tools/ports/utf8proc.py
@@ -8,8 +8,9 @@ import shutil
 import logging
 
 VERSION = '2.8.0'
-HASH    = '10b99739167bbcf3fec55446f0c54b4ddf04ad1403b52aeca26de937f95615d6f1ea880b031a4457559c345ea3c0bcff9af87f27b9f8cea98dc5444e8146962a'
+HASH = '10b99739167bbcf3fec55446f0c54b4ddf04ad1403b52aeca26de937f95615d6f1ea880b031a4457559c345ea3c0bcff9af87f27b9f8cea98dc5444e8146962a'
 PROJECT = 'utf8proc'
+
 
 def needed(settings):
   return settings.USE_UTF8PROC
@@ -24,16 +25,16 @@ def get(ports, settings, shared):
     def create(final):
         logging.info(f'building port: {PROJECT}')
         ports.clear_project_build(PROJECT)
-        
+
         source_path = os.path.join(ports.get_dir(), PROJECT, f'{PROJECT}-{VERSION}')
         dest_path = os.path.join(ports.get_build_dir(), PROJECT)
-        
+
         shutil.rmtree(dest_path, ignore_errors=True)
         shutil.copytree(source_path, dest_path)
-        
+
         ports.install_headers(dest_path)
         ports.build_port(dest_path, final, PROJECT, srcs=['utf8proc.c'])
-    
+
     return [shared.cache.get_lib(f'lib{PROJECT}.a', create, what='port')]
 
 
