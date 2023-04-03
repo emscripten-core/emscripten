f3c4c2ac0ab0630a18bd3c262b0692919d8f6da3
Sam Clegg <sbc@chromium.org>
2023-04-03 13:27:34 -0700

    [Wasm64] Get test_anisotropic with MEMORY64 (#18971)
    This required fixing some basic SDL and OpenGL operations.

--- src/library_glemu.js
+++ src/library_glemu.js
@@ -417,8 +417,7 @@ var LibraryGLEmulation = {
                    (GL.currentContext.compressionExt ? ' GL_ARB_texture_compression GL_EXT_texture_compression_s3tc' : '') +
                    (GL.currentContext.anisotropicExt ? ' GL_EXT_texture_filter_anisotropic' : '')
             );
-            GL.stringCache[name_] = ret;
-            return ret;
+            return GL.stringCache[name_] = {{{ to64('ret') }}};
         }
         return glGetString(name_);
       };
@@ -3672,6 +3671,7 @@ var LibraryGLEmulation = {
     GLImmediate.matrixLib.mat4.set({{{ makeHEAPView('F64', 'matrix', 'matrix+' + (16*8)) }}}, GLImmediate.matrix[GLImmediate.currentMatrix]);
   },
 
+  glLoadMatrixf__sig: 'vp',
   glLoadMatrixf: function(matrix) {
 #if GL_DEBUG
     if (GL.debug) dbg('glLoadMatrixf receiving: ' + Array.prototype.slice.call(HEAPF32.subarray(matrix >> 2, (matrix >> 2) + 16)));
