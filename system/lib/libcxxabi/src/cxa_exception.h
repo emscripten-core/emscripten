emscripten-core/emscripten

commit df3a308e7c54462a99c9e6946be4d5d4b9dbae39

Move parts of emscripten exception handling to native code. NFC (#16627)
Co-authored-by: Ingvar Stepanyan <me@rreverser.com>

system/lib/libcxxabi/src/cxa_exception_emscripten.cpp
@@ -1,21 +1,30 @@
-//===------------------- cxa_exception_emscripten.cpp ---------------------===//
+//===-------------------- cxa_exception_emscripten.cpp --------------------===//
 //
-// This code contains Emscripten specific code for exception handling.
-// Emscripten has two modes of exception handling: Emscripten EH, which uses JS
-// glue code, and Wasm EH, which uses the new Wasm exception handling proposal
-// and meant to be faster. Code for different modes is demarcated with
-// '__USING_EMSCRIPTEN_EXCEPTIONS__' and '__USING_WASM_EXCEPTIONS__'.
+//  Most code in the file is directly copied from cxa_exception.cpp.
+//  TODO(sbc): consider merging them
+//
+//  Notable changes:
+ //   __cxa_allocate_exception doesn't add get_cxa_exception_offset
+//    __cxa_decrement_exception_refcount dosn't call the destructor if rethrown
+//  Both of these changes are mirrored from the historical JS implemenation of
+//  thse functions.
 //
 //===----------------------------------------------------------------------===//
 
+#include "cxxabi.h"
 #include "cxa_exception.h"
-#include "private_typeinfo.h"
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
+#include "include/atomic_support.h"
+#include "fallback_malloc.h"
+#include "stdio.h"
+#include "assert.h"
 
-#if !defined(__USING_WASM_EXCEPTIONS__)
+// Define to enable extra debugging on stderr.
+#if EXCEPTIONS_DEBUG
+#define DEBUG printf
+#else
+#define DEBUG(...)
+#endif
+
 // Until recently, Rust's `rust_eh_personality` for emscripten referred to this
 // symbol. If Emscripten doesn't provide it, there will be errors when linking
 // rust. The rust personality function is never called so we can just abort.
@@ -30,121 +39,116 @@ __gxx_personality_v0(int version,
                      _Unwind_Context* context) {
     abort();
 }
-#endif // !defined(__USING_WASM_EXCEPTIONS__)
 
-using namespace __cxxabiv1;
+namespace __cxxabiv1 {
 
-//  Some utility routines are copied from cxa_exception.cpp
-static inline __cxa_exception*
-cxa_exception_from_thrown_object(void* thrown_object) {
-  return static_cast<__cxa_exception*>(thrown_object) - 1;
+//  Utility routines
+static
+inline
+__cxa_exception*
+cxa_exception_from_thrown_object(void* thrown_object)
+{
+    DEBUG("cxa_exception_from_thrown_object %p -> %p\n",
+          thrown_object, static_cast<__cxa_exception*>(thrown_object) - 1);
+    return static_cast<__cxa_exception*>(thrown_object) - 1;
 }
 
 // Note:  This is never called when exception_header is masquerading as a
 //        __cxa_dependent_exception.
-static inline void*
-thrown_object_from_cxa_exception(__cxa_exception* exception_header) {
-  return static_cast<void*>(exception_header + 1);
+static
+inline
+void*
+thrown_object_from_cxa_exception(__cxa_exception* exception_header)
+{
+    DEBUG("thrown_object_from_cxa_exception %p -> %p\n",
+          exception_header, static_cast<void*>(exception_header + 1));
+    return static_cast<void*>(exception_header + 1);
 }
 
-#if defined(__USING_EMSCRIPTEN_EXCEPTIONS__) ||                                \
-  defined(__USING_WASM_EXCEPTIONS__)
-
-//  Get the exception object from the unwind pointer.
-//  Relies on the structure layout, where the unwind pointer is right in
-//  front of the user's exception object
-static inline __cxa_exception* cxa_exception_from_unwind_exception(
-  _Unwind_Exception* unwind_exception) {
-  return cxa_exception_from_thrown_object(unwind_exception + 1);
+// Round s up to next multiple of a.
+static inline
+size_t aligned_allocation_size(size_t s, size_t a) {
+    return (s + a - 1) & ~(a - 1);
 }
 
-static inline void* thrown_object_from_unwind_exception(
-  _Unwind_Exception* unwind_exception) {
-  __cxa_exception* exception_header =
-    cxa_exception_from_unwind_exception(unwind_exception);
-  return thrown_object_from_cxa_exception(exception_header);
+static inline
+size_t cxa_exception_size_from_exception_thrown_size(size_t size) {
+    return aligned_allocation_size(size + sizeof (__cxa_exception),
+                                   alignof(__cxa_exception));
 }
 
 extern "C" {
 
-void* __thrown_object_from_unwind_exception(
-  _Unwind_Exception* unwind_exception) {
-  return thrown_object_from_unwind_exception(unwind_exception);
+//  Allocate a __cxa_exception object, and zero-fill it.
+//  Reserve "thrown_size" bytes on the end for the user's exception
+//  object. Zero-fill the object. If memory can't be allocated, call
+//  std::terminate. Return a pointer to the memory to be used for the
+//  user's exception object.
+void *__cxa_allocate_exception(size_t thrown_size) _NOEXCEPT {
+    size_t actual_size = cxa_exception_size_from_exception_thrown_size(thrown_size);
+
+    char *raw_buffer =
+        (char *)__aligned_malloc_with_fallback(actual_size);
+    if (NULL == raw_buffer)
+        std::terminate();
+    __cxa_exception *exception_header =
+        static_cast<__cxa_exception *>((void *)(raw_buffer));
+    ::memset(exception_header, 0, actual_size);
+    return thrown_object_from_cxa_exception(exception_header);
 }
 
-// Given a thrown_object, puts the information about its type and message into
-// 'type' and 'message' output parameters. 'type' will contain the string
-// representation of the type of the exception, e.g., 'int'. 'message' will
-// contain the result of 'std::exception::what()' method if the type of the
-// exception is a subclass of std::exception; otherwise it will be NULL. The
-// caller is responsible for freeing 'type' buffer and also 'message' buffer, if
-// it is not NULL.
-void __get_exception_message(void* thrown_object, char** type, char** message) {
-  __cxa_exception* exception_header =
-    cxa_exception_from_thrown_object(thrown_object);
-  const __shim_type_info* thrown_type =
-    static_cast<const __shim_type_info*>(exception_header->exceptionType);
-  const char* type_name = thrown_type->name();
 
-  int status = 0;
-  char* demangled_buf = __cxa_demangle(type_name, 0, 0, &status);
-  if (status == 0 && demangled_buf) {
-    *type = demangled_buf;
-  } else {
-    if (demangled_buf) {
-      free(demangled_buf);
-    }
-    *type = (char*)malloc(strlen(type_name) + 1);
-    strcpy(*type, type_name);
-  }
-
-  *message = NULL;
-  const __shim_type_info* catch_type =
-    static_cast<const __shim_type_info*>(&typeid(std::exception));
-  int can_catch = catch_type->can_catch(thrown_type, thrown_object);
-  if (can_catch) {
-    const char* what =
-      static_cast<const std::exception*>(thrown_object)->what();
-    *message = (char*)malloc(strlen(what) + 1);
-    strcpy(*message, what);
-  }
-}
-
-// Returns a message saying that execution was terminated due to an exception.
-// This message is freshly malloc'd and should be freed.
-char* __get_exception_terminate_message(void* thrown_object) {
-  char* type;
-  char* message;
-  __get_exception_message(thrown_object, &type, &message);
-  char* result;
-  if (message != NULL) {
-    asprintf(
-      &result, "terminating with uncaught exception %s: %s", type, message);
-    free(message);
-  } else {
-    asprintf(&result, "terminating with uncaught exception of type %s", type);
-  }
-  free(type);
-  return result;
-}
-}
-
-#endif // __USING_EMSCRIPTEN_EXCEPTIONS__ || __USING_WASM_EXCEPTIONS__
-
-#ifndef __USING_WASM_EXCEPTIONS__
-
-namespace __cxxabiv1 {
-
-void* __cxa_allocate_exception(size_t size) _NOEXCEPT {
-  // Thrown object is prepended by exception metadata block
-  __cxa_exception* ex = (__cxa_exception*)malloc(size + sizeof(__cxa_exception));
-  return thrown_object_from_cxa_exception(ex);
-}
-
+//  Free a __cxa_exception object allocated with __cxa_allocate_exception.
 void __cxa_free_exception(void *thrown_object) _NOEXCEPT {
-  free(cxa_exception_from_thrown_object(thrown_object));
+    // Compute the size of the padding before the header.
+    char *raw_buffer =
+        ((char *)cxa_exception_from_thrown_object(thrown_object));
+    __aligned_free_with_fallback((void *)raw_buffer);
 }
 
+/*
+    If thrown_object is not null, atomically increment the referenceCount field
+    of the __cxa_exception header associated with the thrown object referred to
+    by thrown_object.
+
+    Requires:  If thrown_object is not NULL, it is a native exception.
+*/
+void
+__cxa_increment_exception_refcount(void *thrown_object) _NOEXCEPT {
+    if (thrown_object != NULL )
+    {
+        __cxa_exception* exception_header = cxa_exception_from_thrown_object(thrown_object);
+        DEBUG("INC: %p refcnt=%zu\n", thrown_object, exception_header->referenceCount);
+        std::__libcpp_atomic_add(&exception_header->referenceCount, size_t(1));
+    }
 }
 
-#endif // !__USING_WASM_EXCEPTIONS__
+/*
+    If thrown_object is not null, atomically decrement the referenceCount field
+    of the __cxa_exception header associated with the thrown object referred to
+    by thrown_object.  If the referenceCount drops to zero, destroy and
+    deallocate the exception.
+
+    Requires:  If thrown_object is not NULL, it is a native exception.
+*/
+_LIBCXXABI_NO_CFI
+void __cxa_decrement_exception_refcount(void *thrown_object) _NOEXCEPT {
+    if (thrown_object != NULL )
+    {
+        __cxa_exception* exception_header = cxa_exception_from_thrown_object(thrown_object);
+        DEBUG("DEC: %p refcnt=%zu rethrown=%d\n", thrown_object,
+              exception_header->referenceCount, exception_header->rethrown);
+        assert(exception_header->referenceCount > 0);
+        if (std::__libcpp_atomic_add(&exception_header->referenceCount, size_t(-1)) == 0 && !exception_header->rethrown)
+        {
+            DEBUG("DEL: %p\n", thrown_object);
+            if (NULL != exception_header->exceptionDestructor)
+                exception_header->exceptionDestructor(thrown_object);
+            __cxa_free_exception(thrown_object);
+        }
+    }
+}
+
+}  // extern "C"
+
+}  // abi

emcc.py
@@ -2785,6 +2785,13 @@ def phase_linker_setup(options, state, newargs):
       '__cxa_is_pointer_type',
       '__cxa_can_catch',
 
+      # __cxa_begin_catch depends on this but we can't use deps info in this
+      # case because that only works for user-level code, and __cxa_begin_catch
+      # can be used by the standard library.
+      '__cxa_increment_exception_refcount',
+      # Same for __cxa_end_catch
+      '__cxa_decrement_exception_refcount',
+
       # Emscripten exception handling can generate invoke calls, and they call
       # setThrew(). We cannot handle this using deps_info as the invokes are not
       # emitted because of library function usage, but by codegen itself.

src/library_exceptions.js
@@ -23,11 +23,9 @@ var LibraryExceptions = {
   //
   // excPtr - Thrown object pointer to wrap. Metadata pointer is calculated from it.
   $ExceptionInfo__docs: '/** @constructor */',
-  $ExceptionInfo__deps: ['__cxa_is_pointer_type',
 #if EXCEPTION_DEBUG
-    '$ptrToString',
+  $ExceptionInfo__deps: ['$ptrToString'],
 #endif
-  ],
   $ExceptionInfo: function(excPtr) {
     this.excPtr = excPtr;
     this.ptr = excPtr - {{{ C_STRUCTS.__cxa_exception.__size__ }}};
@@ -46,10 +44,6 @@ var LibraryExceptions = {
 
     this.get_destructor = function() {
       return {{{ makeGetValue('this.ptr', C_STRUCTS.__cxa_exception.exceptionDestructor, '*') }}};
-    };
-
-    this.set_refcount = function(refcount) {
-      {{{ makeSetValue('this.ptr', C_STRUCTS.__cxa_exception.referenceCount, 'refcount', 'i32') }}};
     };
 
     this.set_caught = function (caught) {
@@ -78,33 +72,7 @@ var LibraryExceptions = {
       this.set_adjusted_ptr(0);
       this.set_type(type);
       this.set_destructor(destructor);
-      this.set_refcount(0);
-      this.set_caught(false);
-      this.set_rethrown(false);
     }
-
-    this.add_ref = function() {
-#if SHARED_MEMORY
-      Atomics.add(HEAP32, (this.ptr + {{{ C_STRUCTS.__cxa_exception.referenceCount }}}) >> 2, 1);
-#else
-      var value = {{{ makeGetValue('this.ptr', C_STRUCTS.__cxa_exception.referenceCount, 'i32') }}};
-      {{{ makeSetValue('this.ptr', C_STRUCTS.__cxa_exception.referenceCount, 'value + 1', 'i32') }}};
-#endif
-    };
-
-    // Returns true if last reference released.
-    this.release_ref = function() {
-#if SHARED_MEMORY
-      var prev = Atomics.sub(HEAP32, (this.ptr + {{{ C_STRUCTS.__cxa_exception.referenceCount }}}) >> 2, 1);
-#else
-      var prev = {{{ makeGetValue('this.ptr', C_STRUCTS.__cxa_exception.referenceCount, 'i32') }}};
-      {{{ makeSetValue('this.ptr', C_STRUCTS.__cxa_exception.referenceCount, 'prev - 1', 'i32') }}};
-#endif
-#if ASSERTIONS
-      assert(prev > 0);
-#endif
-      return prev === 1;
-    };
 
     this.set_adjusted_ptr = function(adjustedPtr) {
       {{{ makeSetValue('this.ptr', C_STRUCTS.__cxa_exception.adjustedPtr, 'adjustedPtr', '*') }}};
@@ -129,52 +97,6 @@ var LibraryExceptions = {
       if (adjusted !== 0) return adjusted;
       return this.excPtr;
     };
-  },
-
-  $exception_addRef: function (info) {
-#if EXCEPTION_DEBUG
-    dbg('exception_addRef ' + ptrToString(info.excPtr));
-#endif
-    info.add_ref();
-  },
-
-  $exception_decRef__deps: ['__cxa_free_exception'
-#if EXCEPTION_DEBUG
-    , '$exceptionLast', '$exceptionCaught'
-#endif
-  ],
-  $exception_decRef: function(info) {
-#if EXCEPTION_DEBUG
-    dbg('exception_decRef ' + ptrToString(info.excPtr));
-#endif
-    // A rethrown exception can reach refcount 0; it must not be discarded
-    // Its next handler will clear the rethrown flag and addRef it, prior to
-    // final decRef and destruction here
-    if (info.release_ref() && !info.get_rethrown()) {
-      var destructor = info.get_destructor();
-      if (destructor) {
-        // In Wasm, destructors return 'this' as in ARM
-        {{{ makeDynCall('pp', 'destructor') }}}(info.excPtr);
-      }
-      ___cxa_free_exception(info.excPtr);
-#if EXCEPTION_DEBUG
-      dbg('decref freeing exception ' + [ptrToString(info.excPtr), exceptionLast, 'stack', exceptionCaught]);
-#endif
-    }
-  },
-
-  __cxa_increment_exception_refcount__deps: ['$exception_addRef', '$ExceptionInfo'],
-  __cxa_increment_exception_refcount__sig: 'vp',
-  __cxa_increment_exception_refcount: function(ptr) {
-    if (!ptr) return;
-    exception_addRef(new ExceptionInfo(ptr));
-  },
-
-  __cxa_decrement_exception_refcount__deps: ['$exception_decRef', '$ExceptionInfo'],
-  __cxa_decrement_exception_refcount__sig: 'vp',
-  __cxa_decrement_exception_refcount: function(ptr) {
-    if (!ptr) return;
-    exception_decRef(new ExceptionInfo(ptr));
   },
 
   // Here, we throw an exception after recording a couple of values that we need to remember
@@ -224,7 +146,8 @@ var LibraryExceptions = {
     return type;
   },
 
-  __cxa_begin_catch__deps: ['$exceptionCaught', '$exception_addRef', '$uncaughtExceptionCount'],
+  __cxa_begin_catch__deps: ['$exceptionCaught', '__cxa_increment_exception_refcount',
+                            '$uncaughtExceptionCount'],
   __cxa_begin_catch__sig: 'pp',
   __cxa_begin_catch: function(ptr) {
     var info = new ExceptionInfo(ptr);
@@ -237,7 +160,7 @@ var LibraryExceptions = {
 #if EXCEPTION_DEBUG
     dbg('__cxa_begin_catch ' + [ptrToString(ptr), 'stack', exceptionCaught]);
 #endif
-    exception_addRef(info);
+    ___cxa_increment_exception_refcount(info.excPtr);
     return info.get_exception_ptr();
   },
 
@@ -245,7 +168,7 @@ var LibraryExceptions = {
   // and free the exception. Note that if the dynCall on the destructor fails
   // due to calling apply on undefined, that means that the destructor is
   // an invalid index into the FUNCTION_TABLE, so something has gone wrong.
-  __cxa_end_catch__deps: ['$exceptionCaught', '$exceptionLast', '$exception_decRef'],
+  __cxa_end_catch__deps: ['$exceptionCaught', '$exceptionLast', '__cxa_decrement_exception_refcount', 'setThrew'],
   __cxa_end_catch__sig: 'v',
   __cxa_end_catch: function() {
     // Clear state flag.
@@ -259,17 +182,18 @@ var LibraryExceptions = {
 #if EXCEPTION_DEBUG
     dbg('__cxa_end_catch popped ' + [info, exceptionLast, 'stack', exceptionCaught]);
 #endif
-    exception_decRef(info);
+    ___cxa_decrement_exception_refcount(info.excPtr);
     exceptionLast = 0; // XXX in decRef?
   },
 
   __cxa_get_exception_ptr__deps: ['$ExceptionInfo'],
   __cxa_get_exception_ptr__sig: 'pp',
   __cxa_get_exception_ptr: function(ptr) {
+    var rtn = new ExceptionInfo(ptr).get_exception_ptr();
 #if EXCEPTION_DEBUG
-    dbg('__cxa_get_exception_ptr ' + ptrToString(ptr));
+    err('__cxa_get_exception_ptr ' + ptrToString(ptr) + ' -> ' + ptrToString(rtn));
 #endif
-    return new ExceptionInfo(ptr).get_exception_ptr();
+    return rtn;
   },
 
   __cxa_uncaught_exceptions__deps: ['$uncaughtExceptionCount'],
@@ -285,13 +209,13 @@ var LibraryExceptions = {
     throw exception;
   },
 
-  __cxa_current_primary_exception__deps: ['$exceptionCaught', '$exception_addRef'],
+  __cxa_current_primary_exception__deps: ['$exceptionCaught', '__cxa_increment_exception_refcount'],
   __cxa_current_primary_exception: function() {
     if (!exceptionCaught.length) {
       return 0;
     }
     var info = exceptionCaught[exceptionCaught.length - 1];
-    exception_addRef(info);
+    ___cxa_increment_exception_refcount(info.excPtr);
     return info.excPtr;
   },
 

src/library_exceptions_stub.js
@@ -7,10 +7,6 @@
 var LibraryExceptions = {};
 
 [
-  '__cxa_allocate_exception',
-  '__cxa_free_exception',
-  '__cxa_increment_exception_refcount',
-  '__cxa_decrement_exception_refcount',
   '__cxa_throw',
   '__cxa_rethrow',
   'llvm_eh_typeid_for',

system/lib/libcxxabi/src/cxa_exception_js_utils.cpp
@@ -1,0 +1,105 @@
+#include "cxxabi.h"
+
+#include "cxa_exception.h"
+#include "private_typeinfo.h"
+#include <stdio.h>
+// #include <stdint.h>
+// #include <stdlib.h>
+#include <string.h>
+
+namespace __cxxabiv1 {
+
+//  Utility routines
+static
+inline
+__cxa_exception*
+cxa_exception_from_thrown_object(void* thrown_object)
+{
+    return static_cast<__cxa_exception*>(thrown_object) - 1;
+}
+
+// Note:  This is never called when exception_header is masquerading as a
+//        __cxa_dependent_exception.
+static
+inline
+void*
+thrown_object_from_cxa_exception(__cxa_exception* exception_header)
+{
+    return static_cast<void*>(exception_header + 1);
+}
+
+//  Get the exception object from the unwind pointer.
+//  Relies on the structure layout, where the unwind pointer is right in
+//  front of the user's exception object
+static inline __cxa_exception* cxa_exception_from_unwind_exception(
+  _Unwind_Exception* unwind_exception) {
+  return cxa_exception_from_thrown_object(unwind_exception + 1);
+}
+
+extern "C" {
+
+void* __thrown_object_from_unwind_exception(
+  _Unwind_Exception* unwind_exception) {
+  __cxa_exception* exception_header =
+    cxa_exception_from_unwind_exception(unwind_exception);
+  return thrown_object_from_cxa_exception(exception_header);
+}
+
+// Given a thrown_object, puts the information about its type and message into
+// 'type' and 'message' output parameters. 'type' will contain the string
+// representation of the type of the exception, e.g., 'int'. 'message' will
+// contain the result of 'std::exception::what()' method if the type of the
+// exception is a subclass of std::exception; otherwise it will be NULL. The
+// caller is responsible for freeing 'type' buffer and also 'message' buffer, if
+// it is not NULL.
+void __get_exception_message(void* thrown_object, char** type, char** message) {
+  __cxa_exception* exception_header =
+    cxa_exception_from_thrown_object(thrown_object);
+  const __shim_type_info* thrown_type =
+    static_cast<const __shim_type_info*>(exception_header->exceptionType);
+  const char* type_name = thrown_type->name();
+
+  int status = 0;
+  char* demangled_buf = __cxa_demangle(type_name, 0, 0, &status);
+  if (status == 0 && demangled_buf) {
+    *type = demangled_buf;
+  } else {
+    if (demangled_buf) {
+      free(demangled_buf);
+    }
+    *type = (char*)malloc(strlen(type_name) + 1);
+    strcpy(*type, type_name);
+  }
+
+  *message = NULL;
+  const __shim_type_info* catch_type =
+    static_cast<const __shim_type_info*>(&typeid(std::exception));
+  int can_catch = catch_type->can_catch(thrown_type, thrown_object);
+  if (can_catch) {
+    const char* what =
+      static_cast<const std::exception*>(thrown_object)->what();
+    *message = (char*)malloc(strlen(what) + 1);
+    strcpy(*message, what);
+  }
+}
+
+// Returns a message saying that execution was terminated due to an exception.
+// This message is freshly malloc'd and should be freed.
+char* __get_exception_terminate_message(void* thrown_object) {
+  char* type;
+  char* message;
+  __get_exception_message(thrown_object, &type, &message);
+  char* result;
+  if (message != NULL) {
+    asprintf(
+      &result, "terminating with uncaught exception %s: %s", type, message);
+    free(message);
+  } else {
+    asprintf(&result, "terminating with uncaught exception of type %s", type);
+  }
+  free(type);
+  return result;
+}
+} // extern "C"
+
+} // namespace __cxxabiv1

system/lib/libcxxabi/src/cxa_noexception.cpp
@@ -49,6 +49,17 @@ __cxa_uncaught_exception() throw() { return false; }
 unsigned int
 __cxa_uncaught_exceptions() throw() { return 0; }
 
+#if __EMSCRIPTEN__
+// Under emscripten this code is also linked when building when
+// DISABLE_EXCEPTION_CATCHING is set but DISABLE_EXCEPTION_THROWING is not.
+// TODO(sbc): Perhaps just call std::terminate here. It could
+// just be some test code that needs updating.
+void *__cxa_allocate_exception(size_t thrown_size) _NOEXCEPT {
+  char* allocation = (char*)malloc(thrown_size + sizeof(__cxa_exception));
+  return allocation + sizeof(__cxa_exception);
+}
+#endif
+
 }  // extern "C"
 
 // provide dummy implementations for the 'no exceptions' case.

system/lib/standalone/standalone.c
@@ -171,11 +171,6 @@ void __cxa_throw(void* ptr, void* type, void* destructor) {
   abort();
 }
 
-__attribute__((__weak__))
-void* __cxa_allocate_exception(size_t thrown_size) {
-  abort();
-}
-
 // WasmFS integration. We stub out file preloading and such, that are not
 // expected to work anyhow.
 

test/core/test_exceptions_primary.cpp
@@ -9,6 +9,7 @@
 #include <stdexcept>      // std::logic_error
 
 int main () {
+  std::cout << "in main\n";
   std::exception_ptr p;
   try {
      throw std::logic_error("some logic_error exception");   // throws
@@ -20,7 +21,7 @@ int main () {
   std::cout << "(after exception)\n";
 
   try {
-     std::rethrow_exception (p);
+     std::rethrow_exception(p);
   } catch (const std::exception& e) {
      std::cout << "exception caught: " << e.what() << '\n';
   }

commit 792093950dbdc5a3b9e93ef465d61a0600716684

Fix lto0.test_exceptions_allowed_uncaught failure
This test started failing after #16627.  Prior to that change the
exceptions destructors were called via the following code in JS:

```
   // In Wasm, destructors return 'this' as in ARM
   {{{ makeDynCall('pp', 'destructor') }}}(info.excPtr);
```

For some reason the LTO build produces the following for for the call
to `exception_header->exceptionDestructor(thrown_object)` in
`__cxa_decrement_exception_refcount`:

```
  call_indirect 0 (type 0)
```

Where as the normal non-LTO build produces:

```
  call 13 <invoke_ii>
```

Because invoke_ii goes via JS it uses the sloppy type checking and
doesn't trap, but `call_indirect` has strict type checking and so
does trap.

.circleci/config.yml
@@ -492,6 +492,7 @@ jobs:
           title: "core3+extras"
           test_targets: "
             lto2.test_dylink_syslibs_all
+            lto0.test_exceptions_allowed_uncaught
             core3
             core2g.test_externref
             corez.test_dylink_iostream

system/lib/libcxxabi/src/cxa_exception.h
@@ -24,7 +24,8 @@ namespace __cxxabiv1 {
 struct _LIBCXXABI_HIDDEN __cxa_exception {
   size_t referenceCount;
   std::type_info *exceptionType;
-  void (*exceptionDestructor)(void *);
+  // In wasm, destructors return 'this' as in ARM
+  void* (*exceptionDestructor)(void *);
   uint8_t caught;
   uint8_t rethrown;
   void *adjustedPtr;

system/lib/libcxxabi/src/cxa_exception_emscripten.cpp
@@ -20,7 +20,8 @@
 
 // Define to enable extra debugging on stderr.
 #if EXCEPTIONS_DEBUG
-#define DEBUG printf
+#include "emscripten/console.h"
+#define DEBUG _emscripten_errf
 #else
 #define DEBUG(...)
 #endif
@@ -48,7 +49,7 @@ inline
 __cxa_exception*
 cxa_exception_from_thrown_object(void* thrown_object)
 {
-    DEBUG("cxa_exception_from_thrown_object %p -> %p\n",
+    DEBUG("cxa_exception_from_thrown_object %p -> %p",
           thrown_object, static_cast<__cxa_exception*>(thrown_object) - 1);
     return static_cast<__cxa_exception*>(thrown_object) - 1;
 }
@@ -60,7 +61,7 @@ inline
 void*
 thrown_object_from_cxa_exception(__cxa_exception* exception_header)
 {
-    DEBUG("thrown_object_from_cxa_exception %p -> %p\n",
+    DEBUG("thrown_object_from_cxa_exception %p -> %p",
           exception_header, static_cast<void*>(exception_header + 1));
     return static_cast<void*>(exception_header + 1);
 }
@@ -118,7 +119,7 @@ __cxa_increment_exception_refcount(void *thrown_object) _NOEXCEPT {
     if (thrown_object != NULL )
     {
         __cxa_exception* exception_header = cxa_exception_from_thrown_object(thrown_object);
-        DEBUG("INC: %p refcnt=%zu\n", thrown_object, exception_header->referenceCount);
+        DEBUG("INC: %p refcnt=%zu", thrown_object, exception_header->referenceCount);
         std::__libcpp_atomic_add(&exception_header->referenceCount, size_t(1));
     }
 }
@@ -136,12 +137,12 @@ void __cxa_decrement_exception_refcount(void *thrown_object) _NOEXCEPT {
     if (thrown_object != NULL )
     {
         __cxa_exception* exception_header = cxa_exception_from_thrown_object(thrown_object);
-        DEBUG("DEC: %p refcnt=%zu rethrown=%d\n", thrown_object,
+        DEBUG("DEC: %p refcnt=%zu rethrown=%d", thrown_object,
               exception_header->referenceCount, exception_header->rethrown);
         assert(exception_header->referenceCount > 0);
         if (std::__libcpp_atomic_add(&exception_header->referenceCount, size_t(-1)) == 0 && !exception_header->rethrown)
         {
-            DEBUG("DEL: %p\n", thrown_object);
+            DEBUG("DEL: %p (dtor=%p)", thrown_object, exception_header->exceptionDestructor);
             if (NULL != exception_header->exceptionDestructor)
                 exception_header->exceptionDestructor(thrown_object);
             __cxa_free_exception(thrown_object);