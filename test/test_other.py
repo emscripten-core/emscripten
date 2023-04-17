7ffa57a0ef022f2af7613af6786babf46d9f10c9
Alon Zakai <azakai@google.com>
2023-04-17 12:37:19 -0700

    test more

--- test/test_other.py
+++ test/test_other.py
@@ -13192,6 +13192,9 @@ foo/version.txt
   def test_itimer_standlone(self):
     self.emcc_args += ['-sSTANDALONE_WASM']
     self.do_other_test('test_itimer_standalone.c')
+    for engine in config.WASM_ENGINES:
+      print('wasm engine', engine)
+      self.assertContained('done', self.run_js('test_itimer_standalone.wasm', engine))
 
   @node_pthreads
   @no_mac("Our Mac CI currently has too much contention to run this reliably")
