fcfc6e363d13f0d6c412f9bc96109c15740d9577
Henry Schreiner <henryschreineriii@gmail.com>
2023-05-16 13:12:11 -0400

    WIP: try removing all _PYTHON_SYSCONFIGDATA_NAME hiding
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- emdwp.bat
+++ emdwp.bat
@@ -10,9 +10,6 @@
 
 :: All env. vars specified in this file are to be local only to this script.
 @setlocal
-:: -E will not ignore _PYTHON_SYSCONFIGDATA_NAME an internal
-:: of cpython used in cross compilation via setup.py.
-@set _PYTHON_SYSCONFIGDATA_NAME=
 @set EM_PY=%EMSDK_PYTHON%
 @if "%EM_PY%"=="" (
   set EM_PY=python
