73cb813b1eac7752c8f775c54b6975e89d1c89f6
Alon Zakai <azakai@google.com>
2023-05-19 07:57:19 -0700

    simpler

--- system/lib/wasmfs/backends/noderawfs_backend.cpp
+++ system/lib/wasmfs/backends/noderawfs_backend.cpp
@@ -3,8 +3,6 @@
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
 
-#include <memory>
-
 #include "emscripten/wasmfs.h"
 
 namespace wasmfs {
