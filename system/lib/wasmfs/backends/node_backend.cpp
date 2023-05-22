367b20904b5c873beecf5af3885cd2ff2720d2b8
Sam Clegg <sbc@chromium.org>
2023-05-22 11:59:01 -0700

    Auto-generate signatures for wasmfs functions. NFC

--- system/lib/wasmfs/backends/node_backend.cpp
+++ system/lib/wasmfs/backends/node_backend.cpp
@@ -5,6 +5,7 @@
 
 #include <memory>
 
+#include "node_backend.h"
 #include "backend.h"
 #include "file.h"
 #include "support.h"
@@ -13,44 +14,6 @@
 namespace wasmfs {
 
 class NodeBackend;
-
-extern "C" {
-
-// Fill `entries` and return 0 or an error code.
-int _wasmfs_node_readdir(const char* path,
-                         std::vector<Directory::Entry>* entries);
-// Write `mode` and return 0 or an error code.
-int _wasmfs_node_get_mode(const char* path, mode_t* mode);
-
-// Write `size` and return 0 or an error code.
-int _wasmfs_node_stat_size(const char* path, uint32_t* size);
-int _wasmfs_node_fstat_size(int fd, uint32_t* size);
-
-// Create a new file system entry and return 0 or an error code.
-int _wasmfs_node_insert_file(const char* path, mode_t mode);
-int _wasmfs_node_insert_directory(const char* path, mode_t mode);
-
-// Unlink the given file and return 0 or an error code.
-int _wasmfs_node_unlink(const char* path);
-int _wasmfs_node_rmdir(const char* path);
-
-// Open the file and return the underlying file descriptor.
-[[nodiscard]] int _wasmfs_node_open(const char* path, const char* mode);
-
-// Close the underlying file descriptor.
-[[nodiscard]] int _wasmfs_node_close(int fd);
-
-// Read up to `size` bytes into `buf` from position `pos` in the file, writing
-// the number of bytes read to `nread`. Return 0 on success or an error code.
-int _wasmfs_node_read(
-  int fd, void* buf, uint32_t len, uint32_t pos, uint32_t* nread);
-
-// Write up to `size` bytes from `buf` at position `pos` in the file, writing
-// the number of bytes written to `nread`. Return 0 on success or an error code.
-int _wasmfs_node_write(
-  int fd, const void* buf, uint32_t len, uint32_t pos, uint32_t* nwritten);
-
-} // extern "C"
 
 // The state of a file on the underlying Node file system.
 class NodeState {
