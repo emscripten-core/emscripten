emscripten-core/emscripten

commit 008017bde42a4b891b0fa0175a1dbe000df99caf

Don't compile dynamic linking code into non-PIC version of libc. NFC (#18638)
Replaces #18637

src/library_dylink.js
@@ -240,12 +240,18 @@ var LibraryDylink = {
   _emscripten_dlopen_js__deps: [function() { error(dlopenMissingError); }],
   _dlsym_js__deps: [function() { error(dlopenMissingError); }],
   _dlsym_catchup_js__deps: [function() { error(dlopenMissingError); }],
+  dlopen__deps: [function() { error(dlopenMissingError); }],
+  _emscripten_dlopen__deps: [function() { error(dlopenMissingError); }],
+  __dlsym__deps: [function() { error(dlopenMissingError); }],
 #else
   $dlopenMissingError: `= ${dlopenMissingError}`,
   _dlopen_js__deps: ['$dlopenMissingError'],
   _emscripten_dlopen_js__deps: ['$dlopenMissingError'],
   _dlsym_js__deps: ['$dlopenMissingError'],
   _dlsym_catchup_js__deps: ['$dlopenMissingError'],
+  dlopen__deps: ['$dlopenMissingError'],
+  _emscripten_dlopen__deps: ['$dlopenMissingError'],
+  __dlsym__deps: ['$dlopenMissingError'],
 #endif
   _dlopen_js: function(handle) {
     abort(dlopenMissingError);
@@ -259,6 +265,16 @@ var LibraryDylink = {
   _dlsym_catchup_js: function(handle, symbolIndex) {
     abort(dlopenMissingError);
   },
+  dlopen: function(handle) {
+    abort(dlopenMissingError);
+  },
+  _emscripten_dlopen: function(handle, onsuccess, onerror, user_data) {
+    abort(dlopenMissingError);
+  },
+  __dlsym: function(handle, symbol) {
+    abort(dlopenMissingError);
+  },
+  _dlinit: function(main_dso_handle) {},
   _dlinit: function(main_dso_handle) {},
 #else // MAIN_MODULE != 0
   // dynamic linker/loader (a-la ld.so on ELF systems)

tools/system_libs.py
@@ -1127,7 +1127,6 @@ class libc(MuslInternalLibrary,
     libc_files += files_in_path(
         path='system/lib/libc',
         filenames=[
-          'dynlink.c',
           'emscripten_console.c',
           'emscripten_fiber.c',
           'emscripten_get_heap_size.c',
@@ -1144,6 +1143,9 @@ class libc(MuslInternalLibrary,
           'sigtimedwait.c',
           'wasi-helpers.c',
         ])
+
+    if settings.RELOCATABLE:
+      libc_files += files_in_path(path='system/lib/libc', filenames=['dynlink.c'])
 
     libc_files += files_in_path(
         path='system/lib/pthread',

commit 2ab054e9c1ea5ecdc694416bbad2228d681549c7

Fix regression between 3.1.8 -> 3.1.38 where overriding dlopen() with a gracefully failing version broke, and add a test to verify that end user code overriding dlopen() cannot regress again. Possibly regressed by #18638.

src/library_dylink.js
@@ -299,7 +299,7 @@ var LibraryDylink = {
   _emscripten_dlopen_js: function(handle, onsuccess, onerror, user_data) {
     abort(dlopenMissingError);
   },
-  _dlsym_js: function(handle, symbol) {
+  _dlsym_js: function(handle, symbol, symbolIndex) {
     abort(dlopenMissingError);
   },
   _dlsym_catchup_js: function(handle, symbolIndex) {

test/other/test_dlopen_graceful_fail.c
@@ -1,0 +1,20 @@
+// Emscripten's stub implementation of dlopen() hard aborts execution.
+// Some users want to instead have dlopen() perform a graceful "return 0" failure
+// where they handle the dlopen failures in code, so they want to override
+// Emscripten's built-in JS library functions.
+
+// This test verifies that the ability to override dlopen() to not hard abort
+// will not regress.
+#include <dlfcn.h>
+#include <stdio.h>
+
+int main()
+{
+  printf("Attempting to dlopen\n");
+  void *dll = dlopen("nonexistent.dll", 0);
+  printf("dlopen completed: ptr %p. error: %s\n", dll, dlerror());
+  dlclose(0);
+  printf("dlclose(0) completed.\n");
+  if (!dll) printf("dlopen failed gracefully!\n");
+  return 0;
+}

test/other/test_dlopen_graceful_fail.js
@@ -1,0 +1,56 @@
+#if !MAIN_MODULE
+#if !SIDE_MODULE
+// This code represents stable end-user facing API. Changing any part of this file
+// will break end users!
+mergeInto(LibraryManager.library, {
+  _dlopen_js__deps: ['$warnOnce'],
+  _dlopen_js: function(handle) {
+#if ASSERTIONS
+    warnOnce('_dlopen_js: Unable to open DLL! Dynamic linking is not supported in WebAssembly builds due to limitations to performance and code size. Please statically link in the needed libraries.');
+#endif
+  },
+
+  _emscripten_dlopen_js__deps: ['$warnOnce'],
+  _emscripten_dlopen_js: function(filename, flags, user_data, onsuccess, onerror) {
+#if ASSERTIONS
+    warnOnce('_emscripten_dlopen_js: Unable to open DLL ' + UTF8ToString(filename) + '! Dynamic linking is not supported in WebAssembly builds due to limitations to performance and code size. Please statically link in the needed libraries.');
+#endif
+  },
+
+  _dlsym_js__deps: ['$warnOnce'],
+  _dlsym_js: function(handle, symbol, symbolIndex) {
+#if ASSERTIONS
+    warnOnce('_dlsym_js: Unable to open DLL! Dynamic linking is not supported in WebAssembly builds due to limitations to performance and code size. Please statically link in the needed libraries.');
+#endif
+  },
+
+  _dlsym_catchup_js__deps: ['$warnOnce'],
+  _dlsym_catchup_js: function(handle, symbolIndex) {
+#if ASSERTIONS
+    warnOnce('_dlsym_catchup_js: Unable to open DLL! Dynamic linking is not supported in WebAssembly builds due to limitations to performance and code size. Please statically link in the needed libraries.');
+#endif
+  },
+
+  dlopen__deps: ['$warnOnce'],
+  dlopen: function(handle) {
+#if ASSERTIONS
+    warnOnce('dlopen: Unable to open DLL! Dynamic linking is not supported in WebAssembly builds due to limitations to performance and code size. Please statically link in the needed libraries.');
+#endif
+  },
+
+  emscripten_dlopen__deps: ['$warnOnce'],
+  emscripten_dlopen: function(handle, onsuccess, onerror, user_data) {
+#if ASSERTIONS
+    warnOnce('emscripten_dlopen: Unable to open DLL! Dynamic linking is not supported in WebAssembly builds due to limitations to performance and code size. Please statically link in the needed libraries.');
+#endif
+  },
+
+  __dlsym__deps: ['$warnOnce'],
+  __dlsym: function(handle, symbol) {
+#if ASSERTIONS
+    warnOnce('__dlsym: Unable to open DLL! Dynamic linking is not supported in WebAssembly builds due to limitations to performance and code size. Please statically link in the needed libraries.');
+#endif
+  },
+});
+#endif
+#endif

test/other/test_dlopen_graceful_fail.out
@@ -1,0 +1,5 @@
+Attempting to dlopen
+warning: dlopen: Unable to open DLL! Dynamic linking is not supported in WebAssembly builds due to limitations to performance and code size. Please statically link in the needed libraries.
+dlopen completed: ptr 0. error: (null)
+dlclose(0) completed.
+dlopen failed gracefully!

test/test_other.py
@@ -6635,6 +6635,11 @@ int main(int argc,char** argv) {
     self.set_setting('ASYNCIFY')
     self.do_other_test('test_dlopen_blocking.c')
 
+  # This test verifies that users can override dlopen symbols in their own code to
+  # replace the default aborting dlopen() implementation with a gracefully failing one.
+  def test_dlopen_graceful_fail(self):
+    self.do_other_test('test_dlopen_graceful_fail.c', emcc_args=['--js-library', test_file('other/test_dlopen_graceful_fail.js')])
+
   def test_dlsym_rtld_default(self):
     create_file('side.c', r'''
     int baz() {