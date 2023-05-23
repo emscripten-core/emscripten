f2a5e723e8ea6ae5d8cbe91ab6be80bc67772802
jameshu15869 <jhudson15869@gmail.com>
2023-05-23 11:10:44 -0400

    clean up print

--- test/fs/test_chmod.c
+++ test/fs/test_chmod.c
@@ -72,18 +72,6 @@ int main() {
     stat("testfile", &fileStat);
     assert(fileStat.st_mode & (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH));
     assert(!(fileStat.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)));;
-    printf("File Permissions: \t");
-    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
-    printf("\n\n");
 
     EM_ASM(
         FS.symlink("testfile", "symlinkFile");
