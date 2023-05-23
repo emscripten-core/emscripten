e913a7d3e8cf397349aa43a6999b2e7cb38409f4
jameshu15869 <jhudson15869@gmail.com>
2023-05-23 15:51:19 -0400

    remove whitespace in test_core.py

--- test/test_core.py
+++ test/test_core.py
@@ -6013,7 +6013,7 @@ Module = {
     self.emcc_args += ['-sWASMFS']
     self.emcc_args += ['-sFORCE_FILESYSTEM']
     self.do_runf(test_file('fs/test_open_wasmfs.c'), 'success')
-  
+
   def test_fs_chmod(self):
     self.set_setting("FORCE_FILESYSTEM")
     self.do_runf(test_file('fs/test_chmod.c'), 'success')
