bb5cedccc02025fa595db1848aa8c5c2fcb18b91
jameshu15869 <55058507+jameshu15869@users.noreply.github.com>
2023-05-26 12:38:03 -0400

    WasmFS JS API: Implement close (#19431)
    Add ErrnoError throwing in the JS API as well, to match the old FS as much as possible.

---
+++ test/fs/test_fs_js_api.c
@@ -0,0 +1,63 @@
+#include <emscripten/emscripten.h>
+#include <stdio.h>
+
+int main() {
+    /********** test FS.open() **********/
+    EM_ASM(
+        FS.writeFile('testfile', 'a=1\nb=2\n');
+        var readStream = FS.open('testfile', 'r');
+        var writeStream = FS.open('testfile', 'w');
+        var writePlusStream = FS.open('testfile', 'w+');
+        var appendStream = FS.open('testfile', 'a');
+#if WASMFS
+        assert(readStream >= 0);
+        assert(writeStream >= 0);
+        assert(writePlusStream >= 0);
+        assert(appendStream >= 0);
+#else
+        assert(readStream && readStream.fd >= 0);
+        assert(writeStream && writeStream.fd >= 0);
+        assert(writePlusStream && writePlusStream.fd >= 0);
+        assert(appendStream && appendStream.fd >= 0);
+#endif
+
+        var ex;
+        try {
+            FS.open('filenothere', 'r');
+        } catch(err) {
+            ex = err;
+        }
+        assert(ex.name === "ErrnoError" && ex.errno === 44 /* ENOENT */);
+
+        var createFileNotHere = FS.open('filenothere', 'w+');
+#if WASMFS
+        assert(createFileNotHere >= 0);
+#else
+        assert(createFileNotHere && createFileNotHere.fd >= 0);
+#endif
+    );
+
+    /********** test FS.close() **********/
+    EM_ASM(
+        FS.writeFile("closetestfile", 'a=1\nb=2\n');
+        FS.mkdir("/testdir");
+        var file = FS.open("closetestfile", "r");
+        var error = FS.close(file);
+        assert(!error);
+
+        file = FS.open("/testdir", "r");
+        error = FS.close(file);
+        assert(!error);
+
+        var ex;
+        try {
+            FS.close(file);
+        } catch(err) {
+            ex = err;
+        }
+
+        assert(ex.name === "ErrnoError" && ex.errno === 8 /* EBADF */)
+    );
+
+    puts("success");
+}
