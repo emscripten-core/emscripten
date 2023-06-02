emscripten-core/emscripten

commit db960c80f8b37bd79c34772405674bf55e664910

Simplify FS.fdnext and FS.createStream. NFC (#19391)
In all cases where we want to force a particular fd, we specific a
single one, and not a range.   This means that range based search using
FD.fdnext() is not necessary.

This lays the ground work for moving to HandleAllocator.

src/library_noderawfs.js
@@ -44,9 +44,9 @@ mergeInto(LibraryManager.library, {
       return { path: path, node: { id: st.ino, mode: mode, node_ops: NODERAWFS, path: path }};
     },
     createStandardStreams: function() {
-      FS.streams[0] = FS.createStream({ nfd: 0, position: 0, path: '', flags: 0, tty: true, seekable: false }, 0, 0);
+      FS.createStream({ nfd: 0, position: 0, path: '', flags: 0, tty: true, seekable: false }, 0);
       for (var i = 1; i < 3; i++) {
-        FS.streams[i] = FS.createStream({ nfd: i, position: 0, path: '', flags: 577, tty: true, seekable: false }, i, i);
+        FS.createStream({ nfd: i, position: 0, path: '', flags: 577, tty: true, seekable: false }, i);
       }
     },
     // generic function for all node creation
@@ -81,7 +81,7 @@ mergeInto(LibraryManager.library, {
       fs.ftruncateSync.apply(void 0, arguments);
     },
     utime: function(path, atime, mtime) { fs.utimesSync(path, atime/1000, mtime/1000); },
-    open: function(path, flags, mode, suggestFD) {
+    open: function(path, flags, mode) {
       if (typeof flags == "string") {
         flags = FS_modeStringToFlags(flags)
       }
@@ -93,15 +93,12 @@ mergeInto(LibraryManager.library, {
         throw new FS.ErrnoError(ERRNO_CODES.ENOTDIR);
       }
       var newMode = NODEFS.getMode(pathTruncated);
-      var fd = suggestFD != null ? suggestFD : FS.nextfd(nfd);
       var node = { id: st.ino, mode: newMode, node_ops: NODERAWFS, path: path }
-      var stream = FS.createStream({ nfd: nfd, position: 0, path: path, flags: flags, node: node, seekable: true }, fd, fd);
-      FS.streams[fd] = stream;
-      return stream;
+      return FS.createStream({ nfd: nfd, position: 0, path: path, flags: flags, node: node, seekable: true }, nfd);
     },
-    createStream: function(stream, fd_start, fd_end){
+    createStream: function(stream, fd) {
       // Call the original FS.createStream
-      var rtn = VFS.createStream(stream, fd_start, fd_end);
+      var rtn = VFS.createStream(stream, fd);
       if (typeof rtn.shared.refcnt == 'undefined') {
         rtn.shared.refcnt = 1;
       } else {

src/library_fs.js
@@ -373,8 +373,8 @@ FS.staticInit();` +
     // streams
     //
     MAX_OPEN_FDS: 4096,
-    nextfd: (fd_start = 0, fd_end = FS.MAX_OPEN_FDS) => {
-      for (var fd = fd_start; fd <= fd_end; fd++) {
+    nextfd: () => {
+      for (var fd = 0; fd <= FS.MAX_OPEN_FDS; fd++) {
         if (!FS.streams[fd]) {
           return fd;
         }
@@ -385,7 +385,7 @@ FS.staticInit();` +
     // TODO parameterize this function such that a stream
     // object isn't directly passed in. not possible until
     // SOCKFS is completed.
-    createStream: (stream, fd_start, fd_end) => {
+    createStream: (stream, fd = -1) => {
       if (!FS.FSStream) {
         FS.FSStream = /** @constructor */ function() {
           this.shared = { };
@@ -426,7 +426,9 @@ FS.staticInit();` +
       }
       // clone it, so we can return an instance of FSStream
       stream = Object.assign(new FS.FSStream(), stream);
-      var fd = FS.nextfd(fd_start, fd_end);
+      if (fd == -1) {
+        fd = FS.nextfd();
+      }
       stream.fd = fd;
       FS.streams[fd] = stream;
       return stream;

src/library_syscall.js
@@ -185,7 +185,7 @@ var SyscallsLibrary = {
   },
   __syscall_dup: function(fd) {
     var old = SYSCALLS.getStreamFromFD(fd);
-    return FS.createStream(old, 0).fd;
+    return FS.createStream(old).fd;
   },
   __syscall_pipe__deps: ['$PIPEFS'],
   __syscall_pipe: function(fdPtr) {
@@ -956,15 +956,15 @@ var SyscallsLibrary = {
     FS.allocate(stream, offset, len);
     return 0;
   },
-  __syscall_dup3: function(fd, suggestFD, flags) {
+  __syscall_dup3: function(fd, newfd, flags) {
     var old = SYSCALLS.getStreamFromFD(fd);
 #if ASSERTIONS
     assert(!flags);
 #endif
-    if (old.fd === suggestFD) return -{{{ cDefs.EINVAL }}};
-    var suggest = FS.getStream(suggestFD);
-    if (suggest) FS.close(suggest);
-    return FS.createStream(old, suggestFD, suggestFD + 1).fd;
+    if (old.fd === newfd) return -{{{ cDefs.EINVAL }}};
+    var existing = FS.getStream(newfd);
+    if (existing) FS.close(existing);
+    return FS.createStream(old, newfd).fd;
   },
 };
 

test/other/metadce/test_metadce_cxx_ctors1.jssize
@@ -1,1 +1,1 @@
-25933
+25948

test/other/metadce/test_metadce_cxx_ctors2.jssize
@@ -1,1 +1,1 @@
-25897
+25912

test/other/metadce/test_metadce_cxx_except.jssize
@@ -1,1 +1,1 @@
-30447
+30462

test/other/metadce/test_metadce_cxx_except_wasm.jssize
@@ -1,1 +1,1 @@
-25742
+25757

test/other/metadce/test_metadce_cxx_mangle.jssize
@@ -1,1 +1,1 @@
-30446
+30461

commit 5631f48ea6f95ebc625090e385ccd254b03d44f0

Fix NODERAWFS regression caused by #19391
Thanks to @kleisauke for pointing out that the old code always passed -1
as the second argument and not `nfd`.  `nfd` here is the node raw FD
which is different the FD space we return to the user.

src/library_noderawfs.js
@@ -94,7 +94,7 @@ mergeInto(LibraryManager.library, {
       }
       var newMode = NODEFS.getMode(pathTruncated);
       var node = { id: st.ino, mode: newMode, node_ops: NODERAWFS, path: path }
-      return FS.createStream({ nfd: nfd, position: 0, path: path, flags: flags, node: node, seekable: true }, nfd);
+      return FS.createStream({ nfd: nfd, position: 0, path: path, flags: flags, node: node, seekable: true });
     },
     createStream: function(stream, fd) {
       // Call the original FS.createStream