8b19a73bc853f4a682e8adc9298031a792b72b5b
jameshu15869 <jhudson15869@gmail.com>
2023-06-14 17:05:03 -0400

    clean up whitespace

--- test/fs/test_fs_js_api.c
+++ test/fs/test_fs_js_api.c
@@ -4,27 +4,6 @@
 #include <assert.h>
 #include <fcntl.h>
 
-EM_JS(void, test_fs_open, (), {
-    FS.writeFile('testfile', 'a=1\nb=2\n');
-    var readStream = FS.open('testfile', 'r');
-    var writeStream = FS.open('testfile', 'w');
-    var writePlusStream = FS.open('testfile', 'w+');
-    var appendStream = FS.open('testfile', 'a');
-
-    assert(readStream && readStream.fd >= 0);
-    assert(writeStream && writeStream.fd >= 0);
-    assert(writePlusStream && writePlusStream.fd >= 0);
-    assert(appendStream && appendStream.fd >= 0);
-
-    var ex;
-    try {
-        FS.open('filenothere', 'r');
-    } catch(err) {
-        ex = err;
-    }
-    assert(ex.name === "ErrnoError" && ex.errno === 44 /* ENOENT */);
-});
-
 void test_fs_allocate() {
     EM_ASM(
         FS.writeFile("allocatetestfile", 'a=1\nb=2\n');
@@ -129,84 +108,6 @@ void test_fs_truncate() {
     remove("truncatetest");
 }
 
-EM_JS(void, test_fs_rename, (), {
-    FS.mkdir('renamedir');
-    FS.writeFile('renamedir/renametestfile', "");
-
-    FS.rename('renamedir/renametestfile', 'renamedir/newname');
-    var newnameStream = FS.open('renamedir/newname', 'r');
-    assert(newnameStream);
-
-    var ex;
-    try {
-        FS.open('renamedir/renametestfile', 'r');
-    } catch (err) {
-        ex = err;
-    }
-    assert(ex.name === "ErrnoError" && ex.errno === 44 /* ENOENT */);
-
-    
-    try {
-        FS.rename('renamedir', 'renamedir/newdirname');
-    } catch (err) {
-        ex = err;
-    }
-    // The old path should not be an ancestor of the new path.
-    assert(ex.name === "ErrnoError" && ex.errno === 28 /* EINVAL */);
-
-    FS.writeFile('toplevelfile', "");
-    try {
-        FS.rename('renamedir', 'toplevelfile');
-    } catch (err) {
-        ex = err;
-    }
-    assert(ex.name === "ErrnoError" && ex.errno === 54 /* ENOTDIR */);
-});
-
-EM_JS(void, test_fs_read, (), {
-    FS.writeFile("readtestfile", 'a=1_b=2_');
-
-    // Test read variant, checking that the file offset moves correctly.
-    var buf = new Uint8Array(8);
-    var stream = FS.open("readtestfile", "r");
-    var numRead = FS.read(stream, buf, 0, 4);
-    assert(numRead == 4);
-    assert((new TextDecoder().decode(buf.subarray(0, 4))) === "a=1_");
-    numRead = FS.read(stream, buf, 4, 4);
-    assert(numRead == 4);
-    assert((new TextDecoder().decode(buf)) == 'a=1_b=2_');
-
-    // Test pread variant.
-    stream = FS.open("readtestfile", "r");
-    var extraBuf = new Uint8Array(8);
-    numRead = FS.read(stream, extraBuf, 0, 4, 0);
-    assert(numRead == 4);
-    assert((new TextDecoder().decode(extraBuf.subarray(0, 4))) == 'a=1_');
-
-    // Check that pread did not move the file offset (Offset begins at 0).
-    var doubleBuf = new Uint8Array(8);
-    var firstNumRead = FS.read(stream, doubleBuf, 0, 4);
-    var secondNumRead = FS.read(stream, doubleBuf, 4, 4);
-    assert(firstNumRead == 4 && secondNumRead == 4);
-    assert((new TextDecoder().decode(doubleBuf)) == 'a=1_b=2_');
-
-    // Check that full read works.
-    stream = FS.open("readtestfile", "r");
-    var fullBuf = new Uint8Array(8);
-    numRead = FS.read(stream, fullBuf, 0, 8);
-    assert(numRead == 8);
-    assert((new TextDecoder().decode(fullBuf)) == 'a=1_b=2_');
-
-    FS.close(stream);
-    var ex;
-    try {
-        FS.read(stream, buf, 4, 4);
-    } catch (err) {
-        ex = err;
-    }
-    assert(ex.name === 'ErrnoError' && ex.errno == 8 /* EBADF */);
-});
-
 void test_fs_utime() {
     EM_ASM(
         FS.writeFile('utimetest', 'a=1\nb=2\n');
@@ -233,9 +134,117 @@ void test_fs_utime() {
     assert(utimeStats.st_mtime == 10);
     assert(utimeStats.st_mtim.tv_sec == 10);
 #endif
+
+    remove("utimetest");
 }
 
-void test_fs_rmdir() {
+int main() {
+    /********** test FS.open() **********/
+    EM_ASM(
+        FS.writeFile('testfile', 'a=1\nb=2\n');
+        var readStream = FS.open('testfile', 'r');
+        var writeStream = FS.open('testfile', 'w');
+        var writePlusStream = FS.open('testfile', 'w+');
+        var appendStream = FS.open('testfile', 'a');
+
+        assert(readStream && readStream.fd >= 0);
+        assert(writeStream && writeStream.fd >= 0);
+        assert(writePlusStream && writePlusStream.fd >= 0);
+        assert(appendStream && appendStream.fd >= 0);
+
+        var ex;
+        try {
+            FS.open('filenothere', 'r');
+        } catch(err) {
+            ex = err;
+        }
+        assert(ex.name === "ErrnoError" && ex.errno === 44 /* ENOENT */);
+
+        var createFileNotHere = FS.open('filenothere', 'w+');
+
+        assert(createFileNotHere && createFileNotHere.fd >= 0);
+    );
+
+    /********** test FS.rename() **********/
+    EM_ASM(
+        FS.mkdir('renamedir');
+        FS.writeFile('renamedir/renametestfile', "");
+
+        FS.rename('renamedir/renametestfile', 'renamedir/newname');
+        var newnameStream = FS.open('renamedir/newname', 'r');
+        assert(newnameStream);
+
+        var ex;
+        try {
+            FS.open('renamedir/renametestfile', 'r');
+        } catch (err) {
+            ex = err;
+        }
+        assert(ex.name === "ErrnoError" && ex.errno === 44 /* ENOENT */);
+
+        try {
+            FS.rename('renamedir', 'renamedir/newdirname');
+        } catch (err) {
+            ex = err;
+        }
+        // The old path should not be an ancestor of the new path.
+        assert(ex.name === "ErrnoError" && ex.errno === 28 /* EINVAL */);
+
+        FS.writeFile('toplevelfile', "");
+        try {
+            FS.rename('renamedir', 'toplevelfile');
+        } catch (err) {
+            ex = err;
+        }
+        assert(ex.name === "ErrnoError" && ex.errno === 54 /* ENOTDIR */);
+    );
+
+    /********** test FS.read() **********/
+    EM_ASM(
+        FS.writeFile("readtestfile", 'a=1_b=2_');
+
+        // Test read variant, checking that the file offset moves correctly.
+        var buf = new Uint8Array(8);
+        var stream = FS.open("readtestfile", "r");
+        var numRead = FS.read(stream, buf, 0, 4);
+        assert(numRead == 4);
+        assert((new TextDecoder().decode(buf.subarray(0, 4))) === "a=1_");
+        numRead = FS.read(stream, buf, 4, 4);
+        assert(numRead == 4);
+        assert((new TextDecoder().decode(buf)) == 'a=1_b=2_');
+
+        // Test pread variant.
+        stream = FS.open("readtestfile", "r");
+        var extraBuf = new Uint8Array(8);
+        numRead = FS.read(stream, extraBuf, 0, 4, 0);
+        assert(numRead == 4);
+        assert((new TextDecoder().decode(extraBuf.subarray(0, 4))) == 'a=1_');
+
+        // Check that pread did not move the file offset (Offset begins at 0).
+        var doubleBuf = new Uint8Array(8);
+        var firstNumRead = FS.read(stream, doubleBuf, 0, 4);
+        var secondNumRead = FS.read(stream, doubleBuf, 4, 4);
+        assert(firstNumRead == 4 && secondNumRead == 4);
+        assert((new TextDecoder().decode(doubleBuf)) == 'a=1_b=2_');
+
+        // Check that full read works.
+        stream = FS.open("readtestfile", "r");
+        var fullBuf = new Uint8Array(8);
+        numRead = FS.read(stream, fullBuf, 0, 8);
+        assert(numRead == 8);
+        assert((new TextDecoder().decode(fullBuf)) == 'a=1_b=2_');
+
+        FS.close(stream);
+        var ex;
+        try {
+            FS.read(stream, buf, 4, 4);
+        } catch (err) {
+            ex = err;
+        }
+        assert(ex.name === 'ErrnoError' && ex.errno == 8 /* EBADF */);
+    );
+
+    /********** test FS.rmdir() **********/
     EM_ASM(
         // Create multiple directories
         FS.mkdir('/dir1');
@@ -285,30 +294,30 @@ void test_fs_rmdir() {
         }
         assert(ex.name === "ErrnoError" && ex.errno === 44 /* ENOEN */);
     );
-}
 
-EM_JS(void, test_fs_close, (), {
-    FS.writeFile("closetestfile", 'a=1\nb=2\n');
-    FS.mkdir("/testdir");
-    var file = FS.open("closetestfile", "r");
-    var error = FS.close(file);
-    assert(!error);
+    /********** test FS.close() **********/
+    EM_ASM(
+        FS.writeFile("closetestfile", 'a=1\nb=2\n');
+        FS.mkdir("/testdir");
+        var file = FS.open("closetestfile", "r");
+        var error = FS.close(file);
+        assert(!error);
 
-    file = FS.open("/testdir", "r");
-    error = FS.close(file);
-    assert(!error);
+        file = FS.open("/testdir", "r");
+        error = FS.close(file);
+        assert(!error);
 
-    var ex;
-    try {
-        FS.close(file);
-    } catch(err) {
-        ex = err;
-    }
+        var ex;
+        try {
+            FS.close(file);
+        } catch(err) {
+            ex = err;
+        }
 
-    assert(ex.name === "ErrnoError" && ex.errno === 8 /* EBADF */)
-});
+        assert(ex.name === "ErrnoError" && ex.errno === 8 /* EBADF */)
+    );
 
-void test_fs_mknod() {
+    /********** test FS.mknod() **********/
     EM_ASM(
         FS.mknod("mknodtest", 0100000 | 0777); /* S_IFREG | S_RWXU | S_RWXG | S_RWXO */
 
@@ -324,33 +333,16 @@ void test_fs_mknod() {
     assert(S_ISREG(mknodStats.st_mode));
     assert(mknodStats.st_mode & 0400);
 
-    stat("createtest", &stats);
-    assert(S_ISREG(stats.st_mode));
-    assert(stats.st_mode & 0400);
-}
+    test_fs_allocate();
+    test_fs_truncate();
+    test_fs_utime();
 
-void cleanup() {
     remove("mknodtest");
     remove("createtest");
     remove("testfile");
     remove("renametestfile");
     remove("readtestfile");
     remove("closetestfile");
-    remove("utimetest");
-}
-
-int main() {
-    test_fs_open();
-    test_fs_rename();
-    test_fs_read();
-    test_fs_utime();
-    test_fs_rmdir();
-    test_fs_close();
-    test_fs_mknod();
-    test_fs_truncate();
-    test_fs_allocate();
-    
-    cleanup();
 
     puts("success");
 }
