d05c55175b28412df36f444f9fed0ab28c481055
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2023-07-25 19:49:20 +0200

    Update embind.js

--- src/embind/embind.js
+++ src/embind/embind.js
@@ -907,7 +907,7 @@ var LibraryEmbind = {
         throwBindingError('function ${humanName} called with ${arguments.length} arguments, expected ${argCount - 2} args!');
       }`;
 
-#if EMBIND_TRACING
+#if EMBIND_DEBUG
     invokerFnBody += `if (Object.values(arguments).length > 0){
         console.log('${humanName}(' + Object.values(arguments).join(',') + ')');
     } else {
