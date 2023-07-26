dca159eb7cb6d3a582e5d4cc6a13750a0744b604
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2023-07-26 09:48:11 +0200

    Update emcc.py

--- emcc.py
+++ emcc.py
@@ -3512,6 +3512,8 @@ def parse_args(newargs):
     elif check_arg('--embind-emit-tsd'):
       options.embind_emit_tsd = consume_arg()
       settings.INVOKE_RUN = False
+    elif check_arg('--embind-debug'):
+      settings.EMBIND_DEBUG = consume_arg()
     elif check_flag('--no-entry'):
       options.no_entry = True
     elif check_arg('--js-library'):
