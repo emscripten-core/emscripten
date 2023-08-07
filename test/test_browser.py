538799e32f767f502ca6587700106a580ed5f7c8
James Hu <jhudson15869@gmail.com>
2023-08-07 10:56:23 -0400

    force filesystem in failing browser test

--- test/test_browser.py
+++ test/test_browser.py
@@ -2540,6 +2540,8 @@ void *getBindBuffer() {
 
   @also_with_wasmfs
   def test_emscripten_async_wget2(self):
+    self.emcc_args += ['--profiling', '--profiling-funcs']
+    self.set_setting('FORCE_FILESYSTEM')
     self.btest_exit('test_emscripten_async_wget2.cpp')
 
   @disabled('https://github.com/emscripten-core/emscripten/issues/15818')
