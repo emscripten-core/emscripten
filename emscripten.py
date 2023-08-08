94111f25ee3e66c5d07ab2651bdae8c2d2d7898a
Heejin Ahn <aheejin@gmail.com>
2023-08-07 18:38:22 -0700

    Fix a bug in previous "strip tempfile"

--- emscripten.py
+++ emscripten.py
@@ -504,9 +504,12 @@ def finalize_wasm(infile, outfile, memfile, js_syms):
   if settings.DEBUG_LEVEL >= 3:
     args.append('--dwarf')
 
+  if infile != outfile:
+    shutil.copy(infile, outfile)
+
   if settings.GENERATE_SOURCE_MAP:
-    building.emit_wasm_source_map(infile, infile + '.map', outfile)
-    building.save_intermediate(infile + '.map', 'base_wasm.map')
+    building.emit_wasm_source_map(infile, outfile + '.map', outfile)
+    building.save_intermediate(outfile + '.map', 'base_wasm.map')
     base_url = settings.SOURCE_MAP_BASE + os.path.basename(outfile) + '.map'
     if modify_wasm:
       # If we are already modifying, just let Binaryen add the sourcemap URL
@@ -521,23 +524,19 @@ def finalize_wasm(infile, outfile, memfile, js_syms):
              infile]
       shared.check_call(cmd)
 
-  post_strip_file = infile
   if not settings.GENERATE_DWARF or not settings.EMIT_PRODUCERS_SECTION:
     # For sections we no longer need, strip now to speed subsequent passes
-    post_strip_file = shared.get_temp_files().get('.wasm').name
-    building.save_intermediate(infile, 'strip.wasm')
+    building.save_intermediate(outfile, 'strip.wasm')
     sections = ['producers'] if not settings.EMIT_PRODUCERS_SECTION else []
-    building.strip(infile, post_strip_file, debug=not settings.GENERATE_DWARF,
+    building.strip(infile, outfile, debug=not settings.GENERATE_DWARF,
                    sections=sections)
 
-  metadata = get_metadata(post_strip_file, outfile, modify_wasm, args)
+  metadata = get_metadata(outfile, outfile, modify_wasm, args)
   if modify_wasm:
-    building.save_intermediate(infile, 'post_finalize.wasm')
-  elif infile != outfile:
-    shutil.copy(post_strip_file, outfile)
+    building.save_intermediate(outfile, 'post_finalize.wasm')
 
   if settings.GENERATE_SOURCE_MAP:
-    building.save_intermediate(infile + '.map', 'post_finalize.map')
+    building.save_intermediate(outfile + '.map', 'post_finalize.map')
 
   if memfile:
     # we have a separate .mem file. binaryen did not strip any trailing zeros,
