fca5e94c2eca95f22be67c1d53661f8af0b04526
Ingvar Stepanyan <me@rreverser.com>
2023-09-24 14:28:15 +0100

    Simplify default signal action code

--- system/lib/libc/raise.c
+++ system/lib/libc/raise.c
@@ -20,34 +20,39 @@ bool __sig_is_blocked(int sig);
 // Default handler actions ~auto-generated from https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/signal.h.html.
 // Note that stop and continue actions are not supported and treated as ignored.
 
-typedef enum {
-  ACTION_IGNORE = 0,
-  ACTION_TERMINATE,
-  ACTION_ABORT,
-} default_action_t;
+void action_abort(int sig) {
+  abort();
+}
 
-static default_action_t default_actions[_NSIG] = {
-  [SIGABRT] = ACTION_ABORT,
-  [SIGALRM] = ACTION_TERMINATE,
-  [SIGBUS] = ACTION_ABORT,
-  [SIGFPE] = ACTION_ABORT,
-  [SIGHUP] = ACTION_TERMINATE,
-  [SIGILL] = ACTION_ABORT,
-  [SIGINT] = ACTION_TERMINATE,
-  [SIGKILL] = ACTION_TERMINATE,
-  [SIGPIPE] = ACTION_TERMINATE,
-  [SIGQUIT] = ACTION_ABORT,
-  [SIGSEGV] = ACTION_ABORT,
-  [SIGTERM] = ACTION_TERMINATE,
-  [SIGUSR1] = ACTION_TERMINATE,
-  [SIGUSR2] = ACTION_TERMINATE,
-  [SIGPOLL] = ACTION_TERMINATE,
-  [SIGPROF] = ACTION_TERMINATE,
-  [SIGSYS] = ACTION_ABORT,
-  [SIGTRAP] = ACTION_ABORT,
-  [SIGVTALRM] = ACTION_TERMINATE,
-  [SIGXCPU] = ACTION_ABORT,
-  [SIGXFSZ] = ACTION_ABORT,
+void action_terminate(int sig) {
+  // Prepare to forcibly shut down runtime even if it has async work in flight.
+  emscripten_prepare_force_exit();
+  // Intentionally exiting via a function that doesn't call atexit handlers.
+  _Exit(128 + sig);
+}
+
+static sighandler_t default_actions[_NSIG] = {
+  [SIGABRT] = action_abort,
+  [SIGALRM] = action_terminate,
+  [SIGBUS] = action_abort,
+  [SIGFPE] = action_abort,
+  [SIGHUP] = action_terminate,
+  [SIGILL] = action_abort,
+  [SIGINT] = action_terminate,
+  [SIGKILL] = action_terminate,
+  [SIGPIPE] = action_terminate,
+  [SIGQUIT] = action_abort,
+  [SIGSEGV] = action_abort,
+  [SIGTERM] = action_terminate,
+  [SIGUSR1] = action_terminate,
+  [SIGUSR2] = action_terminate,
+  [SIGPOLL] = action_terminate,
+  [SIGPROF] = action_terminate,
+  [SIGSYS] = action_abort,
+  [SIGTRAP] = action_abort,
+  [SIGVTALRM] = action_terminate,
+  [SIGXCPU] = action_abort,
+  [SIGXFSZ] = action_abort,
 };
 
 int raise(int sig) {
@@ -59,24 +64,17 @@ int raise(int sig) {
     siginfo_t t = {0};
     __sig_actions[sig].sa_sigaction(sig, &t, NULL);
   } else {
-    void (*handler)(int) = __sig_actions[sig].sa_handler;
+    sighandler_t handler = __sig_actions[sig].sa_handler;
     if (handler == SIG_DFL) {
-      switch (default_actions[sig]) {
-        case ACTION_IGNORE:
-          break;
-        case ACTION_TERMINATE:
-          // Prepare to forcibly shut down runtime even if it has async work in flight.
-          emscripten_prepare_force_exit();
-          // Intentionally exiting via a function that doesn't call atexit handlers.
-          _Exit(128 + sig);
-        case ACTION_ABORT:
-          abort();
+      handler = default_actions[sig];
+      if (handler) {
+        handler(sig);
       }
     } else if (handler != SIG_IGN) {
         // Avoid a direct call to the handler, and instead call via JS so we can
         // avoid strict signature checking.
         // https://github.com/emscripten-core/posixtestsuite/issues/6
-        __call_sighandler(__sig_actions[sig].sa_handler, sig);
+        __call_sighandler(handler, sig);
     }
   }
   return 0;
