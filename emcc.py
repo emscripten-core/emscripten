3475c7baf416c4bf9efad0f94191ae4f76b708bc
Alon Zakai <azakai@google.com>
2023-10-09 10:05:24 -0700

    fix

--- emcc.py
+++ emcc.py
@@ -723,13 +723,7 @@ def get_binaryen_passes():
   # fixed and may only be appended to at runtime (that is true even in
   # relocatable mode)
   if optimizing:
-    # wasm-emscripten-finalize will strip the features section for us
-    # automatically, but if we did not modify the wasm then we didn't run it,
-    # and in an optimized build we strip it manually here. (note that in an
-    # unoptimized build we might end up with the features section, if we neither
-    # optimize nor run wasm-emscripten-finalize, but a few extra bytes in the
-    # binary don't matter in an unoptimized build)
-    passes += ['--strip-target-features']
+    passes += ['--pass-arg=directize-initial-contents-immutable']
 
   if settings.BINARYEN_EXTRA_PASSES:
     # BINARYEN_EXTRA_PASSES is comma-separated, and we support both '-'-prefixed and
