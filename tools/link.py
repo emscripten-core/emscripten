bc16666f0da87dabfe0ca6d7340bcd0a3106236d
Giorgio Garasto <giorgio@garasto.me>
2023-11-17 22:39:58 +0100

    chore: disable wasm exceptions when generating ts typings

--- tools/link.py
+++ tools/link.py
@@ -1938,6 +1938,8 @@ def phase_embind_emit_tsd(options, wasm_target, js_syms):
   setup_environment_settings()
   # Use a separate Wasm file so the JS does not need to be modified after emscripten.run.
   settings.SINGLE_FILE = False
+  # Disable support for wasm exceptions
+  settings.WASM_EXCEPTIONS = False
   # Embind may be included multiple times, de-duplicate the list first.
   settings.JS_LIBRARIES = dedup_list(settings.JS_LIBRARIES)
   # Replace embind with the TypeScript generation version.
@@ -1951,8 +1953,6 @@ def phase_embind_emit_tsd(options, wasm_target, js_syms):
   node_args = []
   if settings.MEMORY64:
     node_args += shared.node_memory64_flags()
-  if settings.WASM_EXCEPTIONS:
-    node_args += ['--experimental-wasm-eh']
   # Run the generated JS file with the proper flags to generate the TypeScript bindings.
   out = shared.run_js_tool(outfile_js, [], node_args, stdout=PIPE)
   write_file(
