a621bc137932762280e23084874c44fff248553a
Alon Zakai <azakai@google.com>
2023-12-04 12:24:51 -0800

    Streamline metadce by avoiding wasted work (#20814)
    Clarify that we only remove imports and exports, and use that information to
    only call the "apply" stage if we in fact have something to remove. In practice
    it looks like we end up with nothing to remove only when using the minimal
    runtime, as the normal runtime has stuff like errno that is almost always
    removable.
    
    This is probably not a significant speedup, but it doesn't hurt, and it is nice I
    think to clarify what work we do exactly.

--- tools/building.py
+++ tools/building.py
@@ -810,9 +810,15 @@ def metadce(js_file, wasm_file, debug_info):
   for line in out.splitlines():
     if line.startswith(PREFIX):
       name = line.replace(PREFIX, '').strip()
+      # we only remove imports and exports in applyDCEGraphRemovals
       if name in import_name_map:
         name = import_name_map[name]
-      unused.append(name)
+        unused.append(name)
+      elif name.startswith('emcc$export$'):
+        unused.append(name)
+  if not unused:
+    # nothing found to be unused, so we have nothing to remove
+    return js_file
   # remove them
   passes = ['applyDCEGraphRemovals']
   if settings.MINIFY_WHITESPACE:
