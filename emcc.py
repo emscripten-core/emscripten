3f0eabc8bee1e56bc5b9ab6d07b7399efee062e0
Sam Clegg <sbc@chromium.org>
2023-12-12 09:33:30 -0800

    Defer `'linker' input unused` warnings to clang. NFC
    Split out from #20884

--- emcc.py
+++ emcc.py
@@ -534,6 +534,13 @@ emcc: supported targets: llvm bitcode, WebAssembly, NOT elf
 ''')
     return 0
 
+  ## Process argument and setup the compiler
+  state = EmccState(args)
+  options, newargs = phase_parse_arguments(state)
+
+  if not shared.SKIP_SUBPROCS:
+    shared.check_sanity()
+
   if '--version' in args:
     print(version_string())
     print('''\
@@ -565,13 +572,6 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
       parts = [x for x in parts if x not in ['-c', '-o', '-v', '-emit-llvm'] and input_file not in x and temp_target not in x]
       print(shared.shlex_join(parts[1:]))
     return 0
-
-  ## Process argument and setup the compiler
-  state = EmccState(args)
-  options, newargs = phase_parse_arguments(state)
-
-  if not shared.SKIP_SUBPROCS:
-    shared.check_sanity()
 
   if 'EMMAKEN_NO_SDK' in os.environ:
     exit_with_error('EMMAKEN_NO_SDK is no longer supported.  The standard -nostdlib and -nostdinc flags should be used instead')
@@ -618,9 +618,6 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
       print(libname)
     return 0
 
-  if not input_files and not state.link_flags:
-    exit_with_error('no input files')
-
   if options.reproduce:
     create_reproduce_file(options.reproduce, args)
 
@@ -641,8 +638,6 @@ There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR P
     return link.run(linker_inputs, options, state, newargs)
   else:
     logger.debug('stopping after compile phase')
-    for flag in state.link_flags:
-      diagnostics.warning('unused-command-line-argument', "argument unused during compilation: '%s'" % flag[1])
     return 0
 
 
@@ -670,9 +665,9 @@ def phase_parse_arguments(state):
   newargs = diagnostics.capture_warnings(newargs)
 
   for i in range(len(newargs)):
-    if newargs[i] in ('-l', '-L', '-I'):
-      # Scan for individual -l/-L/-I arguments and concatenate the next arg on
-      # if there is no suffix
+    if newargs[i] in ('-l', '-L', '-I', '-z'):
+      # Scan for flags that can be written as either one or two arguments
+      # and normalize them to the single argument form.
       newargs[i] += newargs[i + 1]
       newargs[i + 1] = ''
 
@@ -764,18 +759,13 @@ def phase_setup(options, state, newargs):
       input_files.append((i, arg))
     elif arg.startswith('-L'):
       state.add_link_flag(i, arg)
-      newargs[i] = ''
     elif arg.startswith('-l'):
       state.add_link_flag(i, arg)
-      newargs[i] = ''
     elif arg == '-z':
       state.add_link_flag(i, newargs[i])
       state.add_link_flag(i + 1, newargs[i + 1])
-      newargs[i] = ''
-      newargs[i + 1] = ''
     elif arg.startswith('-z'):
       state.add_link_flag(i, newargs[i])
-      newargs[i] = ''
     elif arg.startswith('-Wl,'):
       # Multiple comma separated link flags can be specified. Create fake
       # fractional indices for these: -Wl,a,b,c,d at index 4 becomes:
@@ -783,14 +773,10 @@ def phase_setup(options, state, newargs):
       link_flags_to_add = arg.split(',')[1:]
       for flag_index, flag in enumerate(link_flags_to_add):
         state.add_link_flag(i + float(flag_index) / len(link_flags_to_add), flag)
-      newargs[i] = ''
     elif arg == '-Xlinker':
       state.add_link_flag(i + 1, newargs[i + 1])
-      newargs[i] = ''
-      newargs[i + 1] = ''
     elif arg == '-s':
       state.add_link_flag(i, newargs[i])
-      newargs[i] = ''
     elif arg == '-':
       input_files.append((i, arg))
       newargs[i] = ''
@@ -950,11 +936,6 @@ def get_clang_output_extension(state):
 
 @ToolchainProfiler.profile_block('compile inputs')
 def phase_compile_inputs(options, state, newargs, input_files):
-  def is_link_flag(flag):
-    if flag in ('-nostdlib', '-nostartfiles', '-nolibc', '-nodefaultlibs'):
-      return True
-    return flag.startswith(('-l', '-L', '-Wl,'))
-
   if shared.run_via_emxx:
     compiler = [shared.CLANG_CXX]
   else:
@@ -964,7 +945,7 @@ def phase_compile_inputs(options, state, newargs, input_files):
     logger.debug('using compiler wrapper: %s', config.COMPILER_WRAPPER)
     compiler.insert(0, config.COMPILER_WRAPPER)
 
-  compile_args = [a for a in newargs if a and not is_link_flag(a)]
+  compile_args = newargs
   system_libs.ensure_sysroot()
 
   def get_language_mode(args):
@@ -1002,7 +983,7 @@ def phase_compile_inputs(options, state, newargs, input_files):
     # with -MF! (clang seems to not recognize it)
     logger.debug(('just preprocessor ' if state.has_dash_E else 'just dependencies: ') + ' '.join(cmd))
     shared.check_call(cmd)
-    return []
+    sys.exit(0)
 
   # Precompiled headers support
   if state.mode == Mode.PCH:
@@ -1015,7 +996,7 @@ def phase_compile_inputs(options, state, newargs, input_files):
       cmd += ['-o', options.output_file]
     logger.debug(f"running (for precompiled headers): {cmd[0]} {' '.join(cmd[1:])}")
     shared.check_call(cmd)
-    return []
+    sys.exit(0)
 
   if state.mode == Mode.COMPILE_ONLY:
     inputs = [i[1] for i in input_files]
@@ -1027,18 +1008,24 @@ def phase_compile_inputs(options, state, newargs, input_files):
       cmd += ['-o', options.output_file]
       if get_file_suffix(options.output_file) == '.bc' and not settings.LTO and '-emit-llvm' not in state.orig_args:
         diagnostics.warning('emcc', '.bc output file suffix used without -flto or -emit-llvm.  Consider using .o extension since emcc will output an object file, not a bitcode file')
+    ext = get_clang_output_extension(state)
     shared.check_call(cmd)
-    if not options.output_file:
-      # Rename object files to match --default-obj-ext
-      # TODO: Remove '--default-obj-ext' to reduce this complexity
-      ext = get_clang_output_extension(state)
-      if options.default_object_extension != ext:
-        for i in inputs:
-          output = unsuffixed_basename(i) + ext
-          new_output = unsuffixed_basename(i) + options.default_object_extension
-          shutil.move(output, new_output)
-    return []
+    if not options.output_file and options.default_object_extension != ext:
+      for i in inputs:
+        output = unsuffixed_basename(i) + ext
+        new_output = unsuffixed_basename(i) + options.default_object_extension
+        shutil.move(output, new_output)
+    sys.exit(0)
 
+  # In COMPILE_AND_LINK we need to compile source files too, but we also need to
+  # filter out the link flags
+
+  def is_link_flag(flag):
+    if flag in ('-nostdlib', '-nostartfiles', '-nolibc', '-nodefaultlibs', '-s'):
+      return True
+    return flag.startswith(('-l', '-L', '-Wl,', '-z'))
+
+  compile_args = [a for a in compile_args if a and not is_link_flag(a)]
   linker_inputs = []
   seen_names = {}
 
