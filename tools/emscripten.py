9b3b5c7aadc5d0d368d100c496ad64e56c2e5799
Sam Clegg <sbc@chromium.org>
2024-01-22 21:48:20 +0000

    Use single quotes in JS code where possible. NFC
    For some reason this actually saves on code size too.

--- tools/emscripten.py
+++ tools/emscripten.py
@@ -801,7 +801,7 @@ def make_export_wrappers(function_exports, delay_assignment):
       args = ', '.join(args)
       wrapper += f"({args}) => ({mangled} = {exported}wasmExports['{name}'])({args});"
     else:
-      wrapper += 'wasmExports["%s"]' % name
+      wrapper += f"wasmExports['{name}']"
 
     wrappers.append(wrapper)
   return wrappers
