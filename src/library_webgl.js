emscripten-core/emscripten

commit 72c3be342861765b620ea0d00e192aa2ac19b744

[wasm64] Fix glSharedSource in >4gb mode and enable test under wasm64 (#21184)

src/library_webgl.js
@@ -425,8 +425,8 @@ for (/**@suppress{duplicate}*/var i = 0; i < {{{ GL_POOL_TEMP_BUFFERS_SIZE }}}; 
     getSource: (shader, count, string, length) => {
       var source = '';
       for (var i = 0; i < count; ++i) {
-        var len = length ? {{{ makeGetValue('length', 'i*4', 'i32') }}} : -1;
-        source += UTF8ToString({{{ makeGetValue('string', 'i*4', 'i32') }}}, len < 0 ? undefined : len);
+        var len = length ? {{{ makeGetValue('length', 'i*' * POINTER_SIZE, '*') }}} : undefined;
+        source += UTF8ToString({{{ makeGetValue('string', 'i*' + POINTER_SIZE, '*') }}}, len);
       }
 #if LEGACY_GL_EMULATION
       // Let's see if we need to enable the standard derivatives extension

.circleci/config.yml
@@ -816,6 +816,7 @@ jobs:
             browser64_4gb.test_async_*
             browser64_4gb.test_audio_worklet*
             browser64_4gb.test_emscripten_log
+            browser64_4gb.test_clientside_vertex_arrays_es3
             "
   test-browser-firefox:
     executor: bionic

commit 7f8ffcb11a7dcff6a26d354c75778a1a7ece7de2

Fix GL.getSource bug introduced in #21184
This only seems to show up in `browser.test_sdl_ogl_regal`.

src/library_webgl.js
@@ -425,7 +425,7 @@ for (/**@suppress{duplicate}*/var i = 0; i < {{{ GL_POOL_TEMP_BUFFERS_SIZE }}}; 
     getSource: (shader, count, string, length) => {
       var source = '';
       for (var i = 0; i < count; ++i) {
-        var len = length ? {{{ makeGetValue('length', 'i*' * POINTER_SIZE, '*') }}} : undefined;
+        var len = length ? {{{ makeGetValue('length', 'i*' + POINTER_SIZE, '*') }}} : undefined;
         source += UTF8ToString({{{ makeGetValue('string', 'i*' + POINTER_SIZE, '*') }}}, len);
       }
 #if LEGACY_GL_EMULATION