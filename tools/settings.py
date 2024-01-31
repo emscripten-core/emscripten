7d4383119cd3714f060722cb7630c4eefa435037
Yan Pujante <yan@pongasoft.com>
2024-01-31 08:02:24 -0800

    reverted comma

--- tools/settings.py
+++ tools/settings.py
@@ -51,7 +51,7 @@ PORTS_SETTINGS = {
     'USE_FREETYPE',
     'SDL2_MIXER_FORMATS',
     'SDL2_IMAGE_FORMATS',
-    'USE_SQLITE3'
+    'USE_SQLITE3',
 }
 
 # Subset of settings that apply only when generating JS
