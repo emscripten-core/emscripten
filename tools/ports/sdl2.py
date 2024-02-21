d82da3370a312b19e41380c107358895fbb7af43
Sam Clegg <sbc@chromium.org>
2024-02-21 15:22:40 -0800

    Add changelog entry for SDL2 update. NFC
    Also remove the no-longer-needed `linker_setup` function.  SDL2 now
    uses EM_JS_DEPS to achieve this.
    
    Followup to #21337

--- tools/ports/sdl2.py
+++ tools/ports/sdl2.py
@@ -81,11 +81,6 @@ def clear(ports, settings, shared):
   shared.cache.erase_lib(get_lib_name(settings))
 
 
-def linker_setup(ports, settings):
-  # TODO(sbc): Move these into native code use EM_JS_DEPS macro.
-  settings.DEFAULT_LIBRARY_FUNCS_TO_INCLUDE += ['$autoResumeAudioContext', '$dynCall']
-
-
 def process_args(ports):
   return ['-isystem', ports.get_include_dir('SDL2')]
 
