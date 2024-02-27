1b3950d3c50addda0fd9d471d1a729dc9f6337ef
Sam Clegg <sbc@chromium.org>
2024-02-27 14:29:48 -0800

    Add some comments regarding __c_longjmp. NFC (#21442)

--- system/lib/compiler-rt/emscripten_setjmp.c
+++ system/lib/compiler-rt/emscripten_setjmp.c
@@ -81,13 +81,17 @@ struct __WasmLongjmpArgs {
 
 thread_local struct __WasmLongjmpArgs __wasm_longjmp_args;
 
+// llvm uses `1` for the __c_longjmp tag.
+// See https://github.com/llvm/llvm-project/blob/main/llvm/include/llvm/CodeGen/WasmEHFuncInfo.h
+#define C_LONGJMP 1
+
 // Wasm EH allows us to throw and catch multiple values, but that requires
 // multivalue support in the toolchain, whch is not reliable at the time.
 // TODO Consider switching to throwing two values at the same time later.
 void __wasm_longjmp(void *env, int val) {
   __wasm_longjmp_args.env = env;
   __wasm_longjmp_args.val = val;
-  __builtin_wasm_throw(1, &__wasm_longjmp_args);
+  __builtin_wasm_throw(C_LONGJMP, &__wasm_longjmp_args);
 }
 
 #endif
