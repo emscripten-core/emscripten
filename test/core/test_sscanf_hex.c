0481f7891b9472f853a006729b90fdcd03e6d314
Sam Clegg <sbc@chromium.org>
2024-03-21 12:47:07 -0700

    Convert more core tests from C++ to C. NFC (#21584)
    See #12756

--- test/core/test_sscanf_hex.c
+++ test/core/test_sscanf_hex.c
@@ -4,26 +4,25 @@
 // found in the LICENSE file.
 
 #include <stdio.h>
-#include <string>
-#include <cstdlib>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
   unsigned int a, b;
   sscanf("0x12AB 12AB", "%x %x", &a, &b);
   printf("%d %d\n", a, b);
 
-  std::string hexstr("0102037F00FF");
-  const char * cstr = hexstr.c_str();
-  int len = hexstr.length() / 2;
-  char * tmp_data = new char[len];
+  const char* hexstr = "0102037F00FF";
+  int len = strlen(hexstr) / 2;
+  char* tmp_data = malloc(len);
   for (int i = 0; i < len; i++) {
-    sscanf(cstr, "%2hhx", &tmp_data[i]);
-    cstr += 2 * sizeof(char);
+    sscanf(hexstr, "%2hhx", &tmp_data[i]);
+    hexstr += 2 * sizeof(char);
   }
 
   for (int j = 0; j < len; j++) {
     printf("%i, ", tmp_data[j]);
   }
   printf("\n");
-  delete[] tmp_data;
+  free(tmp_data);
 }
