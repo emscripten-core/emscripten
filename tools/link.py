emscripten-core/emscripten

commit 87358b0ed8afacf8e9896384e8e4e5da42e11f1e

Avoid using `forced_stdlibs` for cases that don't need it. NFC (#21639)
This change simplifies both `building.map_to_js_libs` and the code
that calls it.

Avoiding `forced_stdlibs` means we don't do add `--whole-archive`
`--no-whole-archive` around normal libraries when we don't need to.

tools/link.py
@@ -2632,14 +2632,9 @@ def process_libraries(state, linker_inputs):
 
     logger.debug('looking for library "%s"', lib)
 
-    js_libs, native_lib = building.map_to_js_libs(lib)
+    js_libs = building.map_to_js_libs(lib)
     if js_libs is not None:
       libraries += [(i, js_lib) for js_lib in js_libs]
-      # If native_lib is returned then include it in the link
-      # via forced_stdlibs.
-      if native_lib:
-        state.forced_stdlibs.append(native_lib)
-      continue
 
     # We don't need to resolve system libraries to absolute paths here, we can just
     # let wasm-ld handle that.  However, we do want to map to the correct variant.
@@ -2647,6 +2642,9 @@ def process_libraries(state, linker_inputs):
     if 'lib' + lib in system_libs_map:
       lib = system_libs_map['lib' + lib].get_link_flag()
       new_flags.append((i, lib))
+      continue
+
+    if js_libs is not None:
       continue
 
     if building.map_and_apply_to_settings(lib):

test/other/embind_tsgen_ignore_1.d.ts
@@ -26,9 +26,9 @@ declare namespace RuntimeExports {
 interface WasmModule {
   _pthread_self(): number;
   _main(_0: number, _1: number): number;
+  __embind_initialize_bindings(): void;
   __emscripten_tls_init(): number;
   __emscripten_proxy_main(_0: number, _1: number): number;
-  __embind_initialize_bindings(): void;
   __emscripten_thread_init(_0: number, _1: number, _2: number, _3: number, _4: number, _5: number): void;
   __emscripten_thread_crashed(): void;
   __emscripten_thread_exit(_0: number): void;

tools/building.py
@@ -1154,11 +1154,6 @@ def map_to_js_libs(library_name):
     # for compatibility with GNU toolchains.
     'stdc++': [],
   }
-  # And some are hybrid and require JS and native libraries to be included
-  native_library_map = {
-    'embind': 'libembind',
-    'GL': 'libGL',
-  }
   settings_map = {
     'glfw': {'USE_GLFW': 2},
     'glfw3': {'USE_GLFW': 3},
@@ -1172,12 +1167,12 @@ def map_to_js_libs(library_name):
   if library_name in library_map:
     libs = library_map[library_name]
     logger.debug('Mapping library `%s` to JS libraries: %s' % (library_name, libs))
-    return (libs, native_library_map.get(library_name))
+    return libs
 
   if library_name.endswith('.js') and os.path.isfile(path_from_root('src', f'library_{library_name}')):
-    return ([f'library_{library_name}'], None)
+    return [f'library_{library_name}']
 
-  return (None, None)
+  return None
 
 
 # Map a linker flag to a settings. This lets a user write -lSDL2 and it will

commit d12f75b11a79c2be3140a7cb54ee915a6008f174

Fix embind+lto link failure
This was broken by #21639 which means were accidentally depending
on embind being included using `--whole-archive` previously.

See #21653

.circleci/config.yml
@@ -551,6 +551,7 @@ jobs:
             lto2.test_float_builtins
             lto0.test_exceptions_allowed_uncaught
             lto0.test_longjmp_standalone_standalone
+            lto0.test_embind_i64_val
             core3
             core2g.test_externref
             corez.test_dylink_iostream

tools/link.py
@@ -1342,6 +1342,11 @@ def phase_linker_setup(options, state, newargs):
   if state.has_link_flag('-lembind'):
     settings.EMBIND = 1
 
+  if settings.EMBIND:
+    # Workaround for embind+LTO issue:
+    # https://github.com/emscripten-core/emscripten/issues/21653
+    settings.REQUIRED_EXPORTS.append('__getTypeName')
+
   if options.emit_tsd:
     settings.EMIT_TSD = True
 