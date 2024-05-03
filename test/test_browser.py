fa80cf25b8641974d60605b8a24cb7150fd63701
gabe <gabeklav@fastmail.com>
2024-05-03 01:32:03 -0400

    Add openal alext.h header (#21857)

--- test/test_browser.py
+++ test/test_browser.py
@@ -2297,6 +2297,9 @@ void *getBindBuffer() {
   def test_openal_capture_sanity(self):
     self.btest('openal_capture_sanity.c', expected='0')
 
+  def test_openal_extensions(self):
+    self.btest_exit('openal_extensions.c')
+
   def test_runtimelink(self):
     create_file('header.h', r'''
       struct point {
