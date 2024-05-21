cada1ebe7aa8b9633b585cab22810a5534d84e9b
Heejin Ahn <aheejin@gmail.com>
2024-05-21 15:13:21 -0700

    [EH] __USING_WASM_EXCEPTIONS__ -> __WASM_EXCEPTIONS__ (#21970)
    We've decided to change `__USING_WASM_EXCEPTIONS__` preprocessor to
    `__WASM_EXCEPTIONS__` given that it's more concise.

--- system/lib/libcxxabi/src/private_typeinfo.cpp
+++ system/lib/libcxxabi/src/private_typeinfo.cpp
@@ -1533,7 +1533,7 @@ __base_class_type_info::search_below_dst(__dynamic_cast_info* info,
 
 // XXX EMSCRIPTEN
 
-#ifndef __USING_WASM_EXCEPTIONS__
+#ifndef __WASM_EXCEPTIONS__
 
 // These functions are used by the emscripten-style exception handling
 // mechanism.
