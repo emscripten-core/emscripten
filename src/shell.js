bcdaaa8728f13c790938cf3b4cceac65672e70ec
Sean Morris <640101+seanmorris@users.noreply.github.com>
2024-05-29 21:15:27 -0400

    Tweaks.

--- src/shell.js
+++ src/shell.js
@@ -21,7 +21,7 @@
 // before the code. Then that object will be used in the code, and you
 // can continue to use Module afterwards as well.
 #if MODULARIZE
-var Module = moduleArg;
+var Module = Object.assign({}, moduleArg);
 #elif USE_CLOSURE_COMPILER
 // if (!Module)` is crucial for Closure Compiler here as it will otherwise replace every `Module` occurrence with a string
 var /** @type {{
