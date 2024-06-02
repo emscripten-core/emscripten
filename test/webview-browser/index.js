4e6397700b8df9ca10221f022d8ed8ef065d670e
Sean Morris <640101+seanmorris@users.noreply.github.com>
2024-06-02 13:16:02 -0400

    Tweaks.

--- test/webview-browser/index.js
+++ test/webview-browser/index.js
@@ -5,12 +5,15 @@ const Gtk = gi.require('Gtk', '3.0');
 const WebKit2 = gi.require('WebKit2');
 
 const [binary, script, ...args] = process.argv;
+const [url] = args;
 
 gi.startLoop();
 Gtk.init();
 
 const win = new Gtk.Window();
-const webView = new WebKit2.WebView()
+const webView = new WebKit2.WebView();
+
+webView.getSettings().enableDeveloperExtras = true;
 
 win.on('destroy', () => Gtk.mainQuit());
 win.on('delete-event', () => false);
@@ -18,7 +21,7 @@ win.on('delete-event', () => false);
 win.setDefaultSize(800, 600);
 win.add(webView);
 
-args[0] && webView.loadUri(args[0]);
+url && webView.loadUri(url);
 
 win.showAll();
 Gtk.main();
