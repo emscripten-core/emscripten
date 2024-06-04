1c3b491b593279153b0e56e2b61404c0f4d3e80e
zebo.jiao <zebo.jiao@visla.us>
2024-06-04 18:42:35 +0800

    support to locateFile on initializing audioworklet

--- src/library_webaudio.js
+++ src/library_webaudio.js
@@ -170,7 +170,7 @@ let LibraryWebAudio = {
 
     // TODO: In MINIMAL_RUNTIME builds, read this file off of a preloaded Blob,
     // and/or embed from a string like with WASM_WORKERS==2 mode.
-    audioWorklet.addModule('{{{ TARGET_BASENAME }}}.aw.js').then(() => {
+    audioWorklet.addModule(locateFile('{{{ TARGET_BASENAME }}}.aw.js')).then(() => {
 #if WEBAUDIO_DEBUG
       console.log(`emscripten_start_wasm_audio_worklet_thread_async() addModule('audioworklet.js') completed`);
 #endif
