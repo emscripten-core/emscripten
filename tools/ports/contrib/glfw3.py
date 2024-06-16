b5fb41a5144a52a27a7081abdf8cf28ec378a65d
Yan Pujante <yan@pongasoft.com>
2024-06-16 11:26:14 -0700

    new emscripten-glfw version

--- tools/ports/contrib/glfw3.py
+++ tools/ports/contrib/glfw3.py
@@ -6,8 +6,8 @@
 import os
 from typing import Dict
 
-TAG = '3.4.0.20240601'
-HASH = '3083c320b402fd97c3cf9b55c2c1025bb133b3de0ab32cbb4af13e2471f9a9202bb0c9ae11f578829ae59568bb332762a67686be33689b7803343b84d8032254'
+TAG = '3.4.0.20240616'
+HASH = 'a067effe2044020ed36199f7508c7ef143ee19b6d97e2e1e532974a97f8f2d510da486be0187630a9e0793a38ad7e67a52e74cddb40369c049b463cdebe304d4'
 
 # contrib port information (required)
 URL = 'https://github.com/pongasoft/emscripten-glfw'
