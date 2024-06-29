698c65f0a6c5afab8e20c31bf41f7c3ad354150b
Will Usher <will@willusher.io>
2024-06-29 14:42:34 -0700

    add note

--- src/library_pthread.js
+++ src/library_pthread.js
@@ -453,6 +453,10 @@ var LibraryPThread = {
         worker = new Worker(p.createScriptURL('ignored'), workerOptions);
       } else
 #endif
+      // We need to generate the URL with import.meta.url as the base URL of the JS file
+      // instead of just using new URL(import.meta.url) because bundler's only recognize
+      // the first case in their bundling step. The latter ends up producing an invalid
+      // URL to import from the server (e.g., for webpack the file:// path).
       worker = new Worker(new URL('{{{ TARGET_JS_NAME }}}', import.meta.url), workerOptions);
 #else
       var pthreadMainJs = _scriptName;
