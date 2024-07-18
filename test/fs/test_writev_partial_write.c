96682b08a37d8159f9db8822c7317b6e5175b323
Jonah Graham <jonah@kichwacoders.com>
2024-07-18 18:28:04 -0400

    Split test code into functions

--- test/fs/test_writev_partial_write.c
+++ test/fs/test_writev_partial_write.c
@@ -16,8 +16,7 @@
 
 EM_JS_DEPS(main, "$ERRNO_CODES");
 
-int main() {
-
+void setup(void) {
  EM_ASM(
     var device = FS.makedev(80, 0);
     FS.registerDevice(device, {
@@ -32,53 +31,58 @@ int main() {
     });
     FS.mkdev('/device', device);
   );
+}
 
-  // Run the test with writev directly
-  {
-    int fd = open("/device", O_WRONLY);
-    assert(fd);
-    struct iovec iovs[] = {{.iov_base = "ABC", .iov_len = 3},
-                           {.iov_base = "XYZ", .iov_len = 3}};
-    struct iovec* iov = iovs;
+// Run the test with writev directly
+void test_writev_direct(void) {
+  int fd = open("/device", O_WRONLY);
+  assert(fd);
+  struct iovec iovs[] = {{.iov_base = "ABC", .iov_len = 3},
+                         {.iov_base = "XYZ", .iov_len = 3}};
+  struct iovec* iov = iovs;
 
-    size_t rem = iov[0].iov_len + iov[1].iov_len;
-    int iovcnt = 2;
-    ssize_t cnt;
-    for (;;) {
-      cnt = writev(fd, iov, iovcnt);
-      assert(cnt >= 0);
-      if (cnt == rem) {
-        // All data written
-        break;
-      }
-      rem -= cnt;
-      if (cnt > iov[0].iov_len) {
-        cnt -= iov[0].iov_len;
-        iov++;
-        iovcnt--;
-      }
-      iov[0].iov_base = (char*)iov[0].iov_base + cnt;
-      iov[0].iov_len -= cnt;
+  size_t rem = iov[0].iov_len + iov[1].iov_len;
+  int iovcnt = 2;
+  ssize_t cnt;
+  for (;;) {
+    cnt = writev(fd, iov, iovcnt);
+    assert(cnt >= 0);
+    if (cnt == rem) {
+      // All data written
+      break;
     }
-
-    close(fd);
+    rem -= cnt;
+    if (cnt > iov[0].iov_len) {
+      cnt -= iov[0].iov_len;
+      iov++;
+      iovcnt--;
+    }
+    iov[0].iov_base = (char*)iov[0].iov_base + cnt;
+    iov[0].iov_len -= cnt;
   }
 
-  // Run the test using stdio, this test is dependent on specific buffering used
-  // and is included here as this code most closely matches the original bug
-  // report
-  {
-    FILE* f = fopen("/device", "w");
-    assert(f);
-    // Use line buffering. The bug is exposed with line buffering because with
-    // line buffering two entries in __stdio_write's iovs are used.
-    setvbuf(f, NULL, _IOLBF, 0);
-    fputs("abc", f);
-    fputs("\n", f);
-    fflush(f);
-    fclose(f);
-  }
+  close(fd);
+}
 
+// Run the test using stdio, this test is dependent on specific buffering used
+// and is included here as this code most closely matches the original bug
+// report
+void test_via_stdio(void) {
+  FILE* f = fopen("/device", "w");
+  assert(f);
+  // Use line buffering. The bug is exposed with line buffering because with
+  // line buffering two entries in __stdio_write's iovs are used.
+  setvbuf(f, NULL, _IOLBF, 0);
+  fputs("abc", f);
+  fputs("\n", f);
+  fflush(f);
+  fclose(f);
+}
+
+int main() {
+  setup();
+  test_writev_direct();
+  test_via_stdio();
   printf("done\n");
   return 0;
 }
