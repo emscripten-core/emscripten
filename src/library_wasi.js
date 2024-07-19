2ef8ddd4abcdcc9acc6f87e43b58ab95b9dc9bb8
Jonah Graham <jonah@kichwacoders.com>
2024-07-18 22:10:26 -0400

    Fix to match emscripten style
    Co-authored-by: Alon Zakai <alonzakai@gmail.com>

--- src/library_wasi.js
+++ src/library_wasi.js
@@ -223,7 +223,10 @@ var WasiLibrary = {
       var curr = FS.write(stream, HEAP8, ptr, len, offset);
       if (curr < 0) return -1;
       ret += curr;
-      if (curr < len) break; // no more space to write
+      if (curr < len) {
+        // No more space to write.
+        break;
+      }
       if (typeof offset != 'undefined') {
         offset += curr;
       }
