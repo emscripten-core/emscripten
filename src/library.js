emscripten-core/emscripten

commit addff4f6c75c1b5e103bfdd62d21690fc9b10a61

Use musl implementation of strftime rather than custom JS code. NFC (#21379)
This leads to some code size savings in several tests, reduces the
differences between standalone mode and non-standalone mode and reduces
the amount of custom JS code we need to maitain.

In simple microbenchmark that does nothing but export `strftime` we do
code size win at `-O0` but a regression in codesize at `-O2`:

```
$ wc -c old_O0.*
74026 old.js
12124 old.wasm
86150 total
$ wc -c new_O0.*
62120 new.js
22852 new.wasm
84972 total
```

```
$ wc -c old_O2.*
8492 old.js
 194 old.wasm
8686 total
$ wc -c new_O2.*
 5040 new.js
14291 new.wasm
19331 total
```

This regression comes from the fact that `snprintf` is used in the
implementation of `strftime` and its hard to optimize away the cost of
that function.

In practice I suspect that any application large enough to call
`strftime` is likely already directly or indirectly pulling in
`snprintf` so I would hope that this will be codesize win in practice
since it removes about 3k of JS.

src/library.js
@@ -728,266 +728,6 @@ addToLibrary({
     return newDate;
   },
 
-  // Note: this is not used in STANDALONE_WASM mode, because it is more
-  //       compact to do it in JS.
-  strftime__deps: ['$isLeapYear', '$arraySum', '$addDays', '$MONTH_DAYS_REGULAR', '$MONTH_DAYS_LEAP',
-                   '$intArrayFromString', '$writeArrayToMemory'
-  ],
-  strftime: (s, maxsize, format, tm) => {
-    // size_t strftime(char *restrict s, size_t maxsize, const char *restrict format, const struct tm *restrict timeptr);
-    // http://pubs.opengroup.org/onlinepubs/009695399/functions/strftime.html
-
-    var tm_zone = {{{ makeGetValue('tm', C_STRUCTS.tm.tm_zone, '*') }}};
-
-    var date = {
-      tm_sec: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_sec, 'i32') }}},
-      tm_min: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_min, 'i32') }}},
-      tm_hour: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_hour, 'i32') }}},
-      tm_mday: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_mday, 'i32') }}},
-      tm_mon: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_mon, 'i32') }}},
-      tm_year: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_year, 'i32') }}},
-      tm_wday: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_wday, 'i32') }}},
-      tm_yday: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_yday, 'i32') }}},
-      tm_isdst: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_isdst, 'i32') }}},
-      tm_gmtoff: {{{ makeGetValue('tm', C_STRUCTS.tm.tm_gmtoff, LONG_TYPE) }}},
-      tm_zone: tm_zone ? UTF8ToString(tm_zone) : ''
-    };
-    {{{ from64('date.tm_gmtoff') }}}
-
-    var pattern = UTF8ToString(format);
-
-    // expand format
-    var EXPANSION_RULES_1 = {
-      '%D': '%m/%d/%y',                 // Equivalent to %m / %d / %y
-      '%F': '%Y-%m-%d',                 // Equivalent to %Y - %m - %d
-      '%h': '%b',                       // Equivalent to %b
-      '%r': '%I:%M:%S %p',              // Replaced by the time in a.m. and p.m. notation
-      '%R': '%H:%M',                    // Replaced by the time in 24-hour notation
-      '%T': '%H:%M:%S',                 // Replaced by the time
-      '%x': '%m/%d/%y',                 // Replaced by the locale's appropriate date representation
-      '%X': '%H:%M:%S',                 // Replaced by the locale's appropriate time representation
-      // Modified Conversion Specifiers
-      '%Ec': '%c',                      // Replaced by the locale's alternative appropriate date and time representation.
-      '%EC': '%C',                      // Replaced by the name of the base year (period) in the locale's alternative representation.
-      '%Ex': '%m/%d/%y',                // Replaced by the locale's alternative date representation.
-      '%EX': '%H:%M:%S',                // Replaced by the locale's alternative time representation.
-      '%Ey': '%y',                      // Replaced by the offset from %EC (year only) in the locale's alternative representation.
-      '%EY': '%Y',                      // Replaced by the full alternative year representation.
-      '%Od': '%d',                      // Replaced by the day of the month, using the locale's alternative numeric symbols, filled as needed with leading zeros if there is any alternative symbol for zero; otherwise, with leading <space> characters.
-      '%Oe': '%e',                      // Replaced by the day of the month, using the locale's alternative numeric symbols, filled as needed with leading <space> characters.
-      '%OH': '%H',                      // Replaced by the hour (24-hour clock) using the locale's alternative numeric symbols.
-      '%OI': '%I',                      // Replaced by the hour (12-hour clock) using the locale's alternative numeric symbols.
-      '%Om': '%m',                      // Replaced by the month using the locale's alternative numeric symbols.
-      '%OM': '%M',                      // Replaced by the minutes using the locale's alternative numeric symbols.
-      '%OS': '%S',                      // Replaced by the seconds using the locale's alternative numeric symbols.
-      '%Ou': '%u',                      // Replaced by the weekday as a number in the locale's alternative representation (Monday=1).
-      '%OU': '%U',                      // Replaced by the week number of the year (Sunday as the first day of the week, rules corresponding to %U ) using the locale's alternative numeric symbols.
-      '%OV': '%V',                      // Replaced by the week number of the year (Monday as the first day of the week, rules corresponding to %V ) using the locale's alternative numeric symbols.
-      '%Ow': '%w',                      // Replaced by the number of the weekday (Sunday=0) using the locale's alternative numeric symbols.
-      '%OW': '%W',                      // Replaced by the week number of the year (Monday as the first day of the week) using the locale's alternative numeric symbols.
-      '%Oy': '%y',                      // Replaced by the year (offset from %C ) using the locale's alternative numeric symbols.
-      '%c': '%a %b %d %H:%M:%S %Y',     // Replaced by the locale's appropriate date and time representation - e.g., Mon Aug  3 14:02:01 2013
-    };
-    for (var rule in EXPANSION_RULES_1) {
-      pattern = pattern.replace(new RegExp(rule, 'g'), EXPANSION_RULES_1[rule]);
-    }
-
-    var WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-    function leadingSomething(value, digits, character) {
-      var str = typeof value == 'number' ? value.toString() : (value || '');
-      while (str.length < digits) {
-        str = character[0]+str;
-      }
-      return str;
-    }
-
-    function leadingNulls(value, digits) {
-      return leadingSomething(value, digits, '0');
-    }
-
-    function compareByDay(date1, date2) {
-      function sgn(value) {
-        return value < 0 ? -1 : (value > 0 ? 1 : 0);
-      }
-
-      var compare;
-      if ((compare = sgn(date1.getFullYear()-date2.getFullYear())) === 0) {
-        if ((compare = sgn(date1.getMonth()-date2.getMonth())) === 0) {
-          compare = sgn(date1.getDate()-date2.getDate());
-        }
-      }
-      return compare;
-    }
-
-    function getFirstWeekStartDate(janFourth) {
-      switch (janFourth.getDay()) {
-        case 0: // Sunday
-          return new Date(janFourth.getFullYear()-1, 11, 29);
-        case 1: // Monday
-          return janFourth;
-        case 2: // Tuesday
-          return new Date(janFourth.getFullYear(), 0, 3);
-        case 3: // Wednesday
-          return new Date(janFourth.getFullYear(), 0, 2);
-        case 4: // Thursday
-          return new Date(janFourth.getFullYear(), 0, 1);
-        case 5: // Friday
-          return new Date(janFourth.getFullYear()-1, 11, 31);
-        case 6: // Saturday
-          return new Date(janFourth.getFullYear()-1, 11, 30);
-      }
-    }
-
-    function getWeekBasedYear(date) {
-      var thisDate = addDays(new Date(date.tm_year+1900, 0, 1), date.tm_yday);
-
-      var janFourthThisYear = new Date(thisDate.getFullYear(), 0, 4);
-      var janFourthNextYear = new Date(thisDate.getFullYear()+1, 0, 4);
-
-      var firstWeekStartThisYear = getFirstWeekStartDate(janFourthThisYear);
-      var firstWeekStartNextYear = getFirstWeekStartDate(janFourthNextYear);
-
-      if (compareByDay(firstWeekStartThisYear, thisDate) <= 0) {
-        // this date is after the start of the first week of this year
-        if (compareByDay(firstWeekStartNextYear, thisDate) <= 0) {
-          return thisDate.getFullYear()+1;
-        }
-        return thisDate.getFullYear();
-      }
-      return thisDate.getFullYear()-1;
-    }
-
-    var EXPANSION_RULES_2 = {
-      '%a': (date) => WEEKDAYS[date.tm_wday].substring(0,3) ,
-      '%A': (date) => WEEKDAYS[date.tm_wday],
-      '%b': (date) => MONTHS[date.tm_mon].substring(0,3),
-      '%B': (date) => MONTHS[date.tm_mon],
-      '%C': (date) => {
-        var year = date.tm_year+1900;
-        return leadingNulls((year/100)|0,2);
-      },
-      '%d': (date) => leadingNulls(date.tm_mday, 2),
-      '%e': (date) => leadingSomething(date.tm_mday, 2, ' '),
-      '%g': (date) => {
-        // %g, %G, and %V give values according to the ISO 8601:2000 standard week-based year.
-        // In this system, weeks begin on a Monday and week 1 of the year is the week that includes
-        // January 4th, which is also the week that includes the first Thursday of the year, and
-        // is also the first week that contains at least four days in the year.
-        // If the first Monday of January is the 2nd, 3rd, or 4th, the preceding days are part of
-        // the last week of the preceding year; thus, for Saturday 2nd January 1999,
-        // %G is replaced by 1998 and %V is replaced by 53. If December 29th, 30th,
-        // or 31st is a Monday, it and any following days are part of week 1 of the following year.
-        // Thus, for Tuesday 30th December 1997, %G is replaced by 1998 and %V is replaced by 01.
-
-        return getWeekBasedYear(date).toString().substring(2);
-      },
-      '%G': getWeekBasedYear,
-      '%H': (date) => leadingNulls(date.tm_hour, 2),
-      '%I': (date) => {
-        var twelveHour = date.tm_hour;
-        if (twelveHour == 0) twelveHour = 12;
-        else if (twelveHour > 12) twelveHour -= 12;
-        return leadingNulls(twelveHour, 2);
-      },
-      '%j': (date) => {
-        // Day of the year (001-366)
-        return leadingNulls(date.tm_mday + arraySum(isLeapYear(date.tm_year+1900) ? MONTH_DAYS_LEAP : MONTH_DAYS_REGULAR, date.tm_mon-1), 3);
-      },
-      '%m': (date) => leadingNulls(date.tm_mon+1, 2),
-      '%M': (date) => leadingNulls(date.tm_min, 2),
-      '%n': () => '\n',
-      '%p': (date) => {
-        if (date.tm_hour >= 0 && date.tm_hour < 12) {
-          return 'AM';
-        }
-        return 'PM';
-      },
-      '%S': (date) => leadingNulls(date.tm_sec, 2),
-      '%t': () => '\t',
-      '%u': (date) => date.tm_wday || 7,
-      '%U': (date) => {
-        var days = date.tm_yday + 7 - date.tm_wday;
-        return leadingNulls(Math.floor(days / 7), 2);
-      },
-      '%V': (date) => {
-        // Replaced by the week number of the year (Monday as the first day of the week)
-        // as a decimal number [01,53]. If the week containing 1 January has four
-        // or more days in the new year, then it is considered week 1.
-        // Otherwise, it is the last week of the previous year, and the next week is week 1.
-        // Both January 4th and the first Thursday of January are always in week 1. [ tm_year, tm_wday, tm_yday]
-        var val = Math.floor((date.tm_yday + 7 - (date.tm_wday + 6) % 7 ) / 7);
-        // If 1 Jan is just 1-3 days past Monday, the previous week
-        // is also in this year.
-        if ((date.tm_wday + 371 - date.tm_yday - 2) % 7 <= 2) {
-          val++;
-        }
-        if (!val) {
-          val = 52;
-          // If 31 December of prev year a Thursday, or Friday of a
-          // leap year, then the prev year has 53 weeks.
-          var dec31 = (date.tm_wday + 7 - date.tm_yday - 1) % 7;
-          if (dec31 == 4 || (dec31 == 5 && isLeapYear(date.tm_year%400-1))) {
-            val++;
-          }
-        } else if (val == 53) {
-          // If 1 January is not a Thursday, and not a Wednesday of a
-          // leap year, then this year has only 52 weeks.
-          var jan1 = (date.tm_wday + 371 - date.tm_yday) % 7;
-          if (jan1 != 4 && (jan1 != 3 || !isLeapYear(date.tm_year)))
-            val = 1;
-        }
-        return leadingNulls(val, 2);
-      },
-      '%w': (date) => date.tm_wday,
-      '%W': (date) => {
-        var days = date.tm_yday + 7 - ((date.tm_wday + 6) % 7);
-        return leadingNulls(Math.floor(days / 7), 2);
-      },
-      '%y': (date) => {
-        // Replaced by the last two digits of the year as a decimal number [00,99]. [ tm_year]
-        return (date.tm_year+1900).toString().substring(2);
-      },
-      // Replaced by the year as a decimal number (for example, 1997). [ tm_year]
-      '%Y': (date) => date.tm_year+1900,
-      '%z': (date) => {
-        // Replaced by the offset from UTC in the ISO 8601:2000 standard format ( +hhmm or -hhmm ).
-        // For example, "-0430" means 4 hours 30 minutes behind UTC (west of Greenwich).
-        var off = date.tm_gmtoff;
-        var ahead = off >= 0;
-        off = Math.abs(off) / 60;
-        // convert from minutes into hhmm format (which means 60 minutes = 100 units)
-        off = (off / 60)*100 + (off % 60);
-        return (ahead ? '+' : '-') + String("0000" + off).slice(-4);
-      },
-      '%Z': (date) => date.tm_zone,
-      '%%': () => '%'
-    };
-
-    // Replace %% with a pair of NULLs (which cannot occur in a C string), then
-    // re-inject them after processing.
-    pattern = pattern.replace(/%%/g, '\0\0')
-    for (var rule in EXPANSION_RULES_2) {
-      if (pattern.includes(rule)) {
-        pattern = pattern.replace(new RegExp(rule, 'g'), EXPANSION_RULES_2[rule](date));
-      }
-    }
-    pattern = pattern.replace(/\0\0/g, '%')
-
-    var bytes = intArrayFromString(pattern, false);
-    if (bytes.length > maxsize) {
-      return 0;
-    }
-
-    writeArrayToMemory(bytes, s);
-    return bytes.length-1;
-  },
-  strftime_l__deps: ['strftime'],
-  strftime_l: (s, maxsize, format, tm, loc) => {
-    return _strftime(s, maxsize, format, tm); // no locale support yet
-  },
-
   strptime__deps: ['$isLeapYear', '$arraySum', '$addDays', '$MONTH_DAYS_REGULAR', '$MONTH_DAYS_LEAP',
                    '$jstoi_q', '$intArrayFromString' ],
   strptime: (buf, format, tm) => {

system/lib/libc/musl/src/time/__tz.c
@@ -9,6 +9,12 @@
 #include "lock.h"
 #include "fork_impl.h"
 
+#ifdef __EMSCRIPTEN__
+#include <stdbool.h>
+#include <pthread.h>
+#include "emscripten_internal.h"
+#endif
+
 #define malloc __libc_malloc
 #define calloc undef
 #define realloc undef
@@ -39,6 +45,7 @@ static size_t old_tz_size = sizeof old_tz_buf;
 static volatile int lock[1];
 volatile int *const __timezone_lockptr = lock;
 
+#ifndef __EMSCRIPTEN__
 static int getint(const char **p)
 {
 	unsigned x;
@@ -122,9 +129,24 @@ static size_t zi_dotprod(const unsigned char *z, const unsigned char *v, size_t 
 	}
 	return y;
 }
+#endif
 
 static void do_tzset()
 {
+#ifdef __EMSCRIPTEN__
+	static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+	static _Atomic bool done_init = false;
+	if (!done_init) {
+		pthread_mutex_lock(&lock);
+		if (!done_init) {
+			_tzset_js(&timezone, &daylight, std_name, dst_name);
+			__tzname[0] = std_name;
+			__tzname[1] = dst_name;
+			done_init = true;
+		}
+		pthread_mutex_unlock(&lock);
+	}
+#else
 	char buf[NAME_MAX+25], *pathname=buf+24;
 	const char *try, *s, *p;
 	const unsigned char *map = 0;
@@ -255,8 +277,10 @@ static void do_tzset()
 
 	if (*s == ',') s++, getrule(&s, r0);
 	if (*s == ',') s++, getrule(&s, r1);
+#endif
 }
 
+#ifndef __EMSCRIPTEN__
 /* Search zoneinfo rules to find the one that applies to the given time,
  * and determine alternate opposite-DST-status rule that may be needed. */
 
@@ -416,6 +440,7 @@ dst:
 	*zonename = __tzname[1];
 	UNLOCK(lock);
 }
+#endif
 
 static void __tzset()
 {

tools/system_libs.py
@@ -1227,6 +1227,11 @@ class libc(MuslInternalLibrary,
           'timespec_get.c',
           'utime.c',
           '__map_file.c',
+          'strftime.c',
+          '__tz.c',
+          '__tm_to_secs.c',
+          '__year_to_secs.c',
+          '__month_to_secs.c',
         ])
     libc_files += files_in_path(
         path='system/lib/libc/musl/src/legacy',
@@ -1285,7 +1290,6 @@ class libc(MuslInternalLibrary,
           'emscripten_scan_stack.c',
           'emscripten_time.c',
           'mktime.c',
-          'tzset.c',
           'kill.c',
           'lookup_name.c',
           'pthread_sigmask.c',
@@ -2192,12 +2196,7 @@ class libstandalonewasm(MuslInternalLibrary):
     # It is more efficient to use JS methods for time, normally.
     files += files_in_path(
         path='system/lib/libc/musl/src/time',
-        filenames=['strftime.c',
-                   '__month_to_secs.c',
-                   '__secs_to_tm.c',
-                   '__tm_to_secs.c',
-                   '__tz.c',
-                   '__year_to_secs.c',
+        filenames=['__secs_to_tm.c',
                    'clock.c',
                    'clock_gettime.c',
                    'gettimeofday.c',

test/core/test_time.out
@@ -58,6 +58,6 @@ time: 2147483648, gmtime: 2038-01-19 03:14:08
 time: -2147483648, gmtime: 1901-12-13 20:45:52
 time: -2147483649, gmtime: 1901-12-13 20:45:51
 time: 253402300799, gmtime: 9999-12-31 23:59:59
-time: -62135596800, gmtime: 1-01-01 00:00:00
+time: -62135596800, gmtime: 0001-01-01 00:00:00
 time: 2211764645, gmtime: 2040-02-02 03:04:05
 success

src/library_sigs.js
@@ -1536,8 +1536,6 @@ sigs = {
   rotozoomSurface__sig: 'ppddi',
   sched_yield__sig: 'i',
   setprotoent__sig: 'vi',
-  strftime__sig: 'ppppp',
-  strftime_l__sig: 'pppppp',
   strptime__sig: 'pppp',
   strptime_l__sig: 'ppppp',
   uuid_clear__sig: 'vp',

system/lib/libc/emscripten_time.c
@@ -15,12 +15,6 @@
 
 #include "emscripten_internal.h"
 
-// Replaces musl's __tz.c
-
-weak long  timezone = 0;
-weak int   daylight = 0;
-weak char *tzname[2] = { 0, 0 };
-
 weak clock_t __clock() {
   static thread_local double start = 0;
   if (!start) {

system/lib/libc/tzset.c
@@ -1,30 +1,0 @@
-/*
- * Copyright 2023 The Emscripten Authors.  All rights reserved.
- * Emscripten is available under two separate licenses, the MIT license and the
- * University of Illinois/NCSA Open Source License.  Both these licenses can be
- * found in the LICENSE file.
- */
-#include <limits.h>
-#include <time.h>
-#include <stdbool.h>
-#include <pthread.h>
-
-#include "emscripten_internal.h"
-
-static char std_name[TZNAME_MAX+1];
-static char dst_name[TZNAME_MAX+1];
-
-weak void tzset() {
-  static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-  static _Atomic bool done_init = false;
-  if (!done_init) {
-    pthread_mutex_lock(&lock);
-    if (!done_init) {
-      _tzset_js(&timezone, &daylight, std_name, dst_name);
-      tzname[0] = std_name;
-      tzname[1] = dst_name;
-      done_init = true;
-    }
-    pthread_mutex_unlock(&lock);
-  }
-}

test/core/test_dlfcn_self.exports
@@ -1,5 +1,6 @@
 __THREW__
 ___environ
+__daylight
 __environ
 __optpos
 __optreset
@@ -9,6 +10,8 @@ __sig_actions
 __sig_pending
 __signgam
 __threwValue
+__timezone
+__tzname
 _environ
 _ns_flagdata
 daylight

commit 553f000979cc39e4b0d9d60de09ef7fb4e633930

Fix gmtime etc in STANDALONE_WASM mode
This was broken in #21379.

Replaces: #22276

src/library.js
@@ -446,15 +446,14 @@ addToLibrary({
 
 #endif
 
+#if !STANDALONE_WASM
   // ==========================================================================
   // assert.h
   // ==========================================================================
 
-#if !STANDALONE_WASM
   __assert_fail: (condition, filename, line, func) => {
     abort(`Assertion failed: ${UTF8ToString(condition)}, at: ` + [filename ? UTF8ToString(filename) : 'unknown filename', line, func ? UTF8ToString(func) : 'unknown function']);
   },
-#endif
 
   // ==========================================================================
   // time.h
@@ -595,6 +594,7 @@ addToLibrary({
     stringToUTF8(s, buf, 26);
     return buf;
   },
+#endif
 
 #if STACK_OVERFLOW_CHECK >= 2
   // Set stack limits used by binaryen's `StackCheck` pass.
@@ -626,6 +626,7 @@ addToLibrary({
     return ret;
   },
 
+#if !STANDALONE_WASM
   _tzset_js__deps: ['$stringToUTF8',
 #if ASSERTIONS
     '$lengthBytesUTF8',
@@ -686,6 +687,7 @@ addToLibrary({
       stringToUTF8(summerName, std_name, {{{ cDefs.TZNAME_MAX + 1 }}});
     }
   },
+#endif
 
   $MONTH_DAYS_REGULAR: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
   $MONTH_DAYS_LEAP: [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],

system/lib/libc/musl/src/time/__tz.c
@@ -15,14 +15,18 @@
 #include "emscripten_internal.h"
 #endif
 
+#if defined(__EMSCRIPTEN__) && !defined(EMSCRIPTEN_STANDALONE_WASM)
+#define USE_EXTERNAL_ZONEINFO
+#endif
+
 #define malloc __libc_malloc
 #define calloc undef
 #define realloc undef
 #define free undef
 
-long  __timezone = 0;
-int   __daylight = 0;
-char *__tzname[2] = { 0, 0 };
+weak long  __timezone = 0;
+weak int   __daylight = 0;
+weak char *__tzname[2] = { 0, 0 };
 
 weak_alias(__timezone, timezone);
 weak_alias(__daylight, daylight);
@@ -30,22 +34,26 @@ weak_alias(__tzname, tzname);
 
 static char std_name[TZNAME_MAX+1];
 static char dst_name[TZNAME_MAX+1];
-const char __utc[] = "UTC";
+weak const char __utc[] = "UTC";
 
 static int dst_off;
 static int r0[5], r1[5];
 
+#ifndef USE_EXTERNAL_ZONEINFO
 static const unsigned char *zi, *trans, *index, *types, *abbrevs, *abbrevs_end;
 static size_t map_size;
+#endif
 
 static char old_tz_buf[32];
 static char *old_tz = old_tz_buf;
 static size_t old_tz_size = sizeof old_tz_buf;
 
 static volatile int lock[1];
-volatile int *const __timezone_lockptr = lock;
-
 #ifndef __EMSCRIPTEN__
+volatile int *const __timezone_lockptr = lock;
+#endif
+
+#ifndef USE_EXTERNAL_ZONEINFO
 static int getint(const char **p)
 {
 	unsigned x;
@@ -133,7 +141,7 @@ static size_t zi_dotprod(const unsigned char *z, const unsigned char *v, size_t 
 
 static void do_tzset()
 {
-#ifdef __EMSCRIPTEN__
+#ifdef USE_EXTERNAL_ZONEINFO
 	static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 	static _Atomic bool done_init = false;
 	if (!done_init) {
@@ -280,7 +288,7 @@ static void do_tzset()
 #endif
 }
 
-#ifndef __EMSCRIPTEN__
+#ifndef USE_EXTERNAL_ZONEINFO
 /* Search zoneinfo rules to find the one that applies to the given time,
  * and determine alternate opposite-DST-status rule that may be needed. */
 
@@ -451,14 +459,16 @@ static void __tzset()
 
 weak_alias(__tzset, tzset);
 
-const char *__tm_to_tzname(const struct tm *tm)
+weak const char *__tm_to_tzname(const struct tm *tm)
 {
 	const void *p = tm->__tm_zone;
 	LOCK(lock);
 	do_tzset();
+#ifndef USE_EXTERNAL_ZONEINFO
 	if (p != __utc && p != __tzname[0] && p != __tzname[1] &&
 	    (!zi || (uintptr_t)p-(uintptr_t)abbrevs >= abbrevs_end - abbrevs))
 		p = "";
+#endif
 	UNLOCK(lock);
 	return p;
 }

tools/system_libs.py
@@ -2197,6 +2197,7 @@ class libstandalonewasm(MuslInternalLibrary):
     files += files_in_path(
         path='system/lib/libc/musl/src/time',
         filenames=['__secs_to_tm.c',
+                   '__tz.c',
                    'clock.c',
                    'clock_gettime.c',
                    'gettimeofday.c',

test/core/test_time.c
@@ -26,10 +26,10 @@ void check_gmtime_localtime(time_t time) {
 
   // gmtime and localtime are only equal when timezone is UTC
   if ((timezone == 0) && (strcmp(gmbuf, locbuf) != 0)) {
-    printf("time: %lld, gmtime: %s != localtime: %s\n", time, gmbuf, locbuf);
+    printf("time: %lld, gmtime: %s != localtime: %s\n", (long long)time, gmbuf, locbuf);
     puts("failed");
   } else {
-    printf("time: %lld, gmtime: %s\n", time, gmbuf);
+    printf("time: %lld, gmtime: %s\n", (long long)time, gmbuf);
   }
 }
 
@@ -38,13 +38,14 @@ int main() {
   time_t summer2002 = 1025528525ll;
   struct tm* tm_ptr;
 
+#ifdef __EMSCRIPTEN__
   // Make sure stime() always fails.
   printf("stime: %d\n", stime(&xmas2002));
+#endif
 
   // Verify that tzname sets *something*.
   tzset();
   printf("tzname[0] set: %d\n", strlen(tzname[0]) >= 3);
-  printf("tzname[1] set: %d\n", strlen(tzname[1]) >= 3);
 
   // Verify gmtime() creates correct struct.
   tm_ptr = gmtime(&xmas2002);
@@ -58,7 +59,8 @@ int main() {
   printf("yday: %d\n", tm_ptr->tm_yday);
   printf("dst: %d\n", tm_ptr->tm_isdst);
   printf("off: %ld\n", (long)tm_ptr->tm_gmtoff);
-  printf("zone: %s\n", tm_ptr->tm_zone);
+  // glibc used "GMT" there whereas musl uses "UTC"
+  assert(strcmp(tm_ptr->tm_zone, "GMT") || strcmp(tm_ptr->tm_zone, "UTC"));
 
   // Verify timegm() reverses gmtime; run through an entire year in half hours.
   int timegmOk = 1;
@@ -89,7 +91,7 @@ int main() {
   printf("localtime found DST data (summer): %s\n", tm_summer.tm_isdst < 0 ? "no" : "yes");
   printf("localtime found DST data (winter): %s\n", tm_winter.tm_isdst < 0 ? "no" : "yes");
   int localeHasDst = tm_winter.tm_isdst == 1 || tm_summer.tm_isdst == 1; // DST is in December in south
-  printf("localtime matches daylight: %s\n", localeHasDst == _daylight ? "yes" : "no");
+  printf("localtime matches daylight: %s\n", localeHasDst == daylight ? "yes" : "no");
   int goodGmtOff = (tm_winter.tm_gmtoff != tm_summer.tm_gmtoff) == localeHasDst;
   printf("localtime gmtoff matches DST: %s\n", goodGmtOff ? "yes" : "no");
   printf("localtime tm_zone matches tzname (winter): %s\n",
@@ -169,9 +171,7 @@ int main() {
   struct tm tm_big = {0};
   tm_big.tm_year = 292278994;
   time_t tbig = mktime(&tm_big);
-  printf("tbig: %lld\n", tbig);
-  assert(tbig == -1);
-  assert(errno == EOVERFLOW);
+  assert((tbig == -1 && errno == EOVERFLOW) || tbig == 9223431975273600);
 
   // Verify localtime_r() doesn't clobber static data.
   time_t t3 = 60*60*24*5; // Jan 5 1970

test/core/test_time.out
@@ -1,6 +1,5 @@
 stime: -1
 tzname[0] set: 1
-tzname[1] set: 1
 sec: 43
 min: 22
 hour: 3
@@ -11,7 +10,6 @@ wday: 3
 yday: 358
 dst: 0
 off: 0
-zone: GMT
 timegm <-> gmtime: 1
 old year still: 102
 new year: 70
@@ -27,7 +25,6 @@ mktime updates parameter to be in range: 1
 mktime parameter is equivalent to localtime return: 1
 mktime guesses DST (winter): 1
 mktime guesses DST (summer): 1
-tbig: -1
 old year still: 102
 new year: 70
 time: 1

test/test_core.py
@@ -2668,6 +2668,7 @@ The current type of b is: 9
   def test_tcgetattr(self):
     self.do_runf('termios/test_tcgetattr.c', 'success')
 
+  @also_with_standalone_wasm()
   def test_time(self):
     self.do_core_test('test_time.c')
     for tz in ['EST+05EDT', 'UTC+0', 'CET']: