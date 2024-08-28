86008d420cf7c4b178c519faba41739623850fbb
Sam Clegg <sbc@chromium.org>
2024-08-28 10:26:28 -0700

    [js-compiler] Reuse compile time context using `vm.runInNewContex`. NFC

--- src/utility.mjs
+++ src/utility.mjs
@@ -301,10 +301,10 @@ export class Benchmarker {
  * global scope of the compiler itself which avoids exposing all of the compiler
  * internals to user JS library code.
  */
-export const compileTimeContext = {
+export const compileTimeContext = vm.createContext({
   process,
   console,
-};
+});
 
 /**
  * A symbols to the macro context.
@@ -330,7 +330,7 @@ export function loadSettingsFile(f) {
 export function runInMacroContext(code, options) {
   compileTimeContext['__filename'] = options.filename;
   compileTimeContext['__dirname'] = path.dirname(options.filename);
-  return vm.runInNewContext(code, compileTimeContext, options);
+  return vm.runInContext(code, compileTimeContext, options);
 }
 
 addToCompileTimeContext({
