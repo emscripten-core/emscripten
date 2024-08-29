0f4e8abedd3e17011df5f7ff269fa384bf658154
Sam Clegg <sbc@chromium.org>
2024-08-29 12:42:06 -0700

    feedback

--- system/lib/pthread/pthread_kill.c
+++ system/lib/pthread/pthread_kill.c
@@ -16,11 +16,18 @@
 void do_raise(void* arg) {
   int sig = (intptr_t)arg;
   if (sig == SIGCANCEL) {
-    // For SIGCANCEL is simply enough to run the proxied function since we call
-    // pthread_testcancel at the end of _emscripten_check_mailbox.  We
-    // can't/don't call pthread_testcancel or pthread_exit here because the
-    // proxying system itself is on the stack and we want to exit in clean
-    // state. (e.g. without holding any locks).
+    // For `SIGCANCEL` there is no need to actually call raise to run the
+    // handler function.  The calling then (the one calling `pthread_cancel`)
+    // will already have marked us as being cancelled.  All we need to do is
+    // ensure that `pthread_testcancel` is eventually called and that will cause
+    // this thread to exit.  We can't call `pthread_testcancel` here (since we
+    // are being called from the proxy queue process and we don't want to leave
+    // that in a bad state by unwinding).  Instead, we rely on
+    // `pthread_testcancel` at the end of `_emscripten_check_mailbox`.  Before
+    // we return though we do want to make sure we clear the keepalive state so
+    // that the thread will exit even if it has a reason to stay alive.
+    // TODO(sbc): Is this the correct behaviour, should `pthread_cancel`
+    // instead wait for threads to be done with outstanding work/event loops?
     _emscripten_runtime_keepalive_clear();
     return;
   }
@@ -40,7 +47,7 @@ int pthread_kill(pthread_t t, int sig) {
   }
   if (sig == 0) return 0; // signal == 0 is a no-op.
 
-  // The job of pthread_kill is basically the run the (process-wide) signal
+  // The job of pthread_kill is basically to run the (process-wide) signal
   // handler on the target thread.
   emscripten_proxy_async(emscripten_proxy_get_system_queue(), t, do_raise, (void*)(intptr_t)sig);
   return 0;
