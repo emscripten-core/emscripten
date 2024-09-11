57b3cc096e0a13f0edd779a7fcf12c8a8629d1c4
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-09-11 20:56:34 +0200

    Prefer relative path

--- test/test_other.py
+++ test/test_other.py
@@ -9733,7 +9733,7 @@ end
   def test_noderawfs_readfile_prerun(self):
     create_file('foo', 'bar')
     create_file('pre.js', f'''
-      Module.preRun = () => console.log(FS.readFile('{os.path.abspath('foo')}', {{ encoding: 'utf8' }}));
+      Module.preRun = () => console.log(FS.readFile('foo', {{ encoding: 'utf8' }}));
     ''')
     self.do_runf(test_file('hello_world.c'), 'bar\n', emcc_args=['--pre-js', 'pre.js', '-sNODERAWFS', '-sFORCE_FILESYSTEM'])
 
