3a5a16729579ebb344f802a123f3372606005bdc
Jeroen Pfeil <jeroen.pfeil@automattic.com>
2024-09-30 11:35:36 +0700

    Fix test and use 2-space indentation

--- test/fs/test_nodefs_statvfs.c
+++ test/fs/test_nodefs_statvfs.c
@@ -4,32 +4,32 @@
 #include <emscripten.h>
 
 void test_statvfs(const char *path) {
-    printf("Testing statfs for path: %s\n", path);
-    struct statvfs st;
-    int result = statvfs(path, &st);
+  printf("Testing statfs for path: %s\n", path);
+  struct statvfs st;
+  int result = statvfs(path, &st);
 
-    assert(result == 0 && "statvfs should succeed");
+  assert(result == 0 && "statvfs should succeed");
 
-    // Basic sanity checks
-    assert(st.f_bsize > 0 && "Block size should be positive");
-    assert(st.f_blocks > 0 && "Total blocks should be positive");
-    assert(st.f_bfree <= st.f_blocks && "Free blocks should not exceed total blocks");
-    assert(st.f_bavail <= st.f_bfree && "Available blocks should not exceed free blocks");
-    assert(st.f_files > 0 && "Total inodes should be positive");
-    assert(st.f_ffree <= st.f_files && "Free inodes should not exceed total inodes");
+  // Basic sanity checks
+  assert(st.f_bsize > 0 && "Block size should be positive");
+  assert(st.f_blocks > 0 && "Total blocks should be positive");
+  assert(st.f_bfree <= st.f_blocks && "Free blocks should not exceed total blocks");
+  assert(st.f_bavail <= st.f_bfree && "Available blocks should not exceed free blocks");
+  assert(st.f_files >= 0 && "Total inodes should be 0 or positive");
+  assert(st.f_ffree <= st.f_files && "Free inodes should not exceed total inodes");
 }
 
 void setup() {
-	EM_ASM(
-		FS.mkdir('/working');
-		FS.mount(NODEFS, { root: '.' }, '/working');
-	);
+  EM_ASM(
+    FS.mkdir('/working');
+    FS.mount(NODEFS, { root: '.' }, '/working');
+  );
 }
 
 int main() {
-	setup();
-    // Test the root filesystem (which should be MEMFS by default)
-    test_statvfs("/");
-    test_statvfs("/working");
-    puts("success");
+  setup();
+  // Test the root filesystem (which should be MEMFS by default)
+  test_statvfs("/");
+  test_statvfs("/working");
+  puts("success");
 }
