b2bac1e6853ab4f65577517b984ca63a674fa62a
Jukka Jylänki <jujjyl@gmail.com>
2024-09-30 14:54:40 +0300

    Address review

--- src/library_wasi.js
+++ src/library_wasi.js
@@ -270,6 +270,7 @@ var WasiLibrary = {
 #else
   fd_write__deps: ['$printChar'],
 #endif
+  fd_write__proxy: 'none', // Each Worker can do stdout/stderr printing on its own, without needing to proxy to the main thread.
   fd_write: (fd, iov, iovcnt, pnum) => {
 #if SYSCALLS_REQUIRE_FILESYSTEM
     var stream = SYSCALLS.getStreamFromFD(fd);
@@ -295,6 +296,7 @@ var WasiLibrary = {
   fd_pwrite__deps: ['$doWritev'],
 #endif
   fd_pwrite__i53abi: true,
+  fd_pwrite__proxy: 'none', // Each Worker can do stdout/stderr printing on its own, without needing to proxy to the main thread.
   fd_pwrite: (fd, iov, iovcnt, offset, pnum) => {
 #if SYSCALLS_REQUIRE_FILESYSTEM
     if (isNaN(offset)) return {{{ cDefs.EOVERFLOW }}};
--- test/pthread/proxy_abort.c
+++ test/pthread/proxy_abort.c
@@ -6,8 +6,7 @@
 
 void proxied_js_function(void);
 
-int might_throw(void(*func)())
-{
+int might_throw(void(*func)()) {
   int threw = EM_ASM_INT({
     // Patch over assert() so that it does not abort execution on assert failure, but instead
     // throws a catchable exception.
@@ -29,21 +28,18 @@ int might_throw(void(*func)())
   return threw;
 }
 
-void test()
-{
+void test() {
   proxied_js_function();
 }
 
-void *thread_main(void *arg)
-{
+void *thread_main(void *arg) {
   REPORT_RESULT(might_throw(test));
   return 0;
 }
 
 char stack[1024];
 
-int main()
-{
+int main() {
   proxied_js_function(); // Should be callable from main thread
 
   pthread_t thread;
