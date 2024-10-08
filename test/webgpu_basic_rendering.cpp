emscripten-core/emscripten

commit d0d9966daa064eef3281495d9168a5efbef3f692

Add some sanity checks when using `__i53abi` decorator (#22557)
See #21335

src/library_webgpu.js
@@ -2883,7 +2883,10 @@ for (var value in LibraryWebGPU.$WebGPU.FeatureName) {
 
 for (const key of Object.keys(LibraryWebGPU)) {
   if (typeof LibraryWebGPU[key] === 'function') {
-    LibraryWebGPU[key + '__i53abi'] = true;
+    const sig = LibraryWebGPU[key + '__sig'];
+    if (sig?.includes('j')) {
+      LibraryWebGPU[key + '__i53abi'] = true;
+    }
   }
 }
 

src/jsifier.mjs
@@ -299,6 +299,16 @@ function(${args}) {
 
     const sig = LibraryManager.library[symbol + '__sig'];
     const i53abi = LibraryManager.library[symbol + '__i53abi'];
+    if (i53abi) {
+      if (!sig) {
+        error(`JS library error: '__i53abi' decorator requires '__sig' decorator: '${symbol}'`);
+      }
+      if (!sig.includes('j')) {
+        error(
+          `JS library error: '__i53abi' only makes sense when '__sig' includes 'j' (int64): '${symbol}'`,
+        );
+      }
+    }
     if (
       sig &&
       ((i53abi && sig.includes('j')) || ((MEMORY64 || CAN_ADDRESS_2GB) && sig.includes('p')))

test/test_other.py
@@ -4681,6 +4681,26 @@ addToLibrary({
     err = self.expect_fail([EMCC, test_file('hello_world.c'), '--js-library', 'lib.js'])
     self.assertContained("lib.js: Decorator (jslibfunc__async} has wrong type. Expected 'boolean' not 'string'", err)
 
+  def test_js_lib_i53abi(self):
+    create_file('lib.js', r'''
+mergeInto(LibraryManager.library, {
+  jslibfunc__i53abi: true,
+  jslibfunc: (x) => { return 42 },
+});
+''')
+    err = self.expect_fail([EMCC, test_file('hello_world.c'), '-sDEFAULT_LIBRARY_FUNCS_TO_INCLUDE=jslibfunc', '--js-library', 'lib.js'])
+    self.assertContained("error: JS library error: '__i53abi' decorator requires '__sig' decorator: 'jslibfunc'", err)
+
+    create_file('lib.js', r'''
+mergeInto(LibraryManager.library, {
+  jslibfunc__i53abi: true,
+  jslibfunc__sig: 'ii',
+  jslibfunc: (x) => { return 42 },
+});
+''')
+    err = self.expect_fail([EMCC, test_file('hello_world.c'), '-sDEFAULT_LIBRARY_FUNCS_TO_INCLUDE=jslibfunc', '--js-library', 'lib.js'])
+    self.assertContained("error: JS library error: '__i53abi' only makes sense when '__sig' includes 'j' (int64): 'jslibfunc'", err)
+
   def test_js_lib_legacy(self):
     create_file('lib.js', r'''
 mergeInto(LibraryManager.library, {

commit 65fe7c286aefde61f4055b52f74311a476275b93

Fix regression in i64 argument handling in webgpu+bigint
This was caused by #22557, which was checking the `LibraryWebGPU` object
for `__sig` members when the sigs are stored in a separate file and
therefore live on the global `LibraryManager.library` object.

Fixes: #22682

src/library_webgpu.js
@@ -2883,9 +2883,9 @@ for (var value in LibraryWebGPU.$WebGPU.FeatureName) {
 
 for (const key of Object.keys(LibraryWebGPU)) {
   if (typeof LibraryWebGPU[key] === 'function') {
-    const sig = LibraryWebGPU[key + '__sig'];
+    const sig = LibraryManager.library[key + '__sig'];
     if (sig?.includes('j')) {
-      LibraryWebGPU[key + '__i53abi'] = true;
+      LibraryManager.library[key + '__i53abi'] = true;
     }
   }
 }

test/webgpu_basic_rendering.cpp
@@ -225,6 +225,11 @@ void doCopyTestMappedAtCreation(bool useRange) {
         dst = device.CreateBuffer(&descriptor);
     }
 
+    // Write some random data to the buffer, just to verify that
+    // wgpuQueueWriteBuffer works.
+    char data[4];
+    queue.WriteBuffer(dst, 0, data, sizeof(data));
+
     wgpu::CommandBuffer commands;
     {
         wgpu::CommandEncoder encoder = device.CreateCommandEncoder();