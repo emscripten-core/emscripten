814ec05f74eac9025c0442f51fa09bd8d02f8b43
Sam Clegg <sbc@chromium.org>
2024-10-16 14:55:30 -0700

    Fix `__errno_location` under wasm workers. NFC (#22744)
    Previously the wasm workers build of `__errno_location` was using the
    single threaded code path which doesn't use a thead local location.
    
    In addition this change avoids the use of `__EMSCRIPTEN_PTHREADS__` in
    `__errno_location.c` which is helpful for unifying the shared memory
    libc build.  See #22735.

--- test/wasm_worker/thread_stack.c
+++ test/wasm_worker/thread_stack.c
@@ -1,10 +1,12 @@
 #include <emscripten/console.h>
 #include <emscripten/wasm_worker.h>
 #include <emscripten/stack.h>
+#include <emscripten/console.h>
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 
+#define ALIGN(x,y) ((x)+(y)-1 & -(y))
 #define THREAD_STACK_SIZE 2048
 #define NUM_THREADS 2
 void *thread_stack[NUM_THREADS];
@@ -18,7 +20,11 @@ void test_stack(int i) {
     (void*)emscripten_stack_get_end(),
     THREAD_STACK_SIZE);
   assert(emscripten_stack_get_base() == (uintptr_t)thread_stack[i] + THREAD_STACK_SIZE);
-  assert(emscripten_stack_get_end() == (uintptr_t)thread_stack[i]);
+  emscripten_outf("__builtin_wasm_tls_size: %lu", __builtin_wasm_tls_size());
+  // The stack region in wasm workers also incldues TLS, so we need to take
+  // this into account when calulating our expected stack end.
+  size_t expected_stack_end = (uintptr_t)thread_stack[i] + ALIGN(__builtin_wasm_tls_size(), 16);
+  assert(emscripten_stack_get_end() == expected_stack_end);
 
   int ok = __sync_fetch_and_add(&threadsOk, 1);
   emscripten_outf("%d", ok);
@@ -36,7 +42,7 @@ int main() {
   for (int i = 0; i < NUM_THREADS; ++i) {
     thread_stack[i] = memalign(16, THREAD_STACK_SIZE);
     emscripten_wasm_worker_t worker = emscripten_create_wasm_worker(thread_stack[i], THREAD_STACK_SIZE);
-    emscripten_outf("Created thread %d with stack ptr=%p, size=%x", i, thread_stack[i], THREAD_STACK_SIZE);
+    emscripten_outf("Created thread %d with stack ptr=%p, end=%p, size=%x", i, thread_stack[i], thread_stack[i] + THREAD_STACK_SIZE, THREAD_STACK_SIZE);
     emscripten_wasm_worker_post_function_vi(worker, test_stack, i);
   }
 }
