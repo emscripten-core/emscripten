b53978ee3f540dc74761eba127aa7f1b8761a125
Jorrit Rouwe <jrouwe@gmail.com>
2024-10-23 00:47:19 +0200

    WebIDL Binder: Allow all binary C++ operators (#22770)
    This expands the list of supported operators by the 'Operator' WebIDL
    attribute to all binary operators that are supported by C++.
    
    Helps #9415

--- tools/webidl_binder.py
+++ tools/webidl_binder.py
@@ -614,7 +614,10 @@ def render_function(class_name, func_name, sigs, return_type, non_pointer,
         # this function comes from an ancestor class; for operators, we must cast it
         cast_self = 'dynamic_cast<' + type_to_c(func_scope) + '>(' + cast_self + ')'
       maybe_deref = deref_if_nonpointer(raw[0])
-      if '=' in operator:
+      operator = operator.strip()
+      if operator in ["+", "-", "*", "/", "%", "^", "&", "|", "=",
+                      "<", ">", "+=", "-=", "*=", "/=", "%=", "^=", "&=", "|=", "<<", ">>", ">>=",
+                      "<<=", "==", "!=", "<=", ">=", "<=>", "&&", "||"]:
         call = '(*%s %s %s%s)' % (cast_self, operator, maybe_deref, args[0])
       elif operator == '[]':
         call = '((*%s)[%s%s])' % (cast_self, maybe_deref, args[0])
