31f9fb3c71b1aacf5edf65e35515d41b59c10391
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-10-29 22:11:49 +0100

    Fix `ENVIRONMENT_IS_WORKER` detection on Deno (#22778)
    `importScripts()` is always `undefined` on Deno.

--- src/runtime_pthread.js
+++ src/runtime_pthread.js
@@ -31,14 +31,6 @@ if (ENVIRONMENT_IS_PTHREAD) {
 
     Object.assign(globalThis, {
       self: global,
-      // Dummy importScripts.  The presence of this global is used
-      // to detect that we are running on a Worker.
-      // TODO(sbc): Find another way?
-      importScripts: () => {
-#if ASSERTIONS
-        assert(false, 'dummy importScripts called');
-#endif
-      },
       postMessage: (msg) => parentPort.postMessage(msg),
     });
   }
