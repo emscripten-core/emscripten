40144d337a7560e2a3823e0262a739584b81f7fe
İbrahim Uysal <uysalibov@gmail.com>
2024-11-07 23:37:07 +0300

    [GLFW] Fix the bug that prevents any text on the site from being deleted

--- src/library_glfw.js
+++ src/library_glfw.js
@@ -419,7 +419,10 @@ var LibraryGLFW = {
       // This logic comes directly from the sdl implementation. We cannot
       // call preventDefault on all keydown events otherwise onKeyPress will
       // not get called
-      if (event.keyCode === 8 /* backspace */ || event.keyCode === 9 /* tab */) {
+      // Prevent default backspace and tab behavior when the target 
+      // is not an input or textarea. Otherwise, no text in the site can be deleted.
+      if (event.target.tagName !== "INPUT" && event.target.tagName !== "TEXTAREA" &&
+        (event.keyCode === 8 /* backspace */ || event.keyCode === 9 /* tab */)) {
         event.preventDefault();
       }
     },
