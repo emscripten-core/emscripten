b16e887a0da110ad5ea9facd4ce90451dfa3cd6f
Qijia Liu <liumeo@pku.edu.cn>
2024-11-24 18:01:40 -0500

    address review comments

--- test/cmake/whole_archive/lib.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-static void init(void) __attribute__((constructor));
-static void init(void) {
-    std::cout << "init" << std::endl;
-}
--- test/cmake/whole_archive/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "main" << std::endl;
-}
