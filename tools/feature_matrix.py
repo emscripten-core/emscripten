747201c558aa7a4b40fa8c07a714dc9bb8558259
Derek Schuff <dschuff@chromium.org>
2024-11-26 14:26:01 -0800

    review suggestions

--- tools/feature_matrix.py
+++ tools/feature_matrix.py
@@ -167,7 +167,7 @@ def apply_min_browser_versions():
   if settings.PTHREADS:
     enable_feature(Feature.THREADS, 'pthreads')
     enable_feature(Feature.BULK_MEMORY, 'pthreads')
-  if settings.WASM_WORKERS or settings.SHARED_MEMORY:
+  elif settings.WASM_WORKERS or settings.SHARED_MEMORY:
     enable_feature(Feature.BULK_MEMORY, 'wasm-workers')
   if settings.AUDIO_WORKLET:
     enable_feature(Feature.GLOBALTHIS, 'AUDIO_WORKLET')
