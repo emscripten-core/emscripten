1f5a17e0863df9db1d23fc75bc9bc2d1b0b4e5d1
Hood Chatham <roberthoodchatham@gmail.com>
2024-11-26 11:47:46 +0100

    Rename option and add comment

--- src/library_fs.js
+++ src/library_fs.js
@@ -203,7 +203,10 @@ FS.staticInit();
         try {
           current = FS.lookupNode(current, parts[i]);
         } catch (e) {
-          if ((e?.errno === {{{ cDefs.ENOENT }}}) && islast && opts.handleBrokenLink) {
+          // if noent_okay is true, suppress a ENOENT in the last component
+          // and return an object with an undefined node. This is needed for
+          // resolving symlinks in the path when creating a file.
+          if ((e?.errno === {{{ cDefs.ENOENT }}}) && islast && opts.noent_okay) {
             return { path: current_path };
           }
           throw e;
@@ -1038,9 +1041,12 @@ FS.staticInit();
         node = path;
       } else {
         path = PATH.normalize(path);
+        // noent_okay makes it so that if the final component of the path
+        // doesn't exist, lookupPath returns `node: undefined`. `path` will be
+        // updated to point to the target of all symlinks.
         var lookup = FS.lookupPath(path, {
           follow: !(flags & {{{ cDefs.O_NOFOLLOW }}}),
-          handleBrokenLink: true
+          noent_okay: true
         });
         node = lookup.node;
         path = lookup.path;
