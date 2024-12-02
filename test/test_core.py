513588ac66172e6fc8133ab70e2b301de8e4526c
Sam Clegg <sbc@chromium.org>
2024-12-02 14:56:01 -0800

    [test] Avoid seeding random number generators with time. NFC
    Using the current time to seed the random number generator in tests
    will make them non-determinisitic.  Simply using the default seed or
    seeding with a fixed value seem fine all of these test cases.

--- test/test_core.py
+++ test/test_core.py
@@ -5155,68 +5155,7 @@ int main()
     self.do_run(src, '956867869')
 
   def test_rand(self):
-    src = r'''#include <stdlib.h>
-#include <stdio.h>
-#include <assert.h>
-int main()
-{
-    // we need RAND_MAX to be a bitmask (power of 2 minus 1). this assertions guarantees
-    // if RAND_MAX changes the test failure will focus attention on that issue here.
-    assert(RAND_MAX == 0x7fffffff);
-
-    srand(0xdeadbeef);
-    for(int i = 0; i < 10; ++i)
-        printf("%d\n", rand());
-
-    unsigned int seed = 0xdeadbeef;
-    for(int i = 0; i < 10; ++i)
-        printf("%d\n", rand_r(&seed));
-
-    bool haveEvenAndOdd = true;
-    for(int i = 1; i <= 30; ++i)
-    {
-        int mask = 1 << i;
-        if (mask > RAND_MAX) break;
-        bool haveEven = false;
-        bool haveOdd = false;
-        for(int j = 0; j < 1000 && (!haveEven || !haveOdd); ++j)
-        {
-            if ((rand() & mask) == 0)
-                haveEven = true;
-            else
-                haveOdd = true;
-        }
-        haveEvenAndOdd = haveEvenAndOdd && haveEven && haveOdd;
-    }
-    if (haveEvenAndOdd)
-        printf("Have even and odd!\n");
-
-    return 0;
-}
-'''
-    expected = '''490242850
-2074599277
-1480056542
-1912638067
-931112055
-2110392489
-2053422194
-1614832492
-216117595
-174823244
-760368382
-602359081
-1121118963
-1291018924
-1608306807
-352705809
-958258461
-1182561381
-114276303
-1481323674
-Have even and odd!
-'''
-    self.do_run(src, expected)
+    self.do_core_test('test_rand.c')
 
   def test_strtod(self):
     self.do_core_test('test_strtod.c')
