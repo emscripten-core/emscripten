emscripten-core/emscripten

commit 48d9bc0bb930ee3629c518678faf1a9e1449f14f

Remove automatic fallback to $HOME/.emscripten_cache. NFC (#22801)
Putting the cache in the `$HOME` directory is not a great idea since its
a location that is shared by any/all versions of emscripten. If anyone
really wants to use that location they can do so via the config file
setting `CACHE` or via the environment variable `EMCC_CACHE`.

tools/cache.py
@@ -23,12 +23,19 @@ cachelock = None
 cachelock_name = None
 
 
+def is_writable(path):
+  return os.access(path, os.W_OK)
+
+
 def acquire_cache_lock(reason):
   global acquired_count
   if config.FROZEN_CACHE:
     # Raise an exception here rather than exit_with_error since in practice this
     # should never happen
     raise Exception('Attempt to lock the cache but FROZEN_CACHE is set')
+
+  if not is_writable(cachedir):
+    utils.exit_with_error(f'cache directory "{cachedir}" is not writable while accessing cache for: {reason} (see https://emscripten.org/docs/tools_reference/emcc.html for info on setting the cache directory)')
 
   if acquired_count == 0:
     logger.debug(f'PID {os.getpid()} acquiring multiprocess file lock to Emscripten cache at {cachedir}')
@@ -67,7 +74,11 @@ def lock(reason):
 
 def ensure():
   ensure_setup()
-  utils.safe_ensure_dirs(cachedir)
+  if not os.path.isdir(cachedir):
+    parent_dir = os.path.dirname(cachedir)
+    if not is_writable(parent_dir):
+      utils.exit_with_error('unable to create cache directory "{cachdir}": parent directory not writable (see https://emscripten.org/docs/tools_reference/emcc.html for info on setting the cache directory)')
+    utils.safe_ensure_dirs(cachedir)
 
 
 def erase():

tools/config.py
@@ -55,10 +55,6 @@ def fix_js_engine(old, new):
   return new
 
 
-def root_is_writable():
-  return os.access(__rootpath__, os.W_OK)
-
-
 def normalize_config_settings():
   global CACHE, PORTS, LLVM_ADD_VERSION, CLANG_ADD_VERSION, CLOSURE_COMPILER
   global NODE_JS, NODE_JS_TEST, V8_ENGINE, JS_ENGINES, SPIDERMONKEY_ENGINE, WASM_ENGINES
@@ -80,16 +76,7 @@ def normalize_config_settings():
   WASM_ENGINES = [listify(engine) for engine in WASM_ENGINES]
   CLOSURE_COMPILER = listify(CLOSURE_COMPILER)
   if not CACHE:
-    if FROZEN_CACHE or root_is_writable():
-      CACHE = path_from_root('cache')
-    else:
-      # Use the legacy method of putting the cache in the user's home directory
-      # if the emscripten root is not writable.
-      # This is useful mostly for read-only installation and perhaps could
-      # be removed in the future since such installations should probably be
-      # setting a specific cache location.
-      logger.debug('Using home-directory for emscripten cache due to read-only root')
-      CACHE = os.path.expanduser(os.path.join('~', '.emscripten_cache'))
+    CACHE = path_from_root('cache')
   if not PORTS:
     PORTS = os.path.join(CACHE, 'ports')
 
@@ -274,10 +261,6 @@ def find_config_file():
     return emsdk_embedded_config
 
   if os.path.isfile(user_home_config):
-    return user_home_config
-
-  # No config file found.  Return the default location.
-  if not root_is_writable():
     return user_home_config
 
   return embedded_config

commit e4da2627e51d4fcfe3418ce25f5c10a2d429bc05

Fix false positive writability check on cache directory
We were incorrectly reporting non-existent parent directories as
non-writable.

This was broken in #22801.

tools/cache.py
@@ -75,10 +75,10 @@ def lock(reason):
 def ensure():
   ensure_setup()
   if not os.path.isdir(cachedir):
-    parent_dir = os.path.dirname(cachedir)
-    if not is_writable(parent_dir):
-      utils.exit_with_error(f'unable to create cache directory "{cachedir}": parent directory not writable (see https://emscripten.org/docs/tools_reference/emcc.html for info on setting the cache directory)')
-    utils.safe_ensure_dirs(cachedir)
+    try:
+      utils.safe_ensure_dirs(cachedir)
+    except Exception as e:
+      utils.exit_with_error(f'unable to create cache directory "{cachedir}": {e} (see https://emscripten.org/docs/tools_reference/emcc.html for info on setting the cache directory)')
 
 
 def erase():

test/test_other.py
@@ -15412,3 +15412,25 @@ addToLibrary({
        return 0;
     }''')
     self.do_runf('main.cpp', 'Hello from rust!', emcc_args=[lib])
+
+  @crossplatform
+  def test_create_cache_directory(self):
+    if config.FROZEN_CACHE:
+      self.skipTest("test doesn't work with frozen cache")
+
+    # Test that the cache directory (including parent directories) is
+    # created on demand.
+    with env_modify({'EM_CACHE': os.path.abspath('foo/bar')}):
+      self.run_process([EMCC, '-c', test_file('hello_world.c')])
+      self.assertExists('foo/bar/sysroot_install.stamp')
+
+    if not WINDOWS:
+      # Test that we generate a nice error when we cannot create the cache
+      # because it is in a read-only location.
+      # For some reason this doesn't work on windows, at least not in CI.
+      os.mkdir('rodir')
+      os.chmod('rodir', 0o444)
+      self.assertFalse(os.access('rodir', os.W_OK))
+      with env_modify({'EM_CACHE': os.path.abspath('rodir/foo')}):
+        err = self.expect_fail([EMCC, '-c', test_file('hello_world.c')])
+        self.assertContained('emcc: error: unable to create cache directory', err)