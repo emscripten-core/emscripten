6304e9c53c790fc3aa11c68c5ac10abb5ba18ff8
Hood Chatham <roberthoodchatham@gmail.com>
2024-12-04 22:27:48 +0100

    Inline strings

--- test/unistd/test_unistd_write_broken_link.c
+++ test/unistd/test_unistd_write_broken_link.c
@@ -6,17 +6,14 @@
 
 
 int main() {
-  char* p1 = "link_source";
-  char* p2 = "link_target";
-
-  int res = symlink(p2, p1);
+  int res = symlink("link_target", "link_source");
   printf("link result: %d\n", res);
-  int src_fd = open(p1, O_CREAT | O_WRONLY, 0777);
+  int src_fd = open("link_source", O_CREAT | O_WRONLY, 0777);
   printf("source_fd: %d, errno: %d %s\n", src_fd, errno, strerror(errno));
   write(src_fd, "abc", 3);
   close(src_fd);
   {
-    int target_fd = open(p2, O_RDONLY);
+    int target_fd = open("link_target", O_RDONLY);
     printf("target_fd: %d, errno: %d %s\n", target_fd, errno, strerror(errno));
     char buf[10];
     read(target_fd, buf, 10);
@@ -24,7 +21,7 @@ int main() {
     close(target_fd);
   }
   {
-    int target_fd = open(p1, O_RDONLY);
+    int target_fd = open("link_source", O_RDONLY);
     printf("target_fd: %d, errno: %d %s\n", target_fd, errno, strerror(errno));
     char buf[10];
     read(target_fd, buf, 10);
