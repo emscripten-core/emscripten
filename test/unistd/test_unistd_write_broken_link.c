0cdb50f25dd2cae786f611aea8c3cd1da893ea30
Derek Schuff <dschuff@chromium.org>
2024-12-10 12:49:07 -0800

    Fix UB in test_unistd_write_broken_link
    The test was failing under ASan because no null byte was being
    read from the file, so the strlen inside printf was running
    off the buffer. It just happened to work on non-ASan because
    wasm memory is initialized to zero.

--- test/unistd/test_unistd_write_broken_link.c
+++ test/unistd/test_unistd_write_broken_link.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -10,13 +11,14 @@ int main() {
   printf("link result: %d\n", res);
   int src_fd = open("link_source", O_CREAT | O_WRONLY, 0777);
   printf("source_fd: %d, errno: %d %s\n", src_fd, errno, strerror(errno));
-  write(src_fd, "abc", 3);
+  write(src_fd, "abc\0", 4);
   close(src_fd);
   {
     int target_fd = open("link_target", O_RDONLY);
     printf("target_fd: %d, errno: %d %s\n", target_fd, errno, strerror(errno));
     char buf[10];
-    read(target_fd, buf, 10);
+    size_t r = read(target_fd, buf, 10);
+    assert(r == 4);
     printf("buf: '%s'\n", buf);
     close(target_fd);
   }
@@ -24,7 +26,8 @@ int main() {
     int target_fd = open("link_source", O_RDONLY);
     printf("target_fd: %d, errno: %d %s\n", target_fd, errno, strerror(errno));
     char buf[10];
-    read(target_fd, buf, 10);
+    size_t r = read(target_fd, buf, 10);
+    assert(r == 4);
     printf("buf: '%s'\n", buf);
     close(target_fd);
   }
