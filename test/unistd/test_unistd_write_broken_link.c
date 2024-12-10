c712243eec3453c2644ad19232e3d225a37e3552
Derek Schuff <dschuff@chromium.org>
2024-12-10 15:07:16 -0800

    memset

--- test/unistd/test_unistd_write_broken_link.c
+++ test/unistd/test_unistd_write_broken_link.c
@@ -11,14 +11,15 @@ int main() {
   printf("link result: %d\n", res);
   int src_fd = open("link_source", O_CREAT | O_WRONLY, 0777);
   printf("source_fd: %d, errno: %d %s\n", src_fd, errno, strerror(errno));
-  write(src_fd, "abc\0", 4);
+  write(src_fd, "abc", 3);
   close(src_fd);
   {
     int target_fd = open("link_target", O_RDONLY);
     printf("target_fd: %d, errno: %d %s\n", target_fd, errno, strerror(errno));
     char buf[10];
+    memset(buf, 0, 10);
     size_t r = read(target_fd, buf, 10);
-    assert(r == 4);
+    assert(r == 3);
     printf("buf: '%s'\n", buf);
     close(target_fd);
   }
@@ -26,8 +27,9 @@ int main() {
     int target_fd = open("link_source", O_RDONLY);
     printf("target_fd: %d, errno: %d %s\n", target_fd, errno, strerror(errno));
     char buf[10];
+    memset(buf, 0, 10);
     size_t r = read(target_fd, buf, 10);
-    assert(r == 4);
+    assert(r == 3);
     printf("buf: '%s'\n", buf);
     close(target_fd);
   }
