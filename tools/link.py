1ff6cb1fd4e129706c476bae525425fa871f2d18
Carl Woffenden <cwoffenden@gmail.com>
2024-12-11 23:07:24 +0100

    Something trivial to rerun the CI…

--- tools/link.py
+++ tools/link.py
@@ -1403,7 +1403,7 @@ def phase_linker_setup(options, state, newargs):  # noqa: C901, PLR0912, PLR0915
       # MINIMAL_RUNTIME exports these manually, since this export mechanism is placed
       # in global scope that is not suitable for MINIMAL_RUNTIME loader.
       settings.EXPORTED_RUNTIME_METHODS += ['stackSave', 'stackAlloc', 'stackRestore', 'wasmTable']
-      # The following symbols need exposing to load the Audio Worklet:
+      # The following symbols need exposing to load the audio worklet:
       settings.INCOMING_MODULE_JS_API += ['instantiateWasm', 'wasm', 'wasmMemory']
 
   if settings.FORCE_FILESYSTEM and not settings.MINIMAL_RUNTIME:
