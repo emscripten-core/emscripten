ea8a22cc56316fb72a643ee5ac1eeca9a530b156
Alon Zakai <azakai@google.com>
2024-12-13 16:02:17 -0800

    oops

--- test/js_optimizer/emitDCEGraph-prefixing-output.js
+++ test/js_optimizer/emitDCEGraph-prefixing-output.js
@@ -1,15 +1,25 @@
-// wasm backend notation has one fewer _ in the wasm
-__GLOBAL__I_000101(); // var use
-var __GLOBAL__I_000101 = Module["__GLOBAL__I_000101"] = () => wasmExports["_GLOBAL__I_000101"]();
-
-__ATINIT__.push({ func: function() { __GLOBAL__I_iostream() } }); // var use inside other scope
-var __GLOBAL__I_iostream = Module["__GLOBAL__I_iostream"] = () => wasmExports["_GLOBAL__I_iostream.cpp"]();
-
-Module["__DUB"](); // module use
-var __DUB = Module["__DUB"] = () => wasmExports["_DUB"]();
-
-var __UNUSED = Module["__UNUSED"] = () => wasmExports["_UNUSED"]();
-
-var wasmImports = {
-};
-
+[
+ {
+  "name": "emcc$export$__DUB",
+  "export": "_DUB",
+  "reaches": [],
+  "root": true
+ },
+ {
+  "name": "emcc$export$__GLOBAL__I_000101",
+  "export": "_GLOBAL__I_000101",
+  "reaches": [],
+  "root": true
+ },
+ {
+  "name": "emcc$export$__GLOBAL__I_iostream",
+  "export": "_GLOBAL__I_iostream.cpp",
+  "reaches": [],
+  "root": true
+ },
+ {
+  "name": "emcc$export$__UNUSED",
+  "export": "_UNUSED",
+  "reaches": []
+ }
+]
