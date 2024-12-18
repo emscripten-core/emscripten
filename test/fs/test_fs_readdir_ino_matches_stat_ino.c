088b2539f52f250afe5cea54b8d9990017db6e2d
Hood Chatham <roberthoodchatham@gmail.com>
2024-12-18 16:39:22 +0100

    Apply review comments to test

--- test/fs/test_fs_readdir_ino_matches_stat_ino.c
+++ test/fs/test_fs_readdir_ino_matches_stat_ino.c
@@ -9,18 +9,19 @@
 #include <string.h>
 #include <dirent.h>
 
-
-int main() {
+void setup() {
   int res = open("b", O_CREAT, 0777);
   assert(res >= 0);
   assert(close(res) == 0);
   assert(symlink("b", "a") == 0);
+}
+
+int main() {
+  setup();
   int dirfd = open(".", O_RDONLY);
   assert(dirfd > 0);
-  DIR *dirp;
-  dirp = fdopendir(dirfd);
+  DIR *dirp = fdopendir(dirfd);
   assert(dirp != NULL);
-  struct dirent *ep;
   struct stat sta, stb;
   assert(lstat("a", &sta) == 0);
   assert(lstat("b", &stb) == 0);
@@ -30,6 +31,7 @@ int main() {
   assert(chmod(".", 0675) == 0);
   int a_ino = -1;
   int b_ino = -1;
+  struct dirent *ep;
   while ((ep = readdir(dirp))) {
     if (strcmp(ep->d_name, "a") == 0) {
       a_ino = ep->d_ino;
@@ -41,10 +43,10 @@ int main() {
   assert(errno == 0);
   assert(a_ino >= 0);
   assert(b_ino >= 0);
-  assert(chmod(".", 0775) == 0);
   printf("readdir a_ino: %d, b_ino: %d\n", a_ino, b_ino);
   printf("stat    a_ino: %llu, b_ino: %llu\n", sta.st_ino, stb.st_ino);
   assert(a_ino == sta.st_ino);
   assert(b_ino == stb.st_ino);
   printf("success\n");
+  return 0;
 }
