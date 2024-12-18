f66b5d706e174d9e5cc6122c06ea29dcd2735cd0
Heejin Ahn <aheejin@gmail.com>
2024-12-17 22:25:54 -0800

    Bump Binaryen version to 121 (#23207)
    https://github.com/WebAssembly/binaryen/pull/7153 updated Binaryen
    version to 121, and we updated it in Emscripten to 120 to make the CI
    pass (#23197). Now that
    https://chromium-review.googlesource.com/c/emscripten-releases/+/6103023
    has landed, I think we can update it to 121.

--- tools/building.py
+++ tools/building.py
@@ -39,7 +39,7 @@ logger = logging.getLogger('building')
 
 #  Building
 binaryen_checked = False
-EXPECTED_BINARYEN_VERSION = 120
+EXPECTED_BINARYEN_VERSION = 121
 
 _is_ar_cache: Dict[str, bool] = {}
 # the exports the user requested
