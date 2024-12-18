2ee2ce317dd5f1733aaeec69682a07a5e7b36987
Derek Schuff <dschuff@chromium.org>
2024-12-17 16:34:18 -0800

    remove debug print

--- test/test_core.py
+++ test/test_core.py
@@ -6920,7 +6920,6 @@ void* operator new(size_t size) {
     if self.get_setting('MEMORY64'):
       self.skipTest('not compatible with MEMORY64')
     if self.get_setting('WASM_BIGINT'):
-      print('bigint on')
       # define DYNCALLS because this test does test calling them directly, and
       # in WASM_BIGINT mode we do not enable them by default (since we can do
       # more without them - we don't need to legalize)
