071c87dbe6265f8c73fed88536eed6857cff9bf5
Hood Chatham <roberthoodchatham@gmail.com>
2024-12-18 19:20:49 +0100

    use also_with_nodefs_both decorator

--- test/test_core.py
+++ test/test_core.py
@@ -5858,15 +5858,11 @@ Module.onRuntimeInitialized = () => {
       self.set_setting('FORCE_FILESYSTEM')
     self.do_runf('fs/test_fs_rename_on_existing.c', 'success')
 
-  @parameterized({
-    '': ([],),
-    'nodefs': (['-DNODEFS', '-lnodefs.js'],),
-    'noderawfs': (['-sNODERAWFS'],)
-  })
+  @also_with_nodefs_both
   def test_fs_eisdir(self, args):
     if self.get_setting('WASMFS'):
       self.set_setting('FORCE_FILESYSTEM')
-    self.do_runf('fs/test_fs_eisdir.c', 'success', emcc_args=args)
+    self.do_runf('fs/test_fs_eisdir.c', 'success')
 
   def test_sigalrm(self):
     self.do_runf('test_sigalrm.c', 'Received alarm!')
