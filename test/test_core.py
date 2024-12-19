dae4a1e70bc7b4f3ed3d7985427dddfd2b0069fe
Derek Schuff <dschuff@chromium.org>
2024-12-19 15:12:43 -0800

    add parens

--- test/test_core.py
+++ test/test_core.py
@@ -5868,7 +5868,7 @@ Module.onRuntimeInitialized = () => {
   @also_with_nodefs_both
   @crossplatform
   def test_fs_open_no_permissions(self):
-    if '-DNODEFS' in self.emcc_args or '-DNODERAWFS' in self.emcc_args and WINDOWS:
+    if ('-DNODEFS' in self.emcc_args or '-DNODERAWFS' in self.emcc_args) and WINDOWS:
       self.skipTest('fs_open_no_permissions fails on windows')
     self.do_runf('fs/test_fs_open_no_permissions.c', 'success')
 
