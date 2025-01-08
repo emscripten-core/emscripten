ff25a121fb57601a8f1867ef6524e92316ed474c
Sam Clegg <sbc@chromium.org>
2025-01-08 13:36:29 -0800

    [test] Improve test_emmalloc_trim output. NFC (#23342)
    Also, add some assertions.

--- test/core/test_emmalloc_trim.c
+++ test/core/test_emmalloc_trim.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <emscripten/emmalloc.h>
@@ -7,40 +8,47 @@ size_t round_to_4k(size_t val){
   return (val + 4095) & ~4095;
 }
 
-void print_stats(int cnt) {
-  printf("dynamic heap %d: %zu\n", cnt, round_to_4k(emmalloc_dynamic_heap_size()));
-  printf("free dynamic memory %d: %zu\n", cnt, round_to_4k(emmalloc_free_dynamic_memory()));
-  printf("unclaimed heap memory %d: %zu\n", cnt, round_to_4k(emmalloc_unclaimed_heap_memory()));
-  printf("sbrk %d: %#zx\n", cnt, round_to_4k((size_t)sbrk(0)));
+void print_stats(const char* title) {
+  printf("%s: dynamic heap:          %zu\n", title, round_to_4k(emmalloc_dynamic_heap_size()));
+  printf("%s: free dynamic memory:   %zu\n", title, round_to_4k(emmalloc_free_dynamic_memory()));
+  printf("%s: unclaimed heap memory: %zu\n", title, round_to_4k(emmalloc_unclaimed_heap_memory()));
+  printf("%s: sbrk:                  %#zx\n", title, round_to_4k((size_t)sbrk(0)));
 }
 
 int main() {
+  int did_free;
   printf("heap size: %zu\n", emscripten_get_heap_size());
-  print_stats(0);
+  print_stats("init");
 
   void *ptr = malloc(32*1024*1024);
   void *ptr2 = malloc(4*1024*1024);
-  printf("%d\n", (int)(ptr && ptr2));
-  print_stats(1);
+  assert(ptr);
+  assert(ptr2);
+  print_stats("after alloc");
 
-  int success = emmalloc_trim(0);
-  printf("1st trim: %d\n", success);
-  print_stats(1);
+  did_free = emmalloc_trim(0);
+  assert(did_free);
+  printf("1st trim: did_free=%d\n", did_free);
+  print_stats("1");
 
-  success = emmalloc_trim(0);
-  printf("2nd trim: %d\n", success);
-  print_stats(2);
+  did_free = emmalloc_trim(0);
+  assert(!did_free);
+  printf("2nd trim: did_free=%d\n", did_free);
+  print_stats("2");
   free(ptr2);
 
-  success = emmalloc_trim(100000);
-  printf("3rd trim: %d\n", success);
-  print_stats(3);
+  did_free = emmalloc_trim(100000);
+  assert(did_free);
+  printf("3rd trim: did_free=%d\n", did_free);
+  print_stats("3");
 
-  success = emmalloc_trim(100000);
-  printf("4th trim: %d\n", success);
-  print_stats(4);
+  did_free = emmalloc_trim(100000);
+  assert(!did_free);
+  printf("4th trim: did_free=%d\n", did_free);
+  print_stats("4");
 
-  success = emmalloc_trim(0);
-  printf("5th trim: %d\n", success);
-  print_stats(5);
+  did_free = emmalloc_trim(0);
+  assert(did_free);
+  printf("5th trim: did_free=%d\n", did_free);
+  print_stats("5");
 }
