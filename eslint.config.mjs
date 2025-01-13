376438b424b2fc4f96b58dd2b9b905358a005a79
Sam Clegg <sbc@chromium.org>
2025-01-13 13:51:02 -0800

    Cleanup eslint.config.mjs. NFC

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,9 @@
 import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all
 });
