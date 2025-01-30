c818c3f3b0fbf234c6d4431bf95029cf797ff575
Brendan Dahl <brendan.dahl@gmail.com>
2025-01-30 18:24:12 +0000

    review.

--- tools/building.py
+++ tools/building.py
@@ -566,10 +566,12 @@ def closure_compiler(filename, advanced=True, extra_closure_args=None):
   # The ES module exports are generated after closure is run, so add the exports
   # to and extern file to avoid closure complaining about undefined variables.
   if settings.MODULARIZE == 'instance':
-    temp = shared.get_temp_files().get('.js', prefix='emcc_closure_externs_').name
     exports = settings.EXPORTED_FUNCTIONS + settings.EXPORTED_RUNTIME_METHODS
-    utils.write_file(temp, ';\n'.join(['var' +  ' __exp_' + export for export in exports]) + ';\n')
-    CLOSURE_EXTERNS += [temp]
+    export_vars = ';\n'.join(['var' +  ' __exp_' + export for export in exports]) + ';\n'
+    exports_file = shared.get_temp_files().get('.js', prefix='emcc_instance_exports_')
+    exports_file.write(export_vars.encode())
+    exports_file.close()
+    CLOSURE_EXTERNS += [exports_file.name]
 
   if settings.USE_WEBGPU:
     CLOSURE_EXTERNS += [path_from_root('src/closure-externs/webgpu-externs.js')]
