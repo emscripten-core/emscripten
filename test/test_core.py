4b7ea15d3dae776a92a38ea759eeec9e2c233d0f
Hood Chatham <roberthoodchatham@gmail.com>
2025-02-05 23:04:06 +0100

    Apply more review comments

--- test/test_core.py
+++ test/test_core.py
@@ -3761,16 +3761,22 @@ ok
   @requires_jspi
   @needs_dylink
   def test_dlfcn_jspi(self):
-    side_flags = ['-sSIDE_MODULE']
-    if self.get_setting('MEMORY64'):
-      side_flags.append('-sMEMORY64')
-    self.set_setting('JSPI')
-    self.run_process([
-      EMCC,
-      '-o', 'side.so',
-      test_file('core/test_dlfcn_jspi_side.c'),
-      *side_flags])
-    self.do_run_in_out_file_test('core/test_dlfcn_jspi.c', emcc_args=['side.so', '-sMAIN_MODULE=2'])
+      side_flags = []
+      if self.get_setting("MEMORY64"):
+          side_flags.append("-sMEMORY64")
+      self.run_process(
+          [
+              EMCC,
+              "-o",
+              "side.so",
+              test_file("core/test_dlfcn_jspi_side.c"),
+              "-sSIDE_MODULE",
+          ]
+          + self.get_emcc_args()
+      )
+      self.do_run_in_out_file_test(
+          "core/test_dlfcn_jspi.c", emcc_args=["side.so", "-sMAIN_MODULE=2"]
+      )
 
   @needs_dylink
   def test_dlfcn_rtld_local(self):
