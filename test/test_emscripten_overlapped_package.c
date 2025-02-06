3e2760ef4accfbb2f50ebebeab81abec64d89d9e
Joseph C. Osborn <jcoa2018@pomona.edu>
2025-02-06 08:57:46 -0800

    reduce and document overlapped file package test

--- test/test_emscripten_overlapped_package.c
+++ test/test_emscripten_overlapped_package.c
@@ -1,4 +1,4 @@
-// Copyright 2013 The Emscripten Authors.  All rights reserved.
+// Copyright 2025 The Emscripten Authors.  All rights reserved.
 // Emscripten is available under two separate licenses, the MIT license and the
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
@@ -10,15 +10,11 @@
 
 #include <emscripten.h>
 
-int value = 0;
-
-EMSCRIPTEN_KEEPALIVE void set(int x) {
-  printf("set! %d\n", x);
-  value = x;
+void error2() {
+  printf("fail2\n");
 }
 
-void load3() {
-  printf("load3\n");
+void load2() {
   char buffer[10];
   memset(buffer, 0, 10);
   FILE *f = fopen("/target/file1.txt", "r");
@@ -34,25 +30,9 @@ void load3() {
   fclose(f);
   assert(strcmp(buffer, "second") == 0);
   exit(0);
-} 
-
-void error3() {
-  printf("fail3\n");
-}
-
-void error2() {
-  printf("fail2\n");
-}
-
-void load2() {
-  printf("load2\n");
-  emscripten_async_load_script("script3.js", load3, error3);
 }
 
 void load1() {
-  printf("load1\n");
-  assert(value == 456);
-  
   emscripten_async_load_script("script2.js", load2, error2);
 }
 
