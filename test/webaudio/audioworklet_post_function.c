47a7c0b1b1831b276141f51bdc409b075222d95d
Carl Woffenden <cwoffenden@gmail.com>
2025-02-20 17:17:20 +0100

    Migrated test to btest_exit()

--- test/webaudio/audioworklet_post_function.c
+++ test/webaudio/audioworklet_post_function.c
@@ -1,5 +1,4 @@
 #include <emscripten/webaudio.h>
-#include <stdio.h>
 #include <assert.h>
 
 // This test showcases posting messages (function calls) between the main thread
@@ -8,17 +7,17 @@
 
 // This event will fire on the main thread.
 void MessageReceivedOnMainThread(int d, int e, int f) {
-  printf("MessageReceivedOnMainThread: d=%d, e=%d, f=%d\n", d, e, f);
+  emscripten_outf("MessageReceivedOnMainThread: d=%d, e=%d, f=%d", d, e, f);
   assert(!emscripten_current_thread_is_audio_worklet());
   assert(d == 1 && e == 2 && f == 3);
-#ifdef REPORT_RESULT
-  REPORT_RESULT(1); // test succeeded, were able to post a message from main thread to audio thread and back!
-#endif
+
+  // test succeeded, were able to post a message from main thread to audio thread and back!
+  emscripten_force_exit(EXIT_SUCCESS);
 }
 
 // This event will fire on the audio worklet thread.
 void MessageReceivedInAudioWorkletThread(int a, int b) {
-  printf("MessageReceivedInAudioWorkletThread: a=%d, b=%d\n", a, b);
+  emscripten_outf("MessageReceivedInAudioWorkletThread: a=%d, b=%d", a, b);
   assert(emscripten_current_thread_is_audio_worklet());
   assert(a == 42 && b == 9000);
   emscripten_audio_worklet_post_function_viii(EMSCRIPTEN_AUDIO_MAIN_THREAD, MessageReceivedOnMainThread, /*d=*/1, /*e=*/2, /*f=*/3);
@@ -26,7 +25,7 @@ void MessageReceivedInAudioWorkletThread(int a, int b) {
 
 // This callback will fire when the audio worklet thread has been initialized.
 void WebAudioWorkletThreadInitialized(EMSCRIPTEN_WEBAUDIO_T audioContext, bool success, void *userData) {
-  printf("WebAudioWorkletThreadInitialized\n");
+  emscripten_out("WebAudioWorkletThreadInitialized");
   emscripten_audio_worklet_post_function_vii(audioContext, MessageReceivedInAudioWorkletThread, /*a=*/42, /*b=*/9000);
 }
 
@@ -39,4 +38,6 @@ int main() {
   // and kick off Audio Worklet scope initialization, which shares the Wasm
   // Module and Memory to the AudioWorklet scope and initializes its stack.
   emscripten_start_wasm_audio_worklet_thread_async(context, wasmAudioWorkletStack, sizeof(wasmAudioWorkletStack), WebAudioWorkletThreadInitialized, 0);
+
+  emscripten_exit_with_live_runtime();
 }
