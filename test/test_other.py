fe1b7910f58e0186b2042b9ded606fcdb482e07f
Jax Yang <jaxyang39@gmail.com>
2025-03-02 19:52:17 +0800

    Remove unnecessary parameters and calls

--- test/test_other.py
+++ test/test_other.py
@@ -15689,8 +15689,7 @@ addToLibrary({
         return {}; // Compiling asynchronously, no exports.
       }''')
     # Test with ASYNCIFY here to ensure that that wasmExports gets set to the wrapped version of the wasm exports.
-    self.run_process([EMCC, test_file('test_manual_wasm_instantiate.c'), '--pre-js=pre.js','--extern-post-js', test_file('modularize_post_js.js'),'-sASYNCIFY','-o','manual_init_module.mjs'])
-    self.assertContained('testWasmInstantiationSucceeded: 1', self.run_js('manual_init_module.mjs'))
+    self.do_runf(test_file('test_manual_wasm_instantiate.c'),'testWasmInstantiationSucceeded: 1',emcc_args=['--pre-js=pre.js','-sASYNCIFY'])
 
   def test_late_module_api_assignment(self):
     # When sync instantiation is used (or when async/await is used in MODULARIZE mode) certain
