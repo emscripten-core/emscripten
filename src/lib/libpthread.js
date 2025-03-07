4849ae01ead6f00dbfd48ce095f9f3122dd1c4e6
Jonathan Gamble <gamblej@gmail.com>
2025-03-07 13:51:43 -0600

    dont break node builds

--- src/lib/libpthread.js
+++ src/lib/libpthread.js
@@ -440,6 +440,7 @@ var LibraryPThread = {
       }
 #endif
       var options = {{{ pthreadWorkerOptions }}};
+#if EXPORT_ES6
       var worker = workerUrl 
         ? new Worker(workerUrl, options)
       // We need to generate the URL with import.meta.url as the base URL of the JS file
@@ -448,6 +449,9 @@ var LibraryPThread = {
       // URL to import from the server (e.g., for webpack the file:// path).
       // See https://github.com/webpack/webpack/issues/12638
         : new Worker(new URL("{{{ TARGET_JS_NAME }}}", import.meta.url), options);
+#else
+      var worker = new Worker(workerUrl, options);
+#endif
 #if ASSERTIONS
       worker.workerID = PThread.nextWorkerID++;
 #endif
