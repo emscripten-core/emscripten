379170f1cee98d70124083d04b00d9d08ddfa7ba
Derek Schuff <dschuff@chromium.org>
2025-03-06 16:59:24 -0800

    style suggestion

--- test/test_core.py
+++ test/test_core.py
@@ -5989,7 +5989,7 @@ Module.onRuntimeInitialized = () => {
   def test_unistd_unlink(self):
     # symlinks on node.js on non-linux behave differently (e.g. on Windows they require administrative privileges)
     # so skip testing those bits on that combination.
-    if any([arg in self.emcc_args for arg in ('-DNODEFS', '-DNODERAWFS')]) and MACOS:
+    if MACOS and any(arg in self.emcc_args for arg in ('-DNODEFS', '-DNODERAWFS')):
       self.skipTest('only tested on linux')
     if '-DNODEFS' in self.emcc_args:
       if WINDOWS:
