3689d4ef3b5762fd16b60e05e4fc432135042727
Gyeongjae Choi <def6488@gmail.com>
2025-03-09 09:32:22 +0000

    ruff

--- test/test_other.py
+++ test/test_other.py
@@ -2406,7 +2406,6 @@ Module['postRun'] = () => {
     self.assertEqual(get_runtime_paths('libside1.so'), ['$ORIGIN'])
     self.assertEqual(get_runtime_paths('a.out.wasm'), ['$ORIGIN'])
 
-
   def test_dylink_LEGACY_GL_EMULATION(self):
     # LEGACY_GL_EMULATION wraps JS library functions. This test ensure that when it does
     # so it preserves the `.sig` attributes needed by dynamic linking.
@@ -7793,7 +7792,6 @@ int main() {
     self.assertContained('Hello1', out)
     self.assertContained('Hello1_2', out)
     self.assertContained('Ok', out)
-
 
   def test_dlopen_bad_flags(self):
     create_file('main.c', r'''
