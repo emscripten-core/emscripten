9785e83c991c32a76d7a2c361b3f1b55adf9f85f
Hood Chatham <roberthoodchatham@gmail.com>
2025-03-11 17:35:51 +0100

    Two space indentation

--- test/other/test_stub_pthread_getattr_np.c
+++ test/other/test_stub_pthread_getattr_np.c
@@ -4,14 +4,14 @@
 #include "assert.h"
 
 int main() {
-    pthread_attr_t attr;
-    size_t stack_size, guard_size;
-    void *stack_addr;
+  pthread_attr_t attr;
+  size_t stack_size, guard_size;
+  void *stack_addr;
 
-    assert(pthread_getattr_np(pthread_self(), &attr) == 0);
-    assert(pthread_attr_getguardsize(&attr, &guard_size) == 0);
-    assert(pthread_attr_getstack(&attr, &stack_addr, &stack_size) == 0);
-    assert(pthread_attr_destroy(&attr) == 0);
-    printf("stack_addr: %p, stack_size: %zu, guard_size: %zu\n", stack_addr, stack_size, guard_size);
-    return 0;
+  assert(pthread_getattr_np(pthread_self(), &attr) == 0);
+  assert(pthread_attr_getguardsize(&attr, &guard_size) == 0);
+  assert(pthread_attr_getstack(&attr, &stack_addr, &stack_size) == 0);
+  assert(pthread_attr_destroy(&attr) == 0);
+  printf("stack_addr: %p, stack_size: %zu, guard_size: %zu\n", stack_addr, stack_size, guard_size);
+  return 0;
 }
