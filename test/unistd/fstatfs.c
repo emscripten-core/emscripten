b1b9b01bec4510ca593b880396b0755febeff547
Bero <berislav.grgicak@gmail.com>
2025-03-13 12:59:41 +0100

    Test NODERAWFS

--- test/unistd/fstatfs.c
+++ test/unistd/fstatfs.c
@@ -18,11 +18,9 @@ int main() {
   fstatfs_rtn = fstatfs(f, &buf);
   printf("f_type: %ld\n", buf.f_type);
   printf("f_blocks: %d\n", buf.f_blocks);
-#if NODEFS
+#if NODEFS || NODERAWFS
   assert(fstatfs_rtn == 0);
   assert(buf.f_blocks != 0);
-  // 2048000000 is a hardcoded value for NODEFS blocks.
-  assert(buf.f_blocks != 2048000000);
 #else
   assert(fstatfs_rtn == 0);
   assert(buf.f_blocks == 1000000);
@@ -31,9 +29,8 @@ int main() {
   assert(statfs("file", &buf) == 0);
   printf("f_type: %ld\n", buf.f_type);
   printf("f_blocks: %d\n", buf.f_blocks);
-#if NODEFS
+#if NODEFS || NODERAWFS
   assert(buf.f_blocks != 0);
-  assert(buf.f_blocks != 2048000000);
 #else
   assert(buf.f_blocks == 1000000);
 #endif
