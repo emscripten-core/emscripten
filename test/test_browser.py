emscripten-core/emscripten

commit 0de7eee66d3779eca34c798143df51e1dc0a16f5

Use `for .. of` in more places in JS library. NFC (#23867)

src/lib/libopenal.js
@@ -4056,9 +4056,9 @@ var LibraryOpenAL = {
 
     // Find the first non-zero buffer in the queue to determine the proper format
     var templateBuf = AL.buffers[0];
-    for (var i = 0; i < src.bufQueue.length; i++) {
-      if (src.bufQueue[i].id !== 0) {
-        templateBuf = src.bufQueue[i];
+    for (var buf of src.bufQueue.length) {
+      if (buf.id !== 0) {
+        templateBuf = buf;
         break;
       }
     }

src/lib/libaddfunction.js
@@ -68,11 +68,11 @@ addToLibrary({
     // Parameters, length + signatures
     target.push(0x60 /* form: func */);
     uleb128Encode(sigParam.length, target);
-    for (var i = 0; i < sigParam.length; ++i) {
+    for (var paramType of sigParam) {
 #if ASSERTIONS
-      assert(sigParam[i] in typeCodes, 'invalid signature char: ' + sigParam[i]);
+      assert(paramType in typeCodes, `invalid signature char: ${paramType}`);
 #endif
-      target.push(typeCodes[sigParam[i]]);
+      target.push(typeCodes[paramType]);
     }
 
     // Return values, length + signatures

src/lib/libfs.js
@@ -746,9 +746,9 @@ FS.staticInit();
     mkdirTree(path, mode) {
       var dirs = path.split('/');
       var d = '';
-      for (var i = 0; i < dirs.length; ++i) {
-        if (!dirs[i]) continue;
-        d += '/' + dirs[i];
+      for (var dir of dirs) {
+        if (!dir) continue;
+        d += '/' + dir;
         try {
           FS.mkdir(d, mode);
         } catch(e) {
@@ -1550,12 +1550,10 @@ FS.staticInit();
       _fflush(0);
 #endif
       // close all of our streams
-      for (var i = 0; i < FS.streams.length; i++) {
-        var stream = FS.streams[i];
-        if (!stream) {
-          continue;
+      for (var stream of FS.streams) {
+        if (stream) {
+          FS.close(stream);
         }
-        FS.close(stream);
       }
     },
 

src/lib/libglemu.js
@@ -1620,8 +1620,8 @@ var LibraryGLEmulation = {
         getActiveTexture: () => s_activeTexture,
 
         traverseState: (keyView) => {
-          for (var i = 0; i < s_texUnits.length; i++) {
-            s_texUnits[i].traverseState(keyView);
+          for (var texUnit of s_texUnits) {
+            texUnit.traverseState(keyView);
           }
         },
 
@@ -2087,8 +2087,8 @@ var LibraryGLEmulation = {
 
       // By attrib state:
       var enabledAttributesKey = 0;
-      for (var i = 0; i < attributes.length; i++) {
-        enabledAttributesKey |= 1 << attributes[i].name;
+      for (var attr of attributes) {
+        enabledAttributesKey |= 1 << attr.name;
       }
 
       // To prevent using more than 31 bits add another level to the maptree
@@ -2226,8 +2226,7 @@ var LibraryGLEmulation = {
             var texUnitUniformList = '';
             var vsTexCoordInits = '';
             this.usedTexUnitList = GLImmediate.TexEnvJIT.getUsedTexUnitList();
-            for (var i = 0; i < this.usedTexUnitList.length; i++) {
-              var texUnit = this.usedTexUnitList[i];
+            for (var texUnit of this.usedTexUnitList) {
               texUnitAttribList += 'attribute vec4 ' + aTexCoordPrefix + texUnit + ';\n';
               texUnitVaryingList += 'varying vec4 ' + vTexCoordPrefix + texUnit + ';\n';
               texUnitUniformList += 'uniform sampler2D ' + uTexUnitPrefix + texUnit + ';\n';
@@ -2972,16 +2971,14 @@ var LibraryGLEmulation = {
         var start = GLImmediate.restrideBuffer;
         bytes = 0;
         // calculate restrided offsets and total size
-        for (var i = 0; i < attributes.length; i++) {
-          var attr = attributes[i];
+        for (var attr of attributes) {
           var size = attr.sizeBytes;
           if (size % 4 != 0) size += 4 - (size % 4); // align everything
           attr.offset = bytes;
           bytes += size;
         }
         // copy out the data (we need to know the stride for that, and define attr.pointer)
-        for (var i = 0; i < attributes.length; i++) {
-          var attr = attributes[i];
+        for (var attr of attributes) {
           var srcStride = Math.max(attr.sizeBytes, attr.stride);
           if ((srcStride & 3) == 0 && (attr.sizeBytes & 3) == 0) {
             for (var j = 0; j < count; j++) {
@@ -3008,8 +3005,7 @@ var LibraryGLEmulation = {
         } else {
           GLImmediate.vertexPointer = clientStartPointer;
         }
-        for (var i = 0; i < attributes.length; i++) {
-          var attr = attributes[i];
+        for (var attr of attributes) {
           attr.offset = attr.pointer - GLImmediate.vertexPointer; // Compute what will be the offset of this attribute in the VBO after we upload.
         }
         GLImmediate.stride = Math.max(maxStride, bytes);

src/lib/libglew.js
@@ -114,9 +114,8 @@ var LibraryGLEW = {
 
   glewIsSupported: (name) => {
     var exts = UTF8ToString(name).split(' ');
-    for (var i = 0; i < exts.length; ++i) {
-      if (!GLEW.extensionIsSupported(exts[i]))
-        return 0;
+    for (var ext of exts) {
+      if (!GLEW.extensionIsSupported(ext)) return 0;
     }
     return 1;
   },

src/lib/libglfw.js
@@ -1141,12 +1141,14 @@ var LibraryGLFW = {
 #endif
 
       GLFW.windows[win.id - 1] = null;
-      if (GLFW.active.id == win.id)
+      if (GLFW.active.id == win.id) {
         GLFW.active = null;
+      }
 
       // Destroy context when no alive windows
-      for (var i = 0; i < GLFW.windows.length; i++)
-        if (GLFW.windows[i] !== null) return;
+      for (win of GLFW.windows) {
+        if (win !== null) return;
+      }
 
       delete Module['ctx'];
     },

src/lib/libhtml5.js
@@ -1275,10 +1275,10 @@ var LibraryHTML5 = {
     var hiddenElements = [];
     while (child != document.body) {
       var children = parent.children;
-      for (var i = 0; i < children.length; ++i) {
-        if (children[i] != child) {
-          hiddenElements.push({ node: children[i], displayState: children[i].style.display });
-          children[i].style.display = 'none';
+      for (var currChild of children) {
+        if (currChild != child) {
+          hiddenElements.push({ node: currChild, displayState: currChild.style.display });
+          currChild.style.display = 'none';
         }
       }
       child = parent;

src/lib/libpipefs.js
@@ -86,12 +86,9 @@ addToLibrary({
         if ((stream.flags & {{{ cDefs.O_ACCMODE }}}) === {{{ cDefs.O_WRONLY }}}) {
           return ({{{ cDefs.POLLWRNORM }}} | {{{ cDefs.POLLOUT }}});
         }
-        if (pipe.buckets.length > 0) {
-          for (var i = 0; i < pipe.buckets.length; i++) {
-            var bucket = pipe.buckets[i];
-            if (bucket.offset - bucket.roffset > 0) {
-              return ({{{ cDefs.POLLRDNORM }}} | {{{ cDefs.POLLIN }}});
-            }
+        for (var bucket of pipe.buckets) {
+          if (bucket.offset - bucket.roffset > 0) {
+            return ({{{ cDefs.POLLRDNORM }}} | {{{ cDefs.POLLIN }}});
           }
         }
 
@@ -110,8 +107,7 @@ addToLibrary({
         var pipe = stream.node.pipe;
         var currentLength = 0;
 
-        for (var i = 0; i < pipe.buckets.length; i++) {
-          var bucket = pipe.buckets[i];
+        for (var bucket of pipe.buckets) {
           currentLength += bucket.offset - bucket.roffset;
         }
 
@@ -136,22 +132,21 @@ addToLibrary({
         var totalRead = toRead;
         var toRemove = 0;
 
-        for (var i = 0; i < pipe.buckets.length; i++) {
-          var currBucket = pipe.buckets[i];
-          var bucketSize = currBucket.offset - currBucket.roffset;
+        for (var bucket of pipe.buckets) {
+          var bucketSize = bucket.offset - bucket.roffset;
 
           if (toRead <= bucketSize) {
-            var tmpSlice = currBucket.buffer.subarray(currBucket.roffset, currBucket.offset);
+            var tmpSlice = bucket.buffer.subarray(bucket.roffset, bucket.offset);
             if (toRead < bucketSize) {
               tmpSlice = tmpSlice.subarray(0, toRead);
-              currBucket.roffset += toRead;
+              bucket.roffset += toRead;
             } else {
               toRemove++;
             }
             data.set(tmpSlice);
             break;
           } else {
-            var tmpSlice = currBucket.buffer.subarray(currBucket.roffset, currBucket.offset);
+            var tmpSlice = bucket.buffer.subarray(bucket.roffset, bucket.offset);
             data.set(tmpSlice);
             data = data.subarray(tmpSlice.byteLength);
             toRead -= tmpSlice.byteLength;

commit da0ae807ce06261554db9ad860e3adffb2d337da

Fix iteration of OpenAL src.bufQueue (#23916)
This failing test shows up under the interactive tests only, so I guess
CI didn't catch it. Caused by #23867 .

src/lib/libopenal.js
@@ -4056,7 +4056,7 @@ var LibraryOpenAL = {
 
     // Find the first non-zero buffer in the queue to determine the proper format
     var templateBuf = AL.buffers[0];
-    for (var buf of src.bufQueue.length) {
+    for (var buf of src.bufQueue) {
       if (buf.id !== 0) {
         templateBuf = buf;
         break;

test/openal/test_openal_buffers.c
@@ -89,7 +89,9 @@ void iter() {
   alGetSourcef(source, AL_PITCH, &testPitch);
   assert(pitch == testPitch);
 #endif
-
+#ifndef TEST_INTERACTIVE
+  offset = size;
+#endif
   // Exit once we've processed the entire clip.
   if (offset >= size) {
 #if __EMSCRIPTEN__
@@ -192,7 +194,6 @@ int main(int argc, char* argv[]) {
   ALint srcLen = 0;
   alGetSourcei(source, 0x2009 /* AL_BYTE_LENGTH_SOFT */, &srcLen);
   assert(srcLen == NUM_BUFFERS * BUFFER_SIZE);
-
 #ifdef TEST_ANIMATED_PITCH
   printf("You should hear a clip of the 1902 piano song \"The Entertainer\" played back at a high pitch rate, and animated to slow down to half playback speed.\n");
 #else

test/openal/test_openal_playback.c
@@ -295,10 +295,11 @@ int main() {
 
 #ifdef __EMSCRIPTEN__
 
+  intptr_t first_src = sources[0];
 #if defined(TEST_LOOPED_PLAYBACK)
-  emscripten_set_main_loop_arg(main_tick, (void*)sources[0], 0, 0);
+  emscripten_set_main_loop_arg(main_tick, (void*)first_src, 0, 0);
 #else
-  emscripten_async_call(playSource, (void*)(sources[0]), 700);
+  emscripten_async_call(playSource, (void*)first_src, 700);
 #endif
 #else
   usleep(700000);

test/test_browser.py
@@ -2323,6 +2323,13 @@ void *getBindBuffer() {
   def test_openal_extensions(self):
     self.btest_exit('openal/test_openal_extensions.c')
 
+  def test_openal_playback(self):
+    shutil.copy(test_file('sounds/audio.wav'), '.')
+    self.btest_exit('openal/test_openal_playback.c', emcc_args=['-O2', '--preload-file', 'audio.wav'],)
+
+  def test_openal_buffers(self):
+    self.btest_exit('openal/test_openal_buffers.c', emcc_args=['--preload-file', test_file('sounds/the_entertainer.wav') + '@/'],)
+
   def test_runtimelink(self):
     create_file('header.h', r'''
       struct point {

test/test_interactive.py
@@ -159,10 +159,10 @@ class interactive(BrowserCore):
     self.btest('openal/test_openal_playback.c', '1', emcc_args=['-O2', '--preload-file', 'audio.wav'] + args)
 
   def test_openal_buffers(self):
-    self.btest_exit('openal/test_openal_buffers.c', emcc_args=['--preload-file', test_file('sounds/the_entertainer.wav') + '@/'],)
+    self.btest_exit('openal/test_openal_buffers.c', emcc_args=['-DTEST_INTERACTIVE=1', '--preload-file', test_file('sounds/the_entertainer.wav') + '@/'],)
 
   def test_openal_buffers_animated_pitch(self):
-    self.btest_exit('openal/test_openal_buffers.c', emcc_args=['-DTEST_ANIMATED_PITCH=1', '--preload-file', test_file('sounds/the_entertainer.wav') + '@/'],)
+    self.btest_exit('openal/test_openal_buffers.c', emcc_args=['-DTEST_INTERACTIVE=1', '-DTEST_ANIMATED_PITCH=1', '--preload-file', test_file('sounds/the_entertainer.wav') + '@/'],)
 
   def test_openal_looped_pitched_playback(self):
     self.btest('openal/test_openal_playback.c', '1', emcc_args=['-DTEST_LOOPED_PLAYBACK=1', '--preload-file', test_file('sounds/the_entertainer.wav') + '@/audio.wav'],)