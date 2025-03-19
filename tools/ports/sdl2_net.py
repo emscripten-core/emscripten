5317e44a7372d619cc1aa7882f719870591c48f0
Sam Clegg <sbc@chromium.org>
2025-03-19 15:19:06 -0700

    Use common build directory for ports and system libs. NFC (#23932)
    Split out from #23924

--- tools/ports/sdl2_net.py
+++ tools/ports/sdl2_net.py
@@ -3,8 +3,6 @@
 # University of Illinois/NCSA Open Source License.  Both these licenses can be
 # found in the LICENSE file.
 
-import os
-
 TAG = 'version_2'
 HASH = '317b22ad9b6b2f7b40fac7b7c426da2fa2da1803bbe58d480631f1e5b190d730763f2768c77c72affa806c69a1e703f401b15a1be3ec611cd259950d5ebc3711'
 
@@ -16,8 +14,6 @@ def needed(settings):
 
 
 def get(ports, settings, shared):
-  sdl_build = os.path.join(ports.get_build_dir(), 'sdl2')
-  assert os.path.exists(sdl_build), 'You must use SDL2 to use SDL2_net'
   ports.fetch_project('sdl2_net', f'https://github.com/emscripten-ports/SDL2_net/archive/{TAG}.zip', sha512hash=HASH)
 
   def create(final):
