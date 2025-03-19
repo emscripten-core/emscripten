b53842ca0c462dcba197eb663ad2602541776d4d
Bero <berislav.grgicak@gmail.com>
2025-03-19 12:34:41 +0100

    Test with real blocks count

--- test/setup_nodefs.js
+++ test/setup_nodefs.js
@@ -1,3 +1,10 @@
+const statfsSync = require('fs').statfsSync;
+
+Module['preRun'] = () => {
+  const statfs = statfsSync('/');
+  ENV.EXPECTED_BLOCKS = statfs.blocks.toString();
+};
+
 Module['onRuntimeInitialized'] = () => {
   out('mounting node filesystem under /nodefs');
   FS.mkdir('/nodefs');
--- test/unistd/fstatfs.c
+++ test/unistd/fstatfs.c
@@ -3,16 +3,33 @@
 #include <sys/vfs.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>   // for strerror
+#include <errno.h>    // for errno#include <filesystem>
+
+#define DEFAULT_BLOCKS 1000000
 
 int main() {
   struct statfs buf;
+  const char *expected_blocks_str = getenv("EXPECTED_BLOCKS");
+  long expected_blocks = expected_blocks_str ? atol(expected_blocks_str) : DEFAULT_BLOCKS;
 
-  int rtn;
-  int fstatfs_rtn = fstatfs(STDOUT_FILENO, &buf);
+  int fstatfs_rtn = statfs("/nodefs", &buf);
   printf("f_type: %ld\n", buf.f_type);
   assert(fstatfs_rtn == 0);
   printf("f_blocks: %d\n", buf.f_blocks);
-  assert(buf.f_blocks == 1000000);
+  printf("expected_blocks: %ld\n", expected_blocks);
+#if NODEFS || NODERAWFS
+  assert(buf.f_blocks == expected_blocks);
+#else
+  assert(buf.f_blocks == DEFAULT_BLOCKS);
+#endif
+
+  fstatfs_rtn = fstatfs(STDOUT_FILENO, &buf);
+  printf("f_type: %ld\n", buf.f_type);
+  assert(fstatfs_rtn == 0);
+  printf("f_blocks: %d\n", buf.f_blocks);
+  assert(buf.f_blocks == DEFAULT_BLOCKS);
 
   int f = open("file", O_RDWR | O_CREAT);
   fstatfs_rtn = fstatfs(f, &buf);
@@ -23,7 +40,7 @@ int main() {
   assert(buf.f_blocks != 0);
 #else
   assert(fstatfs_rtn == 0);
-  assert(buf.f_blocks == 1000000);
+  assert(buf.f_blocks == DEFAULT_BLOCKS);
 #endif
 
   assert(statfs("file", &buf) == 0);
@@ -32,7 +49,7 @@ int main() {
 #if NODEFS || NODERAWFS
   assert(buf.f_blocks != 0);
 #else
-  assert(buf.f_blocks == 1000000);
+  assert(buf.f_blocks == DEFAULT_BLOCKS);
 #endif
 
   return 0;
