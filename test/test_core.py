87f071322e469f7b2f396c652afdda9cde9cde6f
Guus Waals <_@guusw.nl>
2025-03-21 14:49:52 +0800

    Reenable test_unistd_unlink under wasmfs

--- test/test_core.py
+++ test/test_core.py
@@ -5998,8 +5998,6 @@ Module.onRuntimeInitialized = () => {
     if '-DNODEFS' in self.emcc_args:
       if WINDOWS:
         self.emcc_args += ['-DNO_SYMLINK=1']
-      if self.get_setting('WASMFS'):
-        self.skipTest('https://github.com/emscripten-core/emscripten/issues/18112')
 
     # Several differences/bugs on non-linux including https://github.com/nodejs/node/issues/18014
     # TODO: NODERAWFS in WasmFS
