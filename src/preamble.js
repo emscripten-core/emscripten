2514ef79548630950e73f8d2a10ca7726e515d71
Alexander Guryanov <caiiiycuk@gmail.com>
2025-04-02 17:58:58 +0300

    Fix regression with instantiateWasm + MODULARIZE + Asyncify

--- src/preamble.js
+++ src/preamble.js
@@ -996,8 +996,7 @@ function getWasmImports() {
       try {
 #endif
         Module['instantiateWasm'](info, (mod, inst) => {
-          receiveInstance(mod, inst);
-          resolve(mod.exports);
+          resolve(receiveInstance(mod, inst));
         });
 #if ASSERTIONS
       } catch(e) {
