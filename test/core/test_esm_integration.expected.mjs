02ed7d7e9eadce407a1cba4bd9688e1cee376ef3
Sam Clegg <sbc@chromium.org>
2025-04-10 16:19:05 -0700

    Update test_esm_integration expectations (#24092)
    This was not updated as part of #24079 but we didn't notice because we
    were not running the test as part of CI.

--- test/core/test_esm_integration.expected.mjs
+++ test/core/test_esm_integration.expected.mjs
@@ -1,2 +1,2 @@
 export { __main_argc_argv as main } from './hello_world.wasm';
-export { default, err, HEAPF32, HEAPF64, HEAP8, HEAPU8, HEAP16, HEAPU16, HEAP32, HEAPU32, HEAP64, HEAPU64, stringToNewUTF8 } from './hello_world.support.mjs';
+export { default, err, stringToNewUTF8 } from './hello_world.support.mjs';
