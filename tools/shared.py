d4f1b1397acca5daed67b27ef2645943d3af906a
Sam Clegg <sbc@chromium.org>
2025-04-14 16:16:23 -0700

    Allow __indirect_function_table to be DCE'd
    Prior this this change `__indirect_function_table` would be seen
    as an unexpected user export and therefore added to
    `user_requested_exports` which in turn would mean it would be marked as
    metaDCE root.

--- tools/shared.py
+++ tools/shared.py
@@ -654,7 +654,7 @@ def is_c_symbol(name):
 
 
 def treat_as_user_export(name):
-  return not name.startswith(('dynCall_', 'orig$'))
+  return name not in ['__indirect_function_table', 'memory'] and not name.startswith(('dynCall_', 'orig$'))
 
 
 def asmjs_mangle(name):
