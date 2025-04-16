ab612147327b459905bdd2941391468fce401e9a
Sam Clegg <sbc@chromium.org>
2025-04-16 15:15:05 -0700

    Error on unknown entries in INCOMING_MODULE_JS_API

--- tools/link.py
+++ tools/link.py
@@ -971,6 +971,10 @@ def phase_linker_setup(options, linker_args):  # noqa: C901, PLR0912, PLR0915
     else:
       default_setting('INCOMING_MODULE_JS_API', [])
 
+  for prop in settings.INCOMING_MODULE_JS_API:
+    if prop not in settings.ALL_INCOMING_MODULE_JS_API:
+      diagnostics.warning('unused-command-line-argument', f'invalid entry in INCOMING_MODULE_JS_API: {prop}')
+
   if 'noExitRuntime' in settings.INCOMING_MODULE_JS_API:
     settings.DEFAULT_LIBRARY_FUNCS_TO_INCLUDE.append('$noExitRuntime')
 
