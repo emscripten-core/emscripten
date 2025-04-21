41a730aae6536d1981238d3733e5b7691a0a64f5
Sam Clegg <sbc@chromium.org>
2025-04-21 09:06:17 -0700

    Mark certain JS library functions as `__noleakcheck` (#24146)
    The functions all allocate on first use and cache the results, never
    freeing. Marking them as `__noleakcheck` avoids lsan false positives.

--- src/lib/libegl.js
+++ src/lib/libegl.js
@@ -507,6 +507,8 @@ var LibraryEGL = {
   eglGetError: () => EGL.errorCode,
 
   // EGLAPI const char * EGLAPIENTRY eglQueryString(EGLDisplay dpy, EGLint name);
+  // The allocated strings are cached and never freed.
+  eglQueryString__noleakcheck: true,
   eglQueryString__deps: ['$stringToNewUTF8'],
   eglQueryString__proxy: 'sync',
   eglQueryString: (display, name) => {
