c4dd534f5671f2b523192fecd97b12c4b49ff883
Yan Pujante <yan@pongasoft.com>
2025-04-27 06:11:26 -0700

    attempt #2 at fixing Ruff failure

--- test/test_closure_args_port.py
+++ test/test_closure_args_port.py
@@ -1,14 +1,14 @@
 import os
 
-def get(unused_ports, unused_settings, unused_shared):
+def get(ports, settings, shared): # noqa: ARG001
   return []
 
 
-def clear(unused_ports, unused_settings, unused_shared):
+def clear(ports, settings, shared): # noqa: ARG001
   pass
 
 
-def linker_setup(unused_ports, settings):
+def linker_setup(ports, settings): # noqa: ARG001
   externs_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                              'test_closure_externs.js')
   settings.CLOSURE_ARGS += [ f'--externs={externs_file}']
