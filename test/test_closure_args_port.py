ba093247a029b88e5ae1cd17882f3969e2346219
Yan Pujante <yan@pongasoft.com>
2025-04-27 06:06:37 -0700

    attempt at fixing Ruff failure

--- test/test_closure_args_port.py
+++ test/test_closure_args_port.py
@@ -1,14 +1,14 @@
 import os
 
-def get(ports, settings, shared):
+def get(unused_ports, unused_settings, unused_shared):
   return []
 
 
-def clear(ports, settings, shared):
+def clear(unused_ports, unused_settings, unused_shared):
   pass
 
 
-def linker_setup(ports, settings):
+def linker_setup(unused_ports, settings):
   externs_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                              'test_closure_externs.js')
   settings.CLOSURE_ARGS += [ f'--externs={externs_file}']
