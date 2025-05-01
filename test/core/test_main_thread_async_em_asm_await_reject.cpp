990ac1b5170c074b794a630e0b9b323da3b75f35
Amit Shani <amit.shani1995@gmail.com>
2025-05-01 13:48:49 +0300

    WIP: CR

--- test/core/test_main_thread_async_em_asm_await_reject.cpp
+++ test/core/test_main_thread_async_em_asm_await_reject.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 The Emscripten Authors.  All rights reserved.
+// Copyright 2025 The Emscripten Authors.  All rights reserved.
 // Emscripten is available under two separate licenses, the MIT license and the
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
@@ -7,26 +7,27 @@
 #include <stdio.h>
 #include <pthread.h>
 
-int main()
-{
+void *thread_func(void* arg) {
+  printf("Before MAIN_THREAD_EM_ASM_AWAIT\n");
+  int res = MAIN_THREAD_EM_ASM_AWAIT({
+    out('Inside MAIN_THREAD_EM_ASM_AWAIT: ' + $0 + ' ' + $1);
+    const asyncOp = new Promise((resolve,reject) => {
+      setTimeout(() => {
+        out('Inside asyncOp');
+        reject(new Error('asyncOp rejected'));
+      }, 1000);
+    });
+    return asyncOp;
+  }, 42, 3.5);
+  printf("After MAIN_THREAD_EM_ASM_AWAIT rejected\n");
+  printf("result: %d\n", res);
+  return NULL;
+}
+
+int main() {
   // start new thread
   pthread_t thread;
-  pthread_create(&thread, NULL, [](void*) -> void* {
-    printf("Before MAIN_THREAD_EM_ASM_AWAIT\n");
-    int res = MAIN_THREAD_EM_ASM_AWAIT({
-      out('Inside MAIN_THREAD_EM_ASM_AWAIT: ' + $0 + ' ' + $1);
-      const asyncOp = new Promise((resolve,reject) => {
-        setTimeout(() => {
-          out('Inside asyncOp');
-          reject(new Error('asyncOp rejected'));
-        }, 1000);
-      });
-      return asyncOp;
-    }, 42, 3.5);
-    printf("After MAIN_THREAD_EM_ASM_AWAIT rejected\n");
-    printf("result: %d\n", res);
-    return NULL;
-  }, NULL);
+  pthread_create(&thread, NULL, thread_func, NULL);
 
   // wait for thread to finish
   pthread_join(thread, NULL);
