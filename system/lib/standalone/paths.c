52a4e033066923cf746f8ee62ecac24eae7638a1
Jan Kokemüller <jan.kokemueller@gmail.com>
2025-05-03 16:19:29 +0200

    do preopen handling only in PURE_WASI mode

--- system/lib/standalone/paths.c
+++ system/lib/standalone/paths.c
@@ -21,6 +21,80 @@ typedef struct preopen {
 /// A simple growable array of `preopen`.
 static preopen* preopens;
 static size_t num_preopens;
+
+/// Are the `prefix_len` bytes pointed to by `prefix` a prefix of `path`?
+static bool
+prefix_matches(const char* prefix, size_t prefix_len, const char* path) {
+  // Allow an empty string as a prefix of any relative path.
+  if (path[0] != '/' && prefix_len == 0)
+    return true;
+
+  // Check whether any bytes of the prefix differ.
+  if (memcmp(path, prefix, prefix_len) != 0)
+    return false;
+
+  // Ignore trailing slashes in directory names.
+  size_t i = prefix_len;
+  while (i > 0 && prefix[i - 1] == '/') {
+    --i;
+  }
+
+  // Match only complete path components.
+  char last = path[i];
+  return last == '/' || last == '\0';
+}
+
+bool __paths_resolve_path(int* resolved_dirfd, const char** path_ptr) {
+  const char* path = *path_ptr;
+
+  if (*resolved_dirfd != AT_FDCWD && path[0] != '/') {
+    return true;
+  }
+
+  // Strip leading `/` characters, the prefixes we're mataching won't have
+  // them.
+  while (*path == '/')
+    path++;
+  // Search through the preopens table. Iterate in reverse so that more
+  // recently added preopens take precedence over less recently addded ones.
+  size_t match_len = 0;
+  int fd = -1;
+  for (size_t i = num_preopens; i > 0; --i) {
+    const preopen* pre = &preopens[i - 1];
+    const char* prefix = pre->prefix;
+    size_t len = strlen(prefix);
+
+    // If we haven't had a match yet, or the candidate path is longer than
+    // our current best match's path, and the candidate path is a prefix of
+    // the requested path, take that as the new best path.
+    if ((fd == -1 || len > match_len) && prefix_matches(prefix, len, path)) {
+      fd = pre->fd;
+      match_len = len;
+    }
+  }
+
+  if (fd == -1) {
+    return false;
+  }
+
+  // The relative path is the substring after the portion that was matched.
+  const char* computed = path + match_len;
+
+  // Omit leading slashes in the relative path.
+  while (*computed == '/')
+    ++computed;
+
+  // *at syscalls don't accept empty relative paths, so use "." instead.
+  if (*computed == '\0')
+    computed = ".";
+
+  *resolved_dirfd = fd;
+  *path_ptr = computed;
+  return true;
+}
+
+#if defined(EMSCRIPTEN_PURE_WASI)
+
 static size_t preopen_capacity;
 
 #ifdef NDEBUG
@@ -112,77 +186,6 @@ static bool register_preopened_fd(__wasi_fd_t fd, const char* relprefix) {
   return true;
 }
 
-/// Are the `prefix_len` bytes pointed to by `prefix` a prefix of `path`?
-static bool
-prefix_matches(const char* prefix, size_t prefix_len, const char* path) {
-  // Allow an empty string as a prefix of any relative path.
-  if (path[0] != '/' && prefix_len == 0)
-    return true;
-
-  // Check whether any bytes of the prefix differ.
-  if (memcmp(path, prefix, prefix_len) != 0)
-    return false;
-
-  // Ignore trailing slashes in directory names.
-  size_t i = prefix_len;
-  while (i > 0 && prefix[i - 1] == '/') {
-    --i;
-  }
-
-  // Match only complete path components.
-  char last = path[i];
-  return last == '/' || last == '\0';
-}
-
-bool __paths_resolve_path(int* resolved_dirfd, const char** path_ptr) {
-  const char* path = *path_ptr;
-
-  if (*resolved_dirfd != AT_FDCWD && path[0] != '/') {
-    return true;
-  }
-
-  // Strip leading `/` characters, the prefixes we're mataching won't have
-  // them.
-  while (*path == '/')
-    path++;
-  // Search through the preopens table. Iterate in reverse so that more
-  // recently added preopens take precedence over less recently addded ones.
-  size_t match_len = 0;
-  int fd = -1;
-  for (size_t i = num_preopens; i > 0; --i) {
-    const preopen* pre = &preopens[i - 1];
-    const char* prefix = pre->prefix;
-    size_t len = strlen(prefix);
-
-    // If we haven't had a match yet, or the candidate path is longer than
-    // our current best match's path, and the candidate path is a prefix of
-    // the requested path, take that as the new best path.
-    if ((fd == -1 || len > match_len) && prefix_matches(prefix, len, path)) {
-      fd = pre->fd;
-      match_len = len;
-    }
-  }
-
-  if (fd == -1) {
-    return false;
-  }
-
-  // The relative path is the substring after the portion that was matched.
-  const char* computed = path + match_len;
-
-  // Omit leading slashes in the relative path.
-  while (*computed == '/')
-    ++computed;
-
-  // *at syscalls don't accept empty relative paths, so use "." instead.
-  if (*computed == '\0')
-    computed = ".";
-
-  *resolved_dirfd = fd;
-  *path_ptr = computed;
-  return true;
-}
-
 // Populate WASI preopens.
 __attribute__((constructor(100))) // construct this before user code
 static void _standalone_populate_preopens(void) {
@@ -226,3 +229,5 @@ oserr:
 software:
   _Exit(EX_SOFTWARE);
 }
+
+#endif
