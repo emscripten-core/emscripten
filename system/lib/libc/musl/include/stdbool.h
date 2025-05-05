daa98e05dbf2a840bd8454ce73cf7fc7544ba80b
Hood Chatham <roberthoodchatham@gmail.com>
2025-05-05 13:02:23 -0400

    Change definition of true and false macros to be js friendly

--- system/lib/libc/musl/include/stdbool.h
+++ system/lib/libc/musl/include/stdbool.h
@@ -3,8 +3,11 @@
 
 #ifndef __cplusplus
 
-#define true 1
-#define false 0
+// These work for both C and javascript.
+// In C !!0 ==> 0 and in javascript !!0 ==> false
+// In C !!1 ==> 1 and in javascript !!1 ==> true
+#define true (!!1)
+#define false (!!0)
 #define bool _Bool
 
 #endif
