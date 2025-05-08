160bf62f0582f7ba810ae8065b808d42b6f1693b
Ingvar Stepanyan <me@rreverser.com>
2025-05-08 15:44:00 +0100

    Delete invalid test
    Test expectations were incorrect: it mixed up int64/uint64 and was returning negative number in uint64 function and expecting the negative sign to be preserved. Now that behaviour of uint64 is fixed, this expectation doesn't hold.
    
    Additionally, it was relying on the `n` suffix being printed by `console.log`, which is not portable and is not printed in e.g. latest Node.js.
    
    The correct test is now better served by test_i64_binding which correctly checks the type limits, signs, and will also run across more engines.

--- test/test_other.py
+++ test/test_other.py
@@ -3396,14 +3396,6 @@ More info: https://emscripten.org
 
     self.do_runf('embind/test_return_value_policy.cpp')
 
-  @parameterized({
-    '': [[]],
-    'asyncify': [['-sASYNCIFY=1']],
-  })
-  def test_embind_long_long(self, args):
-    self.do_runf('embind/test_embind_long_long.cpp', '1000000000000n\n-1000000000000n',
-                 emcc_args=['-lembind', '-sWASM_BIGINT'] + args)
-
   @requires_node_canary
   def test_embind_resource_management(self):
     self.node_args.append('--js-explicit-resource-management')
