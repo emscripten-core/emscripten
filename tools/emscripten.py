emscripten-core/emscripten

commit 08d2cc3c559c64f377bc932bcc418fd661998232

Disallow exports that are not valid C/C++ identifiers (#23563)
I use `std.isdentifier()` here from python since it has the same rules
for valid identifiers are C/C++.

We could try instead to allow this but I'm not sure it worth it given
that our inputs are almost exclusively compiled from C/C++/rust.

See #23560

tools/emscripten.py
@@ -578,6 +578,9 @@ def finalize_wasm(infile, outfile, js_syms):
   # These are any exports that were not requested on the command line and are
   # not known auto-generated system functions.
   unexpected_exports = [e for e in metadata.all_exports if treat_as_user_export(e)]
+  for n in unexpected_exports:
+    if not n.isidentifier():
+      exit_with_error(f'invalid export name: {n}')
   unexpected_exports = [asmjs_mangle(e) for e in unexpected_exports]
   unexpected_exports = [e for e in unexpected_exports if e not in expected_exports]
 

test/test_other.py
@@ -15539,3 +15539,8 @@ addToLibrary({
     }
     ''')
     self.do_runf('main.cpp', 'Hello Module!', emcc_args=['-std=c++20', '-fmodules'])
+
+  def test_invalid_export_name(self):
+    create_file('test.c', '__attribute__((export_name("my.func"))) void myfunc() {}')
+    err = self.expect_fail([EMCC, 'test.c'])
+    self.assertContained('emcc: error: invalid export name: my.func', err)

tools/shared.py
@@ -651,7 +651,7 @@ def is_c_symbol(name):
 
 
 def treat_as_user_export(name):
-  return not name.startswith('dynCall_')
+  return not name.startswith(('dynCall_', 'orig$'))
 
 
 def asmjs_mangle(name):

commit 9ed9266e41bb57958f7e7520e2dc6b586617129f

Don't check exports for being valid C/C++ identifiers in side modules
This unbreaks Rust side modules, which have been broken since
https://github.com/emscripten-core/emscripten/pull/23563
was merged.

tools/emscripten.py
@@ -562,9 +562,13 @@ def finalize_wasm(infile, outfile, js_syms):
   # These are any exports that were not requested on the command line and are
   # not known auto-generated system functions.
   unexpected_exports = [e for e in metadata.all_exports if shared.is_user_export(e)]
-  for n in unexpected_exports:
-    if not n.isidentifier():
-      exit_with_error(f'invalid export name: {n}')
+  # Rust side modules may have exported symbols that are not valid
+  # identifiers. They are meant to be called from native code in the main
+  # module not from JavaScript anyways, so don't perform this check on them.
+  if not settings.SIDE_MODULE:
+    for n in unexpected_exports:
+      if not n.isidentifier():
+        exit_with_error(f'invalid export name: {n}')
   unexpected_exports = [asmjs_mangle(e) for e in unexpected_exports]
   unexpected_exports = [e for e in unexpected_exports if e not in expected_exports]
 