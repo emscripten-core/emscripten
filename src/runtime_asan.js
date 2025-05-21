8545d0e6e3f0bd3118b5dec37d49097177389b45
Ingvar Stepanyan <me@rreverser.com>
2025-05-21 12:57:46 +0100

    Fix ASan checks for newly hit code paths

--- src/runtime_asan.js
+++ src/runtime_asan.js
@@ -12,18 +12,23 @@
 // ASan instrumentation on them. However, until the wasm module is ready, we
 // must access things directly.
 
-function _asan_js_load(arr, index) {
+function _asan_js_check_index(arr, index, asanFn) {
+#if EXIT_RUNTIME
+  if (runtimeInitialized && !runtimeExited) {
+#else
   if (runtimeInitialized) {
+#endif
     const elemSize = arr.BYTES_PER_ELEMENT;
-    ___asan_loadN(index * elemSize, elemSize);
+    asanFn(index * elemSize, elemSize);
   }
+}
+
+function _asan_js_load(arr, index) {
+  _asan_js_check_index(arr, index, ___asan_loadN);
   return arr[index];
 }
 
 function _asan_js_store(arr, index, value) {
-  if (runtimeInitialized) {
-    const elemSize = arr.BYTES_PER_ELEMENT;
-    ___asan_storeN(index * elemSize, elemSize);
-  }
+  _asan_js_check_index(arr, index, ___asan_storeN);
   return arr[index] = value;
 }
