91062368c36756dd3888130ffbd57a546cd7280f
Bas Hendri <dipidoo@users.noreply.github.com>
2025-05-28 13:21:39 -0700

    fixes alignment and tests async

--- test/fetch/test_fetch_redirect.c
+++ test/fetch/test_fetch_redirect.c
@@ -9,16 +9,48 @@
 #include <math.h>
 #include <emscripten/fetch.h>
 
+int fetchSync() {
+  emscripten_fetch_attr_t attr;
+  emscripten_fetch_attr_init(&attr);
+  strcpy(attr.requestMethod, "GET");
+  attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY | EMSCRIPTEN_FETCH_SYNCHRONOUS | EMSCRIPTEN_FETCH_REPLACE;
+  emscripten_fetch_t *fetch = emscripten_fetch(&attr, "https://httpbin.org/status/307");
+  assert(fetch);
+  printf("Fetch sync finished with status %d\n", fetch->status);
+  assert(fetch->status == 200);
+  printf("Downloaded %llu bytes", fetch->numBytes);
+  assert(strcmp(fetch->responseUrl, "https://httpbin.org/get") == 0);
+  exit(0);
+}
+
+void onsuccess(emscripten_fetch_t *fetch) {
+  printf("Fetch async finished with status %d\n", fetch->status);
+  assert(fetch->status == 200);
+  printf("Downloaded %llu bytes", fetch->numBytes);
+  assert(strcmp(fetch->responseUrl, "https://httpbin.org/get") == 0);
+  emscripten_fetch_close(fetch);
+  fetchSync();
+}
+
+void onreadystatechange(emscripten_fetch_t *fetch) {
+  printf("Fetch readyState %d responseUrl %s\n", fetch->readyState, fetch->responseUrl ? fetch->responseUrl : "is null");
+  if (fetch->readyState < 2 && ) {
+    if (fetch->responseUrl) {
+      assert(0 == strcmp(fetch->responseUrl, ""));
+    }
+  } else {
+    assert(0 == strcmp(fetch->responseUrl, "https://httpbin.org/get"));
+  }
+}
+
 int main() {
   emscripten_fetch_attr_t attr;
   emscripten_fetch_attr_init(&attr);
   strcpy(attr.requestMethod, "GET");
-  attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY | EMSCRIPTEN_FETCH_SYNCHRONOUS;
+  attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
+  attr.onsuccess = onsuccess;
+  attr.onreadystatechange = onreadystatechange;
   emscripten_fetch_t *fetch = emscripten_fetch(&attr, "https://httpbin.org/status/307");
   assert(fetch);
-  printf("Fetch finished with status %d\n", fetch->status);
-  assert(fetch->status == 200);
-  printf("Downloaded %llu bytes", fetch->numBytes);
-  assert(strcmp(fetch->responseUrl, "https://httpbin.org/get") == 0);
-  return 0;
+  return 99;
 }
