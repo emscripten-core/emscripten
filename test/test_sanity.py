97f974cd22d2ab21292611509d70b449741431be
juj <jujjyl@gmail.com>
2025-05-29 12:17:11 +0300

    Fix bootstrap.py script to not depend on tools/shared.py (#24424)
    Fix bootstrap.py script to not depend on tools/shared.py so that it can
    be run during emsdk installation phase when no .emscripten config yet
    exists. Fixes https://github.com/emscripten-core/emsdk/issues/1545

--- test/test_sanity.py
+++ test/test_sanity.py
@@ -817,3 +817,20 @@ fi
 
     # Now the compiler should work again
     self.run_process([EMCC, test_file('hello_world.c')])
+
+  # Verify that running bootstrap.py in a first-time run scenario should not
+  # cause an exception. (A first time run scenario is before .emscripten, env.
+  # vars nor PATH has been configured)
+  def test_bootstrap_without_em_config(self):
+    # Remove all environment variables that might help config.py to locate Emscripten tools.
+    env = os.environ.copy()
+    for e in ['LLVM_ROOT', 'EMSDK_NODE', 'EMSDK_PYTHON', 'EMSDK', 'EMSCRIPTEN', 'BINARYEN_ROOT', 'EMCC_SKIP_SANITY_CHECK', 'EM_CONFIG']:
+      env.pop(e, None)
+
+    # Remove from PATH every directory that contains clang.exe so that bootstrap.py cannot
+    # accidentally succeed by virtue of locating tools in PATH.
+    new_path = [d for d in env['PATH'].split(os.pathsep) if not os.path.isfile(os.path.join(d, shared.exe_suffix('clang')))]
+    env['PATH'] = os.pathsep.join(new_path)
+
+    # Running bootstrap.py should not fail
+    self.run_process([shared.bat_suffix(shared.path_from_root('bootstrap'))], env=env)
