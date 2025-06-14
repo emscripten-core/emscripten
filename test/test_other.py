emscripten-core/emscripten

commit 9a3961030fb02e2149cd59755338b49d6dae752e

Rename JS library files from library_foo.js to libfoo.js (#23348)
At the same time, move all the library file into a separate `src/lib`
directory. This helps distinguish library files from other files
in the `lib` directory, which is currently very overcrowded/overloaded.
These files were also include-able via `-lfoo` and we previously had to
use a special case for system libraries where `library_foo.js` could be
used in addition to more standard `libfoo.js`. This change removes to
special case.

This only file that was not renamed using the simple pattern was
`library.py` itself which becomes `libcore.py`. I also considered
`libemscripten.js`.

To some this may seem like needless churn, but for years now the `src`
directory (IMHO) has been asking for cleanup. I think having files
logically separated into directories helps a lot with understanding the
project structure for both newcomers and old timers alike.
As a followup, I am also considering moving all the runtime files into
`src/include` (since these are all files that get `#included` into the
generated code directly. That would leave the `src` directory
containing only the JS compiler code itself which I think is another
important distinction.

src/parseTools.mjs
@@ -907,7 +907,7 @@ function makeModuleReceiveWithVar(localName, moduleName, defaultValue, noAssert)
 function makeRemovedFSAssert(fsName) {
   assert(ASSERTIONS);
   const lower = fsName.toLowerCase();
-  if (JS_LIBRARIES.includes(`library_${lower}.js`)) return '';
+  if (JS_LIBRARIES.includes(path.resolve(path.join('lib', `lib${lower}.js`)))) return '';
   return `var ${fsName} = '${fsName} is no longer included by default; build with -l${lower}.js';`;
 }
 

test/test_other.py
@@ -4857,7 +4857,7 @@ extraLibraryFuncs.push('jsfunc');
   def test_jslib_bad_config(self):
     create_file('lib.js', '''
       addToLibrary({
-       foo__sig: 'ii',
+        foo__sig: 'ii',
       });
       ''')
     err = self.expect_fail([EMCC, test_file('hello_world.c'), '--js-library=lib.js'])
@@ -4942,6 +4942,24 @@ extraLibraryFuncs.push('jsfunc');
     create_file('post.js', '#preprocess\n#error This is an error')
     err = self.expect_fail([EMCC, test_file('hello_world.c'), '--post-js', 'post.js'])
     self.assertContained('post.js:2: #error This is an error', err)
+
+  def test_jslib_has_library(self):
+    create_file('libfoo.js', '''
+    // libwasi.js should be included.
+    #if !LibraryManager.has('libwasi.js')
+    #error "oops 1"
+    #endif
+
+    // Checking for the legacy name should also work
+    #if !LibraryManager.has('library_wasi.js')
+    #error "oops 2"
+    #endif
+
+    #if LibraryManager.has('libbar.js')
+    #error "oops 3"
+    #endif
+    ''')
+    self.do_runf(test_file('hello_world.c'), emcc_args=['-L', '-lfoo.js'])
 
   def test_EMCC_BUILD_DIR(self):
     # EMCC_BUILD_DIR env var contains the dir we were building in, when running the js compiler (e.g. when
@@ -11445,7 +11463,7 @@ int main () {
   # Tests the library_c_preprocessor.js functionality.
   @crossplatform
   def test_c_preprocessor(self):
-    self.do_runf('test_c_preprocessor.c', emcc_args=['--js-library', path_from_root('src/library_c_preprocessor.js')])
+    self.do_runf('test_c_preprocessor.c', emcc_args=['--js-library', path_from_root('src/lib/libc_preprocessor.js')])
 
   # Test that legacy settings that have been fixed to a specific value and their value can no longer be changed,
   def test_legacy_settings_forbidden_to_change(self):
@@ -13159,7 +13177,7 @@ exec "$@"
     # function is added or its signature changed.  However it's easy to
     # rebaseline with --rebaseline.
     self.run_process([PYTHON, path_from_root('tools/maint/gen_sig_info.py'), '-o', 'out.js'])
-    self.assertFileContents(path_from_root('src/library_sigs.js'), read_file('out.js'))
+    self.assertFileContents(path_from_root('src/lib/libsigs.js'), read_file('out.js'))
 
   def test_gen_struct_info_env(self):
     # gen_struct_info.py builds C code in a very specific and low level way.  We don't want

commit bf5d63c2c0cf244a5a8c31f2261c0d01c49d8f7b

Fix `makeRemovedFSAssert()` resolved path (#24567)
This fixes an issue where `path.resolve()` resolves using the CWD (which
is usually the target project path) instead of the directory of the
script itself.

The current behavior [causes issues with the Closure
compiler](https://github.com/godotengine/godot/pull/107460#issuecomment-2970895256),
as stubs are erroneously created instead of being skipped in
`src/shell.js`. Closure then complains that variables are declared
twice.

The issue was caused by #23348 which changed the logic of the search.

src/parseTools.mjs
@@ -19,6 +19,7 @@ import {
   runInMacroContext,
   pushCurrentFile,
   popCurrentFile,
+  localFile,
   warn,
   srcDir,
 } from './utility.mjs';
@@ -921,7 +922,7 @@ function makeModuleReceiveWithVar(localName, moduleName, defaultValue) {
 function makeRemovedFSAssert(fsName) {
   assert(ASSERTIONS);
   const lower = fsName.toLowerCase();
-  if (JS_LIBRARIES.includes(path.resolve(path.join('lib', `lib${lower}.js`)))) return '';
+  if (JS_LIBRARIES.includes(localFile(path.join('lib', `lib${lower}.js`)))) return '';
   return `var ${fsName} = '${fsName} is no longer included by default; build with -l${lower}.js';`;
 }
 

test/test_other.py
@@ -16234,3 +16234,18 @@ addToLibrary({
     # and in debug mode at least we expect to see the error message from libc++
     expected = 'system_error was thrown in -fno-exceptions mode with error 6 and message "thread constructor failed"'
     self.do_runf('main.cpp', expected, assert_returncode=NON_ZERO)
+
+  def test_parsetools_make_removed_fs_assert(self):
+    """
+    This tests that parseTools.mjs `makeRemovedFSAssert()` works as intended,
+    if it creates a stub when a builtin library isn't included, but not when
+    it is.
+    """
+
+    removed_fs_assert_content = "IDBFS is no longer included by default"
+
+    self.emcc(test_file('hello_world.c'), output_filename='hello_world.js')
+    self.assertContained(removed_fs_assert_content, read_file('hello_world.js'))
+
+    self.emcc(test_file('hello_world.c'), ['-lidbfs.js'], output_filename='hello_world.js')
+    self.assertNotContained(removed_fs_assert_content, read_file('hello_world.js'))