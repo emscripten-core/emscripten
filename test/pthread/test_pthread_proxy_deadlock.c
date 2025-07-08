f6868efda987929127c4878b27869b8bf754b3f1
Brendan Dahl <brendan.dahl@gmail.com>
2025-07-08 22:13:24 +0000

    add test

---
+++ test/pthread/test_pthread_proxy_deadlock.c
@@ -0,0 +1,47 @@
+// Copyright 2025 The Emscripten Authors.  All rights reserved.
+// Emscripten is available under two separate licenses, the MIT license and the
+// University of Illinois/NCSA Open Source License.  Both these licenses can be
+// found in the LICENSE file.
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <emscripten/console.h>
+#include <emscripten/heap.h>
+#include <emscripten/proxying.h>
+#include <emscripten/threading.h>
+
+bool should_quit = false;
+pthread_t looper;
+
+// In the actual implementation of malloc the system queue may be executed
+// non-deterministically if malloc is waiting on a mutex. This wraps malloc and
+// executes the system queue during every allocation to make the behavior
+// deterministic.
+void *malloc(size_t size) {
+  if (emscripten_proxy_get_system_queue() && emscripten_is_main_runtime_thread()) {
+    emscripten_proxy_execute_queue(emscripten_proxy_get_system_queue());
+  }
+  void *ptr = emscripten_builtin_malloc(size);
+  return ptr;
+}
+
+void run_on_looper(void* arg) {
+  emscripten_out("run_on_looper\n");
+  should_quit = true;
+}
+
+void* looper_main(void* arg) {
+  while (!should_quit) {
+    emscripten_proxy_execute_queue(emscripten_proxy_get_system_queue());
+    sched_yield();
+  }
+  return NULL;
+}
+
+int main() {
+  pthread_create(&looper, NULL, looper_main, NULL);
+  emscripten_proxy_async(emscripten_proxy_get_system_queue(), looper, run_on_looper, NULL);
+  pthread_join(looper, NULL);
+  emscripten_out("done\n");
+}
--- test/test_core.py
+++ test/test_core.py
@@ -2564,6 +2564,10 @@ The current type of b is: 9
   def test_pthread_cancel_async(self):
     self.do_run_in_out_file_test('pthread/test_pthread_cancel_async.c')
 
+  @node_pthreads
+  def test_pthread_proxy_deadlock(self):
+    self.do_runf('pthread/test_pthread_proxy_deadlock.c')
+
   @no_asan('test relies on null pointer reads')
   def test_pthread_specific(self):
     self.do_run_in_out_file_test('pthread/specific.c')
