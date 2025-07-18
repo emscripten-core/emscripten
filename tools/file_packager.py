a81f4a49670a098f60cd396879c08493695f3799
Łukasz Kwinta <33606064+lkwinta@users.noreply.github.com>
2025-07-18 11:37:18 +0000

    fix python format

--- tools/file_packager.py
+++ tools/file_packager.py
@@ -160,7 +160,7 @@ def has_hidden_attribute(filepath):
 
   try:
     attrs = ctypes.windll.kernel32.GetFileAttributesW(
-        u'%s' % filepath)
+        '%s' % filepath)
     assert attrs != -1
     result = bool(attrs & 2)
   except Exception:
@@ -633,7 +633,7 @@ def generate_js(data_target, data_files, metadata):
   ret +=  '''
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;'''
-  
+
   if not options.modularize and not options.from_emcc:
     ret +=  '''
   (() => {'''
@@ -1154,7 +1154,7 @@ def generate_js(data_target, data_files, metadata):
     ret += '''
     }
     loadPackage(%s);\n''' % json.dumps(metadata)
-  
+
   if options.modularize and not options.from_emcc:
     ret += '''
     });'''
@@ -1167,7 +1167,7 @@ def generate_js(data_target, data_files, metadata):
   (() => {
   var real_createModule = createModule;
 
-  createModule = function (moduleArg) { 
+  createModule = function (moduleArg) {
     if (new.target) throw new Error("createModule() should not be called with `new createModule()`");
     return real_createModule(moduleArg);
   }
