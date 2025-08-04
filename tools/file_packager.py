93e64b1ce62a39fbf2bc09b139cd3f811bc2d2da
Łukasz Kwinta <33606064+lkwinta@users.noreply.github.com>
2025-08-04 21:47:35 +0000

    fix some stylistic issues

--- tools/file_packager.py
+++ tools/file_packager.py
@@ -633,7 +633,7 @@ def generate_js(data_target, data_files, metadata):
     ret = ''
   else:
     if options.modularize:
-      ret = '''export default function loadDataFile(Module) {'''
+      ret = 'export default function loadDataFile(Module) {'
 
     else:
       ret = '''
@@ -1165,7 +1165,7 @@ def generate_js(data_target, data_files, metadata):
     loadPackage(%s);\n''' % json.dumps(metadata)
 
   if options.modularize:
-    ret += '''\n};\n// END the loadDataFile function'''
+    ret += '\n};\n// END the loadDataFile function'
   else:
     ret += '''
   })();\n'''
