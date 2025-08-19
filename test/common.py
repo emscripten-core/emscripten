59848be9e974ded7afd3c9cb1df6b6283a0ca9a1
juj <jujjyl@gmail.com>
2025-08-19 14:30:43 +0300

    Fix regression to enable specifying EMTEST_BROWSER env. var in native path format on Windows. (#24962)
    Fix regression to enable specifying EMTEST_BROWSER env. var in native
    path format on Windows.
    
    This is something that used to work a long time ago, and at some
    refactor got lost. On Windows environment variables that point to
    directories or files canonically use native path slashes `\` to do so.

--- test/common.py
+++ test/common.py
@@ -2340,6 +2340,13 @@ class BrowserCore(RunnerCore):
     if not EMTEST_BROWSER:
       logger.info('No EMTEST_BROWSER set. Defaulting to `google-chrome`')
       EMTEST_BROWSER = 'google-chrome'
+    if WINDOWS:
+      # On Windows env. vars canonically use backslashes as directory delimiters, e.g.
+      # set EMTEST_BROWSER=C:\Program Files\Mozilla Firefox\firefox.exe
+      # and spaces are not escaped. But make sure to also support args, e.g.
+      # set EMTEST_BROWSER="C:\Users\clb\AppData\Local\Google\Chrome SxS\Application\chrome.exe" --enable-unsafe-webgpu
+      if '"' not in EMTEST_BROWSER and "'" not in EMTEST_BROWSER:
+        EMTEST_BROWSER = '"' + EMTEST_BROWSER.replace("\\", "/") + '"'
     browser_args = shlex.split(EMTEST_BROWSER)
     logger.info('Launching browser: %s', str(browser_args))
     cls.browser_proc = subprocess.Popen(browser_args + [url])
