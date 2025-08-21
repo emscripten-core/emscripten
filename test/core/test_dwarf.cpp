f20fd5e56f7e25f853fdb8aafa367d8f34920791
landerlyoung <landerlyoung@gmail.com>
2025-08-21 13:04:04 +0800

    fix emsymbolizer failed to parse symbol map from C++ project #24982
    1. fix symbol map line parse
    2. unescape ascii code
    3. update tests: test/runner "other.test_emsymbolizer*"

--- test/core/test_dwarf.cpp
+++ test/core/test_dwarf.cpp
@@ -1,19 +1,21 @@
 #include <emscripten.h>
 
+namespace NameSpace {
 EM_JS(int, out_to_js, (int x), {})
 
 void __attribute__((noinline)) foo() {
-  out_to_js(0); // line 6
+  out_to_js(0); // line 7
   out_to_js(1);
   out_to_js(2);
 }
 
 void __attribute__((always_inline)) bar() {
   out_to_js(3);
-  __builtin_trap(); // line 13
+  __builtin_trap(); // line 14
 }
+};
 
 int main() {
-  foo();
-  bar();
+  NameSpace::foo();
+  NameSpace::bar();
 }
