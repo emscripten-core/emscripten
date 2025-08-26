a753a2e9d402b760951a9a82c00e818750ef5256
Sam Clegg <sbc@chromium.org>
2025-08-26 15:00:50 -0700

    Use NODE_JS_TEST over NODE_JS in test code
    See #20573

--- test/test_sanity.py
+++ test/test_sanity.py
@@ -610,8 +610,8 @@ fi
     jsengines = [('d8',     config.V8_ENGINE),
                  ('d8_g',   config.V8_ENGINE),
                  ('js',     config.SPIDERMONKEY_ENGINE),
-                 ('node',   config.NODE_JS),
-                 ('nodejs', config.NODE_JS)]
+                 ('node',   config.NODE_JS_TEST),
+                 ('nodejs', config.NODE_JS_TEST)]
     for filename, engine in jsengines:
       delete_file(SANITY_FILE)
       if type(engine) is list:
