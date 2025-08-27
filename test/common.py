455143f40b0d4560d8aebd7b6f4e2b65f88dddf0
Jukka Jylänki <jujjyl@gmail.com>
2025-08-28 00:30:50 +0300

    Review

--- test/common.py
+++ test/common.py
@@ -138,6 +138,9 @@ PYTHON = sys.executable
 if not config.NODE_JS_TEST:
   config.NODE_JS_TEST = config.NODE_JS
 
+if not config.JS_ENGINES:
+  config.JS_ENGINES = [config.NODE_JS]
+
 
 requires_network = unittest.skipIf(os.getenv('EMTEST_SKIP_NETWORK_TESTS'), 'This test requires network access')
 
