d1943c06095b4822b88675027ca2ea224ca4339e
Slavomir Kucera <slavomir.kucera@broadcom.com>
2025-08-29 10:20:59 +0200

    BE flag not propagated

--- test/test_core.py
+++ test/test_core.py
@@ -6910,6 +6910,7 @@ void* operator new(size_t size) {
   @is_slow_test
   @crossplatform
   @no_wasmfs('depends on MEMFS which WASMFS does not have')
+  @no_big_endian('SUPPORT_BIG_ENDIAN is not propagated')
   def test_poppler(self):
     # See https://github.com/emscripten-core/emscripten/issues/20757
     self.cflags.extend(['-Wno-deprecated-declarations', '-Wno-nontrivial-memaccess'])
