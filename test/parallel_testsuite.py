8e7221d00bfe251a04daca8a5ca7bdb010dfc1ae
Jukka Jylänki <jujjyl@gmail.com>
2025-08-29 15:03:57 +0300

    Cleanup

--- test/parallel_testsuite.py
+++ test/parallel_testsuite.py
@@ -212,7 +212,9 @@ class BufferedParallelTestResult:
       # Write profiling entries for emprofile.py tool to visualize. This needs a unique identifier for each
       # block, so generate one on the fly.
       dummy_test_task_counter = os.path.getsize(profiler_log_file) if os.path.isfile(profiler_log_file) else 0
-      prof.write(f',\n{{"pid":{dummy_test_task_counter},"op":"start","time":{self.start_time},"cmdLine":["{self.test_short_name()}"],"color":"{colors[self.test_result]}"}}')
+      # Remove the redundant 'test_' suffix from each test, since character space is at a premium in the visualized graph.
+      test_name = self.test_short_name().removeprefix('test_')
+      prof.write(f',\n{{"pid":{dummy_test_task_counter},"op":"start","time":{self.start_time},"cmdLine":["{test_name}"],"color":"{colors[self.test_result]}"}}')
       prof.write(f',\n{{"pid":{dummy_test_task_counter},"op":"exit","time":{self.start_time + self.test_duration},"returncode":0}}')
 
   def startTest(self, test):
