fa795df959993e228af861cabfe8aaebf435b6ab
Jukka Jylänki <jujjyl@gmail.com>
2025-09-02 23:34:23 +0300

    Automatic rebaseline of codesize expectations. NFC
    This is an automatic change generated by tools/maint/rebaseline_tests.py.
    
    The following (26) test expectation files were updated by
    running the tests with `--rebaseline`:
    
    ```
    code_size/test_codesize_cxx_ctors1.json: 149258 => 149263 [+5 bytes / +0.00%]
    code_size/test_codesize_cxx_ctors2.json: 148663 => 148668 [+5 bytes / +0.00%]
    code_size/test_codesize_cxx_except.json: 194681 => 194686 [+5 bytes / +0.00%]
    code_size/test_codesize_cxx_except_wasm.json: 164268 => 164273 [+5 bytes / +0.00%]
    code_size/test_codesize_cxx_except_wasm_legacy.json: 161857 => 161862 [+5 bytes / +0.00%]
    code_size/test_codesize_cxx_lto.json: 125540 => 125545 [+5 bytes / +0.00%]
    code_size/test_codesize_cxx_mangle.json: 258772 => 258777 [+5 bytes / +0.00%]
    code_size/test_codesize_cxx_noexcept.json: 151675 => 151680 [+5 bytes / +0.00%]
    code_size/test_codesize_cxx_wasmfs.json: 176935 => 176940 [+5 bytes / +0.00%]
    code_size/test_codesize_files_wasmfs.json: 55775 => 55780 [+5 bytes / +0.01%]
    code_size/test_codesize_hello_dylink.json: 45534 => 45543 [+9 bytes / +0.02%]
    code_size/test_codesize_hello_dylink_all.json: 843921 => 844093 [+172 bytes / +0.02%]
    code_size/test_codesize_mem_O3.json: 9654 => 9659 [+5 bytes / +0.05%]
    code_size/test_codesize_mem_O3_grow.json: 9940 => 9945 [+5 bytes / +0.05%]
    code_size/test_codesize_mem_O3_grow_standalone.json: 9667 => 9672 [+5 bytes / +0.05%]
    code_size/test_codesize_mem_O3_standalone.json: 9525 => 9530 [+5 bytes / +0.05%]
    code_size/test_codesize_mem_O3_standalone_lib.json: 8836 => 8841 [+5 bytes / +0.06%]
    code_size/test_codesize_mem_O3_standalone_narg.json: 8865 => 8870 [+5 bytes / +0.06%]
    code_size/test_codesize_mem_O3_standalone_narg_flto.json: 7677 => 7682 [+5 bytes / +0.07%]
    code_size/test_codesize_minimal_pthreads.json: 27242 => 27251 [+9 bytes / +0.03%]
    code_size/test_codesize_minimal_pthreads_memgrowth.json: 27670 => 27679 [+9 bytes / +0.03%]
    code_size/test_minimal_runtime_code_size_audio_worklet.json: 5882 => 5892 [+10 bytes / +0.17%]
    code_size/test_minimal_runtime_code_size_hello_embind.json: 15112 => 15122 [+10 bytes / +0.07%]
    code_size/test_minimal_runtime_code_size_hello_embind_val.json: 11750 => 11760 [+10 bytes / +0.09%]
    code_size/test_minimal_runtime_code_size_hello_wasm_worker.json: 3203 => 3215 [+12 bytes / +0.37%]
    test/parallel_testsuite.py updated
    
    Average change: +0.05% (+0.00% - +0.37%)
    ```

--- test/parallel_testsuite.py
+++ test/parallel_testsuite.py
@@ -154,7 +154,7 @@ class ParallelTestSuite(unittest.BaseTestSuite):
     # stderr, if stderr is in nonblocking mode, like it is on Buildbot CI:
     # See https://github.com/buildbot/buildbot/issues/8659
     # To work around that problem, set stderr to blocking mode before printing.
-    os.set_blocking(sys.stderr.fileno(), True)
+#    os.set_blocking(sys.stderr.fileno(), True)
 
     for r in results:
       r.updateResult(result)
