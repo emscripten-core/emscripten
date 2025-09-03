5df8e7b986584812c8ba9df9a397b31dd7fa11a8
juj <jujjyl@gmail.com>
2025-09-03 15:26:15 +0300

    Create a stress test that more deterministically reproduces #20067, #25026 and #15014. (#25066)
    Create a stress test that more deterministically reproduces flaky test
    errors.
    
    Ran the test https://github.com/emscripten-core/emscripten/issues/20067
    20 times, with the following results:
    ```
    Failed in 5.167s
    Failed in 2.002s
    Failed in 2.324s
    Failed in 2.564s
    Passed (no repro), took 12.276s
    Failed in 3.277s
    Passed (no repro), took 12.955s
    Failed in 5.717s
    Failed in 2.092s
    Failed in 3.836s
    Failed in 2.001s
    Failed in 3.475s
    Failed in 1.601s
    Failed in 5.307s
    Failed in 1.757s
    Failed in 4.716s
    Failed in 2.036s
    Failed in 3.001s
    Failed in 3.600s
    Failed in 7.481s
    ```
    
    giving a repro rate of ~90% on that test.

--- test/common.py
+++ test/common.py
@@ -28,6 +28,7 @@ import subprocess
 import sys
 import tempfile
 import textwrap
+import threading
 import time
 import webbrowser
 import unittest
@@ -1632,6 +1633,19 @@ class RunnerCore(unittest.TestCase, metaclass=RunnerMeta):
     """Return the default JS engine to run tests under"""
     return self.js_engines[0]
 
+  def get_engine_with_args(self, engine=None):
+    if not engine:
+      engine = self.get_current_js_engine()
+    # Make a copy of the engine command before we modify/extend it.
+    engine = list(engine)
+    if engine == config.NODE_JS_TEST:
+      engine += self.node_args
+    elif engine == config.V8_ENGINE:
+      engine += self.v8_args
+    elif engine == config.SPIDERMONKEY_ENGINE:
+      engine += self.spidermonkey_args
+    return engine
+
   def run_js(self, filename, engine=None, args=None,
              assert_returncode=0,
              interleaved_output=True,
@@ -1647,16 +1661,7 @@ class RunnerCore(unittest.TestCase, metaclass=RunnerMeta):
     stdout = open(stdout_file, 'w')
     error = None
     timeout_error = None
-    if not engine:
-      engine = self.get_current_js_engine()
-    # Make a copy of the engine command before we modify/extend it.
-    engine = list(engine)
-    if engine == config.NODE_JS_TEST:
-      engine += self.node_args
-    elif engine == config.V8_ENGINE:
-      engine += self.v8_args
-    elif engine == config.SPIDERMONKEY_ENGINE:
-      engine += self.spidermonkey_args
+    engine = self.get_engine_with_args(engine)
     try:
       jsrun.run_js(filename, engine, args,
                    stdout=stdout,
@@ -2135,6 +2140,57 @@ class RunnerCore(unittest.TestCase, metaclass=RunnerMeta):
           print('(test did not pass in JS engine: %s)' % engine)
           raise
     return js_output
+
+  def parallel_stress_test_js_file(self, js_file, assert_returncode=None, expected=None, not_expected=None):
+    # If no expectations were passed, expect a successful run exit code
+    if assert_returncode is None and expected is None and not_expected is None:
+      assert_returncode = 0
+
+    # We will use Python multithreading, so prepare the command to run in advance, and keep the threading kernel
+    # compact to avoid accessing unexpected data/functions across threads.
+    cmd = self.get_engine_with_args() + [js_file]
+
+    exception_thrown = threading.Event()
+    error_lock = threading.Lock()
+    error_exception = None
+
+    def test_run():
+      nonlocal error_exception
+      try:
+        # Each thread repeatedly runs the test case in a tight loop, which is critical to coax out timing related issues
+        for _ in range(16):
+          # Early out from the test, if error was found
+          if exception_thrown.is_set():
+            return
+          result = subprocess.run(cmd, capture_output=True, text=True)
+
+          output = f'\n----------------------------\n{result.stdout}{result.stderr}\n----------------------------'
+          if not_expected is not None and not_expected in output:
+            raise Exception(f'\n\nWhen running command "{cmd}",\nexpected string "{not_expected}" to NOT be present in output:{output}')
+          if expected is not None and expected not in output:
+            raise Exception(f'\n\nWhen running command "{cmd}",\nexpected string "{expected}" was not found in output:{output}')
+          if assert_returncode is not None:
+            if assert_returncode == NON_ZERO:
+              if result.returncode != 0:
+                raise Exception(f'\n\nCommand "{cmd}" was expected to fail, but did not (returncode=0). Output:{output}')
+            elif assert_returncode != result.returncode:
+              raise Exception(f'\n\nWhen running command "{cmd}",\nreturn code {result.returncode} does not match expected return code {assert_returncode}. Output:{output}')
+      except Exception as e:
+        if not exception_thrown.is_set():
+          exception_thrown.set()
+          with error_lock:
+            error_exception = e
+        return
+
+    threads = []
+    # Oversubscribe hardware threads to make sure scheduling becomes erratic
+    while len(threads) < 2 * multiprocessing.cpu_count() and not exception_thrown.is_set():
+      threads += [threading.Thread(target=test_run)]
+      threads[-1].start()
+    for t in threads:
+      t.join()
+    if error_exception:
+      raise error_exception
 
   def get_freetype_library(self):
     self.cflags += [
