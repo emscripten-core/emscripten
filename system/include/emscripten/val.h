68962040cf0245fbfed2e0184db3f64c538e6571
Brendan Dahl <brendan.dahl@gmail.com>
2025-09-04 18:50:42 +0000

    use differen variable name

--- system/include/emscripten/val.h
+++ system/include/emscripten/val.h
@@ -581,8 +581,8 @@ private:
     using Policy = WithPolicies<FilterTypes<isPolicy, Args...>>;
     auto filteredArgs = Filter<isNotPolicy>(args...);
     return std::apply(
-        [&](auto&&... filteredArgs) -> decltype(auto) {
-          return internalCallWithPolicy<Kind, Policy, Ret>(handle, methodName, std::forward<decltype(filteredArgs)>(filteredArgs)...);
+        [&](auto&&... actualArgs) -> decltype(auto) {
+          return internalCallWithPolicy<Kind, Policy, Ret>(handle, methodName, std::forward<decltype(actualArgs)>(actualArgs)...);
         },
         filteredArgs
     );
