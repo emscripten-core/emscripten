2784f486f59cc4ddc2cdd1e275f3dcb10f8f7e7b
Jukka Jylänki <jujjyl@gmail.com>
2025-09-11 00:43:58 +0300

    Fix startup config

--- test/common.py
+++ test/common.py
@@ -2486,15 +2486,15 @@ def configure_test_browser():
 
   if EMTEST_BROWSER_AUTO_CONFIG:
     logger.info('Using default CI configuration.')
+    config = None
     if is_chrome():
       config = ChromeConfig()
     elif is_firefox():
       config = FirefoxConfig()
-    else:
-      exit_with_error(f'EMTEST_BROWSER_AUTO_CONFIG only currently works with firefox or chrome. Browser was "{EMTEST_BROWSER}"')
-    EMTEST_BROWSER += ' ' + ' '.join(config.default_flags)
-    if EMTEST_HEADLESS == 1:
-      EMTEST_BROWSER += f" {config.headless_flags}"
+    if config:
+      EMTEST_BROWSER += ' ' + ' '.join(config.default_flags)
+      if EMTEST_HEADLESS == 1:
+        EMTEST_BROWSER += f" {config.headless_flags}"
 
 
 class BrowserCore(RunnerCore):
