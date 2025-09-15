3004076e17cbbc155ee42e522a6c373e5f1ee462
Christian Kündig <christian@kuendig.info>
2025-09-15 22:01:56 +0200

    Update Vorbis and Ogg ports (#25274)
    The current ogg and vorbis ports are ancient (2015) and pull in repos
    from the old _emscripten-ports_ github org. From what I can tell, they
    do not have any patches and both pre-date Xiph.org's migration to
    Github, so presumably are just old mirrors:
    - https://github.com/emscripten-ports/Vorbis/releases/tag/version_1 is
    the same as
    https://github.com/xiph/vorbis/commit/7187e7a48f0c3ba32cc080f6bc3d921fe4ec6cc2
    - https://github.com/emscripten-ports/Ogg/releases/tag/version_1 is the
    same as xiph/ogg/commit@9b2ba419aecb4a1c97114545d57174593dc13111
    
    This updates both to the most recent update.(ogg one before the most
    recent as I haven't yet tested the most recent update)

--- tools/ports/ogg.py
+++ tools/ports/ogg.py
@@ -6,8 +6,8 @@
 import os
 import shutil
 
-TAG = 'version_1'
-HASH = '929e8d6003c06ae09593021b83323c8f1f54532b67b8ba189f4aedce52c25dc182bac474de5392c46ad5b0dea5a24928e4ede1492d52f4dd5cd58eea9be4dba7'
+VERSION = '1.3.5'
+HASH = 'e7b5f5d469090b66dbb33634591ae6de41af3a5644c10488e59ba7428ed78912e208a2c0fbb5003ec5b7eb2a0102a2f85cecb21fa9512d790139ecc45b6d03f4'
 
 
 def needed(settings):
@@ -15,14 +15,14 @@ def needed(settings):
 
 
 def get(ports, settings, shared):
-  ports.fetch_project('ogg', f'https://github.com/emscripten-ports/ogg/archive/{TAG}.zip', sha512hash=HASH)
+  ports.fetch_project('ogg', f'https://github.com/xiph/ogg/releases/download/v{VERSION}/libogg-{VERSION}.zip', sha512hash=HASH)
 
   def create(final):
-    source_path = ports.get_dir('ogg', 'Ogg-' + TAG)
+    source_path = ports.get_dir('ogg', 'libogg-' + VERSION)
     config_types_h = os.path.join(os.path.dirname(__file__), 'ogg/config_types.h')
     shutil.copyfile(config_types_h, os.path.join(source_path, 'include/ogg/config_types.h'))
     ports.install_headers(os.path.join(source_path, 'include', 'ogg'), target='ogg')
-    ports.make_pkg_config('ogg', TAG, '-sUSE_OGG')
+    ports.make_pkg_config('ogg', VERSION, '-sUSE_OGG')
     ports.build_port(os.path.join(source_path, 'src'), final, 'ogg')
 
   return [shared.cache.get_lib('libogg.a', create)]
