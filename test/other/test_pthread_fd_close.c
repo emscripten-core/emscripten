275800bd449ba49ca294aabddc91016f52bccf30
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-09-29 18:49:58 +0200

    Incorporate feedback

--- test/other/test_pthread_fd_close.c
+++ test/other/test_pthread_fd_close.c
@@ -4,26 +4,26 @@
 #include <unistd.h>
 
 void* thread_func(void* arg) {
-    int* fd = (int*)arg;
+  int* fd = (int*)arg;
 
-    printf("thread main\n");
-    int rc = close(*fd);
-    printf("close: %d\n", rc);
+  printf("thread main\n");
+  int rc = close(*fd);
+  printf("close: %d\n", rc);
 
-    return NULL;
+  return NULL;
 }
 
 int main() {
-    printf("main\n");
-    int fd = open("example.txt", O_RDWR | O_CREAT, 0644);
+  printf("main\n");
+  int fd = open("example.txt", O_RDWR | O_CREAT, 0644);
 
-    pthread_t t;
-    int rc = pthread_create(&t, NULL, thread_func, &fd);
-    printf("pthread_create: %d\n", rc);
-    rc = pthread_join(t, NULL);
-    printf("pthread_join: %d\n", rc);
+  pthread_t t;
+  int rc = pthread_create(&t, NULL, thread_func, &fd);
+  printf("pthread_create: %d\n", rc);
+  rc = pthread_join(t, NULL);
+  printf("pthread_join: %d\n", rc);
 
-    printf("done\n");
+  printf("done\n");
 
-    return 0;
+  return 0;
 }
