6d7971567920fdcd113f5dd07b79e1cf37f38af4
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2025-10-08 01:37:23 +0900

    test: Add tests for timeout on select with PROXY_TO_PTHREAD
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- test/test_core.py
+++ test/test_core.py
@@ -9665,6 +9665,9 @@ NODEFS is no longer included by default; build with -lnodefs.js
   def test_syscall_intercept(self):
     self.do_core_test('test_syscall_intercept.c')
 
+  def test_select_blocking(self):
+    self.do_runf('core/test_select_blocking.c', cflags=['-pthread', '-sPROXY_TO_PTHREAD=1', '-sEXIT_RUNTIME=1'])
+
   @also_without_bigint
   def test_jslib_i64_params(self):
     # Tests the defineI64Param and receiveI64ParamAsI53 helpers that are
