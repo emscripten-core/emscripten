emscripten-core/emscripten

commit dae65b226d014ff857a4dc021072da27287d00b0

Support JS symbols that are aliases of native ones (#25376)
We can then use this mechanism to replace the hacky way to we map things
like `memory` -> `wasmMemory` and `__indirect_function_table` =>
`wasmTable`.

src/modules.mjs
@@ -29,6 +29,9 @@ import {preprocess, processMacros} from './parseTools.mjs';
 
 // List of symbols that were added from the library.
 export const librarySymbols = [];
+// Map of library symbols which are aliases for native symbols
+// e.g. `wasmTable` -> `__indirect_function_table`
+export const nativeAliases = {};
 
 const srcDir = fileURLToPath(new URL('.', import.meta.url));
 const systemLibdir = path.join(srcDir, 'lib');
@@ -448,7 +451,6 @@ function exportRuntimeSymbols() {
     'err',
     'callMain',
     'abort',
-    'wasmMemory',
     'wasmExports',
     'HEAPF32',
     'HEAPF64',
@@ -562,13 +564,7 @@ function exportLibrarySymbols() {
   assert(MODULARIZE != 'instance');
   const results = ['// Begin JS library exports'];
   for (const ident of librarySymbols) {
-    if (EXPORT_ALL || EXPORTED_FUNCTIONS.has(ident)) {
-      // Special case for wasmTable which can be both a JS library symbol but
-      // also a wasm export. See isDirectWasmExport in jsifier.mjs.
-      // FIXME: Remove this hack 
-      if (ident == 'wasmTable' && WASM_EXPORTS.has('__indirect_function_table')) {
-        continue;
-      }
+    if ((EXPORT_ALL || EXPORTED_FUNCTIONS.has(ident)) && !nativeAliases[ident]) {
       results.push(exportSymbol(ident));
     }
   }

tools/emscripten.py
@@ -421,8 +421,10 @@ def emscript(in_wasm, out_wasm, outfile_js, js_syms, finalize=True, base_metadat
     function_exports['asyncify_stop_rewind'] = webassembly.FuncType([], [])
 
   parts = [pre]
-  parts += create_module(metadata, function_exports, global_exports, other_exports, forwarded_json['librarySymbols'])
+  parts += create_module(metadata, function_exports, global_exports, other_exports,
+                         forwarded_json['librarySymbols'], forwarded_json['nativeAliases'])
   parts.append(post)
+  settings.ALIASES = list(forwarded_json['nativeAliases'].keys())
 
   full_js_module = ''.join(parts)
   full_js_module = apply_static_code_hooks(forwarded_json, full_js_module)
@@ -906,7 +908,7 @@ def should_export(sym):
   return settings.EXPORT_ALL or (settings.EXPORT_KEEPALIVE and sym in settings.EXPORTED_FUNCTIONS)
 
 
-def create_receiving(function_exports, other_exports, library_symbols):
+def create_receiving(function_exports, other_exports, library_symbols, aliases):
   generate_dyncall_assignment = 'dynCalls' in library_symbols
   receiving = ['\n// Imports from the Wasm binary.']
 
@@ -919,6 +921,9 @@ def create_receiving(function_exports, other_exports, library_symbols):
         exports.append(f'{sym} as {mangled}')
       else:
         exports.append(sym)
+    for alias, target in aliases.items():
+      exports.append(f'{target} as {alias}')
+
     receiving.append('import {')
     receiving.append('  ' + ',\n  '.join(exports))
     receiving.append(f"}} from './{settings.WASM_BINARY_FILE}';")
@@ -948,28 +953,32 @@ def create_receiving(function_exports, other_exports, library_symbols):
   for name in other_exports:
     exports[name] = None
 
+  mangled = [asmjs_mangle(s) for s in exports] + list(aliases.keys())
   if settings.ASSERTIONS:
     # In debug builds we generate trapping functions in case
     # folks try to call/use a reference that was taken before the
     # wasm module is available.
-    for sym in exports:
-      mangled = asmjs_mangle(sym)
-      assignment = mangled
-      if (settings.MODULARIZE or not settings.MINIMAL_RUNTIME) and should_export(mangled) and settings.MODULARIZE != 'instance':
-        assignment += f" = Module['{mangled}']"
-      receiving.append(f"var {assignment} = makeInvalidEarlyAccess('{mangled}');")
+    for sym in mangled:
+      assignment = sym
+      if (settings.MODULARIZE or not settings.MINIMAL_RUNTIME) and should_export(sym) and settings.MODULARIZE != 'instance':
+        assignment += f" = Module['{sym}']"
+      receiving.append(f"var {assignment} = makeInvalidEarlyAccess('{sym}');")
   else:
     # Declare all exports in a single var statement
     sep = ',\n  '
-    mangled = [asmjs_mangle(s) for s in exports]
     receiving.append(f'var {sep.join(mangled)};\n')
 
   if settings.MODULARIZE == 'instance':
-    mangled = [asmjs_mangle(e) for e in exports]
     esm_exports = [e for e in mangled if should_export(e)]
     if esm_exports:
       esm_exports = ', '.join(esm_exports)
       receiving.append(f'export {{ {esm_exports} }};')
+
+  alias_inverse_map = {}
+  logger.debug(json.dumps(aliases))
+  for sym, alias in aliases.items():
+    assert alias in exports, f'expected alias target ({alias}) to be exported'
+    alias_inverse_map.setdefault(alias, []).append(sym)
 
   receiving.append('\nfunction assignWasmExports(wasmExports) {')
   for sym, sig in exports.items():
@@ -980,7 +989,10 @@ def create_receiving(function_exports, other_exports, library_symbols):
       sig_str = sym.replace('dynCall_', '')
       assignment += f" = dynCalls['{sig_str}']"
     if (settings.MODULARIZE or not settings.MINIMAL_RUNTIME) and should_export(mangled) and settings.MODULARIZE != 'instance':
-       assignment += f" = Module['{mangled}']"
+      assignment += f" = Module['{mangled}']"
+    if sym in alias_inverse_map:
+      for target in alias_inverse_map[sym]:
+        assignment += f" = {target}"
     if is_function and install_debug_wrapper(sym):
       nargs = len(sig.params)
       receiving.append(f"  {assignment} = createExportWrapper('{sym}', {nargs});")
@@ -991,9 +1003,9 @@ def create_receiving(function_exports, other_exports, library_symbols):
   return '\n'.join(receiving)
 
 
-def create_module(metadata, function_exports, global_exports, other_exports, library_symbols):
+def create_module(metadata, function_exports, global_exports, other_exports, library_symbols, aliases):
   module = []
-  module.append(create_receiving(function_exports, other_exports, library_symbols))
+  module.append(create_receiving(function_exports, other_exports, library_symbols, aliases))
   module.append(create_global_exports(global_exports))
 
   sending = create_sending(metadata, library_symbols)

test/test_other.py
@@ -4888,6 +4888,62 @@ extraLibraryFuncs.push('jsfunc');
     err = self.expect_fail([EMCC, test_file('hello_world.c'), '--js-library', 'foo.js'])
     self.assertContained('foo.js:5: file not found: inc.js', err)
 
+  @parameterized({
+    '': ([],),
+    'closure': (['--closure=1'],),
+  })
+  def test_jslib_aliases(self, args):
+    create_file('foo.js', '''
+      addToLibrary({
+        foo: () => 42,
+        foo_alias: 'foo',
+
+        // Normal JS function that calls a native function
+        call_native__deps: ['native_func'],
+        call_native: () => _native_func(),
+
+        // JS function that calls nativeAlias
+        call_native_alias__deps: ['$nativeAlias'],
+        call_native_alias: () => nativeAlias(),
+
+        // This is a JS-only symbol (i.e. no leading underscore) that
+        // aliases a native symbol.
+        $nativeAlias__deps: ['native_func2'],
+        $nativeAlias: 'native_func2',
+      });
+    ''')
+    create_file('main.c', r'''
+      #include <stdio.h>
+      #include <emscripten.h>
+      int foo();
+      int foo_alias();
+      int call_native();
+      int call_native_alias();
+
+      int native_func() {
+        return 43;
+      }
+
+      int native_func2() {
+        return 44;
+      }
+
+      int main() {
+        printf("foo: %d\n", foo());
+        printf("foo_alias: %d\n", foo_alias());
+        printf("call_native: %d\n", call_native());
+        printf("call_native_alias: %d\n", call_native_alias());
+        return 0;
+      }
+    ''')
+    expected = '''\
+foo: 42
+foo_alias: 42
+call_native: 43
+call_native_alias: 44
+'''
+    self.do_runf('main.c', expected, cflags=['--js-library', 'foo.js'] + args)
+
   def test_postjs_errors(self):
     create_file('post.js', '#preprocess\n#error This is an error')
     err = self.expect_fail([EMCC, test_file('hello_world.c'), '--post-js', 'post.js'])

src/jsifier.mjs
@@ -40,7 +40,7 @@ import {
   warningOccured,
   localFile,
 } from './utility.mjs';
-import {LibraryManager, librarySymbols} from './modules.mjs';
+import {LibraryManager, librarySymbols, nativeAliases} from './modules.mjs';
 
 const addedLibraryItems = {};
 
@@ -128,9 +128,13 @@ function isDefined(symName) {
 }
 
 function resolveAlias(symbol) {
-  var value = LibraryManager.library[symbol];
-  if (typeof value == 'string' && value[0] != '=' && LibraryManager.library.hasOwnProperty(value)) {
-    return value;
+  while (true) {
+    var value = LibraryManager.library[symbol];
+    if (typeof value == 'string' && value[0] != '=' && (LibraryManager.library.hasOwnProperty(value) || WASM_EXPORTS.has(value))) {
+      symbol = value;
+    } else {
+      break;
+    }
   }
   return symbol;
 }
@@ -639,6 +643,7 @@ function(${args}) {
       });
 
       let isFunction = false;
+      let isNativeAlias = false;
 
       const postsetId = symbol + '__postset';
       const postset = LibraryManager.library[postsetId];
@@ -654,13 +659,22 @@ function(${args}) {
 
       if (typeof snippet == 'string') {
         if (snippet[0] != '=') {
-          if (LibraryManager.library[snippet]) {
+          if (LibraryManager.library[snippet] || WASM_EXPORTS.has(snippet)) {
             // Redirection for aliases. We include the parent, and at runtime
             // make ourselves equal to it.  This avoid having duplicate
             // functions with identical content.
-            const aliasTarget = snippet;
-            snippet = mangleCSymbolName(aliasTarget);
-            deps.push(aliasTarget);
+            const aliasTarget = resolveAlias(snippet);
+            if (WASM_EXPORTS.has(aliasTarget)) {
+              //printErr(`native alias: ${mangled} -> ${snippet}`);
+              //console.error(WASM_EXPORTS);
+              nativeAliases[mangled] = aliasTarget;
+              snippet = undefined;
+              isNativeAlias = true;
+            } else {
+              //printErr(`js alias: ${mangled} -> ${snippet}`);
+              deps.push(aliasTarget);
+              snippet = mangleCSymbolName(aliasTarget);
+            }
           }
         }
       } else if (typeof snippet == 'object') {
@@ -728,15 +742,11 @@ function(${args}) {
           contentText += ';';
         }
       } else if (typeof snippet == 'undefined') {
-        // wasmTable is kind of special.  In the normal configuration we export
-        // it from the wasm module under the name `__indirect_function_table`
-        // but we declare it as an 'undefined' in `libcore.js`.
-        // Since the normal export mechanism will declare this variable we don't
-        // want the JS library version of this symbol be declared (otherwise
-        // it would be a duplicate decl).
-        // TODO(sbc): This is kind of hacky, we should come up with a better solution.
-        var isDirectWasmExport = mangled == 'wasmTable';
-        if (isDirectWasmExport) {
+        // For JS library functions that are simply aliases of native symbols,
+        // we don't need to generate anything here.  Instead these get included
+        // and exported alongside native symbols.
+        // See `create_receiving` in `tools/emscripten.py`.
+        if (isNativeAlias) {
           contentText = '';
         } else {
           contentText = `var ${mangled};`;
@@ -752,7 +762,7 @@ function(${args}) {
         contentText = `var ${mangled} = ${snippet};`;
       }
 
-      if (MODULARIZE == 'instance' && (EXPORT_ALL || EXPORTED_FUNCTIONS.has(mangled)) && !isStub) {
+      if (contentText && MODULARIZE == 'instance' && (EXPORT_ALL || EXPORTED_FUNCTIONS.has(mangled)) && !isStub) {
         // In MODULARIZE=instance mode mark JS library symbols are exported at
         // the point of declaration.
         contentText = 'export ' + contentText;
@@ -775,11 +785,11 @@ function(${args}) {
         }
       }
 
-      let commentText = '';
-      let docs = LibraryManager.library[symbol + '__docs'];
       // Add the docs if they exist and if we are actually emitting a declaration.
       // See the TODO about wasmTable above.
-      if (docs && contentText != '') {
+      let docs = LibraryManager.library[symbol + '__docs'];
+      let commentText = '';
+      if (contentText != '' && docs) {
         commentText += docs + '\n';
       }
 
@@ -886,6 +896,7 @@ var proxiedFunctionTable = [
       '//FORWARDED_DATA:' +
         JSON.stringify({
           librarySymbols,
+          nativeAliases,
           warnings: warningOccured(),
           asyncFuncs,
           libraryDefinitions: LibraryManager.libraryDefinitions,

src/lib/libbootstrap.js
@@ -15,6 +15,8 @@ assert(Object.keys(LibraryManager.library).length === 0);
 addToLibrary({
   $callRuntimeCallbacks: () => {},
 
+  $wasmMemory: 'memory',
+
   $ExitStatus: class {
     name = 'ExitStatus';
     constructor(status) {

src/lib/libcore.js
@@ -1641,7 +1641,7 @@ addToLibrary({
 #endif
       }
     }
-
+    exportAliases(wasmExports);
   },
 #endif
 
@@ -2150,8 +2150,6 @@ addToLibrary({
 #endif // MINIMAL_RUNTIME
 
   $asmjsMangle: (x) => {
-    if (x == 'memory') return 'wasmMemory';
-    if (x == '__indirect_function_table') return 'wasmTable';
     if (x == '__main_argc_argv') {
       x = 'main';
     }
@@ -2287,7 +2285,14 @@ addToLibrary({
 });
 `,
 #else
-  $wasmTable: undefined,
+  $wasmTable: '__indirect_function_table',
+#endif
+
+#if IMPORTED_MEMORY
+  // This gets defined in src/runtime_init_memory.js
+  $wasmMemory: undefined,
+#else
+  $wasmMemory: 'memory',
 #endif
 
   $getUniqueRunDependency: (id) => {

src/lib/liblegacy.js
@@ -114,8 +114,8 @@ legacyFuncs = {
   },
 
   // Legacy names for runtime `out`/`err` symbols.
-  $print: 'out',
-  $printErr: 'err',
+  $print: '=out',
+  $printErr: '=err',
 
   // Converts a JS string to an integer base-10. Despite _s, which
   // suggests signaling error handling, this returns NaN on error.

src/parseTools.mjs
@@ -24,6 +24,8 @@ import {
   srcDir,
 } from './utility.mjs';
 
+import { nativeAliases } from './modules.mjs';
+
 const FOUR_GB = 4 * 1024 * 1024 * 1024;
 const WASM_PAGE_SIZE = 64 * 1024;
 const FLOAT_TYPES = new Set(['float', 'double']);
@@ -1155,6 +1157,17 @@ function nodeWWDetection() {
   }
 }
 
+function makeExportAliases() {
+  var res = ''
+  for (var [alias, ex] of Object.entries(nativeAliases)) {
+    if (ASSERTIONS) {
+      res += `  assert(wasmExports['${ex}'], 'alias target "${ex}" not found in wasmExports');\n`;
+    }
+    res += `  globalThis['${alias}'] = wasmExports['${ex}'];\n`;
+  }
+  return res;
+}
+
 addToCompileTimeContext({
   ATEXITS,
   ATPRERUNS,
@@ -1204,6 +1217,7 @@ addToCompileTimeContext({
   isSymbolNeeded,
   makeDynCall,
   makeEval,
+  makeExportAliases,
   makeGetValue,
   makeHEAPView,
   makeModuleReceive,

commit 6d21d9f40397b46d7cf9ef0faf244e4f682d32c6

Fix exporting of native symbol aliases (e.g. wasmTable)
This was broken by #25376.

src/modules.mjs
@@ -433,6 +433,11 @@ function exportSymbol(name) {
 function exportRuntimeSymbols() {
   // optionally export something.
   function shouldExport(name) {
+    // Native exports are not available to be exported initially.  Instead,
+    // they get exported later in `assignWasmExports`.
+    if (nativeAliases[name]) {
+      return false;
+    }
     // If requested to be exported, export it.
     if (EXPORTED_RUNTIME_METHODS.has(name)) {
       // Unless we are in MODULARIZE=instance mode then HEAP objects are

tools/emscripten.py
@@ -980,6 +980,7 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
     assert alias in exports, f'expected alias target ({alias}) to be exported'
     alias_inverse_map.setdefault(alias, []).append(sym)
 
+  do_module_exports = (settings.MODULARIZE or not settings.MINIMAL_RUNTIME) and settings.MODULARIZE != 'instance'
   receiving.append('\nfunction assignWasmExports(wasmExports) {')
   for sym, sig in exports.items():
     is_function = sig is not None
@@ -988,11 +989,13 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
     if generate_dyncall_assignment and is_function and sym.startswith('dynCall_'):
       sig_str = sym.replace('dynCall_', '')
       assignment += f" = dynCalls['{sig_str}']"
-    if (settings.MODULARIZE or not settings.MINIMAL_RUNTIME) and should_export(mangled) and settings.MODULARIZE != 'instance':
+    if do_module_exports and should_export(mangled):
       assignment += f" = Module['{mangled}']"
     if sym in alias_inverse_map:
       for target in alias_inverse_map[sym]:
         assignment += f" = {target}"
+        if do_module_exports and target in settings.EXPORTED_RUNTIME_METHODS:
+          assignment += f" = Module['{target}']"
     if is_function and install_debug_wrapper(sym):
       nargs = len(sig.params)
       receiving.append(f"  {assignment} = createExportWrapper('{sym}', {nargs});")

test/test_other.py
@@ -4948,6 +4948,35 @@ call_native_alias: 44
 '''
     self.do_runf('main.c', expected, cflags=['--js-library', 'foo.js'] + args)
 
+  @parameterized({
+    '': ([],),
+    'closure': (['--closure=1'],),
+  })
+  def test_jslib_export_alias(self, args):
+    create_file('lib.js', '''
+      addToLibrary({
+        $foo: 'main',
+        $bar: '__indirect_function_table',
+        $baz: 'memory',
+      });
+    ''')
+    create_file('extern_pre.js', r'''
+      Module = {
+        onRuntimeInitialized: () => {
+          const assert = require('assert');
+          console.log("onRuntimeInitialized");
+          console.log('foo:', typeof Module.foo)
+          console.log('bar:', typeof Module.bar)
+          console.log('baz:', typeof Module.baz)
+          assert(Module.foo instanceof Function);
+          assert(Module.bar instanceof WebAssembly.Table);
+          assert(Module.baz instanceof WebAssembly.Memory);
+          console.log('done');
+        }
+      };
+    ''')
+    self.do_runf('hello_world.c', 'done\n', cflags=['--js-library=lib.js', '--extern-pre-js=extern_pre.js', '-sEXPORTED_RUNTIME_METHODS=foo,bar,baz'] + args)
+
   def test_postjs_errors(self):
     create_file('post.js', '#preprocess\n#error This is an error')
     err = self.expect_fail([EMCC, test_file('hello_world.c'), '--post-js', 'post.js'])