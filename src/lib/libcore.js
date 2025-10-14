2c3efc213d396166b7d18b2e8e847d4e5d3810e1
Sam Clegg <sbc@chromium.org>
2025-10-14 12:52:48 -0700

    Export data addresses from the Wasm module just like other symbols.
    This removes the special handling of exported immutable globals which
    simplifies the code in a few different ways.
    
    For programs that export data addresses (this is relatively rare) this
    means codesize reduction for the generated JS (since it no longer
    contains the constant values) and the codesize increase for the Wasm
    binary (since it now contains extra exports).
    
    The main reason for this is consistency with dynamic linking (where data
    exports are always needed) and a reduction in complexity.
    
    The following (14) test expectation files were updated by
    running the tests with `--rebaseline`:
    
    ```
    codesize/test_codesize_minimal_64.json: 2657 => 2671 [+14 bytes / +0.53%]
    test/codesize/test_codesize_minimal_O0.expected.js updated
    codesize/test_codesize_minimal_O0.json: 20398 => 20501 [+103 bytes / +0.50%]
    codesize/test_codesize_minimal_O1.json: 3498 => 3510 [+12 bytes / +0.34%]
    codesize/test_codesize_minimal_O2.json: 2622 => 2632 [+10 bytes / +0.38%]
    codesize/test_codesize_minimal_O3.json: 2354 => 2368 [+14 bytes / +0.59%]
    codesize/test_codesize_minimal_Os.json: 2354 => 2368 [+14 bytes / +0.59%]
    codesize/test_codesize_minimal_Os_mr.json: 559 => 572 [+13 bytes / +2.33%]
    codesize/test_codesize_minimal_Oz-ctors.json: 2322 => 2336 [+14 bytes / +0.60%]
    codesize/test_codesize_minimal_Oz.json: 2354 => 2368 [+14 bytes / +0.59%]
    codesize/test_codesize_minimal_esm.json: 2489 => 2504 [+15 bytes / +0.60%]
    codesize/test_codesize_minimal_pthreads.json: 27208 => 27220 [+12 bytes / +0.04%]
    codesize/test_codesize_minimal_pthreads_memgrowth.json: 27636 => 27648 [+12 bytes / +0.04%]
    codesize/test_codesize_minimal_wasmfs.json: 2354 => 2368 [+14 bytes / +0.59%]
    
    Average change: +0.60% (+0.04% - +2.33%)
    ```

--- src/lib/libcore.js
+++ src/lib/libcore.js
@@ -1630,16 +1630,11 @@ addToLibrary({
         dynCalls[name.substr(8)] = exportedSymbol;
       }
 #endif
-      // Globals are currently statically enumerated into the output JS.
-      // TODO: If the number of Globals grows large, consider giving them a
-      // similar DECLARE_ASM_MODULE_EXPORTS = 0 treatment.
-      if (typeof exportedSymbol.value === 'undefined') {
 #if MINIMAL_RUNTIME
-        globalThis[name] = exportedSymbol;
+      globalThis[name] = exportedSymbol;
 #else
-        globalThis[name] = Module[name] = exportedSymbol;
+      globalThis[name] = Module[name] = exportedSymbol;
 #endif
-      }
     }
     exportAliases(wasmExports);
   },
