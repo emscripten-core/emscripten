45d0e77c9c0c0e7378fd8d40983883cdb2fa247a
Ben Ling <hongyao.ling@autodesk.com>
2025-10-22 22:27:06 +0800

    Repro issue where we are passing in the wrong imports into deferred module upon loading

--- test/other/test_split_module.c
+++ test/other/test_split_module.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <emscripten.h>
+#include <stdlib.h> // For malloc and free
+#include <stdint.h> // For int64_t
 
 int foo() {
   return 42;
@@ -7,4 +9,12 @@ int foo() {
 
 EMSCRIPTEN_KEEPALIVE void say_hello() {
   printf("Hello! answer: %d\n", foo());
+
+  int64_t* ptr = (int64_t*)malloc(sizeof(int64_t) * 10);
+  if (ptr == NULL) {
+    return;
+  }
+  
+  free(ptr);
+  ptr = NULL;
 }
