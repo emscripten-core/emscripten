779c8f22bea370389ff9468f6ff3ada91835f1d6
Sam Clegg <sbc@chromium.org>
2025-10-22 09:50:08 -0700

    Skip jsmathz.test_emscripten_api_export_all (#25622)
    Fixes: #25620

--- tools/link.py
+++ tools/link.py
@@ -1089,9 +1089,10 @@ def phase_linker_setup(options, linker_args):  # noqa: C901, PLR0912, PLR0915
 
   # Use settings
 
-  if settings.JS_MATH:
-    if settings.MAIN_MODULE == 1:
-      exit_with_error('JS_MATH is not compatible with dynamic linking (MAIN_MODULE=1)')
+  if settings.JS_MATH and settings.MAIN_MODULE == 1:
+    # MODULE_MODULE=1 adds`--whole-archive` around all the system libraries which
+    # results in duplicate math symbols when JS_MATH is used.
+    exit_with_error('JS_MATH is not compatible with dynamic linking (MAIN_MODULE=1)')
 
   if settings.WASM == 2 and settings.SINGLE_FILE:
     exit_with_error('cannot have both WASM=2 and SINGLE_FILE enabled at the same time')
