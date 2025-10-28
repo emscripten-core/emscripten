79bf3e0d2497c0a92d15f94bb9e0bd6d4c33e347
Benjamin Ling <hongyao.ling@autodesk.com>
2025-10-28 10:10:20 +0800

    [SPLIT_MODULE] Fix imports into deferred module when loading (#25621)
    Fix an error when attempted to reserve a new memory space in the
    deferred module when building with MEMORY64 + SPLIT_MODULE.
    ```
    TypeError: Cannot convert <func ptr> to a BigInt
    ```
    
    In a mem64 build, we call applySignatureConversions on `malloc()` in the
    wasmExports. `applySignatureConversions()` wraps the exports and
    converts the return value to a javascript number. The wasmExports is
    passed into the deferred module during instantiation so when we call a
    malloc in the deferred module, we'd be calling the wrapped function,
    hitting into this error.
    
    Furthermore, we are making a direct call when calling a malloc, so
    strictly speaking, we should not need to go through the JS trampoline.

--- test/other/test_split_module.c
+++ test/other/test_split_module.c
@@ -7,4 +7,12 @@ int foo() {
 
 EMSCRIPTEN_KEEPALIVE void say_hello() {
   printf("Hello! answer: %d\n", foo());
+
+  void* ptr = malloc(10);
+  if (ptr == NULL) {
+    return;
+  }
+  
+  free(ptr);
+  ptr = NULL;
 }
