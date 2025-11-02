emscripten-core/emscripten

commit 1294dd59e01c01df5123fa083778f5ea73c22019

Remove more dead python code found using `deadcode` module. NFC (#25359)
The remaining dead code being reported is false positives I believe.

emrun.py
@@ -577,8 +577,9 @@ class HTTPWebServer(socketserver.ThreadingMixIn, HTTPServer):
 
 # Processes HTTP request back to the browser.
 class HTTPHandler(SimpleHTTPRequestHandler):
+  protocol_version = 'HTTP/1.1'
+
   def send_head(self):
-    self.protocol_version = 'HTTP/1.1'
     global page_last_served_time
     path = self.translate_path(self.path)
     f = None
@@ -669,7 +670,6 @@ class HTTPHandler(SimpleHTTPRequestHandler):
       sys.stderr.write(msg)
 
   def do_POST(self):
-    self.protocol_version = 'HTTP/1.1'
     global page_exit_code, have_received_messages
 
     (_, _, path, query, _) = urlsplit(self.path)

emcc.py
@@ -165,17 +165,6 @@ def create_reproduce_file(name, args):
       reproduce_file.add(rsp_name, os.path.join(root, 'response.txt'))
 
 
-def get_library_basename(filename):
-  """Similar to get_file_suffix this strips off all numeric suffixes and then
-  then final non-numeric one.  For example for 'libz.so.1.2.8' returns 'libz'"""
-  filename = os.path.basename(filename)
-  while filename:
-    filename, suffix = os.path.splitext(filename)
-    # Keep stipping suffixes until we strip a non-numeric one.
-    if not suffix[1:].isdigit():
-      return filename
-
-
 #
 # Main run() function
 #

pyproject.toml
@@ -110,4 +110,4 @@ module = ["psutil", "win32con", "win32gui", "win32process"]
 ignore_missing_imports = true
 
 [tool.deadcode]
-exclude = ["out", "third_party", "test/third_party"]
+exclude = ["out", "third_party", "test/third_party", "node_modules", "site/source/_themes", "site/source/conf.py"]

test/common.py
@@ -414,17 +414,6 @@ def requires_wasm64(func):
   return decorated
 
 
-def requires_wasm_legacy_eh(func):
-  assert callable(func)
-
-  @wraps(func)
-  def decorated(self, *args, **kwargs):
-    self.require_wasm_legacy_eh()
-    return func(self, *args, **kwargs)
-
-  return decorated
-
-
 def requires_wasm_eh(func):
   assert callable(func)
 
@@ -630,31 +619,6 @@ def also_with_noderawfs(func):
   parameterize(metafunc, {'': (False,),
                           'rawfs': (True,)})
   return metafunc
-
-
-# Decorator version of env_modify
-def also_with_env_modify(name_updates_mapping):
-
-  def decorated(f):
-    @wraps(f)
-    def metafunc(self, updates, *args, **kwargs):
-      if DEBUG:
-        print('parameterize:env_modify=%s' % (updates))
-      if updates:
-        with env_modify(updates):
-          return f(self, *args, **kwargs)
-      else:
-        return f(self, *args, **kwargs)
-
-    params = {'': (None,)}
-    for name, updates in name_updates_mapping.items():
-      params[name] = (updates,)
-
-    parameterize(metafunc, params)
-
-    return metafunc
-
-  return decorated
 
 
 def also_with_minimal_runtime(f):
@@ -1637,32 +1601,6 @@ class RunnerCore(unittest.TestCase, metaclass=RunnerMeta):
           return src[start:t + 1]
       t += 1
       assert t < len(src)
-
-  def count_funcs(self, javascript_file):
-    num_funcs = 0
-    start_tok = "// EMSCRIPTEN_START_FUNCS"
-    end_tok = "// EMSCRIPTEN_END_FUNCS"
-    start_off = 0
-    end_off = 0
-
-    js = read_file(javascript_file)
-    blob = "".join(js.splitlines())
-
-    start_off = blob.find(start_tok) + len(start_tok)
-    end_off = blob.find(end_tok)
-    asm_chunk = blob[start_off:end_off]
-    num_funcs = asm_chunk.count('function ')
-    return num_funcs
-
-  def count_wasm_contents(self, wasm_binary, what):
-    out = self.run_process([os.path.join(building.get_binaryen_bin(), 'wasm-opt'), wasm_binary, '--metrics'], stdout=PIPE).stdout
-    # output is something like
-    # [?]        : 125
-    for line in out.splitlines():
-      if '[' + what + ']' in line:
-        ret = line.split(':')[1].strip()
-        return int(ret)
-    self.fail('Failed to find [%s] in wasm-opt output' % what)
 
   def get_wasm_text(self, wasm_binary):
     return self.run_process([WASM_DIS, wasm_binary], stdout=PIPE).stdout

test/runner.py
@@ -119,8 +119,8 @@ def check_js_engines():
 def get_and_import_modules():
   modules = []
   for filename in glob.glob(os.path.join(common.TEST_ROOT, 'test*.py')):
-    module_dir, module_file = os.path.split(filename)
-    module_name, module_ext = os.path.splitext(module_file)
+    module_file = os.path.basename(filename)
+    module_name = os.path.splitext(module_file)[0]
     __import__(module_name)
     modules.append(sys.modules[module_name])
   return modules

test/test_core.py
@@ -3775,11 +3775,6 @@ ok
   @no_js_math('JS_MATH is not compatible with MAIN_MODULE')
   def test_dlfcn_handle_alloc(self):
     # verify that dlopen does not allocate already used handles
-    dirname = self.get_dir()
-
-    def indir(name):
-      return os.path.join(dirname, name)
-
     create_file('a.cpp', r'''
       #include <stdio.h>
 

test/test_other.py
@@ -51,7 +51,6 @@ from tools import webassembly
 from tools.settings import settings
 from tools.system_libs import DETERMINISTIC_PREFIX
 
-scons_path = shutil.which('scons')
 emmake = shared.bat_suffix(path_from_root('emmake'))
 emconfig = shared.bat_suffix(path_from_root('em-config'))
 emsize = shared.bat_suffix(path_from_root('emsize'))
@@ -59,7 +58,6 @@ empath_split = shared.bat_suffix(path_from_root('empath-split'))
 emprofile = shared.bat_suffix(path_from_root('emprofile'))
 emstrip = shared.bat_suffix(path_from_root('emstrip'))
 emsymbolizer = shared.bat_suffix(path_from_root('emsymbolizer'))
-wasm_opt = Path(building.get_binaryen_bin(), 'wasm-opt')
 
 
 def is_bitcode(filename):
@@ -3055,9 +3053,6 @@ More info: https://emscripten.org
 
   def verify_dwarf_exists(self, wasm_file):
     self.verify_dwarf(wasm_file, self.assertIn)
-
-  def verify_dwarf_does_not_exist(self, wasm_file):
-    self.verify_dwarf(wasm_file, self.assertNotIn)
 
   # Verify if the given file name contains a source map
   def verify_source_map_exists(self, map_file):

tools/cmdline.py
@@ -61,7 +61,6 @@ class EmccOptions:
     self.post_link = False
     self.save_temps = False
     self.executable = False
-    self.compiler_wrapper = None
     self.oformat = None
     self.requested_debug = None
     self.emit_symbol_map = False
@@ -78,7 +77,6 @@ class EmccOptions:
     self.shell_path = None
     self.source_map_base = ''
     self.emit_tsd = ''
-    self.embind_emit_tsd = ''
     self.emrun = False
     self.cpu_profiler = False
     self.memory_profiler = False

commit d113134ef0800c4360251c2055114c4d1d43d77d

Fix for emrun --kill-exit
This was broken by #25359.  I though I was just moving this assignment
to the correct place, but it seems like it broken emrun because the 1.1
protocol was not actually enabled before.  Once enabled it seems like
this was causing `httpd.server_close` to hang forever.

Fixes: #25654

emrun.py
@@ -578,8 +578,6 @@ class HTTPWebServer(socketserver.ThreadingMixIn, HTTPServer):
 
 # Processes HTTP request back to the browser.
 class HTTPHandler(SimpleHTTPRequestHandler):
-  protocol_version = 'HTTP/1.1'  # noqa: DC01
-
   def send_head(self):
     global page_last_served_time
     path = self.translate_path(self.path)