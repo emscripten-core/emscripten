ad2bf6f6e1f7432515fe009f9a9cf4f1bd751b38
juj <jujjyl@gmail.com>
2025-11-10 23:45:05 +0200

    Fix ccall() returning a pointer in CAN_ADDRESS_2GB mode. (#25765)
    `ccall()` returned pointers as negative numbers in CAN_ADDRESS_2GB
    modes, breaking pointer `begin < end` comparisons.

--- test/test_core.py
+++ test/test_core.py
@@ -7173,6 +7173,12 @@ void* operator new(size_t size) {
     self.set_setting('EXPORTED_FUNCTIONS', ['_print_bool'])
     self.do_runf('core/test_ccall.cpp', 'true')
 
+  @no_modularize_instance('ccall is not yet compatible with MODULARIZE=instance')
+  def test_ccall_return_pointer(self):
+    if self.get_setting('MINIMAL_RUNTIME'):
+      self.skipTest('ccall is not available in MINIMAL_RUNTIME')
+    self.do_core_test('test_ccall_return_pointer.c', cflags=['-sDEFAULT_LIBRARY_FUNCS_TO_INCLUDE=$ccall'])
+
   @no_modularize_instance('uses Module object directly')
   def test_EXPORTED_RUNTIME_METHODS(self):
     self.set_setting('DEFAULT_LIBRARY_FUNCS_TO_INCLUDE', ['$dynCall', '$ASSERTIONS'])
