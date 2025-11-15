emscripten-core/emscripten

commit c7c65cc66430cf5be6d29b7f8e7d15ed95cbb5c9

Export data addresses from the Wasm module just like other symbols. (#25568)
This removes the special handling of exported immutable globals which
simplifies the code in a few different ways.

For programs that export data addresses (this is relatively rare) this
means codesize reduction for the generated JS (since it no longer
contains the constant values) and the codesize increase for the Wasm
binary (since it now contains extra exports).

The main reason for this is consistency with dynamic linking (where data
exports are always needed) and a reduction in complexity.

Fixes: #25556

tools/emscripten.py
@@ -112,7 +112,6 @@ def update_settings_glue(wasm_file, metadata, base_metadata):
     settings.WASM_EXPORTS = base_metadata.all_exports
   else:
     settings.WASM_EXPORTS = metadata.all_exports
-  settings.DATA_EXPORTS = list(metadata.data_exports.keys())
   settings.HAVE_EM_ASM = bool(settings.MAIN_MODULE or len(metadata.em_asm_consts) != 0)
 
   # start with the MVP features, and add any detected features.
@@ -270,30 +269,6 @@ def trim_asm_const_body(body):
   return body
 
 
-def create_data_exports(data_exports):
-  lines = []
-  for k, v in data_exports.items():
-    if shared.is_internal_global(k):
-      continue
-
-    v = int(v)
-
-    if not settings.MEMORY64:
-      # We assume that global exports are addresses, which need to be interpreted as unsigned.
-      # This is not necessary (and does not work) under wasm64 when the globals are i64.
-      v = v & 0xFFFFFFFF
-
-    if settings.RELOCATABLE:
-      v += settings.GLOBAL_BASE
-    mangled = asmjs_mangle(k)
-    if should_export(mangled) and not settings.MINIMAL_RUNTIME:
-      lines.append("var %s = Module['%s'] = %s;" % (mangled, mangled, v))
-    else:
-      lines.append("var %s = %s;" % (mangled, v))
-
-  return '\n'.join(lines)
-
-
 def emscript(in_wasm, out_wasm, outfile_js, js_syms, finalize=True, base_metadata=None):
   # Overview:
   #   * Run wasm-emscripten-finalize to extract metadata and modify the binary
@@ -407,13 +382,11 @@ def emscript(in_wasm, out_wasm, outfile_js, js_syms, finalize=True, base_metadat
   if base_metadata:
     function_exports = base_metadata.function_exports
     other_exports = base_metadata.other_exports
-    # We want the real values from the final metadata but we only want to
-    # include names from the base_metadata.  See phase_link() in link.py.
-    data_exports = {k: v for k, v in metadata.data_exports.items() if k in base_metadata.data_exports}
   else:
     function_exports = metadata.function_exports
     other_exports = metadata.other_exports
-    data_exports = metadata.data_exports
+
+  other_exports = [e for e in other_exports if not shared.is_internal_global(e[0].name)]
 
   if settings.ASYNCIFY == 1:
     function_exports['asyncify_start_unwind'] = webassembly.FuncType([webassembly.Type.I32], [])
@@ -422,7 +395,7 @@ def emscript(in_wasm, out_wasm, outfile_js, js_syms, finalize=True, base_metadat
     function_exports['asyncify_stop_rewind'] = webassembly.FuncType([], [])
 
   parts = [pre]
-  parts += create_module(metadata, function_exports, data_exports, other_exports,
+  parts += create_module(metadata, function_exports, other_exports,
                          forwarded_json['librarySymbols'], forwarded_json['nativeAliases'])
   parts.append(post)
   settings.ALIASES = list(forwarded_json['nativeAliases'].keys())
@@ -915,7 +888,7 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
   receiving = ['\n// Imports from the Wasm binary.']
 
   if settings.WASM_ESM_INTEGRATION:
-    exported_symbols = other_exports + list(function_exports.keys())
+    exported_symbols = [e[0].name for e in other_exports] + list(function_exports.keys())
     exports = []
     for sym in exported_symbols:
       mangled = asmjs_mangle(sym)
@@ -952,8 +925,8 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
   # function assignWasmExports(wasmExport) {
   #   _main = wasmExports["_main"];
   exports = {name: sig for name, sig in function_exports.items() if name != building.WASM_CALL_CTORS}
-  for name in other_exports:
-    exports[name] = None
+  for export, info in other_exports:
+    exports[export.name] = (export, info)
 
   mangled = [asmjs_mangle(s) for s in exports] + list(aliases.keys())
   if settings.ASSERTIONS:
@@ -984,8 +957,8 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
 
   do_module_exports = (settings.MODULARIZE or not settings.MINIMAL_RUNTIME) and settings.MODULARIZE != 'instance'
   receiving.append('\nfunction assignWasmExports(wasmExports) {')
-  for sym, sig in exports.items():
-    is_function = sig is not None
+  for sym, info in exports.items():
+    is_function = type(info) == webassembly.FuncType
     mangled = asmjs_mangle(sym)
     assignment = mangled
     if generate_dyncall_assignment and is_function and sym.startswith('dynCall_'):
@@ -1001,8 +974,16 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
         if do_module_exports and target in settings.EXPORTED_RUNTIME_METHODS:
           assignment += f" = Module['{target}']"
     if is_function and install_debug_wrapper(sym):
-      nargs = len(sig.params)
+      nargs = len(info.params)
       receiving.append(f"  {assignment} = createExportWrapper('{sym}', {nargs});")
+    elif not is_function and info[0].kind == webassembly.ExternType.GLOBAL and not info[1].mutable:
+      if settings.LEGACY_VM_SUPPORT:
+        value = f"typeof wasmExports['{sym}'] == 'object' ? wasmExports['{sym}'].value : wasmExports['{sym}']"
+      else:
+        value = f"wasmExports['{sym}'].value"
+      if settings.MEMORY64:
+        value = f'Number({value})'
+      receiving.append(f"  {assignment} = {value};")
     else:
       receiving.append(f"  {assignment} = wasmExports['{sym}'];")
   receiving.append('}')
@@ -1010,10 +991,9 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
   return '\n'.join(receiving)
 
 
-def create_module(metadata, function_exports, data_exports, other_exports, library_symbols, aliases):
+def create_module(metadata, function_exports, other_exports, library_symbols, aliases):
   module = []
   module.append(create_receiving(function_exports, other_exports, library_symbols, aliases))
-  module.append(create_data_exports(data_exports))
 
   sending = create_sending(metadata, library_symbols)
   if settings.WASM_ESM_INTEGRATION:

test/codesize/test_codesize_minimal_O0.expected.js
@@ -1323,6 +1323,7 @@ var __emscripten_stack_restore = makeInvalidEarlyAccess('__emscripten_stack_rest
 var __emscripten_stack_alloc = makeInvalidEarlyAccess('__emscripten_stack_alloc');
 var _emscripten_stack_get_current = makeInvalidEarlyAccess('_emscripten_stack_get_current');
 var memory = makeInvalidEarlyAccess('memory');
+var _global_val = Module['_global_val'] = makeInvalidEarlyAccess('_global_val');
 var __indirect_function_table = makeInvalidEarlyAccess('__indirect_function_table');
 var wasmMemory = makeInvalidEarlyAccess('wasmMemory');
 
@@ -1347,11 +1348,11 @@ function assignWasmExports(wasmExports) {
   _emscripten_stack_get_current = wasmExports['emscripten_stack_get_current'];
   assert(typeof wasmExports['memory'] != 'undefined', 'missing Wasm export: memory');
   memory = wasmMemory = wasmExports['memory'];
+  assert(typeof wasmExports['global_val'] != 'undefined', 'missing Wasm export: global_val');
+  _global_val = Module['_global_val'] = wasmExports['global_val'].value;
   assert(typeof wasmExports['__indirect_function_table'] != 'undefined', 'missing Wasm export: __indirect_function_table');
   __indirect_function_table = wasmExports['__indirect_function_table'];
 }
-
-var _global_val = Module['_global_val'] = 65536;
 
 var wasmImports = {
   

test/test_core.py
@@ -9725,7 +9725,6 @@ NODEFS is no longer included by default; build with -lnodefs.js
     'dylink': [True],
   })
   @no_esm_integration('https://github.com/emscripten-core/emscripten/issues/25543')
-  @no_omit_asm_module_exports('https://github.com/emscripten-core/emscripten/issues/25556')
   def test_wasm_global(self, dynlink):
     if '-flto' in self.cflags or '-flto=thin' in self.cflags:
       self.skipTest('https://github.com/emscripten-core/emscripten/issues/25555')

src/lib/libcore.js
@@ -1632,16 +1632,20 @@ addToLibrary({
         dynCalls[name.substr(8)] = exportedSymbol;
       }
 #endif
-      // Globals are currently statically enumerated into the output JS.
-      // TODO: If the number of Globals grows large, consider giving them a
-      // similar DECLARE_ASM_MODULE_EXPORTS = 0 treatment.
-      if (typeof exportedSymbol.value === 'undefined') {
-#if MINIMAL_RUNTIME
-        globalThis[name] = exportedSymbol;
+      // Special handling for Wasm globals.  See `create_receiving` for the
+      // static version of this code.
+      if (typeof exportedSymbol.value != 'undefined') {
+#if MEMORY64
+        exportedSymbol = Number(exportedSymbol.value);
 #else
-        globalThis[name] = Module[name] = exportedSymbol;
+        exportedSymbol = exportedSymbol.value
 #endif
       }
+#if MINIMAL_RUNTIME
+      globalThis[name] = exportedSymbol;
+#else
+      globalThis[name] = Module[name] = exportedSymbol;
+#endif
     }
     exportAliases(wasmExports);
   },

src/lib/libdylink.js
@@ -249,7 +249,7 @@ var LibraryDylink = {
         var newValue;
         if (typeof value == 'function') {
           newValue = {{{ to64('addFunction(value)') }}};
-        } else if (typeof value == {{{ POINTER_JS_TYPE }}}) {
+        } else if (typeof value.value == {{{ POINTER_JS_TYPE }}}) {
           newValue = value;
         } else {
           // The GOT can only contain addresses (i.e data addresses or function
@@ -306,7 +306,7 @@ var LibraryDylink = {
       // Detect immuable wasm global exports. These represent data addresses
       // which are relative to `memoryBase`
       if (isImmutableGlobal(value)) {
-        return value.value + {{{ to64('memoryBase') }}};
+        return new WebAssembly.Global({'value': '{{{ POINTER_WASM_TYPE }}}'}, value.value + {{{ to64('memoryBase') }}});
       }
 
       // Return unmodified value (no relocation required).
@@ -354,10 +354,8 @@ var LibraryDylink = {
 #endif
         } else if (typeof value == 'number') {
           entry.value = {{{ to64('value') }}};
-#if MEMORY64
-        } else if (typeof value == 'bigint') {
+        } else if (typeof value.value == {{{ POINTER_JS_TYPE }}}) {
           entry.value = value;
-#endif
         } else {
           throw new Error(`bad export type for '${symName}': ${typeof value} (${value})`);
         }
@@ -422,6 +420,8 @@ var LibraryDylink = {
     // Keep __heap_base stack aligned.
     var end = ret + alignMemory(size, {{{ STACK_ALIGN }}});
 #if ASSERTIONS
+    //dbg(ret);
+    //dbg(HEAP8.length);
     assert(end <= HEAP8.length, 'failure to getMemory - memory growth etc. is not supported there, call malloc/sbrk directly or increase INITIAL_MEMORY');
 #endif
     ___heap_base = end;
@@ -859,10 +859,15 @@ var LibraryDylink = {
 
         // Add any EM_ASM function that exist in the side module
         if ('__start_em_asm' in moduleExports) {
-          var start = moduleExports['__start_em_asm'];
-          var stop = moduleExports['__stop_em_asm'];
+          var start = moduleExports['__start_em_asm'].value;
+          var stop = moduleExports['__stop_em_asm'].value;
+#if CAN_ADDRESS_2GB
+          start >>>= 0;
+          stop >>>= 0;
+#else
           {{{ from64('start') }}}
           {{{ from64('stop') }}}
+#endif
           while (start < stop) {
             var jsString = UTF8ToString(start);
             addEmAsm(start, jsString);
@@ -892,7 +897,7 @@ var LibraryDylink = {
 
         for (var name in moduleExports) {
           if (name.startsWith('__em_js__')) {
-            var start = moduleExports[name]
+            var start = moduleExports[name].value
             var jsString = UTF8ToString({{{ from64Expr('start') }}});
             // EM_JS strings are stored in the data section in the form
             // SIG<::>BODY.

src/preamble.js
@@ -719,13 +719,10 @@ function getWasmImports() {
     wasmExports = instance.exports;
 
 #if MAIN_MODULE
-    // No relocation needed here.. but calling this just so that updateGOT is
-    // called.
 #if RELOCATABLE
-    var origExports = wasmExports = relocateExports(wasmExports, {{{ GLOBAL_BASE }}});
-#else
-    var origExports = wasmExports = relocateExports(wasmExports);
+    wasmExports = relocateExports(wasmExports, {{{ GLOBAL_BASE }}});
 #endif
+    var origExports = wasmExports;
 #endif
 
 #if ASYNCIFY

src/settings_internal.js
@@ -16,9 +16,6 @@
 // underscore.
 var WASM_EXPORTS = [];
 
-// Similar to above but only includes the data symbols (address exports).
-var DATA_EXPORTS = [];
-
 // An array of all symbols exported from all the side modules specified on the
 // command line.
 // These are raw symbol names and are not mangled to include the leading

tools/building.py
@@ -821,21 +821,7 @@ def get_last_binaryen_opts():
 def metadce(js_file, wasm_file, debug_info, last):
   logger.debug('running meta-DCE')
   temp_files = shared.get_temp_files()
-  # first, get the JS part of the graph
-  if settings.MAIN_MODULE:
-    # For the main module we include all exports as possible roots, not just function exports.
-    # This means that any usages of data symbols within the JS or in the side modules can/will keep
-    # these exports alive on the wasm module.
-    # This is important today for weak data symbols that are defined by the main and the side module
-    # (i.e.  RTTI info).  We want to make sure the main module's symbols get added to wasmImports
-    # when the main module is loaded.  If this doesn't happen then the symbols in the side module
-    # will take precedence.
-    exports = settings.WASM_EXPORTS
-  else:
-    # Ignore exported wasm globals.  Those get inlined directly into the JS code.
-    exports = sorted(set(settings.WASM_EXPORTS) - set(settings.DATA_EXPORTS))
-
-  extra_info = {"exports": [[asmjs_mangle(x), x] for x in exports]}
+  extra_info = {"exports": [[asmjs_mangle(x), x] for x in settings.WASM_EXPORTS]}
 
   txt = acorn_optimizer(js_file, ['emitDCEGraph', '--no-print'], return_output=True, extra_info=extra_info)
   if shared.SKIP_SUBPROCS:

commit 228a62619c60f5ad7d176ddd872cd10c8d1cd6c8

Fix core_2gb.test_memprof_requirements
This was broken by #25568. This fixes the regression and updates an
existing test to catch it.

tools/emscripten.py
@@ -957,6 +957,9 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
 
   do_module_exports = (settings.MODULARIZE or not settings.MINIMAL_RUNTIME) and settings.MODULARIZE != 'instance'
   receiving.append('\nfunction assignWasmExports(wasmExports) {')
+  if settings.ASSERTIONS:
+    for sym in exports:
+      receiving.append(f"  assert(typeof wasmExports['{sym}'] != 'undefined', 'missing Wasm export: {sym}');")
   for sym, info in exports.items():
     is_function = type(info) == webassembly.FuncType
     mangled = asmjs_mangle(sym)
@@ -966,8 +969,6 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
       assignment += f" = dynCalls['{sig_str}']"
     if do_module_exports and should_export(mangled):
       assignment += f" = Module['{mangled}']"
-    if settings.ASSERTIONS:
-      receiving.append(f"  assert(typeof wasmExports['{sym}'] != 'undefined', 'missing Wasm export: {sym}');")
     if sym in alias_inverse_map:
       for target in alias_inverse_map[sym]:
         assignment += f" = {target}"
@@ -983,6 +984,8 @@ def create_receiving(function_exports, other_exports, library_symbols, aliases):
         value = f"wasmExports['{sym}'].value"
       if settings.MEMORY64:
         value = f'Number({value})'
+      elif settings.CAN_ADDRESS_2GB:
+        value = f'({value}) >>> 0'
       receiving.append(f"  {assignment} = {value};")
     else:
       receiving.append(f"  {assignment} = wasmExports['{sym}'];")

test/codesize/test_codesize_minimal_O0.expected.js
@@ -1329,28 +1329,28 @@ var wasmMemory = makeInvalidEarlyAccess('wasmMemory');
 
 function assignWasmExports(wasmExports) {
   assert(typeof wasmExports['add'] != 'undefined', 'missing Wasm export: add');
-  _add = Module['_add'] = createExportWrapper('add', 2);
   assert(typeof wasmExports['fflush'] != 'undefined', 'missing Wasm export: fflush');
-  _fflush = createExportWrapper('fflush', 1);
   assert(typeof wasmExports['emscripten_stack_init'] != 'undefined', 'missing Wasm export: emscripten_stack_init');
-  _emscripten_stack_init = wasmExports['emscripten_stack_init'];
   assert(typeof wasmExports['emscripten_stack_get_free'] != 'undefined', 'missing Wasm export: emscripten_stack_get_free');
-  _emscripten_stack_get_free = wasmExports['emscripten_stack_get_free'];
   assert(typeof wasmExports['emscripten_stack_get_base'] != 'undefined', 'missing Wasm export: emscripten_stack_get_base');
-  _emscripten_stack_get_base = wasmExports['emscripten_stack_get_base'];
   assert(typeof wasmExports['emscripten_stack_get_end'] != 'undefined', 'missing Wasm export: emscripten_stack_get_end');
-  _emscripten_stack_get_end = wasmExports['emscripten_stack_get_end'];
   assert(typeof wasmExports['_emscripten_stack_restore'] != 'undefined', 'missing Wasm export: _emscripten_stack_restore');
-  __emscripten_stack_restore = wasmExports['_emscripten_stack_restore'];
   assert(typeof wasmExports['_emscripten_stack_alloc'] != 'undefined', 'missing Wasm export: _emscripten_stack_alloc');
-  __emscripten_stack_alloc = wasmExports['_emscripten_stack_alloc'];
   assert(typeof wasmExports['emscripten_stack_get_current'] != 'undefined', 'missing Wasm export: emscripten_stack_get_current');
-  _emscripten_stack_get_current = wasmExports['emscripten_stack_get_current'];
   assert(typeof wasmExports['memory'] != 'undefined', 'missing Wasm export: memory');
-  memory = wasmMemory = wasmExports['memory'];
   assert(typeof wasmExports['global_val'] != 'undefined', 'missing Wasm export: global_val');
-  _global_val = Module['_global_val'] = wasmExports['global_val'].value;
   assert(typeof wasmExports['__indirect_function_table'] != 'undefined', 'missing Wasm export: __indirect_function_table');
+  _add = Module['_add'] = createExportWrapper('add', 2);
+  _fflush = createExportWrapper('fflush', 1);
+  _emscripten_stack_init = wasmExports['emscripten_stack_init'];
+  _emscripten_stack_get_free = wasmExports['emscripten_stack_get_free'];
+  _emscripten_stack_get_base = wasmExports['emscripten_stack_get_base'];
+  _emscripten_stack_get_end = wasmExports['emscripten_stack_get_end'];
+  __emscripten_stack_restore = wasmExports['_emscripten_stack_restore'];
+  __emscripten_stack_alloc = wasmExports['_emscripten_stack_alloc'];
+  _emscripten_stack_get_current = wasmExports['emscripten_stack_get_current'];
+  memory = wasmMemory = wasmExports['memory'];
+  _global_val = Module['_global_val'] = wasmExports['global_val'].value;
   __indirect_function_table = wasmExports['__indirect_function_table'];
 }
 

test/test_core.py
@@ -4626,10 +4626,12 @@ res64 - external 64\n''', header='''\
 
       int main(void) {
         void* js_address = EM_ASM_PTR({
-          console.log("JS:_my_number:", _my_number, HEAP32[_my_number/4]);
+          var value = HEAP32[_my_number/4];
+          console.log("JS:_my_number:", _my_number, value);
+          assert(value == 123456, value);
           return _my_number;
         });
-        printf("C: my_number: %ld %d\n", (long)&my_number, my_number);
+        printf("C: my_number: %lu %d\n", (uintptr_t)&my_number, my_number);
         assert(js_address == &my_number);
         return 0;
       }