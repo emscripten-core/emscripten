b0503f417445083e49e2b6464d575fca84ef42f3
Brendan Dahl <brendan.dahl@gmail.com>
2025-12-09 17:02:29 -0800

    Revert "Support names field in source maps (#25870)"
    This reverts commit 4a330e54ce8a3622d00f947d707ad98ac4925a84.

--- tools/emsymbolizer.py
+++ tools/emsymbolizer.py
@@ -117,7 +117,6 @@ class WasmSourceMap:
   def __init__(self):
     self.version = None
     self.sources = []
-    self.funcs = []
     self.mappings = {}
     self.offsets = []
 
@@ -129,7 +128,6 @@ class WasmSourceMap:
 
     self.version = source_map_json['version']
     self.sources = source_map_json['sources']
-    self.funcs = source_map_json['names']
 
     chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
     vlq_map = {c: i for i, c in enumerate(chars)}
@@ -157,7 +155,6 @@ class WasmSourceMap:
     src = 0
     line = 1
     col = 1
-    func = 0
     for segment in source_map_json['mappings'].split(','):
       data = decodeVLQ(segment)
       info = []
@@ -172,9 +169,7 @@ class WasmSourceMap:
       if len(data) >= 4:
         col += data[3]
         info.append(col)
-      if len(data) == 5:
-        func += data[4]
-        info.append(func)
+      # TODO: see if we need the name, which is the next field (data[4])
 
       self.mappings[offset] = WasmSourceMap.Location(*info)
       self.offsets.append(offset)
@@ -212,7 +207,6 @@ class WasmSourceMap:
         self.sources[info.source] if info.source is not None else None,
         info.line,
         info.column,
-        self.funcs[info.func] if info.func is not None else None,
       )
 
 
