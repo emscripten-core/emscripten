20da9c678252dc8da3d1918000699c73516c0909
Sam Clegg <sbc@chromium.org>
2025-12-15 13:51:45 -0800

    Use subprocess.call in emrun. NFC (#25956)
    This function is already used on the lines below but for some reason
    this usage was missed.
    
    Also, fix a broken comment and add use f-strings in couple more places.

--- emrun.py
+++ emrun.py
@@ -389,14 +389,15 @@ def kill_browser_process():
 
   if processname_killed_atexit:
     if emrun_options.android:
-      logv("Terminating Android app '" + processname_killed_atexit + "'.")
+      logv(f"Terminating Android app '{processname_killed_atexit}'.")
       subprocess.call([ADB, 'shell', 'am', 'force-stop', processname_killed_atexit])
     else:
-      logv("Terminating all processes that have string '" + processname_killed_atexit + "' in their name.")
+      logv(f"Terminating all processes that have string '{processname_killed_atexit}' in their name.")
       if WINDOWS:
-        process_image = processname_killed_atexit if '.exe' in processname_killed_atexit else (processname_killed_atexit + '.exe')
-        process = subprocess.Popen(['taskkill', '/F', '/IM', process_image, '/T'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        process.communicate()
+        process_image = processname_killed_atexit
+        if not process_image.endswith('.exe'):
+          process_image += '.exe'
+        subprocess.call(['taskkill', '/F', '/IM', process_image, '/T'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       else:
         try:
           subprocess.call(['pkill', processname_killed_atexit])
@@ -749,7 +750,7 @@ class HTTPHandler(SimpleHTTPRequestHandler):
     self.wfile.write(b'OK')
 
 
-# Returns stdout by running command with universal_newlines=True
+# Returns stdout by running command with text=True
 def check_output(cmd, *args, **kwargs):
   return subprocess.run(cmd, text=True, stdout=subprocess.PIPE, check=True, *args, **kwargs).stdout
 
