f66394b4dd5a5586e5ebf914ff8b9096e4591a36
Jaap Aarts <jaap.aarts1@gmail.com>
2025-12-16 02:08:31 +0100

    Fix python issues

--- tools/cmdline.py
+++ tools/cmdline.py
@@ -563,9 +563,9 @@ def parse_args(newargs):  # noqa: C901, PLR0912, PLR0915
       else:
         exit_with_error(f'invalid value for --output-eol: `{style}`')
     # Record PTHREADS setting because it controls whether --shared-memory is passed to lld
-    elif arg == '-fopenmp' or arg == '-fopenmp=libomp':
+    elif arg in ('-fopenmp', '-fopenmp=libomp'):
       settings.OPENMP = 1
-      # Openmp needs pthreads, they are implied by -ofopenmp
+      # Openmp needs pthreads, they are implied by -fopenmp
       settings.PTHREADS = 1
       # Also set the legacy setting name, in case use JS code depends on it.
       settings.USE_PTHREADS = 1
