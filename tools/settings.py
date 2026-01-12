0467c1bb2397870c771d3929994f2d4b451fda77
Sam Clegg <sbc@chromium.org>
2026-01-12 11:12:24 -0800

    Simplify settings type checking. NFC
    All settings should have known types.

--- tools/settings.py
+++ tools/settings.py
@@ -403,12 +403,11 @@ class SettingsManager:
     self.attrs[name] = value
 
   def check_type(self, name, value):
-    # These settings have a variable type so cannot be easily type checked.
-    if name in ('EXECUTABLE', 'SUPPORT_LONGJMP', 'PTHREAD_POOL_SIZE', 'SEPARATE_DWARF', 'LTO', 'MODULARIZE'):
+    # These settings have a variable type so cannot be easily type checked, or they
+    # are legacy settings not declared in settings.js (and therefore not having any type).
+    if name in ('USE_PTHREADS', 'EXECUTABLE', 'SUPPORT_LONGJMP', 'PTHREAD_POOL_SIZE', 'SEPARATE_DWARF', 'LTO', 'MODULARIZE'):
       return
-    expected_type = self.types.get(name)
-    if not expected_type:
-      return
+    expected_type = self.types[name]
     # Allow integers 1 and 0 for type `bool`
     if expected_type == bool:
       if value in (1, 0):
