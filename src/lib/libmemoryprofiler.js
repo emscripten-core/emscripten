1067759ef1445354356f712bc4109f036af31d31
Matthias Frei <frei@ubique.ch>
2026-02-27 21:58:53 +0100

    memoryprofiler: fixup syntax

--- src/lib/libmemoryprofiler.js
+++ src/lib/libmemoryprofiler.js
@@ -1,7 +1,7 @@
 var memoryProfiler = {
   emscripten_memprof_sbrk_grow: (old_brk, new_brk) => {
 #if MEMORYPROFILER
-    Module['onSbrkGrow'] && Module['onSbrkGrow'](old_brk, new_brk, new Error().stack.toString());
+    Module['onSbrkGrow']?.(old_brk, new_brk, new Error().stack.toString());
 #endif
   },
 };
