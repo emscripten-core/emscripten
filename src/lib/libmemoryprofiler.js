2f0e331b37fdd65942d5c7466181b2552db9ade1
Sam Clegg <sbc@chromium.org>
2026-02-27 16:34:55 -0800

    Move `emscripten_memprof_sbrk_grow` into libtrace.js (#26364)
    This allows for the complete removal of `libmemoryprofiler.js` since the
    memory profiler can now just use the same libtrace-based hooks for all
    events.
    
    I noticed this while reviewing #26175

--- src/lib/libmemoryprofiler.js
+++
@@ -1,9 +0,0 @@
-var memoryProfiler = {
-  emscripten_memprof_sbrk_grow: (old_brk, new_brk) => {
-#if MEMORYPROFILER
-    Module['onSbrkGrow']?.(old_brk, new_brk, new Error().stack.toString());
-#endif
-  },
-};
-
-addToLibrary(memoryProfiler);
