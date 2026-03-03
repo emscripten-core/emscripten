f821b062da44994dac517419448617b583747867
Ingvar Stepanyan <me@rreverser.com>
2026-03-03 18:09:48 +0000

    Produce reproducible pylauncher.exe

--- tools/pylauncher/build.bat
+++ tools/pylauncher/build.bat
@@ -1,4 +1,5 @@
 :: /MT :  Statically link to the C runtime library for max portability
 :: /O1 : Favor small code (optimization for size)
+:: /Brepro : Use deterministic (reproducible) output without timestamps
 
-cl pylauncher.c /Fe:pylauncher.exe /MT /O1
+cl pylauncher.c /Fe:pylauncher.exe /MT /O1 /link /Brepro
