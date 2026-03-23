1d715f04c1b5ed3e7efb0ec3662807f0dc4c125d
walshal <alexwalsh6x7@gmail.com>
2026-03-23 10:31:41 -0700

    Upload renamed test
    Forgot to include test_collision_icase.js, fixed
    
    Signed-off-by: walshal <alexwalsh6x7@gmail.com>

---
+++ test/other/test_collision_icase.js
@@ -0,0 +1,14 @@
+FS.createDataFile('/', "file.txt", "foo");
+var errCode = 0;
+try {
+  FS.createDataFile('/', "fILe.txt", "foo2");
+}
+catch (e)
+{
+  errCode = e.errno;
+}
+var fileContents = FS.readFile("/file.txt");
+out('file.txt: ' + fileContents);
+var ret = FS.analyzePath('/file.txt');
+out('file.txt collison: ' + ret.object.name_next);
+out('errorCode: ' + errCode);
