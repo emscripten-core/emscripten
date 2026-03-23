10ae3403b915bcc63d89beaeb9dfdab9f6cc4b1c
Sam Clegg <sbc@chromium.org>
2026-03-23 11:14:11 -0700

    [EmscriptenEH] Always use custom JS class for C/C++ exceptions
    This has a slight codesize cost but it means we no longer have to worry
    about the ambiguity in when we catch a Number.

--- src/runtime_exceptions.js
+++ src/runtime_exceptions.js
@@ -4,23 +4,35 @@
  * SPDX-License-Identifier: MIT
  */
 
-#if EXCEPTION_STACK_TRACES && !WASM_EXCEPTIONS
+#if !WASM_EXCEPTIONS
+
 // Base Emscripten EH error class
+#if EXCEPTION_STACK_TRACES
 class EmscriptenEH extends Error {}
+#else
+class EmscriptenEH {}
+#endif
 
 #if SUPPORT_LONGJMP == 'emscripten'
 class EmscriptenSjLj extends EmscriptenEH {}
 #endif
 
+#if !DISABLE_EXCEPTION_CATCHING
 class CppException extends EmscriptenEH {
   constructor(excPtr) {
+#if EXCEPTION_STACK_TRACES
     super(excPtr);
+#else
+    super();
+#endif
     this.excPtr = excPtr;
-#if !DISABLE_EXCEPTION_CATCHING
+#if !DISABLE_EXCEPTION_CATCHING && EXCEPTION_STACK_TRACES
     const excInfo = getExceptionMessage(excPtr);
     this.name = excInfo[0];
     this.message = excInfo[1];
 #endif
   }
 }
-#endif // EXCEPTION_STACK_TRACES && !WASM_EXCEPTIONS
+#endif
+
+#endif // !WASM_EXCEPTIONS
