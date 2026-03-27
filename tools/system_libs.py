fea123f508f8a79248565d2b5e12f8aaa4e58f53
stephenduong1004 <stephenduong@google.com>
2026-03-27 15:24:41 -0400

    Revert "Simplify libprintf_long_double. NFC (#26428)"
    This reverts commit 5eeef58f20903896e4c3cac1a93e94f7806d9880.

--- tools/system_libs.py
+++ tools/system_libs.py
@@ -1506,8 +1506,11 @@ class libc_optz(libc):
 class libprintf_long_double(libc):
   name = 'libprintf_long_double'
   cflags = ['-DEMSCRIPTEN_PRINTF_LONG_DOUBLE']
-  src_dir = 'system/lib/libc/musl/src/stdio'
-  src_files = ['vfprintf.c']
+
+  def get_files(self):
+    return files_in_path(
+        path='system/lib/libc/musl/src/stdio',
+        filenames=['vfprintf.c'])
 
   def can_use(self):
     return super().can_use() and settings.PRINTF_LONG_DOUBLE
