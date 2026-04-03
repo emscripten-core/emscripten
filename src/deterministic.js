33f0af4382c8dc16a9a1fe962bcee3a8ca09101e
Sam Clegg <sbc@chromium.org>
2026-04-03 08:40:05 -0700

    Remove dead code from src/deterministic.js. NFC
    The `hashMemory` function was added back 2013 (156fe33bf) but has never
    had any testing of documenation.
    
    The string hash function was added in 2015 (e1344f0df) also without
    any testing of example uses.
    
    I'm hoping to simply remove the `-sDETERMINISTIC` setting, but as part
    of that I'm first trying to strip it down to whats actually used/tested.

--- src/deterministic.js
+++ src/deterministic.js
@@ -23,22 +23,5 @@ Date.now = deterministicNow;
 // See getPerformanceNow in parseTools.mjs for how we deal with this.
 if (globalThis.performance) performance.now = deterministicNow;
 
-Module['thisProgram'] = 'thisProgram'; // for consistency between different builds than between runs of the same build
-
-function hashMemory(id) {
-  var ret = 0;
-  var len = _sbrk(0);
-  for (var i = 0; i < len; i++) {
-    ret = (ret*17 + HEAPU8[i])|0;
-  }
-  return id + ':' + ret;
-}
-
-function hashString(s) {
-  var ret = 0;
-  for (var i = 0; i < s.length; i++) {
-    ret = (ret*17 + s.charCodeAt(i))|0;
-  }
-  return ret;
-}
-
+// for consistency between different builds than between runs of the same build
+Module['thisProgram'] = 'thisProgram';
