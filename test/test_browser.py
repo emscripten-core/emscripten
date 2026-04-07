a77220ec545a9d0d03f97311ef084749bc498ad0
SlowRiot <slowriot@voxelstorm.com>
2026-04-07 03:17:00 +0100

    remove self.set_setting('ASSERTIONS')

--- test/test_browser.py
+++ test/test_browser.py
@@ -5244,7 +5244,6 @@ Module["preRun"] = () => {
     self.btest('wasm_worker/proxied_function.c', expected='0', cflags=['--js-library', test_file('wasm_worker/proxied_function.js'), '-sWASM_WORKERS', '-sASSERTIONS=0'])
 
   def test_wasm_worker_pthread_mutex_debug_allocator_regression(self):
-    self.set_setting('ASSERTIONS')
     self.btest_exit('wasm_worker/pthread_mutex_debug_allocator_regression.cpp',
                     cflags=['-pthread', '-sWASM_WORKERS'])
 
