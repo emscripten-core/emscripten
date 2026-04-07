af43dd135c8e0f53797f9c39f054935664a235c8
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-04-07 20:05:50 +0200

    Revert "Ensure Closure doesn't minify `require()`"
    This reverts commit ef36474b9347f074f01dbdbe6be96827413be2ef.

--- src/shell.js
+++ src/shell.js
@@ -112,7 +112,8 @@ if (ENVIRONMENT_IS_NODE) {
   // When building an ES module `require` is not normally available.
   // We need to use `createRequire()` to construct the require()` function.
   const { createRequire } = await import('node:module');
-  global.require = createRequire(import.meta.url);
+  /** @suppress{duplicate} */
+  var require = createRequire(import.meta.url);
 #endif
 
 #if PTHREADS || WASM_WORKERS
