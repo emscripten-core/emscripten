4780f216e604218eda1d119ac9fd5e34b47c076a
Sam Clegg <sbc@chromium.org>
2026-04-07 15:55:49 -0700

    Fix/remove TODO in is_lock_free tests. NFC
    Returning true here is apparently the correct behavior.
    
    See https://issues.chromium.org/issues/40742723

--- test/wasm_worker/hardware_concurrency_is_lock_free.c
+++ test/wasm_worker/hardware_concurrency_is_lock_free.c
@@ -11,9 +11,7 @@ void test() {
   assert(emscripten_atomics_is_lock_free(1));
   assert(emscripten_atomics_is_lock_free(2));
   assert(emscripten_atomics_is_lock_free(4));
-  // Chrome is buggy, see
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=1167449
-  //assert(emscripten_atomics_is_lock_free(8));
+  assert(emscripten_atomics_is_lock_free(8));
   assert(!emscripten_atomics_is_lock_free(16));
   assert(!emscripten_atomics_is_lock_free(31));
 
