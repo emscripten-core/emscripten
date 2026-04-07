a8e48ee0c4d408a03a15fe829f46a963bba8e613
stephenduong1004 <stephenduong@google.com>
2026-04-07 15:48:01 -0400

    revert changes to preamble.js
    Will be fixed in https://github.com/emscripten-core/emscripten/pull/26641

--- src/preamble.js
+++ src/preamble.js
@@ -301,18 +301,17 @@ function abort(what) {
 #if ASSERTIONS && !('$FS' in addedLibraryItems)
 // show errors on likely calls to FS when it was not included
 var FS = {
-  /** @return {*} */
   error() {
     abort('Filesystem support (FS) was not included. The problem is that you are using files from JS, but files were not used from C/C++, so filesystem support was not auto-included. You can force-include filesystem support with -sFORCE_FILESYSTEM');
   },
   init() { FS.error() },
   createDataFile() { FS.error() },
   createPreloadedFile() { FS.error() },
-  createLazyFile() { return (/** @type {?} */ (FS.error())); },
-  open() { return (/** @type {?} */ (FS.error())); },
-  mkdev() { return (/** @type {?} */ (FS.error())); },
+  createLazyFile() { FS.error() },
+  open() { FS.error() },
+  mkdev() { FS.error() },
   registerDevice() { FS.error() },
-  analyzePath() { return (/** @type {?} */ (FS.error())); },
+  analyzePath() { FS.error() },
 
   ErrnoError() { FS.error() },
 };
