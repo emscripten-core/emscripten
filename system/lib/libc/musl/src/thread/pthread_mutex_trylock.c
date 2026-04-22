e6e83bd5fd0fefbd2015e32e2bddff0e5d095ab0
Sam Clegg <sbc@chromium.org>
2026-04-22 13:26:16 -0700

    Fix preprocessor logic error in pthread_mutex_trylock.c. NFC
    This was a mistake from #24607.  The only effect was that this extra
    (redundant) check was mistakenly included in release builds where its
    technically not needed.

--- system/lib/libc/musl/src/thread/pthread_mutex_trylock.c
+++ system/lib/libc/musl/src/thread/pthread_mutex_trylock.c
@@ -53,10 +53,10 @@ success:
 	}
 #endif
 
-#if defined(__EMSCRIPTEN__) || !defined(NDEBUG)
-	// We can get here for normal mutexes too, but only in debug builds
-	// (where we track ownership purely for debug purposes).
-	if ((type & 15) == PTHREAD_MUTEX_NORMAL) return 0;
+#if defined(__EMSCRIPTEN__) && !defined(NDEBUG)
+	// Under emscripten we can get here for normal mutexes too, but only in debug
+	// Jbuilds (where we track ownership purely for debug purposes).
+	if ((type&15) == PTHREAD_MUTEX_NORMAL) return 0;
 #endif
 
 	next = self->robust_list.head;
