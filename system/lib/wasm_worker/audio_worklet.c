6bd2b027e7927e0dc83803e40abb523254ab687c
Sam Clegg <sbc@chromium.org>
2026-04-24 17:45:34 -0700

    Extend pthread-hybrid support to audio worklets. NFC
    Followup to #26777.
    
    Without this change calling pthread functions from an audio worklet
    lets to undefined behaviour.

--- system/lib/wasm_worker/audio_worklet.c
+++ system/lib/wasm_worker/audio_worklet.c
@@ -10,8 +10,19 @@
 #include "emscripten_internal.h"
 #include "threading_internal.h"
 
+#ifdef __EMSCRIPTEN_PTHREADS__
+void* init_pthread_struct(void *stackPlusTLSAddress, pid_t tid, size_t* stackPlusTLSSize);
+#endif
+
 // Simple wrapper function around the JS _emscripten_create_audio_worklet
 // function that adds the _emscripten_get_next_tid() as arg0
 void emscripten_start_wasm_audio_worklet_thread_async(EMSCRIPTEN_WEBAUDIO_T audioContext, void *stackLowestAddress, uint32_t stackSize, EmscriptenStartWebAudioWorkletCallback callback, void *userData2) {
-  _emscripten_create_audio_worklet(_emscripten_get_next_tid(), audioContext, stackLowestAddress, stackSize, callback, userData2);
+  emscripten_wasm_worker_t wwID = _emscripten_get_next_tid();
+  void* pthreadPtr = stackLowestAddress;
+#ifdef __EMSCRIPTEN_PTHREADS__
+  size_t stackSize_ = stackSize;
+  stackLowestAddress = init_pthread_struct(stackLowestAddress, wwID, &stackSize_);
+  stackSize = stackSize_;
+#endif
+  _emscripten_create_audio_worklet(wwID, audioContext, stackLowestAddress, stackSize, pthreadPtr, callback, userData2);
 }
