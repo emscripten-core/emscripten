3f4b2ae18b9e1ee4da49a4c3cbe467ba1d38ca19
Jukka Jylänki <jujjyl@gmail.com>
2026-05-14 11:41:12 +0300

    f

--- emrun.py
+++ emrun.py
@@ -1610,7 +1610,7 @@ def run(args):  # noqa: C901, PLR0912, PLR0915
   options = emrun_options = parse_args(args)
 
   if MACOS and options.browser.endswith('.app') and not options.browser.startswith('open'):
-    options.browser_args = '--new --fresh --background -a ' + options.browser + ' ' + str(options.browser_args)
+    options.browser_args = f'--new --fresh --background -a {options.browser} {options.browser_args}'
     options.browser = 'open'
 
   if options.android_tunnel:
