f6845b492c0d9e5465e709bf8c0d285ed92615b5
Derek Schuff <dschuff@chromium.org>
2026-06-01 23:39:03 +0000

    rename main

--- tools/pylauncher/build.bat
+++ tools/pylauncher/build.bat
@@ -6,8 +6,8 @@
 :: /O1 : Favor small code (optimization for size)
 :: /GS- : Disable buffer security checks (requires vc runtime and not necessary for our tiny command line wrapper)
 :: /NODEFAULTLIB : Do not link the default libraries
-:: /ENTRY:main : Use main() as entry point directly (no CRT startup)
+:: /ENTRY:launcher_main : Use launcher_main() as entry point directly (no CRT startup)
 :: /Brepro : Deterministic (reproducible) output
 :: ucrt.lib : Link only against Universal CRT (no vcruntime dependency)
 
-cl pylauncher.c /Fe:pylauncher.exe /O1 /GS- /link /NODEFAULTLIB /ENTRY:main /MACHINE:X64 /Brepro ucrt.lib kernel32.lib
+cl pylauncher.c /Fe:pylauncher.exe /O1 /GS- /link /NODEFAULTLIB /ENTRY:launcher_main /MACHINE:X64 /Brepro ucrt.lib kernel32.lib
