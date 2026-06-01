580e462d98a36daca5e2de06442c605909ffa88a
Kleis Auke Wolthuizen <github@kleisauke.nl>
2026-06-01 18:13:05 +0200

    Add stub for `pause()` syscall in standalone mode (#27038)
    Fixes: #27036.

--- system/lib/standalone/standalone.c
+++ system/lib/standalone/standalone.c
@@ -115,6 +115,10 @@ weak int __syscall_newfstatat(int dirfd, intptr_t path, intptr_t buf, int flags)
   return -ENOSYS;
 }
 
+weak int __syscall_pause() {
+  return -ENOSYS;
+}
+
 weak int __syscall_lstat64(intptr_t path, intptr_t buf) {
   return -ENOSYS;
 }
