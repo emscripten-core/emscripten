adba75eb18b21a28a17bb0601cd14895d3a5d36c
Thomas Steiner <tomac@google.com>
2026-06-10 18:41:17 +0200

    fix: copyright year and remove redundant build comment from main.cpp
    Build instructions belong in README.md, not the source file.
    Copyright year corrected to 2026. Per reviewer feedback.

--- test/cross_origin_storage/main.cpp
+++ test/cross_origin_storage/main.cpp
@@ -1,21 +1,7 @@
-// Copyright 2025 The Emscripten Authors.  All rights reserved.
+// Copyright 2026 The Emscripten Authors.  All rights reserved.
 // Emscripten is available under two separate licenses, the MIT license and the
 // University of Illinois/NCSA Open Source License.  Both these licenses can be
 // found in the LICENSE file.
-
-// Example: Cross-Origin Storage (COS) integration
-//
-// Build with:
-//   emcc main.cpp -o index.js \
-//       -O2 \
-//       -sCROSS_ORIGIN_STORAGE \
-//       -sENVIRONMENT=web \
-//       -sEXPORTED_RUNTIME_METHODS=ccall \
-//       -sEXPORTED_FUNCTIONS=_greet \
-//       -sALLOW_MEMORY_GROWTH
-//
-// Serve the directory over HTTP (e.g. `emrun .` or `python3 -m http.server`)
-// and open index.html in a browser that has the COS extension installed.
 
 #include <stdio.h>
 #include <emscripten/emscripten.h>
