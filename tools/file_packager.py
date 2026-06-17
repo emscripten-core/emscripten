7c1fecaded0e6753b4e87965446111c6841f0f61
Sam Clegg <sbc@chromium.org>
2026-06-17 12:22:21 -0700

    Fix late preRun additions in file_packager (separate metadata)
    In the separate-metadata case, file preloading happens asynchronously
    after the metadata is fetched. Previously, this would result in
    `runWithFS` being pushed to `Module['preRun']` after `preRun()` had
    already been called, meaning the files were never actually preloaded.
    
    Instead of waiting for `preRun` (which has already passed), we can run
    `runWithFS` immediately as long as the FS library has loaded. We now
    check for `Module['FS_createPath']` to detect if the library is loaded
    and ready, rather than checking `Module['isInitialized']` (which is
    only true after runtime initialization, which is too late).
    
    This allows us to remove the `isInitialized` helper property entirely,
    as it is no longer needed.
    
    Also, improve `consumedModuleProp` to detect late modifications to
    `Module['preRun']` (using a Proxy) to prevent similar issues in the future.

--- tools/file_packager.py
+++ tools/file_packager.py
@@ -1033,7 +1033,8 @@ def generate_preload_js(data_target, data_files, metadata):
   ret += code
   ret += '''
     }
-    if (Module['isInitialized']?.()) {
+    // Detect whether the module JS file has already been loaded.
+    if (Module['FS_createPath']) {
       runWithFS(Module)%s;
     } else {
       if (!Module['preRun']) Module['preRun'] = [];
@@ -1069,7 +1070,8 @@ def generate_preload_js(data_target, data_files, metadata):
     loadPackage(json);
   }
 
-  if (Module['isInitialized']?.()) {
+  // Detect whether the module JS file has already been loaded.
+  if (Module['FS_createPath']) {
     runMetaWithFS();
   } else {
     if (!Module['preRun']) Module['preRun'] = [];
