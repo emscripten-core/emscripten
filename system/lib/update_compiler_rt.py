10d8b6defdac95ef6f22282d3ac20acd40f95dbe
Heejin Ahn <aheejin@gmail.com>
2026-07-03 23:16:38 -0700

    Factor out common methods from update_***.py (NFC) (#27252)

--- system/lib/update_compiler_rt.py
+++ system/lib/update_compiler_rt.py
@@ -9,9 +9,8 @@ import os
 import sys
 import shutil
 
-script_dir = os.path.abspath(os.path.dirname(__file__))
-emscripten_root = os.path.dirname(os.path.dirname(script_dir))
-default_llvm_dir = os.path.join(os.path.dirname(emscripten_root), 'llvm-project')
+from update_common import *
+
 local_src = os.path.join(script_dir, 'compiler-rt')
 
 copy_dirs = [
@@ -30,26 +29,8 @@ copy_dirs = [
 preserve_files = ('readme.txt',)
 
 
-def clear(dirname):
-  for f in os.listdir(dirname):
-    if f in preserve_files or 'emscripten' in f:
-      continue
-    full = os.path.join(dirname, f)
-    if os.path.isdir(full):
-      shutil.rmtree(full)
-    else:
-      os.remove(full)
-
-
 def main():
-  if len(sys.argv) > 1:
-    llvm_dir = os.path.join(os.path.abspath(sys.argv[1]))
-  else:
-    llvm_dir = default_llvm_dir
-  if not os.path.isdir(llvm_dir):
-    print(f'LLVM directory not found: {llvm_dir}', file=sys.stderr)
-    print(f'Usage: {sys.argv[0]} [llvm_dir]', file=sys.stderr)
-    sys.exit(1)
+  llvm_dir = get_llvm_dir()
   upstream_dir = os.path.join(llvm_dir, 'compiler-rt')
   assert os.path.exists(upstream_dir)
   upstream_src = os.path.join(upstream_dir, 'lib', 'builtins')
@@ -61,7 +42,7 @@ def main():
     srcdir = os.path.join(upstream_dir, *dirname)
     assert os.path.exists(srcdir)
     dest = os.path.join(local_src, *dirname)
-    clear(dest)
+    clean_dir(dest, preserve_files)
     for name in os.listdir(srcdir):
       if name in ('.clang-format', 'CMakeLists.txt', 'README.txt', 'weak_symbols.txt'):
         continue
--- system/lib/update_llvm_libc.py
+++ system/lib/update_llvm_libc.py
@@ -9,9 +9,7 @@ import sys
 import shutil
 import glob
 
-script_dir = os.path.abspath(os.path.dirname(__file__))
-emscripten_root = os.path.dirname(os.path.dirname(script_dir))
-default_llvm_dir = os.path.join(os.path.dirname(emscripten_root), 'llvm-project')
+from update_common import *
 
 preserve_files = ('readme.txt', '__assertion_handler', '__config_site')
 # ryu_long_double_constants.h from libc is unused (and very large)
@@ -46,56 +44,21 @@ libc_exclusion_patterns = [
     'src/strings/str*casecmp_l*',  # locale_t is unsupported in Overlay Mode.
 ]
 
-def clean_dir(dirname):
-  if not os.path.exists(dirname):
-    return
-  for f in os.listdir(dirname):
-    if f in preserve_files or 'emscripten' in f:
-      continue
-    full = os.path.join(dirname, f)
-    if os.path.isdir(full):
-      shutil.rmtree(full)
-    else:
-      os.remove(full)
-
-
-def copy_tree(upstream_dir, local_dir):
-  if not os.path.exists(local_dir):
-    os.makedirs(local_dir)
-  for f in os.listdir(upstream_dir):
-    full = os.path.join(upstream_dir, f)
-    if os.path.isdir(full):
-      shutil.copytree(full, os.path.join(local_dir, f))
-    elif f not in excludes:
-      shutil.copy2(full, os.path.join(local_dir, f))
-  for root, dirs, files in os.walk(local_dir):
-    for f in files:
-      if f in excludes:
-        full = os.path.join(root, f)
-        os.remove(full)
-
 
 def main():
-  if len(sys.argv) > 1:
-    llvm_dir = os.path.abspath(sys.argv[1])
-  else:
-    llvm_dir = default_llvm_dir
-  if not os.path.isdir(llvm_dir):
-    print(f'LLVM directory not found: {llvm_dir}', file=sys.stderr)
-    print(f'Usage: {sys.argv[0]} [llvm_dir]', file=sys.stderr)
-    sys.exit(1)
+  llvm_dir = get_llvm_dir()
   libc_upstream_dir = os.path.join(llvm_dir, 'libc')
   assert os.path.exists(libc_upstream_dir)
   libc_local_dir = os.path.join(script_dir, 'llvm-libc')
 
   for dirname in libc_copy_dirs:
     local_dir = os.path.join(libc_local_dir, dirname)
-    clean_dir(local_dir)
+    clean_dir(local_dir, preserve_files)
 
   for dirname in libc_copy_dirs:
     upstream_dir = os.path.join(libc_upstream_dir, dirname)
     local_dir = os.path.join(libc_local_dir, dirname)
-    copy_tree(upstream_dir, local_dir)
+    copy_tree(upstream_dir, local_dir, excludes)
 
   # Certain llvm-libc files that are incompatible in Emscripten
   for exclusion_pattern in libc_exclusion_patterns:
