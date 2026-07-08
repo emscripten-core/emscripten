22383d7400b93ea4404b72d6cbe0bff837b8dc22
kcang <kengchew.ang@duerrdental.com>
2026-07-08 12:41:21 +0800

    Add openmp library from llvm (#27073)
    Imports the openmp library from LLVM and enables it using the standard
    -fopenmp flag.
    
    Fixes: #13892, #17637

--- tools/cmdline.py
+++ tools/cmdline.py
@@ -78,6 +78,7 @@ class EmccOptions:
   no_entry = False
   no_minify = False
   nodefaultlibs = False
+  openmp = False
   nolibc = False
   nostartfiles = False
   nostdlib = False
@@ -539,6 +540,8 @@ def parse_args(newargs):  # noqa: C901, PLR0912, PLR0915
       else:
         exit_with_error(f'invalid value for --output-eol: `{style}`')
     # Record PTHREADS setting because it controls whether --shared-memory is passed to lld
+    elif arg in {'-fopenmp', '-fopenmp=libomp'}:
+      options.openmp = 1
     elif arg == '-pthread':
       settings.PTHREADS = 1
       # Also set the legacy setting name, in case use JS code depends on it.
