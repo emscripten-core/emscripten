96dc078fc82515784217e84a777198fda985f863
Sam Clegg <sbc@chromium.org>
2026-07-10 15:10:23 -0700

    [test] Use @also_with_proxy_to_pthread on test_fetch_post
    Replace the manual `@parameterized` decorator on `test_fetch_post` in
    `test_browser.py` with `@also_with_proxy_to_pthread`.

--- test/test_browser.py
+++ test/test_browser.py
@@ -4524,12 +4524,9 @@ Module["preRun"] = () => {
     self.btest_exit('fetch/test_fetch_idb_delete.cpp', cflags=['-pthread', '-sFETCH_DEBUG', '-sFETCH', '-sWASM=0', '-sPROXY_TO_PTHREAD'])
 
   @also_with_fetch_streaming
-  @parameterized({
-    '': ([],),
-    'pthread': (['-pthread', '-sPROXY_TO_PTHREAD'],),
-  })
-  def test_fetch_post(self, args):
-    self.btest_exit('fetch/test_fetch_post.c', cflags=['-sFETCH'] + args)
+  @also_with_proxy_to_pthread
+  def test_fetch_post(self):
+    self.btest_exit('fetch/test_fetch_post.c', cflags=['-sFETCH'])
 
   @also_with_fetch_streaming
   def test_fetch_progress(self):
