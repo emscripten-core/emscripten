c0fdf64ecf3363ea2edaae275a614937a3d7d036
webreflection <andrea.giammarchi@gmail.com>
2026-08-18 09:57:39 +0200

    Avoid throwing on blob: created via URL.createObjectURL()

--- src/preamble.js
+++ src/preamble.js
@@ -101,6 +101,12 @@ function _free() {
 #endif // ASSERTIONS
 
 /**
+ * Indicates whether filename is delivered via URL.createObjectURL()
+ * @noinline
+ */
+var isBlobURI = (filename) => filename.startsWith('blob:');
+
+/**
  * Indicates whether filename is delivered via file protocol (as opposed to http/https)
  * @noinline
  */
@@ -685,13 +691,18 @@ async function instantiateAsync(binary, binaryFile, imports) {
       && !isFileURI(binaryFile)
 #endif
 #if ENVIRONMENT_MAY_BE_NODE
-      // Avoid instantiateStreaming() on Node.js environment for now, as while
-      // Node.js v18.1.0 implements it, it does not have a full fetch()
-      // implementation yet.
-      //
-      // Reference:
-      //   https://github.com/emscripten-core/emscripten/pull/16917
-      && !ENVIRONMENT_IS_NODE
+      && (
+        // Avoid instantiateStreaming() on Node.js environment for now, as while
+        // Node.js v18.1.0 implements it, it does not have a full fetch()
+        // implementation yet.
+        //
+        // Reference:
+        //   https://github.com/emscripten-core/emscripten/pull/16917
+        !ENVIRONMENT_IS_NODE
+        // On browsers this check won't be reached but on Node.js it is the only way to avoid
+        // throwing when URL.createObjectURL(blob) has been passed as the binaryFile.
+        || isBlobURI(binaryFile)
+      )
 #endif
 #if ENVIRONMENT_MAY_BE_SHELL
       // Shell environments don't have fetch.
