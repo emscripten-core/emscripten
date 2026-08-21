74f1829105276e77798993db77308cabbc6eaafa
Sam Clegg <sbc@chromium.org>
2026-08-21 12:58:43 -0700

    [CI] Fix test_other after #27579

--- tools/ports/sdl2_image.py
+++ tools/ports/sdl2_image.py
@@ -8,6 +8,8 @@ HASH = '2175d11a90211871f2289c8d57b31fe830e4b46af7361925c2c30cd521c1c677d2ee244f
 
 deps = ['sdl2']
 variants = {
+  'sdl2_image-wasmsjlj': {'SUPPORT_LONGJMP': 'wasm', 'WASM_LEGACY_EXCEPTIONS': 0},
+  'sdl2_image-legacysjlj': {'SUPPORT_LONGJMP': 'wasm', 'WASM_LEGACY_EXCEPTIONS': 1},
   'sdl2_image-jpg':    {'SDL2_IMAGE_FORMATS': ["jpg"]},
   'sdl2_image-png':    {'SDL2_IMAGE_FORMATS': ["png"]},
   'sdl2_image-jpg-mt': {'SDL2_IMAGE_FORMATS': ["jpg"], 'PTHREADS': 1},
