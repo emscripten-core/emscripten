166035d8cd8d631efdbcaad0998235d155eccf76
Sam Clegg <sbc@chromium.org>
2026-09-16 11:18:01 -0700

    Simplify LLVM sanity check. NFC (#27713)
    The CLANG_CC and LLVM_AR variables are already expanded to include the
    .exe component on windows. See find_exe in utils.py.

--- tools/shared.py
+++ tools/shared.py
@@ -366,7 +366,7 @@ def perform_sanity_checks(quiet=False):
 
   with ToolchainProfiler.profile_block('sanity LLVM'):
     for cmd in (CLANG_CC, LLVM_AR):
-      if not os.path.exists(cmd) and not os.path.exists(cmd + '.exe'):  # .exe extension required for Windows
+      if not os.path.exists(cmd):
         exit_with_error('cannot find %s, check the paths in %s', cmd, config.EM_CONFIG)
 
 
