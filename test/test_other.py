fc8da5871fd67b20118953028a7c9d3100d8a004
Heejin Ahn <aheejin@gmail.com>
2026-09-16 00:42:20 +0000

    Don't use closure and run the test

--- test/test_other.py
+++ test/test_other.py
@@ -11862,13 +11862,14 @@ int main(void) {
       ''')
     self.run_process([EMXX, 'src.cpp', '-fexceptions', '--closure=1'])
 
-  def test_exceptions_with_closure_and_separate_compilation(self):
+  def test_exceptions_no_link_flag(self):
     # We compile this code using -fexceptions but link without it. The necessary
     # functions and variables still should be in depending on the existing
     # functions and the compilation should not crash with undeclared variable
     # errors.
     create_file('src.cpp', r'''
       #include <string>
+      #include <stdio.h>
 
       void foo() {
         std::string a = std::to_string(42);
@@ -11885,11 +11886,13 @@ int main(void) {
 
       int main() {
         foo();
-        return bar();
+        printf("%d\n", bar());
+        return 0;
       }
       ''')
     self.run_process([EMXX, '-fexceptions', '-c', 'src.cpp', '-o', 'src.o'])
-    self.run_process([EMXX, 'src.o', '--closure=1', '-o', 'out.js'])
+    self.run_process([EMXX, 'src.o', '-o', 'out.js'])
+    self.assertContained('1', self.run_js('out.js'))
 
   def test_assertions_on_incoming_module_api_changes(self):
     create_file('pre.js', 'Module.read = () => {};')
