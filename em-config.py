9639c92c1940af22fb726db52ed5e478ea4ee4d2
Sam Clegg <sbc@chromium.org>
2026-09-15 17:03:38 -0700

    Improve em-config utility
    - More precise key matching
    - Better error messages

--- em-config.py
+++ em-config.py
@@ -16,20 +16,34 @@ This tool prints the value of the variable to stdout if one
 is found, or exits with 1 if the variable does not exist.
 """
 
-import re
 import sys
 
-from tools import config
+from tools import config, utils
+
+
+def get_config_setting(key):
+  # Special case for EMSCRIPTEN_ROOT, which is not part of `config`
+  if key == 'EMSCRIPTEN_ROOT':
+    return utils.path_from_root()
+
+  if key not in config.CONFIG_KEYS:
+    print(f'Invalid config key: {key}', file=sys.stderr)
+    print('Valid keys:')
+    for k in config.CONFIG_KEYS:
+      print(f'  {k}', file=sys.stderr)
+    sys.exit(1)
+
+  return getattr(config, key)
 
 
 def main():
-  if len(sys.argv) != 2 or \
-    not re.match(r"^[\w\W_][\w\W_\d]*$", sys.argv[1]) or \
-    not hasattr(config, sys.argv[1]):
+  if len(sys.argv) != 2:
     print('Usage: em-config VAR_NAME', file=sys.stderr)
     sys.exit(1)
 
-  print(getattr(config, sys.argv[1]))
+  key = sys.argv[1]
+  value = get_config_setting(key)
+  print(value)
   return 0
 
 
