64b96487dcc334150d9b994ce656f6bceb97720c
Guy Bedford <guybedford@gmail.com>
2026-09-15 21:51:39 -0700

    Guard SOCK_NONBLOCK handling to NODERAWSOCKETS for code size

--- src/lib/libsockfs.js
+++ src/lib/libsockfs.js
@@ -77,7 +77,9 @@ addToLibrary({
         throw new FS.ErrnoError({{{ cDefs.EAFNOSUPPORT }}});
       }
       var flags = {{{ cDefs.O_RDWR }}};
+#if NODERAWSOCKETS
       if (type & {{{ cDefs.SOCK_NONBLOCK }}}) flags |= {{{ cDefs.O_NONBLOCK }}};
+#endif
       type &= ~{{{ cDefs.SOCK_CLOEXEC | cDefs.SOCK_NONBLOCK }}}; // SOCK_CLOEXEC makes no sense for a single process.
       // Emscripten only supports SOCK_STREAM and SOCK_DGRAM
       if (type != {{{ cDefs.SOCK_STREAM }}} && type != {{{ cDefs.SOCK_DGRAM }}}) {
